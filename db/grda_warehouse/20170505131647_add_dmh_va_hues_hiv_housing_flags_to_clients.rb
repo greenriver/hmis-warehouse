15bf9c48a5941e0d2ab0d699a1643278ec50a39f
Elliot Anders <eanders@greenriver.org>
2017-05-08 21:29:37 -0400

    postgres is case sensitive?

--- db/grda_warehouse/20170505131647_add_dmh_va_hues_hiv_housing_flags_to_clients.rb
+++ db/grda_warehouse/20170505131647_add_dmh_va_hues_hiv_housing_flags_to_clients.rb
@@ -1,17 +1,17 @@
 class AddDmhVaHuesHivHousingFlagsToClients < ActiveRecord::Migration
   def up
-    add_column :client, :dmh_eligible, :boolean, default: false, null: false, index: true
-    add_column :client, :va_eligible, :boolean, default: false, null: false, index: true
-    add_column :client, :hues_eligible, :boolean, default: false, null: false, index: true
-    add_column :client, :hiv_positive, :boolean, default: false, null: false, index: true
-    add_column :client, :housing_release_status, :string, index: true
+    add_column :Client, :dmh_eligible, :boolean, default: false, null: false, index: true
+    add_column :Client, :va_eligible, :boolean, default: false, null: false, index: true
+    add_column :Client, :hues_eligible, :boolean, default: false, null: false, index: true
+    add_column :Client, :hiv_positive, :boolean, default: false, null: false, index: true
+    add_column :Client, :housing_release_status, :string, index: true
   end
 
   def down
-    remove_column :client, :dmh_eligible, :boolean, default: false, null: false, index: true
-    remove_column :client, :va_eligible, :boolean, default: false, null: false, index: true
-    remove_column :client, :hues_eligible, :boolean, default: false, null: false, index: true
-    remove_column :client, :hiv_positive, :boolean, default: false, null: false, index: true
-    remove_column :client, :housing_release_status, :string, index: true
+    remove_column :Client, :dmh_eligible, :boolean, default: false, null: false, index: true
+    remove_column :Client, :va_eligible, :boolean, default: false, null: false, index: true
+    remove_column :Client, :hues_eligible, :boolean, default: false, null: false, index: true
+    remove_column :Client, :hiv_positive, :boolean, default: false, null: false, index: true
+    remove_column :Client, :housing_release_status, :string, index: true
   end
 end
