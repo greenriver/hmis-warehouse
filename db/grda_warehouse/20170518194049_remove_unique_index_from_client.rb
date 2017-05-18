0018d6f878432e680920ff50d4ea863dfae600fb
Elliot Anders <eanders@greenriver.org>
2017-05-18 16:16:53 -0400

    removing unneccessary and incorrect index from client table

--- db/grda_warehouse/20170518194049_remove_unique_index_from_client.rb
+++ db/grda_warehouse/20170518194049_remove_unique_index_from_client.rb
@@ -2,28 +2,14 @@ class RemoveUniqueIndexFromClient < ActiveRecord::Migration
   TABLES = [   # all the tables I found which had PersonalID
     GrdaWarehouse::Hud::Client,
   ]
-
-  COLS = %w( data_source_id PersonalID )
-
-  def down
-    TABLES.each do |m|
-      idxes = m.connection.indexes(m.table_name)
-      existing = idxes.select{ |idx| ( idx.columns & COLS ).length == COLS.length }
-      next if existing.any?
-      add_index m.table_name, COLS, name: christen(m)
-    end
-  end
-
   def up
     TABLES.each do |m|
-      n = christen(m)
       idxes = m.connection.indexes(m.table_name)
-      next unless idxes.any?{ |idx| idx.name == n && ( idx.columns & COLS ).length == COLS.length }
-      remove_index m.table_name, name: n
+      existing = idxes.select{ |idx| idx.name == 'unk_Client' }
+      if existing.any?
+        remove_index m.table_name, name: 'unk_Client'
+      end
     end
   end
 
-  def christen(m)
-    "index_#{m.table_name}_on_#{COLS.to_sentence.gsub(/[, ]/, '_')}"
-  end
 end
