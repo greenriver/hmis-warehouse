e4e2e02ffad10289a5d860d74ff54bb28677d0a5
Elliot Anders <eanders@greenriver.org>
2017-05-22 12:44:40 -0400

    Arel cleanup of DQ reports for postgres

--- app/models/report_generators/data_quality/fy2016/q5.rb
+++ app/models/report_generators/data_quality/fy2016/q5.rb
@@ -274,24 +274,28 @@ module ReportGenerators::DataQuality::Fy2016
 
 
     def fetch_clients(project_types)
+      et = GrdaWarehouse::Hud::Enrollment.arel_table
+      ct = GrdaWarehouse::Hud::Client.arel_table
+      sh_t = GrdaWarehouse::ServiceHistory.arel_table
       columns = {
-        client_id: :client_id, 
-        age: :age,
-        DOB: :DOB,
+        client_id: sh_t[:client_id].as('client_id').to_sql, 
+        age: sh_t[:age].as('age').to_sql,
+        DOB: ct[:DOB].as('DOB').to_sql,
         project_type: act_as_project_overlay, 
-        project_id: :project_id, 
-        data_source_id: :data_source_id,
-        first_date_in_program: :first_date_in_program,
-        last_date_in_program: :last_date_in_program,
-        project_name: :project_name,
-        destination: :destination,
-        enrollment_group_id: :enrollment_group_id,
-        ResidencePrior: :ResidencePrior,
-        ResidencePriorLengthOfStay: :ResidencePriorLengthOfStay,
-        DateToStreetESSH: :DateToStreetESSH,
-        TimesHomelessPastThreeYears: :TimesHomelessPastThreeYears,
-        MonthsHomelessPastThreeYears: :MonthsHomelessPastThreeYears,
-        PreviousStreetESSH: :PreviousStreetESSH,
+        project_id: sh_t[:project_id].as('project_id').to_sql,
+        data_source_id: sh_t[:data_source_id].as('data_source_id').to_sql,
+        first_date_in_program: sh_t[:first_date_in_program].as('first_date_in_program').to_sql,
+        last_date_in_program: sh_t[:last_date_in_program].as('last_date_in_program').to_sql,
+        project_name: sh_t[:project_name].as('project_name').to_sql,
+        destination: sh_t[:destination].as('destination').to_sql,
+        enrollment_group_id: sh_t[:enrollment_group_id].as('enrollment_group_id').to_sql, 
+        ResidencePrior: et[:ResidencePrior].as('ResidencePrior').to_sql,
+        ResidencePriorLengthOfStay: et[:ResidencePriorLengthOfStay].as('ResidencePriorLengthOfStay').to_sql,
+        DateToStreetESSH: et[:DateToStreetESSH].as('DateToStreetESSH').to_sql,
+        TimesHomelessPastThreeYears: et[:TimesHomelessPastThreeYears].as('TimesHomelessPastThreeYears').to_sql,
+        MonthsHomelessPastThreeYears: et[:MonthsHomelessPastThreeYears].as('MonthsHomelessPastThreeYears').to_sql,
+        MonthsHomelessPastThreeYears: et[:MonthsHomelessPastThreeYears].as('MonthsHomelessPastThreeYears').to_sql,
+        PreviousStreetESSH: et[:PreviousStreetESSH].as('PreviousStreetESSH').to_sql,
       }
       
       all_client_scope.
