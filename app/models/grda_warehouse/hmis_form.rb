8c057109cd0e10f9444d5b44fa23f4e5a59ef858
Elliot Anders <eanders@greenriver.org>
2017-05-22 09:55:57 -0400

    Fixed serialization of hmis forms

--- app/models/grda_warehouse/hmis_form.rb
+++ app/models/grda_warehouse/hmis_form.rb
@@ -1,7 +1,7 @@
 class GrdaWarehouse::HmisForm < GrdaWarehouseBase
   belongs_to :client, class_name: GrdaWarehouse::Hud::Client.name
-  serialize :response, JSON
-  serialize :answers, JSON
+  serialize :response, Hash
+  serialize :answers, Hash
 
   scope :hud_assessment, -> { where name: 'HUD Assessment (Entry/Update/Annual/Exit)' }
   scope :triage, -> { where name: 'Triage Assessment'}
