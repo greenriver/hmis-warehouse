3b109bf10d853e17ceffc019b4937b0cc27a1785
Elliot Anders <eanders@greenriver.org>
2017-05-25 11:10:14 -0400

    Adding delayed job for capistrano

--- config/deploy/aws_production.rb
+++ config/deploy/aws_production.rb
@@ -3,13 +3,19 @@ set :rails_env, "production"
 
 raise "You must specify DEPLOY_USER" if ENV['DEPLOY_USER'].to_s == ''
 
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Delayed Job
+set :delayed_job_workers, 4
+set :delayed_job_prefix, 'hmis'
+set :delayed_job_roles, [:job]
+
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
 
 puts "Allowable hosts: #{ENV['HOSTS']}"
 puts "Hosts specified for deployment: #{ENV['HOST1']} #{ENV['HOST2']}"
 
 server ENV['HOST1'], user: ENV['DEPLOY_USER'], roles: %w{app db web}
-server ENV['HOST2'], user: ENV['DEPLOY_USER'], roles: %w{app web}
+server ENV['HOST2'], user: ENV['DEPLOY_USER'], roles: %w{app web job}
 
 set :linked_dirs, fetch(:linked_dirs, []).push('certificates', 'key', '.well_known', 'challenge')
 set :linked_files, fetch(:linked_files, []).push('config/letsencrypt_plugin.yml')
