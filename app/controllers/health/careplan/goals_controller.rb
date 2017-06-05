fe9b91eebdf5e0816c2c5e736058756ba7d3f72a
Elliot Anders <eanders@greenriver.org>
2017-06-05 16:56:18 -0400

    Some cleanup of care plan design

--- app/controllers/health/careplan/goals_controller.rb
+++ app/controllers/health/careplan/goals_controller.rb
@@ -1,5 +1,5 @@
-module Health::Careplan
-  class GoalsController < ::Window::Health::Careplan::GoalsController
+module Health
+  class Careplan::GoalsController < ::Window::Health::Careplan::GoalsController
     include ClientPathGenerator
     def create_success_path 
       client_health_careplan_path(client_id: @client.id)
