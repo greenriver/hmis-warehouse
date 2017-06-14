67b199f972efe1e618f9136040e8da960afd1936
Elliot Anders <eanders@greenriver.org>
2017-06-14 16:09:59 -0400

    Adding detail to veteran exit details

---
+++ app/controllers/warehouse_reports/veteran_details/exits_controller.rb
@@ -0,0 +1,65 @@
+module WarehouseReports::VeteranDetails
+  class ExitsController < ApplicationController
+    include ArelHelper
+    before_action :require_can_view_reports!
+    before_action :require_can_view_clients!
+
+    def index
+      date_range_options = params.permit(range: [:start, :end])[:range]
+      # Also handle month based requests from javascript
+      if date_range_options.blank?
+        month = params.permit(:month)
+        @range = DateRange.new(
+          start: Date.strptime(month[:month], "%B %Y").beginning_of_month,
+          end: Date.strptime(month[:month], "%B %Y").end_of_month,
+        )
+      else
+        @range = DateRange.new(date_range_options)
+      end
+      columns = {
+        client_id: sh_t[:client_id].as('client_id').to_sql,
+        date: sh_t[:date].as('date').to_sql, 
+        destination: sh_t[:destination].as('destination').to_sql, 
+        first_name: c_t[:FirstName].as('first_name').to_sql, 
+        last_name: c_t[:LastName].as('last_name').to_sql,
+        project_name: sh_t[:project_name].as('project_name').to_sql,
+      }
+      @buckets = Hash.new(0)
+      @clients = exits_from_homelessness.
+        ended_between(start_date: @range.start, end_date: @range.end).
+        order(date: :asc).
+        pluck(*columns.values).
+        map do |row|
+          Hash[columns.keys.zip(row)]
+        end.each do |row|
+          row[:destination] = 99 unless HUD.valid_destinations.keys.include?(row[:destination])
+          @buckets[row[:destination]] += 1
+        end
+    end
+
+     def exits_from_homelessness
+      GrdaWarehouse::ServiceHistory.exit.
+        joins(:client).
+        where(
+          project_type: GrdaWarehouse::Hud::Project::HOMELESS_PROJECT_TYPES
+        ).
+        where(client_id: client_source)
+    end
+
+    def client_source
+      GrdaWarehouse::Hud::Client.destination.veteran
+    end
+
+    def sh_t
+      GrdaWarehouse::ServiceHistory.arel_table
+    end
+
+    def c_t
+      GrdaWarehouse::Hud::Client.arel_table
+    end
+
+    def p_t
+      GrdaWarehouse::Hud::Project.arel_table
+    end
+  end
+end
