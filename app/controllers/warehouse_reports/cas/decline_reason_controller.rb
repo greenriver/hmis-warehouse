c628f361d6775cea48ee263831eafdd781317e01
dfhoughton <dfhoughton@gmail.com>
2017-06-16 11:27:04 -0400

    [pt#145231367] decline reason report

--- app/controllers/warehouse_reports/cas/decline_reason_controller.rb
+++ app/controllers/warehouse_reports/cas/decline_reason_controller.rb
@@ -3,6 +3,8 @@ module WarehouseReports::Cas
     before_action :require_can_view_reports!
 
     def index
+      @reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil ).group(:decline_reason).count
+      @reasons.sort_by(&:last).reverse
     end
   end
 end
