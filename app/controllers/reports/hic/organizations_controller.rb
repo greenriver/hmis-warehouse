aaf02d96c8a7708d0fcdf63c18f00db1e3adf858
dfhoughton <dfhoughton@gmail.com>
2017-06-21 11:34:07 -0400

    [pt#147449987] scope the organizations controller

--- app/controllers/reports/hic/organizations_controller.rb
+++ app/controllers/reports/hic/organizations_controller.rb
@@ -3,7 +3,7 @@ module Reports
 
     def show
       pt = GrdaWarehouse::Hud::Project.arel_table
-      @organizations = GrdaWarehouse::Hud::Organization.joins(:projects).
+      @organizations = organization_scope.joins(:projects).
         where((pt[:ProjectType].in(PROJECT_TYPES).
           and(pt[:act_as_project_type].eq(nil))).
           or(pt[:act_as_project_type].in(PROJECT_TYPES))).
@@ -12,6 +12,10 @@ module Reports
         format.html
         format.csv { send_data GrdaWarehouse::Hud::Organization.to_csv(scope: @organizations), filename: "organization-#{Time.now}.csv" }
       end
+    end
+
+    def organization_scope
+      GrdaWarehouse::Hud::Organization.viewable_by current_user
     end
   end
 end
