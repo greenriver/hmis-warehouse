412d56734635008e19412fd257a5395971fc759e
Elliot Anders <eanders@greenriver.org>
2017-06-22 09:08:36 -0400

    Filtering by date for CAS decline reasons report

--- app/controllers/warehouse_reports/cas/decline_reason_controller.rb
+++ app/controllers/warehouse_reports/cas/decline_reason_controller.rb
@@ -6,13 +6,13 @@ module WarehouseReports::Cas
       date_range_options = params.permit(range: [:start, :end])[:range]
       unless date_range_options.present?
         date_range_options = {
-          start: 1.month.ago,
-          end: 13.months.ago,
+          start: 13.month.ago.to_date,
+          end: 1.months.ago.to_date,
         }
       end
       @range = DateRange.new(date_range_options)
       @raw_reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil ).
-        started_between(start_date: @range.start, end_date: @range.end)
+        started_between(start_date: @range.start, end_date: @range.end + 1.day)
       @reasons = @raw_reasons.map{|row| row[:decline_reason].gsub(/Other:.*/,'Other').strip}.
         each_with_object(Hash.new(0)) { |reason,counts| counts[reason] += 1 }
 
