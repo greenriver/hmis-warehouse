f20289702599b3dbd99c74affc21ec7e660af9dd
Elliot Anders <eanders@greenriver.org>
2017-06-22 09:07:46 -0400

    Filtering by date for CAS decline reasons report

--- app/controllers/warehouse_reports/cas/decline_reason_controller.rb
+++ app/controllers/warehouse_reports/cas/decline_reason_controller.rb
@@ -4,13 +4,20 @@ module WarehouseReports::Cas
 
     def index
       date_range_options = params.permit(range: [:start, :end])[:range]
+      unless date_range_options.present?
+        date_range_options = {
+          start: 1.month.ago,
+          end: 13.months.ago,
+        }
+      end
       @range = DateRange.new(date_range_options)
-      @raw_reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil )
+      @raw_reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil ).
+        started_between(start_date: @range.start, end_date: @range.end)
       @reasons = @raw_reasons.map{|row| row[:decline_reason].gsub(/Other:.*/,'Other').strip}.
         each_with_object(Hash.new(0)) { |reason,counts| counts[reason] += 1 }
 
       @reasons.sort_by(&:last).reverse
-      # placeholder
+      
       respond_to do |format|
         format.html
         format.xlsx
--- app/models/grda_warehouse/cas_report.rb
+++ app/models/grda_warehouse/cas_report.rb
@@ -6,12 +6,16 @@ module GrdaWarehouse
     end
     belongs_to :client, class_name: 'GrdaWarehouse::Hud::Client', inverse_of: :cas_reports
 
+    scope :started_between, -> (start_date:, end_date:) do
+      where(match_started_at: (start_date..end_date))
+    end
+
     def self.reason_attributes
       {
         client_id: 'Client',
         match_id: 'Match',
         decline_reason: 'Decline Reason',
-        created_at: 'Match Created',
+        match_started_at: 'Match Started',
       }
     end
 
