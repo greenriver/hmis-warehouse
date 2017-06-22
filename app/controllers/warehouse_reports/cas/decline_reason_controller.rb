eaf39c67240a3707f951c53174cc40b3d4ff88c4
Elliot Anders <eanders@greenriver.org>
2017-06-22 08:53:51 -0400

    Simplified CAS decline report, more details in download

--- app/controllers/warehouse_reports/cas/decline_reason_controller.rb
+++ app/controllers/warehouse_reports/cas/decline_reason_controller.rb
@@ -3,7 +3,12 @@ module WarehouseReports::Cas
     before_action :require_can_view_reports!
 
     def index
-      @reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil ).group(:decline_reason).count
+      date_range_options = params.permit(range: [:start, :end])[:range]
+      @range = DateRange.new(date_range_options)
+      @raw_reasons = GrdaWarehouse::CasReport.where.not( decline_reason: nil )
+      @reasons = @raw_reasons.map{|row| row[:decline_reason].gsub(/Other:.*/,'Other').strip}.
+        each_with_object(Hash.new(0)) { |reason,counts| counts[reason] += 1 }
+
       @reasons.sort_by(&:last).reverse
       # placeholder
       respond_to do |format|
--- app/models/grda_warehouse/cas_report.rb
+++ app/models/grda_warehouse/cas_report.rb
@@ -5,5 +5,15 @@ module GrdaWarehouse
       true
     end
     belongs_to :client, class_name: 'GrdaWarehouse::Hud::Client', inverse_of: :cas_reports
+
+    def self.reason_attributes
+      {
+        client_id: 'Client',
+        match_id: 'Match',
+        decline_reason: 'Decline Reason',
+        created_at: 'Match Created',
+      }
+    end
+
   end
 end
