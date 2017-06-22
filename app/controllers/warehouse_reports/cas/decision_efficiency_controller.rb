0cc6b940e9318bc4bcfd519774390a1eab4b424e
Elliot Anders <eanders@greenriver.org>
2017-06-22 11:06:25 -0400

    Filtering on CAS decision efficiency report

--- app/controllers/warehouse_reports/cas/decision_efficiency_controller.rb
+++ app/controllers/warehouse_reports/cas/decision_efficiency_controller.rb
@@ -11,7 +11,19 @@ module WarehouseReports::Cas
       render json: @data.to_a
     end
 
+    def date_range_options
+      options = params.permit(range: [:start, :end])[:range]
+      unless options.present?
+        options = {
+          start: 23.month.ago.to_date,
+          end: Date.today,# 1.months.ago.to_date,
+        }
+      end
+      options
+    end
+
     private def load_vars
+      @range = DateRange.new(date_range_options)
       @step_range = StepRange.new(step_params)
       @data = step_time_histogram(@step_range) if @step_range.first.present?
     end
@@ -43,7 +55,8 @@ module WarehouseReports::Cas
       at = GrdaWarehouse::CasReport.arel_table
       at2 = Arel::Table.new at.table_name
       at2.table_alias = 'at2'
-      query = at.join(at2).on(
+      query = at.where(at[:match_started_at].between(@range.start..@range.end+1.day)).
+        join(at2).on(
         at[:client_id].eq(at2[:client_id]).and(
           at[:match_id].eq at2[:match_id]
         ).and(
@@ -51,10 +64,12 @@ module WarehouseReports::Cas
         ).and(
           at2[:match_step].eq second_step
         )
-      ).project(
+      ).where(at2[:match_started_at].between(@range.start..@range.end+1.day)).
+      project(
         seconds_diff( at.engine, at2[:updated_at], at[:updated_at] )
       )
       times = at.engine.connection.select_rows(query.to_sql).flatten.map(&:to_f).map{ |i| ( i / divisor ).round.to_i }
+      return {} if times.empty?
       min, max = times.minmax
       histogram = times.group_by(&:itself).map{ |v,ar| [ v, ar.length ] }.to_h
       (min..max).each{ |v| histogram[v] ||= 0 }
@@ -77,7 +92,7 @@ module WarehouseReports::Cas
       # hash from steps to steps that may follow them
       def ordered_steps
         @ordered_steps ||= begin
-          scope = GrdaWarehouse::CasReport
+          scope = GrdaWarehouse::CasReport#.started_between(start_date: @range.start, end_date: @range.end + 1.day)
           steps = scope.uniq.order(:match_step).pluck :match_step
           at = scope.arel_table
           at2 = Arel::Table.new at.table_name
