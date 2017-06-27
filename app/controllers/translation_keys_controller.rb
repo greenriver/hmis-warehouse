5d6d797cedd4bd136a1c181cd56bdd7729a64f8f
everestwitman <everesttwitman@gmail.com>
2017-06-27 10:48:35 -0400

    views and controllers for translation db

---
+++ app/controllers/translation_keys_controller.rb
@@ -0,0 +1,77 @@
+# class TranslationKeysController < ApplicationController
+#   before_filter :authenticate
+#   before_filter :find_translation_key, :only=>%w[show edit update destroy]
+# 
+#   def index
+#     @translation_keys = TranslationKey.find(:all)
+#   end
+# 
+#   def new
+#     @translation_key = TranslationKey.new
+#     add_default_locales_to_translation
+#     render :action=>:edit
+#   end
+# 
+#   def create
+#     @translation_key = TranslationKey.new(params[:translation_key])
+#     if @translation_key.save
+#       flash[:notice] = 'Created!'
+#       redirect_to translation_key_path(@translation_key)
+#     else
+#       flash[:error] = 'Failed to save!'
+#       render :action=>:edit
+#     end
+#   end
+# 
+#   def show
+#     add_default_locales_to_translation
+#     render :action=>:edit
+#   end
+# 
+#   def edit
+#   end
+# 
+#   def update
+#     if @translation_key.update_attributes(params[:translation_key])
+#       flash[:notice] = 'Saved!'
+#       redirect_to @translation_key
+#     else
+#       flash[:error] = 'Failed to save!'
+#       render :action=>:edit
+#     end
+#   end
+# 
+#   def destroy
+#     @translation_key.destroy
+#     redirect_to translation_keys_path
+#   end
+# 
+#   protected
+# 
+#   def self.tbe_config
+#     @@tbe_config ||= YAML::load(File.read(Rails.root.join('config','translation_db_engine.yml'))).with_indifferent_access rescue {}
+#   end
+# 
+#   def choose_layout
+#     self.class.tbe_config[:layout] || 'application'
+#   end
+# 
+#   def authenticate
+#     return unless auth = self.class.tbe_config[:auth]
+#     authenticate_or_request_with_http_basic do |username, password|
+#       username == auth[:name] && password == auth[:password]
+#     end
+#   end
+# 
+#   def find_translation_key
+#     @translation_key = TranslationKey.find(params[:id])
+#   end
+# 
+#   def add_default_locales_to_translation
+#     existing_translations = @translation_key.translations.map(&:locale)
+#     missing_translations = TranslationKey.available_locales.map(&:to_sym) - existing_translations.map(&:to_sym)
+#     missing_translations.each do |locale|
+#       @translation_key.translations.build(:locale => locale)
+#     end
+#   end
+# end
---
+++ config/initializers/fast_gettext.rb
@@ -0,0 +1,15 @@
+# return
+# # db access is cached <-> only first lookup hits the db
+# require "fast_gettext/translation_repository/db"
+# FastGettext::TranslationRepository::Db.require_models #load and include default models
+# 
+# FastGettext.add_text_domain(
+#   'database_warehouse', 
+#   :path => 'locale', 
+#   :type => :db,
+#   :model => TranslationKey,
+#   :ignore_fuzzy => true,
+#   # :report_warning => false
+# )
+# FastGettext.default_available_locales = ['en', 'es', 'pt', 'ind', 'fr']
+# FastGettext.default_text_domain = 'starcafe'
