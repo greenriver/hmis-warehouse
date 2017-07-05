e1adef45d07091d39e60ceadc5f1b67de9f31985
everestwitman <everesttwitman@gmail.com>
2017-07-05 13:05:55 -0400

    Overwrite default styles with all styles in assets/stylesheets/custom_styles/

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( print.css )
 Rails.application.config.assets.precompile += %w( chart.js )
+Rails.application.config.assets.precompile += %w( custom_styles/* )
