06530567a887e37f190f84dad93f14e9b1097d54
Todd Blackman <tdblackman@gmail.com>
2017-07-11 12:22:49 -0400

    wip

--- db/warehouse/migrate/20160927202506_create_hmis_answer_table.rb
+++ db/warehouse/migrate/20160927202506_create_hmis_answer_table.rb
@@ -1,11 +1,11 @@
 class CreateHmisAnswerTable < ActiveRecord::Migration
   def change
-    table_name = :hmis_answers
-    create_table table_name do |t|
+    table_nameo = :hmis_answers
+    create_table table_nameo do |t|
       t.integer :assessment_id, null: false
       t.integer :question_id, null: false
       t.string :text
     end
-    add_index table_name, [:assessment_id, :question_id], unique: true
+    add_index table_nameo, [:assessment_id, :question_id], unique: true
   end
 end
