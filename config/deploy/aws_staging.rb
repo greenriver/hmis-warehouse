5dcc2451f9c1b785908f86dfb41a348bf7d0529f
Elliot Anders <eanders@greenriver.org>
2017-07-18 12:37:12 -0400

    Adding site name to delayed job scope

--- config/deploy/aws_staging.rb
+++ config/deploy/aws_staging.rb
@@ -4,7 +4,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Delayed Job
 set :delayed_job_workers, 2
-set :delayed_job_prefix, 'hmis'
+set :delayed_job_prefix, "#{ENV['CLIENT']}-hmis"
 set :delayed_job_roles, [:job]
 
 server ENV['STAGING_HOST'], user: 'ubuntu', roles: %w{app db web job}
