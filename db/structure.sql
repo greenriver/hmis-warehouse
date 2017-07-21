13bcdb4cdbbc6449c5d9259f175a1f7645552122
Elliot Anders <eanders@greenriver.org>
2017-07-21 10:34:58 -0400

    We are still using lets encrypt on staging

--- db/structure.sql
+++ db/structure.sql
@@ -138,29 +138,10 @@ CREATE TABLE client_service_history (
 
 CREATE TABLE clients_unduplicated (
     id integer NOT NULL,
-    client_unique_id character varying NOT NULL,
-    unduplicated_client_id integer NOT NULL,
+    client_unique_id character varying,
+    unduplicated_client_id integer,
     dc_id integer
 );
-
-
---
--- Name: clients_unduplicated_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE clients_unduplicated_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: clients_unduplicated_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE clients_unduplicated_id_seq OWNED BY clients_unduplicated.id;
 
 
 --
@@ -203,6 +184,25 @@ ALTER SEQUENCE delayed_jobs_id_seq OWNED BY delayed_jobs.id;
 
 
 --
+-- Name: hud_performance_unduplicated_clients_new_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE hud_performance_unduplicated_clients_new_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: hud_performance_unduplicated_clients_new_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE hud_performance_unduplicated_clients_new_id_seq OWNED BY clients_unduplicated.id;
+
+
+--
 -- Name: imports; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -237,6 +237,68 @@ CREATE SEQUENCE imports_id_seq
 --
 
 ALTER SEQUENCE imports_id_seq OWNED BY imports.id;
+
+
+--
+-- Name: letsencrypt_plugin_challenges; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE letsencrypt_plugin_challenges (
+    id integer NOT NULL,
+    response text,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: letsencrypt_plugin_challenges_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE letsencrypt_plugin_challenges_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: letsencrypt_plugin_challenges_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE letsencrypt_plugin_challenges_id_seq OWNED BY letsencrypt_plugin_challenges.id;
+
+
+--
+-- Name: letsencrypt_plugin_settings; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE letsencrypt_plugin_settings (
+    id integer NOT NULL,
+    private_key text,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: letsencrypt_plugin_settings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE letsencrypt_plugin_settings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: letsencrypt_plugin_settings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE letsencrypt_plugin_settings_id_seq OWNED BY letsencrypt_plugin_settings.id;
 
 
 --
@@ -391,30 +453,31 @@ CREATE TABLE roles (
     can_view_clients boolean DEFAULT false,
     can_edit_clients boolean DEFAULT false,
     can_view_reports boolean DEFAULT false,
-    can_view_censuses boolean DEFAULT false,
-    can_view_census_details boolean DEFAULT false,
     can_edit_users boolean DEFAULT false,
     can_view_full_ssn boolean DEFAULT false,
     can_view_full_dob boolean DEFAULT false,
-    can_view_hiv_status boolean DEFAULT false,
-    can_view_dmh_status boolean DEFAULT false,
     can_view_imports boolean DEFAULT false,
     can_edit_roles boolean DEFAULT false,
+    can_view_censuses boolean DEFAULT false,
+    can_view_census_details boolean DEFAULT false,
     can_view_projects boolean DEFAULT false,
-    can_edit_projects boolean DEFAULT false,
-    can_edit_project_groups boolean DEFAULT false,
     can_view_organizations boolean DEFAULT false,
-    can_edit_organizations boolean DEFAULT false,
-    can_edit_data_sources boolean DEFAULT false,
     can_view_client_window boolean DEFAULT false,
     can_upload_hud_zips boolean DEFAULT false,
-    can_edit_translations boolean DEFAULT false,
-    can_manage_assessments boolean DEFAULT false,
-    can_edit_anything_super_user boolean DEFAULT false,
+    can_view_hiv_status boolean DEFAULT false,
+    can_view_dmh_status boolean DEFAULT false,
+    health_role boolean DEFAULT false NOT NULL,
     can_administer_health boolean DEFAULT false,
     can_edit_client_health boolean DEFAULT false,
     can_view_client_health boolean DEFAULT false,
-    health_role boolean DEFAULT false NOT NULL
+    can_edit_project_groups boolean DEFAULT false,
+    can_view_everything boolean DEFAULT false,
+    can_edit_anything_super_user boolean DEFAULT false,
+    can_edit_projects boolean DEFAULT false,
+    can_edit_organizations boolean DEFAULT false,
+    can_edit_data_sources boolean DEFAULT false,
+    can_manage_assessments boolean DEFAULT false,
+    can_edit_translations boolean DEFAULT false
 );
 
 
@@ -755,7 +818,7 @@ ALTER TABLE ONLY activity_logs ALTER COLUMN id SET DEFAULT nextval('activity_log
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY clients_unduplicated ALTER COLUMN id SET DEFAULT nextval('clients_unduplicated_id_seq'::regclass);
+ALTER TABLE ONLY clients_unduplicated ALTER COLUMN id SET DEFAULT nextval('hud_performance_unduplicated_clients_new_id_seq'::regclass);
 
 
 --
@@ -770,6 +833,20 @@ ALTER TABLE ONLY delayed_jobs ALTER COLUMN id SET DEFAULT nextval('delayed_jobs_
 --
 
 ALTER TABLE ONLY imports ALTER COLUMN id SET DEFAULT nextval('imports_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY letsencrypt_plugin_challenges ALTER COLUMN id SET DEFAULT nextval('letsencrypt_plugin_challenges_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY letsencrypt_plugin_settings ALTER COLUMN id SET DEFAULT nextval('letsencrypt_plugin_settings_id_seq'::regclass);
 
 
 --
@@ -872,14 +949,6 @@ ALTER TABLE ONLY activity_logs
 
 
 --
--- Name: clients_unduplicated_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY clients_unduplicated
-    ADD CONSTRAINT clients_unduplicated_pkey PRIMARY KEY (id);
-
-
---
 -- Name: delayed_jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -888,11 +957,35 @@ ALTER TABLE ONLY delayed_jobs
 
 
 --
+-- Name: hud_performance_unduplicated_clients_new_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY clients_unduplicated
+    ADD CONSTRAINT hud_performance_unduplicated_clients_new_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: imports_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY imports
     ADD CONSTRAINT imports_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: letsencrypt_plugin_challenges_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY letsencrypt_plugin_challenges
+    ADD CONSTRAINT letsencrypt_plugin_challenges_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: letsencrypt_plugin_settings_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY letsencrypt_plugin_settings
+    ADD CONSTRAINT letsencrypt_plugin_settings_pkey PRIMARY KEY (id);
 
 
 --
@@ -1171,7 +1264,7 @@ CREATE INDEX index_versions_on_item_type_and_item_id ON versions USING btree (it
 -- Name: unduplicated_clients_unduplicated_client_id; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX unduplicated_clients_unduplicated_client_id ON clients_unduplicated USING btree (unduplicated_client_id);
+CREATE INDEX unduplicated_clients_unduplicated_client_id ON client_service_history USING btree (unduplicated_client_id);
 
 
 --
@@ -1310,4 +1403,8 @@ INSERT INTO schema_migrations (version) VALUES ('20170627182531');
 INSERT INTO schema_migrations (version) VALUES ('20170703125950');
 
 INSERT INTO schema_migrations (version) VALUES ('20170705123919');
+
+INSERT INTO schema_migrations (version) VALUES ('20170721143408');
+
+INSERT INTO schema_migrations (version) VALUES ('20170721143409');
 
