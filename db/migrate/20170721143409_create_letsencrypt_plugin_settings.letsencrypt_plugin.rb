220ec197bdc65243243e14dab56227affb2619a4
Elliot Anders <eanders@greenriver.org>
2017-07-21 10:39:28 -0400

    Let's encrypt sometimes might already be there

--- db/migrate/20170721143409_create_letsencrypt_plugin_settings.letsencrypt_plugin.rb
+++ db/migrate/20170721143409_create_letsencrypt_plugin_settings.letsencrypt_plugin.rb
@@ -1,6 +1,7 @@
 # This migration comes from letsencrypt_plugin (originally 20160412195212)
 class CreateLetsencryptPluginSettings < ActiveRecord::Migration
   def change
+    return if ActiveRecord::Base.connection.table_exists? :letsencrypt_plugin_settings
     create_table :letsencrypt_plugin_settings do |t|
       t.text :private_key
 
