f964e0d4d7f3ff09b7fd493a452a1ef585ef434d
Elliot Anders <eanders@greenriver.org>
2017-07-28 10:30:58 -0400

    caching settings in memory per request

--- app/models/grda_warehouse/config.rb
+++ app/models/grda_warehouse/config.rb
@@ -25,10 +25,10 @@ module GrdaWarehouse
     end
 
     def self.get(config)
-      settings = Rails.cache.fetch(self.name) do
+      @settings ||= Rails.cache.fetch(self.name) do
         self.first_or_create
       end
-      settings.public_send(config)
+      @settings.public_send(config)
     end
   end
 end
