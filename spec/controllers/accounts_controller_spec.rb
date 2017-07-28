e1f3bba8253453e06ca583ca77a78eb76220a246
Peter Clark <peter@greenriver.org>
2017-07-28 00:01:39 -0500

    get controller specs working with auth

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -3,20 +3,17 @@ require 'rails_helper'
 RSpec.describe AccountsController, type: :controller do
 
   # TODO - get auth working in tests
-  # let(:user) { create :user }
-  #
-  # before(:each) do
-  #   @request.env["devise.mapping"] = Devise.mappings[:user]
-  #   sign_in user
-  # end
-  #
-  # describe "GET edit" do
-  #   before(:each) do
-  #     get :edit
-  #   end
-  #   it 'renders edit' do
-  #     expect( response ).to render_template :edit
-  #   end
-  # end
+  let(:user) { create :user }
+
+  before(:each) do
+    authenticate(user)
+  end
+
+  describe "GET edit" do
+    it 'renders edit' do
+      get :edit
+      expect( response ).to render_template :edit
+    end
+  end
 
 end
