eb8931842a4f5a84d937097216baae0e0fd22893
everestwitman <everesttwitman@gmail.com>
2017-08-02 10:37:36 -0400

    Refactored files controller permissions

--- app/controllers/clients/files_controller.rb
+++ app/controllers/clients/files_controller.rb
@@ -1,11 +1,13 @@
 module Clients
   class FilesController < Window::Clients::FilesController
     include ClientPathGenerator
-    skip_before_action :require_can_manage_window_client_files!
-    before_action :require_can_manage_client_files!
     
     def file_scope
       file_source.where(client_id: @client.id)
+    end
+    
+    def require_can_manage_these_client_files!
+      require_can_manage_client_files!
     end
   end
 end
--- app/controllers/window/clients/files_controller.rb
+++ app/controllers/window/clients/files_controller.rb
@@ -1,7 +1,7 @@
 module Window::Clients
   class FilesController < ApplicationController
     include WindowClientPathGenerator
-    before_action :require_can_manage_window_client_files!
+    before_action :require_can_manage_these_client_files!
     before_action :set_client, only: [:index, :show, :new, :create, :edit, :update]
     before_action :set_files, only: [:index]
     before_action :set_file, only: [:show, :edit, :update]
@@ -19,7 +19,6 @@ module Window::Clients
     end
     
     def create
-      run_import = false
       file = file_params["file"]
       @file = @client.client_files.create(file_params.merge({
         user_id: current_user.id,
@@ -27,14 +26,12 @@ module Window::Clients
         content: file.read,
         }))
       if @file.save
-        run_import = true
-        flash[:notice] = _("Upload queued to start.")
+        flash[:notice] = _("File successfully uploaded.")
         redirect_to action: :index
       else
-        flash[:alert] = _("Upload failed to queue.")
+        flash[:alert] = _("File could not be uploaded.")
         render :new
       end
-      Importing::RunImportHudZipJob.perform_later(@file.id) if run_import
     end
     
     def destroy
@@ -52,12 +49,6 @@ module Window::Clients
     
     def download 
       send_data(@file.content, type: @file.content_type, filename: File.basename(@file.file.to_s))
-      # begin
-      #   send_data(@file.content, type: @file.content_type, filename: File.basename(@file.file.to_s))
-      #   flash[:notice] = "File was successfully downloaded."
-      # rescue Exception => e
-      #   flash[:error] = "File could not be downloaded."
-      # end
     end
     
     def file_params
@@ -96,6 +87,10 @@ module Window::Clients
     
     def file_scope
       file_source.where(client_id: @client.id, visible_in_window: true)
+    end
+    
+    def require_can_manage_these_client_files!
+      require_can_manage_window_client_files!
     end
   end
 end
