33520e281811ff56f67a01c7decf563a23c7c17d
everestwitman <everesttwitman@gmail.com>
2017-08-14 13:34:43 -0400

    Testing or flash on #create

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -14,35 +14,34 @@ RSpec.describe ClientsController, type: :controller do
   
   describe "POST #create_note" do
     context "with valid attributes" do
+      before { post :create_note, note: attributes_for(:grda_warehouse_client_notes_chronic_justification), id: client.id }
+      
       it "creates client note" do
-        note_params = attributes_for(:grda_warehouse_client_notes_chronic_justification)
-        post :create_note, note: note_params, id: client.id 
         expect(GrdaWarehouse::ClientNotes::ChronicJustification.count).to eq(1)
       end
       
       it "redirects to #show" do
-        note_params = attributes_for(:grda_warehouse_client_notes_chronic_justification)
-        post :create_note, note: note_params, id: client.id 
-        
         expect( response ).to redirect_to :action => :show
+      end
+      
+      it "flashes notice" do
+        expect(flash[:notice]).to be_present
       end
     end
     
-    #TODO test for error messages 
-    #TODO test for other types of invalid attributes
     context "with invalid attributes" do
-      it "does not save the new contact" do 
-        note_params = {:note=>""} #invalid because note is an empty string
-        post :create_note, note: note_params, id: client.id 
-        
+      before { post :create_note, note: { :note=>"" }, id: client.id } #invalid because note is an empty string
+      
+      it "does not save the new contact" do   
         expect(GrdaWarehouse::ClientNotes::ChronicJustification.count).to eq(0)
       end
-      
-      it "re-renders #show" do 
-        note_params = {:note=>""} #invalid because note is an empty string
-        post :create_note, note: note_params, id: client.id 
-        
+    
+      it "re-renders #show" do
         expect( response ).to render_template :show
+      end
+      
+      it "flashes error" do
+        expect(flash[:error]).to be_present
       end
     end
   end
