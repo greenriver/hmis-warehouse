6712fdd34b1ba4125ed4f1b447281095bea49735
Elliot Anders <eanders@greenriver.org>
2017-09-01 16:38:01 -0400

    Individual roster api for health claims

--- app/controllers/api/health/claims/patients/roster_controller.rb
+++ app/controllers/api/health/claims/patients/roster_controller.rb
@@ -2,7 +2,34 @@ module Api::Health::Claims::Patients
   class RosterController < BaseController
     
     def load_data      
-      @data = 'FIXME'
+      @data = begin
+        client_roster = scope.first
+        individual = {
+          name: @patient.client.name,
+        }.merge(
+          client_roster.
+          attributes.with_indifferent_access.except(:id, :medicaid_id, :last_name, :first_name)
+        )
+
+        sums = Hash.new(0)
+        counts = Hash.new(0)
+        meta = [sums, counts] 
+        source.all.each do |row|
+          metrics = row.attributes.with_indifferent_access.
+            slice(:norm_risk_score, :mbr_months, :total_ty, :ed_visits, :acute_ip_admits, :average_days_to_readmit)
+          metrics.each_with_object(meta) do |client, (sums, counts)|
+            sums[client.first] += client.last || 0
+            counts[client.first] += 1 if client.last.present?
+          end
+        end
+        sdh = sums.each do |metric, total|
+          sums[metric] = (total.to_f/counts[metric])&.round(2)
+        end
+        {
+          individual: individual,
+          sdh: sdh,
+        }
+      end
     end
 
     def source
