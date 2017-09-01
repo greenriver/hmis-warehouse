b7b86616e04136106b59b661fc660a638bf31dc6
Elliot Anders <eanders@greenriver.org>
2017-09-01 09:29:57 -0400

    Better error checking for notifier config setup, handles situations where .env is not defined

--- app/models/concerns/notifier_config.rb
+++ app/models/concerns/notifier_config.rb
@@ -4,8 +4,8 @@ module NotifierConfig
     attr_accessor :notifier_config
     
     def setup_notifier(username)
-      @notifier_config = Rails.application.config_for(:exception_notifier)['slack']
-      @send_notifications = notifier_config['webhook_url'].present? && ( Rails.env.development? || Rails.env.production? )
+      @notifier_config = Rails.application.config_for(:exception_notifier).fetch('slack', nil)
+      @send_notifications = notifier_config.present? && notifier_config['webhook_url'].present? && ( Rails.env.development? || Rails.env.production? )
       if @send_notifications
         slack_url = notifier_config['webhook_url']
         channel   = notifier_config['channel']
