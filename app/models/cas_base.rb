7f463e5d73944d5df9deb9107a2145468ed2757a
Elliot Anders <eanders@greenriver.org>
2017-09-01 10:22:03 -0400

    Allow daily script to run even if there's no CAS database setup

--- app/models/cas_base.rb
+++ app/models/cas_base.rb
@@ -1,4 +1,8 @@
 class CasBase < ActiveRecord::Base
   establish_connection "#{Rails.env}_cas".parameterize.underscore.to_sym
   self.abstract_class = true
+
+  def self.db_exists?
+    self.connection_pool.with_connection(&:active?) rescue false
+  end
 end
