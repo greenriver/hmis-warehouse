77d55e608bf2e100aae6e5b99bda6a9141d9dd19
Elliot Anders <eanders@greenriver.org>
2017-09-01 13:15:52 -0400

    Further stubs for health overview reports

--- app/controllers/warehouse_reports/health/overview_controller.rb
+++ app/controllers/warehouse_reports/health/overview_controller.rb
@@ -1,6 +1,6 @@
 module WarehouseReports::Health
   class OverviewController < ApplicationController
-    before_action :require_view_aggregate_health!
+    before_action :require_can_view_aggregate_health!
 
     def index
       
