a3768a55fdb7fb37dec9c18caaf5e3297a447856
Elliot Anders <eanders@greenriver.org>
2017-09-13 14:23:23 -0400

    link patient to claims roster

--- app/models/health/patient.rb
+++ app/models/health/patient.rb
@@ -14,6 +14,8 @@ module Health
 
     belongs_to :client, class_name: GrdaWarehouse::Hud::Client.name
 
+    has_one :claims_roster, class_name: Health::Claims::Roster.name, primary_key: :medicaid_id, :foreign_key: :medicaid_id
+
     scope :unprocessed, -> { where client_id: nil}
     scope :consent_revoked, -> {where.not(consent_revoked: nil)}
     scope :consented, -> {where(consent_revoked: nil)}
