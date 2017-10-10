a096de94f779787181859938e548f8ac73183857
dfhoughton <dfhoughton@gmail.com>
2017-10-10 15:47:54 -0400

    [pt#151452937] q8a

--- app/models/report_generators/caper/fy2017/q8a.rb
+++ app/models/report_generators/caper/fy2017/q8a.rb
@@ -1,169 +1,176 @@
 module ReportGenerators::CAPER::Fy2017
   class Q8a < Base
-
+    # Number of Households Served
+    attr :all_clients, :clients_by_household
+    # Number of Persons Served
+    # using logic from app/models/report_generators/ahar/fy2017/base.rb
     def run!
       if start_report(Reports::CAPER::Fy2017::Q8a.first)
         @answers = setup_questions
         @support = @answers.deep_dup
-        @all_clients = fetch_all_clients
+        fetch_all_clients
         update_report_progress percent: 50
-        if @all_clients.any?
-          data_methods = %i[
-            xxx
-            yyy
-            zzz
-          ]
-          data_methods.each_with_index do |method, i|
-            send("add_#{method}")
-            if i < data_methods.length - 1
-              update_report_progress percent: 50 + ( 50 * i.to_f / data_methods.length ).round
-            end
+        @clients_by_household = {}.tap do |bhid|
+          all_clients.each do |id, (*,enrollment)|
+            household = ( bhid[household_id(enrollment)] ||= [] )
+            household << id
           end
         end
-        finish_report()
+        if @all_clients.any?
+          add_answers
+        end
+        finish_report
       else
         Rails.logger.info 'No Report Queued'
       end
     end
 
+    def add_answers
+      bins = households_served
+      bins.keys.zip(%w( b c d e f )).each do |type, column|
+        next unless column
+        ids = bins[type]
+        key = "q8a_#{column}2".to_sym
+        @answers[key][:value] = ids.size
+        @support[key][:support] = add_support(
+          headers: ['Client ID', 'Project', 'Household ID', 'Age'],
+          data: all_clients.slice(*ids.flatten).map do |id, (*,enrollment)|
+            age = enrollment[:age] || HUD.dob_data_quality(enrollment[:DOBDataQuality])
+            [ id, enrollment[:project_name], enrollment[:household_id], age ]
+          end
+        )
+      end
+    end
+
+    # bin the households
+    def households_served
+      bins = {
+        total:               all_clients.keys,
+        without_children:    [],
+        children_and_adults: [],
+        only_children:       [],
+        unknown:             [],
+      }
+      households.each do |id, type|
+        bins[type] << clients_by_household[id]
+      end
+      bins
+    end
+
+    # I'm not certain that all enrollments will have a household id
+    # in this case, the individual is treated as the head of their own household
+    def household_id(enrollment)
+      enrollment[:household_id].presence || enrollment.values_at( :PersonalID, :data_source_id )
+    end
+
+    def fetch_all_clients
+      columns = columnize(
+        age:                   sh_t,
+        client_id:             sh_t,
+        first_date_in_program: sh_t,
+        data_source_id:        sh_t,
+        household_id:          sh_t,
+        project_name:          sh_t,
+        DOB:            c_t,
+        DOBDataQuality: c_t,
+        PersonalID:     c_t,
+      )
+
+      @all_clients = all_client_scope.
+        joins(:project).
+        order(date: :asc).
+        pluck(*columns.values).
+        map do |row|
+          Hash[columns.keys.zip(row)]
+        end.each do |enrollment|
+          # see HUD.dob_data_quality
+          enrollment[:age] = if [8,9,99].include? enrollment[:DOBDataQuality]
+            nil
+          else
+            age_for_report(dob: enrollment[:DOB], enrollment: enrollment)
+          end
+        end.group_by do |row|
+          row[:client_id]
+        end
+    end
+
+    # copied with considerable modification from ahar
+    def households
+      @households ||= clients_by_household.map do |household_id, client_ids|
+        child = adult = unknown = 0
+        client_ids.each do |id|
+          enrollment = all_clients[id].last
+          case enrollment[:age]
+          when nil
+            unknown += 1
+          when 0...ADULT
+            child += 1
+          else
+            adult += 1
+          end
+        end
+        household_type = if adult > 0 && child > 0
+          :children_and_adults
+        elsif adult > 0 && child == 0 && unknown == 0
+          :without_children
+        elsif adult == 0 && child > 0 && unknown == 0
+          :only_children
+        else
+          :unknown
+        end
+          
+        [ household_id, household_type ]
+      end.to_h
+    end
+
     def setup_questions
       {
-        q6a_a1: {
+        q8a_b1: {
           title:  nil,
-          value: 'Data Element',
+          value: 'Total',
         },
-        q6a_b1: {
+        q8a_c1: {
           title:  nil,
-          value: 'Client Doesn’t Know/Refused',
+          value: 'Without Children',
         },
-        q6a_c1: {
+        q8a_d1: {
           title:  nil,
-          value: 'Information Missing',
+          value: 'With Children and Adults',
         },
-        q6a_d1: {
+        q8a_e1: {
           title:  nil,
-          value: 'Data Issues',
+          value: 'With Only Children',
         },
-        q6a_e1: {
+        q8a_f1: {
           title:  nil,
-          value: '% of Error Rate',
+          value: 'Unknown Household Type',
         },
-        q6a_a2: {
+
+        q8a_a2: {
           title:  nil,
-          value: 'Name (3.1)',
+          value: 'Total Households',
         },
-        q6a_a3: {
-          title:  nil,
-          value: 'Social Security Number (3.2)',
+        q8a_b2: {
+          title: 'Total',
+          value:  0,
         },
-        q6a_a4: {
-          title:  nil,
-          value: 'Date of Birth (3.3)',
+        q8a_c2: {
+          title: 'Without Children',
+          value:  0,
         },
-        q6a_a5: {
-          title:  nil,
-          value: 'Race (3.4)',
+        q8a_d2: {
+          title: 'With Children and Adults',
+          value:  0,
         },
-        q6a_a6: {
-          title:  nil,
-          value: 'Ethnicity (3.5)',
+        q8a_e2: {
+          title: 'With Only Children',
+          value:  0,
         },
-        q6a_a7: {
-          title:  nil,
-          value: 'Gender (3.6)',
+        q8a_f2: {
+          title: 'Unknown Household Type',
+          value:  0,
         },
-        q6a_a8: {
-          title:  nil,
-          value: 'Overall Score',
-        },
-        q6a_b2: {
-          title:  'Name - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c2: {
-          title:  'Name - Information Missing',
-          value: 0,
-        },
-        q6a_d2: {
-          title:  'Name - Data Issues',
-          value: 0,
-        },
-        q6a_e2: {
-          title:  'Name - % of Error Rate',
-          value: 0,
-        },
-        q6a_b3: {
-          title:  'SSN - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c3: {
-          title:  'SSN - Information Missing',
-          value: 0,
-        },
-        q6a_d3: {
-          title:  'SSN - Data Issues',
-          value: 0,
-        },
-        q6a_e3: {
-          title:  'SSN - % of Error Rate',
-          value: 0,
-        },
-        q6a_b4: {
-          title:  'DOB - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c4: {
-          title:  'DOB - Information Missing',
-          value: 0,
-        },
-        q6a_d4: {
-          title:  'DOB - Data Issues',
-          value: 0,
-        },
-        q6a_e4: {
-          title:  'DOB - % of Error Rate',
-          value: 0,
-        },
-        q6a_b5: {
-          title:  'Race - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c5: {
-          title:  'Race - Information Missing',
-          value: 0,
-        },
-        q6a_e5: {
-          title:  'Race - % of Error Rate',
-          value: 0,
-        },
-        q6a_b6: {
-          title:  'Ethnicity - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c6: {
-          title:  'Ethnicity - Information Missing',
-          value: 0,
-        },
-        q6a_e6: {
-          title:  'Ethnicity - % of Error Rate',
-          value: 0,
-        },
-        q6a_b7: {
-          title:  'Gender - Client Doesn\'t Know/Refused',
-          value: 0,
-        },
-        q6a_c7: {
-          title:  'Gender - Information Missing',
-          value: 0,
-        },
-        q6a_e7: {
-          title:  'Gender - % of Error Rate',
-          value: 0,
-        },
-        q6a_e8: {
-          title:  'Overall Score - % of Error Rate',
-          value: 0,
-        },
+
       }
     end
 
