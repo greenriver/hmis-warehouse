c9590fac41ea04787db3af2a33a7732a03f40dce
Elliot Anders <eanders@greenriver.org>
2017-10-30 19:56:41 -0400

    Only allow delayed jobs to run for 10 hours

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,5 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 24.hours
+Delayed::Worker.max_run_time = 10.hours
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
