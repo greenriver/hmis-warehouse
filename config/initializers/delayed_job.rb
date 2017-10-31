e8e0d338b6d79b4d433bcccf740b4cfcc1582c3a
Elliot Anders <eanders@greenriver.org>
2017-10-31 16:26:04 -0400

    WIP, need to finish deploy setup

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -3,6 +3,9 @@ Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 10.hours
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.default_queue_name = :default
 Delayed::Worker.queue_attributes = {
-  service_history: { priority: 5 }
+  high_priority: { priority: -5 },
+  default: { priority: 0 },
+  low_priority: { priority: 5 },
 }
