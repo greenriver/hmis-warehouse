a9f3c35d4c6003ee8c5bc0e9b3eb065d6a271f06
Elliot Anders <eanders@greenriver.org>
2017-11-04 08:04:18 -0400

    recent service history

---
+++ app/models/grda_warehouse/recent_service_history.rb
@@ -0,0 +1,19 @@
+module GrdaWarehouse
+  class RecentServiceHistory < GrdaWarehouseBase
+    self.table_name = :recent_service_history
+
+    # NOTE: This is backed by a materialized view that will not be dumped with
+    # warehouse:db:schema:dump, nor loaded with warehouse:db:schema:load
+    # it will only be created with warehouse:db:migrate
+
+    def readonly?
+      true
+    end
+
+    def self.refresh_view
+      connection = GrdaWarehouseBase.connection
+      connection.execute('REFRESH MATERIALIZED VIEW recent_service_history;')
+    end
+
+  end
+end
