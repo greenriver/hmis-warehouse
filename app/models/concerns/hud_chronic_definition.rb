74c23fe4f5f2902f5a7408c63fc4621e792ad271
Peter Clark <peter@greenriver.org>
2017-11-05 15:27:42 -0600

    daily job run

--- app/models/concerns/hud_chronic_definition.rb
+++ app/models/concerns/hud_chronic_definition.rb
@@ -3,6 +3,8 @@ module HudChronicDefinition
 
   # added as instance methods
   included do
+    attr_accessor :hud_chronic_data
+
     has_many :hud_chronics, class_name: GrdaWarehouse::HudChronic.name, inverse_of: :client
 
     # HUD Chronic:
@@ -13,73 +15,83 @@ module HudChronicDefinition
     def hud_chronic? on_date: Date.today
       if hoh_disabled?(on_date: on_date)
         if months_12_homeless?(on_date: on_date)
+          hud_chronic_data[:trigger] = "12 months homeless"
           true
         elsif times_4_homeless?(on_date: on_date)
+          hud_chronic_data[:trigger] = "Homeles 4+ times in 3 years AND "
           if months_homeless_past_three_years_more_than_12?(on_date: on_date)
+            hud_chronic_data[:trigger] += "12+ months homeless"
             true
           elsif total_months_homeless_more_than_12?(on_date: on_date)
+            hud_chronic_data[:trigger] += "12+ months homeless or in project"
             true
           end
         end
       end
     end
-      
 
+    # Has is been at least 12 months since client was
+    # first homeless to on_date?
+    def months_12_homeless? on_date:
+      date_to_street = service_history.hud_homeless.entry.ongoing(on_date: on_date).order(first_date_in_program: :desc).first&.enrollment&.DateToStreetESSH
+      return false unless date_to_street
+      # how many unique months between data_to_street and on_date
+      months_on_street = (on_date.year * 12 + on_date.month) - (date_to_street.year * 12 + date_to_street.month) + 1 # plus one for current month 
+      months_on_street >= 12
+    end
+    
+    # Has the client been homeless 4 times within the past
+    # 3 years?
+    def times_4_homeless? on_date:
+      times_on_street = service_history.hud_homeless.entry.ongoing(on_date: on_date).order(first_date_in_program: :desc).first&.enrollment&.TimesHomelessPastThreeYears
+      times_on_street == 4
+    end
+
+    # Has the client been homeless for more than 12 months
+    # in the past 3 years
+    #
+    # MonthsHomelessPastThreeYears
+    # ----------------------------
+    # 8   Client doesn’t know
+    # 9   Client refused
+    # 99  Data not collected
+    # 101 1
+    # 102 2
+    # 103 3
+    # 104 4
+    # 105 5
+    # 106 6
+    # 107 7
+    # 108 8
+    # 109 9
+    # 110 10
+    # 111 11
+    # 112 12
+    # 113 More than 12 months
+
+    def months_homeless_past_three_years_more_than_12? on_date:
+      months_on_street = service_history.hud_homeless.entry.ongoing(on_date: on_date).order(first_date_in_program: :desc).first&.enrollment&.MonthsHomelessPastThreeYears
+      return false unless months_on_street
+      # 8, 9, 99 are missing, reused etc.
+      hud_chronic_data[:months_in_last_three_years] = months_on_street
+      months_on_street > 111
+    end
+    
+    # Has the client been homeless more than 12 months
     def total_months_homeless_more_than_12? on_date:
-      entry = service_history.hud_homeless.
-        entry.ongoing(date: on_date).first
-        order(first_date_in_program: :desc).first
-      months_on_street = entry&.enrollment&.
-        MonthsHomelessPastThreeYears
+      entry = service_history.hud_homeless.entry.ongoing(on_date: on_date).order(first_date_in_program: :desc).first
+      months_on_street = entry&.enrollment&.MonthsHomelessPastThreeYears
       return false unless months_on_street
       return false unless months_on_street > 100
       months_in_project = (on_date.year * 12 + on_date.month) - (entry.first_date_in_program.year * 12 + entry.first_date_in_program.month) + 1
       (months_on_street - 100) + months_in_project >= 12
     end
 
-    def months_homeless_past_three_years_more_than_12? on_date:
-      months_on_street = service_history.hud_homeless.
-        entry.ongoing(date: on_date).
-        order(first_date_in_program: :desc).first&.enrollment&.
-        MonthsHomelessPastThreeYears
-      return false unlesss months_on_street
-      # 8, 9, 99 are missing, reused etc.
-      months_on_street > 111
-    end
-    def times_4_homeless? on_date:
-      times_on_street = service_history.hud_homeless.
-        entry.ongoing(date: on_date).
-        order(first_date_in_program: :desc).first&.enrollment&.TimesHomelessPastThreeYears
-      times_on_street == 4
-    end
-
-    def months_12_homeless? on_date:
-      date_to_street = service_history.hud_homeless.
-        entry.ongoing(date: on_date).
-        order(first_date_in_program: :desc).first&.enrollment&.DateToStreetESSH
-      return false unless date_to_street
-      # how many unique months between data_to_street and on_date
-      months_on_street = (on_date.year * 12 + on_date.month) - (date_to_street.year * 12 + date_to_street.month) + 1 # plus one for current month 
-      months_on_street >= 12
-    end
+    # Is the head of household for this client disabled?
     def hoh_disabled? on_date:
-      entry = service_history.hud_homeless.
-        entry.ongoing(date: on_date).
-        order(first_date_in_program: :desc).first
-      entry&.head_of_household.source_enrollments.pluck(:DisablingCondition).include?(1)
-    end
-
-    def hoh_residing? in_places:, on_date:, days: nil
-      if days
-        date_to_street = service_history.hud_homeless.
-          entry.ongoing(date: on_date).
-          order(first_date_in_program: :desc).first&.enrollment&.DateToStreetESSH
-        return false unless date_to_street
-        (on_date - date_to_street).to_i >= days
-      else
-        service_history.hud_homeless.
-          entry.ongoing(date: on_date).exists?
-      end
+      entry = service_history.hud_homeless.entry.ongoing(on_date: on_date).order(first_date_in_program: :desc).first
+      return false unless entry
+      entry.head_of_household&.source_enrollments&.pluck(:DisablingCondition)&.include?(1)
     end
 
   end
--- app/models/grda_warehouse/tasks/hud_chronically_homeless.rb
+++ app/models/grda_warehouse/tasks/hud_chronically_homeless.rb
@@ -3,6 +3,56 @@ module GrdaWarehouse::Tasks
 
     # 1. clients = all clients with an open enrollment in a homeless project
     # 2. client.hud_chronic? Date.today => save to hud_chronics
+
+    attr_accessor :date, :client_ids, :total
+
+    def initialize date: Date.today, client_ids: []
+      @date = date
+      @client_ids = client_ids
+      @total = 0
+    end
+
+    def run!
+      # turn off SQL logging
+      ActiveRecord::Base.logger.level = 1
+      log "\nCalculating HUD Chronically Homeless for #{client_ids.size} clients on #{@date}", underline: true
+
+      client_ids.each_with_index do |id, index|
+        client = GrdaWarehouse::Hud::Client.where( id: id ).first
+        next unless client && client.hud_chronic?
+
+        log " #{index} => Client #{id} (#{client.full_name}) is HUD chronic"
+
+        data = client.hud_chronic_data
+
+        hc = GrdaWarehouse::HudChronic.new
+        hc.date = @date
+        hc.client_id = id
+        hc.days_in_last_three_years = nil
+        hc.months_in_last_three_years = data[:months_in_last_three_years]
+        hc.individual = !client.presented_with_family?(after: @date - 3.years, before: @date)
+        hc.age = client.age
+        hc.homeless_since = client.service_history.first_date&.first.try(:date)
+        hc.dmh = false
+        hc.trigger = data[:trigger]
+        hc.project_names = nil
+
+        hc.save
+        total = total + 1
+      end
+
+      log "\nCreated #{total} records for clients found to be HUD Chronic"
+    end
+
+    def client_ids
+      return @client_ids if @client_ids&.any?
+      GrdaWarehouse::ServiceHistory.hud_currently_homeless(date: @date).distinct.pluck(:client_id)
+    end
+
+    def log msg, underline: false
+      Rails.logger.info msg
+      Rails.logger.info "="*msg.length if underline
+    end
     
   end
 end
