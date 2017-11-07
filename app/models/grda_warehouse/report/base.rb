d4e5242c95988bd2fda07caf17d99e9e57837289
Elliot Anders <eanders@greenriver.org>
2017-11-07 09:46:43 -0500

    Better recent report enrollments table creation

--- app/models/grda_warehouse/report/base.rb
+++ app/models/grda_warehouse/report/base.rb
@@ -66,9 +66,57 @@ module GrdaWarehouse::Report
       end
     end
 
+
+    def self.source_client_table
+      @source_client_table ||= Arel::Table.new(
+        GrdaWarehouse::Hud::Client.table_name
+      ).tap{ |t| t.table_alias = 'source_clients' }
+    end
+
+    def self.destination_client_table
+      @destination_client_table ||= Arel::Table.new(
+        GrdaWarehouse::Hud::Client.table_name
+      ).tap{ |t| t.table_alias = 'destination_clients' }
+    end
+
+    def self.client_join_table
+      GrdaWarehouse::WarehouseClient.arel_table
+    end
+
     def self.update_recent_report_enrollments_table
       range = ::Filters::DateRange.new(start: 1.years.ago.to_date, end: Date.today)
-      sql = GrdaWarehouse::Report::Enrollment.open_during_range(range).to_sql.gsub('FROM', 'INTO recent_report_enrollments FROM')
+
+      d_1_start = range.start
+      d_1_end = range.end
+      d_2_start = e_t[:EntryDate]
+      d_2_end = ex_t[:ExitDate]
+
+      # This is a copy of the code that creates the report_enrollments view
+      # combined with the limit for open during a date range from Enrollmnt
+      # because it is way faster than limiting the view
+      query =  e_t.join(source_client_table).on(
+          e_t[:data_source_id].eq(source_client_table[:data_source_id]).
+          and( e_t[:PersonalID].eq source_client_table[:PersonalID] ).
+          and( source_client_table[:DateDeleted].eq nil )
+        ).join(client_join_table).on(
+          source_client_table[:id].eq client_join_table[:source_id]
+        ).join(destination_client_table).on(
+          destination_client_table[:id].eq(client_join_table[:destination_id]).
+          and( destination_client_table[:DateDeleted].eq nil )
+        ).project(
+          *e_t.engine.column_names.map(&:to_sym).map{ |c| e_t[c] },  # all the enrollment columns
+          source_client_table[:id].as('demographic_id'),                                         # the source client id
+          destination_client_table[:id].as('client_id')                                          # the destination client id
+        ).where(
+          e_t[:DateDeleted].eq nil
+        ).
+        join(ex_t, Arel::Nodes::OuterJoin).
+          on(e_t[:ProjectEntryID].eq(ex_t[:ProjectEntryID]).
+          and(e_t[:PersonalID].eq(ex_t[:PersonalID]).
+          and(e_t[:data_source_id].eq(ex_t[:data_source_id])))).
+        where(d_2_end.gt(d_1_start).or(d_2_end.eq(nil)).and(d_2_start.lt(d_1_end))).to_sql
+      # Turn this into a table
+      sql = query.gsub('FROM', 'INTO recent_report_enrollments FROM')
       self.connection.execute <<-SQL
         DROP TABLE IF EXISTS recent_report_enrollments;
       SQL
