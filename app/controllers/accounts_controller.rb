a1aea005f87fb4bb779c48dbcbfa869794bde15f
Peter Clark <peter@greenriver.org>
2017-11-08 09:32:26 -0600

    add notify_on_vispdat_completed field

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,14 +7,18 @@ class AccountsController < ApplicationController
   def update
     changed_notes = []
     if @user.first_name != account_params[:first_name] || @user.last_name != account_params[:last_name]
-      changed_notes << "Account name was updated."
+      changed_notes << "Account name was updated. "
     end
     if @user.email != account_params[:email]
-      changed_notes << "Account email was updated, check your inbox for a confirmation link."
+      changed_notes << "Account email was updated, check your inbox for a confirmation link. "
     end
-    if  account_params[:password] == account_params[:password_confirmation]
-      changed_notes << "Password was changed."
+    if account_params[:password_confirmation].present? && (account_params[:password] == account_params[:password_confirmation])
+      changed_notes << "Password was changed. "
     end
+    if account_params[:notify_on_vispdat_completed] != (@user.notify_on_vispdat_completed ? '1' : '0')
+      changed_notes << " Email Notifications were updated."
+    end
+    changed_notes << "Account updated." if changed_notes.empty?
     if @user.update_with_password(account_params)
       flash[:notice] = changed_notes.join(' ')
 
@@ -38,6 +42,7 @@ class AccountsController < ApplicationController
           :current_password,
           :password,
           :password_confirmation,
+          :notify_on_vispdat_completed
         )
     end
 
