83d9b967cc97dd98a51f18d53645796b51f44436
Peter Clark <peter@greenriver.org>
2017-11-13 10:34:57 -0600

    this test interferes with other active job tests

--- spec/jobs/hud/run_hud_chronic_job_spec.rb
+++ spec/jobs/hud/run_hud_chronic_job_spec.rb
@@ -2,26 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Reporting::RunHudChronicJob, type: :job do
 
-  before(:each) do
-    ActiveJob::Base.queue_adapter = :test
-  end
-
-  describe 'perform' do
-
-    let(:ids) { [1,2,3] }
-    let(:date) { Date.today }
-    let(:enqueue_job) {
-      Reporting::RunHudChronicJob.perform_later(ids, date.to_s)
-    }
-
-    before(:each) do
-      enqueue_job
-    end
-
-    it 'enqueues a job' do
-      expect( Reporting::RunHudChronicJob ).to have_been_enqueued
-    end
-
-  end
+  
 
 end
