6357ce0527c3bb2138ff346ffc1eac5cdb283abf
Elliot Anders <eanders@greenriver.org>
2017-11-27 18:42:34 -0500

    Splitting ahar config migration into two steps to get around the transaction

--- db/warehouse/migrate/20171127203632_add_aharpsh_config.rb
+++ db/warehouse/migrate/20171127203632_add_aharpsh_config.rb
@@ -1,7 +1,5 @@
 class AddAharpshConfig < ActiveRecord::Migration
   def change
     add_column :configs, :ahar_psh_includes_rrh, :boolean, default: true
-    GrdaWarehouse::Config.invalidate_cache
-    GrdaWarehouse::Config.first.update(ahar_psh_includes_rrh: true)
   end
 end
