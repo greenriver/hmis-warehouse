9e13d78b84b9e1e642d0e32ffecdd764ce5cf276
Elliot Anders <eanders@greenriver.org>
2017-12-19 13:31:00 -0500

    Error correction for missing dashboard reports

--- app/controllers/dashboards/base_controller.rb
+++ app/controllers/dashboards/base_controller.rb
@@ -16,44 +16,47 @@ module Dashboards
 
     def active
       @report = active_report_class.ordered.first
-      data = @report[:data].with_indifferent_access
-      @data = data[:data]
-      @clients = data[:clients]
-      @client_count = data[:client_count]
-      @enrollments = data[:enrollments]
-      @month_name = data[:month_name]
-      @range = ::Filters::DateRange.new(data[:range])
-      @labels = data[:labels]
-
+      if @report.present?
+        data = @report[:data].with_indifferent_access
+        @data = data[:data]
+        @clients = data[:clients]
+        @client_count = data[:client_count]
+        @enrollments = data[:enrollments]
+        @month_name = data[:month_name]
+        @range = ::Filters::DateRange.new(data[:range])
+        @labels = data[:labels]
+      end
       render layout: !request.xhr?
     end
 
     def housed
       @report = housed_report_class.ordered.first
-      data = @report[:data].with_indifferent_access
-      @ph_clients = data[:ph_clients]
-      @ph_exits = data[:ph_exits]
-      @buckets = data[:buckets]
-      @all_exits_labels = data[:all_exits_labels]
-      @start_date = data[:start_date]
-      @end_date = data[:end_date]
-
+      if @report.present?
+        data = @report[:data].with_indifferent_access
+        @ph_clients = data[:ph_clients]
+        @ph_exits = data[:ph_exits]
+        @buckets = data[:buckets]
+        @all_exits_labels = data[:all_exits_labels]
+        @start_date = data[:start_date]
+        @end_date = data[:end_date]
+      end
       render layout: !request.xhr?
     end
 
     def entered
       @report = entered_report_class.ordered.first
-      data = @report[:data].with_indifferent_access
-      @enrollments_by_type = data[:enrollments_by_type]
-      @client_enrollment_totals_by_type = data[:client_enrollment_totals_by_type]
-      @client_entry_totals_by_type = data[:client_entry_totals_by_type]
-      @first_time_total_deduplicated = data[:first_time_total_deduplicated]
-      @first_time_ever = data[:first_time_ever]
-      @data = data[:data]
-      @labels = data[:labels]
-      @start_date = data[:start_date]
-      @end_date = data[:end_date]
-
+      if @report.present?
+        data = @report[:data].with_indifferent_access
+        @enrollments_by_type = data[:enrollments_by_type]
+        @client_enrollment_totals_by_type = data[:client_enrollment_totals_by_type]
+        @client_entry_totals_by_type = data[:client_entry_totals_by_type]
+        @first_time_total_deduplicated = data[:first_time_total_deduplicated]
+        @first_time_ever = data[:first_time_ever]
+        @data = data[:data]
+        @labels = data[:labels]
+        @start_date = data[:start_date]
+        @end_date = data[:end_date]
+      end
       render layout: !request.xhr?
     end
 
