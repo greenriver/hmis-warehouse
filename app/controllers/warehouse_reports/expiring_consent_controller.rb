cdac6500728d384f826d80c1c1b1d27e8ca34681
Elliot Anders <eanders@greenriver.org>
2018-02-05 13:49:12 -0500

    Consent report updates

--- app/controllers/warehouse_reports/expiring_consent_controller.rb
+++ app/controllers/warehouse_reports/expiring_consent_controller.rb
@@ -4,10 +4,15 @@ module WarehouseReports
     include ArelHelper
     def index
       consented_clients = client_source.where.not(consent_form_signed_on: nil)
-      @expired_clients = consented_clients.where(housing_release_status: nil).
+      @expired_clients = consented_clients.
+        where(housing_release_status: [nil, '']).
+        where(c_t[:consent_form_signed_on].lt(client_source.consent_validity_period.ago.to_date)).
         preload(:user_clients)
-      @expiring_clients = consented_clients.where.not(housing_release_status: nil).
+      @expiring_clients = consented_clients.where.not(housing_release_status: [nil, '']).
         where(c_t[:consent_form_signed_on].lt(client_source.consent_validity_period.ago + 30.days)).
+        preload(:user_clients)
+      @unconfirmed = consented_clients.where(housing_release_status: [nil, '']).
+        where(c_t[:consent_form_signed_on].gteq(client_source.consent_validity_period.ago.to_date)).
         preload(:user_clients)
       # These exist in a different database, so we'll need to fetch them separately
       @users = (@expired_clients + @expiring_clients).map do |client|
