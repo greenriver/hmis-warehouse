64bc39d8b7b2541b667f4c6ac50db2bc2ea4275f
Elliot Anders <eanders@greenriver.org>
2018-02-19 15:38:15 -0500

    Updates to service history for PDF generation

--- app/controllers/window/clients/history_controller.rb
+++ app/controllers/window/clients/history_controller.rb
@@ -4,14 +4,25 @@ module Window::Clients
     
     before_action :require_can_see_this_client_demographics!
     before_action :set_client, :check_release
+    before_action :set_dates, only: [:show]
     
     def show
+      @ordered_dates = @dates.keys.sort
+      @start = @ordered_dates.first
+      @end = @ordered_dates.last
+      @date_range = (@start.beginning_of_month..@end.end_of_month)
+      @months = @date_range.map do |date|
+        [date.year, date.month]
+      end.uniq
+    end
+
+    def set_dates
       @dates = {}
-      @client.service_history_enrollments.
+      enrollment_scope.
         includes(:service_history_services, :organization).
         each do |enrollment|
           project_type = enrollment.project_type
-          project_name = GrdaWarehouse::Hud::Project.confidentialize(name: enrollment.project_name)
+          project_name = name_for_project(enrollment.project_name)
           @dates[enrollment.date] ||= []
           record = {
             record_type: enrollment.record_type,
@@ -33,13 +44,14 @@ module Window::Clients
             }
           end
         end
-      @ordered_dates = @dates.keys.sort
-      @start = @ordered_dates.first
-      @end = @ordered_dates.last
-      @date_range = (@start.beginning_of_month..@end.end_of_month)
-      @months = @date_range.map do |date|
-        [date.year, date.month]
-      end.uniq
+    end
+
+    def name_for_project project_name
+      GrdaWarehouse::Hud::Project.confidentialize(name: project_name)
+    end
+
+    def enrollment_scope
+      @client.service_history_enrollments.visible_in_window
     end
     
     def set_client
