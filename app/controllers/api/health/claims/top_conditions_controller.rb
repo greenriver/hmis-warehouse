ccdcab3b900a38a9b4aabc9c6df128eb27c18803
Elliot Anders <eanders@greenriver.org>
2018-02-21 09:58:30 -0500

    Updates to aggregate claims metrics

--- app/controllers/api/health/claims/top_conditions_controller.rb
+++ app/controllers/api/health/claims/top_conditions_controller.rb
@@ -2,15 +2,23 @@ module Api::Health::Claims
   class TopConditionsController < BaseController
     
     def load_data      
+      total_paid_baseline = source.sum(:baseline_paid)
+      total_paid_implementation = source.sum(:implementation_paid)
+      baseline_paid_by_provider_name = source.group(:description).
+        sum(:baseline_paid)
+        
       @data = source.group(:description).
-        sum(:indiv_pct).
+        sum(:implementation_paid).
         sort_by{|k,v| v}.
         reverse.
-        first(5).map do |description, indiv_pct|
+        first(5).
+        map do |description, sum_paid|
           {
             description: description, 
-            sdh_pct: indiv_pct / 100
+            sdh_pct: baseline_paid_by_provider_name[description] / total_paid_baseline,
+            indiv_pct: sum_paid / total_paid_implementation, 
           }
+
       end
     end
 
--- app/controllers/api/health/claims/top_providers_controller.rb
+++ app/controllers/api/health/claims/top_providers_controller.rb
@@ -2,14 +2,21 @@ module Api::Health::Claims
   class TopProvidersController < BaseController
     
     def load_data
+      total_paid_baseline = source.sum(:baseline_paid)
+      total_paid_implementation = source.sum(:implementation_paid)
+      baseline_paid_by_provider_name = source.group(:provider_name).
+        sum(:baseline_paid)
+        
       @data = source.group(:provider_name).
-        sum(:indiv_pct).
+        sum(:implementation_paid).
         sort_by{|k,v| v}.
         reverse.
-        first(5).map do |provider_name, indiv_pct|
+        first(5).
+        map do |provider_name, sum_paid|
           {
             provider_name: provider_name, 
-            sdh_pct: indiv_pct / 100
+            sdh_pct: baseline_paid_by_provider_name[provider_name] / total_paid_baseline,
+            indiv_pct: sum_paid / total_paid_implementation, 
           }
       end
     end
