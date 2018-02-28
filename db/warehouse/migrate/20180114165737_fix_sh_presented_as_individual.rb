2db55c8fc79253c9491e0f24e5f737995839d3ed
Peter Clark <peter@greenriver.org>
2018-02-28 09:14:01 -0600

    commenting out migrations that won’t run

--- db/warehouse/migrate/20180114165737_fix_sh_presented_as_individual.rb
+++ db/warehouse/migrate/20180114165737_fix_sh_presented_as_individual.rb
@@ -1,9 +1,9 @@
 class FixShPresentedAsIndividual < ActiveRecord::Migration
   def up
-    # Set all to true
-    GrdaWarehouse::ServiceHistory.update_all(presented_as_individual: true)
-    # Set family enrollments to false
-    GrdaWarehouse::ServiceHistory.joins(:project).merge(GrdaWarehouse::Hud::Project.serves_families).update_all(presented_as_individual: false)
+    # # Set all to true
+    # GrdaWarehouse::ServiceHistory.update_all(presented_as_individual: true)
+    # # Set family enrollments to false
+    # GrdaWarehouse::ServiceHistory.joins(:project).merge(GrdaWarehouse::Hud::Project.serves_families).update_all(presented_as_individual: false)
 
   end
 end
