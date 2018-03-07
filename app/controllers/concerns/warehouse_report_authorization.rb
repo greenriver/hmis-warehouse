56f90634f6406037d4e5224d1f833a98b1104f88
jgoley <jgoley@gmail.com>
2018-03-07 14:03:16 -0500

    Update warehouse reports and tabs

--- app/controllers/concerns/warehouse_report_authorization.rb
+++ app/controllers/concerns/warehouse_report_authorization.rb
@@ -3,7 +3,7 @@ module WarehouseReportAuthorization
   included do
     before_action :report_visible?
     before_action :require_can_view_any_reports!
-    
+
     def report_visible?
       return true if related_report.viewable_by(current_user).exists?
       not_authorized!
