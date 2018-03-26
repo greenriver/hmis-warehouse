1f362e827aa80209be2e9e1bd88f7d25290d377c
Elliot Anders <eanders@greenriver.org>
2018-03-26 14:25:50 -0400

    WIP, place to store import changes

---
+++ db/warehouse/migrate/20180326140546_create_hud_create_logs.rb
@@ -0,0 +1,10 @@
+class CreateHudCreateLogs < ActiveRecord::Migration
+  def change
+    create_table :hud_create_logs do |t|
+      t.string :hud_key, null: false
+      t.string :type, null: false
+      t.datetime :imported_at, null: false, index: true
+      t.integer :data_source_id, null: false
+    end
+  end
+end
