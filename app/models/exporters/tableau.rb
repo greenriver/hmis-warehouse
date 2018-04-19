fc611e1891c3d698f4d67daa288ac26fa9d7199f
dfhoughton <dfhoughton@gmail.com>
2018-04-19 14:22:09 -0400

    improved receiving_income_source; grouping and de-duping things (keep the most recent income record); changed date format

--- app/models/exporters/tableau.rb
+++ app/models/exporters/tableau.rb
@@ -8,37 +8,41 @@ module Exporters::Tableau
     def income(start_date: 3.years.ago, end_date: DateTime.current, coc_code:)
       model = ib_t.engine
       spec = {
+        grouping_variable:       she_t[:id],
         entry_exit_uid:          e_t[:ProjectEntryID],
         entry_exit_client_id:    she_t[:client_id],
         earned_income:           ib_t[:Earned],
         ssi_ssdi:                ib_t[:SSI],
         tanf:                    ib_t[:TANF],
-        source_of_income:        ib_t[:SSDI], # ???
-        receiving_income_source: ib_t[:SSI],  # ??? -- repeat for sake of simplicity
+        source_of_income:        ib_t[:SSDI],
+        receiving_income_source: ib_t[:IncomeFromAnySource],
         start_date:              she_t[:first_date_in_program],
         end_date:                she_t[:last_date_in_program],
       }
       incomes = model.
         joins( enrollment: [ :enrollment_coc_at_entry, :service_history_enrollment ] ).
         merge( she_t.engine.open_between start_date: start_date, end_date: end_date ).
-        where( ec_t[:CoCCode].eq coc_code )
+        where( ec_t[:CoCCode].eq coc_code ).
+        order(ib_t[:InformationDate].desc)
       spec.each do |header, selector|
         incomes = incomes.select selector.as(header.to_s)
       end
 
       csv = CSV.generate headers: true do |csv|
-        headers = spec.keys
+        headers = spec.keys - [:grouping_variable]
         csv << headers
 
         last = []
-        model.connection.select_all(incomes.to_sql).each do |income|
+        incomes = model.connection.select_all(incomes.to_sql)
+        # get the *most recent* ib per enrollment and ignore the rest
+        incomes.group_by{ |h| h['grouping_variable'] }.each do |_,(income,*)|
           row = []
           ssi, ssdi, tanf, earned_income = %w( ssi_ssdi source_of_income tanf earned_income ).map{ |f| income[f].presence&.to_i == 1 }
           headers.each do |h|
             value = income[h.to_s].presence
             value = case h
             when :start_date, :end_date
-              value && DateTime.parse(value).strftime('%Y-%m-%d %H:%M')
+              value && DateTime.parse(value).strftime('%Y-%m-%d')
             when :earned_income
               earned_income ? 'Yes' : 'No'
             when :tanf
@@ -62,7 +66,7 @@ module Exporters::Tableau
               end
               "#{source} (HUD)" if source
             when :receiving_income_source
-              ssi || ssdi || tanf || earned_income ? 'Yes' : 'No'
+              value&.to_i == 1 ? 'Yes' : 'No'
             else
               value
             end
