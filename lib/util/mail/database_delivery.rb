a47ccc3d16caf7ca628e3117e863a96837479cc6
dfhoughton <dfhoughton@gmail.com>
2018-04-19 12:56:05 -0400

    only set sent_at after the message sends; get letter opener to work

--- lib/util/mail/database_delivery.rb
+++ lib/util/mail/database_delivery.rb
@@ -15,16 +15,23 @@ module Mail
           body:    mail.body.to_s,
           from:    mail[:from].addresses.first,
           html:    /\A<html>.*<\/html>\z/im === mail.body.to_s.strip,
-          sent_at: ( DateTime.current if user.continuous_email_delivery? ),
         )
         if user.continuous_email_delivery?
           # use the configured delivery method
           delivery_method = Rails.configuration.action_mailer.delivery_method
+          options = case delivery_method
+          when :letter_opener
+            { location: Rails.root.join( 'tmp', 'letter_opener' ) } # for some reason it isn't getting the default
+          else
+            {}
+          end
+          delivery_method = ActionMailer::Base.delivery_methods[delivery_method]
           copy = mail.dup
           copy.to = user.email
-          copy.delivery_method delivery_method
+          copy.delivery_method delivery_method, options
           copy.perform_deliveries = true
           copy.deliver
+          message.update_attribute :sent_at, DateTime.current
         end
       end
     end
