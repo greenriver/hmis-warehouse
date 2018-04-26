03a1f5c038574e2949e3af9773e049ace69ed505
Elliot Anders <eanders@greenriver.org>
2018-04-26 19:48:58 -0400

    Fix for email schedule migration

--- db/migrate/20180419165841_made_default_schedule_immediate.rb
+++ db/migrate/20180419165841_made_default_schedule_immediate.rb
@@ -1,7 +1,6 @@
 class MadeDefaultScheduleImmediate < ActiveRecord::Migration
   def up
-    change_column_default :users, :email_schedule, "immediate"
-    User.where( email_schedule: nil ).update_all email_schedule: "immediate"
-    change_column_null :users, :email_schedule, false
+    remove_column :users, :email_schedule, :string
+    add_column :users, :email_schedule, :string, default: 'immediate', null: false
   end
 end
