46dd721b7d64c14337eb7e4816fe28d89b8f4b78
Elliot Anders <eanders@greenriver.org>
2018-04-30 15:03:54 -0400

    Enable the email interceptor in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.sandbox_email_mode = false
+  config.sandbox_email_mode = true
   config.action_mailer.delivery_method = deliver_method
   config.action_mailer.default_url_options = { host: ENV['HOSTNAME'], protocol: 'https'}
   if deliver_method == :smtp
