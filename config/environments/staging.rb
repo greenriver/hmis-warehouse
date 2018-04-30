59c7cefe8c178ef85d523e716b3422067e8307db
Elliot Anders <eanders@greenriver.org>
2018-04-30 14:56:48 -0400

    Don't use SMTP_DOMAIN

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -23,7 +23,6 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       address: ENV['SMTP_SERVER'],
       port: 587,
-      domain: ENV['SMTP_DOMAIN'],
       user_name: ENV['SMTP_USERNAME'],
       password: ENV['SMTP_PASSWORD'],
       authentication: :login,
