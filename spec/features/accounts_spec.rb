73d55a154e2984a47eaa82db4fcafe0784f4ce84
everestwitman <everesttwitman@gmail.com>
2018-05-08 11:52:20 +0200

    feature test for devise lock

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -24,6 +24,27 @@ RSpec.feature "Accounts", type: :feature do
       click_button 'Log in'
       expect( page ).to have_content 'You have successfully signed in.'
     end
+    
+    scenario "Devise lockable" do
+      #Account should lock after a certain number of failed attemps
+      click_link 'Sign In'
+      Devise.maximum_attempts.times do
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Log in'
+      end    
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'abcd1234'
+      click_button 'Log in'
+      expect( page ).to have_content  'Your account is locked.'
+      
+      #Jump forward the necessary amount of time and verify that the account is unlocked
+      Timecop.travel(Time.now + Devise.unlock_in)
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'abcd1234'
+      click_button 'Log in'
+      expect( page ).to have_content  'You have successfully signed in.'
+    end
   end
 
 end
