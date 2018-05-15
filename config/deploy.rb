8383958a69021d7baab40f7884b665050b4d37d4
Todd Blackman <tdblackman@gmail.com>
2018-05-15 13:32:32 -0400

    chmod cleanup at very end

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ task :group_writable do
     execute "chgrp --quiet ubuntu -R #{fetch(:deploy_to)} || echo ok"
   end
 end
-after 'passenger:restart', :group_writable
+after 'deploy:log_revision', :group_writable
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
