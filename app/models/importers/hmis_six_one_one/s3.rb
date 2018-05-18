edafb47d7f99cf4601a000c47a5ea43f093dd72f
Elliot Anders <eanders@greenriver.org>
2018-05-18 14:30:00 -0400

    Only present actually available connections

--- app/models/importers/hmis_six_one_one/s3.rb
+++ app/models/importers/hmis_six_one_one/s3.rb
@@ -35,7 +35,7 @@ module Importers::HMISSixOneOne
 
     def self.available_connections
       connections = YAML::load(ERB.new(File.read(Rails.root.join("config","hmis_s3.yml"))).result)[Rails.env]
-      connections.select{|conn| conn[:access_key_id].present?}
+      connections.select{|_,conn| conn['access_key_id'].present?}
     end
         
     def import!
