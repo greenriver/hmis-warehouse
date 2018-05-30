ef71b435a283ad07d9d991522e74bee81db6edd2
Elliot Anders <eanders@greenriver.org>
2018-05-30 15:33:41 -0400

    Missing files

---
+++ app/controllers/health/patients_controller.rb
@@ -0,0 +1,26 @@
+module Health
+  class PatientsController < ApplicationController
+    before_action :require_can_view_patients_for_own_agency!
+    before_action :require_user_has_health_agency!
+    before_action :set_patients
+    include ClientPathGenerator
+
+    def index
+
+    end
+    
+
+    def set_patients
+      @patients = patient_source.joins(:health_agency).where(agencies: {id: current_user.health_agency.id})
+    end
+    
+    def patient_source
+      Health::Patient
+    end
+
+    def require_user_has_health_agency!
+      return true if current_user.health_agency.present?
+      not_authorized!
+    end
+  end
+end
