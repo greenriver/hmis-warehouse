d8332d95c4dfde0c3205b4b3989bfe77f540e728
Elliot Anders <eanders@greenriver.org>
2018-06-01 11:04:58 -0400

    Fixing Qualifying Activities codes

--- app/models/health/qualifying_activity.rb
+++ app/models/health/qualifying_activity.rb
@@ -2,33 +2,33 @@ module Health
   class QualifyingActivity < HealthBase
 
     MODE_OF_CONTACT = {
-      'U2' => 'In Person',
-      'U3' => 'Phone call',
-      'U3' => 'Email',
-      'U3' => 'Video call',
-      '' => 'Other'
+      'In Person' => 'U2',
+      'Phone call' => 'U3',
+      'Email' => 'U3',
+      'Video call' => 'U3',
+      'Other' => '',
     }
     MODE_OF_CONTACT_OTHER = 'Other'
 
     REACHED_CLIENT = {
-      'U1' => 'Yes (face to face, phone call answered, response to email)',
-      'HQ' => 'Group session',
-      '' => 'Did not reach',
-      'UK' => 'Collateral contact - not with client directly'
+      'Yes (face to face, phone call answered, response to email)' => 'U1',
+      'Group session' => 'HQ',
+      'Did not reach' => '',
+      'Collateral contact - not with client directly' => 'UK',
     }
     REACHED_CLIENT_OTHER = 'Collateral contact - not with client directly'
 
     ACTIVITY = {
-      'G9011' => 'Outreach for enrollment',
-      'G9005' => 'Care coordination',
-      'T2024' => 'Care planning',
-      'G0506' => 'Comprehensive Health Assessment',
-      'G9007 U5' => 'Follow-up within 3 days of hospital discharge (with client)',
-      'G9007' => 'Care transitions (working with care team)',
-      'G9006' => 'Health and wellness coaching',
-      'G9004' => 'Connection to community and social services',
-      'T1023' => 'Social services screening completed',
-      'T1023 U6' => 'Referral to ACO for Flexible Services'
+      'Outreach for enrollment' => 'G9011',
+      'Care coordination' => 'G9005',
+      'Care planning' => 'T2024',
+      'Comprehensive Health Assessment' => 'G0506',
+      'Follow-up within 3 days of hospital discharge (with client)' => 'G9007 U5',
+      'Care transitions (working with care team)' => 'G9007',
+      'Health and wellness coaching' => 'G9006',
+      'Connection to community and social services' => 'G9004',
+      'Social services screening completed' => 'T1023',
+      'Referral to ACO for Flexible Services' => 'T1023 U6',
     }
 
     scope :submitted, -> {where.not(claim_submitted_on: nil)}
@@ -37,9 +37,9 @@ module Health
     belongs_to :source, polymorphic: true
     belongs_to :user
 
-    validates :mode_of_contact, inclusion: {in: MODE_OF_CONTACT.values}, allow_blank: true
-    validates :reached_client, inclusion: {in: REACHED_CLIENT.values}, allow_blank: true
-    validates :activity, inclusion: {in: ACTIVITY.values}, allow_blank: true 
+    validates :mode_of_contact, inclusion: {in: MODE_OF_CONTACT.keys}, allow_blank: true
+    validates :reached_client, inclusion: {in: REACHED_CLIENT.keys}, allow_blank: true
+    validates :activity, inclusion: {in: ACTIVITY.keys}, allow_blank: true 
     validates_presence_of :user, :user_full_name, :source, :follow_up, :date_of_activity
     validates_presence_of :mode_of_contact_other, if: :mode_of_contact_is_other?
     validates_presence_of :reached_client_collateral_contact, if: :reached_client_is_collateral_contact?
@@ -59,15 +59,15 @@ module Health
     end
 
     def self.mode_of_contact_collection
-      self.load_string_collection(MODE_OF_CONTACT.values)
+      self.load_string_collection(MODE_OF_CONTACT.keys)
     end
 
     def self.reached_client_collection
-      self.load_string_collection(REACHED_CLIENT.values)
+      self.load_string_collection(REACHED_CLIENT.keys)
     end
 
     def self.activity_collection
-      self.load_string_collection(ACTIVITY.values)
+      self.load_string_collection(ACTIVITY.keys)
     end
 
     def mode_of_contact_is_other?
@@ -105,15 +105,15 @@ module Health
 
     def procedure_code
       # ignore any modifiers
-      ACTIVITY.invert[activity].split(' ')[0]
+      ACTIVITY[activity].split(' ')[0]
     end
 
     def modifiers
       modifiers = []
       # attach modifiers from activity
-      modifiers << ACTIVITY.invert[activity].split(' ')[1]
-      modifiers << MODE_OF_CONTACT.invert[mode_of_contact] 
-      modifiers << REACHED_CLIENT.invert[reached_client]
+      modifiers << ACTIVITY[activity].split(' ')[1]
+      modifiers << MODE_OF_CONTACT[mode_of_contact] 
+      modifiers << REACHED_CLIENT[reached_client]
       return modifiers.reject(&:blank?).compact
     end
   end
