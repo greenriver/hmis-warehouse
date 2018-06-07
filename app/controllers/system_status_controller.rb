79edb642fb9fae383e400b3e035607a7ab6ed790
Todd Blackman <tdblackman@gmail.com>
2018-06-07 10:11:37 -0400

    Check on cache

--- app/controllers/system_status_controller.rb
+++ app/controllers/system_status_controller.rb
@@ -1,13 +1,25 @@
 class SystemStatusController < ApplicationController
   skip_before_action :authenticate_user!
 
-  # Provide a path for nagios or other system checker to determine if the system is 
+  # Provide a path for nagios or other system checker to determine if the system is
   # operational
   def operational
     user_count = User.all.count
     data_source_count = GrdaWarehouse::DataSource.count
     patient_count = Health::Patient.count
     if user_count > 0 && data_source_count.present? && patient_count.present?
+      render plain: 'OK'
+    else
+      render status: 500, plain: 'FAIL'
+    end
+  end
+
+  def cache_status
+    set_value = SecureRandom.hex(10)
+    Rails.cache.write('cache-test',  set_value)
+    pulled_value = Rails.cache.read('cache-test')
+
+    if set_value == pulled_value
       render plain: 'OK'
     else
       render status: 500, plain: 'FAIL'
