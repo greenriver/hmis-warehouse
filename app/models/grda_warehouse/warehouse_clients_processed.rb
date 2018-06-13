d9e0632d33e86a8eee7fdf49ff22e2eda454241c
Aaron Michal <amichal@greenriver.org>
2018-06-13 16:48:19 -0400

    encapsulate the cohort calcs into thier own module and make a note to make batch versions

--- app/models/grda_warehouse/warehouse_clients_processed.rb
+++ app/models/grda_warehouse/warehouse_clients_processed.rb
@@ -39,7 +39,9 @@ class GrdaWarehouse::WarehouseClientsProcessed < GrdaWarehouseBase
         days_homeless_last_three_years: calcs.all_homeless_in_last_three_years[client_id] || 0,
         literally_homeless_last_three_years: calcs.all_literally_homeless_last_three_years[client_id] || 0,
       )
-      processed.assign_attributes cohort_attributes(processed.client)
+      processed.assign_attributes(
+        CohortCalcs.new(processed.client).as_hash
+      )
       processed.save if processed.changed?
       GrdaWarehouse::Hud::Client.destination.clear_view_cache(client_id)
     end
@@ -255,56 +257,66 @@ class GrdaWarehouse::WarehouseClientsProcessed < GrdaWarehouseBase
     end
   end
 
-  # stats used by Cohort reports
-  def self.cohort_attributes(client)
-    puts "cohort_attributes   #{client.to_s}"
-    {
-      disability_verification_date: client.most_recent_verification_of_disability&.created_at&.to_date,
-      enrolled_homeless_shelter: client.service_history_enrollments.homeless_sheltered.ongoing.exists?,
-      enrolled_homeless_unsheltered: client.service_history_enrollments.homeless_unsheltered.ongoing.exists?,
-      enrolled_permanent_housing: client.service_history_enrollments.permanent_housing.ongoing.exists?,
-      eto_coordinated_entry_assessment_score: client.most_recent_coc_assessment_score,
-      household_members: cohort_household_members(client),
-      last_homeless_visit: cohort_last_homeless_visit(client),
-      missing_documents: cohort_missing_documents(client),
-      open_enrollments: cohort_open_enrollments(client),
-      rrh_desired: client.rrh_desired,
-      vispdat_priority_score: client.calculate_vispdat_priority_score,
-      vispdat_score: client.most_recent_vispdat_score
-    }
-  end
+  # stats used by Cohort reports --
+  # FIXME: these are N+1 on client *and then some* so ideally
+  # thy get rewritten as batched versions
+  # like StatsCalculator
+  class CohortCalcs
+    attr_reader :client
 
-  def self.cohort_household_members(client)
-    households = client.households
-    if households.present?
-      households.values.flatten.map do |member|
-        "#{member['FirstName']} #{member['LastName']} (#{member['age']} in #{member['date'].year})"
-      end.uniq.join('; ')
+    def initialize(client)
+      @client = client
     end
-  end
 
-  def self.cohort_last_homeless_visit(client)
-    client.last_homeless_visits.map do |row|
-      row.join(': ')
-    end.join('; ')
-  end
+    def as_hash
+      {
+        disability_verification_date: client.most_recent_verification_of_disability&.created_at&.to_date,
+        enrolled_homeless_shelter: client.service_history_enrollments.homeless_sheltered.ongoing.exists?,
+        enrolled_homeless_unsheltered: client.service_history_enrollments.homeless_unsheltered.ongoing.exists?,
+        enrolled_permanent_housing: client.service_history_enrollments.permanent_housing.ongoing.exists?,
+        eto_coordinated_entry_assessment_score: client.most_recent_coc_assessment_score,
+        household_members: cohort_household_members(client),
+        last_homeless_visit: cohort_last_homeless_visit(client),
+        missing_documents: cohort_missing_documents(client),
+        open_enrollments: cohort_open_enrollments(client),
+        rrh_desired: client.rrh_desired,
+        vispdat_priority_score: client.calculate_vispdat_priority_score,
+        vispdat_score: client.most_recent_vispdat_score
+      }
+    end
 
-  def self.cohort_missing_documents(client)
-    required_documents = GrdaWarehouse::AvailableFileTag.document_ready
-    client.document_readiness(required_documents).select do |m|
-      m.available == false
-    end.map(&:name).join('; ')
-  end
-
-  def self.cohort_open_enrollments(client)
-    client.service_history_enrollments.ongoing.
-      distinct.residential.
-      pluck(:project_type).map do |project_type|
-        if project_type == 13
-          [project_type, 'RRH']
-        else
-          [project_type, ::HUD.project_type_brief(project_type)]
-        end
+    private def cohort_household_members(client)
+      households = client.households
+      if households.present?
+        households.values.flatten.map do |member|
+          "#{member['FirstName']} #{member['LastName']} (#{member['age']} in #{member['date'].year})"
+        end.uniq.join('; ')
       end
+    end
+
+    private def cohort_last_homeless_visit(client)
+      client.last_homeless_visits.map do |row|
+        row.join(': ')
+      end.join('; ')
+    end
+
+    private def cohort_missing_documents(client)
+      required_documents = GrdaWarehouse::AvailableFileTag.document_ready
+      client.document_readiness(required_documents).select do |m|
+        m.available == false
+      end.map(&:name).join('; ')
+    end
+
+    private def cohort_open_enrollments(client)
+      client.service_history_enrollments.ongoing.
+        distinct.residential.
+        pluck(:project_type).map do |project_type|
+          if project_type == 13
+            [project_type, 'RRH']
+          else
+            [project_type, ::HUD.project_type_brief(project_type)]
+          end
+        end
+    end
   end
 end
