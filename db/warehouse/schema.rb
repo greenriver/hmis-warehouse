fdf347afb646c65a05eb9ed914a9667f46b4ada5
Elliot Anders <eanders@greenriver.org>
2018-06-16 08:37:36 -0400

    Enabled flag on report definitions

--- db/warehouse/schema.rb
+++ db/warehouse/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615232905) do
+ActiveRecord::Schema.define(version: 20180616123004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1591,7 +1591,8 @@ ActiveRecord::Schema.define(version: 20180615232905) do
     t.text    "url"
     t.text    "name"
     t.text    "description"
-    t.integer "weight",       default: 0, null: false
+    t.integer "weight",       default: 0,    null: false
+    t.boolean "enabled",      default: true, null: false
   end
 
   create_table "report_tokens", force: :cascade do |t|
