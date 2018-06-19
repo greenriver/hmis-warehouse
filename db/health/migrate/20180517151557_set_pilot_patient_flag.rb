815739617044674bc991dbd9c6caf3ec2b83a2db
Todd Blackman <tdblackman@gmail.com>
2018-06-19 11:45:19 -0400

    do data update in a separate migration so there's no deadlock

--- db/health/migrate/20180517151557_set_pilot_patient_flag.rb
+++ db/health/migrate/20180517151557_set_pilot_patient_flag.rb
@@ -1,6 +1,10 @@
 class SetPilotPatientFlag < ActiveRecord::Migration
-  def change
+  def up
     # Convert all current patients to pilot pattients
-    Health::Patient.update_all(pilot: true)
+    add_column(:patients, :deleted_at, :datetime) unless column_exists? :patients, :deleted_at
+  end
+
+  def down
+    remove_column :patients, :deleted_at
   end
 end
