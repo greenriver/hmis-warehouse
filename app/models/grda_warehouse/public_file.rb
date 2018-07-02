7adf1ae19ff17790ce7a2c062b6b17100ade7c67
Elliot Anders <eanders@greenriver.org>
2018-07-02 16:28:21 -0400

    Allow 4mb file uploads

--- app/models/grda_warehouse/public_file.rb
+++ app/models/grda_warehouse/public_file.rb
@@ -2,18 +2,18 @@ module GrdaWarehouse
   class PublicFile < GrdaWarehouse::File
     include ArelHelper
     acts_as_taggable
-  
+
     validates_presence_of :name
     validate :file_exists_and_not_too_large
 
     def file_exists_and_not_too_large
       errors.add :file, "No uploaded file found" if (content&.size || 0) < 100
-      errors.add :file, "File size should be less than 2 MB" if (content&.size || 0) > 2.megabytes
+      errors.add :file, "File size should be less than 6 MB" if (content&.size || 0) > 2.megabytes
     end
 
     def self.known_locations
       {
-        'client/hmis_consent' => 'HMIS Consent Form', # app/controllers/window/clients/vispdats_controller.rb & app/controllers/window/clients/files_controller.rb 
+        'client/hmis_consent' => 'HMIS Consent Form', # app/controllers/window/clients/vispdats_controller.rb & app/controllers/window/clients/files_controller.rb
         'patient/release' => 'Patient Release Form', # app/controllers/window/health/release_forms_controller.rb
         'patient/participation' => 'Participation Form', # app/controllers/window/health/participation_forms_controller.rb
         'patient/participation_spanish' => 'Participation Form (SPANISH)', # app/controllers/window/health/participation_forms_controller.rb
@@ -31,4 +31,4 @@ module GrdaWarehouse
     end
 
   end
-end  
+end
