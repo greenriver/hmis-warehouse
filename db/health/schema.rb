f46e9ee400426f096056ace56b4d4682d1a0c076
Megan Eborn <mneborn@gmail.com>
2018-07-14 13:24:26 -0600

    update cha review by anyone, add reviewer input, and update to allow name that is not review_by user

--- db/health/schema.rb
+++ db/health/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711174711) do
+ActiveRecord::Schema.define(version: 20180714180735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
