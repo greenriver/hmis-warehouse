1025778393b666ae9ceefe8a97b9d58debfb7b67
everestwitman <everesttwitman@gmail.com>
2018-07-23 14:32:39 -0400

    DRYer with controller inheritance

--- app/controllers/warehouse_reports/hashed_only_hmis_exports_controller.rb
+++ app/controllers/warehouse_reports/hashed_only_hmis_exports_controller.rb
@@ -1,34 +1,5 @@
 module WarehouseReports
-  class HashedOnlyHmisExportsController < ApplicationController
-    before_action :require_can_export_hmis_data!
-    before_action :set_export, only: [:show, :destroy]
-    before_action :set_jobs, only: [:index, :running, :create]
-    before_action :set_exports, only: [:index, :running, :create]
-
-    def index
-      @filter = ::Filters::HmisExport.new(user_id: current_user.id)
-      @all_project_names = GrdaWarehouse::Hud::Project.order(ProjectName: :asc).pluck(:ProjectName)
-    end
-
-    def running
-
-    end
-
-    def set_jobs
-      @job_reports = Delayed::Job.where(queue: :hmis_six_one_one_export).order(run_at: :desc).map do |job|
-        parameters = YAML.load(job.handler).job_data['arguments'].first
-        parameters.delete('_aj_symbol_keys')
-        parameters['project_ids'] = parameters.delete('projects')
-        report = GrdaWarehouse::HmisExport.new(parameters)
-        [job.run_at, report]
-      end
-    end
-
-    def set_exports
-      @exports = export_source.ordered.
-        for_list.
-        limit(50)
-    end
+  class HashedOnlyHmisExportsController < HmisExportsController
 
     def create
       @filter = ::Filters::HmisExport.new(report_params.merge(user_id: current_user.id, hash_status: "4"))
@@ -45,19 +16,6 @@ module WarehouseReports
       respond_with @export, location: warehouse_reports_hashed_only_hmis_exports_path
     end
 
-    def show
-      # send_data GrdaWarehouse::Hud::Geography.to_csv(scope: @sites), filename: "site-#{Time.now}.csv"
-      send_data @export.content, filename: "HMIS_export_#{Time.now.to_s.gsub(',', '')}.zip", type: @export.content_type, disposition: 'attachment'
-    end
-
-    def set_export
-      @export = export_source.find(params[:id].to_i)
-    end
-
-    def export_source
-      GrdaWarehouse::HmisExport
-    end
-
     def report_params
       params.require(:filter).permit(
         :start_date,
@@ -70,10 +28,5 @@ module WarehouseReports
         data_source_ids: []
       )
     end
-
-    def flash_interpolation_options
-      { resource_name: 'Export' }
-    end
-
   end
 end
