e72ae2eb1de371aec403a709857c2aab7250e8d3
everestwitman <everesttwitman@gmail.com>
2018-07-31 12:15:34 -0400

    affilated and residential program relations

--- app/models/grda_warehouse/hud/affiliation.rb
+++ app/models/grda_warehouse/hud/affiliation.rb
@@ -4,6 +4,7 @@ module GrdaWarehouse::Hud
     self.table_name = 'Affiliation'
     self.hud_key = :AffiliationID
     acts_as_paranoid column: :DateDeleted
+    has_many :users
 
     def self.hud_csv_headers(version: nil)
       [
@@ -19,6 +20,7 @@ module GrdaWarehouse::Hud
     end
 
     belongs_to :project, class_name: GrdaWarehouse::Hud::Project.name, primary_key: [:ProjectID, :data_source_id], foreign_key: [:ProjectID, :data_source_id], inverse_of: :affiliations
+    belongs_to :residential_project, class_name: GrdaWarehouse::Hud::Project.name, primary_key: [:ProjectID, :data_source_id], foreign_key: [:ResProjectID, :data_source_id], inverse_of: :affiliations
     belongs_to :export, **hud_belongs(Export), inverse_of: :affiliations
   end
 end
