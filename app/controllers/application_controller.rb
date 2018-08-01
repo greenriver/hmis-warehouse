ca9fe7b4b79c7c2bfe2a8e74f3e0714c1053cdf2
Elliot Anders <eanders@greenriver.org>
2018-08-01 09:33:38 -0400

    Allow reponses for text requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html, :js, :json, :csv, :text
+  respond_to :html, :js, :json, :csv
 
   include ControllerAuthorization
   include ActivityLogger
