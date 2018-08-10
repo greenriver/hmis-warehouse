ff669bb78f0f894ebe2324c68c92c31660b0539d
Elliot Anders <eanders@greenriver.org>
2018-08-10 15:33:17 -0400

    Adjustments to date range

--- app/controllers/warehouse_reports/health/agency_performance_controller.rb
+++ app/controllers/warehouse_reports/health/agency_performance_controller.rb
@@ -5,7 +5,9 @@ module WarehouseReports::Health
     before_action :require_can_administer_health!
 
     def index
-      @report = Health::AgencyPerformance.new(range: (1.months.ago.to_date..Date.today))
+      start_date = 1.months.ago.beginning_of_month.to_date
+      end_date = start_date.end_of_month
+      @report = Health::AgencyPerformance.new(range: (start_date..end_date))
 
       @agencies = @report.agency_counts()
 
