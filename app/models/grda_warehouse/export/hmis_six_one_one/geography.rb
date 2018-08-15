b2bdcc27fd2c4d5ca07ff9d743e30493defef564
Elliot Anders <eanders@greenriver.org>
2018-08-15 16:07:09 -0400

    Allow overrides for various project descriptors for HMIS export and LSA

--- app/models/grda_warehouse/export/hmis_six_one_one/geography.rb
+++ app/models/grda_warehouse/export/hmis_six_one_one/geography.rb
@@ -7,6 +7,41 @@ module GrdaWarehouse::Export::HMISSixOneOne
 
     belongs_to :project_with_deleted, class_name: GrdaWarehouse::Hud::WithDeleted::Project.name, primary_key: [:ProjectID, :data_source_id], foreign_key: [:ProjectID, :data_source_id], inverse_of: :geographies
 
+    def apply_overrides row, data_source_id:
+      if override = geography_type_override_for(geography_id: row[:GeographyID].to_i, data_source_id: data_source_id)
+        row[:GeographyType] = override
+      end
 
+      if override = geocode_override_for(geography_id: row[:GeographyID].to_i, data_source_id: data_source_id)
+        row[:Geocode] = override
+      end
+      return row
+    end
+
+    def geography_type_override_for geography_id:, data_source_id:
+      @geography_type_overrides ||= self.class.where.not(geography_type_override: nil).
+        pluck(:data_source_id, :id, :geography_type_override).
+        map do |data_source_id, geography_id, geography_type_override|
+          if geography_type_override.present?
+            [[data_source_id, geography_id], geography_type_override]
+          else
+            nil
+          end
+        end.compact.to_h
+      @geography_type_overrides[[data_source_id, geography_id]]
+    end
+
+    def geocode_override_for geography_id:, data_source_id:
+      @geocode_overrides ||= self.class.where.not(geocode_override: nil).
+        pluck(:data_source_id, :id, :geocode_override).
+        map do |data_source_id, geography_id, geocode_override|
+          if geocode_override.present?
+            [[data_source_id, geography_id], geocode_override]
+          else
+            nil
+          end
+        end.compact.to_h
+      @geocode_overrides[[data_source_id, geography_id]]
+    end
   end
 end
