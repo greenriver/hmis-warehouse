cfa8aa21e664101c509765066631d2584ca925f8
Elliot Anders <eanders@greenriver.org>
2018-08-24 11:00:18 -0400

    removing debugging for LSA

--- app/models/report_generators/lsa/fy2018/all.rb
+++ app/models/report_generators/lsa/fy2018/all.rb
@@ -77,7 +77,7 @@ module ReportGenerators::Lsa::Fy2018
 
     def create_hmis_csv_export
       # debugging
-      return GrdaWarehouse::HmisExport.find(18)
+      # return GrdaWarehouse::HmisExport.find(18)
 
       Exporters::HmisSixOneOne::Base.new(
         start_date: '2012-10-01', # @report_end # using 10/1/2012 so we can determine continuous homelessness
@@ -195,7 +195,7 @@ module ReportGenerators::Lsa::Fy2018
 
     def remove_temporary_rds
       # Commented out for debugging
-      # @rds&.terminate!
+      @rds&.terminate!
     end
 
     def setup_hmis_table_structure
