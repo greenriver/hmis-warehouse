02768db9b1ed491fc45e2eebd507f4173c3507a8
Todd Blackman <tdblackman@gmail.com>
2018-09-13 10:02:10 -0400

    migration fixes

--- db/warehouse/migrate/20170815174824_alter_unique_enrollment_constraint.rb
+++ db/warehouse/migrate/20170815174824_alter_unique_enrollment_constraint.rb
@@ -2,7 +2,7 @@ class AlterUniqueEnrollmentConstraint < ActiveRecord::Migration
   def change
     model = GrdaWarehouse::Hud::Enrollment
     cols = [:data_source_id, model.hud_csv_headers.first].map(&:to_s)
-    remove_index model.table_name, name: "unk_#{model.table_name}"
+    #remove_index model.table_name, name: "unk_#{model.table_name}"
     cols = [:data_source_id, model.hud_csv_headers.first, :PersonalID].map(&:to_s)
     add_index model.table_name,  cols, name: "unk_#{model.table_name}", unique: true
   end
