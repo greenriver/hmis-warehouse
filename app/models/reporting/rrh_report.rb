2bf0f17b3ef5848ede5319a2d3f7ad4ee7e1add9
Elliot Anders <eanders@greenriver.org>
2018-09-25 20:43:31 -0400

    WIP; RRH report in R

--- app/models/reporting/rrh_report.rb
+++ app/models/reporting/rrh_report.rb
@@ -130,8 +130,8 @@ module Reporting
           housed_2 <- housed %>% filter(residential_project == program_2)
         }
 
-        print(head(housed_1, n=5))
-        print(head(housed_2, n=5))
+        # print(head(housed_1, n=5))
+        # print(head(housed_2, n=5))
 
         #retrieve post-housing ES or SO records for clients housed through the first program
         first_housed_dates <- housed_1 %>%
@@ -141,20 +141,21 @@ module Reporting
           distinct(client_id, .keep_all=TRUE) %>%
           select(client_id, housing_exit)
 
-        print(head(first_housed_dates, n=5))
+        # print(head(first_housed_dates, n=5))
 
         services_with_housed_dates <- returns %>%
           filter(client_id %in% first_housed_dates$client_id) %>%
           left_join(first_housed_dates, on = c("client_id" = "client_id"))
 
-        print(head(services_with_housed_dates, n=5))
+        # print(head(services_with_housed_dates, n=5))
+        # print(head(services_with_housed_dates$housing_exit, n=5))
 
         post_housing <- services_with_housed_dates %>%
           filter(start_date > housing_exit) %>%
           filter(project_type %in% c(1,2,4)) %>%
           arrange(client_id, start_date)
 
-        print(head(post_housing, n=5))
+        # print(head(post_housing, n=5))
 
         post_housing <- post_housing[!is.na(post_housing$start_date),]
         post_housing <- post_housing[!is.na(post_housing$end_date),]
@@ -170,7 +171,7 @@ module Reporting
           select(-dates)
         post_housing_1 <- post_housing
 
-        print(head(post_housing_1, n=5))
+        # print(head(post_housing_1, n=5))
 
         #retrieve post-housing ES or SO records for clients housed through the second program
         first_housed_dates <- housed_2 %>%
@@ -180,32 +181,76 @@ module Reporting
           distinct(client_id, .keep_all=TRUE) %>%
           select(client_id, housing_exit)
 
-        print(head(first_housed_dates, n=5))
+        # print(head(first_housed_dates, n=5))
 
-        # services_with_housed_dates <- returns %>%
-        #   filter(client_id %in% first_housed_dates$client_id) %>%
-        #   left_join(first_housed_dates, on = c("client_id" = "client_id"))
+        services_with_housed_dates <- returns %>%
+          filter(client_id %in% first_housed_dates$client_id) %>%
+          left_join(first_housed_dates, on = c("client_id" = "client_id"))
 
-        # post_housing <- services_with_housed_dates %>%
-        #   filter(start_date > housing_exit) %>%
-        #   filter(project_type %in% c(1,2,4)) %>%
-        #   arrange(client_id, start_date)
+        post_housing <- services_with_housed_dates %>%
+          filter(start_date > housing_exit) %>%
+          filter(project_type %in% c(1,2,4)) %>%
+          arrange(client_id, start_date)
 
-        # post_housing <- post_housing[!is.na(post_housing$start_date),]
-        # post_housing <- post_housing[!is.na(post_housing$end_date),]
-        # l <- mapply(seq.Date, post_housing$start_date, post_housing$end_date, 1)
-        # df2 <- data.frame(group = rep(post_housing$client_id,sapply(l,length)),
-        #                   dates = unlist(l))
-        # unique_dates = aggregate(dates ~ group, df2, function(x) length(unique(x)))
-        # post_housing = left_join(post_housing, unique_dates, by=c("client_id"="group"))
-        # post_housing %<>% mutate(
-        #   adjusted_days_homeless = dates
-        # ) %>%
-        #   select(-dates)
-        # post_housing_2 <-post_housing
+        post_housing <- post_housing[!is.na(post_housing$start_date),]
+        post_housing <- post_housing[!is.na(post_housing$end_date),]
+        l <- mapply(seq.Date, post_housing$start_date, post_housing$end_date, 1)
+        df2 <- data.frame(group = rep(post_housing$client_id,sapply(l,length)),
+                          dates = unlist(l))
+        unique_dates = aggregate(dates ~ group, df2, function(x) length(unique(x)))
+        post_housing = left_join(post_housing, unique_dates, by=c("client_id"="group"))
+        post_housing %<>% mutate(
+          adjusted_days_homeless = dates
+        ) %>%
+          select(-dates)
+        post_housing_2 <-post_housing
 
         # print(head(post_housing_2, n=5))
 
+        #number housed
+        num_housed_1 <- paste(length(unique(housed_1$client_id)), "total clients", sep=" ")
+        num_housed_2 <- paste(length(unique(housed_2$client_id)), "total clients", sep=" ")
+
+        # print(num_housed_1)
+        # print(num_housed_2)
+
+        housedPlot_1 <- housed_1 %>%
+          arrange(desc(housed_date)) %>%
+          group_by(month_year) %>%
+          summarise(
+          n_clients = n_distinct(client_id)
+          ) %>%
+          mutate(
+            cumsum = cumsum(n_clients)
+          ) %>%
+          filter(month_year > as.Date('2012-01-01')) %>%
+          filter(month_year < as.Date('2018-08-01'))
+
+        print(housedPlot_1)
+
+        housedPlot_2 <- housed_2 %>%
+          arrange(desc(housed_date)) %>%
+          group_by(month_year) %>%
+          summarise(
+            n_clients = n_distinct(client_id)
+          ) %>%
+          mutate(
+            cumsum = cumsum(n_clients)
+          ) %>%
+          filter(month_year > as.Date('2012-01-01')) %>%
+          filter(month_year < as.Date('2018-08-01'))
+
+        print(housedPlot_2)
+
+        time_to_housing_1 <- paste(round(mean(housed_1$search_end - housed_1$search_start, na.rm=TRUE), digits=2), "days to find housing", sep=" ")
+
+        time_to_housing_2 <- paste(round(mean(housed_2$search_end - housed_2$search_start, na.rm = TRUE), digits = 2), "days to find housing", sep=" ")
+
+        time_in_housing_1 <- paste(round(mean(housed_1$housing_exit - housed_1$housed_date, na.rm=TRUE), digits=2), "days in program", sep=" ")
+
+        time_in_housing_2 <- paste(round(mean(housed_2$housing_exit - housed_2$housed_date, na.rm = TRUE), digits = 2), "days in program", sep=" ")
+
+        success_failure_1 <-
       REOF
 
       # housed_file.close
