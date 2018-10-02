b1da1b99085c5e11f784d817f0e3fc71597e7c0c
Elliot Anders <eanders@greenriver.org>
2018-10-02 11:19:35 -0400

    limit access to anomalies report

--- app/controllers/warehouse_reports/anomalies_controller.rb
+++ app/controllers/warehouse_reports/anomalies_controller.rb
@@ -4,13 +4,21 @@ module WarehouseReports
     include ArelHelper
 
     def index
-      @new = anomaly_source.newly_minted.order(created_at: :asc).preload(:client, :user)
-      @unresolved = anomaly_source.unresolved.order(created_at: :asc).preload(:client, :user)
-      @resolved = anomaly_source.resolved.order(created_at: :asc).preload(:client, :user)
+      @new = anomaly_scope.newly_minted.order(created_at: :asc).preload(:client, :user)
+      @unresolved = anomaly_scope.unresolved.order(created_at: :asc).preload(:client, :user)
+      @resolved = anomaly_scope.resolved.order(created_at: :asc).preload(:client, :user)
     end
 
     def anomaly_source
       GrdaWarehouse::Anomaly
+    end
+
+    def anomaly_scope
+      if can_edit_anything_super_user?
+        anomaly_source
+      else
+        anomaly_source.where(submitted_by: current_user.id)
+      end
     end
   end
 end
