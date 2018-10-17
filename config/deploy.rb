e11847ff113d33b5096615a6b1320495e7de72cd
Elliot Anders <eanders@greenriver.org>
2018-10-17 12:40:31 -0400

    namespace for redis

--- config/deploy.rb
+++ config/deploy.rb
@@ -132,16 +132,6 @@ namespace :deploy do
       end
     end
   end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
 
 after 'deploy:migrating', :check_for_bootability do
