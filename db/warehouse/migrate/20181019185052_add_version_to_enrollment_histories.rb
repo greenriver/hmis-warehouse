947db9e7af003da727cb81c0f391f98219d89c8a
Elliot Anders <eanders@greenriver.org>
2018-10-19 14:58:58 -0400

    Adding version column for enrollment histories

--- db/warehouse/migrate/20181019185052_add_version_to_enrollment_histories.rb
+++ db/warehouse/migrate/20181019185052_add_version_to_enrollment_histories.rb
@@ -1,5 +1,5 @@
 class AddVersionToEnrollmentHistories < ActiveRecord::Migration
   def change
-    add_column :enrollment_change_histories, :version, :integer, null: false, default: false
+    add_column :enrollment_change_histories, :version, :integer, null: false, default: 1
   end
 end
