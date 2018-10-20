9f484cc7bddbd1b664f8b9afce554a27d1689a0c
Elliot Anders <eanders@greenriver.org>
2018-10-20 08:40:10 -0400

    Fix for empty client data and enrollment history

--- app/models/grda_warehouse/enrollment_change_history.rb
+++ app/models/grda_warehouse/enrollment_change_history.rb
@@ -17,22 +17,25 @@ module GrdaWarehouse
     def self.create_for_clients_on_date! client_ids:, date:
       clients = GrdaWarehouse::Hud::Client.destination.where(id: client_ids)
       rows = clients.map do |client|
-        attributes_for_client_on_date(client: client, date: date)
+        attributes_for_client_on_date(client: client, date: date) rescue nil
+      end.compact
+      if rows.present?
+        self.new.insert_batch(self, rows.first.keys, rows.map(&:values), transaction: false)
       end
-      self.new.insert_batch(self, rows.first.keys, rows.map(&:values), transaction: false)
     end
 
     def self.attributes_for_client_on_date client:, date:
-      {
+      attributes_for_client = {
         client_id: client.id, 
         on: date,
-        residential: client.enrollments_for_rollup(en_scope: client.scope_for_residential_enrollments).to_json,
-        other: client.enrollments_for_rollup(en_scope: client.scope_for_other_enrollments).to_json,
-        days_homeless: client.days_homeless,
         created_at: Time.now,
         updated_at: Time.now,
         version: 1,
       }
+      attributes_for_client[:residential] = client.enrollments_for_rollup(en_scope: client.scope_for_residential_enrollments).to_json rescue []
+      attributes_for_client[:other] = client.enrollments_for_rollup(en_scope: client.scope_for_other_enrollments).to_json rescue []
+      attributes_for_client[:days_homeless] = client.days_homeless rescue 0
+      return attributes_for_client
     end
   end
 end
