697066aeae145186ef2831187730bde978a1b659
Elliot Anders <eanders@greenriver.org>
2018-10-23 09:10:28 -0400

    Correctly format patient address for claims

--- app/models/health/claim.rb
+++ app/models/health/claim.rb
@@ -117,7 +117,7 @@ module Health
           b.SBR 'P', '18', nil, nil, nil, nil, nil, nil, 'MC'
           b.NM1 'IL', '1', pr.last_name, pr.first_name, nil, nil, nil, 'MI', pr.medicaid_id
           b.N3 pr.address_line_1.presence || @sender.address_1
-          b.N4 city_state_zip.join(', ')
+          b.N4 *city_state_zip
           b.DMG 'D8', pr.birthdate&.strftime('%Y%m%d'), pr.gender || 'U'
           b.NM1 'PR', '2', @sender.receiver_name, nil, nil, nil, nil, 'PI', @sender.receiver_id
           valid_qa = patient_qa.where(hqa_t[:date_of_activity].lteq(max_date)).
@@ -206,123 +206,7 @@ module Health
         "Running since #{started_at}"
       end
     end
-
-    # def isa_line
-    #   line = "ISA*00*#{' ' * 10}*00*#{' ' * 10}*ZZ*"
-    #   line += "#{@sender.pid}#{@sender.sl}".ljust(15)
-    #   line += '*ZZ*'
-    #   line += "#{@sender.receiver_id}".ljust(15)
-    #   line += "*#{created_at.strftime('%y%m%d*%H%M')}"
-    #   line += "*#{repetition_separator}*00501*"
-    #   line += "#{@isa_control_number}".rjust(9, '0')
-    #   line += "*0*#{interchange_usage_indicator}*#{component_element_separator}~"
-    # end
-
-    # def gs_line
-    #   line = 'GS*HC*'
-    #   line += "#{@sender.pid}#{@sender.sl}"
-    #   line += "*#{@sender.receiver_id}"
-    #   line += "*#{created_at.strftime('%Y%m%d*%H%M')}"
-    #   line += "*#{@group_control_number}"
-    #   line += "*X*#{implementation_convention_reference_number}~"
-    # end
-
-    # def st_line
-    #   line = 'ST*837*'
-    #   line += id.to_s.rjust(4, '0')
-    #   line += "*#{implementation_convention_reference_number}~"
-    # end
-
-    # def bht_line
-    #   line = 'BHT*0019*'
-    #   line += '00' # transaction set purpose code, 18 for resubmit
-    #   line += '*'
-    #   line += id.to_s.rjust(4, '0')
-    #   line += "*#{created_at.strftime('%Y%m%d*%H%M')}"
-    #   line += '*CH~'
-    # end
-
-    # def submitter_line
-    #   line = 'NM1*41*2'
-    #   line += "*#{@sender.mmis_enrollment_name}#{'*' * 5}46*"
-    #   line += "#{@sender.pid}#{@sender.sl}"
-    #   line += '~'
-    # end
-
-    # def submitter_edi_contact_line
-    #   line = 'PER*IC'
-    #   line += "*#{@sender.key_contact_first_name} #{@sender.key_contact_last_name}"
-    #   line += "*TE*#{@sender.key_contact_phone.delete('^0-9')}~"
-    # end
-
-    # def receiver_line
-    #   line = 'NM1*40*2'
-    #   line += "*#{@sender.receiver_name}#{'*' * 5}46"
-    #   line += "*#{@sender.receiver_id}"
-    #   line += '~'
-    # end
-
-    # def open_2005_loop_line
-    #   @hl += 1
-    #   line = "HL*#{@hl}**20*1~"
-    # end
-
-    # def billing_2010_aa_line
-    #   line = "NM1*85*2"
-    #   line += "*#{@sender.mmis_enrollment_name}#{'*' * 5}XX*#{@sender.npi}~\n"
-    #   line += "N3*#{@sender.address_1}~\n"
-    #   line += "N4*#{@sender.city}*#{@sender.state}*#{@sender.zip.delete('^0-9')}~\n"
-    #   line += "REF*EI*#{@sender.ein}~"
-    # end
-
-    # def patient_header patient
-    #   @hl += 1
-    #   pr = patient.patient_referral
-    #   line = "HL*#{@hl}*1*22*0~\n"
-    #   line += "SBR*P*18#{'*' * 7}MC~\n"
-    #   line += "NM1*IL*1*#{pr.last_name}*#{pr.first_name}#{'*' * 4}MI*#{pr.medicaid_id}~\n"
-    #   line += "N3*#{pr.address_line_1 || @sender.address_1}~\n"
-    #   line += "N4*#{pr.address_city || @sender.city}*#{pr.address_state || @sender.state}*#{pr.address_zip || @sender.zip}~\n"
-    #   line += "DMG*D8*#{pr.birthdate&.strftime('%Y%m%d')}*#{pr.gender || 'U'}~"
-    # end
-
-    # def patient_payer patient
-    #   pr = patient.patient_referral
-    #   line = "NM1*PR*2"
-    #   line += "*#{@sender.receiver_name}#{'*' * 5}PI"
-    #   line += "*#{@sender.receiver_id}"
-    #   line += '~'
-    # end
-
-    # def patient_claims_header patient
-    #   pr = patient.patient_referral
-    #   line = "CLM*#{pr.id}*0***11"
-    #   line += "#{component_element_separator}B#{component_element_separator}1*Y*A*Y*Y~"
-    # end
-
-    # def patient_diagnosis patient
-    #   pr = patient.patient_referral
-    #   line = "HI*ABK#{component_element_separator}Z029~"
-    # end
-
-    # def claim_lines qa
-    #   @lx += 1
-    #   line = "LX*#{@lx}~\n"
-    #   line += "SV1*HC#{component_element_separator}"
-    #   line += "#{qa.procedure_code}#{component_element_separator}#{qa.modifiers.join(component_element_separator).to_s}"
-    #   line += "*0*UN*1***1~\n"
-    #   line += "DTP*472*D8*#{qa.date_of_activity.strftime('%Y%m%d')}~"
-    # end
-
-    # def trailer
-    #   lines = self.claims_file.lines.count - 1
-    #   line = "SE*#{lines}*#{id.to_s.rjust(4, '0')}~\n"
-    #   line += "GE*1*#{@group_control_number}~\n"
-    #   line += "IEA*1*"
-    #   line += "#{@isa_control_number}".rjust(9, '0')
-    #   line += '~'
-    # end
-
+    
     def implementation_convention_reference_number
       '005010X222A1'
     end
