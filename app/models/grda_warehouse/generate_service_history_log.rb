351871fb22a7950e86283c546f90ada837b896ff
Sandy Wise <sandy@greenriver.org>
2018-10-23 11:13:02 -0400

    Compute latest updated_at in db

--- app/models/grda_warehouse/generate_service_history_log.rb
+++ app/models/grda_warehouse/generate_service_history_log.rb
@@ -5,7 +5,7 @@ class GrdaWarehouse::GenerateServiceHistoryLog < GrdaWarehouseBase
   has_many :generate_service_history_batch_logs
 
   def import_time
-    completed_at = generate_service_history_batch_logs.max { |left, right| left.updated_at <=> right.updated_at }.updated_at
+    completed_at = generate_service_history_batch_logs.maximum(:updated_at)
     seconds = ((completed_at - started_at)/1.minute).round * 60
     distance_of_time_in_words(seconds)
   end
