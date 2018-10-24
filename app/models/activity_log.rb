acdaae8a6fa880038da210fd0d04308396100a70
Sandy Wise <sandy@greenriver.org>
2018-10-24 10:18:11 -0400

    More page cleanup

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,14 +2,6 @@ class ActivityLog < ActiveRecord::Base
 
   belongs_to :user
 
-  def describe_action
-    if action_name == "edit"
-      "Edit"
-    else
-      "View"
-    end
-  end
-
   def clean_object_name
     item_model&.gsub('GrdaWarehouse::Hud::', '')
   end
