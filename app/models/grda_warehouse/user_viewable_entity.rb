f8230164773c934bbfe3bca20123c84abfd93723
Sandy Wise <sandy@greenriver.org>
2018-10-30 10:56:32 -0400

    Add PaperTrail metadata to track referenced entity information

--- app/models/grda_warehouse/user_viewable_entity.rb
+++ app/models/grda_warehouse/user_viewable_entity.rb
@@ -3,8 +3,16 @@ module GrdaWarehouse
   # NOTE, this is a join table where the users are in one database and the entities are in another.  You will usually have to pluck ids to be successful.
 
   class UserViewableEntity < GrdaWarehouseBase
-    has_paper_trail
+    has_paper_trail(
+      meta: { referenced_user_id: :user_id, referenced_entity_name: :entity_name }
+    )
+    acts_as_paranoid
+
     belongs_to :entity, polymorphic: true
     belongs_to :user
+
+    def entity_name
+      entity.name
+    end
   end
 end
