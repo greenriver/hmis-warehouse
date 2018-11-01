c4fd95b02c9ffa39de1b0aa645818b89c3610036
Sandy Wise <sandy@greenriver.org>
2018-11-01 15:33:35 -0400

    Add client notes to cohort clients

--- app/models/grda_warehouse/client_notes/cohort_note.rb
+++ app/models/grda_warehouse/client_notes/cohort_note.rb
@@ -3,5 +3,10 @@ module GrdaWarehouse::ClientNotes
     def self.type_name
       "Cohort Note"
     end
+
+    # only destroyable by admins for now
+    def destroyable_by user
+      user.can_edit_cohort_clients? || user.can_manage_cohorts? # || user_id == user.id
+    end
   end
 end
