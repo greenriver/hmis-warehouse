e25ab2f402b9b5e6f0d1d3c75993b2e210558a3d
Elliot Anders <eanders@greenriver.org>
2018-11-05 15:01:40 -0500

    JS ES6 fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( print.css )
-Rails.application.config.assets.precompile += %w( chart.js table_search.js viewable_entities.js )
+Rails.application.config.assets.precompile += %w( chart.js )
 Rails.application.config.assets.precompile += ['theme/styles/*.css']
 Rails.application.config.assets.precompile += %w( moment.min.js )
 Rails.application.config.assets.precompile += %w( handsontable.full.min.css )
