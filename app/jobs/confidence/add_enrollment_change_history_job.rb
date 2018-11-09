aea243650a3d48c3183c80149af494811ab4b11c
Elliot Anders <eanders@greenriver.org>
2018-11-08 19:56:29 -0500

    Queue enrollment change histories differently, attempting to fix missing model

--- app/jobs/confidence/add_enrollment_change_history_job.rb
+++ app/jobs/confidence/add_enrollment_change_history_job.rb
@@ -1,16 +1,11 @@
 module Confidence
   class AddEnrollmentChangeHistoryJob < BaseJob
+    queue_as :low_priority
 
-    def initialize client_ids:, date:
+    def perform client_ids:, date:
       @client_ids = client_ids
-      @date = date
-    end
-
-    def perform
+      @date = date.to_date
       GrdaWarehouse::EnrollmentChangeHistory.create_for_clients_on_date! client_ids: @client_ids, date: @date
-    end
-
-    def enqueue(job, queue: :low_priority)
     end
 
     def max_attempts
--- app/models/grda_warehouse/enrollment_change_history.rb
+++ app/models/grda_warehouse/enrollment_change_history.rb
@@ -10,7 +10,7 @@ module GrdaWarehouse
         joins(:source_enrollments).
         merge(GrdaWarehouse::Hud::Enrollment.open_during_range(range)).
         pluck_in_batches(:id, batch_size: 250) do |batch|
-          Delayed::Job.enqueue(::Confidence::AddEnrollmentChangeHistoryJob.new(client_ids: batch, date: date), queue: :low_priority)
+          Confidence::AddEnrollmentChangeHistoryJob.perform_later(client_ids: batch, date: date.to_s)
       end
     end
 
