4f9f11f079dc20382f0de612fa07a36a2cc88f9d
Sandy Wise <sandy@greenriver.org>
2018-11-09 14:31:02 -0500

    Remove project_type field from nightly_census_by_project_type_clients

--- db/warehouse/migrate/20181107184258_create_nightly_census_by_project_type_clients.rb
+++ db/warehouse/migrate/20181107184258_create_nightly_census_by_project_type_clients.rb
@@ -2,7 +2,6 @@ class CreateNightlyCensusByProjectTypeClients < ActiveRecord::Migration
   def change
     create_table :nightly_census_by_project_type_clients do |t|
       t.date :date, null: false
-      t.integer :project_type, null: false
 
       [  'literally_homeless', 'system', 'homeless', 'ph', 'es', 'th', 'so', 'sh' ].each do |type|
         [ 'veterans', 'non_veterans', 'children', 'adults', 'youth', 'families', 'individuals',
