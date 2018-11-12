97a587045a14676667c76d68041355d6b6fcbcca
Sandy Wise <sandy@greenriver.org>
2018-11-12 15:47:12 -0500

    Remove bed count from census by project type

--- db/warehouse/migrate/20181107184057_create_nightly_census_by_project_types.rb
+++ db/warehouse/migrate/20181107184057_create_nightly_census_by_project_types.rb
@@ -3,9 +3,9 @@ class CreateNightlyCensusByProjectTypes < ActiveRecord::Migration
     create_table :nightly_census_by_project_types do |t|
       t.date :date, null: false
 
-      [  'literally_homeless', 'system', 'homeless', 'ph', 'es', 'th', 'so', 'sh' ].each do |type|
+      [ 'literally_homeless', 'system', 'homeless', 'ph', 'es', 'th', 'so', 'sh' ].each do |type|
         [ 'veterans', 'non_veterans', 'children', 'adults', 'youth', 'families', 'individuals',
-          'parenting_youth', 'parenting_juveniles', 'all_clients', 'beds' ].each do |count|
+          'parenting_youth', 'parenting_juveniles', 'all_clients' ].each do |count|
           t.integer "#{type}_#{count}", default: 0
         end
       end
