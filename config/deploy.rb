950ab59c1076f6a2d27a327859167d727ceee6fb
Elliot Anders <eanders@greenriver.org>
2018-11-14 12:14:20 -0500

    Use a consistent whenever identifier

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.11.0'
 
-set :application, 'boston_hmis'
+set :application, 'warhouse'
 set :repo_url, 'git@github.com:greenriver/hmis-warehouse.git'
 set :client, ENV.fetch('CLIENT')
 
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+set :whenever_identifier, ->{ "#{fetch(:client)}-#{fetch(:application)}_#{fetch(:stage)}" }
 set :cron_user, ENV.fetch('CRON_USER') { 'ubuntu'}
 set :whenever_roles, [:cron, :production_cron, :staging_cron]
 set :whenever_command, -> { "bash -l -c 'cd #{fetch(:release_path)} && /usr/share/rvm/bin/rvmsudo ./bin/bundle exec whenever -u #{fetch(:cron_user)} --update-crontab #{fetch(:whenever_identifier)} --set \"environment=#{fetch(:rails_env)}\" '" }
