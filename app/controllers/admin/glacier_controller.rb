2de81acdb91b749f45f75f00d722430574ca5cbd
Elliot Anders <eanders@greenriver.org>
2018-11-28 14:10:17 -0500

    Increase pagination limit for glacier and adjust vault name display

--- app/controllers/admin/glacier_controller.rb
+++ app/controllers/admin/glacier_controller.rb
@@ -4,10 +4,10 @@ module Admin
     before_action :require_can_add_administrative_event!
 
     def index
-      @vaults = Glacier::Vault.all.group_by(&:id)
+      @vaults = Glacier::Vault.all.index_by(&:id)
       @total_size = Glacier::Archive.sum(:size_in_bytes)
       @archives = Glacier::Archive.all.order(upload_started_at: :desc, upload_finished_at: :desc).
-        page(params[:page]).per(5)
+        page(params[:page]).per(50)
     end
 
   end
