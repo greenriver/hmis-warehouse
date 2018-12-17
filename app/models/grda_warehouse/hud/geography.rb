014deaddd6df179bf608ff594bc5cfea3a58c1f2
Elliot Anders <eanders@greenriver.org>
2018-12-17 10:43:21 -0500

    Don't go through ProjectCoC for Geography relation to project, it can have unintended consequences

--- app/models/grda_warehouse/hud/geography.rb
+++ app/models/grda_warehouse/hud/geography.rb
@@ -48,7 +48,7 @@ module GrdaWarehouse::Hud
 
     belongs_to :project_coc, class_name: 'GrdaWarehouse::Hud::ProjectCoc', primary_key: [:ProjectID, :CoCCode, :data_source_id], foreign_key: [:ProjectID, :CoCCode, :data_source_id], inverse_of: :geographies
     belongs_to :export, **hud_belongs(Export), inverse_of: :geographies
-    has_one :project, through: :project_coc, source: :project
+    has_one :project, **hud_belongs(Project), inverse_of: :geographies
 
     scope :viewable_by, -> (user) do
       if user.can_edit_anything_super_user?
