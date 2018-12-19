c88bb245aee9cc2c803c66a695cc34247d3db35b
Sandy Wise <sandy@greenriver.org>
2018-12-19 16:27:31 -0500

    Change default date range for CAS Vacancies report to be the last month

--- app/controllers/warehouse_reports/cas/vacancies_controller.rb
+++ app/controllers/warehouse_reports/cas/vacancies_controller.rb
@@ -19,24 +19,11 @@ module WarehouseReports::Cas
       date_range_options = params.permit(range: [:start, :end])[:range]
       unless date_range_options.present?
         date_range_options = {
-            start: default_start_date,
-            end: default_end_date,
+            start: Date.today - 1.month,
+            end: Date.today,
         }
       end
       @range = ::Filters::DateRange.new(date_range_options)
-    end
-
-    def default_start_date
-      default_end_date - 1.years + 1.days
-    end
-
-    def default_end_date
-      if Date.today.month > 6
-        year = Date.today.year
-      else
-        year = Date.today.year - 1.years
-      end
-      Date.new(year, 6, 30)
     end
   end
 end
