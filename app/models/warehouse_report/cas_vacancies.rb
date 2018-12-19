2bead87803e5fcf0105520ca87fa79ddd3b7dd1c
Sandy Wise <sandy@greenriver.org>
2018-12-19 15:59:34 -0500

    CAS Vacancies report

--- app/models/warehouse_report/cas_vacancies.rb
+++ app/models/warehouse_report/cas_vacancies.rb
@@ -4,4 +4,28 @@ class WarehouseReport::CasVacancies < OpenStruct
   attr_accessor :start_date
   attr_accessor :end_date
 
+  def vacancies_by_route
+    vacancy_scope.by_route.count
+  end
+
+  def vacancies_by_program
+    vacancy_scope.by_program.count
+  end
+
+  def vacancies_by_sub_program
+    vacancy_scope.by_sub_program.count
+  end
+
+  def vacancies_by_program_type
+    vacancy_scope.by_program_type.count
+  end
+
+  def vacancies
+    vacancy_scope.order(:vacancy_created_at)
+  end
+
+  def vacancy_scope
+    GrdaWarehouse::CasVacancy.created_between(self[:start_date], self[:end_date])
+  end
+
 end
