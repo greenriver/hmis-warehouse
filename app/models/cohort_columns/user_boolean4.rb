e9a43c3fbb9a51d5592a9c11a30cbfdad57e2084
Sandy Wise <sandy@greenriver.org>
2019-02-01 16:07:11 -0500

    Make user defined cohort columns in-place editable

--- app/models/cohort_columns/user_boolean4.rb
+++ app/models/cohort_columns/user_boolean4.rb
@@ -1,6 +1,7 @@
 module CohortColumns
   class UserBoolean4 < CohortBoolean
     attribute :column, Boolean, lazy: true, default: :user_boolean_4
-    attribute :title, String, lazy: true, default: _('User Boolean 4')
+    attribute :translation_key, String, lazy: true, default: 'User Boolean 4'
+    attribute :title, String, lazy: true, default: -> (model, attr) { _(model.translation_key)}
   end
 end
