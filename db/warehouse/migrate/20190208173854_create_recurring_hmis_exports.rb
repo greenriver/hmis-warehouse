762dfe457aef0ad22c16c8373253ffdb1bf33f6e
Sandy Wise <sandy@greenriver.org>
2019-02-13 12:03:00 -0500

    Clean up migration file

--- db/warehouse/migrate/20190208173854_create_recurring_hmis_exports.rb
+++ db/warehouse/migrate/20190208173854_create_recurring_hmis_exports.rb
@@ -25,19 +25,3 @@ class CreateRecurringHmisExports < ActiveRecord::Migration
     end
   end
 end
-# attribute :start_date, Date, default: 1.years.ago.to_date
-# attribute :end_date, Date, default: Date.today
-# attribute :hash_status, Integer, default: 1
-# attribute :period_type, Integer, default: 3
-# attribute :directive, Integer, default: 2
-# attribute :include_deleted,  Boolean, default: false
-# attribute :project_ids, Array, default: []
-# attribute :project_group_ids, Array, default: []
-# attribute :organization_ids, Array, default: []
-# attribute :data_source_ids, Array, default: []
-# attribute :user_id, Integer, default: nil
-# attribute :faked_pii, Boolean, default: false
-#
-# attribute :every_n_days, Integer, default: 0
-# attribute :reporting_range, Integer, default: 0
-# attribute :reporting_range_days, Integer, default: 0
