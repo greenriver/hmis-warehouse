d2d91e3e32911f77b2cd2771de7fd6678872b4dd
Elliot Anders <eanders@greenriver.org>
2019-02-21 14:58:05 -0800

    Fix for missing names

--- app/models/unique_name.rb
+++ app/models/unique_name.rb
@@ -4,7 +4,7 @@ class UniqueName < ActiveRecord::Base
     Rails.logger.info 'Updating the unique names table'
 
     # Build double metaphone representations for all names in the database
-    names = GrdaWarehouse::Hud::Client.source.select('FirstName').distinct.pluck('FirstName')&.map(&:downcase) + GrdaWarehouse::Hud::Client.source.select('LastName').distinct.pluck('LastName')&.map(&:downcase)
+    names = GrdaWarehouse::Hud::Client.source.select('FirstName').distinct.pluck('FirstName')&.map{ |n| n&.downcase || ''} + GrdaWarehouse::Hud::Client.source.select('LastName').distinct.pluck('LastName')&.map{ |n| n&.downcase || ''}
     names.uniq.each do |name|
       double_metaphone = Text::Metaphone.double_metaphone(name)
       un = UniqueName.where(name: name).first_or_create
