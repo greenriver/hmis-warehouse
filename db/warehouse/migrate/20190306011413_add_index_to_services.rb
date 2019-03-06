3b14a758dec8e895ba0b83368416a9cf7efaa36e
Elliot Anders <eanders@greenriver.org>
2019-03-05 20:36:16 -0500

    Add missing indexes to HUD tables

---
+++ db/warehouse/migrate/20190306011413_add_index_to_services.rb
@@ -0,0 +1,45 @@
+class AddIndexToServices < ActiveRecord::Migration
+  def tables
+    [
+      'Affiliation',
+      'Disabilities',
+      'EmploymentEducation',
+      'EnrollmentCoC',
+      'Exit',
+      'Export',
+      'Funder',
+      'HealthAndDV',
+      'IncomeBenefits',
+      'Inventory',
+      'Organization',
+      'Project',
+      'ProjectCoC',
+      'Services',
+      'Geography',
+    ]
+  end
+
+  def up
+    tables.each do |t|
+      unless index_exists?(t, ["data_source_id", "#{t}ID"], name: "unk_#{t}")
+        add_index t, ["data_source_id", "#{t}ID"], name: "unk_#{t}", unique: true, using: :btree 
+      end
+    end
+    t = 'Enrollment'
+    unless index_exists?(t, ["data_source_id", "#{t}ID", "PersonalID"], name: "unk_#{t}")
+      add_index t, ["data_source_id", "#{t}ID", "PersonalID"], name: "unk_#{t}", unique: true, using: :btree 
+    end
+  end
+
+  def down
+    tables.each do |t|
+      if index_exists?(t, ["data_source_id", "#{t}ID"], name: "unk_#{t}")
+        remove_index t, column: ["data_source_id", "#{t}ID"], name: "unk_#{t}"
+      end
+    end
+    t = 'Enrollment'
+    if index_exists?(t, ["data_source_id", "#{t}ID", "PersonalID"], name: "unk_#{t}")
+      remove_index t, column: ["data_source_id", "#{t}ID", "PersonalID"], name: "unk_#{t}"
+    end
+  end
+end
