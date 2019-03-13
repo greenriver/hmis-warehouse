4cd8f2cbd2bf256ff86db91a7dcfd3131dba7592
Elliot Anders <eanders@greenriver.org>
2019-03-13 15:20:06 -0400

    Store ETO API configs in the DB

---
+++ db/warehouse/migrate/20190313191758_create_eto_api_configs.rb
@@ -0,0 +1,10 @@
+class CreateEtoApiConfigs < ActiveRecord::Migration
+  def change
+    create_table :eto_api_configs do |t|
+      t.references :data_source, null: false, index: true
+      t.jsonb :touchpoint_fields
+      t.jsonb :demographic_fields
+      t.timestamps
+    end
+  end
+end
