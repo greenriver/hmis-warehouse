16d5c7a47233c1125d57bc595055d385c5ac7623
Elliot Anders <eanders@greenriver.org>
2019-03-14 19:48:56 -0400

    Add consent fields to hmis client

--- db/warehouse/migrate/20190314233300_store_consent_on_hmis_client.rb
+++ db/warehouse/migrate/20190314233300_store_consent_on_hmis_client.rb
@@ -1,6 +1,6 @@
 class StoreConsentOnHmisClient < ActiveRecord::Migration
   def change
-    add_column :hmis_clients, :consent_confirmed_date, :date
-    add_column :hmis_clients, :consent_expires_date, :date
+    add_column :hmis_clients, :consent_confirmed_on, :date
+    add_column :hmis_clients, :consent_expires_on, :date
   end
 end
