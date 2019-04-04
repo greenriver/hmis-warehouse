c2803335511a6dd5b66f14a3abdee21c0445a8d9
Sandy Wise <sandy@greenriver.org>
2019-04-04 12:09:59 -0400

    Updates based on review

--- db/health/schema.rb
+++ db/health/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190402142851) do
+ActiveRecord::Schema.define(version: 20190404153621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
