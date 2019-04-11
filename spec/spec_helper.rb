5a3fa5b73580f35b08c1462edaeb9cb02871f735
Elliot Anders <eanders@greenriver.org>
2019-04-11 19:35:09 -0400

    Disable RDS SQL Server connections for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   ENV['NO_LSA_RDS']='true'
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
