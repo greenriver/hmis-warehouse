9b1d9b692d23fad47f0082b846e5e01c0e45d800
Sandy Wise <sandy@greenriver.org>
2019-04-24 11:57:02 -0400

    Wrap a transaction around 271 patient flagging

--- app/jobs/health/flag_ineligible_patients_job.rb
+++ app/jobs/health/flag_ineligible_patients_job.rb
@@ -5,38 +5,40 @@ module Health
       response = inquiry.eligibility_response
 
       if response
-        # Update the last check date
-        Health::Patient.where(medicaid_id: response.subscriber_ids).find_each do |patient|
-          patient.update(coverage_inquiry_date: inquiry.service_date)
-        end
+        Health::Patient.transaction do
+          # Update the last check date
+          patient_scope(response.subscriber_ids).find_each do |patient|
+            patient.update!(coverage_inquiry_date: inquiry.service_date)
+          end
 
-        # Clear the eligibility notification flag on managed patients, so that if they lose eligibility again,
-        # we will re-flag them
-        Health::Patient.where(medicaid_id: response.managed_care_ids).find_each do |patient|
-          patient.update(eligibility_notification: nil)
-        end
+          # Clear the eligibility notification flag on managed patients, so that if they lose eligibility again,
+          # we will re-flag them
+          patient_scope(response.managed_care_ids).find_each do |patient|
+            patient.update!(eligibility_notification: nil)
+          end
 
-        # Set the eligibility level for covered patients
-        Health::Patient.where(medicaid_id: response.eligible_ids).find_each do |patient|
-          patient.update(coverage_level: Health::Patient.coverage_level_standard_value)
-        end
+          # Set the eligibility level for covered patients
+          patient_scope(response.eligible_ids).find_each do |patient|
+            patient.update!(coverage_level: Health::Patient.coverage_level_standard_value)
+          end
 
-        # Set the eligibility level for managed care patients
-        Health::Patient.where(medicaid_id: response.managed_care_ids).find_each do |patient|
-          patient.update(coverage_level: Health::Patient.coverage_level_managed_value)
-        end
+          # Set the eligibility level for managed care patients
+          patient_scope(response.managed_care_ids).find_each do |patient|
+            patient.update!(coverage_level: Health::Patient.coverage_level_managed_value)
+          end
 
-        # Mark ineligible patients
-        Health::Patient.where(medicaid_id: response.ineligible_ids).find_each do |patient|
-          patient.update(coverage_level: Health::Patient.coverage_level_none_value)
-        end
+          # Mark ineligible patients
+          patient_scope(response.ineligible_ids).find_each do |patient|
+            patient.update!(coverage_level: Health::Patient.coverage_level_none_value)
+          end
 
-        notify_care_coordinators(response)
+          notify_care_coordinators(response)
+        end
       end
     end
 
     def notify_care_coordinators(response)
-      groups = Health::Patient.where(medicaid_id: response.ineligible_ids).
+      groups = patient_scope(response.ineligible_ids).
         joins(:team_members).
         where(team_members: {type: 'Health::Team::CareCoordinator'}).
         where(eligibility_notification: nil).
@@ -48,11 +50,11 @@ module Health
           IneligiblePatientMailer.ineligible_patients(coordinator.email, ids).deliver_later
         end
         patients.each do |patient|
-          patient.update(eligibility_notification: Time.now)
+          patient.update!(eligibility_notification: Time.now)
         end
       end
 
-      groups = Health::Patient.where(medicaid_id: response.eligible_ids - response.managed_care_ids).
+      groups = patient_scope(response.eligible_ids - response.managed_care_ids).
         joins(:team_members).
         where(team_members: {type: 'Health::Team::CareCoordinator'}).
         where(eligibility_notification: nil).
@@ -64,9 +66,13 @@ module Health
           IneligiblePatientMailer.no_managed_care_patients(coordinator.email, ids).deliver_later
         end
         patients.each do |patient|
-          patient.update(eligibility_notification: Time.now)
+          patient.update!(eligibility_notification: Time.now)
         end
       end
+    end
+
+    def patient_scope(ids)
+      Health::Patient.where(medicaid_id: ids)
     end
   end
 end
