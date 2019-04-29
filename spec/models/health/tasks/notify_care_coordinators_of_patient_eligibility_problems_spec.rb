8a0aa8498ecb095f15186c6235fb7aeb4ae9197d
Sandy Wise <sandy@greenriver.org>
2019-04-29 14:04:21 -0400

    Coordinators may only have patients in one of the two buckets

--- spec/models/health/tasks/notify_care_coordinators_of_patient_eligibility_problems_spec.rb
+++ spec/models/health/tasks/notify_care_coordinators_of_patient_eligibility_problems_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems
   let(:coordinator_a) { create :user }
   let(:coordinator_b) { create :user }
   let(:coordinator_c) { create :user }
+  let(:coordinator_d) { create :user }
 
   # "user" has no patients
 
@@ -18,7 +19,11 @@ RSpec.describe Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems
   let!(:coordinator_b_standard_patients) { create_list :patient, 5, coverage_level: 'standard', care_coordinator_id: coordinator_b.id }
   let!(:coordinator_b_uncovered_patients) { create_list :patient, 5, coverage_level: 'none', care_coordinator_id: coordinator_b.id }
 
+  # "coordinator_c" has patients, but no notifications
   let!(:coordinator_c_aco_patients) { create_list :patient, 5, coverage_level: 'managed', care_coordinator_id: coordinator_c.id }
+
+  # "coordinator_d" has only one kind of notifications
+  let!(:coordinator_d_uncovered_patients) { create_list :patient, 5, coverage_level: 'none', care_coordinator_id: coordinator_d.id }
 
   before(:each) do
     ActionMailer::Base.deliveries.clear
@@ -27,7 +32,7 @@ RSpec.describe Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems
   it 'sends each coordinator at most one email' do
     Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems.new.notify!
 
-    expect(ActionMailer::Base.deliveries.size).to eq 2
+    expect(ActionMailer::Base.deliveries.size).to eq 3
   end
 
   it 'sends only the relevant patients to a coordinator' do
@@ -50,11 +55,11 @@ RSpec.describe Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems
 
     Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems.new.notify!
 
-    expect(ActionMailer::Base.deliveries.size).to eq 2
+    expect(ActionMailer::Base.deliveries.size).to eq 3
 
     Health::Tasks::NotifyCareCoordinatorsOfPatientEligibilityProblems.new.notify!
 
-    expect(ActionMailer::Base.deliveries.size).to eq 2
+    expect(ActionMailer::Base.deliveries.size).to eq 3
   end
 
   def patient_links(patients)
