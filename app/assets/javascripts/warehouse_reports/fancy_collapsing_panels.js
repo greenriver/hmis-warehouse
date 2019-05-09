997ce63e6bd6eff5db96f612fbb7a1d9aa3aeaa1
jgoley <jgoley@gmail.com>
2019-05-09 11:20:16 -0400

    Refactor panel collapsible views and JS - create common parials and reduce JS logic

--- app/assets/javascripts/warehouse_reports/fancy_collapsing_panels.js
+++ app/assets/javascripts/warehouse_reports/fancy_collapsing_panels.js
@@ -1,21 +1,8 @@
-$('.panel-collapsible .panel-heading').on('click', '.toggle', function(e) {
-  var $button = $(e.currentTarget);
-  var $collapsed_arrow = $(e.currentTarget).closest('.panel-heading').find('.icon-arrow-circled-right');
-  var $expanded_arrow = $(e.currentTarget).closest('.panel-heading').find('.icon-arrow-circled-down');
-  var collapsible_parent_id = $(e.currentTarget).data('parent');
-  var $all_down_arrows = $(collapsible_parent_id).find('.icon-arrow-circled-down');
-  var $all_side_arrows = $(collapsible_parent_id).find('.icon-arrow-circled-right');
-  // console.log($button.hasClass('collapsed'));
-  if($button.hasClass('collapsed')) {
-    $all_down_arrows.addClass('hide');
-    $all_side_arrows.removeClass('hide');
-    $collapsed_arrow.addClass('hide');
-    $expanded_arrow.removeClass('hide');
-  }
-  else {
-    // console.log($collapsed_arrow, $expanded_arrow);
-    $all_side_arrows.removeClass('hide');
-    $collapsed_arrow.removeClass('hide');
-    $expanded_arrow.addClass('hide');
-  }
-});
+$('.panel-collapsible .panel-collapse').on('show.bs.collapse hide.bs.collapse', function(e) {
+  const state = e.handleObj.type === 'show' ? 'down' : 'right'
+  $(this)
+    .parent()
+    .find('.j-toggle-arrow')
+    .removeClass()
+    .addClass('icon-arrow-circled-' + state + ' j-toggle-arrow')
+})
