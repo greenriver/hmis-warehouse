ad404ad91194d34fa5b5474190bfc9ede33d6ace
jgoley <jgoley@gmail.com>
2019-05-10 11:54:54 -0400

    Fix overlooked instances of collapsible panel

--- app/assets/javascripts/warehouse_reports/fancy_collapsing_panels.js
+++ app/assets/javascripts/warehouse_reports/fancy_collapsing_panels.js
@@ -1,8 +1,15 @@
 $('.panel-collapsible .panel-collapse').on('show.bs.collapse hide.bs.collapse', function(e) {
   const state = e.handleObj.type === 'show' ? 'down' : 'right'
-  $(this)
-    .parent()
+  const $parent = $(this).parent()
+  $parent
     .find('.j-toggle-arrow')
     .removeClass()
     .addClass('icon-arrow-circled-' + state + ' j-toggle-arrow')
+  $parent
+    .find('input')
+    .first()
+    .focus()
+  console.log($parent
+    .find('input')
+  )
 })
