229657d3dfc39267a98b49fd323222f232700752
Sandy Wise <sandy@greenriver.org>
2019-05-21 09:55:09 -0400

    Fixes based on test failure

--- app/models/health/tasks/notify_care_coordinators_of_patient_eligibility_problems.rb
+++ app/models/health/tasks/notify_care_coordinators_of_patient_eligibility_problems.rb
@@ -7,22 +7,30 @@ module Health::Tasks
         pluck(:care_coordinator_id)
 
       User.where(id: care_coordinator_ids).each do |user|
-        ineligible_patient_ids = patient_scope.
+        ineligible_patients = patient_scope.
           no_coverage.
           where(care_coordinator_id: user.id).
-          pluck(:client_id)
+          pluck(:id, :client_id)
 
-        no_aco_patient_ids = patient_scope.
+        no_aco_patients = patient_scope.
           standard_coverage.
           where(care_coordinator_id: user.id).
-          pluck(:client_id)
+          pluck(:id, :client_id)
 
-        IneligiblePatientMailer.patients_with_eligibility_problems(user.email, ineligible_patient_ids, no_aco_patient_ids).deliver_now
+        IneligiblePatientMailer.patients_with_eligibility_problems(user.email, client_ids(ineligible_patients), client_ids(no_aco_patients)).deliver_now
 
-        Health::Patient.find(ineligible_patient_ids + no_aco_patient_ids).each do |patient|
+        Health::Patient.find(patient_ids(ineligible_patients) + patient_ids(no_aco_patients)).each do |patient|
           patient.update!(eligibility_notification: Time.now)
         end
       end
+    end
+
+    def patient_ids(list)
+      list.map{|entry| entry.first}
+    end
+
+    def client_ids(list)
+      list.map{|entry| entry.last}
     end
 
     def patient_scope
--- spec/factories/health/patients.rb
+++ spec/factories/health/patients.rb
@@ -9,5 +9,6 @@ FactoryBot.define do
   factory :patient, class: 'Health::Patient' do
     sequence(:id_in_source)
     patient_referral
+    sequence(:client_id)
   end
 end
