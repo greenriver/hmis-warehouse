09efc83cdb9d051607118593aacd8b651e2ed597
Sandy Wise <sandy@greenriver.org>
2019-05-29 14:25:07 -0400

    Test cases for patient referral refreshes

--- app/models/health/tasks/import_patient_referral_refreshes.rb
+++ app/models/health/tasks/import_patient_referral_refreshes.rb
@@ -21,51 +21,53 @@ module Health::Tasks
           remove_files()
           return
         end
-        @incoming_medicaid_ids = []
-        @unprocessed.each do |file_path|
-          local_path = File.join(directory, file_path)
-          notify "Processing patient referral refreshes in #{local_path}"
-          file = load_file(local_path)
-          validate_headers(file, file_path)
-          headers = file.row(file.first_row)
-          db_headers = Health::PatientReferralRefresh.column_headers.invert.values_at(*headers)
-
-          (2..file.last_row).each do |i|
-            row = Hash[db_headers.zip(file.row(i))]
-
-            # send a note, and skip if we found anything other than a new or active referral
-            if ! row[:record_status].in?(['A', 'N'])
-              notify "Patient Referral Refresh Importer found a record that is not Active or New, please see import_patient_referrals.rb, skipping for now, value: #{row[:record_status]}"
-              next
-            end
-            @incoming_medicaid_ids << row[:medicaid_id]
-            patient_referral = Health::PatientReferral.
-              where(medicaid_id: row[:medicaid_id]).
-              first_or_initialize
-            # attempt to find ACO ID
-            aco_id = Health::AccountableCareOrganization.find_by(mco_pid: row[:aco_mco_pid], mco_sl: row[:aco_mco_sl])&.id
-            patient_referral.accountable_care_organization_id = aco_id if aco_id.present?
-            # if we have a new row or an update
-            # save it
-            updated_on = Date.strptime(row[:updated_on].to_s, '%Y%m%d')
-            if patient_referral.updated_on.blank? || updated_on > patient_referral.updated_on
-              patient_referral.assign_attributes(row)
-              # Make sure people are not marked rejected if they are appear on this list
-              # and haven't been marked as dis-enrolled
-              if patient_referral.disenrollment_date.blank?
-                patient_referral.rejected = false
-                patient_referral.rejected_reason = :Remove_Removal
-                patient_referral.removal_acknowledged = false
-              end
-              patient_referral.save!
-            end
-          end
-          Health::PatientReferralImport.create(file_name: file_path)
-        end
-        reject_any_not_included()
+        process_files @unprocessed
       end
-
       remove_files()
+    end
+
+    def process_files files
+      @incoming_medicaid_ids = []
+      files.each do |file_path|
+        local_path = File.join(directory, file_path)
+        notify "Processing patient referral refreshes in #{local_path}"
+        file = load_file(local_path)
+        validate_headers(file, file_path)
+        headers = file.row(file.first_row)
+        db_headers = Health::PatientReferralRefresh.column_headers.invert.values_at(*headers)
+
+        (2..file.last_row).each do |i|
+          row = Hash[db_headers.zip(file.row(i))]
+
+          # send a note, and skip if we found anything other than a new or active referral
+          if ! row[:record_status].in?(['A', 'N'])
+            notify "Patient Referral Refresh Importer found a record that is not Active or New, please see import_patient_referrals.rb, skipping for now, value: #{row[:record_status]}"
+            next
+          end
+          @incoming_medicaid_ids << row[:medicaid_id]
+          patient_referral = Health::PatientReferral.
+            where(medicaid_id: row[:medicaid_id]).
+            first_or_initialize
+          # attempt to find ACO ID
+          aco_id = Health::AccountableCareOrganization.find_by(mco_pid: row[:aco_mco_pid], mco_sl: row[:aco_mco_sl])&.id
+          patient_referral.accountable_care_organization_id = aco_id if aco_id.present?
+          # if we have a new row or an update
+          # save it
+          updated_on = Date.strptime(row[:updated_on].to_s, '%Y%m%d')
+          if patient_referral.updated_on.blank? || updated_on > patient_referral.updated_on
+            patient_referral.assign_attributes(row)
+            patient_referral.assign_attributes(updated_on: updated_on)
+            # Make sure people are not marked rejected if they are appear on this list
+            # and haven't been marked as dis-enrolled
+            if patient_referral.disenrollment_date.blank?
+              patient_referral.assign_attributes(rejected: false, rejected_reason: :Remove_Removal, removal_acknowledged: false)
+            end
+            patient_referral.save!
+          end
+        end
+        Health::PatientReferralImport.create(file_name: file_path)
+      end
+      reject_any_not_included()
     end
 
     def reject_any_not_included
