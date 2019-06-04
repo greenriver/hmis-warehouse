b9fe35f911378abfb01b4460db479fb7fd3e11eb
Elliot Anders <eanders@greenriver.org>
2019-06-04 14:14:19 -0400

    A simple report to find clients enrolled in multiple projects of the same type on the same day

---
+++ db/warehouse/migrate/20190604164934_add_double_enrollment_report.rb
@@ -0,0 +1,34 @@
+class AddDoubleEnrollmentReport < ActiveRecord::Migration
+  REPORTS = {
+    'Data Quality' => [
+      {
+        url: 'warehouse_reports/double_enrollments',
+        name: 'Doubly Enrolled Clients',
+        description: 'Clients enrolled in multiple simultaneous projects of the same type.',
+        limitable: true,
+      },
+    ],
+  }
+
+
+  def up
+    REPORTS.each do |group, reports|
+      reports.each do |report|
+        GrdaWarehouse::WarehouseReports::ReportDefinition.create(
+          report_group: group,
+          url: report[:url],
+          name: report[:name],
+          description: report[:description]
+        )
+      end
+    end
+  end
+
+  def down
+    REPORTS.each do |group, reports|
+      reports.each do |report|
+        GrdaWarehouse::WarehouseReports::ReportDefinition.where(url: report[:url]).delete_all
+      end
+    end
+  end
+end
