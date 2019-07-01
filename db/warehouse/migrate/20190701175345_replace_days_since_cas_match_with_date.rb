e8aeac9da8e70af6bcef245b64e91c81daaa7ce9
Elliot Anders <eanders@greenriver.org>
2019-07-01 16:39:11 -0400

    Break migration into stages

--- db/warehouse/migrate/20190701175345_replace_days_since_cas_match_with_date.rb
+++ db/warehouse/migrate/20190701175345_replace_days_since_cas_match_with_date.rb
@@ -1,6 +1,5 @@
 class ReplaceDaysSinceCasMatchWithDate < ActiveRecord::Migration
   def change
     add_column :warehouse_clients_processed, :last_cas_match_date, :datetime
-    remove_column :warehouse_clients_processed, :days_since_cas_match, :integer
   end
 end
