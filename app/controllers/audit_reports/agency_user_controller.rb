d0e6e909767135e5283246ec8843985a5a66049c
Sandy Wise <sandy@greenriver.org>
2019-07-22 15:11:43 -0400

    Fix syntax errors

--- app/controllers/audit_reports/agency_user_controller.rb
+++ app/controllers/audit_reports/agency_user_controller.rb
@@ -19,12 +19,12 @@ module AuditReports
 
     def view_history
       @view_history ||= ActivityLog.where(
-          user_id: user_scope.pluck(:id),
-          item_model: GrdaWarehouse::Hud::Client.name,
-          created_at: 2.months.ago.beginning_of_month .. Date.today,
-        ).
-        pluck(:user_id, :item_id, :created_at)
-        group_by(&:user_id).
+        user_id: user_scope.pluck(:id),
+        item_model: GrdaWarehouse::Hud::Client.name,
+        created_at: 2.months.ago.beginning_of_month .. Date.today,
+      ).
+      select(:user_id, :item_id, :created_at).
+      group_by(&:user_id).
         map do |user_id, dates|
           current = dates.select { |date| date.created_at >= Date.today.beginning_of_month }.map(&:item_id).uniq.count
           last = dates.select { |date| date.created_at < Date.today.beginning_of_month && date.created_at >= 1.month.ago.beginning_of_month }.map(&:item_id).uniq.count
@@ -36,9 +36,9 @@ module AuditReports
 
     def user_scope
       if current_user.can_manage_all_agencies
-        scope = User.all
+        scope = User.active
       else
-        scope = User.where(agency: current_user.agency)
+        scope = User.active.where(agency: current_user.agency)
       end
       scope.order(:last_name, :first_name)
     end
