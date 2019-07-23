c5cd8861e9e3ede4867baae36bf183952f5fc6d5
Sandy Wise <sandy@greenriver.org>
2019-07-23 15:32:08 -0400

    Excel download

--- app/controllers/audit_reports/user_login_controller.rb
+++ app/controllers/audit_reports/user_login_controller.rb
@@ -3,7 +3,23 @@ module AuditReports
     include WarehouseReportAuthorization
 
     def index
-      @users = User.active.where.not(current_sign_in_at: nil).order(current_sign_in_at: :desc).page(params[:page])
+      @users = users_scope.page(params[:page])
+      respond_to do |format|
+        format.html {}
+        format.xlsx do
+          date = Date.today.strftime('%Y-%m-%d')
+          filename = "user-logins-#{date}.xlsx"
+          headers['Content-Disposition'] = "attachment; filename=#{filename}"
+        end
+      end
+    end
+
+    def users_scope
+      User.
+        joins(:agency).
+        active.
+        where.not(current_sign_in_at: nil).
+        order(current_sign_in_at: :desc)
     end
 
   end
