d68da043978c699eb7f1532da3c0b0ec9b5128c9
Elliot Anders <eanders@greenriver.org>
2019-08-19 13:40:02 -0400

    Notes for how to set literally homeless

--- db/warehouse/migrate/20190715371835_set_all_shs_literally_homeless_false.rb
+++ db/warehouse/migrate/20190715371835_set_all_shs_literally_homeless_false.rb
@@ -4,7 +4,7 @@ class SetAllShsLiterallyHomelessFalse < ActiveRecord::Migration
     # years = (1900..2020).each do |year|
     #    range = Date.new(year, 01, 01) .. Date.new(year, 12, 31)
     #    puts "Updating #{range.inspect}"
-    #    GrdaWarehouse::ServiceHistoryService.where(date: range).update_all(literally_homeless: false)
+    #    GrdaWarehouse::ServiceHistoryService.where(date: range, literally_homeless: nil).update_all(literally_homeless: false)
     # end
 
     change_column_default :service_history_services, :literally_homeless, false
