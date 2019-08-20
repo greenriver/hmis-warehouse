1940716063b6862ff289fa78337ff7ffe53d8936
Elliot Anders <eanders@greenriver.org>
2019-08-20 10:12:56 -0400

    2020 updates to destinations

--- lib/util/hud.rb
+++ lib/util/hud.rb
@@ -606,34 +606,10 @@ module HUD
   end
 
   def living_situations
-    {
-      1 => 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher',
-      2 => 'Transitional housing for homeless persons',
-      3 => 'Permanent housing for formerly homeless persons',
-      4 => 'Psychiatric hospital or other psychiatric facility',
-      5 => 'Substance abuse treatment facility or detox center',
-      6 => 'Hospital or other residential non-psychiatric medical facility',
-      7 => 'Jail, prison or juvenile detention facility',
-      8 => 'Client doesn’t know',
-      9 => 'Client refused',
-      12 => 'Staying or living in a family member’s room, apartment or house',
-      13 => 'Staying or living in a friend’s room, apartment or house',
-      14 => 'Hotel or motel paid for without emergency shelter voucher',
-      15 => 'Foster care home or foster care group home',
-      16 => 'Place not meant for habitation',
-      17 => 'Other',
-      18 => 'Safe Haven',
-      19 => 'Rental by client, with VASH subsidy',
-      20 => 'Rental by client, with other ongoing housing subsidy',
-      21 => 'Owned by client, with ongoing housing subsidy',
-      22 => 'Rental by client, no ongoing housing subsidy',
-      23 => 'Owned by client, no ongoing housing subsidy',
-      24 => 'Long-term care facility or nursing home',
-      25 => 'Rental by client, with GPD TIP subsidy',
-      26 => 'Residential project or halfway house with no homeless criteria',
-      27 => 'Interim housing',
-      99 => 'Data not collected',
-    }
+    # Technically this should exclude 13, 12, 22, 23, 26, 27, 30, 17, 24, 37
+    available_living_situations = available_situations
+    available_living_situations[27] = 'Interim Housing' # for backwards compatibility
+    return available_living_situations
   end
 
   # 3.917.2
@@ -751,10 +727,14 @@ module HUD
   end
 
   def valid_destinations()
+    available_situations
+  end
+
+  def available_situations
     {
-      1 => 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher',
+      1 => 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter ',
       2 => 'Transitional housing for homeless persons (including homeless youth)',
-      3 => 'Permanent housing for formerly homeless persons (such as: CoC project; or HUD legacy programs; or HOPWA PH)',
+      3 => 'Permanent housing (other than RRH) for formerly homeless persons',
       4 => 'Psychiatric hospital or other psychiatric facility',
       5 => 'Substance abuse treatment facility or detox center',
       6 => 'Hospital or other residential non-psychiatric medical facility',
@@ -763,8 +743,8 @@ module HUD
       9 => 'Client refused',
       10 => 'Rental by client, no ongoing housing subsidy',
       11 => 'Owned by client, no ongoing housing subsidy',
-      12 => 'Staying or living with family, temporary tenure (e.g., room, apartment or house)',
-      13 => 'Staying or living with friends, temporary tenure (e.g., room apartment or house)',
+      12 => 'Staying or living with family, temporary tenure (e.g. room, apartment or house)',
+      13 => 'Staying or living with friends, temporary tenure (e.g. room apartment or house)',
       14 => 'Hotel or motel paid for without emergency shelter voucher',
       15 => 'Foster care home or foster care group home',
       16 => 'Place not meant for habitation (e.g., a vehicle, an abandoned building, bus/train/subway station/airport or anywhere outside)',
@@ -783,62 +763,114 @@ module HUD
       29 => 'Residential project or halfway house with no homeless criteria',
       30 => 'No exit interview completed',
       31 => 'Rental by client, with RRH or equivalent subsidy',
+      32 => 'Host Home (non-crisis)',
+      33 => 'Rental by client, with HCV voucher (tenant or project based)',
+      34 => 'Rental by client in a public housing unit',
+      35 => 'Staying or living in a family member’s room, apartment or house',
+      36 => 'Staying or living in a friend’s room, apartment or house',
+      37 => 'Worker unable to determine',
       99 => 'Data not collected',
     }
   end
 
+  def homeless_situations
+    [
+      16,
+      1,
+      18,
+      27,
+    ]
+  end
+
+  def institutional_situations
+    [
+      15,
+      6,
+      7,
+      24,
+      4,
+      5,
+    ]
+  end
+
+  def transitional_and_permanent_situations
+    [
+      14,
+      23,
+      21,
+      3,
+      22,
+      19,
+      25,
+      20,
+      26,
+      12,
+      13,
+      2,
+      8
+      9,
+      99,
+    ]
+  end
+
   def permanent_destinations()
     # Permanent destinations
-    {
-      3 => 'Permanent housing for formerly homeless persons (such as: CoC project; or HUD legacy programs; or HOPWA PH)',
-      10 => 'Rental by client, no ongoing housing subsidy',
-      11 => 'Owned by client, no ongoing housing subsidy',
-      19 => 'Rental by client, with VASH housing subsidy',
-      20 => 'Rental by client, with other ongoing housing subsidy',
-      21 => 'Owned by client, with ongoing housing subsidy',
-      22 => 'Staying or living with family, permanent tenure',
-      23 => 'Staying or living with friends, permanent tenure',
-      26 => 'Moved from one HOPWA funded project to HOPWA PH',
-      28 => 'Rental by client, with GPD TIP housing subsidy',
-      31 => 'Rental by client, with RRH or equivalent subsidy',
-    }.keys
+    [
+      3,
+      10,
+      11,
+      19,
+      20,
+      21,
+      22,
+      23,
+      26,
+      28,
+      29,
+      31,
+      32,
+      33,
+      34,
+      35,
+      36,
+    ]
   end
 
   def temporary_destinations()
     # Temporary destinations
-    {
-      1 => 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher',
-      2 => 'Transitional housing for homeless persons (including homeless youth)',
-      12 => 'Staying or living with family, temporary tenure (e.g., room, apartment or house)',
-      13 => 'Staying or living with friends, temporary tenure (e.g., room apartment or house)',
-      14 => 'Hotel or motel paid for without emergency shelter voucher',
-      16 => 'Place not meant for habitation (e.g., a vehicle, an abandoned building, bus/train/subway station/airport or anywhere outside)',
-      18 => 'Safe Haven',
-      27 => 'Moved from one HOPWA funded project to HOPWA TH',
-    }.keys
+    [
+      1,
+      2,
+      12,
+      13,
+      14,
+      16,
+      18,
+      27,
+    ]
   end
   def institutional_destinations()
     # Institutional Destinations
-    {
-      4 => 'Psychiatric hospital or other psychiatric facility',
-      5 => 'Substance abuse treatment facility or detox center',
-      6 => 'Hospital or other residential non-psychiatric medical facility',
-      7 => 'Jail, prison or juvenile detention facility',
-      15 => 'Foster care home or foster care group home',
-      25 => 'Long-term care facility or nursing home',
-      29 => 'Residential project or halfway house with no homeless criteria',
-    }.keys
+    [
+      4,
+      5,
+      6,
+      7,
+      15,
+      25,
+      29,
+    ]
   end
   def other_destinations()
-    {
+    [
       # Other Destinations
-      8 => 'Client doesn’t know',
-      9 => 'Client refused',
-      17 => 'Other',
-      24 => 'Deceased',
-      30 => 'No exit interview completed',
-      99 => 'Data not collected',
-    }.keys
+      8,
+      9,
+      17,
+      24,
+      30,
+      99,
+    ]
   end
 
   # 3.15.1
