54b75275074fd980f541bfb451c4b86ac46979c1
Todd Blackman <tdblackman@gmail.com>
2019-08-21 10:45:56 -0400

    fixes inflector-related class name issues in migrations

--- db/warehouse/migrate/20160913160306_add_household_id_to_enrollment_coc.rb
+++ db/warehouse/migrate/20160913160306_add_household_id_to_enrollment_coc.rb
@@ -1,4 +1,4 @@
-class AddHouseholdIdToEnrollmentCoc < ActiveRecord::Migration
+class AddHouseholdIdToEnrollmentCoC < ActiveRecord::Migration
   def change
     table = GrdaWarehouse::Hud::EnrollmentCoc.table_name
     add_column table, 'HouseholdID', :string, limit: 32
