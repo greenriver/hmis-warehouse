8b78729a73eddb90f6c18507260cbc15437fd167
Sandy Wise <sandy@greenriver.org>
2019-08-27 16:58:17 -0400

    Edit view permissions from project groups

--- app/controllers/project_groups_controller.rb
+++ app/controllers/project_groups_controller.rb
@@ -7,6 +7,7 @@
 class ProjectGroupsController < ApplicationController
   before_action :require_can_edit_project_groups!
   before_action :set_project_group, only: [:edit, :update]
+  before_action :set_users_with_access, only: [:edit, :update]
 
   def index
     @project_groups = project_group_scope
@@ -17,11 +18,12 @@ class ProjectGroupsController < ApplicationController
   end
 
   def create
-      @project_group = project_group_source.new
+    @project_group = project_group_source.new
     begin
       @project_group.assign_attributes(name: group_params[:name])
       @project_group.project_ids = group_params[:projects]
       @project_group.save
+      update_permissions(@project_group)
     rescue Exception => e
       flash[:error] = e.message
       render action: :new
@@ -39,6 +41,7 @@ class ProjectGroupsController < ApplicationController
       @project_group.assign_attributes(name: group_params[:name])
       @project_group.project_ids = group_params[:projects]
       @project_group.save
+      update_permissions(@project_group)
       rescue Exception => e
       flash[:error] = e.message
       render action: :edit
@@ -51,6 +54,17 @@ class ProjectGroupsController < ApplicationController
   
   end
 
+  def update_permissions(project_group)
+    user_ids = user_params[:users].reject(&:empty?).map(&:to_i)
+    # add new user permissions
+    added_users = user_ids - @project_group.user_viewable_entities.pluck(:user_id)
+    added_users.each do |id|
+      @project_group.user_viewable_entities.create(user_id: id)
+    end
+    # remove users that were removed from the permissions
+    @project_group.user_viewable_entities.where.not(user_id: user_ids).destroy_all
+  end
+
   def group_params
     params.require(:grda_warehouse_project_group).
       permit(
@@ -59,8 +73,19 @@ class ProjectGroupsController < ApplicationController
       )
   end
 
+  def user_params
+    params.require(:grda_warehouse_project_group).
+      permit(
+        users: [],
+      )
+  end
+
   def set_project_group
     @project_group = project_group_source.find(params[:id].to_i)
+  end
+
+  def set_users_with_access
+    @user_ids_with_access = @project_group.user_viewable_entities.pluck(:user_id)
   end
 
   def project_group_source
