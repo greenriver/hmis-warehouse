b3f5d610a64026e308a6488729ba700176cd0b36
Elliot Anders <eanders@greenriver.org>
2019-08-30 20:20:37 -0400

    Fix for field that was too long

--- app/models/grda_warehouse/export/hmis_six_one_one/income_benefit.rb
+++ app/models/grda_warehouse/export/hmis_six_one_one/income_benefit.rb
@@ -15,5 +15,11 @@ module GrdaWarehouse::Export::HMISSixOneOne
     # enrollment has been deleted
     belongs_to :enrollment_with_deleted, class_name: GrdaWarehouse::Hud::WithDeleted::Enrollment.name, primary_key: [:EnrollmentID, :PersonalID, :data_source_id], foreign_key: [:EnrollmentID, :PersonalID, :data_source_id]
 
+    def apply_overrides row, data_source_id:
+
+      # Technical limit of HMIS spec is 50 characters
+      row[:OtherInsuranceIdentify] = row[:OtherInsuranceIdentify][0...50] if row[:OtherInsuranceIdentify]
+      return row
+    end
   end
 end
