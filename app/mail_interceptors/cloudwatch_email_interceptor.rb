40527e5e727a2e3e033f81d2072cf86b020bf1dc
Todd Blackman <tdblackman@gmail.com>
2019-09-03 15:45:24 -0400

    [168144908] sets up server-side for bounce/delivery/complaint monitoring

---
+++ app/mail_interceptors/cloudwatch_email_interceptor.rb
@@ -0,0 +1,15 @@
+###
+# Copyright 2016 - 2019 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/master/LICENSE.md
+###
+
+class CloudwatchEmailInterceptor
+  def self.delivering_email message
+    message.headers({
+      'X-SES-CLIENT' => ENV.fetch('CLIENT') { 'UnknownClient' },
+      'X-SES-APP' => 'Warehouse',
+      'X-SES-CONFIGURATION-SET' => ENV.fetch('SES_CONFIG_SET') { 'OpenPathConfigSet' }
+    })
+  end
+end
