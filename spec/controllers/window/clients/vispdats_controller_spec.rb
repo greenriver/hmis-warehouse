e0ca201c7a46f5ac2cc8926e666dca7d6cf2c795
Elliot Anders <eanders@greenriver.org>
2019-09-06 09:06:43 -0400

    removing Window scope from controller tests

--- spec/controllers/window/clients/vispdats_controller_spec.rb
+++ spec/controllers/window/clients/vispdats_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Window::Clients::VispdatsController, type: :controller do
+RSpec.describe Clients::VispdatsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Vispdat. As you add validations to Vispdat, be sure to
   # adjust the attributes here as well.
