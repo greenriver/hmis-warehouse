ab3bc4749d78615a529be822b99d2f27aebe1b52
Sandy Wise <sandy@greenriver.org>
2019-09-20 11:12:54 -0400

    Minimal tests

--- spec/models/grda_warehouse/warehouse_clients_processed_spec.rb
+++ spec/models/grda_warehouse/warehouse_clients_processed_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe GrdaWarehouse::WarehouseClientsProcessed, type: :model do
     setup_initial_imports
     GrdaWarehouse::Tasks::ServiceHistory::Add.new.run!
     Delayed::Worker.new.work_off(2)
+
+    # Add includes_verified_days_homeless override
+    GrdaWarehouse::Hud::Project.find_by(ProjectName: 'Services Only').update(include_in_days_homeless_override: true)
+    # Update
     GrdaWarehouse::ServiceHistoryServiceMaterialized.rebuild!
+    @client_ids = GrdaWarehouse::ServiceHistoryServiceMaterialized.distinct.pluck(:client_id)
+    GrdaWarehouse::WarehouseClientsProcessed.update_cached_counts(client_ids: @client_ids)
   end
   after(:all) do
     # Because we are only running the import once, we have to do our own DB and file cleanup
@@ -18,8 +24,33 @@ RSpec.describe GrdaWarehouse::WarehouseClientsProcessed, type: :model do
     Delayed::Job.delete_all
   end
 
-  it 'has some records in the materialized view' do
+  it 'sets up the harness' do
     expect(GrdaWarehouse::ServiceHistoryServiceMaterialized.count).to_not eq(0)
+    expect(GrdaWarehouse::WarehouseClientsProcessed.count).to be > 0
+    expect(GrdaWarehouse::WarehouseClientsProcessed.count).to eq(@client_ids.count)
+    expect(GrdaWarehouse::Hud::Project.homeless.count).to be > 0
+    expect(GrdaWarehouse::Hud::Project.includes_verified_days_homeless.count).to eq(1)
+  end
+
+  it 'counts homeless days' do
+    client = GrdaWarehouse::Hud::Client.find_by(LastName: 'Two').destination_client
+
+    expect(client.source_enrollments.joins(:project).merge(GrdaWarehouse::Hud::Project.homeless).count).to be > 0
+  end
+
+  it 'includes the override days in the plus overrides count' do
+    client = GrdaWarehouse::Hud::Client.find_by(LastName: 'Two').destination_client
+
+    expect(client.processed_service_history.days_homeless_plus_overrides).to be > client.processed_service_history.homeless_days
+  end
+
+  it 'excludes overlapping homeless days' do
+    client = GrdaWarehouse::Hud::Client.find_by(LastName: 'Two').destination_client
+
+    homeless_count = client.service_history_services.joins(service_history_enrollment: :project).merge(GrdaWarehouse::Hud::Project.homeless).count
+    override_count = client.service_history_services.joins(service_history_enrollment: :project).merge(GrdaWarehouse::Hud::Project.includes_verified_days_homeless).count
+
+    expect(client.processed_service_history.days_homeless_plus_overrides).to be < homeless_count + override_count
   end
 
   def setup_initial_imports
