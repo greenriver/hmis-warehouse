5f64c20218edf301c8daea53bc06f70e248c88fa
Sandy Wise <sandy@greenriver.org>
2019-09-24 12:55:46 -0400

    Report data

--- app/controllers/warehouse_reports/dv_victim_service_controller.rb
+++ app/controllers/warehouse_reports/dv_victim_service_controller.rb
@@ -11,7 +11,34 @@ module WarehouseReports
     before_action :set_limited, only: [:index]
 
     def index
+      @start_date = params.dig(:filter, :start) || Date.current - 30.days
+      @end_date = params.dig(:filter, :end) || Date.current
+      project_ids = params.dig(:filter, :project_ids)|| []
 
+      @project_ids = project_ids.reject{ |id| id.blank? }
+      @clients = clients.page(params[:page].to_i)
+    end
+
+    def clients
+      project_types = GrdaWarehouse::Hud::Project::RESIDENTIAL_PROJECT_TYPES.values_at(:th, :ph).flatten
+      if @project_ids.length > 0
+        scope = client_scope.
+          joins(enrollments: :project).
+          where(p_t[:id].in(@project_ids))
+      else
+        scope = client_scope.
+          joins(enrollments: :project).
+          merge(GrdaWarehouse::Hud::Project.with_project_type(project_types))
+      end
+      scope.order(:FirstName, :LastName)
+    end
+
+    def client_scope
+      GrdaWarehouse::Hud::Client.
+        joins(:health_and_dvs, enrollments: :project).
+        where(hdv_t[:InformationDate].gteq(@start_date).and(hdv_t[:InformationDate].lteq(@end_date).and(hdv_t[:CurrentlyFleeing].eq(1)))).
+        merge(GrdaWarehouse::Hud::Project.viewable_by(current_user)).
+        distinct
     end
   end
 end
