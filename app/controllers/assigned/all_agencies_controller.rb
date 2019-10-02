5e16aceaef17a3a2b682c999bcb5663a1efabeb6
Sandy Wise <sandy@greenriver.org>
2019-10-02 12:47:26 -0400

    Add agency filter to All Assigned Clients

--- app/controllers/assigned/all_agencies_controller.rb
+++ app/controllers/assigned/all_agencies_controller.rb
@@ -11,12 +11,13 @@ module Assigned
     before_action :require_can_manage_all_agencies!
 
     def index
-
-      @users = User.
+      @selection = params.dig(:filter, :agency_ids)&.reject(&:empty?)&.map(&:to_i) || []
+      users = User.
         active.
         where(id:  GrdaWarehouse::UserClient.active.distinct.pluck(:user_id)).
-        order(:first_name, :last_name).
-        sort_by { |user| user.agency.name }
+        order(:first_name, :last_name)
+      users = users.where(agency_id: @selection) if @selection.present?
+      @users = users.sort_by { |user| user.agency.name }
     end
 
   end
