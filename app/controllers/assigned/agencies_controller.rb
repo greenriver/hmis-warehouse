781606ef39bf2fe6c08f9618c9b2709c72841f13
Elliot Anders <eanders@greenriver.org>
2019-10-03 11:56:27 -0400

    Fix for missing agency on user

--- app/controllers/assigned/agencies_controller.rb
+++ app/controllers/assigned/agencies_controller.rb
@@ -11,11 +11,15 @@ module Assigned
     before_action :require_can_manage_an_agency!
 
     def index
-      @users = User.
-        active.
-        where(agency_id: current_user.agency.id).
-        where(id:  GrdaWarehouse::UserClient.active.distinct.pluck(:user_id)).
-        order(:first_name, :last_name)
+      if current_user.agency
+        @users = User.
+          active.
+          where(agency_id: current_user.agency.id).
+          where(id:  GrdaWarehouse::UserClient.active.distinct.pluck(:user_id)).
+          order(:first_name, :last_name)
+      else
+        @users = User.where(id: current_user.id)
+      end
     end
 
   end
