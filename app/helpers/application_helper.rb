bcdf2d0ba4dc8fdb979a34460736ade3137ff764
Elliot Anders <eanders@greenriver.org>
2019-10-07 08:41:04 -0400

    Minor adjustments to minimize the branch info status bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -197,7 +197,7 @@ module ApplicationHelper
 
   def branch_info
     branch_name = `git rev-parse --abbrev-ref HEAD`
-    content_tag :p, class: 'navbar-text' do
+    content_tag :div, class: 'navbar-text' do
       content_tag :span, branch_name, class: 'label label-warning'
     end
   end
