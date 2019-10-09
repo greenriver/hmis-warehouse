3bdedbe16790b79d8e1b4fee1ab7e3a09c9e97e4
Elliot Anders <eanders@greenriver.org>
2019-10-08 20:37:17 -0400

    Pull LGBTQ from HMIS from warehouse client processed

--- app/models/cohort_columns/lgbtq_from_hmis.rb
+++ app/models/cohort_columns/lgbtq_from_hmis.rb
@@ -9,5 +9,9 @@ module CohortColumns
     attribute :column, String, lazy: true, default: :lgbtq_from_hmis
     attribute :translation_key, String, lazy: true, default: 'Sexual Orientation (from HMIS)'
     attribute :title, String, lazy: true, default: -> (model, attr) { _(model.translation_key)}
+
+    def value(cohort_client) # OK
+      cohort_client.client.processed_service_history&.lgbtq_from_hmis
+    end
   end
 end
