407c42f740fc2b6e7a965459e2acac4f2308c9c0
Elliot Anders <elliot@greenriver.org>
2019-10-21 20:55:35 -0400

    Pass in a user for LSA running tests

--- app/models/report_generators/lsa/fy2018/base.rb
+++ app/models/report_generators/lsa/fy2018/base.rb
@@ -9,7 +9,7 @@ module ReportGenerators::Lsa::Fy2018
   include ArelHelper
     attr_accessor :report
 
-    def initialize destroy_rds: true, hmis_export_id: nil
+    def initialize destroy_rds: true, hmis_export_id: nil, options:
       @destroy_rds = destroy_rds
       @hmis_export_id = hmis_export_id
       @user = User.find(options[:user_id].to_i)
