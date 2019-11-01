477e26c474330e3d8fecb846b30debd56b4b4e24
Elliot Anders <elliot@greenriver.org>
2019-11-01 10:58:49 -0400

    WIP; Adjustments for CoC ROI tests

--- app/models/grda_warehouse/hud/client.rb
+++ app/models/grda_warehouse/hud/client.rb
@@ -537,25 +537,26 @@ module GrdaWarehouse::Hud
     # Viewable by must be called on source clients
     scope :viewable_by, -> (user) do
       if user.can_view_clients_with_roi_in_own_coc?
+        # At a high level if you can see clients with ROI in your COC, you need to be able
+          # to see everyone for searching purposes.
+          # limits will be imposed on accessing the actual client dashboard pages
+          current_scope
+
         # If the user has coc-codes specified, this will limit to users
         # with a valid consent form in the coc or with no-coc specified
         # If the user does not have a coc-code specified, only clients with a full (CoC not specified) release
         # are included.
-
-        # joins(:client_files).
-        # where(id: GrdaWarehouse::ClientFile.consent_forms.confirmed.for_coc(user.coc_codes).pluck(:client_id))
-
-        if user&.can_see_clients_in_window_for_assigned_data_sources?
-          ds_ids = user.data_sources.pluck(:id)
-          where(
-            arel_table[:data_source_id].in(ds_ids).
-            or(arel_table[:id].in(active_confirmed_consent_in_cocs(user.coc_codes).select(:id))).
-            or(arel_table[:id].in(Arel.sql(visible_by_project_to(user).select(:id).to_sql))).
-            or(arel_table[:id].in(Arel.sql(visible_in_window_to(user).select(:id).to_sql)))
-          )
-        else
-          active_confirmed_consent_in_cocs(user.coc_codes)
-        end
+        # if user&.can_see_clients_in_window_for_assigned_data_sources?
+        #   ds_ids = user.data_sources.pluck(:id)
+        #   where(
+        #     arel_table[:data_source_id].in(ds_ids).
+        #     or(arel_table[:id].in(active_confirmed_consent_in_cocs(user.coc_codes).select(:id))).
+        #     or(arel_table[:id].in(Arel.sql(visible_by_project_to(user).select(:id).to_sql))).
+        #     or(arel_table[:id].in(Arel.sql(visible_in_window_to(user).select(:id).to_sql)))
+        #   )
+        # else
+        #   # active_confirmed_consent_in_cocs(user.coc_codes)
+        # end
       elsif user.can_view_clients?
         current_scope
       else
@@ -887,16 +888,28 @@ module GrdaWarehouse::Hud
     end
 
     def visible_because_of_permission?(user)
-      (
         user.can_view_clients? ||
-        (
-          (
-            release_valid? ||
-            ! GrdaWarehouse::Config.get(:window_access_requires_release)
-          ) && user.can_view_client_window?
-        ) ||
-        user.can_see_clients_in_window_for_assigned_data_sources? &&
+        visible_because_of_release?(user) ||
+        visible_because_of_assigned_data_source?(user) ||
+        visible_because_of_coc_association?(user)
+    end
+
+    def visible_because_of_release?(user)
+      user.can_view_client_window? &&
+      (release_valid? || ! GrdaWarehouse::Config.get(:window_access_requires_release))
+    end
+
+    def visible_because_of_assigned_data_source?(user)
+      user.can_see_clients_in_window_for_assigned_data_sources? &&
         (source_clients.pluck(:data_source_id) & user.data_sources.pluck(:id)).present?
+    end
+
+    def visible_because_of_coc_association?(user)
+      user.can_view_clients_with_roi_in_own_coc? &&
+      release_valid? &&
+      (
+        consented_coc_codes == [] ||
+        (consented_coc_codes & user.coc_codes).present?
       )
     end
 
--- spec/requests/client_visibility_spec.rb
+++ spec/requests/client_visibility_spec.rb
@@ -390,7 +390,6 @@ RSpec.describe ClientsController, type: :request do
     end
   end
 
-  # TODO: still need to implement tests for can_view_clients_with_roi_in_own_coc
   context 'when config ma is in affect' do
     before do
       GrdaWarehouse::Config.delete_all
@@ -493,6 +492,108 @@ RSpec.describe ClientsController, type: :request do
           it 'user can see client dashboard for assigned client' do
             get client_path(non_window_destination_client)
             expect(response).to have_http_status(200)
+          end
+          it 'user cannot see client dashboard for window client' do
+            get client_path(window_destination_client)
+            expect(response).to redirect_to(root_path)
+          end
+        end
+      end
+    end
+
+    describe 'and the user has a role granting visibility by coc release' do
+      before do
+        user.roles << can_view_clients_with_roi_in_own_coc
+        sign_in user
+      end
+      it 'user can search for all clients' do
+        get clients_path(q: 'bob')
+        doc = Nokogiri::HTML(response.body)
+        expect(doc.text).to include('Displaying all 2 clients')
+        expect(response).to have_http_status(200)
+      end
+      describe 'and the user is assigned a CoC' do
+        before do
+          user.coc_codes = ['ZZ-000']
+        end
+        it 'user cannot see client details' do
+          get client_path(non_window_destination_client)
+          expect(response).to have_http_status(302)
+        end
+        describe 'when the client has a valid consent in any coc' do
+          before do
+            past_date = 5.days.ago
+            future_date = Date.current + 1.years
+            non_window_destination_client.update(
+              housing_release_status: non_window_destination_client.class.full_release_string,
+              consent_form_signed_on: past_date,
+              consent_expires_on: future_date,
+              consented_coc_codes: [],
+            )
+          end
+          it 'user can see client dashboard for assigned client' do
+            get client_path(non_window_destination_client)
+            expect(response).to have_http_status(200)
+          end
+          it 'user cannot see client dashboard for window client' do
+            get client_path(window_destination_client)
+            expect(response).to redirect_to(root_path)
+          end
+        end
+        describe 'when the client has a valid consent in the user\'s coc' do
+          before do
+            past_date = 5.days.ago
+            future_date = Date.current + 1.years
+            non_window_destination_client.update(
+              housing_release_status: non_window_destination_client.class.full_release_string,
+              consent_form_signed_on: past_date,
+              consent_expires_on: future_date,
+              consented_coc_codes: ['ZZ-000'],
+            )
+          end
+          it 'user can see client dashboard for assigned client' do
+            get client_path(non_window_destination_client)
+            expect(response).to have_http_status(200)
+          end
+          it 'user cannot see client dashboard for window client' do
+            get client_path(window_destination_client)
+            expect(response).to redirect_to(root_path)
+          end
+        end
+        describe 'when the client has a valid consent in the user\'s coc and another coc' do
+          before do
+            past_date = 5.days.ago
+            future_date = Date.current + 1.years
+            non_window_destination_client.update(
+              housing_release_status: non_window_destination_client.class.full_release_string,
+              consent_form_signed_on: past_date,
+              consent_expires_on: future_date,
+              consented_coc_codes: ['ZZ-000', 'AA-000'],
+            )
+          end
+          it 'user can see client dashboard for assigned client' do
+            get client_path(non_window_destination_client)
+            expect(response).to have_http_status(200)
+          end
+          it 'user cannot see client dashboard for window client' do
+            get client_path(window_destination_client)
+            expect(response).to redirect_to(root_path)
+          end
+        end
+        describe 'when the client has a valid consent in another coc' do
+          before do
+            past_date = 5.days.ago
+            future_date = Date.current + 1.years
+            non_window_destination_client.update(
+              housing_release_status: non_window_destination_client.class.full_release_string,
+              consent_form_signed_on: past_date,
+              consent_expires_on: future_date,
+              consented_coc_codes: ['AA-000'],
+            )
+          end
+          it 'user cannot see client dashboard for assigned client' do
+            get client_path(non_window_destination_client)
+            expect(response).to redirect_to(root_path)
           end
           it 'user cannot see client dashboard for window client' do
             get client_path(window_destination_client)
