d52b15c8d2663018603417dd45c8be9b7e1749e7
Elliot Anders <elliot@greenriver.org>
2019-11-15 15:23:47 -0500

    Disable un-runnable migration

--- db/migrate/20191115192256_copy_permissions_to_groups_via_rake.rb
+++ db/migrate/20191115192256_copy_permissions_to_groups_via_rake.rb
@@ -1,6 +1,7 @@
 class CopyPermissionsToGroupsViaRake < ActiveRecord::Migration
   def up
-    system("RAILS_ENV=#{Rails.env} bundle exec rake group:create_groups")
-    system("RAILS_ENV=#{Rails.env} bundle exec rake group:copy_user_viewables")
+    # Moved to warehouse migrations so that it happens after all tables are available.
+    # system("RAILS_ENV=#{Rails.env} bundle exec rake group:create_groups")
+    # system("RAILS_ENV=#{Rails.env} bundle exec rake group:copy_user_viewables")
   end
 end
