0427ec8f2e281d24d66622736d6344c3ebde1e82
Sandy Wise <sandy@greenriver.org>
2019-11-18 15:23:29 -0500

    Remove chattiness from middleware

--- app/middleware/id_protector.rb
+++ app/middleware/id_protector.rb
@@ -9,17 +9,14 @@ class IdProtector
   end
 
   def call(env)
-    Rails.logger.error("PROTECTOR: PATH_INFO #{env['PATH_INFO']}")
     request = ActionDispatch::Request.new env
     Rails.application.routes.router.recognize(request) do | route, params |
-      Rails.logger.error("PROTECTOR: PARAMS #{params}")
       params.each do |key, value|
         if key == :id || key.to_s.ends_with?('_id')
           params[key] = ProtectedId::Encoder.decode(value)
         end
       end
       env['PATH_INFO'] = route.format(params)
-      Rails.logger.error("PROTECTOR: FORMAT PATH_INFO #{env['PATH_INFO']}")
     end
     @app.call(env)
   end
