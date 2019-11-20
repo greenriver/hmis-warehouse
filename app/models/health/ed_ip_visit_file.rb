4ef0e96ef1cc3f8a64ff1b01c9f9b0569b4abdd2
Elliot Anders <elliot@greenriver.org>
2019-11-20 14:57:06 -0500

    Delete related items when deleting an ED & IP upload

--- app/models/health/ed_ip_visit_file.rb
+++ app/models/health/ed_ip_visit_file.rb
@@ -34,7 +34,7 @@ module Health
     phi_attr :attending_physician, Phi::SmallPopulation
 
     belongs_to :patient, primary_key: :medicaid_id, foreign_key: :medicaid_id
-    has_many :ed_ip_visits
+    has_many :ed_ip_visits, dependent: :destroy
 
     def self.header_map
       {
