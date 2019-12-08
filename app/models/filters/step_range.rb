11b678710fe41095f74601d1587bdf16922be3c8
Elliot Anders <elliot@greenriver.org>
2019-12-08 09:40:16 -0500

    Adjust Decision Efficiency and CAS Process reports to use routes

--- app/models/filters/step_range.rb
+++ app/models/filters/step_range.rb
@@ -6,8 +6,9 @@
 
 module Filters
   class StepRange < ::ModelForm
-    attribute :first,  String, lazy: true, default: -> (o,_) { o.ordered_steps&.first&.first }
-    attribute :second, String, lazy: true, default: -> (o,_) { o.ordered_steps[o&.first]&.first }
+    attribute :route, String, lazy: false, default: -> (o,_) { o.available_routes.first }
+    attribute :first,  String, lazy: false, default: -> (o,_) { o.ordered_steps&.first&.first }
+    attribute :second, String, lazy: false, default: -> (o,_) { o.ordered_steps[o.ordered_steps&.first&.first]&.first }
     attribute :unit,   String, default: 'day'
 
     def units
@@ -21,7 +22,7 @@ module Filters
     # hash from steps to steps that may follow them
     def ordered_steps
       @ordered_steps ||= begin
-        scope = GrdaWarehouse::CasReport#.started_between(start_date: @range.start, end_date: @range.end + 1.day)
+        scope = GrdaWarehouse::CasReport.on_route(route)#.started_between(start_date: @range.start, end_date: @range.end + 1.day)
         step_order = scope.distinct.
           pluck(:match_step, :decision_order).to_h
         steps = step_order.keys
@@ -31,27 +32,32 @@ module Filters
         followups = steps.map do |step|
           followups = scope.where(
             at2.project(Arel.star).
-              where( at2[:client_id].      eq at[:client_id] ).
-              where( at2[:match_id].       eq at[:match_id] ).
-              where( at2[:decision_order]. lt at[:decision_order] ).
-              where( at2[:match_step].     eq step ).
+              where(at2[:client_id].eq(at[:client_id])).
+              where(at2[:match_id].eq(at[:match_id])).
+              where(at2[:decision_order].lt(at[:decision_order])).
+              where(at2[:match_step].eq(step)).
               exists
-          ).distinct.pluck(:match_step, :decision_order).map do |match_step, decision_order|
-            "(#{decision_order}) #{match_step}"
-          end
+          ).distinct.pluck(:match_step, :decision_order).
+            map do |match_step, decision_order|
+              "(#{decision_order}) #{match_step}"
+            end
           [ step, followups ]
         end.to_h
-        
+
         followups.select do |_, followup_steps|
-          followup_steps.any? 
-        end.sort_by do |step,_| 
-          step_order[step] 
+          followup_steps.any?
+        end.sort_by do |step,_|
+          step_order[step]
         end.map do |step,followup_steps|
           [
             "(#{step_order[step]}) #{step}", followup_steps.sort
-          ] 
+          ]
         end.to_h
       end
+    end
+
+    def available_routes
+      @available_routes ||= GrdaWarehouse::CasReport.match_routes
     end
   end
 end
