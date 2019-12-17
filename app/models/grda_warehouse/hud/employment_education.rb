5087675040b633c1bc1b7524477aa745fc00ff64
Sandy Wise <sandy@greenriver.org>
2019-12-17 14:52:06 -0500

    Move more :enrollment before :client

--- app/models/grda_warehouse/hud/employment_education.rb
+++ app/models/grda_warehouse/hud/employment_education.rb
@@ -88,10 +88,10 @@ module GrdaWarehouse::Hud
       end
     end
 
+    belongs_to :enrollment, **hud_enrollment_belongs, inverse_of: :employment_educations
     belongs_to :direct_client, **hud_assoc(:PersonalID, 'Client'), inverse_of: :direct_employment_educations
     has_one :client, through: :enrollment, inverse_of: :employment_educations
     belongs_to :export, **hud_assoc(:ExportID, 'Export'), inverse_of: :employment_educations, optional: true
-    belongs_to :enrollment, **hud_enrollment_belongs, inverse_of: :employment_educations
     has_one :project, through: :enrollment
     belongs_to :data_source
 
