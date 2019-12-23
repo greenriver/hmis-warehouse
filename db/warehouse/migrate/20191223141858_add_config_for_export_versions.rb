5e7db86ae31631d3fc4a3e7f72c4dda43d15616a
Sandy Wise <sandy@greenriver.org>
2019-12-23 11:32:56 -0500

    Fix typo

--- db/warehouse/migrate/20191223141858_add_config_for_export_versions.rb
+++ db/warehouse/migrate/20191223141858_add_config_for_export_versions.rb
@@ -1,4 +1,4 @@
-class AddConfigForExportVerions < ActiveRecord::Migration
+class AddConfigForExportVersions < ActiveRecord::Migration
   def change
     add_column :configs, :only_most_recent_import, :boolean, default: :false
   end
