70e704479dee191e20587b8d62683703661f8b3b
Elliot Anders <elliot@greenriver.org>
2019-12-27 10:49:47 -0500

    Missing active record declaration

--- db/warehouse/migrate/20191219154817_add_college_questions_to_youth_intake.rb
+++ db/warehouse/migrate/20191219154817_add_college_questions_to_youth_intake.rb
@@ -1,4 +1,4 @@
-class AddCollegeQuestionsToYouthIntake < ActiveRecord::Migration
+class AddCollegeQuestionsToYouthIntake < ActiveRecord::Migration[4.2]
   def change
     add_column :youth_intakes, :college_pilot, :string, null: false, default: 'No'
     add_column :youth_intakes, :graduating_college, :string, null: false, default: 'No'
