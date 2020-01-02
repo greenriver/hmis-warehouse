a745dd4c5395b880b5e9b509a85136cf9c592b2d
Elliot Anders <elliot@greenriver.org>
2020-01-02 13:53:01 -0500

    Migration tagging

--- db/migrate/20191227161954_add_ad_hoc_data_source_permission.rb
+++ db/migrate/20191227161954_add_ad_hoc_data_source_permission.rb
@@ -1,4 +1,4 @@
-class AddAdHocDataSourcePermission < ActiveRecord::Migration
+class AddAdHocDataSourcePermission < ActiveRecord::Migration[4.2]
   def up
     Role.ensure_permissions_exist
     Role.reset_column_information
