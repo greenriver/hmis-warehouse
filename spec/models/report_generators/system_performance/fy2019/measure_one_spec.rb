21024c5446b8b9685e849ef3aceffb9bad33090b
Sandy Wise <sandy@greenriver.org>
2020-01-02 16:17:06 -0500

    Measure One Example from SPM Spec 3.1

--- spec/models/report_generators/system_performance/fy2019/measure_one_spec.rb
+++ spec/models/report_generators/system_performance/fy2019/measure_one_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ReportGenerators::SystemPerformance::Fy2019::MeasureOne, type: :model do
-  let(:user) { create :user }
-  let(:report) { create :spm_measure_one_fy2019 }
+  let!(:super_user_role) { create :can_edit_anything_super_user }
+  let!(:user) { create :user, roles: [super_user_role] }
+  let!(:report) { create :spm_measure_one_fy2019 }
   let!(:report_result) do
     create :report_result,
            report: report,
            user: user,
            options: {
-             report_start: Date.today,
-             report_end: Date.today,
+             report_start: Date.parse('2016-1-1'),
+             report_end: Date.parse('2016-12-31'),
              project_group_ids: [],
              project_id: [],
            }
@@ -20,7 +21,8 @@ RSpec.describe ReportGenerators::SystemPerformance::Fy2019::MeasureOne, type: :m
   before(:all) do
     @delete_later = []
     @data_source = GrdaWarehouse::DataSource.create(name: 'Green River', short_name: 'GR', source_type: :sftp)
-    file_path = 'spec/fixtures/files/system_performance/hmis_twenty_twenty'
+    GrdaWarehouse::DataSource.create(name: 'Warehouse', short_name: 'W')
+    file_path = 'spec/fixtures/files/system_performance/measure_one'
     import(file_path, @data_source)
     GrdaWarehouse::Tasks::IdentifyDuplicates.new.run!
     GrdaWarehouse::Tasks::ProjectCleanup.new.run!
@@ -36,8 +38,29 @@ RSpec.describe ReportGenerators::SystemPerformance::Fy2019::MeasureOne, type: :m
     Delayed::Job.delete_all
   end
 
-  it 'runs the measure' do
+  before(:each) do
     measure.run!
+    report_result.reload
+  end
+
+  it 'finds one 1a client' do
+    expect(report_result.results['onea_c2']['value']).to eq(1)
+  end
+
+  it 'reports 5 months 1a days' do
+    days = (Date.parse('2016-4-30') - Date.parse('2016-2-1')).to_i +
+      (Date.parse('2016-10-31') - Date.parse('2016-9-1')).to_i
+    expect(report_result.results['onea_e2']['value']).to eq(days)
+  end
+
+  it 'finds one 1b client' do
+    expect(report_result.results['oneb_c2']['value']).to eq(1)
+  end
+
+  it 'reports 13 months 1b days' do
+    days = (Date.parse('2016-4-30') - Date.parse('2015-8-1')).to_i +
+      (Date.parse('2016-10-31') - Date.parse('2016-7-1')).to_i
+    expect(report_result.results['oneb_e2']['value']).to eq(days)
   end
 
   def import(file_path, data_source)
