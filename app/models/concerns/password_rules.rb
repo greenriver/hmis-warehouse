52f972e5c94c1f16a8f6c391bbe02f5bdb3d456e
Elliot Anders <elliot@greenriver.org>
2020-01-03 11:54:19 -0500

    Simplification of regex

--- app/models/concerns/password_rules.rb
+++ app/models/concerns/password_rules.rb
@@ -90,7 +90,7 @@ module PasswordRules
     return false unless password.present?
 
     count = 3
-    regex = Regexp.new(/(?:(?:0(?=1)|1(?=2)|2(?=3)|3(?=4)|4(?=5)|5(?=6)|6(?=7)|7(?=8)|8(?=9)){#{count},}\d|(?:a(?=b)|b(?=c)|c(?=d)|d(?=e)|e(?=f)|f(?=g)|g(?=h)|h(?=i)|i(?=j)|j(?=k)|k(?=l)|l(?=m)|m(?=n)|n(?=o)|o(?=p)|p(?=q)|q(?=r)|r(?=s)|s(?=t)|t(?=u)|u(?=v)|v(?=w)|w(?=x)|x(?=y)|y(?=z)){#{count},}[a-z])/i, Regexp::IGNORECASE)
+    regex = Regexp.new(/(?:(?:0(?=1)|1(?=2)|2(?=3)|3(?=4)|4(?=5)|5(?=6)|6(?=7)|7(?=8)|8(?=9)){#{count},}\d|(?:a(?=b)|b(?=c)|c(?=d)|d(?=e)|e(?=f)|f(?=g)|g(?=h)|h(?=i)|i(?=j)|j(?=k)|k(?=l)|l(?=m)|m(?=n)|n(?=o)|o(?=p)|p(?=q)|q(?=r)|r(?=s)|s(?=t)|t(?=u)|u(?=v)|v(?=w)|w(?=x)|x(?=y)|y(?=z)){#{count},}[a-z])/i)
     password.match?(regex) || password.reverse.match?(regex)
   end
 
