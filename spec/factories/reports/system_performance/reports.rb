c41cdaec00ad71724389c3afa3c4b2ea185cb81b
Sandy Wise <sandy@greenriver.org>
2020-01-03 16:22:59 -0500

    Measure Two Example from SPM Spec 3.1

--- spec/factories/reports/system_performance/reports.rb
+++ spec/factories/reports/system_performance/reports.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   factory :spm_measure_one_fy2019, class: 'Reports::SystemPerformance::Fy2019::MeasureOne' do
     name { 'SPM Measure One' }
   end
+
+  factory :spm_measure_two_fy2019, class: 'Reports::SystemPerformance::Fy2019::MeasureTwo' do
+    name { 'SPM Measure Two' }
+  end
 end
---
+++ spec/models/report_generators/system_performance/fy2019/measure_two_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe ReportGenerators::SystemPerformance::Fy2019::MeasureTwo, type: :model do
+  let!(:super_user_role) { create :can_edit_anything_super_user }
+  let!(:user) { create :user, roles: [super_user_role] }
+  let!(:report) { create :spm_measure_two_fy2019 }
+  let!(:report_result) do
+    create :report_result,
+           report: report,
+           user: user,
+           options: {
+             report_start: Date.parse('2015-1-1'),
+             report_end: Date.parse('2015-12-31'),
+             project_group_ids: [],
+             project_id: [],
+           }
+  end
+
+  let(:measure) { ReportGenerators::SystemPerformance::Fy2019::MeasureTwo.new({}) }
+
+  before(:all) do
+    @delete_later = []
+    @data_source = GrdaWarehouse::DataSource.create(name: 'Green River', short_name: 'GR', source_type: :sftp)
+    GrdaWarehouse::DataSource.create(name: 'Warehouse', short_name: 'W')
+    file_path = 'spec/fixtures/files/system_performance/measure_two'
+    import(file_path, @data_source)
+    GrdaWarehouse::Tasks::IdentifyDuplicates.new.run!
+    GrdaWarehouse::Tasks::ProjectCleanup.new.run!
+    GrdaWarehouse::Tasks::ServiceHistory::Add.new.run!
+
+    Delayed::Worker.new.work_off(2)
+  end
+
+  after(:all) do
+    # Because we are only running the import once, we have to do our own DB and file cleanup
+    GrdaWarehouse::Utility.clear!
+    cleanup_files
+    Delayed::Job.delete_all
+  end
+
+  before(:each) do
+    measure.run!
+    report_result.reload
+  end
+
+  it 'counts 3 clients exiting to PH' do
+    expect(report_result.results['two_b7']['value']).to eq(3)
+  end
+
+  it 'counts 2 clients returning to homelessness' do
+    expect(report_result.results['two_i7']['value']).to eq(2)
+  end
+
+  it 'counts no clients returning to homelessness in less than 6 months' do
+    expect(report_result.results['two_c7']['value']).to eq(0)
+  end
+
+  it 'counts no clients returning to homelessness in 6-12 months' do
+    expect(report_result.results['two_e7']['value']).to eq(0)
+  end
+
+  it 'count 2 clients returning to homelessness in 13-24 months' do
+    expect(report_result.results['two_g7']['value']).to eq(2)
+  end
+
+  def import(file_path, data_source)
+    source_file_path = File.join(file_path, 'source')
+    import_path = File.join(file_path, data_source.id.to_s)
+    # duplicate the fixture file as it gets manipulated
+    FileUtils.cp_r(source_file_path, import_path)
+    @delete_later << import_path unless import_path == source_file_path
+
+    importer = Importers::HmisTwentyTwenty::Base.new(file_path: file_path, data_source_id: data_source.id, remove_files: false)
+    importer.import!
+  end
+
+  def cleanup_files
+    @delete_later.each do |path|
+      FileUtils.rm_rf(path)
+    end
+  end
+end
