d0620fdbd0015d1a083902aa874391ac9bcf1445
Sandy Wise <sandy@greenriver.org>
2020-01-08 12:57:27 -0500

    Add copyright notice to files

--- app/models/bo/config.rb
+++ app/models/bo/config.rb
@@ -1,3 +1,9 @@
+###
+# Copyright 2016 - 2020 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/master/LICENSE.md
+###
+
 module Bo
   class Config < GrdaWarehouseBase
     self.table_name = :bo_configs
