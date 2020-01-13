20cbaa5b0e6c8fdd63e1bdfcce57c5311d59f25f
Elliot Anders <elliot@greenriver.org>
2020-01-13 13:42:05 -0500

    Batch insert rows for Encounters report

--- app/models/health/encounter_report.rb
+++ app/models/health/encounter_report.rb
@@ -16,32 +16,36 @@ module Health
 
     def run_and_save!
       update(started_at: Time.current)
-      activity_scope.find_each do |activity|
-        patient = activity.patient
-        next unless patient
+      activity_scope.find_in_batches do |batch|
+        records = []
+        batch.each do |activity|
+          patient = activity.patient
+          next unless patient
 
-        record = {
-          medicaid_id: patient.medicaid_id,
-          dob: patient.birthdate,
-          gender: patient.gender,
-          race: patient.race,
-          ethnicity: patient.ethnicity,
-          veteran_status: patient.veteran_status,
+          record = {
+            medicaid_id: patient.medicaid_id,
+            dob: patient.birthdate,
+            gender: patient.gender,
+            race: patient.race,
+            ethnicity: patient.ethnicity,
+            veteran_status: patient.veteran_status,
 
-          date: activity.date_of_activity,
-          contact_reached: activity.reached_client,
-          mode_of_contact: activity.mode_of_contact,
-          provider_name: activity.user_full_name,
+            date: activity.date_of_activity,
+            contact_reached: activity.reached_client,
+            mode_of_contact: activity.mode_of_contact,
+            provider_name: activity.user_full_name,
 
-          encounter_report: self
-        }
-        encounter = activity.source
-        next unless encounter
+            encounter_report_id: self.id,
+          }
+          encounter = activity.source
+          next unless encounter
 
-        record.merge(encounter.encounter_source)
-        record.merge(encounter.encounter_report_details)
+          record.merge(encounter.encounter_source)
+          record.merge(encounter.encounter_report_details)
 
-        Health::EncounterRecord.create(record)
+          records << record
+        end
+        Health::EncounterRecord.import(records)
       end
       update(completed_at: Time.current)
     end
