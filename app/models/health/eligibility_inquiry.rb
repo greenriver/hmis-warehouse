8b51d0dd49c5911beb347acfd686e412de2dca8a
Sandy Wise <sandy@greenriver.org>
2020-01-14 13:20:44 -0500

    Change if ! to unless

--- app/models/health/eligibility_inquiry.rb
+++ app/models/health/eligibility_inquiry.rb
@@ -33,7 +33,7 @@ module Health
     end
 
     def eligible_ids
-      return eligibility_response.eligible_ids if !has_batch
+      return eligibility_response.eligible_ids unless has_batch
 
       ids = []
       batch_responses.each do |response|
@@ -43,7 +43,7 @@ module Health
     end
 
     def ineligible_ids
-      return eligibility_response.ineligible_ids if !has_batch
+      return eligibility_response.ineligible_ids unless has_batch
 
       ids = []
       batch_responses.each do |response|
