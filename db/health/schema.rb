372607c5fefc9f76a64622fd4b4b13ca67702029
Sandy Wise <sandy@greenriver.org>
2020-01-24 15:27:52 -0500

    Fix missing visits table in schema

--- db/health/schema.rb
+++ db/health/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_160822) do
+ActiveRecord::Schema.define(version: 2020_01_24_194225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
