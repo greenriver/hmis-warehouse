c2acb9c463e154a509b40cff1b3d418f42242718
Elliot Anders <elliot@greenriver.org>
2020-02-04 09:11:54 -0500

    Fixes for bootstrapping with fast gettext

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,14 +1,25 @@
 # db access is cached <-> only first lookup hits the db
-require "fast_gettext/translation_repository/db"
-FastGettext::TranslationRepository::Db.require_models #load and include default models
+def database_exists?
+  ActiveRecord::Base.connection
+rescue ActiveRecord::NoDatabaseError
+  false
+else
+  true
+end
+if database_exists? && ActiveRecord::Base.connection.table_exists?('translation_keys')
+  require 'gettext_i18n_rails'
+  require 'fast_gettext'
+  require "fast_gettext/translation_repository/db"
+  FastGettext::TranslationRepository::Db.require_models #load and include default models
 
-FastGettext.add_text_domain(
-  'hmis_warehouse', 
-  :path => 'locale', 
-  :type => :db,
-  :model => TranslationKey,
-  :ignore_fuzzy => true,
-  # :report_warning => false
-)
-FastGettext.default_available_locales = ['en']
-FastGettext.default_text_domain = 'hmis_warehouse'
+  FastGettext.add_text_domain(
+    'hmis_warehouse',
+    :path => 'locale',
+    :type => :db,
+    :model => TranslationKey,
+    :ignore_fuzzy => true,
+    # :report_warning => false
+  )
+  FastGettext.default_available_locales = ['en']
+  FastGettext.default_text_domain = 'hmis_warehouse'
+end
