45f6e7d73f732910a78f2309ed1047f4130e035e
Elliot Anders <elliot@greenriver.org>
2020-02-14 14:10:51 -0500

    Cleanup regex

--- app/models/consent_limit.rb
+++ app/models/consent_limit.rb
@@ -12,7 +12,7 @@ class ConsentLimit < ActiveRecord::Base
   has_many :agencies, through: :agencies_consent_limits
 
   validates_presence_of :name, :color
-  validates :name, format: { with: /[\A-\Z][\A-\Z]-\d\d\d/, message: "Must match the format ZZ-000" }
+  validates :name, format: { with: /\A[A-Z][A-Z]-\d\d\d\z/, message: "Must match the format ZZ-000" }
 
   def self.available_cocs
     all.map do |c|
