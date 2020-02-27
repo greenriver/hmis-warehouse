76740488601086465c508e45692865a0c21d7faa
Sandy Wise <sandy@greenriver.org>
2020-02-27 11:59:04 -0500

    Minimal drilldown on Youth Intakes report

--- app/controllers/warehouse_reports/youth_intakes_controller.rb
+++ app/controllers/warehouse_reports/youth_intakes_controller.rb
@@ -6,8 +6,31 @@
 
 module WarehouseReports
   class YouthIntakesController < ApplicationController
+    include PjaxModalController
+
+    before_action :set_filter
+    before_action :set_report
+
     def index
+    end
+
+    def details
+      raise 'Key required' if params[:key].blank?
+
+      @key = @report.report_whitelist.detect { |key| key.to_s == params[:key] }
+      client_ids = @report.send(@key)
+      @clients = GrdaWarehouse::Hud::Client.
+        destination.
+        where(id: client_ids).
+        order(:first_name, :last_name).
+        pluck(:id, :first_name, :last_name)
+    end
+
+    private def set_filter
       @filter = ::Filters::DateRange.new(report_params[:filter])
+    end
+
+    private def set_report
       @report = GrdaWarehouse::WarehouseReports::Youth::HomelessYouthReport.new(@filter)
     end
 
@@ -19,5 +42,10 @@ module WarehouseReports
         ],
       )
     end
+
+    private def data_link(data_point)
+      helpers.link_to @report.send(data_point).count, details_warehouse_reports_youth_intakes_path(filter: { start: @filter.start, end: @filter.end }, key: data_point), data: { loads_in_pjax_modal: true }
+    end
+    helper_method :data_link
   end
 end
