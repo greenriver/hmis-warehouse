22fc20f964376e73a5982d0cd1b7a02939a86157
Elliot Anders <elliot@greenriver.org>
2020-03-06 14:23:42 -0500

    WIP; initial pass at sending files directly to S3

--- app/uploaders/import_uploader.rb
+++ app/uploaders/import_uploader.rb
@@ -12,7 +12,7 @@ class ImportUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
