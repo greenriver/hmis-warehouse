063ed35fd1ceade56df24bd9917f488906ed2cd1
Elliot Anders <elliot@greenriver.org>
2020-03-24 18:17:39 -0400

    Test fixes for new sign-in form

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -4,12 +4,11 @@ RSpec.feature 'Accounts', type: :feature do
   let(:user) { create(:user) }
 
   before(:each) do
-    visit root_path
+    visit new_user_session_path
   end
 
   feature 'Logging In' do
     scenario 'with wrong password' do
-      click_link 'Sign In'
       fill_in 'Email', with: 'noreply@example.com'
       fill_in 'Password', with: 'password'
       click_button 'Log in'
@@ -17,7 +16,6 @@ RSpec.feature 'Accounts', type: :feature do
     end
 
     scenario 'with correct password' do
-      click_link 'Sign In'
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button 'Log in'
@@ -27,7 +25,7 @@ RSpec.feature 'Accounts', type: :feature do
     feature 'Devise lockable' do
       scenario 'account locks after maximum failed attempts' do
         # Account should lock after a certain number of failed attempts
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
@@ -41,7 +39,7 @@ RSpec.feature 'Accounts', type: :feature do
 
       scenario 'account remains locked up until the lockout time is reached' do
         # Jump forward the to just before the account should be unlocked, it should still be locked
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
@@ -57,7 +55,7 @@ RSpec.feature 'Accounts', type: :feature do
 
       scenario 'account is unlocked after time passes' do
         # Jump forward the necessary amount of time and verify that the account is unlocked
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
