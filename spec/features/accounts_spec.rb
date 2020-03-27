b6c7d7bc19bbbb122da097c3b2496bfd2faf4678
Elliot Anders <elliot@greenriver.org>
2020-03-27 12:40:18 -0400

    Check that we can logout

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -65,7 +65,7 @@ RSpec.feature 'Accounts', type: :feature do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: user.password
           click_button 'Log in'
-          expect(page).to_not have_content 'Sign In'
+          expect(page).to have_content 'Sign Out'
         end
       end
     end
