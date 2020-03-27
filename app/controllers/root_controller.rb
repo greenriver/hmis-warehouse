5ac80435ec282e96c6cdae5b3aa8ed7af40ad0f8
Elliot Anders <elliot@greenriver.org>
2020-03-27 09:16:33 -0400

    Redirect to client search if you can see it and there's no content on the homepage

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -7,5 +7,7 @@
 class RootController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    custom_content = lookup_context.exists?('homepage_content', ['root'], true)
+    redirect_to clients_path if current_user&.can_access_some_client_search? && ! custom_content
   end
 end
