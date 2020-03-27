72e74ad1f6109ce5749290f3d2fa18fead1918e5
Elliot Anders <elliot@greenriver.org>
2020-03-27 13:39:02 -0400

    Fix for root path redirection

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -8,7 +8,9 @@ class RootController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     custom_content = lookup_context.exists?('homepage_content', ['root'], true)
-    already_there = current_user && current_user.my_root_path != root_path
-    redirect_to current_user.my_root_path if already_there && ! custom_content
+    return unless current_user
+
+    already_there = current_user.my_root_path == root_path
+    redirect_to current_user.my_root_path if ! already_there && ! custom_content
   end
 end
