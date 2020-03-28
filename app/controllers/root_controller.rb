f02d983197d149b27328224edc9ce7f2e7af5399
Elliot Anders <elliot@greenriver.org>
2020-03-28 12:20:09 -0400

    Always redirect to my_root_path if logged in

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -7,10 +7,10 @@
 class RootController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    custom_content = lookup_context.exists?('homepage_content', ['root'], true)
+    # custom_content = lookup_context.exists?('homepage_content', ['root'], true)
     return unless current_user
 
     already_there = current_user.my_root_path == root_path
-    redirect_to current_user.my_root_path if ! already_there && ! custom_content
+    redirect_to current_user.my_root_path unless already_there
   end
 end
