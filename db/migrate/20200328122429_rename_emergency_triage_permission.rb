f5b7fc0628c998420231446a51926e37d2aeaa4b
Elliot Anders <elliot@greenriver.org>
2020-03-30 11:26:20 -0400

    Removing un-deployed permission

--- db/migrate/20200328122429_rename_emergency_triage_permission.rb
+++ db/migrate/20200328122429_rename_emergency_triage_permission.rb
@@ -1,6 +1,6 @@
 class RenameEmergencyTriagePermission < ActiveRecord::Migration[5.2]
   def up
-    remove_column :roles, :can_edit_health_emergency_triage
+    # remove_column :roles, :can_edit_health_emergency_triage
 
     Role.ensure_permissions_exist
     Role.reset_column_information
