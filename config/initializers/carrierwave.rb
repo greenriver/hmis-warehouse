4f2199ee788adc4a43c66305989f41a08e5cbd2f
Elliot Anders <elliot@greenriver.org>
2020-03-31 09:45:20 -0400

    Use present, and re-use variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
 CarrierWave.configure do |config|
   tmp_bucket = ENV['S3_TMP_BUCKET']
-  tmp_user = ENV['S3_TMP_ACCESS_KEY_ID']
-  tmp_key = ENV['S3_TMP_ACCESS_KEY_SECRET']
-  if tmp_bucket && tmp_user && tmp_key
+  tmp_access_key = ENV['S3_TMP_ACCESS_KEY_ID']
+  tmp_secret_key = ENV['S3_TMP_ACCESS_KEY_SECRET']
+  if tmp_bucket.present? && tmp_access_key.present? && tmp_secret_key.present?
     config.storage = :aws
     config.aws_acl  = 'private'
-    config.aws_bucket = ENV.fetch('S3_TMP_BUCKET')
+    config.aws_bucket = tmp_bucket
 
     config.aws_credentials = {
-      access_key_id: ENV.fetch('S3_TMP_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('S3_TMP_ACCESS_KEY_SECRET'),
+      access_key_id: tmp_access_key,
+      secret_access_key: tmp_secret_key,
       region: ENV.fetch('S3_TMP_REGION'),
       stub_responses:    Rails.env.test?, # Optional, avoid hitting S3 actual during tests
     }
