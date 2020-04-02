62f52113d5ed95f2bf7f15bd04cb63844afeb485
Elliot Anders <elliot@greenriver.org>
2020-04-02 16:33:30 -0400

    Fix for client performance issues?

--- app/controllers/concerns/client_dependent_controllers.rb
+++ app/controllers/concerns/client_dependent_controllers.rb
@@ -12,18 +12,27 @@ module ClientDependentControllers
       GrdaWarehouse::Hud::Client
     end
 
-    def searchable_client_scope
+    def searchable_client_scope(id: nil)
       # client_source.destination.searchable_by(current_user)
-      client_source.destination.where(
+      scope = client_source.destination
+      scope = scope.where(id: id) if id
+
+      # Query the source clients
+      source_client_query = GrdaWarehouse::WarehouseClient.joins(:source).
+        merge(GrdaWarehouse::Hud::Client.searchable_by(current_user)).
+        select(:destination_id)
+      source_client_query = source_client_query.where(destination_id: id) if id
+
+      # Query the destination clients
+      destination_client_query = GrdaWarehouse::Hud::Client.searchable_by(current_user)
+      destination_client_query = destination_client_query.where(id: id) if id
+
+      scope.where(
         client_source.arel_table[:id].in(
-          Arel.sql(
-            GrdaWarehouse::WarehouseClient.joins(:source).
-              merge(GrdaWarehouse::Hud::Client.searchable_by(current_user)).
-              select(:destination_id).to_sql,
-          ),
+          Arel.sql(source_client_query.to_sql),
         ).
         or(
-          client_source.arel_table[:id].in(Arel.sql(GrdaWarehouse::Hud::Client.searchable_by(current_user).select(:id).to_sql)),
+          client_source.arel_table[:id].in(Arel.sql(destination_client_query.select(:id).to_sql)),
         ),
       )
     end
