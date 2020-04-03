54eca54a42473dd45b01cfd3a41a60261507c614
Elliot Anders <elliot@greenriver.org>
2020-04-03 15:09:16 -0400

    QA fixes

--- app/inputs/collection_check_boxes_input.rb
+++ app/inputs/collection_check_boxes_input.rb
@@ -9,7 +9,7 @@ class CollectionCheckBoxesInput < SimpleForm::Inputs::CollectionCheckBoxesInput
     if @builder.options[:wrapper] == :readonly
       label_method = detect_collection_methods.first
       value_method = detect_collection_methods.last
-      selected_value = object.send(attribute_name)
+      selected_value = object.send(attribute_name) || []
       selected_object = collection.select { |m| m.send(value_method).to_s.in?(selected_value) }
       value = selected_object.map { |m| m.send(label_method) }.join(', ')
       existing_classes = label_html_options.try(:[], :class)
