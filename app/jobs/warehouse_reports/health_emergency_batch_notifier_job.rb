6d9acd4f1abc1747e0afce2dde89d9d9993ff3b8
Elliot Anders <elliot@greenriver.org>
2020-04-24 14:20:01 -0400

    Use an advisory lock to prevent duplicate emails

--- app/jobs/warehouse_reports/health_emergency_batch_notifier_job.rb
+++ app/jobs/warehouse_reports/health_emergency_batch_notifier_job.rb
@@ -7,30 +7,35 @@
 module WarehouseReports
   class HealthEmergencyBatchNotifierJob < BaseJob
     def perform
-      medical_restriction_batch_id = nil
-      test_batch_id = nil
+      advisory_lock_name = he_batch_notifications
+      return if GrdaWarehouse::HealthEmergency::AmaRestriction.advisory_lock_exists?(advisory_lock_name)
 
-      unsent_medical_restrictions = GrdaWarehouse::HealthEmergency::AmaRestriction.active.unsent.count
-      medical_restriction_batch_id = GrdaWarehouse::HealthEmergency::AmaRestriction.next_batch_id if unsent_medical_restrictions.positive?
+      with_advisory_lock(advisory_lock_name) do
+        medical_restriction_batch_id = nil
+        test_batch_id = nil
 
-      unsent_test_results = GrdaWarehouse::HealthEmergency::Test.unsent.count
-      test_batch_id = GrdaWarehouse::HealthEmergency::Test.next_batch_id if unsent_test_results.positive?
+        unsent_medical_restrictions = GrdaWarehouse::HealthEmergency::AmaRestriction.active.unsent.count
+        medical_restriction_batch_id = GrdaWarehouse::HealthEmergency::AmaRestriction.next_batch_id if unsent_medical_restrictions.positive?
 
-      return unless medical_restriction_batch_id || test_batch_id
+        unsent_test_results = GrdaWarehouse::HealthEmergency::Test.unsent.count
+        test_batch_id = GrdaWarehouse::HealthEmergency::Test.next_batch_id if unsent_test_results.positive?
 
-      User.receives_medical_restriction_notifications.distinct.pluck(:id).each do |user_id|
-        NotifyUser.health_emergency_change(
-          user_id, medical_restriction_batch_id:
-          medical_restriction_batch_id,
-                   unsent_medical_restrictions: unsent_medical_restrictions,
-                   test_batch_id: test_batch_id,
-                   unsent_test_results: unsent_test_results
-        ).deliver_later
+        return unless medical_restriction_batch_id || test_batch_id
+
+        User.receives_medical_restriction_notifications.distinct.pluck(:id).each do |user_id|
+          NotifyUser.health_emergency_change(
+            user_id,
+            medical_restriction_batch_id: medical_restriction_batch_id,
+            unsent_medical_restrictions: unsent_medical_restrictions,
+            test_batch_id: test_batch_id,
+            unsent_test_results: unsent_test_results,
+          ).deliver_later
+        end
+
+        sent_at = Time.current
+        GrdaWarehouse::HealthEmergency::AmaRestriction.unsent.update_all(notification_at: sent_at, notification_batch_id: medical_restriction_batch_id)
+        GrdaWarehouse::HealthEmergency::Test.unsent.update_all(notification_at: sent_at, notification_batch_id: test_batch_id)
       end
-
-      sent_at = Time.current
-      GrdaWarehouse::HealthEmergency::AmaRestriction.unsent.update_all(notification_at: sent_at, notification_batch_id: medical_restriction_batch_id)
-      GrdaWarehouse::HealthEmergency::Test.unsent.update_all(notification_at: sent_at, notification_batch_id: test_batch_id)
     end
 
     def max_attempts
