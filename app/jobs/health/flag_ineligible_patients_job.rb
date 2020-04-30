6ffd1ac07f3a00a3d1fb7cb0ea6ee3930a423009
Sandy Wise <sandy@greenriver.org>
2020-04-30 16:44:24 -0400

    Store ACO changes on 271 record

--- app/jobs/health/flag_ineligible_patients_job.rb
+++ app/jobs/health/flag_ineligible_patients_job.rb
@@ -13,6 +13,8 @@ module Health
       return unless response
 
       Health::Patient.transaction do
+        patient_aco_changes = {}
+
         # Update the last check date, and since we got a response, we know the id is valid
         patient_scope(response.subscriber_ids).find_each do |patient|
           patient.update!(coverage_inquiry_date: inquiry.service_date,
@@ -34,15 +36,26 @@ module Health
         # Set the eligibility level for managed care patients
         patient_scope(response.managed_care_ids - response.ineligible_ids).find_each do |patient|
           aco = response.aco_names[patient.medicaid_id]
+
           previous_aco = patient.aco_name if patient.aco_name.present? && patient.aco_name != aco
           patient.update!(coverage_level: Health::Patient.coverage_level_managed_value,
                           previous_aco_name: previous_aco,
                           aco_name: aco)
+
           if aco.present?
             aco_id = Health::AccountableCareOrganization.active.find_by(edi_name: aco)&.id
             patient.patient_referral.update!(accountable_care_organization_id: aco_id) if aco_id.present?
+
+            if previous_aco.present?
+              patient_aco_changes[patient.id] = {
+                previous_aco: previous_aco,
+                updated: aco_id.present?,
+              }
+            end
           end
         end
+
+        response.update(patient_aco_changes: patient_aco_changes)
 
         # Mark ineligible patients
         patient_scope(response.ineligible_ids).find_each do |patient|
