cae7e73dee603d724423f385176f248d89611793
Elliot Anders <elliot@greenriver.org>
2020-05-02 08:36:34 -0400

    Use 99 for missin TypeProvided

--- app/models/grda_warehouse/export/hmis_twenty_twenty/service.rb
+++ app/models/grda_warehouse/export/hmis_twenty_twenty/service.rb
@@ -15,5 +15,10 @@ module GrdaWarehouse::Export::HmisTwentyTwenty
     # enrollment has been deleted
     belongs_to :enrollment_with_deleted, class_name: 'GrdaWarehouse::Hud::WithDeleted::Enrollment', primary_key: [:EnrollmentID, :PersonalID, :data_source_id], foreign_key: [:EnrollmentID, :PersonalID, :data_source_id]
 
+    def apply_overrides row, data_source_id:
+      row[:TypeProvided] = 99 if row[:TypeProvided].blank?
+
+      return row
+    end
   end
 end
