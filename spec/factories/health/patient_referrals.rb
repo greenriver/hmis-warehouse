d5067bb7783a6d87e65f2b462201681f3ad254b1
Sandy Wise <sandy@greenriver.org>
2020-05-07 16:17:33 -0400

    Rework test to handle multiple referrals

--- spec/factories/health/patient_referrals.rb
+++ spec/factories/health/patient_referrals.rb
@@ -7,4 +7,36 @@ FactoryBot.define do
     enrollment_start_date { Date.current }
     current { true }
   end
+
+  factory :prior_referral, class: 'Health::PatientReferral' do
+    first_name { 'Patient' }
+    last_name { 'A' }
+    birthdate { Date.current }
+    sequence(:medicaid_id)
+    enrollment_start_date { Date.current - 1.year }
+    disenrollment_date { Date.current - 11.months }
+    current { false }
+    contributing { false }
+  end
+
+  factory :contributing_referral, class: 'Health::PatientReferral' do
+    first_name { 'Patient' }
+    last_name { 'A' }
+    birthdate { Date.current }
+    sequence(:medicaid_id)
+    enrollment_start_date { Date.current - 2.months }
+    disenrollment_date { Date.current - 1.months }
+    current { false }
+    contributing { true }
+  end
+
+  factory :current_referral, class: 'Health::PatientReferral' do
+    first_name { 'Patient' }
+    last_name { 'A' }
+    birthdate { Date.current }
+    sequence(:medicaid_id)
+    enrollment_start_date { Date.current - 2.weeks }
+    current { true }
+    contributing { true }
+  end
 end
--- spec/factories/health/patients.rb
+++ spec/factories/health/patients.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :patient, class: 'Health::Patient' do
     sequence(:id_in_source)
     patient_referral
-    sequence(:client_id)
+    association :client, factory: :hud_client
+  end
+
+  factory :patient_a, class: 'Health::Patient' do
+    sequence(:id_in_source)
+    patient_referrals { [create(:prior_referral), create(:contributing_referral), create(:current_referral)] }
+    association :client, factory: :hud_client
   end
 end
