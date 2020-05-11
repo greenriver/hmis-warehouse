f4113f02d836775230e638690243f8438c71bd53
Elliot Anders <elliot@greenriver.org>
2020-05-11 16:14:08 -0400

    Correctly encode non SVG images

--- lib/util/serialized_asset.rb
+++ lib/util/serialized_asset.rb
@@ -26,12 +26,12 @@ class SerializedAsset
       next unless ENV[key].present? && ENV[content_key].present?
 
       logo_content = Base64.decode64(ENV[content_key])
-
+      logo_content.force_encoding('ascii-8bit')
       next unless logo_content.present?
 
       view_path = "/theme/logo/#{ENV[key]}.#{VERSION}.#{extension}"
       path = "public#{view_path}"
-      File.write(path, logo_content)
+      File.write(path, logo_content, encoding: 'ascii-8bit')
 
       paths[ENV[key]] = OpenStruct.new(src: view_path, server_path: path)
     end
