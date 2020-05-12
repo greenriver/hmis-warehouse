d82ba8de2a5a5cc5e01f369b0bf325e9667249a0
Elliot Anders <elliot@greenriver.org>
2020-05-12 13:43:44 -0400

    Missing CoC Codes report

--- app/controllers/warehouse_reports/hud/missing_coc_codes_controller.rb
+++ app/controllers/warehouse_reports/hud/missing_coc_codes_controller.rb
@@ -7,11 +7,31 @@
 module WarehouseReports::Hud
   class MissingCoCCodesController < ApplicationController
     include WarehouseReportAuthorization
+    include ArelHelper
+    before_action :set_filter
 
     # This logic is based on 9.2 from the 2020 LSA
     def index
-      @enrollments = GrdaWarehouse::Hud::Enrollment.joins(:client, project: :project_cocs).
-        left_outer_joins(:enrollment_cocs)
+      @enrollments = GrdaWarehouse::Hud::Enrollment.
+        heads_of_households.
+        where(e_t[:EntryDate].lt(@filter.end)).
+        joins(:client, project: :project_cocs).
+        left_outer_joins(:enrollment_cocs, :exit).
+        preload(client: :destination_client).
+        merge(
+          GrdaWarehouse::Hud::Project.
+          viewable_by(current_user).
+          coc_funded.
+          with_hud_project_type(required_for_project_types).
+          where(id: @filter.effective_project_ids),
+        ).
+        merge(GrdaWarehouse::Hud::ProjectCoc.with_coc).
+        where(
+          ex_t[:ExitDate].gteq(@filter.start - 3.years).
+          or(ex_t[:ExitDate].eq(nil)),
+        ).
+        order(EntryDate: :desc).
+        page(params[:page]).per(50)
 
       # NoCoC = (select count (distinct n.HouseholdID)
       # from hmis_Enrollment n
@@ -29,6 +49,25 @@ module WarehouseReports::Hud
       #   and n.RelationshipToHoH = 1
       #   and coc.CoCCode is null
       #   and coc.DateDeleted is null)
+    end
+
+    private def required_for_project_types
+      [1, 2, 3, 8, 13]
+    end
+    helper_method :required_for_project_types
+
+    def set_filter
+      @filter = ::Filters::DateRangeAndSources.new(filter_params.merge(user_id: current_user.id))
+    end
+
+    def filter_params
+      return {} unless params[:filter].present?
+
+      params.require(:filter).permit(
+        :start,
+        :end,
+        coc_codes: [],
+      )
     end
   end
 end
