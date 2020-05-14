3f9bbb05b355dcbca04df7a361f40a5d73b3bda3
Aaron Michal <amichal@greenriver.com>
2020-05-14 16:14:25 -0400

    Write out to a table

--- app/jobs/materialize_permissions.rb
+++ app/jobs/materialize_permissions.rb
@@ -2,26 +2,49 @@
 # have access to which clients.
 # At this point all it does is walk the existing scopes
 # and output timing data
+
 require 'ruby-progressbar'
 require 'pp'
 require 'benchmark'
+
 class MaterializePermissions < BaseJob
+  COLS = ['user_id', 'client_id', 'viewable'].freeze
+
   # WIP:
   def perform
     bar = ProgressBar.create(starting_at: 0, total: nil, format: '%c - %R')
+    insert_batch_size = 1000
     reduce_logging do
+      wh_db_exec("TRUNCATE TABLE #{GrdaWarehouse::UserClientPermission.quoted_table_name}")
+      batch = []
       bm = Benchmark.measure do
         User.find_each do |user|
-          GrdaWarehouse::Hud::Client.destination.searchable_by(user).pluck(:id).map do |client_id|
-            bar.increment
-            JSON.generate(user_id: user.id, client_id: client_id, viewable: true)
+          GrdaWarehouse::Hud::Client.searchable_by(user).pluck(:id).map do |client_id|
+            batch << [user.id, client_id, true] # see COLS
           end
         end
+        if batch.size >= insert_batch_size
+          process_batch(batch)
+          batch.clear
+        end
       end
+      wh_db_exec("ANALYZE VERBOSE #{GrdaWarehouse::UserClientPermission.quoted_table_name}")
       pp bar.to_h.slice('elapsed_time_in_seconds', 'progress')
       pp bm.to_s
     end
     nil
+  end
+
+  private def process_batch(rows)
+    GrdaWarehouse::UserClientPermission.import(COLS, rows, validate: false)
+  end
+
+  private def wh_db
+    GrdaWarehouseBase.connection
+  end
+
+  private def wh_db_exec(sql)
+    puts wh_db.execute(sql).inspect
   end
 
   private def reduce_logging
---
+++ spec/factories/grda_warehouse/user_client_permissions.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :grda_warehouse_user_client_permission, class: 'GrdaWarehouse::UserClientPermission' do
+  end
+end
