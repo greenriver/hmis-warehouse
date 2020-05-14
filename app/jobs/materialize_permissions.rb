0ff03330a65171655dd18079b7468970688e9236
Aaron Michal <amichal@greenriver.com>
2020-05-14 14:49:01 -0400

    Reduce logging and cleanup output

--- app/jobs/materialize_permissions.rb
+++ app/jobs/materialize_permissions.rb
@@ -2,19 +2,33 @@
 # have access to which clients.
 # At this point all it does is walk the existing scopes
 # and output timing data
+require 'ruby-progressbar'
+require 'pp'
+require 'benchmark'
 class MaterializePermissions < BaseJob
   # WIP:
   def perform
     bar = ProgressBar.create(starting_at: 0, total: nil, format: '%c - %R')
-    bm = Benchmark.measure do
-      User.find_each do |user|
-        GrdaWarehouse::Hud::Client.destination.searchable_by(user).pluck(:id).map do |client_id|
-          bar.increment
-          JSON.generate(user_id: user.id, client_id: client_id, viewable: true)
+    reduce_logging do
+      bm = Benchmark.measure do
+        User.find_each do |user|
+          GrdaWarehouse::Hud::Client.destination.searchable_by(user).pluck(:id).map do |client_id|
+            bar.increment
+            JSON.generate(user_id: user.id, client_id: client_id, viewable: true)
+          end
         end
       end
+      pp bar.to_h.slice('elapsed_time_in_seconds', 'progress')
+      pp bm.to_s
     end
-    pp bar.to_h
-    pp bm.to_s
+    nil
+  end
+
+  private def reduce_logging
+    User.logger.silence(Logger::INFO) do
+      GrdaWarehouse::Hud::Client.logger.silence(Logger::INFO) do
+        yield
+      end
+    end
   end
 end
