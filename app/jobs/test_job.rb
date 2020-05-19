e34dcbeda784de6deea3f872cf79659cb6314181
Todd Blackman <tdblackman@gmail.com>
2020-05-19 15:43:20 -0400

    Updates TestJob to be able to consume lots of RAM

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -5,12 +5,17 @@
 ###
 
 class TestJob < BaseJob
-  def perform(length_in_seconds = 10)
+  SLEEP_TIME = 5
+
+  def perform(length_in_seconds: 10, memory_bloat_per_second: 10_000_000)
     a = Time.now
+
+    bloater = {}
 
     while (Time.now - a) < length_in_seconds
       Rails.logger.info "Simulating processing. In `#{STARTING_PATH}` directory."
-      sleep 5
+      bloater[Random.rand.to_s] = Array.new(memory_bloat_per_second * SLEEP_TIME) if memory_bloat_per_second
+      sleep SLEEP_TIME
     end
   end
 
