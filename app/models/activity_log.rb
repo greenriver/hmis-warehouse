5421249ae8e3370e74b49dd8b13767edac3b4004
Elliot Anders <elliot@greenriver.org>
2020-05-22 16:31:08 -0400

    User activity chart

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -5,6 +5,7 @@
 ###
 
 class ActivityLog < ApplicationRecord
+  include ArelHelper
 
   belongs_to :user
 
@@ -14,5 +15,26 @@ class ActivityLog < ApplicationRecord
 
   def clean_object_name
     item_model&.gsub('GrdaWarehouse::Hud::', '')
+  end
+
+  # increment can be: minute, hour, day, week, month, year
+  def self.for_chart(increment: 'hour', range: 1.weeks.ago..Time.current)
+    return [] unless valid_increments.include?(increment)
+    data = {}
+    where(created_at: range).
+    group(:created_at_trunc, :user_id).
+    pluck(Arel.sql("date_trunc('#{increment}', created_at) as created_at_trunc"), :user_id).
+    each do |time, user_id|
+      data[time.strftime('%Y-%m-%d %H:%M')] ||= 0
+      data[time.strftime('%Y-%m-%d %H:%M')] += 1
+    end
+    [
+      ['x'] + data.keys,
+      ['Active Users'] + data.values
+    ]
+  end
+
+  def self.valid_increments
+    ['minute', 'hour', 'day', 'week', 'month', 'year']
   end
 end
