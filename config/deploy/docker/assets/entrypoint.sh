fa26e254fcf41549079e22665f29d18feba1f8a3
Elliot Anders <elliot@greenriver.org>
2020-06-01 15:43:15 -0400

    Better entrypoint for pgpass

--- config/deploy/docker/assets/entrypoint.sh
+++ config/deploy/docker/assets/entrypoint.sh
@@ -25,7 +25,8 @@ echo Syncing the assets from s3
 ./bin/sync_app_assets.rb
 
 echo Setting PGPass
-echo $DATABASE_HOST:*:*:$DATABASE_USER:$DATABASE_PASS > /root/.pgpass
+echo "$DATABASE_HOST:*:*:$DATABASE_USER:$DATABASE_PASS" > /root/.pgpass
+chmod 600 /root/.pgpass
 
 if [ "$NEEDS_PRECOMPILE" = "true" ]
 then
