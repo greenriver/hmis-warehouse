bf6422caeb6c8df6c6dd84606448b5aa655c465b
Todd Blackman <tdblackman@gmail.com>
2020-06-02 15:19:43 -0400

    Puma unification with CAS

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,9 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }.to_i
+threads min_threads_count, max_threads_count
 
 if ENV['PUMA_BIND']
   bind ENV['PUMA_BIND']
