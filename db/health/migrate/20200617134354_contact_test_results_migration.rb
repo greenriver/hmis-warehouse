b2cd6ce60d345b6bbd05d0dcfbf2df93d8f76f23
Sandy Wise <sandy@greenriver.org>
2020-06-18 09:25:57 -0400

    Don't migrate empty test results

--- db/health/migrate/20200617134354_contact_test_results_migration.rb
+++ db/health/migrate/20200617134354_contact_test_results_migration.rb
@@ -1,8 +1,8 @@
 class ContactTestResultsMigration < ActiveRecord::Migration[5.2]
-  def change
+  def up
     Health::Tracing::Contact.with_deleted.each do |contact|
       result = contact.results.build
-      result.update(
+      result.assign_attributes(
         test_result: contact.test_result,
         isolated: contact.isolated,
         isolation_location: contact.isolation_location,
@@ -10,6 +10,17 @@ class ContactTestResultsMigration < ActiveRecord::Migration[5.2]
         quarantine_location: contact.quarantine_location,
         deleted_at: contact.deleted_at,
       )
+      result.save if live?(result)
     end
+  end
+
+  def down
+    Health::Tracing::Result.with_deleted.delete_all
+  end
+
+  def live?(result)
+    result.test_result.present? ||
+      result.isolated.present? || result.isolation_location.present? ||
+      result.quarantine.present? || result.quarantine_location.present?
   end
 end
