ff0d4323fe3751bb0fc48641122e2afd25f46f81
Elliot Anders <elliot@greenriver.org>
2020-06-24 08:57:22 -0400

    Rename master to production

--- app/models/model_form.rb
+++ app/models/model_form.rb
@@ -1,7 +1,7 @@
 ###
 # Copyright 2016 - 2020 Green River Data Analysis, LLC
 #
-# License detail: https://github.com/greenriver/hmis-warehouse/blob/master/LICENSE.md
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
 # for making arbitrary "models" for use in forms which may or may not actually be tied to
--- app/models/report_generators/pit/fy2018/by_project.rb
+++ app/models/report_generators/pit/fy2018/by_project.rb
@@ -1,7 +1,7 @@
 ###
 # Copyright 2016 - 2020 Green River Data Analysis, LLC
 #
-# License detail: https://github.com/greenriver/hmis-warehouse/blob/master/LICENSE.md
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
 # This version of the AHAR report is scoped to a particular project at a particular data source
