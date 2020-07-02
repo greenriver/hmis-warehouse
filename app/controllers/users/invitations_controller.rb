efd9a33da84dc6c784613a40786289243fec8fc2
Elliot Anders <elliot@greenriver.org>
2020-07-02 08:13:44 -0400

    Fix to allow groups to be added to a user at creation time

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -72,6 +72,7 @@ class Users::InvitationsController < Devise::InvitationsController
       :notify_on_client_added,
       :notify_on_anomaly_identified,
       role_ids: [],
+      access_group_ids: [],
       coc_codes: [],
       contact_attributes: [:id, :first_name, :last_name, :phone, :email, :role],
     )
