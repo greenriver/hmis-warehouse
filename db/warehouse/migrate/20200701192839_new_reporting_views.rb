4ec2910b0e8a13c8537f45f2bff33fbacca0b39d
Aaron Michal <amichal@greenriver.com>
2020-07-02 13:36:49 -0400

    HUD 2020 Standard Tables behind a new "bi" role

--- db/warehouse/migrate/20200701192839_new_reporting_views.rb
+++ db/warehouse/migrate/20200701192839_new_reporting_views.rb
@@ -1,77 +1,171 @@
 class NewReportingViews < ActiveRecord::Migration[5.2]
   PG_SCHEMA = 'bi'
-  def change
-    GrdaWarehouseBase.connection.execute("CREATE SCHEMA IF NOT EXISTS #{PG_SCHEMA}");
+  PG_ROLE = 'bi'
+  DEMOGRAPHICS_VIEW = "\"#{PG_SCHEMA}_Demographics\""
 
-    non_client_view 'Organization'
-    non_client_view 'Project'
-    non_client_view 'ProjectCoC'
-    non_client_view 'Affiliation'
-    non_client_view 'Export'
-    non_client_view 'Inventory'
-    non_client_view 'Funder'
-
-    client_view
-    #TODO report_clients (holds destination client records)
-    #TODO report_demographics (holds source client records)
-    enrollment_view
-
-    enrollment_info_view 'Service'
-    enrollment_info_view 'Exit'
-    enrollment_info_view 'EnrollmentCoC'
-    enrollment_info_view 'Service'
-    enrollment_info_view 'Disabilities'
-    enrollment_info_view 'HealthAndDv'
-    enrollment_info_view 'IncomeBenefit'
-    enrollment_info_view 'EmploymentEducation'
-    enrollment_info_view 'CurrentLivingSituation'
-    enrollment_info_view 'Event'
-    enrollment_info_view 'Assessment'
-
-
-    #TODO AssessmentQuestions
-    #TODO AssessmentResults
-
-    # TODO service_history_enrollments (view can limit to where GrdaWarehouse::ServiceHistoryEnrollment.open_between(start_date: 5.years.ago, end_date: Date.current))
-    # TODO service_history_services (view should limit to past 5 years)
+  def safe_drop_view(name)
+    sql = "DROP VIEW IF EXISTS #{name}"
+    say_with_time sql do
+      GrdaWarehouseBase.connection.execute sql
+    end
   end
 
-  def klass(name)
-    "GrdaWarehouse::HUD::#{model}".constantize
+
+  def safe_create_role(role: PG_ROLE)
+    sql = <<~SQL
+      DO $$
+      BEGIN
+        CREATE ROLE #{role} WITH NOLOGIN;
+        EXCEPTION WHEN DUPLICATE_OBJECT THEN
+        RAISE NOTICE 'not creating role #{role} -- it already exists';
+      END
+      $$;
+    SQL
+    say_with_time sql do
+      GrdaWarehouseBase.connection.execute sql
+    end
   end
 
-  def create_view(name, sql)
-    puts "#{name}: #{sql}"
-    super
+  def safe_create_view(name, sql_definition:)
+    sql = "CREATE OR REPLACE VIEW #{name} AS #{sql_definition}"
+    say_with_time sql do
+      GrdaWarehouseBase.connection.execute sql
+    end
+    sql = "GRANT SELECT ON #{name} TO #{PG_ROLE}"
+    say_with_time sql do
+      GrdaWarehouseBase.connection.execute sql
+    end
+  end
+
+  def down
+    safe_drop_view view_name(GrdaWarehouse::Hud::Service)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Exit)
+    safe_drop_view view_name(GrdaWarehouse::Hud::EnrollmentCoc)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Disability)
+    safe_drop_view view_name(GrdaWarehouse::Hud::HealthAndDv)
+    safe_drop_view view_name(GrdaWarehouse::Hud::IncomeBenefit)
+    safe_drop_view view_name(GrdaWarehouse::Hud::EmploymentEducation)
+    safe_drop_view view_name(GrdaWarehouse::Hud::CurrentLivingSituation)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Event)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Assessment)
+
+    safe_drop_view view_name(GrdaWarehouse::Hud::Enrollment)
+
+    safe_drop_view view_name(GrdaWarehouse::Hud::Client)
+    safe_drop_view DEMOGRAPHICS_VIEW
+    #GrdaWarehouseBase.connection.execute("DROP SCHEMA IF EXISTS #{PG_SCHEMA} CASCADE");
+
+    safe_drop_view view_name(GrdaWarehouse::Hud::Funder)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Inventory)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Export)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Affiliation)
+    safe_drop_view view_name(GrdaWarehouse::Hud::ProjectCoc)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Project)
+    safe_drop_view view_name(GrdaWarehouse::Hud::Organization)
+  end
+
+  def up
+    # TODO: Permissions
+    #GrdaWarehouseBase.connection.execute("CREATE SCHEMA IF NOT EXISTS #{PG_SCHEMA}");
+    safe_create_role
+
+    non_client_view GrdaWarehouse::Hud::Organization
+    non_client_view GrdaWarehouse::Hud::Project
+    non_client_view GrdaWarehouse::Hud::ProjectCoc
+    non_client_view GrdaWarehouse::Hud::Affiliation
+    non_client_view GrdaWarehouse::Hud::Export
+    non_client_view GrdaWarehouse::Hud::Inventory
+    non_client_view GrdaWarehouse::Hud::Funder
+
+    client_view # destination
+    demographics_view # source
+
+    safe_drop_view view_name(GrdaWarehouse::Hud::Enrollment)
+    enrollments_view
+
+    # #TODO report_clients (holds destination client records)
+    # #TODO report_demographics (holds source client records)
+    # enrollment_view
+
+    enrollment_info_view GrdaWarehouse::Hud::Service
+    enrollment_info_view GrdaWarehouse::Hud::Exit
+    enrollment_info_view GrdaWarehouse::Hud::EnrollmentCoc
+    enrollment_info_view GrdaWarehouse::Hud::Disability
+    enrollment_info_view GrdaWarehouse::Hud::HealthAndDv
+    enrollment_info_view GrdaWarehouse::Hud::IncomeBenefit
+    enrollment_info_view GrdaWarehouse::Hud::EmploymentEducation
+    enrollment_info_view GrdaWarehouse::Hud::CurrentLivingSituation
+    enrollment_info_view GrdaWarehouse::Hud::Event
+    enrollment_info_view GrdaWarehouse::Hud::Assessment
+  end
+
+
+  def client_view
+    model = GrdaWarehouse::Hud::Client
+    hmis_cols = model.hmis_structure(version: '2020').keys.map(&:to_sym)
+    # HUD PII columns
+    # De-identified per
+    # HMIS CSV FORMAT Specifications FY2020 – January 2020
+    # Hash Status=4 SHA256
+    hmis_cols -= %i/FirstName MiddleName LastName NameSuffix SSN/
+    de_identified = [
+      '4 as "HashStatus"',
+      'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("FirstName")))::bytea), \'hex\') as "FirstName"',
+      'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("MiddleName")))::bytea), \'hex\') as "MiddleName"',
+      'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("LastName")))::bytea), \'hex\') as "LastName"',
+      'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("NameSuffix")))::bytea), \'hex\') as "NameSuffix"',
+      'CONCAT(right("SSN",4), encode(sha256("SSN"::bytea), \'hex\')) as "SSN"'
+    ]
+    safe_create_view view_name(model), sql_definition: model.destination.select(:id, *de_identified, *hmis_cols).to_sql
+  end
+
+
+  def demographics_view
+    model = GrdaWarehouse::Hud::Client
+    safe_create_view DEMOGRAPHICS_VIEW, sql_definition: model.source.to_sql
+  end
+
+   def enrollments_view
+    model = GrdaWarehouse::Hud::Enrollment
+    query = join_source_and_client(model.arel_table)
+    query = query.project(
+      destination_client_table[:id].as('client_id'),
+      enrollments_table[:id].as('enrollment_id'),
+      *model.hmis_structure(version: '2020').keys.map{|col| model.arel_table[col]},
+      source_client_table[:id].as('demographic_id')
+    )
+    if model.paranoid?
+      query = query.where( model.arel_table[model.paranoia_column.to_sym].eq nil )
+    end
+    safe_create_view view_name(GrdaWarehouse::Hud::Enrollment), sql_definition: query.to_sql
   end
 
   def non_client_view(model)
-    model = klass(model)
     at = model.arel_table
     query = at
     query = query.project('*') #TODO
     if model.paranoid?
       query = query.where( model.arel_table[model.paranoia_column.to_sym].eq nil )
     end
-    create_view view_name(model.table_name), sql_definition: query.to_sql
+    safe_create_view view_name(model), sql_definition: query.to_sql
   end
 
   def enrollment_info_view(model)
-    query = join_to_enrollments(join_to_enrollments(model))
+    query = join_to_enrollments(join_source_and_client(model.arel_table))
     query = query.project(
       destination_client_table[:id].as('client_id'),
       enrollments_table[:id].as('enrollment_id'),
-      *model.column_names.map{|col| model.arel_table[col]},
+      *model.hmis_structure(version: '2020').keys.map{|col| model.arel_table[col]},
       source_client_table[:id].as('demographic_id')
     )
     if model.paranoid?
       query = query.where( model.arel_table[model.paranoia_column.to_sym].eq nil )
     end
-    create_view view_name(model.table_name), sql_definition: query.to_sql
+    safe_create_view view_name(model), sql_definition: query.to_sql
   end
 
-  def view_name(base)
-    "#{PG_SCHEMA}.Service"
+  def view_name(model)
+    "\"#{PG_SCHEMA}_#{model.table_name}\""
   end
 
   def source_client_table
