3001b889eef4e764084df49700844553d7490b8b
Elliot Anders <elliot@greenriver.org>
2020-07-19 13:36:05 -0400

    Remove duplicate client_source call

--- drivers/service_scanning/app/controllers/service_scanning/scanner_ids_controller.rb
+++ drivers/service_scanning/app/controllers/service_scanning/scanner_ids_controller.rb
@@ -54,7 +54,7 @@ module ServiceScanning
     # should always return a destination client, but some visibility
     # is governed by the source client, some by the destination
     private def client_scope(id: nil)
-      client_source.client_source.destination_client_viewable_by_user(client_id: id, user: current_user)
+      client_source.destination_client_viewable_by_user(client_id: id, user: current_user)
     end
 
     private def client_search_scope
