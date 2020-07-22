f12e6285be0fde9018e655e657cd445669a8d603
Aaron Michal <amichal@greenriver.com>
2020-07-22 16:46:01 -0400

    Style

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     invitations: 'users/invitations',
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
   }
   devise_scope :user do
     match 'active' => 'users/sessions#active', via: :get
