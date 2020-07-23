cb8809e5d888a49f60d0ec60691949bc4b1018d1
Todd Blackman <tdblackman@gmail.com>
2020-07-23 14:25:13 +0000

    updates ECS deployment for PII

--- db/migrate/20200721153237_initialize_secrets_table.rb
+++ db/migrate/20200721153237_initialize_secrets_table.rb
@@ -2,6 +2,8 @@ class InitializeSecretsTable < ActiveRecord::Migration[5.2]
   def up
     if Encryption::Util.encryption_enabled?
       Rake::Task['secrets:init'].execute
+      Rake::Task['secrets:copy_cleartext'].execute
+      Rake::Task['secrets:wipe'].execute
     end
   end
 end
