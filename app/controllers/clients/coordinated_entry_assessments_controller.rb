f44b9ec9933e0911462be4dd40311787f5d673e1
Sandy Wise <sandy@greenriver.org>
2020-08-06 15:08:26 -0400

    Roll back the active CE assessment if we delete the current one

--- app/controllers/clients/coordinated_entry_assessments_controller.rb
+++ app/controllers/clients/coordinated_entry_assessments_controller.rb
@@ -40,6 +40,10 @@ module Clients
 
     def destroy
       @assessment.destroy
+      if @assessment.active?
+        previous_completed = @client.ce_assessments.completed.order(submitted_at: :desc).first
+        previous_completed.update(active: true) if previous_completed.present?
+      end
       respond_with(@assessment, location: client_coordinated_entry_assessments_path(@client))
     end
 
