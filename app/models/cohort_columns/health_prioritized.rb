b78e04036a120b7d7888b61bef93ba6885129c7d
Elliot Anders <elliot@greenriver.org>
2020-08-31 11:31:14 -0400

    Health Priority on cohorts

---
+++ app/models/cohort_columns/health_prioritized.rb
@@ -0,0 +1,25 @@
+###
+# Copyright 2016 - 2020 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module CohortColumns
+  class HealthPrioritized < ReadOnly
+    attribute :column, String, lazy: true, default: :health_prioritized
+    attribute :translation_key, String, lazy: true, default: 'Health Priority'
+    attribute :title, String, lazy: true, default: ->(model, _attr) { _(model.translation_key) }
+
+    def renderer
+      'html'
+    end
+
+    def value(cohort_client) # OK
+      checkmark_or_x text_value(cohort_client)
+    end
+
+    def text_value(cohort_client)
+      cohort_client.client.health_prioritized
+    end
+  end
+end
