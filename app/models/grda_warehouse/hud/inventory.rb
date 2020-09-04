e327a1d84fba0904e4ed46a51d3f777d56b407d4
Elliot Anders <elliot@greenriver.org>
2020-09-04 14:11:08 -0400

    HMIS Cross-walk export

--- app/models/grda_warehouse/hud/inventory.rb
+++ app/models/grda_warehouse/hud/inventory.rb
@@ -37,13 +37,9 @@ module GrdaWarehouse::Hud
     alias_attribute :beds, :BedInventory
 
     scope :within_range, -> (range) do
-      where(
-        i_t[:InventoryEndDate].gteq(range.first).
-        or(i_t[:InventoryEndDate].eq(nil)).
-        and(i_t[:InventoryStartDate].lteq(range.last).
-          or(i_t[:InventoryStartDate].eq(nil))
-        )
-      )
+      i_start = cl(i_t[:inventory_start_date_override], i_t[:InventoryStartDate])
+      i_end = i_t[:InventoryEndDate]
+      where(i_end.gteq(range.first).or(i_end.eq(nil)).and(i_start.lteq(range.last)))
     end
 
     scope :serves_families, -> do
