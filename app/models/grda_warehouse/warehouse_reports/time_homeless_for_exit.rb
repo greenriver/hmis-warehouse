7879a853d83b0f8031c562de80183f891b0b35ce
Elliot Anders <elliot@greenriver.org>
2020-09-09 17:06:09 -0400

    WIP; calculations for report

--- app/models/grda_warehouse/warehouse_reports/time_homeless_for_exit.rb
+++ app/models/grda_warehouse/warehouse_reports/time_homeless_for_exit.rb
@@ -6,6 +6,7 @@
 
 module GrdaWarehouse::WarehouseReports
   class TimeHomelessForExit
+    include ArelHelper
     attr_reader :filter
 
     def initialize(filter)
@@ -13,7 +14,52 @@ module GrdaWarehouse::WarehouseReports
     end
 
     def clients_housed_scope
-      GrdaWarehouse::Hud::Client
+      GrdaWarehouse::Hud::Client.
+        where(id: clients_with_permanent_exits.select(:id)).
+        or(GrdaWarehouse::Hud::Client.where(id: clients_with_housed_enrollments.select(:id)))
+    end
+
+    def client_housed_dates
+      @client_housed_dates ||= begin
+        dates = {}
+        # Find the first date exited to a permanent destination within the range
+        clients_with_permanent_exits.order(ex_t[:ExitDate].asc).pluck(:id, ex_t[:ExitDate]).each do |client_id, date|
+          dates[client_id] ||= date
+        end
+        # Layer on the first enrollment in PH if it is earlier
+        clients_with_housed_enrollments.order(e_t[:MoveInDate].asc).pluck(:id, e_t[:MoveInDate]).each do |client_id, date|
+          dates[client_id] = date if dates[client_id].blank? || date < dates[client_id]
+        end
+        dates
+      end
+    end
+
+    def client_homeless_entry_dates
+      @client_homeless_entry_dates ||= begin
+        dates = {}
+        # Find the latest entry into homelessness for the client occurring before the housed date
+        homeless_entries.order(e_t[:EntryDate].desc).pluck(:id, e_t[:EntryDate]).each do |client_id, date|
+          next if client_housed_dates[client_id] < date
+
+          dates[client_id] ||= date
+        end
+      end
+    end
+
+    def homeless_entries
+      GrdaWarehouse::Hud::Client.destination.joins(:source_enrollments).
+        merge(GrdaWarehouse::Hud::Enrollment.homeless.where(e_t[:EntryDate].lt(filter.end))).
+        where(id: clients_housed_scope.select(:id))
+    end
+
+    private def clients_with_permanent_exits
+      GrdaWarehouse::Hud::Client.destination.joins(:source_enrollments).
+        merge(GrdaWarehouse::Hud::Enrollment.with_permanent_exit(filter.range))
+    end
+
+    private def clients_with_housed_enrollments
+      GrdaWarehouse::Hud::Client.destination.joins(:source_enrollments).
+        merge(GrdaWarehouse::Hud::Enrollment.housed(filter.range))
     end
   end
 end
