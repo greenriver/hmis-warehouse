5149c8af4a6f77cf6afe73a14c29f56141e1e95e
Sandy Wise <sandy@greenriver.org>
2020-09-14 08:51:22 -0400

    Q7b Point-in-Time Count of Persons on the Last Wednesday

--- drivers/hud_apr/app/models/hud_apr/generators/shared/fy2020/question_seven.rb
+++ drivers/hud_apr/app/models/hud_apr/generators/shared/fy2020/question_seven.rb
@@ -267,8 +267,7 @@ module HudApr::Generators::Shared::Fy2020
     end
 
     private def q7b_pit_count
-      a_t = report_client_universe.arel_table
-
+      table_name = 'Q7b'
       metadata = {
         header_row: HEADER_ROW,
         row_labels: [
@@ -282,7 +281,91 @@ module HudApr::Generators::Shared::Fy2020
         first_row: 2,
         last_row: 5,
       }
-      @report.answer(question: 'Q7b').update(metadata: metadata)
+      @report.answer(question: table_name).update(metadata: metadata)
+
+      pit_row(month: 1, table_name: table_name, row: 2)
+      pit_row(month: 4, table_name: table_name, row: 3)
+      pit_row(month: 7, table_name: table_name, row: 4)
+      pit_row(month: 10, table_name: table_name, row: 5)
+    end
+
+    private def pit_row(month:, table_name:, row:)
+      a_t = report_client_universe.arel_table
+      row_universe = pit_universe(month: month)
+
+      # Total
+      answer = @report.answer(question: table_name, cell: 'B' + row.to_s)
+      members = row_universe
+      answer.add_members(members)
+      answer.update(summary: members.count)
+
+      # Without children
+      answer = @report.answer(question: table_name, cell: 'C' + row.to_s)
+      members = row_universe.where(a_t[:household_type].eq(:adults_only))
+      answer.add_members(members)
+      answer.update(summary: members.count)
+
+      #  Adults and children
+      answer = @report.answer(question: table_name, cell: 'D' + row.to_s)
+      members = row_universe.where(a_t[:household_type].eq(:adults_and_children))
+      answer.add_members(members)
+      answer.update(summary: members.count)
+
+      # Without adults
+      answer = @report.answer(question: table_name, cell: 'E' + row.to_s)
+      members = row_universe.where(a_t[:household_type].eq(:children_only))
+      answer.add_members(members)
+      answer.update(summary: members.count)
+
+      # Unknown family type
+      answer = @report.answer(question: table_name, cell: 'F' + row.to_s)
+      members = row_universe.where(a_t[:household_type].eq(:unknown))
+      answer.add_members(members)
+      answer.update(summary: members.count)
+    end
+
+    private def pit_universe(month:)
+      a_t = report_client_universe.arel_table
+
+      pit_date = pit_date(month: month, before: @report.end_date)
+      psh_rrh_universe = universe.members.where(
+        a_t[:first_date_in_program].lteq(pit_date).
+          and(a_t[:last_date_in_program].gt(pit_date).
+            or(a_t[:last_date_in_program].eq(nil))).
+          and(a_t[:move_in_date].lteq(pit_date)).
+          and(a_t[:project_type].in([3, 13])),
+      )
+      so_serv_ce__universe = universe.members.where(
+        a_t[:first_date_in_program].lteq(pit_date).
+          and(a_t[:last_date_in_program].gteq(pit_date).
+            or(a_t[:last_date_in_program].eq(nil))).
+          and(a_t[:project_type].in([4, 6, 14])),
+      )
+      other_universe = universe.members.where(
+        a_t[:first_date_in_program].lteq(pit_date).
+          and(a_t[:last_date_in_program].gt(pit_date).
+            or(a_t[:last_date_in_program].eq(nil))).
+          and(a_t[:project_type].in([2, 3, 8, 9, 10, 13])),
+      )
+
+      psh_rrh_universe.or(so_serv_ce__universe).or(other_universe)
+    end
+
+    private def pit_date(month:, before:)
+      year = before.year if month < before.month
+      year = before.year if month == before.month && before.day >= last_wednesday_of(month: before.month, year: before.year )
+      year = before.year - 1 if month > before.month
+      year = before.year - 1 if month == before.month && before.day < last_wednesday_of(month: before.month, year: before.year )
+
+      last_wednesday_of(month: month, year: year)
+    end
+
+    private def last_wednesday_of(month:, year:)
+      date = Date.new(year, month, -1) # end of the month
+      until date.wednesday? do
+        date = date.prev_day
+      end
+      date
     end
 
     private def universe
