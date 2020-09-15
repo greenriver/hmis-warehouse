b02ada681be8075807e0da2ada1b492bff49cb65
Sandy Wise <sandy@greenriver.org>
2020-09-15 14:00:21 -0400

    Common HUD context for tests

--- drivers/hud_apr/spec/models/fy2020/question_five_spec.rb
+++ drivers/hud_apr/spec/models/fy2020/question_five_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative 'hud_context.rb'
 
 RSpec.describe HudApr::Generators::Shared::Fy2020::QuestionFive, type: :model do
   let!(:super_user_role) { create :can_edit_anything_super_user }
@@ -15,16 +16,14 @@ RSpec.describe HudApr::Generators::Shared::Fy2020::QuestionFive, type: :model do
   end
 
   describe 'Q5' do
+    include_context 'hud context'
+
     before(:all) do
-      GrdaWarehouse::Utility.clear!
       setup('drivers/hud_apr/spec/fixtures/files/fy2020/q5')
     end
 
     after(:all) do
-      # Because we are only running the import once, we have to do our own DB and file cleanup
-      GrdaWarehouse::Utility.clear!
-      cleanup_files
-      Delayed::Job.delete_all
+      cleanup
     end
 
     before(:each) do
@@ -94,39 +93,6 @@ RSpec.describe HudApr::Generators::Shared::Fy2020::QuestionFive, type: :model do
 
     it 'counts heads of household and stayers over 365 days' do
       expect(report_result.answer(question: 'Q5a', cell: 'B16').summary).to eq(2)
-    end
-  end
-
-  def setup(file_path)
-    @delete_later = []
-    @data_source = GrdaWarehouse::DataSource.create(name: 'Green River', short_name: 'GR', source_type: :sftp)
-    GrdaWarehouse::DataSource.create(name: 'Warehouse', short_name: 'W')
-    import(file_path, @data_source)
-    GrdaWarehouse::Tasks::IdentifyDuplicates.new.run!
-    GrdaWarehouse::Tasks::ProjectCleanup.new.run!
-    GrdaWarehouse::Tasks::ServiceHistory::Add.new.run!
-
-    Delayed::Worker.new.work_off(2)
-  end
-
-  def import(file_path, data_source)
-    source_file_path = File.join(file_path, 'source')
-    import_path = File.join(file_path, data_source.id.to_s)
-    # duplicate the fixture file as it gets manipulated
-    FileUtils.cp_r(source_file_path, import_path)
-    @delete_later << import_path unless import_path == source_file_path
-
-    importer = Importers::HmisTwentyTwenty::Base.new(file_path: file_path, data_source_id: data_source.id, remove_files: false)
-    importer.import!
-  end
-
-  def report_result
-    HudReports::ReportInstance.last
-  end
-
-  def cleanup_files
-    @delete_later.each do |path|
-      FileUtils.rm_rf(path)
     end
   end
 end
