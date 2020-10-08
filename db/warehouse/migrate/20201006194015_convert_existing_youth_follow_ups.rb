56a004e03464a12952da38b731b51420cbc04fa3
Elliot Anders <elliot@greenriver.org>
2020-10-08 18:43:32 -0400

    Don't run the youth changes in a migration

--- db/warehouse/migrate/20201006194015_convert_existing_youth_follow_ups.rb
+++ db/warehouse/migrate/20201006194015_convert_existing_youth_follow_ups.rb
@@ -1,43 +1,44 @@
 class ConvertExistingYouthFollowUps < ActiveRecord::Migration[5.2]
   def up
-    return unless GrdaWarehouse::Youth::YouthFollowUp.exists?
+    # This migration fails because it tries to write versions and the transaction gets unhappy
+    # return unless GrdaWarehouse::Youth::YouthFollowUp.exists?
 
-    existing_follow_ups = GrdaWarehouse::Youth::YouthFollowUp.joins(:client).order(contacted_on: :asc).all.to_a
-    GrdaWarehouse::Youth::YouthFollowUp.update_all(deleted_at: Date.current)
+    # existing_follow_ups = GrdaWarehouse::Youth::YouthFollowUp.joins(:client).order(contacted_on: :asc).all.to_a
+    # GrdaWarehouse::Youth::YouthFollowUp.update_all(deleted_at: Date.current)
 
-    # Loop over intakes, creating follow-ups as
-    # would have been appropriate
-    GrdaWarehouse::YouthIntake::Base.find_each do |intake|
-      intake.save!
-    end
+    # # Loop over intakes, creating follow-ups as
+    # # would have been appropriate
+    # GrdaWarehouse::YouthIntake::Base.find_each do |intake|
+    #   intake.save!
+    # end
 
-    existing_follow_ups.each do |follow_up|
-      # Convert to case management notes
-      GrdaWarehouse::Youth::YouthCaseManagement.create(
-        client_id: follow_up.client_id,
-        user_id: follow_up.user_id,
-        created_at: follow_up.created_at,
-        updated_at: follow_up.updated_at,
-        activity: 'From Follow-up',
-        engaged_on: follow_up.contacted_on,
-        housing_status: follow_up.housing_status,
-        zip_code: follow_up.zip_code,
-      )
-    end
+    # existing_follow_ups.each do |follow_up|
+    #   # Convert to case management notes
+    #   GrdaWarehouse::Youth::YouthCaseManagement.create(
+    #     client_id: follow_up.client_id,
+    #     user_id: follow_up.user_id,
+    #     created_at: follow_up.created_at,
+    #     updated_at: follow_up.updated_at,
+    #     activity: 'From Follow-up',
+    #     engaged_on: follow_up.contacted_on,
+    #     housing_status: follow_up.housing_status,
+    #     zip_code: follow_up.zip_code,
+    #   )
+    # end
 
-    # Loop over follow-ups and see if there is a case management note on or after the
-    # required_on date
-    # update follow-up with case management note data
-    GrdaWarehouse::Youth::YouthFollowUp.preload(:case_managements).find_each do |follow_up|
-      next_note = follow_up.case_managements.select do |note|
-        note.required_on <= note.engaged_on
-      end.min_by(&:engaged_on)
-      next unless next_note
+    # # Loop over follow-ups and see if there is a case management note on or after the
+    # # required_on date
+    # # update follow-up with case management note data
+    # GrdaWarehouse::Youth::YouthFollowUp.preload(:case_managements).find_each do |follow_up|
+    #   next_note = follow_up.case_managements&.select do |note|
+    #     note.required_on <= note.engaged_on
+    #   end&.min_by(&:engaged_on)
+    #   next unless next_note
 
-      follow_up.update(
-        housing_status: next_note.class.generic_housing_status(next_note.housing_status),
-        contacted_on: next_note.engaged_on,
-      )
-    end
+    #   follow_up.update(
+    #     housing_status: next_note.class.generic_housing_status(next_note.housing_status),
+    #     contacted_on: next_note.engaged_on,
+    #   )
+    # end
   end
 end
