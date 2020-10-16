243dfac392213a2d09cf4132f9d63ad94d5d591b
Sandy Wise <sandy@greenriver.org>
2020-10-16 15:51:32 -0400

    dig() instead of [] for nils

--- config/initializers/simple_form_submit_override.rb
+++ config/initializers/simple_form_submit_override.rb
@@ -4,10 +4,10 @@ module DisableDoubleClickOnSimpleForms
   def submit(field, options = {})
     if field.is_a?(Hash)
       field[:data] ||= {}
-      field[:data][:disable_with] ||= field[:value] || 'Please wait ...' unless options[:data][:disable_with] == false
+      field[:data][:disable_with] ||= field[:value] || 'Please wait ...' unless options.dig(:data, :disable_with) == false
     else
       options[:data] ||= {}
-      options[:data][:disable_with] ||= options[:value] || 'Please wait ...' unless options[:data][:disable_with] == false
+      options[:data][:disable_with] ||= options[:value] || 'Please wait ...' unless options.dig(:data, :disable_with) == false
     end
     super(field, options)
   end
