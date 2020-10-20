38df81cb2a96cd5538e5db845d7435674a6ad418
Elliot Anders <elliot@greenriver.org>
2020-10-20 14:38:09 -0400

    Adjust migration so it will run

--- db/warehouse/migrate/20201020181913_fix_household_i_ds.rb
+++ db/warehouse/migrate/20201020181913_fix_household_i_ds.rb
@@ -1,10 +1,8 @@
 class FixHouseholdIDs < ActiveRecord::Migration[5.2]
   def up
-    include ArelHelper
     incorrect_household_ids = GrdaWarehouse::Hud::Enrollment.
       where(original_household_id: nil).
-      left_outer_joins(:exit).
-      pluck(e_t[:data_source_id], e_t[:ProjectID], e_t[:HouseholdID], e_t[:EnrollmentID]).
+      pluck(:data_source_id, :ProjectID, :HouseholdID, :EnrollmentID).
       map do |ds_id, p_id, h_id, e_id|
         Digest::MD5.hexdigest("e_#{ds_id}_#{p_id}_#{nil}_#{e_id}")
       end
@@ -15,6 +13,6 @@ class FixHouseholdIDs < ActiveRecord::Migration[5.2]
       HouseholdID: nil,
       processed_as: nil,
     )
-    GrdaWarehouse::Tasks::ServiceHistory::Enrollment.queue_batch_process_unprocessed!
+    # GrdaWarehouse::Tasks::ServiceHistory::Enrollment.queue_batch_process_unprocessed!
   end
 end
