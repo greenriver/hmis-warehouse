8af0ebe0e6fbf8efae3a24745999b2ee1e762904
Elliot Anders <elliot@greenriver.org>
2020-10-27 16:13:24 -0400

    Invalidate source hashes for any previously deleted items

---
+++ db/warehouse/migrate/20201027200503_invalidate_deleted_source_hashes.rb
@@ -0,0 +1,8 @@
+class InvalidateDeletedSourceHashes < ActiveRecord::Migration[5.2]
+  def up
+    HmisCsvTwentyTwenty::Importer::Importer.importable_files_map.each do |_, name|
+      klass = "GrdaWarehouse::Hud::#{name}".constantize
+      klass.only_deleted.update_all(source_hash: nil) if klass.paranoid?
+    end
+  end
+end
