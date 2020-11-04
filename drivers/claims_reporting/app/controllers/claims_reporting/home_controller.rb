991dbb9fdefa8a21e3971f9fd9dcc234b7969802
Aaron Michal <amichal@greenriver.com>
2020-11-04 16:22:51 -0500

    better date ranges

--- drivers/claims_reporting/app/controllers/claims_reporting/home_controller.rb
+++
@@ -1,9 +0,0 @@
-module ClaimsReporting
-  class HomeController < ApplicationController
-    before_action :require_can_administer_health!
-
-    def index
-      @patient = Health::Patient.last
-    end
-  end
-end
--- drivers/claims_reporting/app/controllers/claims_reporting/warehouse_reports/reconciliation_controller.rb
+++ drivers/claims_reporting/app/controllers/claims_reporting/warehouse_reports/reconciliation_controller.rb
@@ -6,7 +6,7 @@
 module ClaimsReporting::WarehouseReports
   class ReconciliationController < ApplicationController
     include WarehouseReportAuthorization
-    before_action :require_can_view_member_health_reports!
+    # before_action :require_can_view_member_health_reports!
 
     # include AjaxModalRails::Controller
     include ArelHelper
@@ -14,25 +14,45 @@ module ClaimsReporting::WarehouseReports
 
     # before_action :set_report
     def index
-      @upload = ClaimsReporting::CpPaymentUpload.new
+      dates = if ::Health::Claim.none?
+        ::Health::QualifyingActivity.distinct.pluck(:date_of_activity)
+      else
+        ::Health::Claim.distinct.pluck(:max_date)
+      end
+
+      @available_months = dates.map(&:beginning_of_month).sort.uniq.reverse
+
+      month = begin
+                Date.parse(filter_params[:month]).beginning_of_month
+              rescue StandardError
+                nil
+              end
+      @active_month = @available_months.detect { |m| m == month } || @available_months.first
     end
 
     def create
       if params[:file]
         upload = params.dig(:file, :content)
-        @upload = ClaimsReporting::CpPaymentUpload.new(
+        record = ClaimsReporting::CpPaymentUpload.new(
           user_id: current_user.id,
           content: upload.read,
           original_filename: upload.original_filename,
         )
         if @upload.save
-          @upload.process!
+          record.process!
           flash[:notice] = 'Upload accepted and processed successfully'
         else
-          flash[:notice] = @upload.errors.full_messages.to_sentence
+          flash[:notice] = record.errors.full_messages.to_sentence
         end
       end
       redirect_to request.referrer || url_for(action: :index)
+    end
+
+    private def filter_params
+      params.fetch(:filter, {}).permit(
+        :month,
+        acos: [],
+      )
     end
   end
 end
