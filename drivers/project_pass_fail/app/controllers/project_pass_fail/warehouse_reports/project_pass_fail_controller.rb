902646c2e2e9917753ae4647255ce1e5961d364f
Elliot Anders <elliot@greenriver.org>
2020-11-09 15:06:41 -0500

    Driver for project pass fail report

---
+++ drivers/project_pass_fail/app/controllers/project_pass_fail/warehouse_reports/project_pass_fail_controller.rb
@@ -0,0 +1,10 @@
+# (pick universe from any combination of Project Type, CoC Code, Data Source, Organization, Project, Project Group)
+# Date Range
+# Show:
+#   ES/SO/SH/TH (whatever the top level of the universe is)
+#     Overall Utilization Rate
+#       Show each project
+#     UDE total failed project count
+#       Show each Project
+#     Timeliness Average timeliness
+#       Show each Project
---
+++ drivers/project_pass_fail/config/routes.rb
@@ -0,0 +1,12 @@
+BostonHmis::Application.routes.draw do
+  namespace :project_pass_fail do
+    namespace :warehouse_reports do
+      resources :project_pass_fail, only: [:index] do
+        get :details, on: :collection
+        get 'section/:partial', on: :collection, to: 'project_pass_fail#section', as: :section
+        get :filters, on: :collection
+        get :download, on: :collection
+      end
+    end
+  end
+end
