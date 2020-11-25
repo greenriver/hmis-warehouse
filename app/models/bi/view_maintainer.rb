e30151e7926cc49b43c62ae3be91c311e1853b51
Elliot Anders <elliot@greenriver.org>
2020-11-25 09:12:19 -0500

    Rubocop fixups

--- app/models/bi/view_maintainer.rb
+++ app/models/bi/view_maintainer.rb
@@ -8,11 +8,11 @@ module Bi
   class ViewMaintainer
     include ArelHelper
 
-    HUD_CSV_VERSION = '2020'
-    NAMESPACE = 'bi'
-    PG_ROLE = 'bi'
-    SH_INTERVAL = '\'5 years\'::interval'
-    DEMOGRAPHICS_VIEW = "\"#{NAMESPACE}_Demographics\""
+    HUD_CSV_VERSION = '2020'.freeze
+    NAMESPACE = 'bi'.freeze
+    PG_ROLE = 'bi'.freeze
+    SH_INTERVAL = '\'5 years\'::interval'.freeze
+    DEMOGRAPHICS_VIEW = "\"#{NAMESPACE}_Demographics\"".freeze
 
     def remove_views
       safe_drop_view(view_name(GrdaWarehouse::Hud::Service))
@@ -80,27 +80,27 @@ module Bi
 
       safe_create_view(
         view_name(GrdaWarehouse::Hud::Client),
-        sql_definition: GrdaWarehouse::Hud::Client.destination.select(de_identified_client_cols).to_sql
+        sql_definition: GrdaWarehouse::Hud::Client.destination.select(de_identified_client_cols).to_sql,
       )
 
       safe_create_view(
         DEMOGRAPHICS_VIEW,
         sql_definition: GrdaWarehouse::Hud::Client.source.
           joins(:warehouse_client_source).
-          select(de_identified_demographic_cols, :data_source_id).to_sql
+          select(de_identified_demographic_cols, :data_source_id).to_sql,
       )
 
       non_client_view(GrdaWarehouse::Hud::Enrollment)
 
       client_history_view(
         GrdaWarehouse::ServiceHistoryService.where(
-          "date >= (CURRENT_DATE - #{SH_INTERVAL})"
-        )
+          "date >= (CURRENT_DATE - #{SH_INTERVAL})",
+        ),
       )
       client_history_view(
         GrdaWarehouse::ServiceHistoryEnrollment.entry.where(
-          "last_date_in_program IS NULL OR last_date_in_program >= (CURRENT_DATE - #{SH_INTERVAL})"
-        )
+          "last_date_in_program IS NULL OR last_date_in_program >= (CURRENT_DATE - #{SH_INTERVAL})",
+        ),
       )
       generic_view(GrdaWarehouse::DataSource, GrdaWarehouse::DataSource.view_column_names)
       generic_view(GrdaWarehouse::Lookups::Ethnicity)
@@ -120,7 +120,7 @@ module Bi
       # De-identified per HMIS CSV FORMAT Specifications FY2020 – January 2020
       # https://hudhdx.info/Resources/Vendors/HMIS%20CSV%20Specifications%20FY2020%20v1.8.pdf
       # ~Page 7 HashStatus of ‘SHA-256’ (4)
-      hmis_cols -= %i/PersonalID FirstName MiddleName LastName NameSuffix NameDataQuality SSN SSNDataQuality/
+      hmis_cols -= [:PersonalID, :FirstName, :MiddleName, :LastName, :NameSuffix, :NameDataQuality, :SSN, :SSNDataQuality]
       de_identified = [
         '4 as "HashStatus"',
         'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("FirstName")))::bytea), \'hex\') as "FirstName"',
@@ -128,7 +128,7 @@ module Bi
         'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("LastName")))::bytea), \'hex\') as "LastName"',
         'ENCODE(SHA256(SOUNDEX(UPPER(TRIM("NameSuffix")))::bytea), \'hex\') as "NameSuffix"',
         'NameDataQuality',
-        #'LPAD(RIGHT("SSN",4),9,\'x\') as "MaskedSSN"',
+        # 'LPAD(RIGHT("SSN",4),9,\'x\') as "MaskedSSN"',
         'CONCAT(RIGHT("SSN",4), ENCODE(SHA256(LPAD("SSN",9,\'x\')::bytea), \'hex\')) as "SSN"',
         'SSNDataQuality',
       ]
@@ -137,7 +137,7 @@ module Bi
 
     def de_identified_demographic_cols
       de_identified_client_cols + [
-        wc_t[:destination_id].as('client_id').to_sql
+        wc_t[:destination_id].as('client_id').to_sql,
       ]
     end
 
@@ -184,30 +184,26 @@ module Bi
       cols
     end
 
-    def generic_view(model, cols=model.column_names)
+    def generic_view(model, cols = model.column_names)
       scope = model
-      if model.paranoid?
-        scope = scope.where(model.paranoia_column.to_sym => nil)
-      end
+      scope = scope.where(model.paranoia_column.to_sym => nil) if model.paranoid?
       scope = scope.select(*cols)
       safe_create_view view_name(model.arel_table), sql_definition: scope.to_sql
     end
 
     def client_history_view(model)
       scope = model.joins(:client)
-      if model.paranoid?
-        scope = scope.where(model.paranoia_column.to_sym => nil)
-      end
+      scope = scope.where(model.paranoia_column.to_sym => nil) if model.paranoid?
       cols = model.view_column_names
 
       if cols.include?('project_id')
         scope = scope.joins(:project)
-        cols.reject!{ |c| c == 'project_id' }
+        cols.reject! { |c| c == 'project_id' }
         cols << p_t[:id].as('project_id')
       end
       if cols.include?('enrollment_group_id')
         scope = scope.joins(:enrollment)
-        cols.reject!{ |c| c == 'enrollment_group_id' }
+        cols.reject! { |c| c == 'enrollment_group_id' }
         cols << e_t[:id].as('enrollment_id')
       end
       scope = scope.select(*cols)
@@ -265,7 +261,7 @@ module Bi
       cols << source_client_table[:id].as('demographic_id') if model.column_names.include?('PersonalID')
 
       query = query.project(cols)
-      query = query.where( model.arel_table[model.paranoia_column.to_sym].eq nil ) if model.paranoid?
+      query = query.where(model.arel_table[model.paranoia_column.to_sym].eq(nil)) if model.paranoid?
 
       safe_create_view view_name(model), sql_definition: query.to_sql
     end
@@ -276,14 +272,14 @@ module Bi
 
     def source_client_table
       @source_client_table ||= Arel::Table.new(
-        GrdaWarehouse::Hud::Client.table_name
-      ).tap{ |t| t.table_alias = 'source_clients' }
+        GrdaWarehouse::Hud::Client.table_name,
+      ).tap { |t| t.table_alias = 'source_clients' }
     end
 
     def destination_client_table
       @destination_client_table ||= Arel::Table.new(
-        GrdaWarehouse::Hud::Client.table_name
-      ).tap{ |t| t.table_alias = 'destination_clients' }
+        GrdaWarehouse::Hud::Client.table_name,
+      ).tap { |t| t.table_alias = 'destination_clients' }
     end
 
     def exit_in_range
@@ -299,8 +295,8 @@ module Bi
       model = GrdaWarehouse::Hud::Enrollment.arel_table
       table.join(model).on(
         at[:data_source_id].eq(model[:data_source_id]).
-        and( at[:EnrollmentID].eq model[:EnrollmentID] ).
-        and( model[:DateDeleted].eq nil )
+        and(at[:EnrollmentID].eq(model[:EnrollmentID])).
+        and(model[:DateDeleted].eq(nil)),
       )
     end
 
@@ -313,8 +309,8 @@ module Bi
       model = GrdaWarehouse::Hud::Exit.arel_table
       table.join(model).on(
         at[:data_source_id].eq(model[:data_source_id]).
-        and( at[:EnrollmentID].eq model[:EnrollmentID] ).
-        and( model[:DateDeleted].eq nil )
+        and(at[:EnrollmentID].eq(model[:EnrollmentID])).
+        and(model[:DateDeleted].eq(nil)),
       )
     end
 
@@ -328,12 +324,12 @@ module Bi
       exit_model = GrdaWarehouse::Hud::Exit.arel_table
       table.join(en_model).on(
         at[:data_source_id].eq(en_model[:data_source_id]).
-        and( at[:EnrollmentID].eq en_model[:EnrollmentID] ).
-        and( en_model[:DateDeleted].eq nil )
+        and(at[:EnrollmentID].eq(en_model[:EnrollmentID])).
+        and(en_model[:DateDeleted].eq(nil)),
       ).join(exit_model, Arel::Nodes::OuterJoin).on(
         en_model[:data_source_id].eq(exit_model[:data_source_id]).
-        and( en_model[:EnrollmentID].eq exit_model[:EnrollmentID] ).
-        and( exit_model[:DateDeleted].eq nil )
+        and(en_model[:EnrollmentID].eq(exit_model[:EnrollmentID])).
+        and(exit_model[:DateDeleted].eq(nil)),
       )
     end
 
@@ -345,13 +341,13 @@ module Bi
       end
       table.join(source_client_table).on(
         at[:data_source_id].eq(source_client_table[:data_source_id]).
-        and( at[:PersonalID].eq source_client_table[:PersonalID] ).
-        and( source_client_table[:DateDeleted].eq nil )
+        and(at[:PersonalID].eq(source_client_table[:PersonalID])).
+        and(source_client_table[:DateDeleted].eq(nil)),
       ).join(wc_t).on(
-        source_client_table[:id].eq wc_t[:source_id]
+        source_client_table[:id].eq(wc_t[:source_id]),
       ).join(destination_client_table).on(
         destination_client_table[:id].eq(wc_t[:destination_id]).
-        and( destination_client_table[:DateDeleted].eq nil )
+        and(destination_client_table[:DateDeleted].eq(nil)),
       )
     end
 
@@ -364,8 +360,8 @@ module Bi
       model = GrdaWarehouse::Hud::Project.arel_table
       table.join(model).on(
         at[:data_source_id].eq(model[:data_source_id]).
-        and( at[:ProjectID].eq model[:ProjectID] ).
-        and( model[:DateDeleted].eq nil )
+        and(at[:ProjectID].eq(model[:ProjectID])).
+        and(model[:DateDeleted].eq(nil)),
       )
     end
 
@@ -378,8 +374,8 @@ module Bi
       model = GrdaWarehouse::Hud::Organization.arel_table
       table.join(model).on(
         at[:data_source_id].eq(model[:data_source_id]).
-        and( at[:OrganizationID].eq model[:OrganizationID] ).
-        and( model[:DateDeleted].eq nil )
+        and(at[:OrganizationID].eq(model[:OrganizationID])).
+        and(model[:DateDeleted].eq(nil)),
       )
     end
 
@@ -392,14 +388,14 @@ module Bi
       model = GrdaWarehouse::Hud::Assessment.arel_table
       table.join(model).on(
         at[:data_source_id].eq(model[:data_source_id]).
-        and( at[:AssessmentID].eq model[:AssessmentID] ).
-        and( model[:DateDeleted].eq nil )
+        and(at[:AssessmentID].eq(model[:AssessmentID])).
+        and(model[:DateDeleted].eq(nil)),
       )
     end
 
     def safe_drop_view(name)
       sql = "DROP VIEW IF EXISTS #{name}"
-      GrdaWarehouseBase.connection.execute sql
+      GrdaWarehouseBase.connection.execute(sql)
     end
 
     def safe_create_role(role: PG_ROLE)
