eef1a3d3d033785057c2351d4aecf0f33f88665f
Elliot Anders <elliot@greenriver.org>
2020-11-25 08:37:16 -0500

    Log every time we check to see if RDS is up

---
+++ app/models/grda_warehouse/lsa_rds_state_log.rb
@@ -0,0 +1,13 @@
+###
+# Copyright 2016 - 2020 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# NOTE: Sometimes we need to fetch data to import from odd FTP servers that
+# Ruby can't deal with directly, this will fetch and push to S3 for normal processing
+module GrdaWarehouse
+  class LsaRdsStateLog < GrdaWarehouseBase
+
+  end
+end
