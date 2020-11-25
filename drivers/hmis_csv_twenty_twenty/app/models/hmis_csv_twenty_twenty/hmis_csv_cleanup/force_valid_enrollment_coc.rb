b7a34766284bc2df1552f06d5f87218f6f8a071e
Elliot Anders <elliot@greenriver.org>
2020-11-25 14:44:10 -0500

    WIP; cleanup for CoC Codes

---
+++ drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_twenty_twenty/hmis_csv_cleanup/force_valid_enrollment_coc.rb
@@ -0,0 +1,56 @@
+###
+# Copyright 2016 - 2020 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/master/LICENSE.md
+###
+
+module HmisCsvTwentyTwenty::HmisCsvCleanup
+  class ForceValidEnrollmentCoc < Base
+    def cleanup!
+      enrollment_coc_batch = []
+
+      enrollment_coc_scope.find_each do |e_coc|
+        # ignore any valid CoC-codes
+        next if ::HUD.valid_coc?(e_coc.CoCCode)
+
+        # upcase any that match the format but aren't correctly cased
+        if e_coc.CoCCode.match?(/^[a-z]{2}-[0-9]{3}$/i)
+          e_coc.CoCCode.upcase!
+        # add a dash if we have two characters and 3 numbers
+        elsif e_coc.CoCCode.match?(/^[a-z]{2}[0-9]{3}$/i)
+          e_coc.CoCCode = "#{e_coc.CoCCode[0..1]}-#{e_coc.CoCCode[2..4]}"
+        else
+          # Set all other invalid to blank
+          e_coc.CoCCode = nil
+        end
+        # double check the resulting code is valid, blank it if not
+        c_coc.CocCode = nil if e_coc.CoCCode.present? && ! ::HUD.valid_coc?(e_coc.CoCCode)
+
+        e_coc.set_source_hash
+        enrollment_coc_batch << e_coc
+      end
+
+      enrollment_coc_source.import(
+        enrollment_coc_batch,
+        on_duplicate_key_update: {
+          conflict_target: [:id],
+          columns: [:CoCCode, :source_hash],
+        },
+      )
+    end
+
+    def enrollment_coc_scope
+      enrollment_coc_source.
+        where(importer_log_id: @importer_log.id).
+        where.not(CoCCode: nil)
+    end
+
+    def enrollment_coc_source
+      HmisCsvTwentyTwenty::Importer::EnrollmentCoc
+    end
+
+    def self.description
+      'Fix Enrollment.CoCCode where possible, remove where not.'
+    end
+  end
+end
