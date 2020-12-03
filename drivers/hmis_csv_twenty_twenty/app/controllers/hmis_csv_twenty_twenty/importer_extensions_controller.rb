5544a81079d8e6a7e7b7b3b8a3154feef94cdfc9
Sandy Wise <sandy@greenriver.org>
2020-12-03 16:37:16 -0500

    Enable/disable cleanups via a UI

--- drivers/hmis_csv_twenty_twenty/app/controllers/hmis_csv_twenty_twenty/importer_extensions_controller.rb
+++ drivers/hmis_csv_twenty_twenty/app/controllers/hmis_csv_twenty_twenty/importer_extensions_controller.rb
@@ -6,8 +6,40 @@
 
 class HmisCsvTwentyTwenty::ImporterExtensionsController < ApplicationController
   before_action :require_can_view_imports!
+  before_action :set_data_source
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    config = {
+      import_cleanups: {},
+    }
+    allowed_extensions.each do |extension|
+      next unless params[:extensions][extension.to_s] == '1'
+
+      config.deep_merge!(extension.enable) do |_, v1, v2|
+        v1 + v2
+      end
+    end
+
+    @data_source.update(config)
+    redirect_to action: :show
+  end
+
+  def allowed_extensions
+    @allowed_extensions = [
+      HmisCsvTwentyTwenty::HmisCsvCleanup::ForceValidEnrollmentCoc,
+      HmisCsvTwentyTwenty::HmisCsvCleanup::MoveInOutsideEnrollment,
+      HmisCsvTwentyTwenty::HmisCsvCleanup::PrependProjectId,
+    ].freeze
+  end
+  helper_method :allowed_extensions
+
+  def set_data_source
     @data_source = GrdaWarehouse::DataSource.find(params[:id].to_i)
   end
 end
--- drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_twenty_twenty/hmis_csv_cleanup/base.rb
+++ drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_twenty_twenty/hmis_csv_cleanup/base.rb
@@ -18,5 +18,11 @@ module HmisCsvTwentyTwenty::HmisCsvCleanup
     def self.description
       name.split('::').last.underscore.humanize
     end
+
+    def self.checked?(data_source)
+      # FIXME: This assumes an enable contains a single element
+      enabled_key = enable[:import_cleanups].keys.first
+      data_source[:import_cleanups][enabled_key.to_s]&.include?(enable[:import_cleanups][enabled_key].first) || false
+    end
   end
 end
