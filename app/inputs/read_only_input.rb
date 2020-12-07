93c5063af290dd1a643462bb17c82b69a584fdca
Elliot Anders <elliot@greenriver.org>
2020-12-07 15:55:14 -0500

    Check form wrapper AND input type when determining how to display a read-only input

--- app/inputs/read_only_input.rb
+++ app/inputs/read_only_input.rb
@@ -5,19 +5,23 @@
 ###
 
 class ReadOnlyInput < SimpleForm::Inputs::StringInput
-  def input(_wrapper_options = nil)
-    display_value = object.send(attribute_name)
+  def input(wrapper_options = nil)
+    if @builder.options[:wrapper] == :readonly || input_options.dig(:as) == :read_only
+      display_value = object.send(attribute_name)
 
-    if has_hint?
-      template.content_tag(:p, display_value, label_html_options)
-    else
-      existing_classes = label_html_options.try(:[], :class)
-      existing_classes << 'd-block'
-      if display_value.present?
-        template.content_tag(:p, display_value, label_html_options.merge(class: existing_classes))
+      if has_hint?
+        template.content_tag(:p, display_value, label_html_options)
       else
-        template.content_tag(:em, 'Blank', label_html_options.merge(class: existing_classes))
+        existing_classes = label_html_options.try(:[], :class)
+        existing_classes << 'd-block'
+        if display_value.present?
+          template.content_tag(:p, display_value, label_html_options.merge(class: existing_classes))
+        else
+          template.content_tag(:em, 'Blank', label_html_options.merge(class: existing_classes))
+        end
       end
+    else
+      super(wrapper_options)
     end
   end
 end
