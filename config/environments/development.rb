0c9f56ac2fd5c5247e10c053b574fd4929812bac
Aaron Michal <amichal@greenriver.com>
2020-12-10 18:23:15 -0500

    Doesnt work well in Docker on macOS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Web console from outside of docker
   config.web_console.whitelisted_ips = ['172.16.0.0/12', '192.168.0.0/16']
