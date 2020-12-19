6c677b53d4487978ec4e93c1c9bb220755bdab43
Elliot Anders <elliot@greenriver.org>
2020-12-19 09:46:02 -0500

    Hide health reports even if assigned if healthcare is disabled

--- app/controllers/warehouse_reports_controller.rb
+++ app/controllers/warehouse_reports_controller.rb
@@ -9,8 +9,9 @@ class WarehouseReportsController < ApplicationController
   # This page just lists the available reports, each report is responsible for access
   skip_before_action :report_visible?
   def index
-    report_definitions = current_user.reports.group_by(&:report_group)
-
+    report_definitions = current_user.reports
+    report_definitions = report_definitions.select { |r| r.health == false } unless GrdaWarehouse::Config.get(:healthcare_available)
+    report_definitions = report_definitions.group_by(&:report_group)
     @report_definitions = report_definitions.to_a.sort_by { |group, _| group }
   end
 end
