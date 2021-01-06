2b6bbc145c09d7676c4b9df8c4f9f01c6a21cd92
Elliot Anders <elliot@greenriver.org>
2021-01-06 16:34:38 -0500

    Expose pausing of import on failures; split out validation errors from flags

--- drivers/hmis_csv_twenty_twenty/config/routes.rb
+++ drivers/hmis_csv_twenty_twenty/config/routes.rb
@@ -2,6 +2,7 @@ BostonHmis::Application.routes.draw do
   namespace :hmis_csv_twenty_twenty do
     resources :loader_errors, only: [:show]
     resources :importer_validations, only: [:show]
+    resources :importer_validation_errors, only: [:show]
     resources :importer_errors, only: [:show]
     resources :importer_extensions, only: [:edit, :update]
     resources :loaded, only: [:show]
