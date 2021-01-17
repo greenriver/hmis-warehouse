0408b015010e8660f327e07dbd89242158440b95
Elliot Anders <elliot@greenriver.org>
2021-01-17 14:47:00 -0500

    Include preferred language for vaccine only version; adjustments for sending text messages

--- drivers/text_message/app/models/text_message/message.rb
+++ drivers/text_message/app/models/text_message/message.rb
@@ -21,14 +21,20 @@ module TextMessage
     end
 
     def send!
+      # NOTE: setup of long codes is done in Amazon Pinpoint
+      # Long codes have a 1/second send limit
+      # Responses: https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-two-way.html
       return if sent_at.present?
 
       phone_number = topic_subscriber.phone_number
-      return unless phone_number.present?
+      return unless phone_number.present? && phone_number.length == 10
 
+      phone_number = "1#{phone_number}"
       update(sent_at: Time.current, sent_to: phone_number)
       source&.mark_sent
 
+      # Add a delay for compliance with long-code send restrictions
+      sleep(1.1)
       sns = Aws::SNS::Client.new
       sns.publish(phone_number: phone_number, message: content)
     end
