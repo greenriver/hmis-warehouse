6158746d9038896d898a032e48a034be0d6353bb
HxHexa <tquang1708@gmail.com>
2021-01-21 18:03:35 -0500

    set up alternate migration paths for databases

--- app/models/grda_warehouse_base.rb
+++ app/models/grda_warehouse_base.rb
@@ -4,43 +4,46 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-class GrdaWarehouseBase < ActiveRecord::Base
-  include ArelHelper
-  include  Efind
-  establish_connection DB_WAREHOUSE
+class GrdaWarehouseBase < ApplicationRecord
   self.abstract_class = true
 
-  def self.sql_server?
-    connection.adapter_name == 'SQLServer'
-  end
+  connects_to database: { writing: :warehouse, reading: :warehouse }
+  # include ArelHelper
+  # include  Efind
+  # establish_connection DB_WAREHOUSE
+  # self.abstract_class = true
 
-  def self.postgres?
-    connection.adapter_name.in?(['PostgreSQL', 'PostGIS'])
-  end
+  # def self.sql_server?
+  #   connection.adapter_name == 'SQLServer'
+  # end
 
-  def self.reset_connection
-    self.connection.disconnect!
-    self.establish_connection DB_WAREHOUSE
-  end
+  # def self.postgres?
+  #   connection.adapter_name.in?(['PostgreSQL', 'PostGIS'])
+  # end
 
-  def self.setup_config
-    new_config = {
-      'db' => ['db/warehouse'],
-      'db/migrate' => ['db/warehouse/migrate'],
-      'db/seeds' => ['db/warehouse/seeds'],
-      'config/database' => ['config/database_warehouse.yml'],
-    }
-    ENV['SCHEMA'] = 'db/warehouse/schema.rb'
-    # set config variables for custom database
-    new_config.each do |path, value|
-      Rails.application.config.paths[path] = value
-    end
-    db_config = Rails.application.config.paths['config/database'].to_a.first
-    ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read(db_config)).result)[Rails.env]
-  end
+  # def self.reset_connection
+  #   self.connection.disconnect!
+  #   self.establish_connection DB_WAREHOUSE
+  # end
 
-  def self.needs_migration?
-    # integers from file list
-    (ActiveRecord::MigrationContext.new('db/warehouse/migrate').migrations.collect(&:version) - GrdaWarehouse::SchemaMigration.pluck(:version).map(&:to_i)).any?
-  end
+  # def self.setup_config
+  #   new_config = {
+  #     'db' => ['db/warehouse'],
+  #     'db/migrate' => ['db/warehouse/migrate'],
+  #     'db/seeds' => ['db/warehouse/seeds'],
+  #     'config/database' => ['config/database_warehouse.yml'],
+  #   }
+  #   ENV['SCHEMA'] = 'db/warehouse/schema.rb'
+  #   # set config variables for custom database
+  #   new_config.each do |path, value|
+  #     Rails.application.config.paths[path] = value
+  #   end
+  #   db_config = Rails.application.config.paths['config/database'].to_a.first
+  #   ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read(db_config)).result)[Rails.env]
+  # end
+
+  # def self.needs_migration?
+  #   # integers from file list
+  #   (ActiveRecord::MigrationContext.new('db/warehouse/migrate').migrations.collect(&:version) - GrdaWarehouse::SchemaMigration.pluck(:version).map(&:to_i)).any?
+  # end
 end
--- app/models/health_base.rb
+++ app/models/health_base.rb
@@ -5,61 +5,64 @@
 ###
 
 class HealthBase < ActiveRecord::Base
-  establish_connection DB_HEALTH
   self.abstract_class = true
-  has_paper_trail versions: {class_name: 'Health::HealthVersion'}
 
-  include ArelHelper
-  class << self
-    cattr_accessor :phi_dictionary
+  connects_to database: { writing: :health, reading: :health }
+  # establish_connection DB_HEALTH
+  # self.abstract_class = true
+  # has_paper_trail versions: {class_name: 'Health::HealthVersion'}
 
-    def phi_dictionary_entry
-      self.phi_dictionary ||= {}
-      self.phi_dictionary[self.name] ||= {
-        patient_id: nil,
-        attrbutes: Set.new
-      }
-    end
+  # include ArelHelper
+  # class << self
+  #   cattr_accessor :phi_dictionary
 
-    def phi_attr(attribute, category= nil, description= nil)
-      raise ArgumentError, "category (#{category}) must be a ::Phi::Category" unless category < ::Phi::Category
-      raise ArgumentError, "attr (#{attr})  must method name as a symbol" unless attribute.is_a?(::Symbol)
-      self.phi_dictionary_entry[:attrbutes] << ::Phi::Attribute.new(
-        self.name,
-        attribute,
-        category,
-        description
-      )
-    end
+  #   def phi_dictionary_entry
+  #     self.phi_dictionary ||= {}
+  #     self.phi_dictionary[self.name] ||= {
+  #       patient_id: nil,
+  #       attrbutes: Set.new
+  #     }
+  #   end
 
-    def phi_patient(attribute)
-      raise ArgumentError, "attr (#{attr})  must method name as a symbol" unless attribute.is_a?(::Symbol)
-      if (existing = phi_dictionary_entry[:patient_id]) && existing != attribute
-        raise ArgumentError, "Cannot set more then one phi_patient per class: class:#{self} existing: #{existing}, new: #{attribute}"
-      end
-      self.phi_dictionary_entry[:table_name] = table_name
-      self.phi_dictionary_entry[:patient_id] = attribute
-    end
-  end
+  #   def phi_attr(attribute, category= nil, description= nil)
+  #     raise ArgumentError, "category (#{category}) must be a ::Phi::Category" unless category < ::Phi::Category
+  #     raise ArgumentError, "attr (#{attr})  must method name as a symbol" unless attribute.is_a?(::Symbol)
+  #     self.phi_dictionary_entry[:attrbutes] << ::Phi::Attribute.new(
+  #       self.name,
+  #       attribute,
+  #       category,
+  #       description
+  #     )
+  #   end
 
-  def self.setup_config
-    new_config = {
-      'db' => ['db/health'],
-      'db/migrate' => ['db/health/migrate'],
-      'db/seeds' => ['db/health/seeds'],
-      'config/database' => ['config/database_health.yml'],
-    }
-    ENV['SCHEMA'] = 'db/health/schema.rb'
-    # set config variables for custom database
-    new_config.each do |path, value|
-      Rails.application.config.paths[path] = value
-    end
-    db_config = Rails.application.config.paths['config/database'].to_a.first
-    ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read(db_config)).result)[Rails.env]
-  end
+  #   def phi_patient(attribute)
+  #     raise ArgumentError, "attr (#{attr})  must method name as a symbol" unless attribute.is_a?(::Symbol)
+  #     if (existing = phi_dictionary_entry[:patient_id]) && existing != attribute
+  #       raise ArgumentError, "Cannot set more then one phi_patient per class: class:#{self} existing: #{existing}, new: #{attribute}"
+  #     end
+  #     self.phi_dictionary_entry[:table_name] = table_name
+  #     self.phi_dictionary_entry[:patient_id] = attribute
+  #   end
+  # end
 
-  def self.needs_migration?
-    # integers from file list
-    (ActiveRecord::MigrationContext.new('db/health/migrate').migrations.collect(&:version) - Health::SchemaMigration.pluck(:version).map(&:to_i)).any?
-  end
+  # def self.setup_config
+  #   new_config = {
+  #     'db' => ['db/health'],
+  #     'db/migrate' => ['db/health/migrate'],
+  #     'db/seeds' => ['db/health/seeds'],
+  #     'config/database' => ['config/database_health.yml'],
+  #   }
+  #   ENV['SCHEMA'] = 'db/health/schema.rb'
+  #   # set config variables for custom database
+  #   new_config.each do |path, value|
+  #     Rails.application.config.paths[path] = value
+  #   end
+  #   db_config = Rails.application.config.paths['config/database'].to_a.first
+  #   ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read(db_config)).result)[Rails.env]
+  # end
+
+  # def self.needs_migration?
+  #   # integers from file list
+  #   (ActiveRecord::MigrationContext.new('db/health/migrate').migrations.collect(&:version) - Health::SchemaMigration.pluck(:version).map(&:to_i)).any?
+  # end
 end
