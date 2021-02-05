40539fbce6bb50330d0a768761d51a0176266217
Sandy Wise <sandy@greenriver.org>
2021-02-05 11:09:08 -0500

    Fix references column names

--- db/warehouse/migrate/20210201195631_create_cas_referral_events.rb
+++ db/warehouse/migrate/20210201195631_create_cas_referral_events.rb
@@ -1,10 +1,10 @@
 class CreateCasReferralEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :cas_referral_events do |t|
-      t.references :cas_client_id
-      t.references :hmis_client_id
-      t.references :program_id
-      t.references :client_opportunity_match_id
+      t.references :cas_client
+      t.references :hmis_client
+      t.references :program
+      t.references :client_opportunity_match
       t.date :referral_date
       t.integer :referral_result
       t.date :referral_result_date
@@ -13,8 +13,8 @@ class CreateCasReferralEvents < ActiveRecord::Migration[5.2]
     end
 
     create_table :cas_programs_to_projects do |t|
-      t.references :program_id
-      t.references :project_id
+      t.references :program
+      t.references :project
     end
   end
 end
--- db/warehouse/schema.rb
+++ db/warehouse/schema.rb
@@ -1158,26 +1158,26 @@ ActiveRecord::Schema.define(version: 2021_02_01_195631) do
   end
 
   create_table "cas_programs_to_projects", force: :cascade do |t|
-    t.bigint "program_id_id"
-    t.bigint "project_id_id"
-    t.index ["program_id_id"], name: "index_cas_programs_to_projects_on_program_id_id"
-    t.index ["project_id_id"], name: "index_cas_programs_to_projects_on_project_id_id"
+    t.bigint "program_id"
+    t.bigint "project_id"
+    t.index ["program_id"], name: "index_cas_programs_to_projects_on_program_id"
+    t.index ["project_id"], name: "index_cas_programs_to_projects_on_project_id"
   end
 
   create_table "cas_referral_events", force: :cascade do |t|
-    t.bigint "cas_client_id_id"
-    t.bigint "hmis_client_id_id"
-    t.bigint "program_id_id"
-    t.bigint "client_opportunity_match_id_id"
+    t.bigint "cas_client_id"
+    t.bigint "hmis_client_id"
+    t.bigint "program_id"
+    t.bigint "client_opportunity_match_id"
     t.date "referral_date"
     t.integer "referral_result"
     t.date "referral_result_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cas_client_id_id"], name: "index_cas_referral_events_on_cas_client_id_id"
-    t.index ["client_opportunity_match_id_id"], name: "index_cas_referral_events_on_client_opportunity_match_id_id"
-    t.index ["hmis_client_id_id"], name: "index_cas_referral_events_on_hmis_client_id_id"
-    t.index ["program_id_id"], name: "index_cas_referral_events_on_program_id_id"
+    t.index ["cas_client_id"], name: "index_cas_referral_events_on_cas_client_id"
+    t.index ["client_opportunity_match_id"], name: "index_cas_referral_events_on_client_opportunity_match_id"
+    t.index ["hmis_client_id"], name: "index_cas_referral_events_on_hmis_client_id"
+    t.index ["program_id"], name: "index_cas_referral_events_on_program_id"
   end
 
   create_table "cas_reports", id: :serial, force: :cascade do |t|
