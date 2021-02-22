3e63a33d7c2f03860a7adebb438045d0f8e76c7b
Todd Blackman <tdblackman@gmail.com>
2021-02-22 17:31:44 +0000

    Updates schema.rb for app db

--- db/schema.rb
+++ db/schema.rb
@@ -485,6 +485,18 @@ ActiveRecord::Schema.define(version: 2021_02_06_022724) do
     t.index ["translation_key_id"], name: "index_translation_texts_on_translation_key_id"
   end
 
+  create_table "two_factors_memorized_devices", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "uuid", null: false
+    t.string "name", null: false
+    t.datetime "expires_at", null: false
+    t.integer "session_id"
+    t.string "log_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_two_factors_memorized_devices_on_user_id"
+  end
+
   create_table "unique_names", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "double_metaphone"
@@ -609,6 +621,7 @@ ActiveRecord::Schema.define(version: 2021_02_06_022724) do
   add_foreign_key "glacier_archives", "glacier_vaults"
   add_foreign_key "report_results", "users"
   add_foreign_key "reports", "report_results_summaries"
+  add_foreign_key "two_factors_memorized_devices", "users"
   add_foreign_key "user_roles", "roles", on_delete: :cascade
   add_foreign_key "user_roles", "users", on_delete: :cascade
 end
