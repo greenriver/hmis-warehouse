53679186f2f02845134ab002fe04a8e199d9cd2c
Elliot Anders <elliot@greenriver.org>
2021-02-26 15:38:47 -0500

    Destination report

---
+++ drivers/destination_report/app/controllers/destination_report/warehouse_reports/reports_controller.rb
@@ -0,0 +1,121 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module DestinationReport::WarehouseReports
+  class ReportsController < ApplicationController
+    include WarehouseReportAuthorization
+    include AjaxModalRails::Controller
+    include ArelHelper
+    include BaseFilters
+
+    before_action :require_can_view_clients, only: [:detail]
+    before_action :set_report
+    before_action :set_pdf_export
+
+    def index
+      respond_to do |format|
+        format.html {}
+        format.xlsx do
+          filename = "Destination - #{Time.current.to_s(:db)}.xlsx"
+          headers['Content-Disposition'] = "attachment; filename=#{filename}"
+        end
+      end
+    end
+
+    def details
+      @key = params[:key]
+      @sub_key = params[:sub_key]
+      respond_to do |format|
+        format.html {}
+        format.xlsx do
+          filename = "Destination Support for #{@report.support_title(@key).gsub(',', '')} - #{Time.current.to_s(:db)}.xlsx"
+          headers['Content-Disposition'] = "attachment; filename=#{filename}"
+        end
+      end
+    end
+
+    private def set_report
+      @report = report_class.new(@filter)
+      if @report.include_comparison?
+        @comparison = report_class.new(@comparison_filter)
+      else
+        @comparison = @report
+      end
+    end
+
+    private def report_class
+      DestinationReport::Report
+    end
+
+    def section
+      @section = @report.class.available_section_types.detect do |m|
+        m == params.require(:partial).underscore
+      end
+      @section = 'overall' if @section.blank? && params.require(:partial) == 'overall'
+
+      raise 'Rollup not in allowlist' unless @section.present?
+
+      if @report.section_ready?(@section)
+        @section = @report.section_subpath + @section
+        render partial: @section, layout: false if request.xhr?
+      else
+        render_to_string(partial: @section, layout: false)
+        render status: :accepted, plain: 'Loading'
+      end
+    end
+
+    def breakdown
+      @breakdown ||= params[:breakdown]&.to_sym || :none
+    end
+    helper_method :breakdown
+
+    def filter_params
+      params.permit(
+        filters: [
+          :start,
+          :end,
+          :comparison_pattern,
+          :household_type,
+          :hoh_only,
+          :sub_population,
+          :chronic_status,
+          :coordinated_assessment_living_situation_homeless,
+          coc_codes: [],
+          project_types: [],
+          project_type_codes: [],
+          veteran_statuses: [],
+          age_ranges: [],
+          genders: [],
+          races: [],
+          ethnicities: [],
+          data_source_ids: [],
+          organization_ids: [],
+          project_ids: [],
+          funder_ids: [],
+          project_group_ids: [],
+          destination_report_ids: [],
+          destination_ids: [],
+          disabilities: [],
+          indefinite_disabilities: [],
+          dv_status: [],
+        ],
+      )
+    end
+    helper_method :filter_params
+
+    private def filter_class
+      ::Filters::FilterBase
+    end
+
+    private def set_pdf_export
+      @pdf_export = pdf_export_source.new
+    end
+
+    private def pdf_export_source
+      DestinationReport::DocumentExports::DestinationReportExport
+    end
+  end
+end
---
+++ drivers/destination_report/app/models/destination_report/report.rb
@@ -0,0 +1,245 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module DestinationReport
+  class Report
+    include Filter::ControlSections
+    include Filter::FilterScopes
+    include ActionView::Helpers::NumberHelper
+    include ArelHelper
+
+    attr_reader :filter
+    attr_accessor :comparison_pattern, :project_type_codes
+
+    def initialize(filter)
+      @filter = filter
+      @project_types = filter.project_type_ids || GrdaWarehouse::Hud::Project::HOMELESS_PROJECT_TYPES
+      @comparison_pattern = filter.comparison_pattern
+    end
+
+    def self.comparison_patterns
+      {
+        no_comparison_period: 'None',
+        prior_year: 'Same period, prior year',
+        prior_period: 'Prior Period',
+      }.invert.freeze
+    end
+
+    def self.viewable_by(user)
+      GrdaWarehouse::WarehouseReports::ReportDefinition.where(url: url).
+        viewable_by(user).exists?
+    end
+
+    def self.url
+      'destination_report/warehouse_reports/destination_report'
+    end
+
+    def multiple_project_types?
+      true
+    end
+
+    protected def build_control_sections
+      [
+        build_general_control_section,
+        build_coc_control_section,
+        build_household_control_section,
+        add_demographic_disabilities_control_section,
+        build_enrollment_control_section,
+      ]
+    end
+
+    def report_path_array
+      [
+        :destination_report,
+        :warehouse_reports,
+        :reports,
+      ]
+    end
+
+    def filter_path_array
+      [:filters] + report_path_array
+    end
+
+    def include_comparison?
+      comparison_pattern != :no_comparison_period
+    end
+
+    # @return filtered scope
+    def report_scope(all_project_types: false)
+      # Report range
+      scope = filter_for_range(report_scope_source)
+      scope = filter_for_cocs(scope)
+      scope = filter_for_sub_population(scope)
+      scope = filter_for_household_type(scope)
+      scope = filter_for_head_of_household(scope)
+      scope = filter_for_age(scope)
+      scope = filter_for_gender(scope)
+      scope = filter_for_race(scope)
+      scope = filter_for_ethnicity(scope)
+      scope = filter_for_veteran_status(scope)
+      scope = filter_for_project_type(scope, all_project_types: all_project_types)
+      scope = filter_for_data_sources(scope)
+      scope = filter_for_organizations(scope)
+      scope = filter_for_projects(scope)
+      scope = filter_for_funders(scope)
+      scope = filter_for_disabilities(scope)
+      scope = filter_for_indefinite_disabilities(scope)
+      scope = filter_for_dv_status(scope)
+      scope = filter_for_chronic_status(scope)
+      scope = filter_for_ca_homeless(scope)
+      scope = filter_for_prior_living_situation(scope)
+      scope = filter_for_destination(scope)
+      scope.joins(enrollment: :exit)
+    end
+
+    def report_scope_source
+      GrdaWarehouse::ServiceHistoryEnrollment.entry
+    end
+
+    def yn(boolean)
+      boolean ? 'Yes' : 'No'
+    end
+
+    def total_client_count
+      @total_client_count ||= Rails.cache.fetch([self.class.name, cache_slug, __method__], expires_in: expiration_length) do
+        distinct_client_ids.count
+      end
+    end
+
+    def hoh_count
+      @hoh_count ||= Rails.cache.fetch([self.class.name, cache_slug, __method__], expires_in: expiration_length) do
+        hoh_scope.select(:client_id).distinct.count
+      end
+    end
+
+    def household_count
+      @household_count ||= Rails.cache.fetch([self.class.name, cache_slug, __method__], expires_in: expiration_length) do
+        report_scope.select(:household_id).distinct.count
+      end
+    end
+
+    def data_for_destinations
+      @data_for_destinations ||= begin
+        data = {}
+        report_scope.joins(project: :project_cocs).
+          order(:CoCCode, :first_date_in_program).
+          pluck(
+            :client_id,
+            :Destination,
+            :CoCCode,
+          ).each do |client_id, destination_id, coc_code|
+            destination = HUD.destination_type(destination_id)
+            detailed_destination = HUD.destination(destination_id)
+
+            data[:all] ||= destination_buckets.map { |b| [b, Set.new] }.to_h
+            data[:all][destination] << client_id
+
+            data[:by_coc] ||= {}
+            data[:by_coc][coc_code] ||= {}
+            data[:by_coc][coc_code][:clients] ||= {}
+            next if data[:by_coc][coc_code][:clients][client_id]
+
+            data[:by_coc][coc_code][:clients][client_id] ||= {
+              destination_id: destination_id,
+              destination: destination,
+              detailed_destination: detailed_destination,
+              coc_code: coc_code,
+            }
+
+            data[:by_coc][coc_code][:destinations] ||= destination_buckets.map { |b| [b, Set.new] }.to_h
+
+            data[:by_coc][coc_code][:destination_details] ||= destination_buckets.map { |b| [b, {}] }.to_h
+            destination_buckets.each do |b|
+              HUD.valid_destinations.values.uniq.each do |l|
+                data[:by_coc][coc_code][:destination_details][b][l] ||= Set.new
+              end
+              data[:by_coc][coc_code][:destination_details][b]['Unknown'] ||= Set.new
+            end
+
+            data[:by_coc][coc_code][:destinations][destination] ||= destination_buckets.map { |b| [b, Set.new] }.to_h
+            data[:by_coc][coc_code][:destinations][destination] << client_id
+            data[:by_coc][coc_code][:destination_details][destination][detailed_destination || 'Unknown'] << client_id
+          end
+        data
+      end
+    end
+
+    private def destination_buckets
+      [
+        'Homeless',
+        'Institutional',
+        'Temporary',
+        'Permanent',
+        'Other',
+      ]
+    end
+
+    def self.data_for_export(reports)
+      {}.tap do |rows|
+        reports.each do |report|
+          rows['Date Range'] ||= []
+          rows['Date Range'] += [report.filter.date_range_words, nil, nil, nil]
+          rows['Unique Clients'] ||= []
+          rows['Unique Clients'] += [report.total_client_count, nil, nil, nil]
+          rows['Heads of Household'] ||= []
+          rows['Heads of Household'] += [report.hoh_count, nil, nil, nil]
+          rows['Households'] ||= []
+          rows['Households'] += [report.household_count, nil, nil, nil]
+
+          rows = report.age_data_for_export(rows)
+          rows = report.gender_data_for_export(rows)
+          rows = report.race_data_for_export(rows)
+          rows = report.ethnicity_data_for_export(rows)
+          rows = report.relationship_data_for_export(rows)
+          rows = report.disability_data_for_export(rows)
+          rows = report.dv_status_data_for_export(rows)
+          rows = report.priors_data_for_export(rows)
+          rows = report.household_type_data_for_export(rows)
+        end
+      end
+    end
+
+    private def hoh_scope
+      report_scope.where(she_t[:head_of_household].eq(true))
+    end
+
+    private def distinct_client_ids
+      report_scope.select(:client_id).distinct
+    end
+
+    private def adult_clause
+      Arel.sql("EXTRACT(YEAR FROM #{age_calculation.to_sql})").in((18..110).to_a)
+    end
+
+    private def child_clause
+      Arel.sql("EXTRACT(YEAR FROM #{age_calculation.to_sql})").in((0..17).to_a)
+    end
+
+    private def male_clause
+      c_t[:Gender].eq(1)
+    end
+
+    private def female_clause
+      c_t[:Gender].eq(0)
+    end
+
+    private def average_age(clause:)
+      average_age = nf('AVG', [age_calculation])
+      scope = report_scope.joins(:client).where(clause)
+      scope.joins(:client).pluck(Arel.sql("EXTRACT(YEAR FROM #{average_age.to_sql})"))&.first&.to_i
+    end
+
+    private def cache_slug
+      @filter.attributes
+    end
+
+    private def expiration_length
+      return 30.seconds if Rails.env.development?
+
+      30.minutes
+    end
+  end
+end
