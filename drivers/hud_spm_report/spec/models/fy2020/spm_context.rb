f7b77df54335752410fadcc4b39852abefe3b293
Aaron Michal <amichal@greenriver.com>
2021-02-26 10:23:19 -0500

    HUD FY2020 SPM Specs: tighten up spec context

--- drivers/hud_spm_report/spec/models/fy2020/spm_context.rb
+++ drivers/hud_spm_report/spec/models/fy2020/spm_context.rb
@@ -15,7 +15,7 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
     cleanup
     puts '  Setting up DB'
     @user = create(:user, email: SPM_USER_EMAIL)
-    @delete_later = []
+    @delete_later = [] # We are going to create some temporary folders that need cleanup
   end
 
   after(:context) do
@@ -24,17 +24,14 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
 
   def cleanup
     puts '  Cleaning up DB and temporary files'
-    @user&.really_destroy!
-    User.with_deleted.where(email: SPM_USER_EMAIL).delete_all
-    HudSpmReport::Fy2020::SpmClient.delete_all
-    GrdaWarehouse::Utility.clear!
-
-    return unless @delete_later
-
-    while (path = @delete_later.pop)
+    while (path = @delete_later&.pop)
       # puts "Removing #{path}"
       FileUtils.rm_rf(path)
     end
+
+    HudSpmReport::Fy2020::SpmClient.delete_all
+    GrdaWarehouse::Utility.clear!
+    @user&.really_destroy!
   end
 
   def shared_filter
@@ -59,6 +56,7 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
 
   def run(filter, question_number)
     # puts "Running #{filter} for #{question_number}"
+    @report = nil
 
     klass = HudSpmReport::Generators::Fy2020::Generator
     @generator = klass.new(
@@ -69,11 +67,12 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
       ),
     )
     @generator.run!
+
+    @report_result = @generator.report
+    @report_result.reload
   end
 
-  def report_result
-    ::HudReports::ReportInstance.last
-  end
+  attr_reader :report_result
 
   def setup(file_path)
     @data_source = GrdaWarehouse::DataSource.create(name: 'Green River', short_name: 'GR', source_type: :sftp)
@@ -85,12 +84,15 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
     # relative to our own spec fixture files
     file_path = Rails.root.join('drivers/hud_spm_report/spec/fixtures/files', file_path)
     source_file_path = File.join(file_path, 'source')
-    import_path = File.join(file_path, data_source.id.to_s)
 
-    # duplicate the fixture file as it gets manipulated
+    # Importers::HmisTwentyTwenty::Base expects a directory named after that data source
+    # to work with and wants file_path to point to its parent. It will potentially
+    # tamper with the directory contents so we make a temporary copy of the fixture data
+    # and delete it later
+    import_path = File.join(file_path, data_source.id.to_s)
     # puts "Creating #{import_path}"
     FileUtils.cp_r(source_file_path, import_path)
-    @delete_later << import_path unless import_path == source_file_path
+    @delete_later << import_path
 
     importer = Importers::HmisTwentyTwenty::Base.new(file_path: file_path, data_source_id: data_source.id, remove_files: false)
 
@@ -98,11 +100,14 @@ RSpec.shared_context 'HudSpmReport context', shared_context: :metadata do
 
     importer.import!
 
+    # We need this import to be fully processed right now
+    # so run various normally async processes
     GrdaWarehouse::Tasks::IdentifyDuplicates.new.run!
     GrdaWarehouse::Tasks::ProjectCleanup.new.run!
     GrdaWarehouse::Tasks::ServiceHistory::Add.new.run!
     AccessGroup.maintain_system_groups
-
     Delayed::Worker.new.work_off
+
+    importer
   end
 end
