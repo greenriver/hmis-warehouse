4598e3562c0496ae41d92b37c6ffd8d870d059e7
Elliot Anders <elliot@greenriver.org>
2021-03-01 16:25:23 -0500

    WIP permissions

--- drivers/access_control/extensions/grda_warehouse/hud/client_extension.rb
+++ drivers/access_control/extensions/grda_warehouse/hud/client_extension.rb
@@ -12,8 +12,9 @@ module AccessControl::GrdaWarehouse::Hud
     included do
       scope :destination_visible_to, ->(user) do
         return none unless user
+        return none unless user.can_view_full_client_dashboard? || user.can_view_limited_client_dashboard?
 
-        joins(:source_client).
+        joins(:source_clients).
           merge(source_visible_to(user))
       end
 
@@ -21,8 +22,11 @@ module AccessControl::GrdaWarehouse::Hud
         return none unless user
         return none unless user.can_view_full_client_dashboard? || user.can_view_limited_client_dashboard?
 
-        where(arel_table[:id].in(Arel.sql(joins(:enrollments).merge(GrdaWarehouse::Hud::Enrollment.visible_to(user)).select(:id).to_sql)).
-          or(arel_table[:id].in(Arel.sql(active_confirmed_consent_in_cocs(user.coc_codes).joins(:warehouse_client_source).select(:source_id).to_sql))))
+        from_source_enrollments = joins(:enrollments).
+          merge(GrdaWarehouse::Hud::Enrollment.visible_to(user)).select(:id)
+        from_roi = active_confirmed_consent_in_cocs(user.coc_codes).joins(:warehouse_client_source).select(:source_id)
+        where(arel_table[:id].in(Arel.sql(from_source_enrollments.to_sql)).
+          or(arel_table[:id].in(Arel.sql(from_roi.to_sql))))
         # TODO: handle authoritative clients
         # Arbiter
       end
