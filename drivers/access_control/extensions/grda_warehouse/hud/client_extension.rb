ab7c36c5aefd138a7eb3413e3c23bc455dcb3752
Elliot Anders <elliot@greenriver.org>
2021-03-01 15:22:41 -0500

    WIP tests

--- drivers/access_control/extensions/grda_warehouse/hud/client_extension.rb
+++ drivers/access_control/extensions/grda_warehouse/hud/client_extension.rb
@@ -12,8 +12,6 @@ module AccessControl::GrdaWarehouse::Hud
     included do
       scope :destination_visible_to, ->(user) do
         return none unless user
-        return none unless user.can_view_full_client_dashboard?
-        return none unless user.can_view_limited_client_dashboard?
 
         joins(:source_client).
           merge(source_visible_to(user))
@@ -21,14 +19,10 @@ module AccessControl::GrdaWarehouse::Hud
 
       scope :source_visible_to, ->(user) do
         return none unless user
-        return none unless user.can_view_full_client_dashboard?
-        return none unless user.can_view_limited_client_dashboard?
+        return none unless user.can_view_full_client_dashboard? || user.can_view_limited_client_dashboard?
 
-        where(
-          id: active_confirmed_consent_in_cocs(user.coc_codes).
-          joins(:warehouse_client).select(:source_id),
-        ).
-          or(joins(:enrollment).visible_to(user))
+        where(arel_table[:id].in(Arel.sql(joins(:enrollments).merge(GrdaWarehouse::Hud::Enrollment.visible_to(user)).select(:id).to_sql)).
+          or(arel_table[:id].in(Arel.sql(active_confirmed_consent_in_cocs(user.coc_codes).joins(:warehouse_client_source).select(:source_id).to_sql))))
         # TODO: handle authoritative clients
         # Arbiter
       end
--- drivers/access_control/spec/models/visibility_spec.rb
+++ drivers/access_control/spec/models/visibility_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GrdaWarehouse::Hud::Client, type: :model do
   let!(:source_data_source) { create :vt_source_data_source }
   let!(:vt_destination_data_source) { create :vt_destination_data_source }
-  let!(:project_coc) { create :vt_project_coc, data_source: source_data_source }
+  let!(:project_coc) { create :vt_project_coc, data_source: source_data_source, CoCCode: 'MA-500' }
   let!(:visible_project) { create :vt_project, ProjectName: 'Visible Project', data_source: source_data_source }
   let!(:visible_enrollment) { create :vt_enrollment, data_source: source_data_source }
   let!(:visible_client) { create :vt_source_client, data_source: source_data_source }
@@ -11,7 +11,7 @@ RSpec.describe GrdaWarehouse::Hud::Client, type: :model do
   let!(:warehouse_client) { create :vt_warehouse_client, source_id: visible_client.id, destination_id: destination_client.id }
   let!(:user) { create :vt_user }
   let!(:role) { create :vt_role }
-  # can_view_limited_client_dashboard
+
   context 'Using visible_to client permission model' do
     describe 'and the user does not have a role' do
       it 'user cannot see any clients' do
@@ -22,10 +22,85 @@ RSpec.describe GrdaWarehouse::Hud::Client, type: :model do
       end
     end
 
-    describe 'and the user has permission to see clients, but not any data assignments' do
+    describe 'and the user has permission to see full dashboard, but not any data assignments' do
       before do
         role.update(can_view_full_client_dashboard: true)
         user.roles = [role]
+      end
+      it 'user cannot see any clients' do
+        expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.source_visible_to(user).count).to eq(0)
+        expect(GrdaWarehouse::Hud::Client.destination_visible_to(user).count).to eq(0)
+      end
+    end
+
+    describe 'and the user has permission to see limited dashboard, but not any data assignments' do
+      before do
+        role.update(can_view_limited_client_dashboard: true)
+        user.roles = [role]
+      end
+      it 'user cannot see any clients' do
+        expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.source_visible_to(user).count).to eq(0)
+        expect(GrdaWarehouse::Hud::Client.destination_visible_to(user).count).to eq(0)
+      end
+    end
+
+    describe 'and the user has permission to see clients in wrong CoC, but not any data assignments' do
+      before do
+        user.roles = []
+        user.coc_codes = ['MA-501']
+        destination_client.update(
+          housing_release_status: GrdaWarehouse::Hud::Client.full_release_string,
+          consented_coc_codes: ['MA-500'],
+        )
+      end
+      it 'user cannot see any clients' do
+        expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.source_visible_to(user).count).to eq(0)
+        expect(GrdaWarehouse::Hud::Client.destination_visible_to(user).count).to eq(0)
+      end
+    end
+
+    # Positive test
+    describe 'and the user has permission to see full dashboard can see client based on project enrollment' do
+      before do
+        role.update(can_view_full_client_dashboard: true)
+        user.roles = [role]
+        user.add_viewable(visible_project)
+      end
+      it 'user cannot see any clients' do
+        expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.source_visible_to(user).count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination_visible_to(user).count).to eq(1)
+      end
+    end
+
+    describe 'and the user has permission to see limited dashboard, but not any data assignments' do
+      before do
+        role.update(can_view_limited_client_dashboard: true)
+        user.roles = [role]
+      end
+      it 'user cannot see any clients' do
+        expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.destination.count).to eq(1)
+        expect(GrdaWarehouse::Hud::Client.source_visible_to(user).count).to eq(0)
+        expect(GrdaWarehouse::Hud::Client.destination_visible_to(user).count).to eq(0)
+      end
+    end
+
+    describe 'and the user has permission to see clients in wrong CoC, but not any data assignments' do
+      before do
+        user.roles = []
+        user.coc_codes = ['MA-501']
+        destination_client.update(
+          housing_release_status: GrdaWarehouse::Hud::Client.full_release_string,
+          consented_coc_codes: ['MA-500'],
+        )
       end
       it 'user cannot see any clients' do
         expect(GrdaWarehouse::Hud::Client.source.count).to eq(1)
