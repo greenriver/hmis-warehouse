78115dc148e1ca680a0c71f65052781572097209
Elliot Anders <elliot@greenriver.org>
2021-03-01 15:45:37 -0500

    Include project type selections in project selection criteria

--- drivers/hud_apr/app/models/hud_apr/filters/apr_filter.rb
+++ drivers/hud_apr/app/models/hud_apr/filters/apr_filter.rb
@@ -11,9 +11,16 @@ module HudApr::Filters
       errors.add(:project_ids, 'or project groups must be specified') if project_ids.blank? && project_group_ids.blank?
     end
 
-    # FilterBase defines semantics for coc_codes vs coc_code which this disables
-    def effective_project_ids_from_coc_codes
-      []
+    # NOTE: This differs from the base filter class because it includes all projects based on project type, and doesn't include any projects based on CoCCode
+    def effective_project_ids
+      @effective_project_ids = effective_project_ids_from_projects
+      @effective_project_ids += effective_project_ids_from_project_groups
+      @effective_project_ids += effective_project_ids_from_organizations
+      @effective_project_ids += effective_project_ids_from_data_sources
+      @effective_project_ids += effective_project_ids_from_project_types
+      @effective_project_ids = all_project_ids if @effective_project_ids.empty?
+
+      @effective_project_ids.uniq.reject(&:blank?)
     end
   end
 end
--- drivers/hud_data_quality_report/app/models/hud_data_quality_report/filters/dq_filter.rb
+++ drivers/hud_data_quality_report/app/models/hud_data_quality_report/filters/dq_filter.rb
@@ -6,13 +6,20 @@
 
 module HudDataQualityReport::Filters
   class DqFilter < ::Filters::FilterBase
-    # FilterBase defines semantics for coc_codes vs coc_code which this disables
-    def effective_project_ids_from_coc_codes
+    def default_project_type_codes
       []
     end
 
-    def default_project_type_codes
-      []
+    # NOTE: This differs from the base filter class because it includes all projects based on project type, and doesn't include any projects based on CoCCode
+    def effective_project_ids
+      @effective_project_ids = effective_project_ids_from_projects
+      @effective_project_ids += effective_project_ids_from_project_groups
+      @effective_project_ids += effective_project_ids_from_organizations
+      @effective_project_ids += effective_project_ids_from_data_sources
+      @effective_project_ids += effective_project_ids_from_project_types
+      @effective_project_ids = all_project_ids if @effective_project_ids.empty?
+
+      @effective_project_ids.uniq.reject(&:blank?)
     end
   end
 end
