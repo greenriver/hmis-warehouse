90552a8f750ede48c4c574a3cc71de6ff922604d
Elliot Anders <elliot@greenriver.org>
2021-03-09 15:38:16 -0500

    WIP; scaffolding for permission arbiter

--- app/models/grda_warehouse/hud/client.rb
+++ app/models/grda_warehouse/hud/client.rb
@@ -309,14 +309,6 @@ module GrdaWarehouse::Hud
       where.not(hiv_positive: false)
     end
 
-    scope :visible_in_window_to, ->(user) do
-      joins(:data_source).merge(GrdaWarehouse::DataSource.visible_in_window_to(user))
-    end
-
-    scope :visible_by_project_to, ->(user) do
-      joins(enrollments: :project).merge(GrdaWarehouse::Hud::Project.viewable_by(user))
-    end
-
     scope :has_homeless_service_after_date, ->(date: 31.days.ago) do
       where(id:
         GrdaWarehouse::ServiceHistoryService.homeless(chronic_types_only: true).
@@ -382,118 +374,6 @@ module GrdaWarehouse::Hud
         distinct.pluck(:client_id)
       joins(:client_files).
       where(id: (unconfirmed_consent + unconfirmed_disability).uniq)
-    end
-
-    def self.exists_with_inner_clients(inner_scope)
-      inner_scope = inner_scope.to_sql.gsub('"Client".', '"inner_clients".').gsub('"Client"', '"Client" as "inner_clients"')
-      Arel.sql("EXISTS (#{inner_scope} and \"Client\".\"id\" = \"inner_clients\".\"id\")")
-    end
-
-    scope :searchable_by, -> (user) do
-      if user.can_view_clients_with_roi_in_own_coc?
-        current_scope
-      elsif user.can_view_clients? || user.can_edit_clients?
-        current_scope
-      else
-        ds_ids = user.data_sources.pluck(:id)
-        project_query = exists_with_inner_clients(visible_by_project_to(user))
-        window_query = exists_with_inner_clients(visible_in_window_to(user))
-
-        if user&.can_see_clients_in_window_for_assigned_data_sources? && ds_ids.present?
-          where(
-            arel_table[:data_source_id].in(ds_ids).
-            or(project_query).
-            or(window_query)
-          )
-        else
-          where(
-            arel_table[:id].eq(0). # no client should have a 0 id
-            or(project_query).
-            or(window_query)
-          )
-        end
-      end
-    end
-
-    scope :viewable_by, -> (user) do
-      project_query = exists_with_inner_clients(visible_by_project_to(user))
-      window_query = exists_with_inner_clients(visible_in_window_to(user))
-      active_consent_query = if GrdaWarehouse::Config.get(:multi_coc_installation)
-        exists_with_inner_clients(active_confirmed_consent_in_cocs(user.coc_codes))
-      else
-        exists_with_inner_clients(consent_form_valid)
-      end
-      if user.can_view_clients_with_roi_in_own_coc?
-        # At a high level if you can see clients with ROI in your COC, you need to be able
-        # to see everyone for searching purposes.
-        # limits will be imposed on accessing the actual client dashboard pages
-        # current_scope
-
-        # If the user has coc-codes specified, this will limit to users
-        # with a valid consent form in the coc or with no-coc specified
-        # If the user does not have a coc-code specified, only clients with a full (CoC not specified) release
-        # are included.
-        if user&.can_see_clients_in_window_for_assigned_data_sources?
-          ds_ids = user.data_sources.pluck(:id)
-          sql = arel_table[:data_source_id].in(ds_ids).
-            or(active_consent_query).
-            or(project_query)
-          unless GrdaWarehouse::Config.get(:window_access_requires_release)
-            sql = sql.or(window_query)
-          end
-
-          where(sql)
-        else
-          active_confirmed_consent_in_cocs(user.coc_codes)
-        end
-      elsif user.can_view_clients? || user.can_edit_clients?
-        current_scope
-      else
-        ds_ids = user.data_sources.pluck(:id)
-        if user&.can_see_clients_in_window_for_assigned_data_sources? && ds_ids.present?
-          sql = arel_table[:data_source_id].in(ds_ids)
-          sql = sql.or(project_query)
-          if GrdaWarehouse::Config.get(:window_access_requires_release)
-            sql = sql.or(active_consent_query)
-          else
-            sql = sql.or(window_query)
-          end
-          where(sql)
-        else
-          sql = arel_table[:id].eq(0) # no client should have a 0 id
-          sql = sql.or(project_query)
-          if GrdaWarehouse::Config.get(:window_access_requires_release)
-            sql = sql.or(active_consent_query)
-          else
-            sql = sql.or(window_query)
-          end
-
-          where(sql)
-        end
-      end
-    end
-
-    # should always return a destination client, but some visibility
-    # is governed by the source client, some by the destination
-    def self.destination_client_viewable_by_user(client_id:, user:)
-      destination.where(
-        Arel.sql(
-          arel_table[:id].in(visible_by_source(id: client_id, user: user)).
-          or(arel_table[:id].in(visible_by_destination(id: client_id, user: user))).to_sql,
-        ),
-      )
-    end
-
-    def self.visible_by_source(id:, user:)
-      query = GrdaWarehouse::WarehouseClient.joins(:source).merge(viewable_by(user))
-      query = query.where(destination_id: id) if id.present?
-      Arel.sql(query.select(:destination_id).to_sql)
-    end
-
-    def self.visible_by_destination(id:, user:)
-      query = viewable_by(user)
-      query = query.where(id: id) if id.present?
-      Arel.sql(query.select(:id).to_sql)
     end
 
     def hmis_source_visible_by?(user)
---
+++ drivers/access_control/app/models/access_control/enrollment_arbiter.rb
@@ -0,0 +1,36 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module AccessControl
+  class EnrollmentArbiter
+    def clients_destination_visible_to(scope, user)
+      return none unless user
+
+      scope.joins(:source_client).
+        merge(scope.source_visible_to(user))
+    end
+
+    def clients_source_visible_to(scope, user)
+      return none unless user
+      return none unless user.can_view_full_client_dashboard? || user.can_view_limited_client_dashboard?
+
+      data_source_ids = GrdaWarehouse::DataSource.authoritative.directly_viewable_by(user).pluck(:id)
+      scope.where(
+        arel_table[:id].in(Arel.sql(scope.joins(:enrollments).merge(GrdaWarehouse::Hud::Enrollment.visible_to(user)).select(:id).to_sql)).
+        or(arel_table[:id].in(Arel.sql(scope.joins(:data_source).where(ds_t[:id].in(data_source_ids).select(:id).to_sql)))),
+      )
+    end
+
+    def enrollments_visible_to(scope, user)
+      project_ids = GrdaWarehouse::Hud::Project.visible_to(user).pluck(:id).uniq
+      coc_codes = user.coc_codes
+      scope.where(
+        arel_table[:id].in(Arel.sql(scope.joins(:project).where(p_t[:id].in(project_ids)))).
+        or(arel_table[:id].in(Arel.sql(GrdaWarehouse::Hud::Client.active_confirmed_consent_in_cocs(coc_codes).joins(:source_enrollments).select(arel_table[:id]).to_sql))),
+      )
+    end
+  end
+end
