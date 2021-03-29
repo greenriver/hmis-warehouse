dc7541bec8481ff8584cb716a4abef915bdbd067
Sandy Wise <sandy@greenriver.org>
2021-03-29 17:09:54 -0400

    Migration to clean up and prevent duplicates

---
+++ db/warehouse/migrate/20210325202706_uniq_universe_memberships.rb
@@ -0,0 +1,40 @@
+class UniqUniverseMemberships < ActiveRecord::Migration[5.2]
+  def up
+    cells_with_duplicates = {}.tap do |hash|
+      HudReports::UniverseMember.
+        group(:report_cell_id, :client_id).having('count(*) > 1').
+        pluck(:report_cell_id, :client_id).
+        each do |report_cell_id, client_id|
+        hash[report_cell_id] ||= []
+        hash[report_cell_id] << client_id
+      end
+    end
+
+    duplicate_ids = []
+    cells_with_duplicates.each do |report_cell_id, client_ids|
+      candidate_ids = HudReports::UniverseMember.where(report_cell_id: report_cell_id, client_id: client_ids).pluck(:id)
+      duplicate_ids += (candidate_ids - [candidate_ids.max])
+      puts duplicate_ids.size
+    end
+
+    deletion = Date.current.to_time
+    HudReports::UniverseMember.where(id: duplicate_ids).update(deleted_at: deletion) if duplicate_ids.size.positive?
+
+    cells_with_duplicates.each do |cell_id|
+      HudReports::UniverseMember.where(report_cell_id: cell_id)
+    end
+
+    HudReports::UniverseMember.where.not(id:
+      HudReports::UniverseMember.
+        group(:report_cell_id, :universe_membership_id, :universe_membership_type).
+        select('max(id)'),
+    ).delete_all
+
+    if index_exists? :hud_report_universe_members, [:report_cell_id]
+      remove_index :hud_report_universe_members, column: [:report_cell_id]
+    end
+    add_index :hud_report_universe_members, [:report_cell_id, :universe_membership_id, :universe_membership_type],
+      unique: true,
+      name: 'uniq_hud_report_universe_members'
+  end
+end
