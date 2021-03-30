060b615643641ce99e001dcbccc95f42fc60178e
Sandy Wise <sandy@greenriver.org>
2021-03-30 09:00:23 -0400

    Split uniquness migration into two transactions

--- db/warehouse/migrate/20210325202706_uniq_universe_memberships.rb
+++ db/warehouse/migrate/20210325202706_uniq_universe_memberships.rb
@@ -10,28 +10,14 @@ class UniqUniverseMemberships < ActiveRecord::Migration[5.2]
       end
     end
 
-    duplicate_ids = []
-    cells_with_duplicates.each do |report_cell_id, client_ids|
-      candidate_ids = HudReports::UniverseMember.where(report_cell_id: report_cell_id, client_id: client_ids).pluck(:id)
-      duplicate_ids += (candidate_ids - [candidate_ids.max])
-      puts duplicate_ids.size
+    duplicate_ids = [].tap do |dups|
+      cells_with_duplicates.each do |report_cell_id, client_ids|
+        candidate_ids = HudReports::UniverseMember.where(report_cell_id: report_cell_id, client_id: client_ids).pluck(:id)
+        dups.concat(candidate_ids - [candidate_ids.max])
+      end
     end
 
     deletion = Date.current.to_time
     HudReports::UniverseMember.where(id: duplicate_ids).update(deleted_at: deletion) if duplicate_ids.size.positive?
-
-    if index_exists? :hud_report_universe_members, [:report_cell_id]
-      remove_index :hud_report_universe_members, column: [:report_cell_id]
-    end
-    add_index :hud_report_universe_members, [:report_cell_id, :universe_membership_id, :universe_membership_type],
-      unique: true,
-      name: 'uniq_hud_report_universe_members',
-      where: 'deleted_at IS NULL'
-  end
-
-  def down
-    if index_exists? hud_report_universe_members, [:report_cell_id, :universe_membership_id, :universe_membership_type]
-      remove_index :hud_report_universe_members, name: :uniq_hud_report_universe_members
-    end
   end
 end
--- db/warehouse/schema.rb
+++ db/warehouse/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_202706) do
+ActiveRecord::Schema.define(version: 2021_03_30_124825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
