f544e5715e54c4ce4afbdabf27e5a1720fec20f5
Sandy Wise <sandy@greenriver.org>
2021-03-31 10:07:44 -0400

    Adjust EnforceUniqUniverseMemberships migration to recognize prior indexes

--- db/warehouse/migrate/20210330124825_enforce_uniq_universe_memberships.rb
+++ db/warehouse/migrate/20210330124825_enforce_uniq_universe_memberships.rb
@@ -1,8 +1,14 @@
 class EnforceUniqUniverseMemberships < ActiveRecord::Migration[5.2]
   def up
+    # Remove old indexes if they exist
     if index_exists? :hud_report_universe_members, [:report_cell_id]
       remove_index :hud_report_universe_members, column: [:report_cell_id]
     end
+
+    if index_exists? :hud_report_universe_members, [:report_cell_id, :universe_membership_id, :universe_membership_type]
+      remove_index :hud_report_universe_members, column: [:report_cell_id, :universe_membership_id, :universe_membership_type]
+    end
+
     add_index :hud_report_universe_members, [:report_cell_id, :universe_membership_id, :universe_membership_type],
       unique: true,
       name: 'uniq_hud_report_universe_members',
