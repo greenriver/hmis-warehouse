ef0828231a84fe385caf6c149caebe66711c72d8
Aaron Michal <amichal@greenriver.com>
2021-04-08 16:09:03 -0400

    Run omniauth tests seperately. The main suite assumes we arent using it

--- spec/requests/omniauth_spec.rb
+++ spec/requests/omniauth_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
-
-# Make sure that https://nvd.nist.gov/vuln/detail/CVE-2015-9284 is mitigated
-RSpec.describe Users::OmniauthCallbacksController, type: :request do
-  describe 'GET /users/auth/:provider' do
-    it do
-      get '/users/auth/okta'
-      expect(response).not_to have_http_status(:redirect)
-    end
-  end
-
-  describe 'POST /auth/:provider without CSRF token' do
-    before do
-      @allow_forgery_protection = ActionController::Base.allow_forgery_protection
-      ActionController::Base.allow_forgery_protection = true
+if ENV['OKTA_DOMAIN'].present?
+  # Make sure that https://nvd.nist.gov/vuln/detail/CVE-2015-9284 is mitigated
+  RSpec.describe Users::OmniauthCallbacksController, type: :request do
+    describe 'GET /users/auth/:provider protects against CVE-2015-9284' do
+      it do
+        get '/users/auth/okta'
+        expect(response).not_to have_http_status(:redirect)
+      end
     end
 
-    it do
-      expect do
-        post '/users/auth/okta'
-      end.to raise_error(ActionController::InvalidAuthenticityToken)
-    end
+    describe 'POST /auth/:provider without CSRF token protects against CVE-2015-9284' do
+      before do
+        @allow_forgery_protection = ActionController::Base.allow_forgery_protection
+        ActionController::Base.allow_forgery_protection = true
+      end
 
-    after do
-      ActionController::Base.allow_forgery_protection = @allow_forgery_protection
+      it do
+        expect do
+          post '/users/auth/okta'
+        end.to raise_error(ActionController::InvalidAuthenticityToken)
+      end
+
+      after do
+        ActionController::Base.allow_forgery_protection = @allow_forgery_protection
+      end
     end
   end
 end
