caa6a5a438dd908ffb155ace1ef4e7b7cf6cdbb3
Elliot Anders <elliot@greenriver.org>
2021-05-10 14:20:35 -0400

    Enables to add and remove users from Groups and Roles pages

---
+++ app/controllers/admin/roles/users_controller.rb
@@ -0,0 +1,41 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module Admin::Roles
+  class UsersController < ApplicationController
+    include ActionView::Helpers::TextHelper
+    before_action :require_can_edit_roles!
+    before_action :set_role
+
+    def create
+      # add to the role any users passed through
+      user_ids = clean_params[:user_ids].select(&:present?).map(&:to_i)
+      @role.users = (@role.users + User.where(id: user_ids).to_a).uniq
+      flash[:notice] = "#{pluralize(user_ids.count, 'user')} added"
+      redirect_to edit_admin_role_path(@role)
+    end
+
+    def destroy
+      user = User.find(params[:id].to_i)
+      user.roles = (user.roles.to_a - [@role])
+      flash[:notice] = "#{user.name} removed from #{@role.name}"
+      redirect_to edit_admin_role_path(@role)
+    end
+
+    private def set_role
+      @role = role_scope.find(params[:role_id].to_i)
+    end
+
+    private def role_scope
+      Role.editable
+    end
+
+    private def clean_params
+      params.require(:role_users).
+        permit(user_ids: [])
+    end
+  end
+end
