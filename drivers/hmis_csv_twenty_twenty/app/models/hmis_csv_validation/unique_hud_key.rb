93018eb0fd92b99ba4279d50930949e9f2c0827c
Elliot Anders <elliot@greenriver.org>
2021-05-10 15:49:59 -0400

    Functional tests for importer scrubbing

--- drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_validation/unique_hud_key.rb
+++ drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_validation/unique_hud_key.rb
@@ -23,27 +23,28 @@ class HmisCsvValidation::UniqueHudKey < HmisCsvValidation::Validation
     ).
       update_all(should_import: false)
     # Mark one of each key as importable
-    klass.where(
+    ids = klass.where(
       importer_log_id: importer_log.id,
       klass.hud_key => incorrect_counts.keys,
     ).
       distinct_on(klass.hud_key).
-      update_all(should_import: true)
+      pluck(:id)
+    klass.where(id: ids).update_all(should_import: true)
     # Note those we didn't import
     klass.where(
       importer_log_id: importer_log.id,
       klass.hud_key => incorrect_counts.keys,
       should_import: false,
-    )
-    find_each do |item|
-      failures << new(
-        importer_log_id: importer_log.id,
-        source_id: item.source_id,
-        source_type: item.source_type,
-        status: "Non unique primary key: #{klass.hud_key}",
-        validated_column: klass.hud_key,
-      )
-    end
+    ).
+      find_each do |item|
+        failures << new(
+          importer_log_id: importer_log.id,
+          source_id: item.source_id,
+          source_type: item.source_type,
+          status: "Non unique primary key: #{klass.hud_key}",
+          validated_column: klass.hud_key,
+        )
+      end
     failures
   end
 
