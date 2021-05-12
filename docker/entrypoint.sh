cb9f032067f9b1fe8e301bb2a08d0434a683185e
Aaron Michal <amichal@greenriver.com>
2021-05-12 10:00:00 -0400

    Dont expose internal services to the host, reduce some noise and fix missing yarn dependancy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-cd /app && bundle install
+cd /app
+bundle check || bundle install
 npm install
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
