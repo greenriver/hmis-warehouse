3b836343ce6ef78780ed9fd818fa757376da585f
Aaron Michal <amichal@greenriver.com>
2021-06-11 14:13:52 -0400

    Minimize test case

--- spec/models/file_spec.rb
+++ spec/models/file_spec.rb
@@ -1,29 +1,10 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../../config/boot'
-require 'rails/all'
-Bundler.require(*Rails.groups)
+require 'mini_magick'
 
 RSpec.describe File do
   it 'does sane things with tmp files' do
     test_path = 'spec/fixtures/files/images/test_photo.jpg'
     tmp_path = '/tmp/test_photo.jpg'
     `cp #{test_path} #{tmp_path}`
-
-    # same bytes
-    src_data = File.open(test_path, 'rb', &:read)
-    dst_data = File.open(tmp_path, 'rb', &:read)
-    expect(dst_data).to eq(src_data)
-
-    # file magic groks them
-    filemagic = FileMagic.new(FileMagic::MAGIC_MIME_TYPE)
-    src_type = filemagic.buffer(src_data)
-    expect(src_type).to eq('image/jpeg')
-    dst_type = filemagic.buffer(dst_data)
-    expect(dst_type).to eq(src_type)
-
-    # image magic does too
-    expect(MiniMagick::Image.new(test_path).mime_type).to eq('image/jpeg')
-    expect(MiniMagick::Image.new(tmp_path).mime_type).to eq('image/jpeg')
 
     output = MiniMagick::Tool::Identify.new do |cmd|
       cmd << tmp_path
@@ -43,6 +24,8 @@ RSpec.describe File do
         end
 
         expect(output).to include('JPEG')
+
+        puts output
       end
     end
   end
