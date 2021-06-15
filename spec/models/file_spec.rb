3fa2406f590f694feb16cd7f622b689fade6cac7
Aaron Michal <amichal@greenriver.com>
2021-06-15 10:14:57 -0400

    Add fast-fail test for https://github.com/docker/for-linux/issues/1015

---
+++ spec/models/file_spec.rb
@@ -0,0 +1,50 @@
+require 'mini_magick'
+
+RSpec.describe File do
+  it 'does sane things with tmp files' do
+    test_path = 'spec/fixtures/files/images/test_photo.jpg'
+    tmp_path = '/tmp/test_photo.jpg'
+    `cp #{test_path} #{tmp_path}`
+
+    output = MiniMagick::Tool::Identify.new do |cmd|
+      cmd << tmp_path
+    end
+    expect(output).to include('JPEG')
+
+    File.open(test_path, binmode: true) do |file|
+      Tempfile.new(["mini_magick", '.jpg']).tap do |tempfile|
+        tempfile.binmode
+
+        IO.copy_stream(file, tempfile)
+
+        output = MiniMagick::Tool::Identify.new do |cmd|
+          cmd << tempfile.path
+        end
+
+        expect(output).to include('JPEG')
+
+        puts output
+
+        tempfile.close
+      end
+    end
+
+    File.open(tmp_path, binmode: true) do |file|
+      Tempfile.new(["mini_magick", '.jpg']).tap do |tempfile|
+        tempfile.binmode
+
+        # https://github.com/docker/for-linux/issues/1015 work around
+        file.chmod file.lstat.mode
+        IO.copy_stream(file, tempfile)
+
+        output = MiniMagick::Tool::Identify.new do |cmd|
+          cmd << tempfile.path
+        end
+
+        expect(output).to include('JPEG')
+
+        puts output
+      end
+    end
+  end
+end
