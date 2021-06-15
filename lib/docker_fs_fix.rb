52ca6b69f57d8a6d28673ecb5e2037a7e74bd97a
Aaron Michal <amichal@greenriver.com>
2021-06-15 15:32:49 -0400

    See if we can get away with the monkey-patch

--- lib/docker_fs_fix.rb
+++ lib/docker_fs_fix.rb
@@ -1,5 +1,6 @@
 # https://github.com/docker/for-linux/issues/1015
 
+puts "Patching https://github.com/docker/for-linux/issues/1015"
 module FileUtilsPatch
   def copy_file(dest)
     FileUtils.touch(path())
