dc6edb98bb02f9f4a49990e6659abc97826b5441
Aaron Michal <amichal@greenriver.com>
2021-06-15 15:00:40 -0400

    Restore test layout

--- spec/factories/grda_warehouse/ad_hoc_batch.rb
+++ spec/factories/grda_warehouse/ad_hoc_batch.rb
@@ -3,18 +3,12 @@ require 'docker_fs_fix'
 FactoryBot.define do
   factory :ad_hoc_batch_valid, class: 'GrdaWarehouse::AdHocBatch' do
     description { 'Ad-Hoc Batch' }
-    file do
-      DockerFsFix.upload Rack::Test::UploadedFile.new(
-        Rails.root.join('spec/fixtures/files/ad_hoc_batches/initial_batch.csv'), 'text/plain'
-      )
-    end
+    file { DockerFsFix.upload Rack::Test::UploadedFile.new( Rails.root.join('spec/fixtures/files/ad_hoc_batches/initial_batch.csv'), 'text/plain') }
+    content { file.read }
   end
   factory :ad_hoc_batch_invalid, class: 'GrdaWarehouse::AdHocBatch' do
     description { 'Ad-Hoc Batch' }
-    file do
-      DockerFsFix.upload Rack::Test::UploadedFile.new(
-        Rails.root.join('spec/fixtures/files/ad_hoc_batches/invalid_batch.csv'), 'text/plain'
-      )
-    end
+    file { DockerFsFix.upload Rack::Test::UploadedFile.new( Rails.root.join('spec/fixtures/files/ad_hoc_batches/invalid_batch.csv'), 'text/plain') }
+    content { file.read }
   end
 end
