caeced7dd1999a4ea71139cd04b453bdbdf341b4
Todd Blackman <tdblackman@gmail.com>
2021-06-21 19:24:59 +0000

    Allow wss and https as protocols

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   config.action_cable.allowed_request_origins = [
-    "https://#{ENV.fetch('FQDN') { 'nowhere.openpath.host' } }"
+    "https://#{ENV.fetch('FQDN') { 'nowhere.openpath.host' } }",
+    "wss://#{ENV.fetch('FQDN') { 'nowhere.openpath.host' } }"
   ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
