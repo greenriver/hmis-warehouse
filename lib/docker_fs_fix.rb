ae206366ed86b9731ad77a94015db6907f4d429f
Todd Blackman <tdblackman@gmail.com>
2021-06-21 13:34:11 +0000

    [#178597573] Workoff tasks obey memory analyzer (dynamodb)

--- lib/docker_fs_fix.rb
+++ lib/docker_fs_fix.rb
@@ -1,6 +1,6 @@
 # https://github.com/docker/for-linux/issues/1015
 
-# puts "Patching https://github.com/docker/for-linux/issues/1015"
+puts "Patching https://github.com/docker/for-linux/issues/1015"
 module FileUtilsPatch
   def copy_file(dest)
     FileUtils.touch(path())
@@ -9,7 +9,7 @@ module FileUtilsPatch
 end
 
 module FileUtils
-  class Entry_ # rubocop:disable Naming/ClassAndModuleCamelCase
+  class Entry_
     prepend FileUtilsPatch
   end
 end
