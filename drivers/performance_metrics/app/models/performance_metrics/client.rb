5098b9a2267acf7ef38a76923b25ca99c5e14814
Elliot Anders <elliot@greenriver.org>
2021-06-22 17:08:59 -0400

    Performance metrics clients

---
+++ drivers/performance_metrics/app/models/performance_metrics/client.rb
@@ -0,0 +1,13 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module PerformanceMetrics
+  class Client < GrdaWarehouseBase
+    acts_as_paranoid
+
+    has_many :simple_reports_universe_members, inverse_of: :universe_membership, class_name: 'SimpleReports::UniverseMember', foreign_key: :universe_membership_id
+  end
+end
