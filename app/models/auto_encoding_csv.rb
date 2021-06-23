e9734d6abb4f19f81f65ed9037b24243d4cd2119
Aaron Michal <amichal@greenriver.com>
2021-06-23 12:49:10 -0400

    Use encoding detection in HMIS CSV 2020 Loader

--- app/models/auto_encoding_csv.rb
+++ app/models/auto_encoding_csv.rb
@@ -7,32 +7,36 @@ class AutoEncodingCsv < CSV
   UTF16BE_BOM = "\xFE\xFF".b.freeze
 
   class << self
+    def detect_encoding(filename)
+      # a Unicode BOM is a strong signal
+      magic = File.read(filename, 3, mode: 'rb')
+      guessed_encoding = if magic.first(3) == UTF8_BOM
+         'bom|UTF-8'
+      elsif magic.first(2) == UTF16LE_BOM
+         'bom|UTF-16LE'
+      elsif magic.first(2) == UTF16BE_BOM
+         'bom|UTF-16BE'
+      end
+
+      # no BOM we have to try for a statistical match
+      unless guessed_encoding
+        File.open(filename, mode: 'rb') do |io|
+          res = CharlockHolmes::EncodingDetector.detect(io.read)
+          guessed_encoding = res[:ruby_encoding] if res
+        end
+      end
+
+      Rails.logger.debug do
+        "AutoEncodingCsv.detect_encoding #{filename} => #{guessed_encoding}"
+      end
+
+      guessed_encoding
+    end
+
     def open(filename, mode="r", **options)
       options ||= {}
 
-      if mode.start_with?('r') && options[:encoding].nil?
-        # a Unicode BOM is a strong signal
-        magic = File.read(filename, 3, mode: 'rb')
-        guessed_encoding = if magic.first(3) == UTF8_BOM
-           'bom|UTF-8'
-        elsif magic.first(2) == UTF16LE_BOM
-           'bom|UTF-16LE'
-        elsif magic.first(2) == UTF16BE_BOM
-           'bom|UTF-16BE'
-        end
-
-        # no BOM we have to try for a statistical match
-        unless guessed_encoding
-          File.open(filename, mode: 'rb') do |io|
-            res = CharlockHolmes::EncodingDetector.detect(io.read)
-            guessed_encoding = res[:ruby_encoding] if res
-          end
-        end
-
-        options[:encoding] = guessed_encoding if guessed_encoding
-
-        $stdout.puts "#{filename} #{options}"
-      end
+      options[:encoding] = detect_encoding(filename) if mode.start_with?('r') && options[:encoding].nil?
 
       super filename, mode, **options
     end
