7dbda8818f0fa3c30f2b57dce6a227193c2748ac
Aaron Michal <amichal@greenriver.com>
2021-06-30 19:43:15 -0400

    Support more importer variations

--- spec/support/hmis_csv_fixtures.rb
+++ spec/support/hmis_csv_fixtures.rb
@@ -1,39 +1,56 @@
 module HmisCsvFixtures
-  def import_hmis_csv_fixture(file_path, version: '2020', data_source: nil, run_jobs: true)
+  def import_hmis_csv_fixture(
+    file_path,
+    data_source:,
+    version: '2020',
+    run_jobs: true,
+    allowed_projects: nil,
+    deidentified: nil
+  )
     unless data_source
       data_source = GrdaWarehouse::DataSource.where(
         name: 'Green River',
         short_name: 'GR',
         source_type: :sftp
-      ).first_or_create
+      ).first_or_create!
       GrdaWarehouse::DataSource.where(
         name: 'Warehouse',
         short_name: 'W'
-      ).first_or_create
+      ).first_or_create!
     end
 
     @data_source = data_source
     source_file_path = File.join(file_path, 'source')
 
-    # duplicate the fixture folder since the import is
-    # expecting "#{file_path}/#{data_source.id}" and to be able
-    # to tamper with its contents
+    # duplicate the fixture folder since some
+    # importers expect data in "#{file_path}/#{data_source_id}"
+    # and to be able to tamper with its contents
+    # TODO: fix importers to avoid mutating the source!
     tmp_path = File.join(file_path, data_source.id.to_s)
     FileUtils.cp_r(source_file_path, tmp_path)
 
     importer = if version == '6.11'
-      Importers::HMISSixOneOne::Bases.new(
+      Importers::HMISSixOneOne::Base.new(
         file_path: file_path,
-        data_source_id: data_source.id
+        data_source_id: data_source.id,
       )
     elsif version == '2020'
       HmisCsvTwentyTwenty::Loader::Loader.new(
         file_path: tmp_path,
-        data_source_id: data_source.id
+        data_source_id: data_source.id,
+        deidentified: deidentified,
+      )
+    elsif version == 'AutoDetect'
+      Importers::HmisAutoDetect::Local.new(
+        file_path: tmp_path,
+        data_source_id: data_source.id,
+        deidentified: deidentified,
+        allowed_projects: allowed_projects,
       )
     else
       raise "Unsupported CSV version #{version}"
     end
+
     importer.import!
     FileUtils.rm_rf(tmp_path) if tmp_path
 
@@ -41,15 +58,14 @@ module HmisCsvFixtures
       # 2020 always runs this so we dont need to do it twice
       GrdaWarehouse::Tasks::IdentifyDuplicates.new.run! unless version == '2020'
 
-
       GrdaWarehouse::Tasks::ProjectCleanup.new.run!
       GrdaWarehouse::Tasks::ServiceHistory::Add.new.run!
       AccessGroup.maintain_system_groups
 
+      Delayed::Worker.new.work_off
     end
-    Delayed::Worker.new.work_off
 
-    importer.importer_log # this is a HmisCsvTwentyTwenty::ImportLog record of what happened
+    importer
   end
 
   def cleanup_hmis_csv_fixtures
