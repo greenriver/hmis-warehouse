dd9136a3d161b99f4e2c1c394334559865d10a05
Sandy Wise <sandy@greenriver.org>
2021-07-02 15:06:14 -0400

    Get added dates from cohort client changes

--- db/warehouse/migrate/20210702144442_set_date_added_for_cohort_clients.rb
+++ db/warehouse/migrate/20210702144442_set_date_added_for_cohort_clients.rb
@@ -2,7 +2,8 @@ class SetDateAddedForCohortClients < ActiveRecord::Migration[5.2]
   def up
     PaperTrail.request(enabled: false) do # Migration can't find 'versions' table?
       GrdaWarehouse::CohortClient.find_each do |cohort_client|
-        cohort_client.update(date_added_to_cohort: cohort_client.created_at.to_date)
+        date_added = cohort_client.cohort_client_changes.where(change: 'create')&.last&.changed_at&.to_date || cohort_client.created_at.to_date
+        cohort_client.update(date_added_to_cohort: date_added)
       end
     end
   end
