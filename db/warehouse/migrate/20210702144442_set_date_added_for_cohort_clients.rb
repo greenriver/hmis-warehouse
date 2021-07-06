6b867d51fed051ee68a3256ddcff2f7871778c23
Elliot Anders <elliot@greenriver.org>
2021-07-06 09:02:14 -0400

    Password and OTP form updates; migration adjustment for cohort columns

--- db/warehouse/migrate/20210702144442_set_date_added_for_cohort_clients.rb
+++ db/warehouse/migrate/20210702144442_set_date_added_for_cohort_clients.rb
@@ -1,10 +1,8 @@
 class SetDateAddedForCohortClients < ActiveRecord::Migration[5.2]
   def up
-    PaperTrail.request(enabled: false) do # Migration can't find 'versions' table?
-      GrdaWarehouse::CohortClient.find_each do |cohort_client|
-        date_added = cohort_client.cohort_client_changes.where(change: 'create')&.last&.changed_at&.to_date || cohort_client.created_at.to_date
-        cohort_client.update(date_added_to_cohort: date_added)
-      end
+    GrdaWarehouse::CohortClient.find_each do |cohort_client|
+      date_added = cohort_client.cohort_client_changes.where(change: 'create')&.last&.changed_at&.to_date || cohort_client.created_at.to_date
+      cohort_client.update_columns(date_added_to_cohort: date_added)
     end
   end
 end
