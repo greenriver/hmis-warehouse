854ca16cdc47fb52b44b613cec1d1ea4e18247ef
Sandy Wise <sandy@greenriver.org>
2021-07-30 14:40:12 -0400

    Make test green

--- app/models/grda_warehouse/synthetic/event.rb
+++ app/models/grda_warehouse/synthetic/event.rb
@@ -60,25 +60,28 @@ module GrdaWarehouse::Synthetic
       ds = GrdaWarehouse::DataSource.find_by(short_name: data_source)
       return unless ds.present?
 
-      hud_assessment_hash = {
+      hud_event_hash = {
         EnrollmentID: enrollment.EnrollmentID,
         PersonalID: client.PersonalID,
         EventDate: event_date,
         Event: event,
         ProbSolDivRRResult: client_housed_in_a_safe_alternative,
         ReferralCaseManageAfter: enrolled_in_aftercare_project,
-        LocationCrisisOrPHHousing: location_of_crisis_or_ph_housing,
+        LocationCrisisorPHHousing: location_of_crisis_or_ph_housing,
         ReferralResult: referral_result,
         ResultDate: result_date,
+        DateCreated: source.created_at,
+        DateUpdated: source.updated_at,
+        UserID: User.setup_system_user.name,
         data_source_id: ds.id,
         synthetic: true,
       }
 
-      if hud_assessment.nil?
-        hud_assessment_hash[:EventID] = SecureRandom.uuid.gsub(/-/, '')
-        create_hud_event(hud_assessment_hash)
+      if hud_event.nil?
+        hud_event_hash[:EventID] = SecureRandom.uuid.gsub(/-/, '')
+        create_hud_event(hud_event_hash)
       else
-        hud_event.update(hud_assessment_hash)
+        hud_event.update(hud_event_hash)
       end
     end
   end
---
+++ drivers/cas_ce_events/spec/models/event_spec.rb
@@ -0,0 +1,30 @@
+###
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+
+RSpec.describe 'CasCeEvents::Synthetic::Event', type: :model do
+  let!(:data_source) { create :grda_warehouse_data_source }
+  let!(:project) { create :hud_project, data_source_id: data_source.id, ProjectType: 13 }
+  let!(:source_client) { create :hud_client, data_source_id: data_source.id }
+  let!(:enrollment) { create :hud_enrollment, data_source_id: data_source.id }
+
+  let!(:destination_data_source) { create :destination_data_source }
+  let(:destination_client) { create :hud_client, data_source_id: destination_data_source.id }
+  let!(:warehouse_client) { create :warehouse_client, source_id: source_client.id, destination_id: destination_client.id }
+
+  let!(:mapping) { create :program_to_project, project_id: project.id }
+  let!(:cas_referral_event) { create :cas_referral_event, hmis_client_id: destination_client.id, program_id: mapping.program_id, referral_date: enrollment.EntryDate }
+  let!(:cas_data_source) { create :grda_warehouse_data_source, short_name: 'CAS' }
+
+  it 'creates events from cas referral event' do
+    expect(CasCeEvents::Synthetic::Event.count).to eq(0)
+    CasCeEvents::Synthetic::Event.sync
+    expect(GrdaWarehouse::Synthetic::Event.count).to eq(1)
+    GrdaWarehouse::Synthetic::Event.first.hud_sync
+    expect(GrdaWarehouse::Hud::Event.count).to eq(1)
+  end
+end
