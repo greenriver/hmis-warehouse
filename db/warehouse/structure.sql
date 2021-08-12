8903116b1465886b2606783697285bde46100f1b
schwartzkylej <kschwartz@greenriver.com>
2021-08-12 14:22:57 -0400

    Add extensions back in to resolve rails_test failures

--- db/warehouse/structure.sql
+++ db/warehouse/structure.sql
@@ -10,43 +10,59 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: fuzzystrmatch; Type: EXTENSION; Schema: -; Owner: -
 --
 
-
-
---
---
-
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch WITH SCHEMA public;
 
 
 --
+-- Name: EXTENSION fuzzystrmatch; Type: COMMENT; Schema: -; Owner: -
 --
 
-
-
---
---
-
+COMMENT ON EXTENSION fuzzystrmatch IS 'determine similarities and distance between strings';
 
 
 --
+-- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
 --
 
-
-
---
---
-
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
 
 
 --
+-- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: -
 --
 
+COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
 
 
 --
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
 
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
+--
+-- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
 
 
 --
