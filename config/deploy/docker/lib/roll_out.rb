83e21d4be858a358c118b8c731746fce1c35c82a
schwartzkylej <kschwartz@greenriver.com>
2021-08-12 11:21:08 -0400

    Strip extension installation from initial deployments
    - Remove postgresql extensions from structure.sql files
    - Update regex to allow shortened production staging matching
    - Update deploy tasks to ensure sql extensions are removed

--- config/deploy/docker/lib/roll_out.rb
+++ config/deploy/docker/lib/roll_out.rb
@@ -68,9 +68,9 @@ class RollOut
       exit
     end
 
-    if target_group_name.match?(/production/)
+    if target_group_name.match?(/production|prd/)
       self.rails_env = 'production'
-    elsif target_group_name.match?(/staging/)
+    elsif target_group_name.match?(/staging|stg/)
       self.rails_env = 'staging'
     else
       raise "Cannot figure out environment from target_group_name!"
