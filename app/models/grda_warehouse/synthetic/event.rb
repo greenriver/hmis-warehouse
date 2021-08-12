8d420f4257fcff51b3cc93dbcf214d6b97b17106
Elliot Anders <elliot@greenriver.org>
2021-08-12 12:48:12 -0400

    Undo accidental chage

--- app/models/grda_warehouse/synthetic/event.rb
+++ app/models/grda_warehouse/synthetic/event.rb
@@ -82,7 +82,7 @@ module GrdaWarehouse::Synthetic
         Event: event,
         ProbSolDivRRResult: client_housed_in_a_safe_alternative,
         ReferralCaseManageAfter: enrolled_in_aftercare_project,
-        LocationCrisisOrPHHousing: location_of_crisis_or_ph_housing,
+        LocationCrisisorPHHousing: location_of_crisis_or_ph_housing, # NOTE: case should LocationCrisisOrPHHousing
         ReferralResult: referral_result,
         ResultDate: result_date,
         DateCreated: source.created_at,
