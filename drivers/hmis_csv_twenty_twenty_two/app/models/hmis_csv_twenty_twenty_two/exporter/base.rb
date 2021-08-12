e71a2cd789a35b2070a100ec42d5eea1369d24d3
Sandy Wise <sandy@greenriver.org>
2021-08-12 11:49:06 -0400

    Move generic exporter funcs into concerns

--- drivers/hmis_csv_twenty_twenty_two/app/models/hmis_csv_twenty_twenty_two/exporter/base.rb
+++ drivers/hmis_csv_twenty_twenty_two/app/models/hmis_csv_twenty_twenty_two/exporter/base.rb
@@ -8,8 +8,9 @@ require 'zip'
 require 'csv'
 module HmisCsvTwentyTwentyTwo::Exporter
   class Base
-    include NotifierConfig
     include ArelHelper
+    include ::Export::Exporter
+    include ::Export::Scopes
 
     attr_accessor :logger, :notifier_config, :file_path, :version, :export
 
@@ -87,56 +88,6 @@ module HmisCsvTwentyTwentyTwo::Exporter
         reset_time_format
       end
       @export
-    end
-
-    def set_time_format
-      # We need this for exporting to the appropriate format
-      @default_date_format = Date::DATE_FORMATS[:default]
-      @default_time_format = Time::DATE_FORMATS[:default]
-      Date::DATE_FORMATS[:default] = '%Y-%m-%d'
-      Time::DATE_FORMATS[:default] = '%Y-%m-%d %H:%M:%S'
-    end
-
-    def reset_time_format
-      Date::DATE_FORMATS[:default] = @default_date_format
-      Time::DATE_FORMATS[:default] = @default_time_format
-    end
-
-    def save_fake_data
-      return unless @faked_pii
-
-      @export.fake_data.save
-    end
-
-    def zip_path
-      @zip_path ||= File.join(@file_path, "#{@export.export_id}.zip")
-    end
-
-    def csv_file_path(klass)
-      File.join(@file_path, klass.hud_csv_file_name(version: version))
-    end
-
-    def upload_zip
-      @export.file = Pathname.new(zip_path).open
-      @export.content_type = @export.file.content_type
-      @export.content = @export.file.read
-      @export.save
-    end
-
-    def zip_archive
-      files = Dir.glob(File.join(@file_path, '*')).map { |f| File.basename(f) }
-      Zip::File.open(zip_path, Zip::File::CREATE) do |zipfile|
-        files.each do |filename|
-          zipfile.add(
-            File.join(@export.export_id, filename),
-            File.join(@file_path, filename),
-          )
-        end
-      end
-    end
-
-    def remove_export_files
-      FileUtils.rmtree(@file_path) if File.exist? @file_path
     end
 
     def export_projects
@@ -327,99 +278,6 @@ module HmisCsvTwentyTwentyTwo::Exporter
         path: @file_path,
         export: @export,
       )
-    end
-
-    def enrollment_scope
-      @enrollment_scope ||= begin
-        # Choose all enrollments open during the range at one of the associated projects.
-        if @export.include_deleted
-          e_scope = enrollment_source.with_deleted
-        else
-          e_scope = enrollment_source.joins(:client)
-        end
-        e_scope = e_scope.where(project_exists_for_enrollment)
-        case @export.period_type
-        when 3
-          e_scope = e_scope.open_during_range(@range)
-        when 1
-          # no-op
-        end
-        e_scope
-      end
-    end
-
-    def client_scope
-      # include any client with an open enrollment
-      # during the report period in one of the involved projects
-      @client_scope ||= begin
-        if @export.include_deleted
-          c_scope = client_source.with_deleted
-        else
-          c_scope = client_source
-        end
-        c_scope.joins(:warehouse_client_source).
-          where(enrollment_exists_for_client)
-      end
-    end
-
-    def project_scope
-      @project_scope ||= begin
-        p_scope = project_source.where(id: @projects)
-        p_scope = p_scope.with_deleted if @export.include_deleted
-        p_scope
-      end
-    end
-
-    def enrollment_exists_for_client
-      if @export.include_deleted
-        e_scope = enrollment_source.with_deleted
-      else
-        e_scope = enrollment_source
-      end
-      case @export.period_type
-      when 3
-        e_scope = e_scope.open_during_range(@range)
-      when 1
-        # no-op
-      end
-      e_scope.where(
-        e_t[:PersonalID].eq(c_t[:PersonalID]).
-        and(e_t[:data_source_id].eq(c_t[:data_source_id])),
-      ).where(
-        project_exists_for_enrollment,
-      ).arel.exists
-    end
-
-    def project_exists_for_enrollment
-      project_scope.where(
-        p_t[:ProjectID].eq(e_t[:ProjectID]).
-        and(p_t[:data_source_id].eq(e_t[:data_source_id])),
-      ).arel.exists
-    end
-
-    def setup_export
-      options = {
-        user_id: @user&.id,
-        start_date: @range.start,
-        end_date: @range.end,
-        period_type: @period_type,
-        directive: @directive,
-        hash_status: @hash_status,
-        faked_pii: @faked_pii,
-        project_ids: @projects,
-        include_deleted: @include_deleted,
-        version: @version,
-      }
-      options[:export_id] = Digest::MD5.hexdigest(options.to_s)[0..31]
-
-      @export = GrdaWarehouse::HmisExport.create(options)
-      @export.fake_data = GrdaWarehouse::FakeData.where(environment: @faked_environment).first_or_create
-    end
-
-    def create_export_directory
-      # make sure the path is clean
-      FileUtils.rmtree(@file_path) if File.exist? @file_path
-      FileUtils.mkdir_p(@file_path)
     end
 
     def build_export_file
@@ -657,11 +515,6 @@ module HmisCsvTwentyTwentyTwo::Exporter
 
     def user_source
       self.class.user_source
-    end
-
-    def log(message)
-      @notifier&.ping message
-      logger.info message if @debug
     end
   end
 end
