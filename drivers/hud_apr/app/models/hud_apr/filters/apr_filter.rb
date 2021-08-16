20165f8c9fc8d770d502074e15cf0212025acd16
Sandy Wise <sandy@greenriver.org>
2021-08-16 10:02:35 -0400

    DRY up HUD report filters

--- drivers/hud_apr/app/models/hud_apr/filters/apr_filter.rb
+++ drivers/hud_apr/app/models/hud_apr/filters/apr_filter.rb
@@ -5,24 +5,9 @@
 ###
 
 module HudApr::Filters
-  class AprFilter < ::Filters::FilterBase
-    validates_presence_of :coc_codes
+  class AprFilter < ::Filters::HudFilterBase
     validate do
       errors.add(:project_ids, 'or project groups must be specified') if project_ids.blank? && project_group_ids.blank?
-    end
-
-    # NOTE: This differs from the base filter class because it doesn't include any projects based on CoCCode
-    def effective_project_ids
-      @effective_project_ids = effective_project_ids_from_projects
-      @effective_project_ids += effective_project_ids_from_project_groups
-      @effective_project_ids += effective_project_ids_from_organizations
-      @effective_project_ids += effective_project_ids_from_data_sources
-      @effective_project_ids += effective_project_ids_from_project_types unless uses_default_project_type_codes
-
-      # Add project ids by type if there aren't any projects selected
-      @effective_project_ids += effective_project_ids_from_project_types if @effective_project_ids.empty?
-
-      @effective_project_ids.uniq.reject(&:blank?)
     end
   end
 end
