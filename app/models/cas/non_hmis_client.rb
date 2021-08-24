b66f383ab25bf05c20a541d70d84cb21e0cf2ce9
Elliot Anders <elliot@greenriver.org>
2021-08-24 16:51:46 -0400

    Fixes for errors

--- app/models/cas/non_hmis_client.rb
+++ app/models/cas/non_hmis_client.rb
@@ -27,9 +27,9 @@ module Cas
         to_h
 
       candidates.each_value do |candidate|
-        candidate[:lc_first_name] = candidate[:first_name].downcase.strip.gsub(/[^a-z0-9]/i, '')
-        candidate[:lc_last_name] = candidate[:last_name].downcase.strip.gsub(/[^a-z0-9]/i, '')
-        candidate[:ssn] = candidate[:ssn].strip.gsub(/[^0-9]/, '')
+        candidate[:lc_first_name] = candidate[:first_name]&.downcase&.strip&.gsub(/[^a-z0-9]/i, '')
+        candidate[:lc_last_name] = candidate[:last_name]&.downcase&.strip&.gsub(/[^a-z0-9]/i, '')
+        candidate[:ssn] = candidate[:ssn]&.strip&.gsub(/[^0-9]/, '')
       end
 
       by_first_name = candidates.reject { |_, h| h[:lc_first_name].blank? }.group_by { |_, h| h[:lc_first_name] }.transform_values { |c| c.flatten.first }
@@ -37,26 +37,34 @@ module Cas
       by_dob = candidates.reject { |_, h| h[:date_of_birth].blank? }.group_by { |_, h| h[:date_of_birth] }.transform_values { |c| c.flatten.first }
       by_ssn = candidates.reject { |_, h| h[:ssn].blank? }.group_by { |_, h| h[:ssn] }.transform_values { |c| c.flatten.first }
 
-      first_name_matches = client_source.where(c_t[:FirstName].lower.in(by_first_name.keys)).
-        pluck(c_t[:FirstName].lower, :id).
-        group_by(&:first).
-        transform_values { |v| v.map(&:last) }
-      last_name_matches = client_source.where(c_t[:LastName].lower.in(by_last_name.keys)).
-        pluck(c_t[:LastName].lower, :id).
-        group_by(&:first).
-        transform_values { |v| v.map(&:last) }
+      first_name_matches = {}
+      client_source.where(c_t[:FirstName].lower.in(by_first_name.keys)).
+        pluck(c_t[:FirstName].lower, :id).each do |first_name, id|
+        first_name_matches[first_name] ||= []
+        first_name_matches[first_name] << id
+      end
+      last_name_matches = {}
+      client_source.where(c_t[:LastName].lower.in(by_last_name.keys)).
+        pluck(c_t[:LastName].lower, :id).each do |last_name, id|
+        last_name_matches[last_name] ||= []
+        last_name_matches[last_name] << id
+      end
       db_names = client_source.where(id: (first_name_matches.values + last_name_matches.values).flatten).
         pluck(:id, :FirstName, :LastName).
         map { |id, fn, ln| [id, "#{fn} #{ln}"] }.
         to_h
-      dob_matches = client_source.where(DOB: by_dob.keys).
-        pluck(:DOB, :id).
-        group_by(&:first).
-        transform_values { |v| v.map(&:last) }
-      ssn_matches = client_source.where(SSN: by_ssn.keys).
-        pluck(:SSN, :id).
-        group_by(&:first).
-        transform_values { |v| v.map(&:last) }
+      dob_matches = {}
+      client_source.where(DOB: by_dob.keys).
+        pluck(:DOB, :id).each do |dob, id|
+        dob_matches[dob] ||= []
+        dob_matches[dob] << id
+      end
+      ssn_matches = {}
+      client_source.where(SSN: by_ssn.keys).
+        pluck(:SSN, :id).each do |ssn, id|
+        ssn_matches[ssn] ||= []
+        ssn_matches[ssn] << id
+      end
 
       OpenStruct.new(
         candidates: candidates,
@@ -71,10 +79,11 @@ module Cas
     # An "obvious match" from IdentifyDuplicates
     def self.exact_match(candidate, matches)
       ssn_matches = []
-      ssn_matches = matches.ssn_matches[candidate[:ssn]] if valid_social?(candidate[:ssn])
+      ssn_matches += matches.ssn_matches[candidate[:ssn]] || [] if valid_social?(candidate[:ssn])
       birthdate_matches = []
-      birthdate_matches = matches.dob_matches[candidate[:date_of_birth]] if candidate[:date_of_birth].present?
-      name_matches = (matches.first_name_matches[candidate[:lc_first_name]] || []) & (matches.last_name_matches[candidate[:lc_last_name]] || [])
+      birthdate_matches += matches.dob_matches[candidate[:date_of_birth]] || [] if candidate[:date_of_birth].present?
+      name_matches = []
+      name_matches += (matches.first_name_matches[candidate[:lc_first_name]] || []) & (matches.last_name_matches[candidate[:lc_last_name]] || [])
 
       all_matches = ssn_matches + birthdate_matches + name_matches
       exact_matches = all_matches.uniq.select { |i| all_matches.count(i) > 1 }.compact
@@ -90,12 +99,14 @@ module Cas
 
     # SSN or 2 of first, last, DOB...
     def self.partial_matches(candidate, matches)
-      partial_matches = matches.ssn_matches[candidate[:ssn]]
+      partial_matches = matches.ssn_matches[candidate[:ssn]] || []
       MATCH_TYPES.each do |first_type, first_column|
         MATCH_TYPES.each do |second_type, second_column|
           next if first_type == second_type
 
-          partial_matches += matches.send(first_type)[candidate[first_column]] & matches.send(second_type)[candidate[second_column]]
+          col_one = matches.send(first_type)[candidate[first_column]] || []
+          col_two = matches.send(second_type)[candidate[second_column]] || []
+          partial_matches += col_one & col_two
         rescue TypeError
           next
         end
