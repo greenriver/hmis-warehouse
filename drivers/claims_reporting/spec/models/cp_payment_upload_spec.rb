23bdc78c74ed7d1d409c6a50d9cb839bbe81111e
Sandy Wise <sandy@greenriver.org>
2021-08-26 10:26:23 -0400

    Make cp_payment headers case insensitive

---
+++ drivers/claims_reporting/spec/models/cp_payment_upload_spec.rb
@@ -0,0 +1,18 @@
+# Copyright 2016 - 2021 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+
+describe ClaimsReporting::CpPaymentUpload, model: :type do
+  before(:each) do
+    File.open('drivers/claims_reporting/spec/fixtures/files/cp_payment_upload.xlsx') do |xlsx|
+      @upload = described_class.create(content: xlsx.read)
+    end
+  end
+
+  it 'ignores case in headers' do
+    expect(@upload.content_as_details).not_to be_nil
+  end
+end
