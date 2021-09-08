02e7a0649a1b85302354e84a54bf60510a78e15a
Elliot Anders <elliot@greenriver.org>
2021-09-08 17:10:32 -0400

    Schema update

--- db/warehouse/structure.sql
+++ db/warehouse/structure.sql
@@ -218,7 +218,7 @@ CREATE FUNCTION public.service_history_service_insert_trigger() RETURNS trigger
             INSERT INTO service_history_services_2001 VALUES (NEW.*);
          ELSIF  ( NEW.date BETWEEN DATE '2000-01-01' AND DATE '2000-12-31' ) THEN
             INSERT INTO service_history_services_2000 VALUES (NEW.*);
-
+        
       ELSE
         INSERT INTO service_history_services_remainder VALUES (NEW.*);
         END IF;
@@ -31578,6 +31578,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210901200255'),
 ('20210902113909'),
 ('20210903113401'),
-('20210904021301');
 ('20210904021301'),
 ('20210906163956');
+
+
