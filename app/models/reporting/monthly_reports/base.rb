268fa9f1e224a792a7f31375c2f08f229e36562f
Elliot Anders <elliot@greenriver.org>
2021-09-21 08:43:25 -0400

    API cleanup for monthly report generation

--- app/models/reporting/monthly_reports/base.rb
+++ app/models/reporting/monthly_reports/base.rb
@@ -50,8 +50,8 @@ module Reporting::MonthlyReports
         distinct.
         pluck_in_batches(:client_id, batch_size: 2_500) do |batch|
           batch = batch.flatten
-          set_enrollments_by_client(batch)
-          set_prior_enrollments
+          @enrollments_by_client = enrollments_by_client(batch)
+          apply_prior_enrollments(@enrollments_by_client)
           self.class.transaction do
             _clear!(batch)
             self.class.import @enrollments_by_client.values.flatten
@@ -107,59 +107,60 @@ module Reporting::MonthlyReports
 
     # Group clients by month and client_id
     # Loop over all of the open enrollments,
-    def set_enrollments_by_client ids # rubocop:disable Naming/AccessorMethodName
+    def enrollments_by_client ids
       clear_batch_cache
       # Cleanup RAM before starting the next batch
       GC.start
-      @date_range.map { |d| [d.year, d.month] }.uniq.each do |year, month|
-        # fetch open enrollments for the given month
-        enrollment_scope(start_date: Date.new(year, month, 1), end_date: Date.new(year, month, -1)).
-          joins(:project, :organization).
-          where(client_id: ids).
-          pluck(*enrollment_columns).map do |row|
-            OpenStruct.new(enrollment_columns.zip(row).to_h)
-          end.each do |enrollment|
-          entry_month = enrollment.first_date_in_program.month
-          entry_year = enrollment.first_date_in_program.year
-          exit_month = enrollment.last_date_in_program&.month
-          exit_year = enrollment.last_date_in_program&.year
-          client_id = enrollment.client_id
+      {}.tap do |en_by_client|
+        @date_range.map { |d| [d.year, d.month] }.uniq.each do |year, month|
+          # fetch open enrollments for the given month
+          enrollment_scope(start_date: Date.new(year, month, 1), end_date: Date.new(year, month, -1)).
+            joins(:project, :organization).
+            where(client_id: ids).
+            pluck(*enrollment_columns).map do |row|
+              OpenStruct.new(enrollment_columns.zip(row).to_h)
+            end.each do |enrollment|
+            entry_month = enrollment.first_date_in_program.month
+            entry_year = enrollment.first_date_in_program.year
+            exit_month = enrollment.last_date_in_program&.month
+            exit_year = enrollment.last_date_in_program&.year
+            client_id = enrollment.client_id
 
-          entered_in_month = entry_month == month && entry_year == year
-          exited_in_month = exit_month.present? && exit_month == month && exit_year == year
-          mid_month = Date.new(year, month, 15)
+            entered_in_month = entry_month == month && entry_year == year
+            exited_in_month = exit_month.present? && exit_month == month && exit_year == year
+            mid_month = Date.new(year, month, 15)
 
-          client_enrollment = self.class.new(
-            mid_month: mid_month,
-            month: month,
-            year: year,
-            client_id: client_id,
-            age_at_entry: enrollment[:age],
-            enrollment_id: enrollment.id,
-            head_of_household: enrollment[:head_of_household],
-            household_id: enrollment.household_id.presence || "c_#{client_id}",
-            destination_id: enrollment.destination,
-            enrolled: true, # everyone will be enrolled
-            active: active_in_month?(client_id: client_id, project_type: enrollment.computed_project_type, month: month, year: year, batch: ids),
-            entered: entered_in_month,
-            exited: exited_in_month,
-            project_id: project_id(enrollment.project_id, enrollment.data_source_id),
-            organization_id: organization_id(enrollment.organization_id, enrollment.data_source_id),
-            project_type: enrollment.computed_project_type,
-            entry_date: enrollment.first_date_in_program,
-            exit_date: enrollment.last_date_in_program,
-            first_enrollment: first_record?(enrollment),
-            days_since_last_exit: nil,
-            prior_exit_project_type: nil,
-            prior_exit_destination_id: nil,
+            client_enrollment = self.class.new(
+              mid_month: mid_month,
+              month: month,
+              year: year,
+              client_id: client_id,
+              age_at_entry: enrollment[:age],
+              enrollment_id: enrollment.id,
+              head_of_household: enrollment[:head_of_household],
+              household_id: enrollment.household_id.presence || "c_#{client_id}",
+              destination_id: enrollment.destination,
+              enrolled: true, # everyone will be enrolled
+              active: active_in_month?(client_id: client_id, project_type: enrollment.computed_project_type, month: month, year: year, batch: ids),
+              entered: entered_in_month,
+              exited: exited_in_month,
+              project_id: project_id(enrollment.project_id, enrollment.data_source_id),
+              organization_id: organization_id(enrollment.organization_id, enrollment.data_source_id),
+              project_type: enrollment.computed_project_type,
+              entry_date: enrollment.first_date_in_program,
+              exit_date: enrollment.last_date_in_program,
+              first_enrollment: first_record?(enrollment),
+              days_since_last_exit: nil,
+              prior_exit_project_type: nil,
+              prior_exit_destination_id: nil,
 
-            calculated_at: Time.zone.now,
-          )
-          @enrollments_by_client[client_id] ||= []
-          @enrollments_by_client[client_id] << client_enrollment
+              calculated_at: Time.zone.now,
+            )
+            en_by_client[client_id] ||= []
+            en_by_client[client_id] << client_enrollment
+          end
         end
       end
-      @enrollments_by_client
     end
 
     def enrollment_columns
@@ -182,8 +183,8 @@ module Reporting::MonthlyReports
     # By client, for each enrollment that is an entry in the month,
     # figure out the most recent exit (where there wasn't an ongoing enrollment)
     # and populate the days_since_last_exit and prior_exit_project_type as appropriate
-    def set_prior_enrollments
-      @enrollments_by_client.each do |_client_id, enrollments|
+    def apply_prior_enrollments(en_by_client)
+      en_by_client.each do |_client_id, enrollments|
         # find the next enrollment where entered == true
         # If all other enrollments in the current month are exits and the max exit date is
         # before the entry date, make note.
@@ -262,7 +263,7 @@ module Reporting::MonthlyReports
 
     def actives_in_month batch:
       @actives_in_month ||= begin
-        acitives = {}
+        actives = {}
         GrdaWarehouse::ServiceHistoryService.homeless.
           where(client_id: batch).
           service_within_date_range(start_date: @start_date, end_date: @end_date).
@@ -274,10 +275,10 @@ module Reporting::MonthlyReports
             Arel.sql(cast(datepart(GrdaWarehouse::ServiceHistoryService, 'month', shs_t[:date]), 'INTEGER').to_sql),
             Arel.sql(cast(datepart(GrdaWarehouse::ServiceHistoryService, 'year', shs_t[:date]), 'INTEGER').to_sql),
           ).each do |id, project_type, month, year|
-          acitives[id] ||= []
-          acitives[id] << [year, month, project_type]
+          actives[id] ||= []
+          actives[id] << [year, month, project_type]
         end
-        acitives
+        actives
       end
     end
 
