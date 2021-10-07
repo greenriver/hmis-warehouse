68e0c9797517fe7dcedaf772988b9ab7ed1cef8b
Elliot Anders <elliot@greenriver.org>
2021-10-07 12:38:43 -0400

    We're only adding marginalia for the dev environment for now, only set config there as well.

--- config/initializers/marginalia.rb
+++ config/initializers/marginalia.rb
@@ -1,3 +1,5 @@
-Marginalia::Comment.components = [:application, :controller, :action, :line]
-Marginalia::Comment.prepend_comment = true
-Marginalia::Comment.lines_to_ignore = /^(?!.*\/app\/.*)/
+if Rails.env.development?
+  Marginalia::Comment.components = [:application, :controller, :action, :line]
+  Marginalia::Comment.prepend_comment = true
+  Marginalia::Comment.lines_to_ignore = /^(?!.*\/app\/.*)/
+end
