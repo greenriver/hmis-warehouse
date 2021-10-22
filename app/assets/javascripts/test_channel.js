69bce1a70a0c04318dda56c06e53809b5d2484d3
Elliot Anders <elliot@greenriver.org>
2021-10-22 14:39:15 -0400

    Testing cleaner test channel

--- app/assets/javascripts/test_channel.js
+++ app/assets/javascripts/test_channel.js
@@ -4,26 +4,22 @@
   this.App || (this.App = {});
 
   this.App.initTestChannel = function() {
-    console.log('Subscribing to test channel')
+    console.log('Subscribing to test channel');
 
     App.cable.subscriptions.create({ channel: "TestChannel" }, {
       received(data) {
-        console.log('received')
-        this.appendLine(data)
+        console.log('received');
+        this.appendLine(data);
       },
 
       appendLine(data) {
-        const html = this.createLine(data)
-        const element = document.querySelector("[data-actioncable='here']")
-        element.insertAdjacentHTML("beforeend", html)
+        const html = this.createLine(data);
+        const element = document.querySelector("[data-actioncable='here']");
+        element.insertAdjacentHTML("beforeend", html);
       },
 
       createLine(data) {
-        return `
-          <article class="test-line">
-            <span class="body">${data["message"]}</span>
-          </article>
-        `
+        return '<article class="test-line"><span class="body">' + data["message"] + '</span></article>';
       }
     })
   }
