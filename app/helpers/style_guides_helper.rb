f8800ea802517458c5596fea15c3e69ba98b8860
Jonathan Goley <jgoley@gmail.com>
2021-10-29 12:50:11 -0400

    [#179904096] Update patient card layout and tooltips

--- app/helpers/style_guides_helper.rb
+++ app/helpers/style_guides_helper.rb
@@ -5,15 +5,26 @@
 ###
 
 module StyleGuidesHelper
-  def detail_block title:, value:, wrapper_classes: []
-    base_class = "c-detail"
+  def detail_block title:, value:, wrapper_classes: [], tooltip: nil
+    base_class = "c-detail-block"
     title_el = tag.div(title, class: "#{base_class}__title")
+    value_tooltip_attrs = {}
+    if tooltip
+      value_tooltip_attrs = {
+        title: tooltip,
+        data: { toggle: "tooltip", html: "true" }
+      }
+    end
     value_el = tag.div(value, class: "#{base_class}__value")
-    tag.div title_el + value_el, class: [ base_class ] + wrapper_classes.map{ |c| "#{base_class}--#{c}" }
+    attrs = value_tooltip_attrs.merge( class: [ base_class ] + wrapper_classes.map{ |c| "#{base_class}--#{c}" })
+    tag.div title_el + value_el, **attrs
   end
 
-  def calendar_details_title_attr(metadata)
+  def details_title_attr(metadata)
     return "" unless metadata
-    sanitize(metadata.to_a.join(': ').to_s)
+    title = nil
+    details = metadata.map { |k, v| "<li>#{k.to_s.titleize}: <b>#{v}</b></li>" }
+    title = "<ul class='list-unstyled mb-0'>#{details.join('')}</ul>"
+    title
   end
 end
