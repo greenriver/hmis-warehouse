8721da3ae71f8e52b375dab6fbf85bbf16bef48c
Sandy Wise <sandy@greenriver.org>
2021-11-16 13:26:37 -0500

    DRY up patient dashboard controllers

--- app/controllers/health/my_patients_controller.rb
+++ app/controllers/health/my_patients_controller.rb
@@ -10,6 +10,7 @@ module Health
     before_action :require_user_has_health_agency!
     before_action :set_patients
     include ClientPathGenerator
+    include HealthPatientDashboard
 
     def index
       @q = @patients.ransack(params[:q])
@@ -99,46 +100,6 @@ module Health
             merge(Health::PatientReferral.pending_disenrollment.not_confirmed_rejected).
             where.not(id: patient_ids_with_payable_qas_in_month),
         )
-    end
-
-    def sort_options
-      sort_options = [
-        {
-          column: 'name',
-          direction: :asc,
-          title: 'Name (last, first) A-Z',
-        },
-        {
-          column: 'name',
-          direction: :desc,
-          title: 'Name (last, first) Z-A',
-        },
-      ]
-
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        dashboard_sort_options = dashboard[:calculator].constantize.dashboard_sort_options
-        sort_options << dashboard_sort_options if dashboard_sort_options.present?
-      end
-
-      sort_options
-    end
-    helper_method :sort_options
-
-    def calculate_dashboards(medicaid_ids)
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        [
-          dashboard[:title],
-          dashboard[:calculator].constantize.new(medicaid_ids).to_map,
-        ]
-      end.to_h
-    end
-
-    def determine_sort_order(medicaid_ids, column, direction)
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        sort_order = dashboard[:calculator].constantize.new(medicaid_ids).sort_order(column, direction)
-        return sort_order if sort_order.present?
-      end
-      raise 'Unknown sort column'
     end
 
     def set_patients
--- app/controllers/health/patients_controller.rb
+++ app/controllers/health/patients_controller.rb
@@ -14,6 +14,7 @@ module Health
 
     include ClientPathGenerator
     include AjaxModalRails::Controller
+    include HealthPatientDashboard
 
     def index
       @q = @patients.ransack(params[:q])
@@ -78,46 +79,6 @@ module Health
         merge(Health::Agency.where(id: @active_agency.id)).
         merge(Health::PatientReferral.not_confirmed_rejected).
         distinct
-    end
-
-    def sort_options
-      sort_options = [
-        {
-          column: 'name',
-          direction: :asc,
-          title: 'Name (last, first) A-Z',
-        },
-        {
-          column: 'name',
-          direction: :desc,
-          title: 'Name (last, first) Z-A',
-        },
-      ]
-
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        dashboard_sort_options = dashboard[:calculator].constantize.dashboard_sort_options
-        sort_options << dashboard_sort_options if dashboard_sort_options.present?
-      end
-
-      sort_options
-    end
-    helper_method :sort_options
-
-    def calculate_dashboards(medicaid_ids)
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        [
-          dashboard[:title],
-          dashboard[:calculator].constantize.new(medicaid_ids).to_map,
-        ]
-      end.to_h
-    end
-
-    def determine_sort_order(medicaid_ids, column, direction)
-      Rails.application.config.patient_dashboards.map do |dashboard|
-        sort_order = dashboard[:calculator].constantize.new(medicaid_ids).sort_order(column, direction)
-        return sort_order if sort_order.present?
-      end
-      raise 'Unknown sort column'
     end
 
     def set_patients
