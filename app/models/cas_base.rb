4334b55fadf2082a7aa9ce6986d685f37d773c3e
Todd Blackman <tdblackman@gmail.com>
2021-11-19 20:32:13 +0000

    Attempt to make casbase work correctly

--- app/models/cas_base.rb
+++ app/models/cas_base.rb
@@ -4,19 +4,31 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-class CasBase < ActiveRecord::Base
-  self.abstract_class = true
-  connects_to database: { writing: :cas, reading: :cas}
+if ENV['DATABASE_CAS_DB'].present?
 
-  def self.db_exists?
-    return false unless ENV['DATABASE_CAS_DB'].present?
+  class CasBase < ActiveRecord::Base
+    self.abstract_class = true
 
-    mem_cache.fetch('cas_db_exists', expires_in: 2.minutes) do
-      connection_pool.with_connection(&:active?) rescue false # rubocop:disable Style/RescueModifier
+    connects_to database: { writing: :cas, reading: :cas}
+
+    def self.db_exists?
+      mem_cache.fetch('cas_db_exists', expires_in: 2.minutes) do
+        connection_pool.with_connection(&:active?) rescue false # rubocop:disable Style/RescueModifier
+      end
+    end
+
+    def self.mem_cache
+      @mem_cache ||= ActiveSupport::Cache::MemoryStore.new
     end
   end
 
-  def self.mem_cache
-    @mem_cache ||= ActiveSupport::Cache::MemoryStore.new
+else
+
+  class CasBase
+    self.abstract_class = true
+
+    def self.db_exists?
+      false
+    end
   end
 end
