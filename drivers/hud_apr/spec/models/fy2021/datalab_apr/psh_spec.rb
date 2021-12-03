a08ed48764125af285bf42d75546551ac2c7efa2
Sandy Wise <sandy@greenriver.org>
2021-12-03 10:03:22 -0500

    Q25e swap stayers and leavers

--- drivers/hud_apr/spec/models/fy2021/datalab_apr/psh_spec.rb
+++ drivers/hud_apr/spec/models/fy2021/datalab_apr/psh_spec.rb
@@ -362,12 +362,6 @@ RSpec.describe 'Datalab 2021 APR - PSH', type: :model do
     compare_results(
       file_path: result_file_prefix + 'psh',
       question: 'Q25e',
-      skip: [
-        'C4', # FIXME
-        'D4', # FIXME
-        'C6', # FIXME
-        'C9', # FIXME
-      ],
     )
   end
 
