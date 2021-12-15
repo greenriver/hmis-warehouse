c614f8f136a5705b05a5461f6bf1903b2f239643
Andrew Zito <zitoa@lafayette.edu>
2021-12-15 12:37:38 -0500

    Wrap find_log_stream in a timeout

--- config/initializers/aws_cloudwatch.rb
+++ config/initializers/aws_cloudwatch.rb
@@ -1,45 +1,47 @@
 Rails.logger.debug "Running initializer in #{__FILE__}"
 
 def find_log_stream_name
-  log_group = ENV.fetch('TARGET_GROUP_NAME', nil)
+  Timeout::timeout(15) do
+    log_group = ENV.fetch('TARGET_GROUP_NAME', nil)
 
-  if log_group.nil?
-    Rails.logger.info 'TARGET_GROUP_NAME not set, bailing out'
-    return nil
-  end
-
-  begin
-    task_meta = Net::HTTP.get( URI( "#{ENV['ECS_CONTAINER_METADATA_URI_V4']}/task" ) )
-    task_arn  = JSON.parse(task_meta)["TaskARN"]
-    task_id   = task_arn.split('/').last
-  rescue => e
-    Rails.logger.error 'Something broke when querying for the ECS task id.'
-    Rails.logger.error e.message
-  end
-
-  logs ||= Aws::CloudWatchLogs::Client.new
-  log_stream = nil
-  next_token = nil
-  for i in 1..25 do # Limit to 25 requests so we're not endlessly searching.
-    begin
-      response = logs.describe_log_streams({
-        log_group_name: log_group,
-        order_by: 'LastEventTime',
-        descending: true,
-        next_token: next_token,
-      })
-    rescue Aws::CloudWatchLogs::Errors::ThrottlingException => e
-      Rails.logger.error 'Throttling exception encountered when searching for log stream.'
+    if log_group.nil?
+      Rails.logger.info 'TARGET_GROUP_NAME not set, bailing out'
       return nil
     end
 
-    log_streams = response.log_streams
-    log_stream = log_streams.find { |s| s.log_stream_name.include?(task_id) }
-    if log_stream.nil?
-      next_token = response.next_token
-      next
-    else
-      return log_stream.log_stream_name
+    begin
+      task_meta = Net::HTTP.get( URI( "#{ENV['ECS_CONTAINER_METADATA_URI_V4']}/task" ) )
+      task_arn  = JSON.parse(task_meta)["TaskARN"]
+      task_id   = task_arn.split('/').last
+    rescue => e
+      Rails.logger.error 'Something broke when querying for the ECS task id.'
+      Rails.logger.error e.message
+    end
+
+    logs ||= Aws::CloudWatchLogs::Client.new
+    log_stream = nil
+    next_token = nil
+    for i in 1..25 do # Limit to 25 requests so we're not endlessly searching.
+      begin
+        response = logs.describe_log_streams({
+          log_group_name: log_group,
+          order_by: 'LastEventTime',
+          descending: true,
+          next_token: next_token,
+        })
+      rescue Aws::CloudWatchLogs::Errors::ThrottlingException => e
+        Rails.logger.error 'Throttling exception encountered when searching for log stream.'
+        return nil
+      end
+
+      log_streams = response.log_streams
+      log_stream = log_streams.find { |s| s.log_stream_name.include?(task_id) }
+      if log_stream.nil?
+        next_token = response.next_token
+        next
+      else
+        return log_stream.log_stream_name
+      end
     end
   end
 
