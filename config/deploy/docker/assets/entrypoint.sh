95f0bf56c9621529f06bf2dc93af13074bc86bab
Andrew Zito <zitoa@lafayette.edu>
2021-12-21 14:56:13 -0500

    mkdir after clobber before pull

--- config/deploy/docker/assets/entrypoint.sh
+++ config/deploy/docker/assets/entrypoint.sh
@@ -29,6 +29,7 @@ echo "$DATABASE_HOST:*:*:$DATABASE_USER:$DATABASE_PASS" > /root/.pgpass
 chmod 600 /root/.pgpass
 
 bundle exec rake assets:clobber
+mkdir -p ./public/assets
 cd ./public/assets
 # Pull down the compiled assets. Using ASSETS_PREFIX from .env and GITHASH from Docker args.
 ASSETS_PREFIX="${ASSETS_PREFIX}/${GITHASH}" ASSETS_BUCKET_NAME=openpath-precompiled-assets UPDATE_ONLY=true ../../bin/sync_app_assets.rb
