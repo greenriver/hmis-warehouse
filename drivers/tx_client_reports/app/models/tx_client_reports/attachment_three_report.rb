5bc9604414f71723ac6b072fad3de900bf9f2c33
Elliot Anders <elliot@greenriver.org>
2022-01-21 11:02:16 -0800

    Additional fields to support more reporting options

--- drivers/tx_client_reports/app/models/tx_client_reports/attachment_three_report.rb
+++ drivers/tx_client_reports/app/models/tx_client_reports/attachment_three_report.rb
@@ -20,6 +20,7 @@ module TxClientReports
         preload(
           service_history_enrollment_for_head_of_household: { enrollment: :income_benefits_at_entry },
           project: :project_cocs,
+          household_enrollments: :client,
         ).
         where(client_id: client_ids).
         order(first_date_in_program: :desc). # index by uses the last value, so this selects the oldest enrollment
@@ -33,11 +34,17 @@ module TxClientReports
           enrollment&.
           income_benefits_at_entry&.
           TotalMonthlyIncome
-
+        household = if enrollment.household_id.present?
+          enrollment.household_enrollments&.map(&:client)
+        else
+          [client]
+        end
         {
           project_id: project.id,
           project_name: project.ProjectName,
           service_date: enrollment.first_date_in_program,
+          report_start: @filter.start,
+          entry_after_start: enrollment.first_date_in_program > @filter.start,
           fort_worth_resident: nil, # leave blank
           service_location: nil, # leave blank
           client_name: client.name,
@@ -47,10 +54,15 @@ module TxClientReports
           genders: client.gender,
           ethnicity: client.Ethnicity,
           races: client.race_fields,
+          race_description: client.race_description,
           disabled: client_disabled?(client),
-          hh_size: 1 + enrollment.other_clients_over_25 + enrollment.other_clients_under_18 + enrollment.other_clients_between_18_and_25, # Sum household members
+          hh_size: household.count,
           income: hoh_income, # HoH income
           female_hoh: enrollment.head_of_household? && client.Female == 1,
+          any_veterans: household.map(&:VeteranStatus).any?(1),
+          over_62_in_household: household.map(&:age).any? { |age| age.present? && age >= 62 },
+          child_in_household: household.map(&:age).any? { |age| age.present? && age < 18 },
+          disability_in_household: enrollment.household_enrollments.map(&:DisablingCondition).any?(1),
         }
       end.sort_by { |row| row[:service_date] }
     end
