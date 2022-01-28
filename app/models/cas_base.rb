4f2823068b699784d9249fc6c034b1bfaad56dd0
Elliot Anders <elliot@greenriver.org>
2022-01-28 13:02:34 -0800

    Hide CAS stuff when there's no CAS

--- app/models/cas_base.rb
+++ app/models/cas_base.rb
@@ -9,7 +9,7 @@ if ENV['DATABASE_CAS_DB'].present?
   class CasBase < ActiveRecord::Base
     self.abstract_class = true
 
-    connects_to database: { writing: :cas, reading: :cas}
+    connects_to database: { writing: :cas, reading: :cas }
 
     def self.db_exists?
       mem_cache.fetch('cas_db_exists', expires_in: 2.minutes) do
@@ -28,6 +28,17 @@ else
     def self.db_exists?
       false
     end
-  end
+    class << self
+      def respond_to_missing?(name, include_private)
+      end
 
+      def method_missing(name, *args)
+      end
+    end
+    def respond_to_missing?(name, include_private)
+    end
+
+    def method_missing(name, *args)
+    end
+  end
 end
