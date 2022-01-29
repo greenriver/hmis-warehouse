1313fb89edd9144f181d3c5474f8448fb0fbaa21
Elliot Anders <elliot@greenriver.org>
2022-01-29 09:11:36 -0800

    Expose database.yml for debugging

--- bin/materialize.database.yaml.rb
+++ bin/materialize.database.yaml.rb
@@ -8,4 +8,4 @@ template = ERB.new(File.read('config/database.yml'))
 result = YAML.load(template.result(binding))
 File.write('config/database.yml', result.to_yaml)
 
-# puts result.to_yaml
+puts result.to_yaml
