0b0ff7767aa2f04623556e597cdf375be360ff62
Sandy Wise <sandy@greenriver.org>
2022-02-08 09:53:15 -0500

    Adjust tests to include engagement records

--- drivers/hud_apr/spec/models/fy2021/apr/question_nine_spec.rb
+++ drivers/hud_apr/spec/models/fy2021/apr/question_nine_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe HudApr::Generators::Apr::Fy2021::QuestionNine, type: :model do
 
   describe 'Q9a: Number of Persons Contacted' do
     it 'total of two contacts' do
-      expect(report_result.answer(question: 'Q9a', cell: 'B6').summary).to eq(3)
+      expect(report_result.answer(question: 'Q9a', cell: 'B6').summary).to eq(7)
     end
   end
 
   describe 'Q9b: Number of Persons Engaged' do
     it 'total of one engagement' do
-      expect(report_result.answer(question: 'Q9b', cell: 'B6').summary).to eq(1)
+      expect(report_result.answer(question: 'Q9b', cell: 'B6').summary).to eq(5)
     end
   end
 end
