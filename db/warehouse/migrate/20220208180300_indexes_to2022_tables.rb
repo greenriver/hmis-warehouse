d074882a037630ffd1507c19814b69f33c5e744a
Elliot Anders <elliot@greenriver.org>
2022-02-09 12:26:07 -0800

    Remove dead code

--- db/warehouse/migrate/20220208180300_indexes_to2022_tables.rb
+++ db/warehouse/migrate/20220208180300_indexes_to2022_tables.rb
@@ -1,6 +1,5 @@
 class IndexesTo2022Tables < ActiveRecord::Migration[6.1]
   def change
-    spec_version = '2022'
     HmisCsvImporter::Importer::Importer.importable_files.each do |_, klass|
       add_index klass.table_name, :importer_log_id
     end
