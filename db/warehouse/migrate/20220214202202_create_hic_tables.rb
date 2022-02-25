c648d6f463996836af6d7ac705c6a354ecf35bd9
Elliot Anders <elliot@greenriver.org>
2022-02-25 13:33:46 -0800

    Use updated FY for migration

--- db/warehouse/migrate/20220214202202_create_hic_tables.rb
+++ db/warehouse/migrate/20220214202202_create_hic_tables.rb
@@ -1,10 +1,10 @@
 class CreateHicTables < ActiveRecord::Migration[6.1]
   def change
-    HudHic::Generators::Hic::Fy2021::Generator.table_classes.each do |klass|
+    HudHic::Generators::Hic::Fy2022::Generator.table_classes.each do |klass|
       klass.hmis_table_create!(version: '2022', constraints: false)
     end
 
-    HudHic::Generators::Hic::Fy2021::Generator.table_classes.each do |klass|
+    HudHic::Generators::Hic::Fy2022::Generator.table_classes.each do |klass|
       column_names = klass.column_names
       change_table(klass.table_name) do |t|
         t.integer :report_instance_id,null: false, index: true
