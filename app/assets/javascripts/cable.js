9be49a82ca52dfe1670b2c1f8f892a46ce8520cf
Elliot Anders <elliot@greenriver.org>
2022-03-15 12:41:25 -0700

    Commented code for getting action cable slightly happier on slow machines

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -3,5 +3,6 @@
 (function() {
   this.App || (this.App = {});
   App.cable = ActionCable.createConsumer();
-  ActionCable.logger.enabled = true;
+  // ActionCable.logger.enabled = true;
+  // ActionCable.ConnectionMonitor.staleThreshold = 10;
 }).call(this);
