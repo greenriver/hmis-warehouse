c54c5a4af02b267ed06cff59622ff011f0e5ccff
Sandy Wise <sandy@greenriver.org>
2022-03-24 09:42:49 -0400

    Rollback unrelated structure file

--- db/warehouse_structure.sql
+++ db/warehouse_structure.sql
@@ -13174,8 +13174,7 @@ CREATE TABLE public.hud_report_apr_clients (
     ce_event_referral_case_manage_after integer,
     ce_event_referral_result integer,
     gender_multi character varying,
-    bed_nights integer,
-    pit_enrollments jsonb DEFAULT '[]'::jsonb
+    bed_nights integer
 );
 
 
@@ -47533,7 +47532,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220216204506'),
 ('20220302145218'),
 ('20220302154833'),
-('20220308185943'),
-('20220308193503');
+('20220308185943');
 
 
