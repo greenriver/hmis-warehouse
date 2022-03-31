70128b30a334758c0b8ddf2d62abd66d774f2b47
Gig Ashton <gig@greenriver.org>
2022-03-31 16:18:11 -0400

    Use GrdaWarehouse base

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,7 +4,7 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-class Favorite < ApplicationRecord
+class Favorite < GrdaWarehouseBase
   belongs_to :user
   belongs_to :entity, polymorphic: true
 end
