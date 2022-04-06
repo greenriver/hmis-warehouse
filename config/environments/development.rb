6ed3a1100788ff85bdf23c8cb022ef323295c1e6
Gig Ashton <gig@greenriver.org>
2022-04-06 16:44:08 -0400

    Rename to enable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  if ENV['DISABLE_EVENT_FS_CHECKER'] == '1'
+  if ENV['ENABLE_EVENT_FS_CHECKER'] == '1'
     config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   end
 
