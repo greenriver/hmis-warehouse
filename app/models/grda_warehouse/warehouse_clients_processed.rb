6bfd6f5c7c2a5a0949cf9aa62063d7d44ec1a514
Elliot Anders <elliot@greenriver.org>
2022-04-07 10:48:55 -0700

    Sets don't always behave like arrays

--- app/models/grda_warehouse/warehouse_clients_processed.rb
+++ app/models/grda_warehouse/warehouse_clients_processed.rb
@@ -24,7 +24,7 @@ class GrdaWarehouse::WarehouseClientsProcessed < GrdaWarehouseBase
       routine: :service_history,
     ).index_by(&:client_id)
 
-    cohort_client_ids = GrdaWarehouse::CohortClient.joins(:cohort, :client).distinct.pluck(:client_id).to_set
+    cohort_client_ids = GrdaWarehouse::CohortClient.joins(:cohort, :client).distinct.pluck(:client_id)
     client_ids += cohort_client_ids
 
     client_ids.uniq.each_slice(2_000) do |client_id_batch|
