f0b95789f55ebc2d88ddc55d6894fefe039cd2cb
Andrew Zito <zitoa@lafayette.edu>
2022-04-11 09:42:10 -0400

    Todd feedback

--- config/initializers/aws_cloudwatch.rb
+++ config/initializers/aws_cloudwatch.rb
@@ -12,9 +12,10 @@ def find_log_stream_name
     task_meta = Net::HTTP.get( URI( "#{ENV['ECS_CONTAINER_METADATA_URI_V4']}/task" ) )
     task_arn  = JSON.parse(task_meta)["TaskARN"]
     task_id   = task_arn.split('/').last
-  rescue => e
+  rescue StandardError => e
     Rails.logger.error 'Something broke when querying ENV for the ECS task id.'
     Rails.logger.error e.message
+    return nil
   end
 
   return ENV.fetch('LOG_STREAM_NAME_PREFIX') + "/#{task_id}"
@@ -23,7 +24,7 @@ end
 log_stream_name = find_log_stream_name
 log_group_name = ENV.fetch('TARGET_GROUP_NAME', nil)
 
-unless log_stream_name.nil? || log_group_name.nil?
+if log_stream_name.present? && log_group_name.present?
   log_stream_escaped = log_stream_name.gsub('/', '$252F')
   log_stream_link = "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/#{log_group_name}/log-events/#{log_stream_escaped}"
 
