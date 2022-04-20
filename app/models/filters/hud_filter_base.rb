783576d8e2a1bff9a5f1a03c81787ee39a090595
Gig Ashton <gig@greenriver.org>
2022-04-20 13:25:11 -0400

    Remove CE message that doesn't make sense

--- app/models/filters/hud_filter_base.rb
+++ app/models/filters/hud_filter_base.rb
@@ -42,9 +42,6 @@ module Filters
       scope = filter_for_ethnicity(scope)
       scope = filter_for_sub_population(scope)
 
-      # FIXME: these filter by project type, too, which limits scope a lot
-      # scope = filter_for_ca_homeless(scope)
-      # scope = filter_for_ce_cls_homeless(scope)
       scope
     end
 
