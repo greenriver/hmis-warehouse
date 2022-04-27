ad1eaf9f020c4170b108be78869c866968f8b77a
Gig Ashton <gig@greenriver.org>
2022-04-27 16:46:18 -0400

    Add match_rank to cas sync

---
+++ app/models/grda_warehouse/cas_project_client_calculator/mdha.rb
@@ -0,0 +1,21 @@
+###
+# Copyright 2016 - 2022 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'memoist'
+module GrdaWarehouse::CasProjectClientCalculator
+  class Mdha
+    def value_for_cas_project_client(client:, column:)
+      current_value = client.send(column)
+      return current_value unless column.to_sym == :match_rank
+
+      # return 1 if has encampment decomissioning flag
+
+      return 2 if client.veteran?
+
+      return 3
+    end
+  end
+end
--- app/models/grda_warehouse/tasks/push_clients_to_cas.rb
+++ app/models/grda_warehouse/tasks/push_clients_to_cas.rb
@@ -219,6 +219,7 @@ module GrdaWarehouse::Tasks
         assessor_last_name: :assessor_last_name,
         assessor_email: :assessor_email,
         assessor_phone: :assessor_phone,
+        match_rank: :match_rank,
       }
     end
 
