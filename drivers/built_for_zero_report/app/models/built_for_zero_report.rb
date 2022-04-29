b108d770e9270b13cbaa653e896a8b9cf066a757
Sandy Wise <sandy@greenriver.org>
2022-04-29 14:46:53 -0400

    Don't show sections if there is no cohort to back it up

--- drivers/built_for_zero_report/app/models/built_for_zero_report.rb
+++ drivers/built_for_zero_report/app/models/built_for_zero_report.rb
@@ -3,13 +3,28 @@ module BuiltForZeroReport
     'built_for_zero_report_'
   end
 
-  def self.sections
+  def self.all_report_sections
     {
       veterans: 'Veterans Section',
       chronic: 'Chronic Section',
       adults: 'All Single Adults (Individuals) Section',
       youth: 'Youth Section',
       families: 'Family Section',
-    }.invert.freeze
+    }
+  end
+
+  def self.sections
+    cohort_keys = {
+      veterans: :veteran_cohort,
+      chronic: :chronic_cohort,
+      adults: :adult_only_cohort,
+      youth: :youth_cohort,
+      families: :adult_and_child_cohort,
+    }.freeze
+
+    all_report_sections.
+      select { |k, _| ::GrdaWarehouse::SystemCohorts::Base.find_system_cohort(cohort_keys[k]).present? }.
+      invert.
+      freeze
   end
 end
