11354f565b9441dc47811883c047a6448b7c6d86
Gig Ashton <gig@greenriver.org>
2022-05-09 15:20:51 -0400

    Pagy for import dashboard tables

--- app/controllers/match_logs_controller.rb
+++ app/controllers/match_logs_controller.rb
@@ -7,6 +7,6 @@
 class MatchLogsController < ApplicationController
   before_action :require_can_view_imports!
   def index
-    @duplicates = GrdaWarehouse::IdentifyDuplicatesLog.order(started_at: :desc).page(params[:page]).per(25)
+    @pagy, @duplicates = pagy(GrdaWarehouse::IdentifyDuplicatesLog.order(started_at: :desc))
   end
 end
--- app/controllers/service_history_logs_controller.rb
+++ app/controllers/service_history_logs_controller.rb
@@ -7,6 +7,6 @@
 class ServiceHistoryLogsController < ApplicationController
   before_action :require_can_view_imports!
   def index
-    @service = GrdaWarehouse::GenerateServiceHistoryLog.order(started_at: :desc).page(params[:page]).per(25)
+    @pagy, @service = pagy(GrdaWarehouse::GenerateServiceHistoryLog.order(started_at: :desc))
   end
 end
