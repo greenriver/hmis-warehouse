40c40b0cd3e487f0830d1f3f9186319f5f4d3959
Gig Ashton <gig@greenriver.org>
2022-05-11 14:11:49 -0400

    Limit PATH project list by funder, allow passing any project params to template

--- drivers/hud_path_report/app/models/hud_path_report/filters/path_filter.rb
+++ drivers/hud_path_report/app/models/hud_path_report/filters/path_filter.rb
@@ -10,13 +10,6 @@ module HudPathReport::Filters
       [:so, :services_only]
     end
 
-    def project_options_for_select(user:)
-      path_funded = ::GrdaWarehouse::Hud::Funder.funding_source(funder_code: 21)
-      all_project_scope.with_hud_project_type(path_project_type_ids)
-        .joins(:funders)
-        .options_for_select(user: user, scope: path_funded)
-    end
-
     def path_project_types_for_select
       GrdaWarehouse::Hud::Project::PROJECT_GROUP_TITLES.select { |k, _| k.in?([:so, :services_only]) }.invert.freeze
     end
