9591e937a3f5a7d64a10bbd0eb40db566d362823
Gig <gig@greenriver.org>
2022-05-11 12:14:06 -0400

    Update app/controllers/warehouse_reports/confidential_touch_point_exports_controller.rb
    Co-authored-by: Sandy Wise <33072305+awisema@users.noreply.github.com>

--- app/controllers/warehouse_reports/confidential_touch_point_exports_controller.rb
+++ app/controllers/warehouse_reports/confidential_touch_point_exports_controller.rb
@@ -13,7 +13,8 @@ module WarehouseReports
       options = { search_scope: touch_point_scope }
       options.merge!(filter_params) if filter_params.present?
       @filter = ::Filters::HealthTouchPointExportsFilter.new(options)
-      @pagy, @reports = pagy(report_scope.for_list.order(created_at: :desc))
+      @reports = report_scope.for_list.order(created_at: :desc)
+      @pagy, @reports = pagy(@reports)
     end
 
     def report_scope
