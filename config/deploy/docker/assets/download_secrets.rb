4bd52cbdc2cfdcf5f37e2208079f631b6765e5bb
Andrew Zito <zitoa@lafayette.edu>
2022-05-25 16:03:32 -0400

    Move downloading secrets to lib

--- config/deploy/docker/assets/download_secrets.rb
+++ config/deploy/docker/assets/download_secrets.rb
@@ -1,16 +1,4 @@
 #!/usr/bin/env ruby
+require_relative '../config/deploy/docker/lib/aws_sdk_helpers'
 
-begin
-  require 'aws-sdk-secretsmanager'
-
-  define_method(:client) { Aws::SecretsManager::Client.new }
-
-  resp = client.get_secret_value(
-    secret_id: ENV.fetch('SECRET_ARN')
-  )
-
-  puts resp.to_h[:secret_string]
-rescue Aws::Errors::MissingCredentialsError
-  STDERR.puts "Need credentials to sync secrets (or run on a server/container with a role)"
-  puts "ERROR=secretsyncfailed"
-end
+puts AwsSdkHelpers::Helpers.get_secret(ENV.fetch('SECRET_ARN'))
