28b2059e0fdf281250b1f561856d2d2be893bbbf
Gig Ashton <gig@greenriver.org>
2022-05-26 13:19:10 -0400

    Make sassc build work with M1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-cd /app && bundle install
+cd /app
+bundle config --global set build.sassc --disable-march-tune-native
+bundle install
 npm install
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
