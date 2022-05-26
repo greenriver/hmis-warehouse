a368dca7d9daedf07752e4d88cd6713baf84bc41
Andrew Zito <zitoa@lafayette.edu>
2022-05-26 18:30:49 -0400

    avoid calling .present? on false

--- config/deploy/docker/lib/command_args.rb
+++ config/deploy/docker/lib/command_args.rb
@@ -15,7 +15,7 @@ class CommandArgs
 
     remote_config = YAML.safe_load(remote_config_text, [Symbol], aliases: true)
 
-    if local_config != remote_config && local_config.present?
+    if local_config&.present? && local_config != remote_config
       puts 'Local secrets.yml differs from remote config, would you like to pull down the remote version? This will overwrite your local file. [y/N]'
       unsure = $stdin.readline
       if unsure.chomp.downcase.match?(/y(es)?/)
