ff65dd1666b57023e6f0b0dc0b24b5c40e7908c8
Andrew Zito <zitoa@lafayette.edu>
2022-05-26 15:33:33 -0400

    shuffle stuff

--- config/deploy/docker/lib/command_args.rb
+++ config/deploy/docker/lib/command_args.rb
@@ -11,7 +11,7 @@ class CommandArgs
     remote_config_text = AwsSdkHelpers::Helpers.get_secret(ENV['SECRETS_YML_SECRET_ARN'])
     remote_config = YAML.safe_load(remote_config_text)
 
-    if local_config != remote_config
+    if local_config != remote_config && local_config.present?
       puts 'Local secrets.yml differs from remote config, would you like to pull down the remote version? This will overwrite your local file. [y/N]'
       unsure = $stdin.readline
       if unsure.chomp.downcase.match?(/y(es)?/)
