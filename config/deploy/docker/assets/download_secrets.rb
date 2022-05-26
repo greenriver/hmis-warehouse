cd20d6a88c48efa54bfcadc769cb944d66578586
Andrew Zito <zitoa@lafayette.edu>
2022-05-26 14:54:56 -0400

    fix path in download_secrets.rb?

--- config/deploy/docker/assets/download_secrets.rb
+++ config/deploy/docker/assets/download_secrets.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-require_relative '../config/deploy/docker/lib/aws_sdk_helpers'
+require_relative '../lib/aws_sdk_helpers'
 
 puts AwsSdkHelpers::Helpers.get_secret(ENV.fetch('SECRET_ARN'))
