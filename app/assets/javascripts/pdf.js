691caa14e08e628995bf6864517f7baa387d4b90
Gig Ashton <gig@greenriver.org>
2022-05-27 16:35:27 -0400

    Fix chart path for pdfs

--- app/assets/javascripts/pdf.js
+++ app/assets/javascripts/pdf.js
@@ -2,7 +2,7 @@
 //= require d3/build/d3.min
 //= require d3-interpolate/build/d3-interpolate.min
 //= require d3-scale-chromatic/dist/d3-scale-chromatic.min
-//= require Chart.bundle.min
+//= require chart.js/dist/Chart.bundle.min
 //= require billboard.js/dist/billboard.min
 
 //= require_directory ./dashboards
