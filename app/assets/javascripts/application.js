972ef94dddae7d0b7c9c01ace1b771ffe0c2275d
Gig Ashton <gig@greenriver.org>
2022-05-27 12:50:48 -0400

    Install topojson-client from yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@
 //= require d3/build/d3.min
 //= require d3-interpolate/build/d3-interpolate.min
 //= require d3-scale-chromatic/dist/d3-scale-chromatic.min
-//= require topojson.min.js
+//= require topojson-client/dist/topojson-client.min
 //= require inputmask/jquery.inputmask.bundle.min
 //= require inputmask/inputmask/bindings/inputmask.binding.min
 //= require billboard.js/dist/billboard.min
