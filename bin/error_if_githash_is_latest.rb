b919e3ab5c249865524372947cc71be7fabc1e73
Andrew Zito <zitoa@lafayette.edu>
2022-06-03 15:52:19 -0400

    Give up on ruby, try aws cli

--- bin/error_if_githash_is_latest.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-require_relative '../config/deploy/docker/lib/aws_sdk_helpers'
-
-githash = ENV.fetch('GITHASH')
-variant = ENV.fetch('VARIANT', 'web')
-
-raise "This githash (#{githash}) is being used in a deployed application and cannot be rebuilt" if AwsSdkHelpers::Helpers.githash_is_latest(githash, variant)
