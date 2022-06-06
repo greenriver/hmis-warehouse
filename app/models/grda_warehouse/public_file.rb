2e3685e0ec0def2f0213a091dbda526c576a928f
Elliot Anders <elliot@greenriver.org>
2022-06-06 14:10:33 -0700

    Additional public file option for patient ROI

--- app/models/grda_warehouse/public_file.rb
+++ app/models/grda_warehouse/public_file.rb
@@ -13,8 +13,8 @@ module GrdaWarehouse
     validate :file_exists_and_not_too_large
 
     def file_exists_and_not_too_large
-      errors.add :file, "No uploaded file found" if (content&.size || 0) < 100
-      errors.add :file, "File size should be less than 4 MB" if (content&.size || 0) > 4.megabytes
+      errors.add :file, 'No uploaded file found' if (content&.size || 0) < 100
+      errors.add :file, 'File size should be less than 4 MB' if (content&.size || 0) > 4.megabytes
     end
 
     def self.known_locations
@@ -23,6 +23,7 @@ module GrdaWarehouse
         'client/chronic_homelessness_verification' => 'Verification of Chronic Homelessness', # app/controllers/clients/files_controller.rb,
         'client/disability_verification' => 'Verification of Disability', # app/controllers/clients/files_controller.rb,
         'patient/release' => 'Patient Release Form', # app/controllers/health/release_forms_controller.rb
+        'patient/directed_release' => 'Patient Directed Release Form', # app/controllers/health/release_forms_controller.rb
         'patient/participation' => 'Participation Form', # app/controllers/health/participation_forms_controller.rb
         'patient/participation_spanish' => 'Participation Form (SPANISH)', # app/controllers/health/participation_forms_controller.rb
         'patient/ssm' => 'SSM Form', # app/controllers/health/self_sufficiency_matrix_forms_controller.rb.rb
@@ -34,18 +35,17 @@ module GrdaWarehouse
     end
 
     def self.known_hmis_locations
-      known_locations.select{ |k,_| k.starts_with?('client/') }
+      known_locations.select { |k, _| k.starts_with?('client/') }
     end
 
     def self.known_health_locations
-      known_locations.select{ |k,_| k.starts_with?('patient/') }
+      known_locations.select { |k, _| k.starts_with?('patient/') }
     end
 
     def self.url_for_location location
-      if id = order(id: :desc).where(name: location).pluck(:id)&.first
+      if (id = order(id: :desc).where(name: location).pluck(:id)&.first) # rubocop:disable Style/GuardClause:
         Rails.application.routes.url_helpers.public_file_path(id: id)
       end
     end
-
   end
 end
