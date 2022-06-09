4b1860409ba45a92c78d731f8cc0fdc1f3386cc8
Sandy Wise <sandy@greenriver.org>
2022-06-09 15:34:41 -0400

    First steps towards GraphQL

---
+++ app/graphql/types/hmis_schema/mutation_type.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Types
+  class HmisSchema::MutationType < Types::BaseObject
+  end
+end
---
+++ app/graphql/types/hmis_schema/project_type.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Types
+  class HmisSchema::ProjectType < Types::BaseEnum
+    description 'HUD Project Types'
+
+    value 'ES', 'Emergency Shelter', value: 1
+    value 'TH', 'Transitional Housing', value: 2
+    value 'PSH', 'Permanent Supportive Housing', value: 3
+    value 'SO', 'Street Outreach', value: 4
+    value 'SERVICES_ONLY', 'Services Only', value: 6
+    value 'OTHER', 'Other', value: 7
+    value 'SH', 'Safe Haven', value: 8
+    value 'OPH', 'Permanent Housing Only', value: 9
+    value 'PH', 'Permanent Housing', value: 10
+    value 'DAY_SHELTER', 'Day Shelter', value: 11
+    value 'PREVENTION', 'Homelessness Prevention', value: 12
+    value 'RRH', 'Rapig Re-Housing', value: 13
+    value 'CE', 'Coordinated Entry', value: 14
+  end
+end
