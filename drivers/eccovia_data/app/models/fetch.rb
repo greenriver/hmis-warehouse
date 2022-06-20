75da1177eeb5bee2c6b87cf73ae0f396bb1d62a8
Elliot Anders <elliot@greenriver.org>
2022-06-20 14:22:32 -0700

    WIP; move Eccovia data fetching to driver, update code for assessments, add stubs for clients

--- drivers/eccovia_data/app/models/fetch.rb
+++ drivers/eccovia_data/app/models/fetch.rb
@@ -4,13 +4,13 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-module GrdaWarehouse
-  class HMIS::Eccovia::Fetch < GrdaWarehouseBase
+module EccoviaData
+  class Fetch < GrdaWarehouseBase
     self.table_name = :eccovia_fetches
-    belongs_to :credentials, class_name: 'GrdaWarehouse::RemoteCredentials::Eccovia'
+    belongs_to :credentials, class_name: 'EccoviaData::Credential'
 
     def fetch_updated
-      Assessment.fetch_updated(since: last_fetched_at, data_source_id: data_source_id, credentials: credentials)
+      EccoviaData::Assessment.fetch_updated(since: last_fetched_at, data_source_id: data_source_id, credentials: credentials)
 
       update(last_fetched_at: Time.current)
     end
