62b6a330f207857ed12de50c21d24a3386cdb7aa
Gig Ashton <gig@greenriver.org>
2022-06-21 00:16:15 +0200

    Fix confidentialization logic on DQR

--- app/controllers/data_quality_reports_controller.rb
+++ app/controllers/data_quality_reports_controller.rb
@@ -52,7 +52,7 @@ class DataQualityReportsController < ApplicationController
     if params[:individual].present?
       @data = @report.support_for(support_params)
 
-      unless current_user.can_view_confidential_enrollment_details? && @data[:headers].include?('Project ID')
+      if !current_user.can_view_confidential_enrollment_details? && @data[:headers].include?('Project ID')
         project_id_col = @data[:headers].find_index('Project ID')
         project_name_col = @data[:headers].find_index('Project')
         @data[:counts].each_with_index do |row, i|
