4e0273793bab6e0b7b1cbda7ae092b5590fbddf0
Elliot Anders <elliot@greenriver.org>
2022-06-21 07:24:07 -0700

    Fix over eager find and replace

--- db/migrate/20220615155458_rename_can_view_confidential_enrollment_details.rb
+++ db/migrate/20220615155458_rename_can_view_confidential_enrollment_details.rb
@@ -1,5 +1,5 @@
 class RenameCanViewConfidentialEnrollmentDetails < ActiveRecord::Migration[6.1]
   def change
-    rename_column :roles, :can_view_confidential_project_names, :can_view_confidential_project_names
+    rename_column :roles, :can_view_confidential_enrollment_details, :can_view_confidential_project_names
   end
 end
