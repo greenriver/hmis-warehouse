6a6a6c61ff8cf6d36d74d8e11c583de67638cd06
ZachCMP <zparks@greenriver.org>
2022-07-25 12:51:59 -0700

    Add search method to HMIS client model

--- drivers/hmis/app/models/hmis/hud/client.rb
+++ drivers/hmis/app/models/hmis/hud/client.rb
@@ -19,6 +19,12 @@ class Hmis::Hud::Client < Hmis::Hud::Base
 
   SORT_OPTIONS = [:last_name_asc, :last_name_desc].freeze
 
+  def self.client_search(input:, _user: nil)
+    scope = GrdaWarehouse::Hud::Client.all
+    scope = scope.full_text_search(input.text_search) if input.text_search.present?
+    Hmis::Hud::Client.where(id: scope)
+  end
+
   def self.sort_by_option(option)
     raise NotImplementedError unless SORT_OPTIONS.include?(option)
 
