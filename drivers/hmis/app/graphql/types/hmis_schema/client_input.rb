60c6645ff2526ae4d2bb6beeda1b09c1f1e50a7d
ZachCMP <zparks@greenriver.org>
2022-08-04 15:23:23 -0700

    Add client transformer and tests

--- drivers/hmis/app/graphql/types/hmis_schema/client_input.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/client_input.rb
@@ -2,6 +2,8 @@ module Types
   class HmisSchema::ClientInput < BaseInputObject
     description 'HMIS Client input'
 
+    transform_with Types::HmisSchema::Transformers::ClientInputTransformer
+
     argument :first_name, String, required: false
     argument :middle_name, String, required: false
     argument :last_name, String, required: false
@@ -17,63 +19,5 @@ module Types
     argument :race, [Types::HmisSchema::Enums::Race], required: false
     argument :ethnicity, Types::HmisSchema::Enums::Ethnicity, required: false
     argument :veteran_status, Types::HmisSchema::Enums::VeteranStatus, required: false
-
-    def gender_map
-      Hmis::Hud::Client.gender_enum_map
-    end
-
-    def race_map
-      Hmis::Hud::Client.race_enum_map
-    end
-
-    def multi_field_attrs(input_field, enum_map, none_field)
-      result = {}
-      return result unless input_field.present?
-
-      null_value = input_field.find { |val| enum_map.null_member?(value: val) }
-
-      if null_value.nil?
-        enum_map.base_members.map { |item| item[:value] }.each do |value|
-          member = enum_map.lookup(value: value)
-          result[member[:key]] = input_field.include?(value) ? 1 : 0
-        end
-      else
-        enum_map.base_members.each do |member|
-          result[member[:key]] = 99
-        end
-        result[none_field] = null_value unless none_field.nil?
-      end
-
-      result
-    end
-
-    def gender_attrs
-      multi_field_attrs(gender, gender_map, 'GenderNone')
-    end
-
-    def race_attrs
-      multi_field_attrs(race, race_map, 'RaceNone')
-    end
-
-    def to_params
-      result = {}
-
-      result['FirstName'] = first_name
-      result['LastName'] = last_name
-      result['MiddleName'] = middle_name
-      result['preferred_name'] = preferred_name
-      result['NameDataQuality'] = name_quality
-      result['DOB'] = dob
-      result['DOBDataQuality'] = dob_quality
-      result['SSN'] = ssn.gsub(/\D/, '')
-      result['SSNDataQuality'] = ssn_quality
-      result['Ethnicity'] = ethnicity
-      result['VeteranStatus'] = veteran_status
-
-      result = result.merge(race_attrs)
-      result = result.merge(gender_attrs)
-
-      result
-    end
   end
 end
--- drivers/hmis/spec/support/graphql_helpers.rb
+++ drivers/hmis/spec/support/graphql_helpers.rb
@@ -8,7 +8,7 @@ module GraphqlHelpers
     variables = variables.deep_transform_keys { |k| k.to_s.camelize(:lower) }
     headers = { 'CONTENT_TYPE' => 'application/json', 'ORIGIN' => HMIS_ORIGIN }
     post '/hmis/hmis-gql', params: { query: yield, variables: variables }.to_json, headers: headers
-    puts response.body
+    # puts response.body
     if response.code == '200'
       result = JSON.parse(response.body)
       raise result.to_h['errors'].first['message'] unless result.to_h['errors'].nil? || result.to_h['errors'].empty?
