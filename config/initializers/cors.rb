ff5f88f75e6ff03f38489a67584a91dcfd483fa3
Gig Ashton <gig@greenriver.org>
2022-08-18 11:03:29 -0400

    Allow requests to /hmis from the HMIS frontend host

---
+++ config/initializers/cors.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file.
+# Read more: https://github.com/cyu/rack-cors
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # Allow requests to /hmis from the HMIS frontend host
+  allow do
+    hmis_hostname = ENV['HMIS_HOSTNAME']
+    return unless ENV['ENABLE_HMIS_API'] == 'true' && hmis_hostname.present?
+
+    origins hmis_hostname
+
+    resource '/hmis/*',
+        headers: :any,
+        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        credentials: true,
+        debug: true
+  end
+end
