aa31d3174c7f1bb6a503a4d38b3df72e50c5fd31
Gig Ashton <gig@greenriver.org>
2022-08-18 11:40:22 -0400

    Remove early return, only insert middleware if HMIS enabled

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,17 @@
 # Be sure to restart your server when you modify this file.
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # Allow requests to /hmis from the HMIS frontend host
-  allow do
-    hmis_hostname = ENV['HMIS_HOSTNAME']
-    return unless ENV['ENABLE_HMIS_API'] == 'true' && hmis_hostname.present?
+hmis_hostname = ENV['HMIS_HOSTNAME']
+if ENV['ENABLE_HMIS_API'] == 'true' && hmis_hostname.present?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    # Allow requests to /hmis from the HMIS frontend host
+    allow do
+      origins hmis_hostname
 
-    origins hmis_hostname
-
-    resource '/hmis/*',
-      headers: :any,
-      methods: [:get, :post, :delete, :put, :patch, :options, :head],
-      credentials: true
+      resource '/hmis/*',
+        headers: :any,
+        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        credentials: true
+    end
   end
 end
