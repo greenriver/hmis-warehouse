6d9cfbc65d30ee34effe3bc3ef778b264f1d5288
Sandy Wise <sandy@greenriver.org>
2022-08-23 14:30:15 -0400

    Add sort/filter to teams

--- app/controllers/health/team_patients_controller.rb
+++ app/controllers/health/team_patients_controller.rb
@@ -21,11 +21,69 @@ module Health
       @report = Health::TeamPerformance.new(range: (Date.today..Date.tomorrow), team_scope: Health::CoordinationTeam.all)
       @teams = @report.team_counts
       @totals = @report.total_counts
-      @patients = Health::Patient.where(id: @report.team_counts.detect { |counts| counts.id == @active_team.id }.patient_referrals)
-      medicaid_ids = @patients.map(&:medicaid_id)
-      @scores = calculate_dashboards(medicaid_ids)
+      @patients = patient_source.where(id: @report.team_counts.detect { |counts| counts.id == @active_team.id }.patient_referrals)
 
-      @pagy, @patients = pagy(@patients)
+      @q = @patients.ransack(params[:q])
+      @patients = @q.result(distinct: true) if params[:q].present?
+      if params[:filter].present?
+        @active_filter = true if params[:filter][:population] != 'all'
+        case params[:filter][:population]
+        when 'not_engaged'
+          @patients = @patients.not_engaged
+        when 'no_activities'
+          # @patients = @patients.engaged.no_qualifying_activities_this_month
+          # @elliot engaged means they would have a qualifying activity?
+          @patients = @patients.no_qualifying_activities_this_month
+        when 'engagement_ending'
+          @patients = @patients.engagement_ending
+        end
+
+        if params[:filter][:user].present?
+          @active_filter = true
+          user_id = if params[:filter][:user] == 'unassigned'
+            nil
+          else
+            params[:filter][:user].to_i
+          end
+
+          @patients = @patients.where(care_coordinator_id: user_id)
+        end
+
+        if params[:filter][:nurse_care_manager_id].present?
+          @active_filter = true
+          nurse_care_manager_id = if params[:filter][:nurse_care_manager_id] == 'unassigned'
+            nil
+          else
+            params[:filter][:nurse_care_manager_id].to_i
+          end
+
+          @patients = @patients.where(nurse_care_manager_id: nurse_care_manager_id)
+        end
+      end
+
+      @column = params[:sort] || 'name'
+      @direction = params[:direction]&.to_sym || :asc
+      respond_to do |format|
+        format.html do
+          medicaid_ids = @patients.map(&:medicaid_id)
+          @patients = patient_source.where(id: @patients.pluck(:id))
+          if @column == 'name'
+            @patients = @patients.order(last_name: @direction, first_name: @direction)
+          else
+            sort_order = determine_sort_order(medicaid_ids, @column, @direction)
+            @patients = @patients.order_as_specified(sort_order)
+          end
+          @pagy, @patients = pagy(@patients)
+          @scores = calculate_dashboards(medicaid_ids)
+        end
+        format.xlsx do
+          date = Date.current.strftime('%Y-%m-%d')
+          @patients = @patients.joins(:patient_referral).
+            preload(:patient_referral, :recent_cha_form, client: :processed_service_history)
+          @tracking_sheet = Health::TrackingSheet.new(@patients)
+          render(xlsx: 'index', filename: "Tracking Sheet #{date}.xlsx")
+        end
+      end
     end
 
     def detail
@@ -37,6 +95,10 @@ module Health
         order(last_name: :asc, first_name: :asc)
 
       @team = Health::CoordinationTeam.find(@team_id)
+    end
+
+    private def patient_source
+      Health::Patient
     end
   end
 end
