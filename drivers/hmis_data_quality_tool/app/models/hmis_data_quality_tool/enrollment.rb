c4ebe4a08b2da3deeb1dfacee7e9f1f744392af3
Elliot Anders <elliot@greenriver.org>
2022-08-27 17:11:38 -0700

    WIP; stubs for enrollment calculations

--- drivers/hmis_data_quality_tool/app/models/hmis_data_quality_tool/enrollment.rb
+++ drivers/hmis_data_quality_tool/app/models/hmis_data_quality_tool/enrollment.rb
@@ -12,5 +12,12 @@ module HmisDataQualityTool
     has_many :hud_reports_universe_members, inverse_of: :universe_membership, class_name: 'HudReports::UniverseMember', foreign_key: :universe_membership_id
     belongs_to :client, class_name: 'GrdaWarehouse::Hud::Client', optional: true
     belongs_to :enrollment, class_name: 'GrdaWarehouse::Hud::Enrollment', optional: true
+
+    def self.detail_headers
+      {
+        destination_client_id: 'Warehouse Client ID',
+        hmis_enrollment_id: 'Enrollment ID',
+      }.freeze
+    end
   end
 end
--- drivers/hmis_data_quality_tool/app/models/hmis_data_quality_tool/report.rb
+++ drivers/hmis_data_quality_tool/app/models/hmis_data_quality_tool/report.rb
@@ -17,7 +17,13 @@ module HmisDataQualityTool
 
     belongs_to :user, optional: true
     has_many :clients
-    has_many :results
+    has_many :enrollments
+    has_many :assessments
+    has_many :events
+    has_many :services
+    has_many :current_living_situations
+    has_many :projects
+    has_many :inventories
 
     after_initialize :filter
 
@@ -174,6 +180,112 @@ module HmisDataQualityTool
       report_clients = clients.map { |c| [c.client_id, c] }.to_h
       report_clients = gender_issues(report_clients)
       race_issues(report_clients)
+
+      report_enrollments = enrollments.map { |e| [e.enrollment_id, e] }.to_h
+      report_enrollments = internal_enrollment_issues(report_enrollments)
+      report_enrollments = household_issues(report_enrollments)
+      enrollment_date_issues(report_enrollments)
+    end
+
+    private def internal_enrollment_issues(report_enrollments)
+      intermediate_report_enrollments = {}
+      GrdaWarehouse::Hud::Enrollment.joins(:service_history_enrollment).
+        preload(:exit, :project, :services, client: :warehouse_client_source).
+        merge(report_scope).distinct.
+        where(internal_enrollment_issues_query).
+        find_each do |enrollment|
+          client = enrollment.client
+          report_enrollment = report_enrollments[client] || Enrollment.new(
+            report_id: id,
+            enrollment_id: enrollment.id,
+          )
+          report_enrollment.client_id = client.id
+          report_enrollment.personal_id = client.PersonalID
+          report_enrollment.destination_client_id = client.warehouse_client_source.destination_id
+          report_enrollment.hmis_enrollment_id = enrollment.EnrollmentID
+          report_enrollment.exit_id = enrollment.exit&.ExitID
+          report_enrollment.data_source_id = enrollment.data_source_id
+          report_enrollment.project_name = enrollment.project.name(user)
+          report_enrollment.entry_date = enrollment.EntryDate
+          report_enrollment.move_in_date = enrollment.MoveInDate
+          report_enrollment.exit_date = enrollment.exit&.ExitDate
+          report_enrollment.disabling_condition = enrollment.DisablingCondition
+          report_enrollment.household_id = enrollment.HouseholdID
+          report_enrollment.living_situation = enrollment.LivingSituation
+          report_enrollment.relationship_to_hoh = enrollment.RelationshipToHoH
+          report_enrollment.coc_code = enrollment.CoCCode
+          report_enrollment.destination = enrollment.Destination
+          report_enrollment.project_operating_start_date = enrollment.project.OperatingStartDate
+          report_enrollment.project_operating_end_date = enrollment.project.OperatingEndDate
+          project_tracking_method = enrollment.project.TrackingMethod
+          report_enrollment.project_tracking_method = project_tracking_method
+
+          report_age_date = [enrollment.EntryDate, filter.start].max
+          report_enrollment.age = enrollment.client.age_on(report_age_date)
+          hh = household(enrollment.household_id)
+          report_enrollment.household_max_age = hh.map { |en| en[:age] }.compact.max
+          report_enrollment.head_of_household_count = hh.select { |en| en[:relationship_to_hoh] == 1 }.count
+
+          max_date = [filter.end, Date.current].min
+          en_services = enrollment.services&.select { |s| s.DateProvided <= max_date }
+
+          lot = if project_tracking_method == 3
+            # count services <= min of report end and current date
+            en_services&.count || 0
+          else
+            # count dates between entry and min of report end, current_date, exit_date
+            max_date = [enrollment.exit&.ExitDate, filter.end, Date.current].compact.min
+            (max_date - enrollment.EntryDate).to_i
+          end
+          report_enrollment.lot = lot
+          max_service = if project_tracking_method == 3
+            # most recent service
+            en_services.max_by(&:DateProvided)&.DateProvided
+          else
+            # min of exit date, report end, current date
+            [enrollment.exit&.ExitDate, filter.end, Date.current].compact.min
+          end
+          report_enrollment.days_since_last_service = (filter.end - max_service).to_i if max_service.present?
+          intermediate_report_enrollments
+        end
+
+      Enrollment.import!(
+        intermediate_report_enrollments.values,
+        batch_size: 5_000,
+        on_duplicate_key_update: {
+          conflict_target: [:id],
+          columns: Enrollment.attribute_names.map(&:to_sym),
+        },
+      )
+      universe(internal_enrollment_issues_slug).add_universe_members(intermediate_report_enrollments) if intermediate_report_enrollments.present?
+
+      report_enrollments.merge(intermediate_report_enrollments)
+    end
+
+    private def household_issues(report_enrollments)
+    end
+
+    private def enrollment_date_issues(report_enrollments)
+    end
+
+    private def household(household_id)
+      return unless household_id.present?
+
+      @household ||= {}.tap do |households|
+        GrdaWarehouse::Hud::Enrollment.joins(:service_history_enrollment).
+          preload(:exit, client: :warehouse_client_source).
+          merge(report_scope).distinct.
+          where.not(HouseholdID: nil).
+          find_each do |enrollment|
+            report_age_date = [enrollment.EntryDate, filter.start].max
+            households[enrollment.HouseholdID] ||= []
+            households[enrollment.HouseholdID] << {
+              age: enrollment.client.age_on(report_age_date),
+              relationship_to_hoh: enrollment.RelationshipToHoH,
+            }
+          end
+      end
+      @household[household_id]
     end
 
     private def gender_issues(report_clients)
