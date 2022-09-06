8527f4cffdc5d0f135d1ef603db49e95acec1e27
Elliot Anders <elliot@greenriver.org>
2022-09-05 17:06:49 -0700

    Put back one test

--- drivers/custom_imports_boston_services/spec/models/import_file_spec.rb
+++ drivers/custom_imports_boston_services/spec/models/import_file_spec.rb
@@ -32,91 +32,91 @@ RSpec.describe CustomImportsBostonServices::ImportFile, type: :model do
     config
   end
 
-  # after(:all) do
-  #   cleanup_test_environment
-  # end
-  # after(:each) do
-  #   GrdaWarehouse::CustomImports::Config.delete_all
-  #   CustomImportsBostonServices::ImportFile.delete_all
-  #   CustomImportsBostonServices::Row.delete_all
-  #   GrdaWarehouse::Synthetic::Event.delete_all
-  #   GrdaWarehouse::Generic::Service.delete_all
-  #   GrdaWarehouse::Hud::Event.delete_all
-  # end
-  # describe 'after initial import' do
-  #   before(:each) do
-  #     CustomImportsBostonServices::ImportFile.delete_all
-  #     CustomImportsBostonServices::Row.delete_all
-  #     GrdaWarehouse::Synthetic::Event.delete_all
-  #     GrdaWarehouse::Generic::Service.delete_all
-  #     GrdaWarehouse::Hud::Event.delete_all
-  #     import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/first_service_export.csv', config, ds1)
-  #   end
-  #   it 'inserts 6 rows' do
-  #     expect(CustomImportsBostonServices::Row.count).to eq(6)
-  #   end
+  after(:all) do
+    cleanup_test_environment
+  end
+  after(:each) do
+    GrdaWarehouse::CustomImports::Config.delete_all
+    CustomImportsBostonServices::ImportFile.delete_all
+    CustomImportsBostonServices::Row.delete_all
+    GrdaWarehouse::Synthetic::Event.delete_all
+    GrdaWarehouse::Generic::Service.delete_all
+    GrdaWarehouse::Hud::Event.delete_all
+  end
+  describe 'after initial import' do
+    before(:each) do
+      CustomImportsBostonServices::ImportFile.delete_all
+      CustomImportsBostonServices::Row.delete_all
+      GrdaWarehouse::Synthetic::Event.delete_all
+      GrdaWarehouse::Generic::Service.delete_all
+      GrdaWarehouse::Hud::Event.delete_all
+      import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/first_service_export.csv', config, ds1)
+    end
+    it 'inserts 6 rows' do
+      expect(CustomImportsBostonServices::Row.count).to eq(6)
+    end
 
-  #   it 'creates 6 custom services' do
-  #     expect(GrdaWarehouse::Generic::Service.count).to eq(6)
-  #   end
+    # it 'creates 6 custom services' do
+    #   expect(GrdaWarehouse::Generic::Service.count).to eq(6)
+    # end
 
-  #   it 'creates 0 synthetic events' do
-  #     expect(GrdaWarehouse::Synthetic::Event.count).to eq(0)
-  #   end
+    # it 'creates 0 synthetic events' do
+    #   expect(GrdaWarehouse::Synthetic::Event.count).to eq(0)
+    # end
 
-  #   describe 'after hud processing' do
-  #     before do
-  #       GrdaWarehouse::Synthetic::Event.hud_sync
-  #     end
+    # describe 'after hud processing' do
+    #   before do
+    #     GrdaWarehouse::Synthetic::Event.hud_sync
+    #   end
 
-  #     it 'creates 4 synthetic events' do
-  #       expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
-  #     end
+    #   it 'creates 4 synthetic events' do
+    #     expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
+    #   end
 
-  #     it 'creates 4 HUD events' do
-  #       expect(GrdaWarehouse::Hud::Event.count).to eq(4)
-  #       expect(GrdaWarehouse::Hud::Event.where(Event: 4).count).to eq(2)
-  #       expect(GrdaWarehouse::Hud::Event.where(Event: 9).count).to eq(1)
-  #       expect(GrdaWarehouse::Hud::Event.where(Event: 10).count).to eq(1)
-  #     end
-  #   end
+    #   it 'creates 4 HUD events' do
+    #     expect(GrdaWarehouse::Hud::Event.count).to eq(4)
+    #     expect(GrdaWarehouse::Hud::Event.where(Event: 4).count).to eq(2)
+    #     expect(GrdaWarehouse::Hud::Event.where(Event: 9).count).to eq(1)
+    #     expect(GrdaWarehouse::Hud::Event.where(Event: 10).count).to eq(1)
+    #   end
+    # end
 
-  #   describe 'after second import' do
-  #     before(:each) do
-  #       CustomImportsBostonServices::ImportFile.delete_all
-  #       CustomImportsBostonServices::Row.delete_all
-  #       GrdaWarehouse::Synthetic::Event.delete_all
-  #       GrdaWarehouse::Generic::Service.delete_all
-  #       GrdaWarehouse::Hud::Event.delete_all
-  #       import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/first_service_export.csv', config, ds1)
-  #       import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/second_service_export.csv', config, ds1)
-  #     end
+    # describe 'after second import' do
+    #   before(:each) do
+    #     CustomImportsBostonServices::ImportFile.delete_all
+    #     CustomImportsBostonServices::Row.delete_all
+    #     GrdaWarehouse::Synthetic::Event.delete_all
+    #     GrdaWarehouse::Generic::Service.delete_all
+    #     GrdaWarehouse::Hud::Event.delete_all
+    #     import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/first_service_export.csv', config, ds1)
+    #     import_custom_service('drivers/custom_imports_boston_services/spec/fixtures/second_service_export.csv', config, ds1)
+    #   end
 
-  #     it 'adds 6 rows' do
-  #       expect(CustomImportsBostonServices::Row.count).to eq(12)
-  #     end
+    #   it 'adds 6 rows' do
+    #     expect(CustomImportsBostonServices::Row.count).to eq(12)
+    #   end
 
-  #     describe 'after hud processing' do
-  #       before(:each) do
-  #         GrdaWarehouse::Synthetic::Event.hud_sync
-  #       end
+    #   describe 'after hud processing' do
+    #     before(:each) do
+    #       GrdaWarehouse::Synthetic::Event.hud_sync
+    #     end
 
-  #       it 'adds and removes 1 synthetic event' do
-  #         expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
-  #         expect(CustomImportsBostonServices::Synthetic::Event.where(source_id: CustomImportsBostonServices::Row.where(service_id: 'S-1').select(:id)).count).to eq(0)
-  #         expect(CustomImportsBostonServices::Synthetic::Event.where(source_id: CustomImportsBostonServices::Row.where(service_id: 'S-7').select(:id)).count).to eq(1)
-  #       end
+    #     it 'adds and removes 1 synthetic event' do
+    #       expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
+    #       expect(CustomImportsBostonServices::Synthetic::Event.where(source_id: CustomImportsBostonServices::Row.where(service_id: 'S-1').select(:id)).count).to eq(0)
+    #       expect(CustomImportsBostonServices::Synthetic::Event.where(source_id: CustomImportsBostonServices::Row.where(service_id: 'S-7').select(:id)).count).to eq(1)
+    #     end
 
-  #       it 'creates 4 HUD events' do
-  #         expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
-  #         expect(GrdaWarehouse::Hud::Event.count).to eq(4)
-  #         expect(GrdaWarehouse::Hud::Event.where(Event: 4).count).to eq(3)
-  #         expect(GrdaWarehouse::Hud::Event.where(Event: 9).count).to eq(0)
-  #         expect(GrdaWarehouse::Hud::Event.where(Event: 10).count).to eq(1)
-  #       end
-  #     end
-  #   end
-  # end
+    #     it 'creates 4 HUD events' do
+    #       expect(GrdaWarehouse::Synthetic::Event.count).to eq(4)
+    #       expect(GrdaWarehouse::Hud::Event.count).to eq(4)
+    #       expect(GrdaWarehouse::Hud::Event.where(Event: 4).count).to eq(3)
+    #       expect(GrdaWarehouse::Hud::Event.where(Event: 9).count).to eq(0)
+    #       expect(GrdaWarehouse::Hud::Event.where(Event: 10).count).to eq(1)
+    #     end
+    #   end
+    # end
+  end
 
   def import_custom_service(file, config, data_source)
     GrdaWarehouse::Tasks::IdentifyDuplicates.new.run!
