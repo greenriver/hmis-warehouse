10ec25dcd641ffe178e85ccfc36d453702e052c8
vagrant <zparks@greenriver.org>
2022-09-14 09:43:45 -0700

    Add set HoH mutation

---
+++ drivers/hmis/app/graphql/mutations/set_ho_h_for_enrollment.rb
@@ -0,0 +1,40 @@
+module Mutations
+  class SetHoHForEnrollment < BaseMutation
+    argument :household_id, ID, required: true
+    argument :client_id, ID, required: true
+
+    field :enrollment, Types::HmisSchema::Enrollment, null: true
+    field :errors, [Types::HmisSchema::ValidationError], null: false
+
+    def resolve(household_id:, client_id:)
+      errors = []
+      enrollment = nil
+
+      client = Hmis::Hud::Client.viewable_by(current_user).find_by(id: client_id)
+
+      if client
+        household_enrollments = Hims::Hud::Enrollment.viewable_by(current_user).where(household_id: household_id)
+        hoh_enrollment = household_enrollments.find_by(relationship_to_ho_h: 1)
+        new_hoh_enrollment = household_enrollments.find_by(personal_id: client&.personal_id)
+        if new_hoh_enrollment
+          hoh_enrollment.relationship_to_ho_h = 99
+          new_hoh_enrollment.relationship_to_ho_h = 1
+
+          hoh_enrollment.save!
+          new_hoh_enrollment.save!
+
+          enrollment = new_hoh_enrollment
+        else
+          errors << InputValidationError.new("No enrollment for this client with household ID '#{household_id}'", attribute: 'household_id')
+        end
+      else
+        errors << InputValidationError.new("No client with id '#{client_id}'", attribute: 'client_id')
+      end
+
+      {
+        enrollment: enrollment,
+        errors: errors,
+      }
+    end
+  end
+end
