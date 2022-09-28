3e7a68757ec3b3f53082facaeed1c910dcb61b2c
Sandy Wise <sandy@greenriver.org>
2022-09-28 13:38:09 -0400

    Use answer_from_section instead of iterating over all the answers.

--- drivers/ma_yya_report/extensions/grda_warehouse/hmis_form_extension.rb
+++ drivers/ma_yya_report/extensions/grda_warehouse/hmis_form_extension.rb
@@ -5,20 +5,13 @@ module MaYyaReport
 
       def current_school_attendance
         # First look for the HUD questions
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            return HUD.current_school_attended(q[:answer], true) if q[:answer].present? && q[:question] == 'Current school enrollment and attendance'
-          end
-        end
+        hud_answer = answer_from_section(section_starts_with('First Page'), 'Current school enrollment')
+        return HUD.most_recent_ed_status(hud_answer, true) if hud_answer.present?
 
         # If we didn't find one, try the older format
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            response = q[:answer] if q[:answer].present? && q[:question] == 'Level of education'
-            return 2 if response.contains?('YYA is currently enrolled in college|') # Assume attending if enrolled
-            return 0 if response.contains?('YYA has completed high school or GED/HISET|') # If completed, but not in college, assume not enrolled
-          end
-        end
+        legacy_answers = answer_from_section(section_starts_with('First Page'), 'Level of education')
+        return 2 if legacy_answers&.contains?('YYA is currently enrolled in college|') # Assume attending if enrolled
+        return 0 if legacy_answers&.contains?('YYA has completed high school or GED/HISET|') # If completed, but not in college, assume not enrolled
 
         # Finally, give up
         nil
@@ -26,20 +19,13 @@ module MaYyaReport
 
       def most_recent_educational_status
         # First look for the HUD questions
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            return HUD.most_recent_ed_status(q[:answer], true) if q[:answer].present? && q[:question] == 'C3.A'
-          end
-        end
+        hud_answer = answer_from_section(section_starts_with('First Page'), 'C3.A')
+        return HUD.most_recent_ed_status(hud_answer, true) if hud_answer.present?
 
         # If we didn't find one, try the older format
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            response = q[:answer] if q[:answer].present? && q[:question] == 'Level of education'
-            return 2 if response.contains?('YYA is currently enrolled in college|') # Assume attending if enrolled
-            return 0 if response.contains?('YYA has completed high school or GED/HISET|') # If completed, but not in college, assume not enrolled
-          end
-        end
+        legacy_answers = answer_from_section(section_starts_with('First Page'), 'Level of education')
+        return 2 if legacy_answers&.contains?('YYA is currently enrolled in college|') # Assume attending if enrolled
+        return 0 if legacy_answers&.contains?('YYA has completed high school or GED/HISET|') # If completed, but not in college, assume not enrolled
 
         # Finally, give up
         nil
@@ -47,20 +33,13 @@ module MaYyaReport
 
       def current_educational_status
         # First look for the HUD questions
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            return HUD.current_ed_status(q[:answer], true) if q[:answer].present? && q[:question] == 'C3.B'
-          end
-        end
+        hud_answer = answer_from_section(section_starts_with('First Page'), 'C3.B')
+        return HUD.most_recent_ed_status(hud_answer, true) if hud_answer.present?
 
         # If we didn't find one, try the older format
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            response = q[:answer] if q[:answer].present? && q[:question] == 'Level of education'
-            return 2 if response.contains?('YYA is currently enrolled in college|') # If they are in college, call it a 4-year
-            return nil if response.contains?('YYA has completed high school or GED/HISET|') # reported in most_recent_educational_status
-          end
-        end
+        legacy_answers = answer_from_section(section_starts_with('First Page'), 'Level of education')
+        return 2 if legacy_answers&.contains?('YYA is currently enrolled in college|') # If they are in college, call it a 4-year
+        return nil if legacy_answers&.contains?('YYA has completed high school or GED/HISET|') # reported in most_recent_educational_status
 
         # Finally, give up
         nil
@@ -68,17 +47,12 @@ module MaYyaReport
 
       def flex_funds
         flex_funds = []
-        answers[:sections].each do |s|
-          s[:questions].each do |q|
-            response = q[:answer] if q[:answer].present? && q[:question] == 'Direct financial assistance provided (check all that apply)'
-            next unless response.present?
-
-            parts = response.split('|')
-            parts.each do |part|
-              part = 'Move-in' if part.start_with?('Move-in')
-              part = 'Other' if part.start_with?('Other')
-              flex_funds << part
-            end
+        answers = answer_from_section(section_starts_with('First Page'), 'Direct financial assistance')
+        if answers.present?
+          answers.split('|').each do |part|
+            part = 'Move-in' if part.start_with?('Move-in')
+            part = 'Other' if part.start_with?('Other')
+            flex_funds << part
           end
         end
         flex_funds.uniq
