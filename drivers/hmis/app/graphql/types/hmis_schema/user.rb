34f998353c175cd87fcfd28a661458a8b48a6b21
Zach Parks <zparks88@gmail.com>
2022-10-18 11:10:03 -0400

    Update drivers/hmis/app/graphql/types/hmis_schema/user.rb
    Co-authored-by: Gig <gig@greenriver.org>

--- drivers/hmis/app/graphql/types/hmis_schema/user.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/user.rb
@@ -15,7 +15,7 @@ module Types
     field :date_created, GraphQL::Types::ISO8601DateTime, null: true
 
     def name
-      [user_first_name, user_last_name].compact.join(' ')
+      [object.user_first_name, object.user_last_name].compact.join(' ')
     end
   end
 end
