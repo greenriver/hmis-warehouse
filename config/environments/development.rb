3c0a9869cd48404961fe2931ad19468ad7bc3cdd
Elliot Anders <elliot@greenriver.org>
2022-10-21 09:28:48 -0700

    Revert accidental change to dev class loading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.hosts = [ /.*/ ]
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
