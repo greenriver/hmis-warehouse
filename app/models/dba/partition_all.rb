ab32613bb0893a6144e8fa52a7312d2fe4c10994
Todd Blackman <tdblackman@gmail.com>
2022-10-27 18:07:14 -0400

    remove 2020 tables from partitioning

--- app/models/dba/partition_all.rb
+++ app/models/dba/partition_all.rb
@@ -1,14 +1,5 @@
 class DBA::PartitionAll
   TABLES = %w[
-    hmis_2020_affiliations hmis_2020_aggregated_enrollments
-    hmis_2020_aggregated_exits hmis_2020_assessment_questions
-    hmis_2020_assessment_results hmis_2020_assessments hmis_2020_clients
-    hmis_2020_current_living_situations hmis_2020_disabilities
-    hmis_2020_employment_educations hmis_2020_enrollment_cocs
-    hmis_2020_enrollments hmis_2020_events hmis_2020_exits hmis_2020_exports
-    hmis_2020_funders hmis_2020_health_and_dvs hmis_2020_income_benefits
-    hmis_2020_inventories hmis_2020_organizations hmis_2020_project_cocs
-    hmis_2020_projects hmis_2020_services hmis_2020_users
     hmis_2022_affiliations hmis_2022_assessment_questions
     hmis_2022_assessment_results hmis_2022_assessments hmis_2022_clients
     hmis_2022_current_living_situations hmis_2022_disabilities
@@ -17,23 +8,13 @@ class DBA::PartitionAll
     hmis_2022_funders hmis_2022_health_and_dvs hmis_2022_income_benefits
     hmis_2022_inventories hmis_2022_organizations hmis_2022_project_cocs
     hmis_2022_projects hmis_2022_services hmis_2022_users
-    hmis_2022_youth_education_statuses hmis_csv_2020_affiliations
-    hmis_csv_2020_assessment_questions hmis_csv_2020_assessment_results
-    hmis_csv_2020_assessments hmis_csv_2020_clients
-    hmis_csv_2020_current_living_situations hmis_csv_2020_disabilities
-    hmis_csv_2020_employment_educations hmis_csv_2020_enrollment_cocs
-    hmis_csv_2020_enrollments hmis_csv_2020_events hmis_csv_2020_exits
-    hmis_csv_2020_exports hmis_csv_2020_funders hmis_csv_2020_health_and_dvs
-    hmis_csv_2020_income_benefits hmis_csv_2020_inventories
-    hmis_csv_2020_organizations hmis_csv_2020_project_cocs
-    hmis_csv_2020_projects hmis_csv_2020_services hmis_csv_2020_users
-    hmis_csv_2022_affiliations hmis_csv_2022_assessment_questions
-    hmis_csv_2022_assessment_results hmis_csv_2022_assessments
-    hmis_csv_2022_clients hmis_csv_2022_current_living_situations
-    hmis_csv_2022_disabilities hmis_csv_2022_employment_educations
-    hmis_csv_2022_enrollment_cocs hmis_csv_2022_enrollments
-    hmis_csv_2022_events hmis_csv_2022_exits hmis_csv_2022_exports
-    hmis_csv_2022_funders hmis_csv_2022_health_and_dvs
+    hmis_2022_youth_education_statuses hmis_csv_2022_affiliations
+    hmis_csv_2022_assessment_questions hmis_csv_2022_assessment_results
+    hmis_csv_2022_assessments hmis_csv_2022_clients
+    hmis_csv_2022_current_living_situations hmis_csv_2022_disabilities
+    hmis_csv_2022_employment_educations hmis_csv_2022_enrollment_cocs
+    hmis_csv_2022_enrollments hmis_csv_2022_events hmis_csv_2022_exits
+    hmis_csv_2022_exports hmis_csv_2022_funders hmis_csv_2022_health_and_dvs
     hmis_csv_2022_income_benefits hmis_csv_2022_inventories
     hmis_csv_2022_organizations hmis_csv_2022_project_cocs
     hmis_csv_2022_projects hmis_csv_2022_services hmis_csv_2022_users
