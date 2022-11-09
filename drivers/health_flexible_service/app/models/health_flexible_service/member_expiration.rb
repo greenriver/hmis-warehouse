0a7af3d2952a84d15cfab57501601d33c6c7a70c
Gig Ashton <gig@greenriver.org>
2022-11-09 14:46:45 -0500

    Exclude some PII from some exports

--- drivers/health_flexible_service/app/models/health_flexible_service/member_expiration.rb
+++ drivers/health_flexible_service/app/models/health_flexible_service/member_expiration.rb
@@ -21,5 +21,39 @@ module HealthFlexibleService
     def report_scope_source
       HealthFlexibleService::Vpr
     end
+
+    def self.headers
+      [
+        'Medicaid ID',
+        'Last Name',
+        'First Name',
+        'VPR Start Date',
+        'VPR End Date',
+      ]
+    end
+
+    def self.headers_for_export
+      return headers if ::GrdaWarehouse::Config.get(:include_pii_in_detail_downloads)
+
+      headers.excluding(['Last Name', 'First Name'])
+    end
+
+    def self.columns_for_export(vpr)
+      if ::GrdaWarehouse::Config.get(:include_pii_in_detail_downloads)
+        [
+          vpr.medicaid_id,
+          vpr.last_name,
+          vpr.first_name,
+          vpr.planned_on,
+          vpr.end_date,
+        ]
+      else
+        [
+          vpr.medicaid_id,
+          vpr.planned_on,
+          vpr.end_date,
+        ]
+      end
+    end
   end
 end
