d2eec88df9a34d6e6998ae546ac9eae6814b486e
Elliot Anders <elliot@greenriver.org>
2022-11-14 10:13:29 -0800

    Don't show all clients; handle missing CLS date

--- drivers/ma_yya_followup_report/app/models/ma_yya_followup_report/report.rb
+++ drivers/ma_yya_followup_report/app/models/ma_yya_followup_report/report.rb
@@ -9,21 +9,24 @@ module MaYyaFollowupReport
     include ArelHelper
     include Filter::FilterScopes
 
+    attr_accessor :start_date, :late_date, :filter
+
     def initialize(filter)
       @filter = filter
-      @end_date = @filter.on
-      @start_date = @end_date - 2.months - 3.weeks
+      filter.require_service_during_range = false
+      @end_date = filter.on
       @late_date = @end_date - 3.months
+      @start_date = @late_date + 1.weeks
     end
 
-    attr_accessor :start_date, :late_date
-
     def clients
+      return [] unless filter.project_ids.any? || filter.age_ranges.any?
+
       client_scope.pluck(*columns.values).
         map do |row|
           Hash[columns.keys.zip(row)]
         end.
-        sort_by { |row| row[:last_seen] }
+        sort_by { |row| row[:last_seen] || row[:engagement_date] }
     end
 
     def columns
@@ -59,10 +62,8 @@ module MaYyaFollowupReport
     end
 
     private def enrollment_scope
-      scope = ::GrdaWarehouse::ServiceHistoryEnrollment.
-        entry.
-        open_between(start_date: @start_date, end_date: @end_date)
-
+      scope = ::GrdaWarehouse::ServiceHistoryEnrollment.entry
+      scope = filter_for_range(scope)
       scope = filter_for_projects(scope)
       filter_for_age(scope)
     end
@@ -72,15 +73,15 @@ module MaYyaFollowupReport
         between(start_date: @start_date, end_date: @end_date)
     end
 
-    def self.yya_projects(user)
-      ::GrdaWarehouse::Hud::Project.options_for_select(user: user)
+    def yya_projects(user)
+      filter.project_options_for_select(user: user)
     end
 
-    def self.available_age_ranges
+    def available_age_ranges
       {
         under_eighteen: '< 18',
         eighteen_to_twenty_four: '18 - 24',
-      }
+      }.freeze
     end
   end
 end
