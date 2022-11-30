e4b50eebc44fac10274bfa8cb4720346d1768e0a
Gig Ashton <gig@greenriver.org>
2022-11-30 14:43:02 -0500

    Make reversible and support running before gettext

--- db/warehouse/migrate/20221130180430_migrate_dashboard_translations.rb
+++ db/warehouse/migrate/20221130180430_migrate_dashboard_translations.rb
@@ -1,55 +1,57 @@
 class MigrateDashboardTranslations < ActiveRecord::Migration[6.1]
-  mappings = [
-    {
-      old: 'Search',
-      new: [
-        'Clients in Search',
-        'Clients Entering Search',
-        'entered Search during the reporting period',
-        'Clients Exiting Search',
-        'exited Search during the reporting period',
-        'Average Time in Search for the Reporting Period',
-        'Time in Search with Exits to Housing',
-        'Average Time in Search for the Reporting Period',
-        'Time in Search with an Exit not to Housing',
-        'Average Time in Search for the Reporting Period',
-        'Time in Search with an Exit to Any Destination']
-    },
-    {
-      old: 'search',
-      new: [
-        'were in search',
-        'was in search',
-        'Percentage of clients exiting search to housing out of those who exited search',
-        'Percentage of clients enrolled in housing out of those enrolled in either search or housing']
-    },
-    {
-      old: 'Housing',
-      new: [
-        'Clients in Housing',
-        'Clients Entering Housing',
-        'entered Housing during the reporting period',
-        'Clients Exited Housing','exited Housing during the reporting period',
-        'Time in Search with Exits to Housing',
-        'Time in Search with an Exit not to Housing',
-        'Time in Housing with an Exit to Any Destination',
-        'Average Time in Housing for the Reporting Period']
-    },
-    {
-      old: 'housing',
-      new: [
-        'were enrolled in housing',
-        'was enrolled in housing',
-        'Percentage of clients exiting search to housing out of those who exited search',
-        'Percentage of clients enrolled in housing out of those enrolled in either search or housing',
-        'Percentage of clients exiting housing to a permanent destination out of those who exited housing']
-    }
-  ]
+  def translation_mappings
+    [
+      {
+        old: 'Search',
+        new: [
+          'Clients in Search',
+          'Clients Entering Search',
+          'entered Search during the reporting period',
+          'Clients Exiting Search',
+          'exited Search during the reporting period',
+          'Average Time in Search for the Reporting Period',
+          'Time in Search with Exits to Housing',
+          'Average Time in Search for the Reporting Period',
+          'Time in Search with an Exit not to Housing',
+          'Average Time in Search for the Reporting Period',
+          'Time in Search with an Exit to Any Destination']
+      },
+      {
+        old: 'search',
+        new: [
+          'were in search',
+          'was in search',
+          'Percentage of clients exiting search to housing out of those who exited search',
+          'Percentage of clients enrolled in housing out of those enrolled in either search or housing']
+      },
+      {
+        old: 'Housing',
+        new: [
+          'Clients in Housing',
+          'Clients Entering Housing',
+          'entered Housing during the reporting period',
+          'Clients Exited Housing','exited Housing during the reporting period',
+          'Time in Search with Exits to Housing',
+          'Time in Search with an Exit not to Housing',
+          'Time in Housing with an Exit to Any Destination',
+          'Average Time in Housing for the Reporting Period']
+      },
+      {
+        old: 'housing',
+        new: [
+          'were enrolled in housing',
+          'was enrolled in housing',
+          'Percentage of clients exiting search to housing out of those who exited search',
+          'Percentage of clients enrolled in housing out of those enrolled in either search or housing',
+          'Percentage of clients exiting housing to a permanent destination out of those who exited housing']
+      }
+    ]
+  end
 
   # Generate translations for new keys based on old ones (if any)
   def up
     updated = []
-    mappings.each do |m|
+    translation_mappings.each do |m|
       old_key = m[:old]
       new_keys = m[:new]
       key = TranslationKey.find_by(key: old_key)
@@ -58,7 +60,8 @@ class MigrateDashboardTranslations < ActiveRecord::Migration[6.1]
       key.translations.where.not(text: nil).each do |t|
         # Iterate through all new translations that use 'Search'
         new_keys.each do |k|
-          new_key = TranslationKey.find_by(key: k)
+          new_key = TranslationKey.where(key: k).first_or_create
+          # Create a translation for the new key ('Clients in Search')
           translation = TranslationText.find_or_create_by(
             locale: t.locale,
             translation_key_id: new_key.id
@@ -76,7 +79,7 @@ class MigrateDashboardTranslations < ActiveRecord::Migration[6.1]
 
   # Delete all translations for the new keys
   def down
-    mappings.each do |m|
+    translation_mappings.each do |m|
       m[:new].each do |k|
         key = TranslationKey.find_by(key: k)
         key.translations.where.not(text: nil).destroy_all
