8c3314e9e8a03efe042ec10bbb113de9aef1d45e
Gig Ashton <gig@greenriver.org>
2022-12-09 14:53:09 -0500

    Only run initializer in development

--- drivers/hmis/config/initializers/graphiql.rb
+++ drivers/hmis/config/initializers/graphiql.rb
@@ -1,9 +1,10 @@
-Rails.configuration.to_prepare do
-  # Add before_action to GraphiQL to sign the current user in as an HMIS user.
-  # This controller is only used in development.
-  GraphiQL::Rails::EditorsController.class_eval do
-    before_action do
-      sign_in(:hmis_user, current_user)
+if Rails.env.development?
+  Rails.configuration.to_prepare do
+    # For GraphiQL, sign the current user in as an HMIS user
+    GraphiQL::Rails::EditorsController.class_eval do
+      before_action do
+        sign_in(:hmis_user, current_user)
+      end
     end
   end
 end
