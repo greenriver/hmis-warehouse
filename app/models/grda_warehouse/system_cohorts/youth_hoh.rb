83aaeac21683f775a6814a35b0b76b865eb2d0a5
Elliot Anders <elliot@greenriver.org>
2022-12-12 13:21:33 -0800

    Include non-homeless enrollments in youth HoH system cohort if a project group is defined

--- app/models/grda_warehouse/system_cohorts/youth_hoh.rb
+++ app/models/grda_warehouse/system_cohorts/youth_hoh.rb
@@ -46,5 +46,55 @@ module GrdaWarehouse::SystemCohorts
         end
       end
     end
+
+    private def candidate_enrollments
+      return super unless project_group.present?
+
+      @candidate_enrollments ||= enrollment_source.
+        # homeless. # Not limiting to homeless since we're limiting by project group
+        ongoing(on_date: @processing_date). # who's enrollment is open today
+        with_service_between(start_date: inactive_date, end_date: @processing_date). # who received service in the past 90 days
+        where.not( # who didn't receive a non-homeless (housed) service on the processing date
+          client_id: service_history_source.
+            where(date: @processing_date, homeless: false).
+            select(:client_id),
+        ).
+        where.not(client_id: moved_in_ph). # who aren't currently enrolled and moved-in to PH
+        where.not(client_id: cohort_clients.select(:client_id)). # who aren't on the cohort currently
+        group(:client_id).minimum(:first_date_in_program)
+    end
+
+    private def active_ongoing_homeless_enrollments
+      return super unless project_group.present?
+
+      enrollment_source.
+        # homeless. # Not limiting to homeless since we're limiting by project group
+        ongoing(on_date: @processing_date).
+        with_service_between(start_date: inactive_date, end_date: @processing_date, service_scope: :homeless).
+        where(client_id: cohort_clients.select(:client_id)).
+        distinct.
+        pluck(:client_id)
+    end
+
+    private def active_client_ids
+      return super unless project_group.present?
+
+      enrollment_source.
+        # homeless. # Not limiting to homeless since we're limiting by project group
+        ongoing(on_date: @processing_date).
+        where(client_id: cohort_clients.select(:client_id)).
+        joins(:service_history_services).
+        where(shs_t[:date].between(inactive_date..@processing_date)).
+        distinct.
+        pluck(:client_id)
+    end
+
+    private def with_homeless_enrollment
+      return super unless project_group.present?
+
+      enrollment_source.
+        # homeless. # Not limiting to homeless since we're limiting by project group
+        ongoing(on_date: @processing_date)
+    end
   end
 end
