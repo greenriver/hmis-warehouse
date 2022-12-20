c4561961b282bb833a1882193f9621c4ed6af7ad
vagrant <zparks@greenriver.org>
2022-12-20 08:07:34 -0800

    Adjust client sort option descriptions

--- drivers/hmis/app/graphql/types/hmis_schema/client_sort_option.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/client_sort_option.rb
@@ -12,7 +12,7 @@ module Types
     graphql_name 'ClientSortOption'
 
     Hmis::Hud::Client::SORT_OPTIONS.each do |opt|
-      value opt.to_s.upcase, value: opt, description: opt.to_s.titleize
+      value opt.to_s.upcase, value: opt, description: Hmis::Hud::Client::SORT_OPTION_DESCRIPTIONS[opt]
     end
   end
 end
