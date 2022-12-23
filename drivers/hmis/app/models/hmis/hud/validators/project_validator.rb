d581014379cbcf8a53cfe03f640c5dee413ba75a
Gig Ashton <gig@greenriver.org>
2022-12-23 09:49:06 -0500

    Add more date validations for project, funder, inventory

--- drivers/hmis/app/models/hmis/hud/validators/project_validator.rb
+++ drivers/hmis/app/models/hmis/hud/validators/project_validator.rb
@@ -13,6 +13,9 @@ class Hmis::Hud::Validators::ProjectValidator < Hmis::Hud::Validators::BaseValid
       unless skipped_attributes(record).include?(:project_type)
         record.errors.add :project_type, :required if record.project_type.nil? && record.continuum_project != 1
       end
+
+      # End date must be after start date
+      record.errors.add :operating_end_date, :invalid, message: 'must be on or after start date' if record.operating_end_date && record.operating_start_date && record.operating_end_date < record.operating_start_date
     end
   end
 end
