451e3c2d0d1881ad73388234e508a55e5ce58474
Gig Ashton <gig@greenriver.org>
2023-01-02 17:33:07 -0500

    Clean up mutation

--- drivers/hmis/app/graphql/mutations/update_inventory.rb
+++ drivers/hmis/app/graphql/mutations/update_inventory.rb
@@ -8,25 +8,11 @@ module Mutations
 
     def resolve(id:, input:)
       record = Hmis::Hud::Inventory.editable_by(current_user).find_by(id: id)
-      closes_inventory = record.present? && record.inventory_end_date.blank? && input.inventory_end_date.present?
-      response = default_update_record(
+      default_update_record(
         record: record,
         field_name: :inventory,
         input: input,
       )
-
-      inventory = response[:inventory]
-      return response unless inventory.present?
-
-      close_related_records(inventory) if closes_inventory
-
-      # TODO: warn if closing inventory that has beds that are currently assigned to clients
-
-      response
-    end
-
-    def close_related_records(inventory)
-      # TODO: close related Beds and Units?
     end
   end
 end
