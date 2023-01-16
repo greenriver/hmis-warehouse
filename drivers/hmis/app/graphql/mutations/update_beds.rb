ec845507e46bd31ea0672f27f6a05896e5db48ad
Gig Ashton <gig@greenriver.org>
2023-01-16 16:48:04 -0500

    Add gender to bed mutation

--- drivers/hmis/app/graphql/mutations/update_beds.rb
+++ drivers/hmis/app/graphql/mutations/update_beds.rb
@@ -3,6 +3,7 @@ module Mutations
     argument :inventory_id, ID, required: true
     argument :bed_ids, [ID], required: true
     argument :name, String, required: false
+    argument :gender, String, required: false
     argument :unit, ID, required: false
 
     field :beds, [Types::HmisSchema::Bed], null: false
@@ -14,7 +15,7 @@ module Mutations
 
       beds = inventory.beds.where(id: bed_ids)
       common = { user_id: hmis_user.user_id, updated_at: Time.now }
-      beds.update_all(name: name, **common) if name.present?
+      beds.update_all(gender: gender, name: name, **common)
       beds.update_all(unit_id: unit.to_i, **common) if unit.present?
 
       { beds: beds, errors: [] }
