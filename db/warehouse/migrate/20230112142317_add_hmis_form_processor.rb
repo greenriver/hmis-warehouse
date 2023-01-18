4f0a3ec49098c9f45da59ba769444b8418a256df
Sandy Wise <sandy@greenriver.org>
2023-01-18 09:17:08 -0500

    Move to using Rails IDs instead of HUD keys,

--- db/warehouse/migrate/20230112142317_add_hmis_form_processor.rb
+++ db/warehouse/migrate/20230112142317_add_hmis_form_processor.rb
@@ -3,16 +3,15 @@ class AddHmisFormProcessor < ActiveRecord::Migration[6.1]
     add_reference :hmis_assessment_details, :processor
 
     create_table :hmis_assessment_processors do |t|
-      # HMIS keys are strings
-      t.string :enrollment_coc_id
-      t.string :health_and_dv_id
-      t.string :income_benefit_id
-      t.string :physical_disability_id
-      t.string :developmental_disability_id
-      t.string :chronic_health_condition_id
-      t.string :hiv_aids_id
-      t.string :mental_health_disorder_id
-      t.string :substance_use_disorder_id
+      t.references :enrollment_coc
+      t.references :health_and_dv
+      t.references :income_benefit
+      t.references :physical_disability
+      t.references :developmental_disability
+      t.references :chronic_health_condition
+      t.references :hiv_aids
+      t.references :mental_health_disorder
+      t.references :substance_use_disorder
     end
   end
 end
