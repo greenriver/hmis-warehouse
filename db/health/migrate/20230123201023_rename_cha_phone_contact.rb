6cbc25f386dec9801c9cf221943dc1566ab15e71
Sandy Wise <sandy@greenriver.org>
2023-01-23 15:37:53 -0500

    Disable migration as CHA has a problematic cross-DB issue.

--- db/health/migrate/20230123201023_rename_cha_phone_contact.rb
+++ db/health/migrate/20230123201023_rename_cha_phone_contact.rb
@@ -1,5 +1,5 @@
 class RenameChaPhoneContact < ActiveRecord::Migration[6.1]
   def up
-    Health::ComprehensiveHealthAssessment.where(collection_method: :phone).update_all(collection_method: :phone_call)
+    # Health::ComprehensiveHealthAssessment.where(collection_method: :phone).update_all(collection_method: :phone_call)
   end
 end
