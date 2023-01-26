0485072384bfd05e917de565afb5adac3fcbfbd2
Elliot Anders <elliot@greenriver.org>
2023-01-26 08:55:54 -0800

    WIP; additional calculations

--- drivers/ma_reports/app/models/ma_reports/monthly_performance/enrollment.rb
+++ drivers/ma_reports/app/models/ma_reports/monthly_performance/enrollment.rb
@@ -10,5 +10,13 @@ module MaReports::MonthlyPerformance
     acts_as_paranoid
 
     has_many :simple_reports_universe_members, inverse_of: :universe_membership, class_name: 'SimpleReports::UniverseMember', foreign_key: :universe_membership_id
+
+    scope :open_between, ->(range) do
+      a_t = arel_table
+      entry_date = a_t[:entry_date]
+      exit_date = a_t[:exit_date]
+      # Currently does not count as an overlap if one starts on the end of the other
+      where(exit_date.gteq(range.first).or(exit_date.eq(nil)).and(entry_date.lteq(range.last)))
+    end
   end
 end
--- drivers/ma_reports/app/models/ma_reports/monthly_performance/report.rb
+++ drivers/ma_reports/app/models/ma_reports/monthly_performance/report.rb
@@ -61,7 +61,7 @@ module MaReports::MonthlyPerformance
 
     def filter
       @filter ||= begin
-        f = Filters::FilterBase.new(user_id: user_id, enforce_one_year_range: false)
+        f = ::Filters::FilterBase.new(user_id: user_id, enforce_one_year_range: false)
         f.update(options)
       end
     end
@@ -79,19 +79,20 @@ module MaReports::MonthlyPerformance
           client = enrollment.client
           client_start_date = [filter.start_date, enrollment.first_date_in_program].max
           age = client.age_on(client_start_date)
-
           household_id = enrollment.enrollment.household_id || "#{enrollment.enrollment_group_id}*hh"
-          cocs = enrollment.project.project_cocs || enrollment.project.build_project_coc
+          project = enrollment.project
+          cocs = project.project_cocs || project.build_project_coc
+
           cocs.each do |project_coc|
             new_enrollment = Enrollment.new(
-              report: id,
-              client: client.id,
-              enrollment: enrollment.enrollment.id,
-              project: enrollment.project.id,
-              project_coc: project_coc,
+              report_id: id,
+              client_id: client.id,
+              enrollment_id: enrollment.enrollment.id,
+              project_id: project.id,
+              project_coc_id: project_coc,
               personal_id: client.personal_id,
-              city: project.City,
-              coc_code: project_coc.coc_code,
+              city: project_coc.City,
+              coc_code: project_coc.effective_coc_code,
               entry_date: enrollment.first_date_in_program,
               exit_date: enrollment.last_date_in_program,
               latest_for_client: enrollment.id == @last_enrollment_ids[enrollment.client_id],
@@ -105,26 +106,72 @@ module MaReports::MonthlyPerformance
               ethnicity: client.ethnicity == 1,
               male: client.male == 1,
               female: client.female == 1,
-              gender_other: client.gender_other == 1,
-              transgender: client.trangender == 1,
+              transgender: client.transgender == 1,
               questioning: client.questioning == 1,
               no_single_gender: client.no_single_gender == 1,
-              disabling_condition: enrollment.disabling_condition == 1,
+              disabling_condition: enrollment.enrollment.disabling_condition == 1,
               reporting_age: age,
-              relationship_to_hoh: enrollment.enrollment.relationship_to_hoh,
+              relationship_to_hoh: enrollment.enrollment.relationship_to_ho_h,
               household_id: household_id,
               household_type: household_makeup(get_hh_id(enrollment), enrollment.first_date_in_program),
               household_members: households[get_hh_id(enrollment)],
-              prior_living_situation: enrollment.enrollment.prior_living_situation,
+              prior_living_situation: enrollment.enrollment.living_situation,
               months_homeless_past_three_years: enrollment.enrollment.months_homeless_past_three_years,
               times_homeless_past_three_years: enrollment.enrollment.times_homeless_past_three_years,
             )
-            projects[enrollment.project] ||= {} # TODO
-            enrollment_batch[enrollment] = new_enrollment
+            # Collect up some project information so we don't have to go back for it
+            projects[project] ||= {
+              report_id: id,
+              project_id: project.id,
+              project_coc_id: project_coc.id,
+              project_name: project.name,
+              organization_name: project.organization_name,
+              coc_code: project_coc.effective_coc_code,
+              city: project_coc.city,
+              month_start: month_start,
+              available_beds: available_beds,
+              average_length_of_stay_in_days: average_length_of_stay_in_days,
+              number_chronically_homeless_at_entry: number_chronically_homeless_at_entry,
+            }
+            enrollment_batch[enrollment.id] = new_enrollment
           end
         end
         Enrollment.import(enrollment_batch.values)
         universe.add_universe_members(enrollment_batch)
+      end
+
+      Project.import(project_data(projects))
+    end
+
+    private def project_data(projects)
+      monthly_projects = []
+      a_t = MaReports::MonthlyPerformance::Enrollment.arel_table
+      months.each do |month_start, month_end|
+        projects.each do |project, project_data|
+          available_beds = project.inventories.map { |i| i.average_daily_inventory(range: month_start .. month_end, field: :BedInventory) || 0 }.sum
+          enrollments_for_project = universe.members.where(a_t[:project_id].eq(project.id)).
+            merge(MaReports::MonthlyPerformance::Enrollment.open_between(month_start..month_end))
+          length_of_stays_in_days = enrollments_for_project.pluck(:stay_length_in_days)
+          number_chronically_homeless_at_entry = enrollments_for_project.where(chronically_homeless_at_entry: true).count
+          monthly_projects << project_data.merge(
+            month_start: month_start,
+            available_beds: available_beds,
+            average_length_of_stay_in_days: average(length_of_stays_in_days.sum, length_of_stays_in_days.count),
+            number_chronically_homeless_at_entry: number_chronically_homeless_at_entry,
+          )
+        end
+      end
+    end
+
+    # Returns an array of months covered by the report in the form:
+    # [['2021-05-01', '2021-05-31']]
+    def months
+      @months ||= [].tap do |dates|
+        date = filter.start_date.beginning_of_month
+        while date <= filter.end_date.end_of_month
+          dates << [date, date.end_of_month]
+          date += 1.months
+        end
       end
     end
 
@@ -136,6 +183,12 @@ module MaReports::MonthlyPerformance
     def enrollment_scope_without_preloads
       scope = GrdaWarehouse::ServiceHistoryEnrollment.entry
       filter.apply(scope)
+    end
+
+    def average(value, count)
+      return 0 unless count.positive?
+
+      value.to_f / count
     end
 
     private def calculate_households
@@ -150,7 +203,7 @@ module MaReports::MonthlyPerformance
             @hoh_enrollments[enrollment.client_id] = enrollment if enrollment.head_of_household?
             next unless enrollment&.enrollment&.client.present?
 
-            date = [enrollment.first_date_in_program, @report.start_date].max
+            date = [enrollment.first_date_in_program, filter.start_date].max
             age = GrdaWarehouse::Hud::Client.age(date: date, dob: enrollment.enrollment.client.DOB&.to_date)
             @households[get_hh_id(enrollment)] ||= []
             @households[get_hh_id(enrollment)] << {
@@ -203,6 +256,43 @@ module MaReports::MonthlyPerformance
 
     def report_end_date
       filter.end_date
+    end
+
+    def key_for_display(key)
+      case key
+      when 'data_source_ids'
+        'Data Sources'
+      when 'project_ids'
+        'Projects'
+      when 'organization_ids'
+        'Organizations'
+      else
+        key.humanize.titleize
+      end
+    end
+
+    def value_for_display(key, value)
+      value = case key
+      when 'user_id'
+        User.find_by(id: value)&.name
+      when 'sub_population'
+        GrdaWarehouse::WarehouseReports::Dashboard::Base.available_sub_populations.invert[value.to_sym]
+      when 'data_source_ids'
+        GrdaWarehouse::DataSource.where(id: value).map(&:short_name)
+      when 'project_ids'
+        # We can ignore confidential status here because this renders the selected project list,
+        # and confidential projects are only selectable for users who can view their names.
+        GrdaWarehouse::Hud::Project.where(id: value).map { |project| project.name_and_type(ignore_confidential_status: true) }
+      when 'organization_ids'
+        GrdaWarehouse::Hud::Organization.where(id: value).map(&:OrganizationName)
+      else
+        value
+      end
+      return value unless value.is_a?(Array)
+
+      text = value[0...5].to_sentence(last_word_connector: ', ').to_s
+      text += ', ...' if value.count > 5
+      text
     end
   end
 end
