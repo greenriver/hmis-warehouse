bf1a3fc8bff1875a9799e1af698601ebdb928b17
Gig Ashton <gig@greenriver.org>
2023-02-02 10:38:55 -0500

    Update descriptions

--- drivers/hmis/app/graphql/types/forms/autofill_value.rb
+++ drivers/hmis/app/graphql/types/forms/autofill_value.rb
@@ -10,10 +10,10 @@ module Types
   class Forms::AutofillValue < Types::BaseObject
     description 'Value to autofill based on conditional logic'
 
-    field :value_code, String, 'If question is choice type, autofill value', null: true
-    field :value_number, Integer, 'If question is numeric, autofill value', null: true
-    field :value_boolean, Boolean, 'If question is boolean type, autofill value', null: true
-    field :value_question, String, 'Link ID of question to autofill value from', null: true
+    field :value_code, String, 'Value to autofill if condition is met', null: true
+    field :value_number, Integer, 'Value to autofill if condition is met', null: true
+    field :value_boolean, Boolean, 'Value to autofill if condition is met', null: true
+    field :value_question, String, 'Link ID of question to autofill the value from if condition is med', null: true
     field :autofill_behavior, Types::Forms::Enums::EnableBehavior, null: false
     field :autofill_when, [Types::Forms::EnableWhen], null: false
   end
