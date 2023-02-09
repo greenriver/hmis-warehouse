22f5a953f7a01ef694e4a0585d4c8bb1e5352f44
Gig Ashton <gig@greenriver.org>
2023-02-09 08:49:31 -0500

    Add support for specifying sum in form item

--- drivers/hmis/app/graphql/types/forms/autofill_value.rb
+++ drivers/hmis/app/graphql/types/forms/autofill_value.rb
@@ -10,10 +10,13 @@ module Types
   class Forms::AutofillValue < Types::BaseObject
     description 'Value to autofill based on conditional logic'
 
+    # Value to autofill. Only 1 of the below should be specified
     field :value_code, String, 'Value to autofill if condition is met', null: true
     field :value_number, Integer, 'Value to autofill if condition is met', null: true
     field :value_boolean, Boolean, 'Value to autofill if condition is met', null: true
-    field :value_question, String, 'Link ID of question to autofill the value from if condition is med', null: true
+    field :sum_questions, [String], 'Link IDs of numeric questions to sum up and set as the value if condition is met', null: true
+
+    # Condition specifying when to perform this autofill
     field :autofill_behavior, Types::Forms::Enums::EnableBehavior, null: false
     field :autofill_when, [Types::Forms::EnableWhen], null: false
   end
