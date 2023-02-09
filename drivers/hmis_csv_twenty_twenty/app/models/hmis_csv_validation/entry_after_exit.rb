35225b6d02c05ddd7b6ef7d89e121e0f0e515080
Elliot Anders <elliot@greenriver.org>
2023-02-08 17:17:53 -0800

    Additional test fixes

--- drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_validation/entry_after_exit.rb
+++ drivers/hmis_csv_twenty_twenty/app/models/hmis_csv_validation/entry_after_exit.rb
@@ -8,7 +8,7 @@
 # are no entry dates after exit dates. This needs
 # the full data set in-place to check and should happen only once after the import is complete
 class HmisCsvValidation::EntryAfterExit < HmisCsvValidation::Validation
-  def self.check_validity!(klass, importer_log, _options)
+  def self.check_validity!(klass, importer_log)
     # FIXME: Very slow!!!
     e_t = HmisCsvTwentyTwenty::Importer::Enrollment.arel_table
     ex_t = HmisCsvTwentyTwenty::Importer::Exit.arel_table
