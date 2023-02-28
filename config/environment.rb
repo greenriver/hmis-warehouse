6937eb1455433d5670c4446db42f7797eaecbe8e
Elliot Anders <elliot@greenriver.org>
2023-02-28 08:06:11 -0800

    Note why we rescue

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 begin
   Rails.application.initialize!
+# Rescue when we don't yet have a DB, maybe related to LSA, or just delay, but this
+# only occurs on deploy and console boot on deployed instances, and appears to have
+# no negative impacts
 rescue ActiveRecord::ConnectionNotEstablished
 end
