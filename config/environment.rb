0de3057b60c41bb9be3f90f381eb7140b96ba8db
Elliot Anders <elliot@greenriver.org>
2023-03-01 06:47:15 -0800

    Movew comment so we get better logging

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 begin
+  # Rescue when we don't yet have a DB, maybe related to LSA, or just delay, but this
+  # only occurs on deploy and console boot on deployed instances, and appears to have
+  # no negative impacts
   Rails.application.initialize!
-# Rescue when we don't yet have a DB, maybe related to LSA, or just delay, but this
-# only occurs on deploy and console boot on deployed instances, and appears to have
-# no negative impacts
 rescue ActiveRecord::ConnectionNotEstablished => e
   Sentry.capture_exception_with_info(
     ActiveRecord::ConnectionNotEstablished.new('App failed to boot, you can probably ignore this'),
