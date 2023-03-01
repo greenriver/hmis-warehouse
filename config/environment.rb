0c48ef33deae8f69bc810a0dc80a912a3d642abb
Elliot Anders <elliot@greenriver.org>
2023-03-01 06:18:28 -0800

    Expose backtrace when boot fails from ActiveRecord::ConnectionNotEstablished

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,10 @@ begin
 # Rescue when we don't yet have a DB, maybe related to LSA, or just delay, but this
 # only occurs on deploy and console boot on deployed instances, and appears to have
 # no negative impacts
-rescue ActiveRecord::ConnectionNotEstablished
+rescue ActiveRecord::ConnectionNotEstablished => e
+  Sentry.capture_exception_with_info(
+    ActiveRecord::ConnectionNotEstablished.new('App failed to boot, you can probably ignore this'),
+    'App failed to boot',
+    { backtrace: e.backtrace.to_s }
+  )
 end
