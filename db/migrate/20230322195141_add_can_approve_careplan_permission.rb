12b5e71ee5bfc10c2fc797ef09e012b8023fb302
Sandy Wise <sandy@greenriver.org>
2023-03-22 16:15:23 -0400

    Add approval permissions.

---
+++ db/migrate/20230322195141_add_can_approve_careplan_permission.rb
@@ -0,0 +1,10 @@
+class AddCanApproveCareplanPermission < ActiveRecord::Migration[6.1]
+  def up
+    Role.ensure_permissions_exist
+    Role.reset_column_information
+  end
+
+  def down
+    remove_column :roles, :can_manage_accountable_care_organizations
+  end
+end
