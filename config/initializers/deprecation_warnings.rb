d4faa686eb63a73d01d66345c61792d66b2ceaeb
ZachCMP <zparks@greenriver.org>
2023-03-27 09:40:33 -0700

    Uncomment initializer

--- config/initializers/deprecation_warnings.rb
+++ config/initializers/deprecation_warnings.rb
@@ -1,11 +1,11 @@
-# ActiveSupport::Notifications.subscribe('deprecation.rails') do |name, start, finish, id, payload|
-#   setup_notifier('DeprecationWarning')
-#   @notifier.ping(
-#     payload[:message],
-#     info: {
-#       error_class:   'deprecation_warning',
-#       error_message: payload[:message],
-#       backtrace:     payload[:callstack],
-#     },
-#   )
-# end
+ActiveSupport::Notifications.subscribe('deprecation.rails') do |name, start, finish, id, payload|
+  setup_notifier('DeprecationWarning')
+  @notifier.ping(
+    payload[:message],
+    info: {
+      error_class:   'deprecation_warning',
+      error_message: payload[:message],
+      backtrace:     payload[:callstack],
+    },
+  )
+end
