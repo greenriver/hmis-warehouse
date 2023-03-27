0f55be113e539f7324ef177f9ab8209a6c4908aa
Gig Ashton <gig@greenriver.org>
2023-03-27 17:37:01 -0400

    Test resolution

--- drivers/hmis/spec/requests/hmis/lookup_client_enrollment_spec.rb
+++ drivers/hmis/spec/requests/hmis/lookup_client_enrollment_spec.rb
@@ -160,6 +160,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
       expect(response.status).to eq 200
       enrollment = result.dig('data', 'enrollment')
       expect(enrollment['id']).to eq(e1.id.to_s)
+      expect(enrollment['status']).to eq('ACTIVE')
       expect(enrollment['services']['nodesCount']).to eq(2)
       expect(enrollment['events']['nodesCount']).to eq(1)
       expect(enrollment['assessments']['nodesCount']).to eq(1)
