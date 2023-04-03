6154b4040357e469592da17c1d9d8d728a653b1d
Elliot Anders <elliot@greenriver.org>
2023-04-03 12:50:36 -0700

    Update copyright messages

--- drivers/cas_access/app/models/cas_access/sub_program.rb
+++ drivers/cas_access/app/models/cas_access/sub_program.rb
@@ -1,5 +1,5 @@
 ###
-# Copyright 2016 - 2022 Green River Data Analysis, LLC
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
 #
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
--- lib/util/code.rb
+++ lib/util/code.rb
@@ -1,5 +1,5 @@
 ###
-# Copyright 2016 - 2022 Green River Data Analysis, LLC
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
 #
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
@@ -8,7 +8,7 @@ class Code
   def self.copywright_header
     <<~COPYRIGHT
       ###
-      # Copyright 2016 - 2022 Green River Data Analysis, LLC
+      # Copyright 2016 - 2023 Green River Data Analysis, LLC
       #
       # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
       ###
