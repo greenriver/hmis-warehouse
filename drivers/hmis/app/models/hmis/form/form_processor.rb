a6aaa09db52e6092234954bdce26be08718aa6e0
Gig Ashton <gig@greenriver.org>
2023-04-14 10:52:00 -0400

    Use accessor instead of instance var

--- drivers/hmis/app/models/hmis/form/form_processor.rb
+++ drivers/hmis/app/models/hmis/form/form_processor.rb
@@ -22,8 +22,11 @@ class Hmis::Form::FormProcessor < ::GrdaWarehouseBase
 
   validate :hmis_records_are_valid
 
+  attr_accessor :owner
+
   def run!(owner:)
-    @owner = owner
+    # Set the owner reference so we are updating the correct record. Unpersisted changes can't be validated correctly if you go through custom_form.owner.
+    self.owner = owner
     return unless custom_form.hud_values.present?
 
     custom_form.hud_values.each do |key, value|
@@ -34,7 +37,7 @@ class Hmis::Form::FormProcessor < ::GrdaWarehouseBase
         container, field = match[1..2]
       else
         # Key format is "projectType", and the container is the owner type ("Project")
-        container = @owner.class.name.demodulize
+        container = owner.class.name.demodulize
         field = key
       end
 
@@ -49,21 +52,21 @@ class Hmis::Form::FormProcessor < ::GrdaWarehouseBase
       processor.new(self).information_date(custom_form.assessment.assessment_date) if custom_form.assessment.present?
     end
 
-    @owner.enrollment = enrollment_factory if @owner.is_a?(Hmis::Hud::CustomAssessment)
+    owner.enrollment = enrollment_factory if owner.is_a?(Hmis::Hud::CustomAssessment)
   end
 
   def owner_factory(create: true) # rubocop:disable Lint/UnusedMethodArgument
-    @owner
+    owner
   end
 
   def service_factory(create: true) # rubocop:disable Lint/UnusedMethodArgument
-    @service_factory ||= @owner.owner if @owner.is_a? Hmis::Hud::HmisService
+    @service_factory ||= owner.owner if owner.is_a? Hmis::Hud::HmisService
   end
 
   # Type Factories
   def enrollment_factory(create: true) # rubocop:disable Lint/UnusedMethodArgument
     # The enrollment has already been created, so we can just return it
-    @enrollment_factory ||= @owner.enrollment if @owner.is_a?(Hmis::Hud::CustomAssessment)
+    @enrollment_factory ||= owner.enrollment if owner.is_a?(Hmis::Hud::CustomAssessment)
   end
 
   def common_attributes
