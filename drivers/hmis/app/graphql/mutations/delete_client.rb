4f7ed167eee2ad89d31bbb1e21a28f52e9bbb1be
ZachCMP <zparks@greenriver.org>
2023-04-17 07:48:55 -0700

    Fix Pr feedback

--- drivers/hmis/app/graphql/mutations/delete_client.rb
+++ drivers/hmis/app/graphql/mutations/delete_client.rb
@@ -16,18 +16,20 @@ module Mutations
 
       warnings, resolvable_enrollments = check_enrollments(client, ignore_warnings: confirmed)
 
-      return { client: nil, errors: warnings } if warnings.present?
+      return { client: nil, errors: warnings } if warnings.any?
 
-      default_delete_record(
-        record: client,
-        field_name: :client,
-        permissions: :can_delete_clients,
-        after_delete: -> do
-          resolvable_enrollments.each do |enrollment|
-            enrollment.household_members.where.not(personal_id: client.personal_id).first&.update!(relationship_to_ho_h: 1)
-          end
-        end,
-      )
+      client.transaction do
+        default_delete_record(
+          record: client,
+          field_name: :client,
+          permissions: :can_delete_clients,
+          after_delete: -> do
+            resolvable_enrollments.each do |enrollment|
+              enrollment.household_members.where.not(personal_id: client.personal_id).first&.update!(relationship_to_ho_h: 1)
+            end
+          end,
+        )
+      end
     end
 
     def check_enrollments(client, ignore_warnings: false)
@@ -35,9 +37,7 @@ module Mutations
       warnings = []
       resolvable_enrollments = []
 
-      client.enrollments.viewable_by(current_user).each do |enrollment|
-        next unless enrollment.relationship_to_ho_h == 1
-
+      client.enrollments.preload(:household_members).viewable_by(current_user).heads_of_households.each do |enrollment|
         members = enrollment.household_members
 
         problem_enrollments << enrollment if members.count > 2
@@ -45,23 +45,25 @@ module Mutations
       end
 
       if problem_enrollments.present? && !ignore_warnings
-        warnings << HmisErrors::Error.new(
-          :id,
-          :information,
-          full_message: "If this client is deleted, #{problem_enrollments.size} #{'household'.pluralize(problem_enrollments.size)} will have no Head of Household.",
-          severity: :warning,
-          data: {
-            text: "The Head of Household for the following #{'household'.pluralize(problem_enrollments.size)} should be changed before this client is deleted:",
-            enrollments: problem_enrollments.map do |e|
-              {
-                id: e.id.to_s,
-                name: e.project&.project_name,
-                entryDate: e.entry_date,
-                exitDate: e.exit_date,
-              }
-            end,
-            confirmText: 'Delete client anyway',
-          },
+        warnings = HmisErrors::Errors.new
+        warnings.add(
+          HmisErrors::Error.new(
+            :id,
+            :information,
+            full_message: "If this client is deleted, #{problem_enrollments.size} #{'household'.pluralize(problem_enrollments.size)} will have no Head of Household.",
+            severity: :warning,
+            data: {
+              text: "The Head of Household for the following #{'household'.pluralize(problem_enrollments.size)} should be changed before this client is deleted:",
+              enrollments: problem_enrollments.map do |e|
+                {
+                  id: e.id.to_s,
+                  name: e.project&.project_name,
+                  entryDate: e.entry_date,
+                  exitDate: e.exit_date,
+                }
+              end,
+            },
+          ),
         )
       end
 
