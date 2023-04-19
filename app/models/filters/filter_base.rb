83e4a11c7ec380bc3ccaef1dd8657d69303259e8
Elliot Anders <elliot@greenriver.org>
2023-04-19 10:29:53 -0700

    Switch to using match? instead of starts_with?

--- app/models/filters/filter_base.rb
+++ app/models/filters/filter_base.rb
@@ -650,7 +650,7 @@ module Filters
     def cohort_columns_for_dates
       GrdaWarehouse::Cohort.available_columns.select do |column|
         # Ignore non-dates and untranslated custom dates
-        column.class.ancestors.include?(CohortColumns::CohortDate) && ! column.title.starts_with?('User')
+        column.class.ancestors.include?(CohortColumns::CohortDate) && ! column.title.match?(/^User Date \d+$/)
       end.map do |column|
         [
           column.title,
