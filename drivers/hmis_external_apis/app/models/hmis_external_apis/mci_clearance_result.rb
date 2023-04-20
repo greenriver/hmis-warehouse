e5e5afc31ebff1de1846781f590e086691feb909
Gig <gig@greenriver.org>
2023-04-20 09:14:19 -0400

    MCI Mapping (#2979)
    * Unrelated fix to file sorting
    
    * Add mappings for MCI fields, make clearance response more useful
    
    * Set existing_client_id correctly
    
    * Fix copyright dtes

---
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/mci_clearance_result.rb
@@ -0,0 +1,16 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module HmisExternalApis
+  MciClearanceResult = Struct.new(
+    :mci_id,
+    :score, # Match score
+    :client, # Unpersisted Client containing all the identifying information we got from MCI
+    :existing_client_id, # ID of Hmis::Hud::Client that already exists with this MCI ID
+    keyword_init: true,
+  ) do
+  end
+end
