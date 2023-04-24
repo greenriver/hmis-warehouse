1ea9ef346a501ef3b2538366a6758dc643dd864b
Gig Ashton <gig@greenriver.org>
2023-04-24 18:45:11 -0400

    Support overriding fragments and record forms

--- drivers/hmis/lib/hmis_util/json_forms.rb
+++ drivers/hmis/lib/hmis_util/json_forms.rb
@@ -6,14 +6,65 @@
 
 module HmisUtil
   class JsonForms
+    DATA_DIR = 'drivers/hmis/lib/form_data'.freeze
+
+    def self.fragment_map
+      @@fragment_map ||= begin # rubocop:disable Style/ClassVars
+        fragments = {}
+        Dir.glob("#{DATA_DIR}/default/fragments/*.json") do |file_path|
+          identifier = File.basename(file_path, '.json')
+          file = File.read(file_path)
+          fragments["##{identifier}"] = JSON.parse(file)
+        end
+
+        if ENV['CLIENT'].present?
+          Dir.glob("#{DATA_DIR}/#{ENV['CLIENT']}/fragments/*.json") do |file_path|
+            identifier = File.basename(file_path, '.json')
+            puts "Applying #{ENV['CLIENT']} override for #{identifier} fragment"
+            file = File.read(file_path)
+            fragments["##{identifier}"] = JSON.parse(file)
+          end
+        end
+
+        fragments
+      end
+    end
+
+    def self.record_forms
+      @@record_forms ||= begin # rubocop:disable Style/ClassVars
+        forms = {}
+        Dir.glob("#{DATA_DIR}/default/records/*.json") do |file_path|
+          identifier = File.basename(file_path, '.json')
+          file = File.read(file_path)
+          forms[identifier] = JSON.parse(file)
+        end
+
+        if ENV['CLIENT'].present?
+          Dir.glob("#{DATA_DIR}/#{ENV['CLIENT']}/records/*.json") do |file_path|
+            identifier = File.basename(file_path, '.json')
+            puts "Applying #{ENV['CLIENT']} override for #{identifier} form"
+            file = File.read(file_path)
+            forms[identifier] = JSON.parse(file)
+          end
+        end
+
+        forms
+      end
+    end
+
+    def self.apply_fragment(item)
+      return item unless item['fragment']
+
+      fragment = fragment_map[item['fragment']]
+      raise "Fragment not found #{item['fragment']}" unless fragment.present?
+
+      other_fields = item.except('fragment')
+      { **other_fields, **fragment }
+    end
+
     # Load form definitions for editing and creating records
     def self.seed_record_form_definitions
-      forms = []
-      Dir.glob('drivers/hmis/lib/form_data/records/*.json') do |file_path|
-        identifier = File.basename(file_path, '.json')
-        file = File.read(file_path)
-        form_definition = JSON.parse(file)
-        forms.push(identifier)
+      record_forms.each do |identifier, form_definition|
         role = identifier.upcase.to_sym
         raise "unrecognized role #{role}" unless Hmis::Form::Definition::FORM_ROLES.key?(role)
 
@@ -23,6 +74,8 @@ module HmisUtil
           role: role,
           status: 'draft',
         )
+
+        form_definition['item'] = form_definition['item'].map { |item| apply_fragment(item) }
         definition.definition = form_definition.to_json
         definition.save!
 
@@ -31,35 +84,20 @@ module HmisUtil
         instance.save!
       end
 
-      puts "Saved definitions with identifiers: #{forms}"
+      puts "Saved definitions with identifiers: #{record_forms.keys.join(', ')}"
     end
 
     # Load form definitions for HUD assessments
     def self.seed_assessment_form_definitions
-      # Load fragments
-      fragment_map = {}
-      Dir.glob('drivers/hmis/lib/form_data/fragments/*.json') do |file_path|
-        identifier = File.basename(file_path, '.json')
-        file = File.read(file_path)
-        fragment_map["##{identifier}"] = JSON.parse(file)
-      end
-
       roles = [:INTAKE, :EXIT, :UPDATE, :ANNUAL]
       identifiers = []
       roles.each do |role|
-        file = File.read("drivers/hmis/lib/form_data/assessments/base_#{role.to_s.downcase}.json")
+        file = File.read("#{DATA_DIR}/default/assessments/base_#{role.to_s.downcase}.json")
         next unless file.present?
 
         # Replace fragment references in JSON
         form_definition = JSON.parse(file)
-        form_definition['item'].each_with_index do |item, idx|
-          next unless item['fragment']
-
-          fragment = fragment_map[item['fragment']]
-          raise "Fragment not found #{item['fragment']}" unless fragment.present?
-
-          form_definition['item'][idx] = fragment
-        end
+        form_definition['item'] = form_definition['item'].map { |item| apply_fragment(item) }
 
         # Validate form structure
         Hmis::Form::Definition.validate_json(form_definition)
@@ -80,7 +118,7 @@ module HmisUtil
         instance = Hmis::Form::Instance.find_or_create_by(entity_type: nil, entity_id: nil, definition_identifier: identifier)
         instance.save!
       end
-      puts "Saved definitions with identifiers: #{identifiers}"
+      puts "Saved definitions with identifiers: #{identifiers.join(', ')}"
     end
   end
 end
