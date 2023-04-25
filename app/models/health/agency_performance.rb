b3c29408566b808f24dea0f62bd33309ec3b232b
Sandy Wise <sandy@greenriver.org>
2023-04-25 16:57:35 -0400

    Update healthcare performance dashboards.

--- app/models/health/agency_performance.rb
+++ app/models/health/agency_performance.rb
@@ -18,6 +18,18 @@ module Health
       'warehouse_reports/health/agency_performance'
     end
 
+    DESCRIPTIONS = {
+      without_required_qa: 'Patients who have not completed intake and have not received a QA in the specified date range.',
+      without_required_f2f_visit: 'Patients who have not received a face-to-face visit in the two months preceding the specified end date.',
+      with_discharge_followup_completed: 'Number of discharge follow-up QAs within the specified data range.',
+      with_completed_intake: 'Patients with completed initial intake (Consent, Comp Assessment, HRSN, and Care Plan) as of the specified end date.',
+      initial_intake_due: 'Patients who need to receive an initial intake within 30 days of the specified end date.',
+      initial_intake_overdue: 'Patients who did not complete an initial intake within 153 days of their enrollment as of the earlier of the current date or the specified end date.',
+      intake_renewal_due: 'Patients who need to receive a renewal intake within 30 days of the specified end date.',
+      intake_renewal_overdue: 'Patients who did not receive a renewal intake within 365 days of their last intake as of the earlier of the current date or the specified end date.',
+      without_required_wellcare_visit: 'Patients that did not have a comprehensive well-care visit with a PCP or an OB/GYN practitioner within 12 months of the earlier of the current date or the specified end date. Such visits are identified by paid claims, as specified by the Mathematica Annual Well-Care Visits Measure calculation. *NOTE:* Claims data is approximately 3 months out of date, so any annual well care visits that occured in the past 3 months may not be included.',
+    }.freeze
+
     def agency_counts
       @agency_counts ||= agencies.map do |id, name|
         patient_ids = patient_referrals.select do |_, (agency_id, _)|
@@ -26,56 +38,20 @@ module Health
 
         next unless patient_ids.any?
 
-        consented_patients = consent_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        unconsented_patients = patient_ids - consented_patients
-
-        with_ssms = ssm_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        without_ssms = patient_ids - with_ssms
-
-        with_chas = cha_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        without_chas = patient_ids - with_chas
-
-        with_signed_careplans = careplan_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        without_signed_careplans = patient_ids - with_signed_careplans
-
-        with_qualifying_activities_within_range = qualifying_activity_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        without_qualifying_activities_within_range = patient_ids - with_qualifying_activities_within_range
-
-        with_payable_qualifying_activities_within_range = payable_qualifying_activity_dates.select { |p_id, _| p_id.in?(patient_ids) }.keys
-        without_payable_qualifying_activities_within_range = patient_ids - with_payable_qualifying_activities_within_range
-
-        without_f2f_in_past_6_months = patient_ids - with_f2f_in_past_6_months
-        without_annual_well_care_visit_in_12_months = patient_ids - with_annual_well_care_visit_in_12_months
-
-        with_discharge_followup_within_range = with_discharge_followup.select { |p_id| p_id.in?(patient_ids) }
-
         OpenStruct.new(
           {
             id: id,
             name: name,
             patient_referrals: patient_ids,
-            consented_patients: consented_patients,
-            unconsented_patients: unconsented_patients,
-            with_ssms: with_ssms,
-            without_ssms: without_ssms,
-            with_chas: with_chas,
-            without_chas: without_chas,
-            with_signed_careplans: with_signed_careplans,
-            without_signed_careplans: without_signed_careplans,
-            without_initial_careplan_90_122_days: filter_days_since_enrollment(without_signed_careplans, 90...122),
-            without_initial_careplan_122_150_days: filter_days_since_enrollment(without_signed_careplans, 122...150),
-            initial_careplan_overdue: filter_days_since_enrollment(without_signed_careplans, 150..),
-            annual_careplan_due_within_30_days: with_annual_careplan_due_within_30_days(patient_ids),
-            annual_careplan_overdue: with_annual_careplan_overdue(patient_ids),
-            without_annual_well_care_visit_in_12_months: without_annual_well_care_visit_in_12_months,
-            without_f2f_in_past_6_months: without_f2f_in_past_6_months,
-            with_discharge_followup_completed_within_range: with_discharge_followup_within_range,
-            with_careplans_in_122_days: with_careplans_in_122_days(patient_ids),
-            with_careplans_signed_within_range: with_careplans_signed_within_range(patient_ids),
-            with_qualifying_activities_within_range: with_qualifying_activities_within_range,
-            without_qualifying_activities_within_range: without_qualifying_activities_within_range,
-            with_payable_qualifying_activities_within_range: with_payable_qualifying_activities_within_range,
-            without_payable_qualifying_activities_within_range: without_payable_qualifying_activities_within_range,
+            without_required_qa: patient_ids - with_required_qa,
+            without_required_f2f_visit: patient_ids - with_required_f2f_visit,
+            with_discharge_followup_completed: with_discharge_followup_completed.select { |p_id| p_id.in?(patient_ids) },
+            with_completed_intake: with_completed_intake.select { |p_id| p_id.in?(patient_ids) },
+            initial_intake_due: initial_intake_due.select { |p_id| p_id.in?(patient_ids) },
+            initial_intake_overdue: initial_intake_overdue.select { |p_id| p_id.in?(patient_ids) },
+            intake_renewal_due: intake_renewal_due.select { |p_id| p_id.in?(patient_ids) },
+            intake_renewal_overdue: intake_renewal_overdue.select { |p_id| p_id.in?(patient_ids) },
+            without_required_wellcare_visit: patient_ids - with_required_wellcare_visit,
           },
         )
       end.compact
@@ -118,243 +94,93 @@ module Health
         end
     end
 
-    # def rejected_patient_referrals
-    #   @rejected_patient_referrals ||= Health::PatientReferral.
-    #     rejected.
-    #     with_patient.
-    #     pluck(:patient_id, :agency_id).to_h
-    # end
-
-    # Consent dates for patients are their earliest participation signature dates
-    def consent_dates
-      @consent_dates ||= Health::Patient.joins(:release_forms).group(:patient_id).minimum(:participation_signature_on)
+    def patient_ids
+      @patient_ids ||= patient_referrals.keys
     end
 
-    # accepts an array of hashes, returns a single hash with most-recent values per key
-    def patients_with_dates_from_various_sources sources
-      patients = {}
-      sources.each do |batch|
-        batch.each do |p_id, date|
-          patients[p_id] = date if patients[p_id].blank? || patients[p_id] < date
-        end
-      end
-      return patients
-    end
-
-    # SSMS
-    def ssm_dates
-      @ssm_dates ||= begin
-        hmis_dates = hmis_ssms_max_dates_by_patient_id
-        warehouse_dates = warehouse_ssm_dates_by_patient_id
-        epic_dates = epic_ssms_by_patient_id
-
-        # determine most recent within range for each patient
-        patients_with_dates_from_various_sources([hmis_dates, warehouse_dates, epic_dates])
-      end
-    end
-
-    def warehouse_ssm_dates_by_patient_id
-      @warehouse_ssm_dates_by_patient_id ||= Health::SelfSufficiencyMatrixForm.completed.
-        distinct.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        order(completed_at: :asc).
-        pluck(:patient_id, :completed_at).to_h
-    end
-
-    def hmis_ssms_max_dates_by_patient_id
-      @hmis_ssms_max_dates_by_patient_id ||=
-        hmis_ssms_max_dates_by_client_id.map do |client_id, date|
-          [@client_ids[client_id], date]
-        end.to_h
-    end
-
-    def hmis_ssms_max_dates_by_client_id
-      @hmis_ssms_max_dates_by_client_id ||= GrdaWarehouse::Hud::Client.joins(:source_hmis_forms).
-        merge(GrdaWarehouse::HmisForm.self_sufficiency).
-        distinct.
-        where(id: client_ids.keys). # limit to clients in scope
-        pluck(:id, hmis_form_t[:collected_at].to_sql).
-        to_h
-    end
-
-    def epic_ssms_by_patient_id
-      @epic_ssms_by_patient_id ||= Health::EpicSsm.distinct.
-        joins(:patient).
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        order(ssm_updated_at: :asc).
-        pluck(hp_t[:id].to_sql, :ssm_updated_at).to_h
-    end
-
-    # CHAS
-    def cha_dates
-      @cha_dates ||= begin
-        warehouse_dates = warehouse_cha_dates_by_patient_id
-        epic_dates = epic_chas_by_patient_id
-
-        # determine most recent within range for each patient
-        patients_with_dates_from_various_sources([warehouse_dates, epic_dates])
-      end
-    end
-
-    def warehouse_cha_dates_by_patient_id
-      @warehouse_cha_dates_by_patient_id ||= Health::ComprehensiveHealthAssessment.completed.
-        distinct.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        order(completed_at: :asc).
-        pluck(:patient_id, :completed_at).to_h
-    end
-
-    def epic_chas_by_patient_id
-      @epic_chas_by_patient_id ||= Health::EpicCha.distinct.
-        joins(:patient).
-        where(hp_t[:id].in(patient_referrals.keys)). # limit to patients in scope
-        order(cha_updated_at: :asc).
-        pluck(hp_t[:id].to_sql, :cha_updated_at).to_h
-    end
-
-    # Careplans
-    def careplan_dates
-      @careplan_dates ||= begin
-        warehouse_dates = warehouse_careplan_dates_by_patient_id
-        # Ignore Epic care plans for now, 8/31/2018
-        # epic_dates = epic_careplan_by_patient_id
-        epic_dates = {}
-        # determine most recent within range for each patient
-        patients_with_dates_from_various_sources([warehouse_dates, epic_dates])
-      end
-    end
-
-    def warehouse_careplan_dates_by_patient_id
-      @warehouse_careplan_dates_by_patient_id ||= Health::Careplan.pcp_signed.
-        distinct.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        order(provider_signed_on: :asc).
-        pluck(:patient_id, :provider_signed_on).to_h
-    end
-
-    def epic_careplan_by_patient_id
-      @epic_careplan_by_patient_id ||= Health::EpicCareplan.distinct.
-        joins(:patient).
-        where(hp_t[:id].in(patient_referrals.keys)). # limit to patients in scope
-        order(careplan_updated_at: :asc).
-        pluck(hp_t[:id].to_sql, :careplan_updated_at).to_h
-    end
-
-    # Qualifying  Activities
-    def qualifying_activity_dates
-      @qualifying_activity_dates ||= Health::QualifyingActivity.submittable.
-        distinct.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        where(date_of_activity: @range).
-        order(date_of_activity: :asc).
-        pluck(:patient_id, :date_of_activity).to_h
-    end
-
-    def payable_qualifying_activity_dates
-      @payable_qualifying_activity_dates ||= Health::QualifyingActivity.
+    def with_required_qa
+      @with_required_qa ||= Health::QualifyingActivity.
         payable.
         not_valid_unpayable.
-        distinct.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        where(date_of_activity: @range).
-        order(date_of_activity: :asc).
-        pluck(:patient_id, :date_of_activity).to_h
+        where(patient_id: patient_ids).
+        in_range(@range.last - 60.days .. @range.last).
+        pluck(:patient_id).uniq
     end
 
-    def qa_signature_dates
-      # Note: using minimum will ensure the first PCTP, subsequent don't matter
-      @qa_signature_dates ||= Health::QualifyingActivity.
+    def with_required_f2f_visit
+      @with_required_f2f_visit ||= Health::QualifyingActivity.
+        payable.
+        not_valid_unpayable.
+        face_to_face.
+        where(patient_id: patient_ids).
+        in_range(@range.last - 60.days .. @range.last).
+        pluck(:patient_id).uniq
+    end
+
+    def with_discharge_followup_completed
+      @with_discharge_followup_completed ||= Health::QualifyingActivity.
         submittable.
-        during_current_enrollment.
-        where(patient_id: patient_referrals.keys). # limit to patients in scope
-        where(date_of_activity: @range).
-        where(activity: :pctp_signed).
-        group(:patient_id).minimum(:date_of_activity)
-    end
-
-    private def with_careplans_in_122_days(patient_ids)
-      patient_ids.select do |p_id|
-        careplan_date = qa_signature_dates[p_id]&.to_date
-        enrollment_date = patient_referrals[p_id][1]&.to_date
-
-        careplan_date.present? &&
-          enrollment_date.present? &&
-          careplan_date.between?(@range.first, @range.last) &&
-          (careplan_date - enrollment_date).to_i <= 122
-      end
-    end
-
-    private def with_careplans_signed_within_range(patient_ids)
-      patient_ids.select do |p_id|
-        careplan_date = qa_signature_dates[p_id]&.to_date
-        careplan_date.present? && careplan_date.between?(@range.first, @range.last)
-      end
-    end
-
-    private def with_discharge_followup
-      @with_discharge_followup ||= Health::QualifyingActivity.
-        submittable.
+        where(patient_id: patient_ids).
         in_range(@range).
-        where(patient_id: patient_referrals.keys).
         where(activity: :discharge_follow_up).
         pluck(:patient_id).uniq
     end
 
-    private def with_f2f_in_past_6_months
-      @with_f2f_in_past_6_months ||= Health::QualifyingActivity.
-        direct_contact.
-        face_to_face.
-        where(date_of_activity: (6.months.ago..Date.today)).
-        where(patient_id: patient_referrals.keys).
-        pluck(:patient_id).uniq
+    def with_completed_intake
+      @with_completed_intake ||= Health::Patient.
+        joins(:careplans).
+        where(h_cp_t[:careplan_sent].eq(true)).
+        distinct.
+        pluck(hp_t[:id])
     end
 
-    private def with_annual_well_care_visit_in_12_months
-      @with_annual_well_care_visit_in_12_months ||= ClaimsReporting::MedicalClaim.
+    def with_initial_intake
+      @with_initial_intake ||= Health::Patient.
+        where(id: patient_ids).
+        joins(:careplans).
+        where(h_cp_t[:careplan_sent].eq(true)).
+        order(h_cp_t[:careplan_sent_on].desc).
+        pluck(hp_t[:id], h_cp_t[:careplan_sent_on]).to_h
+    end
+
+    def initial_intake_due
+      overdue = [@range.last, Date.current].min
+      @initial_intake_due ||= Health::Patient.
+        where(id: patient_ids).
+        where.not(id: with_initial_intake.keys).
+        where(engagement_date: overdue - 30.days .. overdue)
+    end
+
+    def initial_intake_overdue
+      overdue = [@range.last, Date.current].min
+      @initial_intake_overdue ||= Health::Patient.
+        where(id: patient_ids).
+        where.not(id: with_initial_intake.keys).
+        where(engagement_date: ...overdue)
+    end
+
+    def intake_renewal_due
+      @intake_renewal_due ||= Health::Patient.
+        where(id: patient_ids).
+        joins(:recent_pctp_form).
+        where(h_cp_t[:careplan_sent_on].between(@range.last - 1.year - 30.days .. @range.last - 1.year))
+    end
+
+    def intake_renewal_overdue
+      overdue = [@range.last, Date.current].min - 1.year
+      @intake_renewal_overdue = Health::Patient.
+        where(id: patient_ids).
+        joins(:recent_pctp_form).
+        where(h_cp_t[:careplan_sent_on].lt(overdue))
+    end
+
+    def with_required_wellcare_visit
+      @with_required_wellcare_visit ||= ClaimsReporting::MedicalClaim.
         annual_well_care_visits.
         service_in(Date.today - 12.months...Date.today).
         joins(:patient).
         where(hp_t[:id].in(patient_referrals.keys)).
         pluck(hp_t[:id]).uniq
-    end
-
-    private def most_recent_qa_signature_dates
-      # Note: using maximum will ensure the last PCTP, earlier ones don't matter
-      @most_recent_qa_signature_dates ||= Health::QualifyingActivity.
-        submittable.
-        during_current_enrollment.
-        where(patient_id: patient_referrals.keys).
-        where(activity: :pctp_signed).
-        group(:patient_id).maximum(:date_of_activity)
-    end
-
-    private def filter_days_since_enrollment(patient_ids, days_since_enrollment_range)
-      patient_ids.select do |p_id|
-        enrollment_date = patient_referrals[p_id][1]&.to_date
-        enrollment_date.present? && days_since_enrollment_range.cover?((Date.today - enrollment_date).to_i)
-      end
-    end
-
-    private def with_initial_careplan_overdue(patient_ids_without_careplans)
-      patient_ids_without_careplans.select do |p_id|
-        enrollment_date = patient_referrals[p_id][1]&.to_date
-        enrollment_date.present? && enrollment_date + 150.days < Date.today
-      end
-    end
-
-    private def with_annual_careplan_due_within_30_days(patient_ids)
-      due_dates_to_include = (Date.today..Date.today + 30.days)
-      patient_ids.select do |p_id|
-        latest_careplan_date = most_recent_qa_signature_dates[p_id]&.to_date
-        latest_careplan_date.present? && due_dates_to_include.cover?(latest_careplan_date + 12.months)
-      end
-    end
-
-    private def with_annual_careplan_overdue(patient_ids)
-      patient_ids.select do |p_id|
-        latest_careplan_date = most_recent_qa_signature_dates[p_id]&.to_date
-        latest_careplan_date.present? && latest_careplan_date + 12.months < Date.today
-      end
     end
   end
 end
