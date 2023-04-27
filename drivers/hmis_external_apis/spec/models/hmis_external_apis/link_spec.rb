8c5933d436e67a13d9e63fd5c5ae558387c0b04a
Theron Toomey <ttoomey@greenriver.org>
2023-04-27 21:22:32 +0000

    185032522 live external referral tests

--- drivers/hmis_external_apis/spec/models/hmis_external_apis/link_spec.rb
+++ drivers/hmis_external_apis/spec/models/hmis_external_apis/link_spec.rb
@@ -5,18 +5,26 @@
 ###
 
 require 'rails_helper'
-require 'webmock/rspec'
 
 # CAUTION: This is not part of the normal test suite. It runs against a live remote endpoint
 # We need many secrets to test this. Essentially, this runs locally or on staging
 RSpec.describe 'LINK API', type: :model do
   if ENV['OAUTH_CREDENTIAL_TEST'] == 'true'
-    let(:host) { 'example.com' }
+    let(:base_url) { ENV.fetch('LINK_BASE_URL') + '/' }
     let(:client_id) { ENV.fetch('LINK_CLIENT_ID') }
     let(:client_secret) { ENV.fetch('LINK_CLIENT_SECRET') }
     let(:token_url) { ENV.fetch('LINK_TOKEN_URL') }
     let(:oauth_scope) { 'GREEN_RIVER' }
     let(:ocp_apim_subscription_key) { ENV.fetch('LINK_OCP_APIM_SUBSCRIPTION_KEY') }
+
+    let(:requested_by) { 'user1@example.com' }
+    let(:now) { Time.now }
+    let(:program_id) { 867 }
+    let(:unit_type_id) { 20 }
+
+    def format_date(date)
+      date.to_s(:iso8601)
+    end
 
     let(:subject) do
       HmisExternalApis::OauthClientConnection.new(
@@ -24,19 +32,76 @@ RSpec.describe 'LINK API', type: :model do
         client_secret: client_secret,
         token_url: token_url,
         headers: { 'Ocp-Apim-Subscription-Key' => ocp_apim_subscription_key },
-        base_url: "https://#{host}/",
+        base_url: base_url,
         scope: oauth_scope,
       )
     end
 
-    it 'supports a get' do
-      # FIXME: the LINK API endpoints aren't built yet. This just tests auth
-      path = 'test/resources/1'
-      stub_request(:get, "#{subject.base_url}#{path}")
-        .to_return(status: 200, body: nil, headers: { 'Content-Type' => 'application/json' })
+    it 'creates referral request' do
+      payload = {
+        'requestedDate' => format_date(now),
+        'programID' => program_id,
+        'unitTypeID' => unit_type_id,
+        'estimatedDate' => format_date(now + 1.day),
+        'requestorName' => 'greenriver',
+        'requestorPhoneNumber' => '8028675309',
+        'requestorEmail' => 'test@greenriver.com',
+        'requestedBy' => requested_by,
+      }
 
-      result = subject.get(path)
+      result = subject.post('Referral/ReferralRequest', payload)
+      # {"message"=>"A server error occurred.", "detail"=>"ORA-00942: table or view does not exist", "errorReferenceId"=>"58e06257-3128-43d0-96fe-03cc6af4d8d6"}
+      # byebug unless result.http_status == 200
       expect(result.http_status).to eq(200)
     end
+
+    it 'updates referral request' do
+      payload = { 'isVoid' => true, 'requestedBy' => requested_by }
+
+      result = subject.patch('Referral/ReferralRequest/60', payload)
+      # byebug unless result.http_status == 200
+      expect(result.http_status).to eq(200)
+    end
+
+    it 'updates referral posting status' do
+      payload = {
+        "postingId": 2176,
+        'postingStatusId' => 18,
+        'deniedReasonId' => 1,
+        'referralResultId' => 158,
+        'statusNote' => 'test',
+        'contactDate' => format_date(now),
+        'requestedBy' => requested_by,
+      }
+      payload = {
+        "postingStatusId": 18,
+        "deniedReasonId": 1,
+        "deniedReasonText": 'test',
+        "statusNote": 'test',
+        "contactDate": '2023-04-13T13:11:24.190Z',
+        "requestedBy": 'GreenRiver',
+      }
+
+      # Note: PostingStatus/ID doesn't work
+      # "{\"message\":\"Posting Status Id is not valid. \",\"detail\":\"Invalid Parameters.\"}"
+      result = subject.patch('Referral/PostingStatus', payload)
+      # byebug unless result.http_status == 200
+      expect(result.http_status).to eq(200)
+    end
+
+    it 'updates unit capacity' do
+      payload = {
+        'programID' => program_id,
+        'unitTypeID' => unit_type_id,
+        'availableUnits' => 3,
+        'requestedBy' => requested_by,
+      }
+
+      result = subject.patch('Unit/Capacity', payload)
+      # byebug unless result.http_status == 200
+      # {"message"=>"A server error occurred.", "detail"=>"ORA-00942: table or view does not exist", "errorReferenceId"=>"6f30d636-d276-40dc-ab3a-3e654ec4f431"}
+      expect(result.http_status).to eq(200)
+    end
+
   end
 end
