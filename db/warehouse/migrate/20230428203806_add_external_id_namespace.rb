06a96aaad0778704a838a08d5c19bd400e899b75
Theron Toomey <ttoomey@greenriver.org>
2023-04-28 21:31:50 +0000

    migrations for remote creds and external ids 185000178

---
+++ db/warehouse/migrate/20230428203806_add_external_id_namespace.rb
@@ -0,0 +1,14 @@
+class AddExternalIdNamespace < ActiveRecord::Migration[6.1]
+  # ensure all IDs are unique in a namespace.
+  # https://www.pivotaltracker.com/story/show/185000178/comments/236605163
+  def change
+    add_column :external_ids, :namespace, :string, null: true
+    # The condition here allows two clients can share the same MCI ID
+    # https://github.com/greenriver/hmis-warehouse/pull/2933/files#r1164091887
+    add_index :external_ids,
+              [:source_type, :namespace, :value],
+              unique: true,
+              name: 'uidx_external_id_ns_value',
+              where: "(namespace <> 'ac_hmis_mci')"
+  end
+end
