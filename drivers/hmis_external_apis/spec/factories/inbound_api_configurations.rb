0fffa7049d86258707fc8b6491b923036674447a
Todd Blackman <tdblackman@gmail.com>
2023-04-28 14:36:11 +0000

    Internal systems

--- drivers/hmis_external_apis/spec/factories/inbound_api_configurations.rb
+++ drivers/hmis_external_apis/spec/factories/inbound_api_configurations.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :inbound_api_configuration, class: 'HmisExternalApis::InboundApiConfiguration' do
-    sequence(:internal_system_name) { |n| ['referral', 'involvement', 'somesystem', 'someothersystem'][n % 4] }
+    internal_system
     sequence(:external_system_name) { |n| ['LINK', 'MPER', 'AABB', 'QQRR', 'AAAA'][n % 5] }
   end
 end
