781532d4db3dfa3735bec088326b23e1663e4bbc
Theron Toomey <ttoomey@greenriver.org>
2023-05-09 04:11:47 +0000

    wip 185052017

--- drivers/hmis/app/graphql/types/hmis_schema/referral_request.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/referral_request.rb
@@ -8,15 +8,14 @@ module Types
   class HmisSchema::ReferralRequest < Types::BaseObject
     field :id, ID, null: false
     field :requested_on, String, null: false
-    field :unit_type, ID, null: false
-    #field :living_situation, HmisSchema::Enums::Hud::LivingSituation
+    field :unit_type, HmisSchema::UnitTypeObject, null: false
     field :needed_by, String, null: false
     field :requestor_name, String, null: false
     field :requestor_phone, String, null: false
     field :requestor_email, String, null: false
 
     def unit_type
-      object.unit_type.description
+      load_ar_association(object, :unit_type)
     end
   end
 end
