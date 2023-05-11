d0dd56b40fed4af3a5f8b5bc05d84cb966eb5e12
Gig Ashton <gig@greenriver.org>
2023-05-11 15:10:13 -0400

    Dont join with deleted Exits

--- db/views/hmis_households_v01.sql
+++ db/views/hmis_households_v01.sql
@@ -1,25 +1,30 @@
 -- To update, run rails generate scenic:view hmis_households --replace
 
 SELECT
-	"Enrollment"."HouseholdID" as id,
-  "Enrollment"."HouseholdID" as "HouseholdID",
-	MAX("Enrollment"."ProjectID") as "ProjectID",
-  MAX("Enrollment"."data_source_id") as data_source_id,
-	MIN("Enrollment"."EntryDate") as earliest_entry,
-	CASE
-		WHEN BOOL_OR("Exit"."ExitDate" is NULL) THEN NULL
-		ELSE MAX("Exit"."ExitDate")
-	END as latest_exit,
-	BOOL_OR("Enrollment"."ProjectID" is NULL) as any_wip,
-  NULL as "DateDeleted",
-  MAX("Enrollment"."DateUpdated") as "DateUpdated",
-  MIN("Enrollment"."DateCreated") as "DateCreated"
+  "Enrollment"."HouseholdID" AS id,
+  "Enrollment"."HouseholdID" AS "HouseholdID",
+  MAX("Enrollment"."ProjectID") AS "ProjectID",
+  MAX("Enrollment"."data_source_id") AS data_source_id,
+  MIN("Enrollment"."EntryDate") AS earliest_entry,
+  CASE WHEN BOOL_OR("Exit"."ExitDate" IS NULL) THEN
+    NULL
+  ELSE
+    MAX("Exit"."ExitDate")
+  END AS latest_exit,
+  BOOL_OR("Enrollment"."ProjectID" IS NULL) AS any_wip,
+  NULL AS "DateDeleted",
+  MAX("Enrollment"."DateUpdated") AS "DateUpdated",
+  MIN("Enrollment"."DateCreated") AS "DateCreated"
 FROM
-	"Enrollment"
-	LEFT OUTER JOIN "Exit" ON "Exit"."EnrollmentID" = "Enrollment"."EnrollmentID"
-	AND "Exit"."data_source_id" = "Enrollment"."data_source_id"
-WHERE "Enrollment"."HouseholdID" is not NULL AND "Enrollment"."DateDeleted" IS NULL
-GROUP BY "Enrollment"."HouseholdID";
+  "Enrollment"
+  LEFT OUTER JOIN "Exit" ON "Exit"."EnrollmentID" = "Enrollment"."EnrollmentID"
+  AND "Exit"."data_source_id" = "Enrollment"."data_source_id"
+  AND "Exit"."DateDeleted" IS NULL
+WHERE
+  "Enrollment"."HouseholdID" IS NOT NULL
+  AND "Enrollment"."DateDeleted" IS NULL
+GROUP BY
+  "Enrollment"."HouseholdID";
 
 CREATE RULE attempt_hmis_households_del AS ON DELETE TO hmis_households DO INSTEAD NOTHING;
 CREATE RULE attempt_hmis_households_up AS ON UPDATE TO hmis_households DO INSTEAD NOTHING;
