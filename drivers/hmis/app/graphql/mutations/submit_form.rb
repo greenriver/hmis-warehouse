f60c44cabdd9e53b49f3f8b782b414a5bfd749f9
Theron Toomey <ttoomey@greenriver.org>
2023-05-14 04:20:39 +0000

    call create ref posting 185052017

--- drivers/hmis/app/graphql/mutations/submit_form.rb
+++ drivers/hmis/app/graphql/mutations/submit_form.rb
@@ -96,6 +96,8 @@ module Mutations
           record = Hmis::Hud::HmisService.find_by(owner: record.owner) # Refresh from View
           custom_form.owner = record # Set owner_id to the View id
           custom_form.save!
+        elsif record.is_a? HmisExternalApis::AcHmis::ReferralRequest
+          HmisExternalApis::AcHmis::CreateReferralRequestJob.perform_now(record)
         else
           custom_form.save!
           record.save!
