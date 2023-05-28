b8e6a0a28ed3a197aba66dca06e41c2f150cc539
Theron Toomey <ttoomey@greenriver.org>
2023-05-28 17:35:15 +0000

    referral request bugfixes
    fix active scope for null ids
    remove incorrect referral association

--- drivers/hmis_external_apis/app/models/hmis_external_apis/ac_hmis/referral_request.rb
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/ac_hmis/referral_request.rb
@@ -11,15 +11,19 @@ module HmisExternalApis::AcHmis
     scope :viewable_by, ->(_user) { raise } # this scope is replaced by ::Hmis::Hud::Concerns::ProjectRelated
     include ::Hmis::Hud::Concerns::ProjectRelated
 
+    # needed for duck-typing in submit form mutation
+    attr_accessor :data_source_id
+
     belongs_to :project, class_name: 'Hmis::Hud::Project'
     belongs_to :unit_type, class_name: 'Hmis::UnitType'
     belongs_to :requested_by, class_name: 'Hmis::User'
     belongs_to :voided_by, class_name: 'Hmis::User', optional: true
-    has_many :external_referrals, class_name: 'HmisExternalApis::AcHmis::Referral', dependent: :restrict_with_exception
 
     # active requests are ones that have not yet been fulfilled (not referenced by a referral)
     scope :active, -> {
-      where.not(id: HmisExternalApis::AcHmis::ReferralPosting.select(:referral_request_id))
+      posting_scope = HmisExternalApis::AcHmis::ReferralPosting
+        .where.not(referral_request_id: nil)
+      where.not(id: posting_scope.select(:referral_request_id))
         .where(voided_at: nil)
     }
   end
