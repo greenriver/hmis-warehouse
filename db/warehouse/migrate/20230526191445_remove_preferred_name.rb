2a43932358810a1175f3114e2098a5e5cbbec209
Elliot Anders <elliot@greenriver.org>
2023-06-01 05:52:51 -0700

    Correctly disable migration transaction

--- db/warehouse/migrate/20230526191445_remove_preferred_name.rb
+++ db/warehouse/migrate/20230526191445_remove_preferred_name.rb
@@ -1,6 +1,6 @@
 class RemovePreferredName < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
   def change
-    safety_assured { remove_column :Client, :preferred_name }
+    safety_assured { remove_column :Client, :preferred_name, :string }
   end
 end
