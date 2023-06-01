c22937bfd999bf3fee848dffee3145e3ad68d634
Elliot Anders <elliot@greenriver.org>
2023-06-01 05:51:11 -0700

    Disable transaction in correct location

--- db/warehouse/migrate/20230526191445_remove_preferred_name.rb
+++ db/warehouse/migrate/20230526191445_remove_preferred_name.rb
@@ -1,6 +1,6 @@
 class RemovePreferredName < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
   def change
-    disable_ddl_transaction!
     safety_assured { remove_column :Client, :preferred_name }
   end
 end
