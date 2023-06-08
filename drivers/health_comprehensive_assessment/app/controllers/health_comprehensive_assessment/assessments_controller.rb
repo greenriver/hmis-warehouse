e512fc7a3db3561b5b34da4a1238e42799f0896a
Sandy Wise <sandy@greenriver.org>
2023-06-08 10:14:25 -0400

    Pre-populate CA

--- drivers/health_comprehensive_assessment/app/controllers/health_comprehensive_assessment/assessments_controller.rb
+++ drivers/health_comprehensive_assessment/app/controllers/health_comprehensive_assessment/assessments_controller.rb
@@ -17,6 +17,7 @@ module HealthComprehensiveAssessment
         @patient.comprehensive_assessments.in_progress.first
       else
         ca = @patient.comprehensive_assessments.create!(user: current_user)
+        ca.populate_from_patient
         @patient.ca_assessments.create(instrument: ca)
         ca
       end
