982838ecd93a5c5b5fd5a49b797bb7016d22be68
Elliot <elliot@greenriver.org>
2023-06-12 13:40:23 -0400

    Cohort columns for pathways and transfer assessment dates (#3130)
    * Untested cohort columns for pathways and transfer assessment dates
    
    * Fixes for pathways and transfer cohort columns

--- app/models/cohort_columns/transfer_v3_assessment_date.rb
+++ app/models/cohort_columns/transfer_v3_assessment_date.rb
@@ -5,9 +5,9 @@
 ###
 
 module CohortColumns
-  class PathwaysV3AssessmentDate < ReadOnly
-    attribute :column, String, lazy: true, default: :pathways_v3_assessment_date
-    attribute :translation_key, String, lazy: true, default: 'Pathways V3 Assessment Date'
+  class TransferV3AssessmentDate < ReadOnly
+    attribute :column, String, lazy: true, default: :transfer_v3_assessment_date
+    attribute :translation_key, String, lazy: true, default: 'Transfer V3 Assessment Date'
     attribute :title, String, lazy: true, default: ->(model, _attr) { _(model.translation_key) }
 
     def available_for_rules?
@@ -23,7 +23,7 @@ module CohortColumns
     end
 
     def value(cohort_client) # OK
-      GrdaWarehouse::Config.get(:cas_calculator).constantize.new.most_recent_pathways_assessment_for_destination(cohort_client.client)
+      GrdaWarehouse::Config.get(:cas_calculator).constantize.new.most_recent_transfer_assessment_for_destination(cohort_client.client)
     end
   end
 end
