f6e7f896c9ee2ee372d56ea9e42ee8e84055f933
Sandy Wise <sandy@greenriver.org>
2023-06-15 10:20:04 -0400

    string -> json is not reversible.

--- db/health/migrate/20230614191047_change_health_instrument_race_to_json.rb
+++ db/health/migrate/20230614191047_change_health_instrument_race_to_json.rb
@@ -1,5 +1,6 @@
 class ChangeHealthInstrumentRaceToJson < ActiveRecord::Migration[6.1]
-  def change
+  def up
+    # ARRAY[race] wraps the existing value into an array for to_json
     safety_assured { change_column :hca_assessments, :race, 'jsonb USING to_json(ARRAY[race])' }
     safety_assured { change_column :pctp_careplans, :race, 'jsonb USING to_json(ARRAY[race])' }
   end
