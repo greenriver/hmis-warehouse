b50005ce9ce27de5605d037a96f4ae9b1087dfe2
Sandy Wise <sandy@greenriver.org>
2023-06-16 16:37:01 -0400

    Allow for incompletely assigned patients.

--- drivers/health_pctp/app/models/health_pctp/populate_pctp_concern.rb
+++ drivers/health_pctp/app/models/health_pctp/populate_pctp_concern.rb
@@ -12,7 +12,11 @@ module HealthPctp::PopulatePctpConcern
       ca = patient.recent_ca_assessment&.instrument
       return unless ca.present? && ca.is_a?(::HealthComprehensiveAssessment::Assessment)
 
-      tc = Health::UserCareCoordinator.find_by(care_coordinator_id: patient.care_coordinator.id)&.coordination_team&.team_coordinator
+      tc = if patient.care_coordinator.present?
+        Health::UserCareCoordinator.find_by(care_coordinator_id: patient.care_coordinator.id)&.
+          coordination_team&.
+          team_coordinator
+      end
 
       update(
         initial_date: Date.current,
@@ -22,9 +26,9 @@ module HealthPctp::PopulatePctpConcern
         email: ca.email,
         phone: ca.phone,
 
-        cc_name: patient.care_coordinator.name,
-        cc_phone: patient.care_coordinator.phone,
-        cc_email: patient.care_coordinator.email,
+        cc_name: patient.care_coordinator&.name,
+        cc_phone: patient.care_coordinator&.phone,
+        cc_email: patient.care_coordinator&.email,
 
         ccm_name: tc&.name,
         ccm_phone: tc&.phone,
@@ -34,9 +38,9 @@ module HealthPctp::PopulatePctpConcern
         pcp_phone: ca.pcp_phone,
         pcp_email: ca.pcp_address,
 
-        rn_name: patient.nurse_care_manager.name,
-        rn_phone: patient.nurse_care_manager.phone,
-        rn_email: patient.nurse_care_manager.email,
+        rn_name: patient.nurse_care_manager&.name,
+        rn_phone: patient.nurse_care_manager&.phone,
+        rn_email: patient.nurse_care_manager&.email,
 
         scribe: user.name,
         sex_at_birth: ca.sex_at_birth,
