5565a2195ccb98f566562919b1a202758873f722
Gig <gig@greenriver.org>
2023-06-19 12:21:17 -0400

    Update mutation for adding Household members (#3176)

--- drivers/hmis/spec/factories/hmis/access_groups.rb
+++ drivers/hmis/spec/factories/hmis/access_groups.rb
@@ -23,4 +23,18 @@ FactoryBot.define do
   factory :edit_access_group, class: 'Hmis::AccessGroup' do
     sequence(:name) { |n| "Group #{n}" }
   end
+
+  factory :hmis_access_group, class: 'Hmis::AccessGroup' do
+    sequence(:name) { |n| "Group #{n}" }
+  end
+
+  factory :hmis_access_control, class: 'Hmis::AccessControl' do
+    role { association :hmis_role }
+    access_group { association :hmis_access_group }
+  end
+
+  factory :hmis_user_access_control, class: 'Hmis::UserAccessControl' do
+    user { association :hmis_user }
+    access_control { association :hmis_access_control }
+  end
 end
--- drivers/hmis/spec/requests/hmis/login_and_permissions.rb
+++ drivers/hmis/spec/requests/hmis/login_and_permissions.rb
@@ -8,6 +8,43 @@ def hmis_login(user)
   post hmis_user_session_path(hmis_user: { email: user.email, password: user.password })
 end
 
+def viewable_type(entity)
+  return :projects if entity.is_a? Hmis::Hud::Project
+  return :projects if entity.is_a? GrdaWarehouse::Hud::Project
+  return :organizations if entity.is_a? Hmis::Hud::Organization
+  return :organizations if entity.is_a? GrdaWarehouse::Hud::Organization
+  return :data_sources if entity.is_a? GrdaWarehouse::DataSource
+end
+
+def viewable_hash(entities)
+  viewables_h = Hash.new { |hash, key| hash[key] = [] }
+  Array.wrap(entities).map { |e| [viewable_type(e), e.id] }.each do |k, v|
+    viewables_h[k] << v
+  end
+  viewables_h
+end
+
+# Create a new Access Control and assign the user to it.
+# The specified entity or entities are included. They can be any type (proj, org, ds).
+# If "with_permission" passed, role will only include the permissions passed.
+# If "without_permission" passed, role will include all perms EXCEPT the permissions passed.
+# If neither permission arg passed, role will include all permission.
+def create_access_control(user, entities, with_permission: nil, without_permission: nil)
+  # Create ACL
+  role_factory = with_permission.present? ? :hmis_role_with_no_permissions : :hmis_role
+  access_control = create(:hmis_access_control, role: create(role_factory))
+  # Set entities
+  access_control.access_group.set_viewables(viewable_hash(entities))
+  # Set permissions
+  access_control.role.update(**Array.wrap(with_permission).map { |p| [p, true] }.to_h) if with_permission.present?
+  access_control.role.update(**Array.wrap(without_permission).map { |p| [p, false] }.to_h) if without_permission.present?
+
+  # Assign the user to the ACL
+  create(:hmis_user_access_control, access_control: access_control, user: user)
+
+  access_control
+end
+
 def assign_viewable(access_group, viewable, user)
   # TODO: This is to prevent having to change all the tests now that we have
   # Hmis::GroupViewableEntity, which has different association types. We should
