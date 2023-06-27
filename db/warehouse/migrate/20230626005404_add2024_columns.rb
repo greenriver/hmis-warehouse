c2a0b62ad402431b427b48ab75bd04e881e457b5
Elliot Anders <elliot@greenriver.org>
2023-06-27 08:41:39 -0500

    Attempting to fix migration deployment issue a different way

--- db/warehouse/migrate/20230626005404_add2024_columns.rb
+++ db/warehouse/migrate/20230626005404_add2024_columns.rb
@@ -1,41 +1,40 @@
 class Add2024Columns < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
   def change
-    safety_assured do
-      # Project
-      add_column :Project, :RRHSubType, :integer, null: true
+    # Project
+    add_column :Project, :RRHSubType, :integer, null: true
 
-      # Client
-      # races
-      add_column :Client, :HispanicLatinaeo, :integer, null: true
-      add_column :Client, :MidEastNAfrican, :integer, null: true
-      add_column :Client, :AdditionalRaceEthnicity, :string, null: true
-      # genders
-      add_column :Client, :Woman, :integer, null: true # rename Female
-      add_column :Client, :Man, :integer, null: true # rename Male
-      add_column :Client, :NonBinary, :integer, null: true # rename NoSingleGender
-      add_column :Client, :CulturallySpecific, :integer, null: true
-      add_column :Client, :DifferentIdentity, :integer, null: true
-      add_column :Client, :DifferentIdentityText, :string, null: true
+    # Client
+    # races
+    add_column :Client, :HispanicLatinaeo, :integer, null: true
+    add_column :Client, :MidEastNAfrican, :integer, null: true
+    add_column :Client, :AdditionalRaceEthnicity, :string, null: true
+    # genders
+    add_column :Client, :Woman, :integer, null: true # rename Female
+    add_column :Client, :Man, :integer, null: true # rename Male
+    add_column :Client, :NonBinary, :integer, null: true # rename NoSingleGender
+    add_column :Client, :CulturallySpecific, :integer, null: true
+    add_column :Client, :DifferentIdentity, :integer, null: true
+    add_column :Client, :DifferentIdentityText, :string, null: true
 
-      # Enrollment
-      add_column :Enrollment, :EnrollmentCoC, :string, null: true # limit? non-nullable!
-      add_column :Enrollment, :RentalSubsidyType, :integer, null: true
-      # c4
-      add_column :Enrollment, :TranslationNeeded, :integer, null: true
-      add_column :Enrollment, :PreferredLanguage, :integer, null: true
-      add_column :Enrollment, :PreferredLanguageDifferent, :string, null: true
+    # Enrollment
+    add_column :Enrollment, :EnrollmentCoC, :string, null: true # limit? non-nullable!
+    add_column :Enrollment, :RentalSubsidyType, :integer, null: true
+    # c4
+    add_column :Enrollment, :TranslationNeeded, :integer, null: true
+    add_column :Enrollment, :PreferredLanguage, :integer, null: true
+    add_column :Enrollment, :PreferredLanguageDifferent, :string, null: true
 
-      # Exit
-      add_column :Exit, :DestinationSubsidyType, :integer, null: true
+    # Exit
+    add_column :Exit, :DestinationSubsidyType, :integer, null: true
 
-      # HealthAndDV
-      add_column :HealthAndDV, :DomesticViolenceSurvivor, :integer, null: true # rename DomesticViolenceVictim
+    # HealthAndDV
+    add_column :HealthAndDV, :DomesticViolenceSurvivor, :integer, null: true # rename DomesticViolenceVictim
 
-      # Services
-      # None, V3 fields already added in previous migration
+    # Services
+    # None, V3 fields already added in previous migration
 
-      # CurrentLivingSituation
-      add_column :CurrentLivingSituation, :CLSSubsidyType, :integer, null: true
-    end
+    # CurrentLivingSituation
+    add_column :CurrentLivingSituation, :CLSSubsidyType, :integer, null: true
   end
 end
