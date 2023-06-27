0ca952a7ff71ab921c89f3837e5984df3acb0699
Elliot Anders <elliot@greenriver.org>
2023-06-27 09:54:55 -0500

    Going back to normal migration

--- db/warehouse/migrate/20230626005404_add2024_columns.rb
+++ db/warehouse/migrate/20230626005404_add2024_columns.rb
@@ -1,40 +1,39 @@
 class Add2024Columns < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
   def change
     # Project
-    add_column :Project, :RRHSubType, :integer, null: true
+    add_column :Project, :RRHSubType, :integer
 
     # Client
     # races
-    add_column :Client, :HispanicLatinaeo, :integer, null: true
-    add_column :Client, :MidEastNAfrican, :integer, null: true
-    add_column :Client, :AdditionalRaceEthnicity, :string, null: true
+    add_column :Client, :HispanicLatinaeo, :integer
+    add_column :Client, :MidEastNAfrican, :integer
+    add_column :Client, :AdditionalRaceEthnicity, :string
     # genders
-    add_column :Client, :Woman, :integer, null: true # rename Female
-    add_column :Client, :Man, :integer, null: true # rename Male
-    add_column :Client, :NonBinary, :integer, null: true # rename NoSingleGender
-    add_column :Client, :CulturallySpecific, :integer, null: true
-    add_column :Client, :DifferentIdentity, :integer, null: true
-    add_column :Client, :DifferentIdentityText, :string, null: true
+    add_column :Client, :Woman, :integer # rename Female
+    add_column :Client, :Man, :integer # rename Male
+    add_column :Client, :NonBinary, :integer # rename NoSingleGender
+    add_column :Client, :CulturallySpecific, :integer
+    add_column :Client, :DifferentIdentity, :integer
+    add_column :Client, :DifferentIdentityText, :string
 
     # Enrollment
-    add_column :Enrollment, :EnrollmentCoC, :string, null: true # limit? non-nullable!
-    add_column :Enrollment, :RentalSubsidyType, :integer, null: true
+    add_column :Enrollment, :EnrollmentCoC, :string
+    add_column :Enrollment, :RentalSubsidyType, :integer
     # c4
-    add_column :Enrollment, :TranslationNeeded, :integer, null: true
-    add_column :Enrollment, :PreferredLanguage, :integer, null: true
-    add_column :Enrollment, :PreferredLanguageDifferent, :string, null: true
+    add_column :Enrollment, :TranslationNeeded, :integer
+    add_column :Enrollment, :PreferredLanguage, :integer
+    add_column :Enrollment, :PreferredLanguageDifferent, :string
 
     # Exit
-    add_column :Exit, :DestinationSubsidyType, :integer, null: true
+    add_column :Exit, :DestinationSubsidyType, :integer
 
     # HealthAndDV
-    add_column :HealthAndDV, :DomesticViolenceSurvivor, :integer, null: true # rename DomesticViolenceVictim
+    add_column :HealthAndDV, :DomesticViolenceSurvivor, :integer # rename DomesticViolenceVictim
 
     # Services
     # None, V3 fields already added in previous migration
 
     # CurrentLivingSituation
-    add_column :CurrentLivingSituation, :CLSSubsidyType, :integer, null: true
+    add_column :CurrentLivingSituation, :CLSSubsidyType, :integer
   end
 end
