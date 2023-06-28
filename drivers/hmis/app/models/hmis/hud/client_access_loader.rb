59c7b5dc539c5d299d8a14231477f8e82244c4d1
Theron Toomey <ttoomey@greenriver.org>
2023-06-28 20:44:20 +0000

    use arel for consistency

--- drivers/hmis/app/models/hmis/hud/client_access_loader.rb
+++ drivers/hmis/app/models/hmis/hud/client_access_loader.rb
@@ -13,16 +13,18 @@ class Hmis::Hud::ClientAccessLoader < Hmis::BaseAccessLoader
     validate_items(items, Hmis::Hud::Client)
     client_ids = items.map { |i| i.first.id }.compact.uniq
 
+    group_view_t = Hmis::GroupViewableEntity.arel_table
+    client_project_t = Hmis::Hud::ClientProject.arel_table
     orphan_client_ids = Hmis::Hud::Client
       .left_outer_joins(:client_projects)
       .where(id: client_ids)
-      .where(client_projects: { project_id: nil })
+      .where(client_project_t[:project_id].eq(nil))
       .pluck(arel.c_t[:id])
 
     access_group_ids_by_client_id = Hmis::Hud::Project
       .joins(:client_projects, :group_viewable_entities)
-      .where(client_projects: { client_id: client_ids - orphan_client_ids })
-      .pluck('client_projects.client_id', 'group_viewable_entities.access_group_id')
+      .where(client_project_t[:client_id].in(client_ids - orphan_client_ids))
+      .pluck(client_project_t[:client_id], group_view_t[:access_group_id])
       .group_by(&:shift).transform_values(&:flatten)
 
     orphan_client_ids = orphan_client_ids.to_set
