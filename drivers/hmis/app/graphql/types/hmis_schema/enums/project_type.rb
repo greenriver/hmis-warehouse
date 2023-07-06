53ebbbafd27cd92529f110bfb425c3d31b49b20e
Gig Ashton <gig@greenriver.org>
2023-07-06 18:07:29 -0400

    Rm commented code

--- drivers/hmis/app/graphql/types/hmis_schema/enums/project_type.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/enums/project_type.rb
@@ -12,11 +12,6 @@ module Types
     graphql_name 'ProjectType'
 
     with_enum_map Hmis::Hud::Project.project_type_enum_map, prefix_description_with_key: true
-    # HudLists.project_type_map.each do |id, description|
-    #   brief = HudLists.project_type_brief_map[id].gsub(/\s-?\s?/, '_').upcase
-    #   value brief, "(#{id}) #{description}", value: id
-    # end
-
     invalid_value
   end
 end
