899106584e066fd314e62f0f20a7ed6725536c11
Gig Ashton <gig@greenriver.org>
2023-07-10 17:44:58 -0400

    Disable migration

--- db/warehouse/migrate/20230707143716_add_custom_service_personal_id_index.rb
+++ db/warehouse/migrate/20230707143716_add_custom_service_personal_id_index.rb
@@ -1,7 +1,7 @@
 class AddCustomServicePersonalIdIndex < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
+  # disable_ddl_transaction!
 
   def change
-    add_index :custom_imports_b_services_rows, [:personal_id, :data_source_id], name: "idx_cibs_p_id_ds_id", algorithm: :concurrently
+    # add_index :custom_imports_b_services_rows, [:personal_id, :data_source_id], name: "idx_cibs_p_id_ds_id", algorithm: :concurrently
   end
 end
