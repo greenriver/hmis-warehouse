0180c9f80163040e864bd152b2a096efcae0e0b9
Elliot Anders <elliot@greenriver.org>
2023-07-12 17:42:22 -0500

    Comment adjustments

--- spec/support/s3_utils.rb
+++ spec/support/s3_utils.rb
@@ -1,3 +1,9 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
 RSpec.configure do |config|
   config.before(:suite) do
     Aws.config.update(
