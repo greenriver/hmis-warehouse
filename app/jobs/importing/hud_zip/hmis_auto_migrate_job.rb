afd31447dfde417b8b44f675b3146e6d2295f172
Elliot Anders <elliot@greenriver.org>
2023-07-12 07:43:01 -0500

    Prevent race conditions

--- app/jobs/importing/hud_zip/hmis_auto_migrate_job.rb
+++ app/jobs/importing/hud_zip/hmis_auto_migrate_job.rb
@@ -9,6 +9,7 @@ module Importing::HudZip
     queue_as ENV.fetch('DJ_LONG_QUEUE_NAME', :long_running)
     WAIT_MINUTES = 15
 
+    attr_accessor :job
     def initialize(upload_id:, data_source_id:, deidentified: false, project_whitelist: false)
       @upload_id = upload_id
       @data_source_id = data_source_id
@@ -17,11 +18,37 @@ module Importing::HudZip
     end
 
     def before(job)
+      self.job = job
+    end
+
+    def perform
+      lock_obtained = GrdaWarehouse::DataSource.with_advisory_lock(advisory_lock_name, timeout_seconds: 0) do
+        importer = Importers::HmisAutoMigrate::UploadedZip.new(
+          data_source_id: @data_source_id,
+          upload_id: @upload_id,
+          deidentified: @deidentified,
+          allowed_projects: @project_whitelist,
+        )
+        importer.import!
+      end
+      return if lock_obtained
+
+      requeue_job
+    end
+
+    private def already_running_for_data_source?
+      GrdaWarehouse::DataSource.advisory_lock_exists?(advisory_lock_name)
+    end
+
+    private def advisory_lock_name
+      "hud_import_auto_migrate_#{@data_source_id}"
+    end
+
+    private def requeue_job
       # Re-queue this import before processing if another import is running for the same data_source
       # This should help prevent tying up delayed job workers that are really just waiting
       # for the previous import to complete
       @data_source_id = job.payload_object.instance_variable_get(:@data_source_id)
-      return unless already_running_for_data_source?
 
       Rails.logger.info("Import of Data Source: #{@data_source_id} already running...re-queuing job for #{WAIT_MINUTES} minutes from now")
       # when this exits, it will remove the current job from the queue, so add a new one to replace it
@@ -32,26 +59,6 @@ module Importing::HudZip
         run_at: Time.current + WAIT_MINUTES.minutes,
         attempts: 0,
       )
-    end
-
-    def perform
-      GrdaWarehouse::DataSource.with_advisory_lock(advisory_lock_name, timeout_seconds: 0) do
-        importer = Importers::HmisAutoMigrate::UploadedZip.new(
-          data_source_id: @data_source_id,
-          upload_id: @upload_id,
-          deidentified: @deidentified,
-          allowed_projects: @project_whitelist,
-        )
-        importer.import!
-      end
-    end
-
-    private def already_running_for_data_source?
-      GrdaWarehouse::DataSource.advisory_lock_exists?(advisory_lock_name)
-    end
-
-    private def advisory_lock_name
-      "hud_import_auto_migrate_#{@data_source_id}"
     end
 
     def enqueue(job)
