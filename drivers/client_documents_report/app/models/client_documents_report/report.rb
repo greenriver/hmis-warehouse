877ff54e8383c1232435cac5148834a794d83951
Elliot Anders <elliot@greenriver.org>
2023-07-12 13:31:30 -0500

    WIP; adjustments to report content

--- drivers/client_documents_report/app/models/client_documents_report/report.rb
+++ drivers/client_documents_report/app/models/client_documents_report/report.rb
@@ -21,6 +21,88 @@ module ClientDocumentsReport
       'client_documents_report/warehouse_reports/reports'
     end
 
+    def total_required_documents
+      @total_required_documents ||= filter.required_files.select(&:present?).count
+    end
+
+    def total_required_document_groups
+      required_tag_ids_by_group.count
+    end
+
+    def required_documents(client)
+      client.client_files&.select { |cf| (cf.taggings.map(&:tag_id) & filter.required_files).present? }
+    end
+
+    private def required_tag_ids_by_group
+      required_tags.map do |tag|
+        group = available_tags[tag.name]
+        next unless group.present?
+
+        [
+          group,
+          tag.name,
+        ]
+      end.compact.group_by(&:shift)
+    end
+
+    private def required_tags
+      ActsAsTaggableOn::Tag.where(id: filter.required_files)
+    end
+
+    def total_optional_documents
+      @total_optional_documents ||= filter.optional_files.select(&:present?).count
+    end
+
+    def total_optional_document_groups
+      optional_tag_ids_by_group.count
+    end
+
+    def optional_documents(client)
+      client.client_files&.select { |cf| (cf.taggings.map(&:tag_id) & filter.optional_files).present? }
+    end
+
+    private def optional_tag_ids_by_group
+      optional_tags.map do |tag|
+        group = available_tags[tag.name]
+        next unless group.present?
+
+        [
+          group,
+          tag.name,
+        ]
+      end.compact.group_by(&:shift)
+    end
+
+    private def optional_tags
+      ActsAsTaggableOn::Tag.where(id: filter.optional_files)
+    end
+
+    def total_overall_documents
+      @total_overall_documents ||= overall_document_tag_ids.select(&:present?).count
+    end
+
+    def total_overall_document_groups
+      overall_tag_ids_by_group.count
+    end
+
+    def overall_documents(client)
+      client.client_files&.select { |cf| (cf.taggings.map(&:tag_id) & overall_document_tag_ids).present? }
+    end
+
+    private def overall_document_tag_ids
+      @overall_document_tag_ids ||= (filter.required_files + filter.optional_files).uniq
+    end
+
+    private def overall_tag_ids_by_group
+      available_tags.to_a.map(&:reverse).group_by(&:shift)
+    end
+
+    private def available_tags
+      @available_tags ||= GrdaWarehouse::AvailableFileTag.
+        where(name: required_tags.map(&:name) + optional_tags.map(&:name)).
+        pluck(:name, :group).to_h
+    end
+
     def include_comparison?
       false
     end
@@ -59,7 +141,8 @@ module ClientDocumentsReport
     end
 
     def clients
-      GrdaWarehouse::Hud::Client.where(id: enrollments.select(:client_id))
+      GrdaWarehouse::Hud::Client.where(id: enrollments.select(:client_id)).
+        preload(client_files: { taggings: [:tag] })
     end
 
     def enrollments
