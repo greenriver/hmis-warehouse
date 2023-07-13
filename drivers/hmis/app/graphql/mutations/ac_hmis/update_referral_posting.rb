4a694d4b9767706cc8d58746a467dde32712d4fd
Gig Ashton <gig@greenriver.org>
2023-07-13 19:33:41 -0400

    Fix

--- drivers/hmis/app/graphql/mutations/ac_hmis/update_referral_posting.rb
+++ drivers/hmis/app/graphql/mutations/ac_hmis/update_referral_posting.rb
@@ -43,7 +43,7 @@ module Mutations
         # if moving from assigned to accepted_pending, enroll household and assign to unit
         if errors.empty? && posting_status_change == ['assigned_status', 'accepted_pending_status']
           # choose any available unit of type, error if none available
-          unit_to_assign = Hmis::Unit.active.find_by(unit_type_id: posting.unit_type_id).first
+          unit_to_assign = Hmis::Unit.active.find_by(unit_type_id: posting.unit_type_id)
           errors.add :base, :invalid, full_message: "Unable to accept this referral because there are no #{posting.unit_type.description} units available." unless unit_to_assign.present?
           raise ActiveRecord::Rollback if errors.any?
 
