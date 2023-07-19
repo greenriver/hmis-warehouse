af2c330dbfdf4a6bb4cabdf025849950e6ba880e
Gig Ashton <gig@greenriver.org>
2023-07-19 10:00:21 -0400

    Readonly conditional wip

--- drivers/hmis/app/graphql/types/forms/enable_when.rb
+++ drivers/hmis/app/graphql/types/forms/enable_when.rb
@@ -8,8 +8,19 @@
 
 module Types
   class Forms::EnableWhen < Types::BaseObject
-    field :question, String, 'The linkId of question that determines whether item is enabled/disabled', null: false
-    field :operator, Forms::Enums::EnableOperator, 'How to evaluate the question\'s answer', null: false
+    # Specify show/hide based on whether form is read-only or editable
+    field :readonly, Boolean, 'Whether form is being rendered as read-only', null: true
+
+    # Specify show/hide based on dependency to another question or local value
+
+    # Dependent value
+    field :question, String, 'The linkId of question that determines whether item is enabled/disabled', null: true
+    field :value_local_constant, String, 'Name of local variable that determines whether item is enabled/disabled', null: true
+
+    # Operator
+    field :operator, Forms::Enums::EnableOperator, 'How to evaluate the question\'s answer', null: true
+
+    # Comparison value
     field :answer_code, String, 'If question is choice type, value for comparison', null: true
     field :answer_codes, [String], 'If question is choice type, and operator is IN, values for comparison', null: true
     field :answer_group_code, String, 'If question is choice type and has grouped options, value for comparison', null: true
