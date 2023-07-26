ecdf03b68afddd6c824ba8a66b5a94045b33c998
Gig Ashton <gig@greenriver.org>
2023-07-26 09:27:12 -0400

    Clarify

--- drivers/hmis/app/graphql/types/hmis_schema/custom_data_element.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/custom_data_element.rb
@@ -20,20 +20,19 @@ module Types
     # object is a CustomDataElementDefinition
     # parent is the parent (Enrollment, Client, etc)
 
-    # If this custom element only allows one value, 'value' is set
-    def value(parent:)
-      return if object.repeats
-
+    def all_values(parent:)
       parent = parent.owner if parent.is_a? Hmis::Hud::HmisService # special case for view
-      load_ar_association(object, :values, scope: parent.custom_data_elements)&.first
+      load_ar_association(object, :values, scope: parent.custom_data_elements)
     end
 
     # If this custom element allows multiple values, 'values' is set (repeats: true)
     def values(parent:)
-      return unless object.repeats
+      object.repeats ? all_values(parent: parent) : nil
+    end
 
-      parent = parent.owner if parent.is_a? Hmis::Hud::HmisService # special case for view
-      load_ar_association(object, :values, scope: parent.custom_data_elements)
+    # If this custom element only allows one value, 'value' is set
+    def value(parent:)
+      object.repeats ? nil : all_values(parent: parent)&.first
     end
   end
 end
