15c8226a0e694dde27a96679e1594d56cb86abe9
Theron Toomey <ttoomey@greenriver.org>
2023-07-26 23:43:21 +0000

    lint/fix

--- spec/models/omniauth_user_provisioner_spec.rb
+++ spec/models/omniauth_user_provisioner_spec.rb
@@ -22,10 +22,10 @@ RSpec.describe OmniauthUserProvisioner, type: :model do
 
   describe 'with no existing account' do
     it 'creates the account' do
-      expect {
+      expect do
         OmniauthUserProvisioner.new.perform(auth: auth, user_scope: User)
         perform_enqueued_jobs
-      }
+      end
         .to change(User, :count).by(1)
         .and change(OauthIdentity, :count).by(1)
     end
@@ -34,18 +34,17 @@ RSpec.describe OmniauthUserProvisioner, type: :model do
   describe 'with an existing account with matching email' do
     let(:user) { create(:user, email: email.downcase) }
     it 'updates the matching user and normalizes the email' do
-      expect {
+      expect do
         OmniauthUserProvisioner.new.perform(auth: auth, user_scope: User)
         perform_enqueued_jobs
         user.reload
-      }
+      end
         .to not_change(User, :count)
-        .and change(OauthIdentity.where(user:user), :count).by(1)
+        .and change(OauthIdentity.where(user: user), :count).by(1)
         .and change(ActionMailer::Base.deliveries, :last)
         .and not_change(user, :email)
         .and change(user, :first_name).to(auth.info['first_name'])
         .and change(user, :last_name).to(auth.info['last_name'])
-        receive(:event_added).once
     end
   end
 end
