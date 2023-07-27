4bfc9b443a1958546009a47f7279bc18f9b2ef04
ZachCMP <zparks@greenriver.org>
2023-07-27 09:32:40 -0700

    Add enrollment summary to referral household member

--- drivers/hmis/app/graphql/types/hmis_schema/referral_household_member.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/referral_household_member.rb
@@ -10,5 +10,10 @@ module Types
     field :id, ID, null: false
     field :relationship_to_ho_h, Types::HmisSchema::Enums::Hud::RelationshipToHoH, null: false, method: :relationship_to_hoh_before_type_cast
     field :client, HmisSchema::Client, null: false
+    field :open_enrollment_summary, [HmisSchema::EnrollmentSummary], null: false
+
+    def open_enrollment_summary
+      object.client.enrollments.where.not(id: object.id).not_exited.summary_viewable_by(current_user)
+    end
   end
 end
