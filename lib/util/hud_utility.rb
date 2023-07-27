aec04fe4abc2164e5cbb2b89e91ff93db02aeab8
Gig Ashton <gig@greenriver.org>
2023-07-27 11:48:18 -0400

    Replace hand-coded situation list

--- lib/util/hud_utility.rb
+++ lib/util/hud_utility.rb
@@ -230,47 +230,9 @@ module HudUtility
   end
 
   # See https://www.hudexchange.info/programs/hmis/hmis-data-standards/standards/HMIS-Data-Standards.htm#Appendix_A_-_Living_Situation_Option_List for details
+  # ALL living situations (current/prior/destination)
   def available_situations
-    {
-      1 => 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter ',
-      2 => 'Transitional housing for homeless persons (including homeless youth)',
-      3 => 'Permanent housing (other than RRH) for formerly homeless persons',
-      4 => 'Psychiatric hospital or other psychiatric facility',
-      5 => 'Substance abuse treatment facility or detox center',
-      6 => 'Hospital or other residential non-psychiatric medical facility',
-      7 => 'Jail, prison or juvenile detention facility',
-      8 => 'Client doesn\'t know',
-      9 => 'Client refused',
-      10 => 'Rental by client, no ongoing housing subsidy',
-      11 => 'Owned by client, no ongoing housing subsidy',
-      12 => 'Staying or living with family, temporary tenure (e.g. room, apartment or house)',
-      13 => 'Staying or living with friends, temporary tenure (e.g. room apartment or house)',
-      14 => 'Hotel or motel paid for without emergency shelter voucher',
-      15 => 'Foster care home or foster care group home',
-      16 => 'Place not meant for habitation (e.g., a vehicle, an abandoned building, bus/train/subway station/airport or anywhere outside)',
-      17 => 'Other',
-      18 => 'Safe Haven',
-      19 => 'Rental by client, with VASH housing subsidy',
-      20 => 'Rental by client, with other ongoing housing subsidy',
-      21 => 'Owned by client, with ongoing housing subsidy',
-      22 => 'Staying or living with family, permanent tenure',
-      23 => 'Staying or living with friends, permanent tenure',
-      24 => 'Deceased',
-      25 => 'Long-term care facility or nursing home',
-      26 => 'Moved from one HOPWA funded project to HOPWA PH',
-      27 => 'Moved from one HOPWA funded project to HOPWA TH',
-      28 => 'Rental by client, with GPD TIP housing subsidy',
-      29 => 'Residential project or halfway house with no homeless criteria',
-      30 => 'No exit interview completed',
-      31 => 'Rental by client, with RRH or equivalent subsidy',
-      32 => 'Host Home (non-crisis)',
-      33 => 'Rental by client, with HCV voucher (tenant or project based)',
-      34 => 'Rental by client in a public housing unit',
-      35 => 'Staying or living in a family member\'s room, apartment or house',
-      36 => 'Staying or living in a friend\'s room, apartment or house',
-      37 => 'Worker unable to determine',
-      99 => 'Data not collected',
-    }
+    living_situations
   end
 
   def homeless_situations(as:, version: nil)
