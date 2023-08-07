6ff4e4638db20e118d3f8f14b83a04b41626a82e
Elliot Anders <elliot@greenriver.org>
2023-08-07 08:37:53 -0500

    Attempt to skip adding system flag if already exists

--- db/migrate/20230429102609_add_system_to_roles.rb
+++ db/migrate/20230429102609_add_system_to_roles.rb
@@ -1,5 +1,5 @@
 class AddSystemToRoles < ActiveRecord::Migration[6.1]
   def change
-    add_column :roles, :system, :boolean, default: false, null: false
+    add_column(:roles, :system, :boolean, default: false, null: false) unless column_exists? :roles, :system
   end
 end
