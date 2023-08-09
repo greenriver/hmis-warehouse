e92ef22f90cd070429bd49c6abb5de0857010f50
Gig Ashton <gig@greenriver.org>
2023-08-09 12:19:02 -0400

    Dont update nils

--- db/warehouse/migrate/20230808144340_change_vamc_station_to_string.rb
+++ db/warehouse/migrate/20230808144340_change_vamc_station_to_string.rb
@@ -4,7 +4,7 @@ class ChangeVamcStationToString < ActiveRecord::Migration[6.1]
     view_maintainer = Bi::ViewMaintainer.new
     view_maintainer.remove_views
     add_column :Enrollment, :VAMCStation_new, :string, null: true
-    GrdaWarehouse::Hud::Enrollment.update_all('"VAMCStation_new" = "VAMCStation"')
+    GrdaWarehouse::Hud::Enrollment.where.not(VAMCStation: nil).update_all('"VAMCStation_new" = "VAMCStation"')
     rename_column :Enrollment, :VAMCStation, :VAMCStation_deleted
     rename_column :Enrollment, :VAMCStation_new, :VAMCStation
     view_maintainer.create_views
