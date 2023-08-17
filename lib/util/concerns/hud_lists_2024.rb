52b255df56844db4f32577e5bd28087e9cedfcdd
Gig Ashton <gig@greenriver.org>
2023-08-17 10:50:17 -0400

    Add project_type_brief

--- lib/util/concerns/hud_lists_2024.rb
+++ lib/util/concerns/hud_lists_2024.rb
@@ -177,6 +177,30 @@ module Concerns::HudLists2024
       _translate project_types, id, reverse
     end
 
+    # 2.02.6.brief
+    def project_type_briefs
+      {
+        0 => 'ES - Entry/Exit',
+        1 => 'ES - NBN',
+        2 => 'TH',
+        3 => 'PH - PSH',
+        4 => 'SO',
+        6 => 'SSO',
+        8 => 'SH',
+        9 => 'PH - PH',
+        7 => 'Other',
+        10 => 'PH - OPH',
+        11 => 'Day Shelter',
+        12 => 'HP',
+        13 => 'PH - RRH',
+        14 => 'CE',
+      }.freeze
+    end
+
+    def project_type_brief(id, reverse = false)
+      _translate project_type_briefs, id, reverse
+    end
+
     # 2.02.7
     def target_populations
       {
