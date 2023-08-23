8209b8a2c7c4a5dd227fbb9259aae8786a52a69b
Theron Toomey <ttoomey@greenriver.org>
2023-08-23 01:50:51 +0000

    185500686-periodic-unit-availability-updates
    backout change, pr feedback

--- config/schedule.rb
+++ config/schedule.rb
@@ -25,7 +25,7 @@ Dotenv.load('.env', '.env.local')
 # setup
 daily_schedule = ENV['DAILY_SCHEDULE'] || '3:10 am'
 import_schedule = ENV['IMPORT_SCHEDULE'] || '5:30 pm'
-# glacier_import_schedule = ENV['IMPORT_SCHEDULE'] || '5:30 pm'
+glacier_import_schedule = ENV['IMPORT_SCHEDULE'] || '5:30 pm'
 export_schedule = if ENV['DAILY_EXPORT_SCHEDULE'].nil? || ENV['DAILY_EXPORT_SCHEDULE'].empty? then (Time.parse(daily_schedule) - 2.hours).strftime('%I:%M %P') else ENV['DAILY_EXPORT_SCHEDULE'] end
 file_cleaning_schedule = (Time.parse(daily_schedule) - 5.minutes).strftime('%I:%M %P')
 import_prefetch_schedule = (Time.parse(import_schedule) - 4.hours).strftime('%I:%M %P')
@@ -33,7 +33,7 @@ census_schedule = (Time.parse(import_schedule) - 5.hours).strftime('%I:%M %P')
 # database_backup_time = Time.parse(import_schedule) - 3.hours
 
 health_trigger = ENV['HEALTH_SFTP_HOST'].to_s != '' && ENV['HEALTH_SFTP_HOST'] != 'hostname' && ENV['RAILS_ENV'] == 'production'
-# backup_glacier_trigger = ENV['GLACIER_NEEDS_BACKUP'] == 'true'
+backup_glacier_trigger = ENV['GLACIER_NEEDS_BACKUP'] == 'true'
 # glacier_files_backup_trigger = backup_glacier_trigger && ENV['GLACIER_FILESYSTEM_BACKUP'] == 'true'
 tasks = [
   # temporary task to move files to S3 and ActiveStorage
@@ -154,12 +154,6 @@ tasks = [
     trigger: health_trigger,
     interruptable: true,
   },
-  # FIXME uncomment after launch
-  # {
-  #   task: 'driver:hmis_external_apis:ac_hmis:update_unit_availability',
-  #   frequency: 3.minutes,
-  #   trigger: ENV['HMIS_OKTA_CLIENT_ID'] == 'true' && ENV['RAILS_ENV'] == 'production'
-  # },
   {
     task: 'driver:medicaid_hmis_interchange:medicaid_hmis_transfer',
     frequency: :sunday,
@@ -172,6 +166,12 @@ tasks = [
     frequency: 1.day,
     at: '4:02am',
     interruptable: true,
+  },
+  {
+    # No-op for environments that don't have (1) the HMIS enabled, and (2) an "MPER" credential present
+    task: 'driver:hmis_external_apis:ac_hmis:update_unit_availability',
+    frequency: 3.minutes,
+    trigger: ENV['HMIS_OKTA_CLIENT_ID'] == 'true' && ENV['RAILS_ENV'] == 'production',
   },
   {
     task: 'dba:dry_run',
@@ -203,14 +203,12 @@ tasks.each do |task|
   options = {}
   options[:at] = task[:at] if task[:at].present?
   every task[:frequency], options do
-    # if ENV['ECS'] == 'true' && task[:interruptable]
-    #   rake_short task[:task]
-    # else
-    #   rake_short task[:task]
-    #   # rake task[:task]
-    # end
-
-    # For the time being, move all cron tasks to the "short-term" capacity provider
-    rake_short task[:task]
+    if ENV['ECS'] == 'true' && task[:interruptable]
+      rake_short task[:task]
+    else
+      # For the time being, move all cron tasks to the "short-term" capacity provider
+      rake_short task[:task]
+      # rake task[:task]
+    end
   end
 end
