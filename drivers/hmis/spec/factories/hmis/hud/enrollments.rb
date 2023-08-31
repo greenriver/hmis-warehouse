7cf054b3e8c89e608c4e872ff3cdf2a89f4152e8
Theron Toomey <ttoomey@greenriver.org>
2023-08-31 09:35:30 -0400

    185728844 improve enrollment validator (#3428)

--- drivers/hmis/spec/factories/hmis/hud/enrollments.rb
+++ drivers/hmis/spec/factories/hmis/hud/enrollments.rb
@@ -15,8 +15,8 @@ FactoryBot.define do
     DateUpdated { DateTime.current }
     HouseholdID { SecureRandom.uuid.gsub(/-/, '') }
     DisablingCondition { 99 }
-    EnrollmentCoC { 'XX-500' }
     sequence(:EnrollmentID, 500)
+    EnrollmentCoC { 'XX-500' }
     sequence(:EntryDate) do |n|
       dates = [
         3.weeks.ago,
@@ -32,28 +32,7 @@ FactoryBot.define do
     # end
   end
 
-  factory :hmis_hud_wip_enrollment, class: 'Hmis::Hud::Enrollment' do
-    data_source { association :hmis_data_source }
-    user { association :hmis_hud_user, data_source: data_source }
-    project { association :hmis_hud_project, data_source: data_source }
-    client { association :hmis_hud_client, data_source: data_source }
-    RelationshipToHoH { 1 }
-    DateCreated { DateTime.current }
-    DateUpdated { DateTime.current }
-    HouseholdID { SecureRandom.uuid.gsub(/-/, '') }
-    DisablingCondition { 99 }
-    EnrollmentCoC { 'XX-500' }
-    sequence(:EnrollmentID, 500)
-    sequence(:EntryDate) do |n|
-      dates = [
-        3.weeks.ago,
-        2.weeks.ago,
-        1.week.ago,
-        2.days.ago,
-        Date.yesterday,
-      ]
-      dates[n % 5].to_date
-    end
+  factory :hmis_hud_wip_enrollment, parent: :hmis_hud_enrollment do
     after(:create, &:save_in_progress)
   end
 end
