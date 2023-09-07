b892889306cc6619aa2217d8222da1ad2d9b7276
Gig Ashton <gig@greenriver.org>
2023-09-07 16:06:08 -0400

    Move to Extension

--- drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
+++ drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
@@ -26,6 +26,9 @@ module HmisExternalApis
           # prepend is needed to destroy referrals before household_members are destroyed
           before_destroy :destroy_hoh_external_referrals, prepend: true
 
+          # On form submission, validate that client has MCI ID (if required)
+          validate :validate_mci_id_exists, on: :form_submission
+
           # remove referrals where this client is the the HOH
           def destroy_hoh_external_referrals
             HmisExternalApis::AcHmis::Referral
@@ -41,6 +44,22 @@ module HmisExternalApis
             return where unless client_ids.any?
 
             where.or(arel_table[:id].in(client_ids))
+          end
+
+          def validate_mci_id_exists
+            return unless HmisExternalApis::AcHmis::Mci.enabled?
+            # Skip new records. Frontend should ensure MCI is present when creating. Can't really check it here because enrollment is maybe being created in the same form.
+            return if new_record?
+
+            # Valid if client has an MCI ID, or is going to create one
+            return if ac_hmis_mci_ids.exists? || send(:create_mci_id)
+
+            # MCI clearance is required unless this client is ONLY enrolled at SO/NBN program(s)
+            so_nbn_enrollment_count = enrollments.with_project_type([1, 4]).size
+            only_enrolled_at_so_nbn = so_nbn_enrollment_count.positive? && so_nbn_enrollment_count == enrollments.size
+
+            # Add in some custom options (handled by HmisErrors::Error) so it shows up on the correct fields
+            errors.add :id, :required, attribute_override: :mci_id, readable_attribute: 'MCI ID' unless only_enrolled_at_so_nbn
           end
         end
       end
