75cd61aa0971284a66aca582424389949b9a46de
Todd Blackman <tdblackman@gmail.com>
2023-09-07 19:45:58 +0000

    getting closer

--- drivers/hmis_external_apis/spec/models/hmis_external_apis/ac_hmis/exporters/projects_cross_walk_fetcher_spec.rb
+++ drivers/hmis_external_apis/spec/models/hmis_external_apis/ac_hmis/exporters/projects_cross_walk_fetcher_spec.rb
@@ -11,11 +11,23 @@ RSpec.describe HmisExternalApis::AcHmis::Exporters::ProjectsCrossWalkFetcher, ty
 
   it 'provides projects' do
     subject.run!
-    CSV.parse(subject.projects_csv, headers: true)
+    result = CSV.parse(subject.projects_csv, headers: true)
+
+    expect(result.to_a).to eq(
+      [
+        ['Warehouse ID', 'HMIS ProjectID', 'Project Name', 'HMIS Organization ID', 'Organization Name', 'Data Source', 'Date Updated'],
+      ],
+    )
   end
 
   it 'provides organizations' do
     subject.run!
-    CSV.parse(subject.orgs_csv, headers: true)
+    result = CSV.parse(subject.orgs_csv, headers: true)
+
+    expect(result.to_a).to eq(
+      [
+        ['Warehouse ID', 'HMIS Organization ID', 'Organization Name', 'Data Source', 'Date Updated'],
+      ],
+    )
   end
 end
