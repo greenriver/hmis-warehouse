fded97665fe2c1296851ee5a0c65dfd9614cf6ba
Gig Ashton <gig@greenriver.org>
2023-09-08 13:45:01 -0400

    Validate MCI required-ness on Client form

--- drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
+++ drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
@@ -26,8 +26,11 @@ module HmisExternalApis
           # prepend is needed to destroy referrals before household_members are destroyed
           before_destroy :destroy_hoh_external_referrals, prepend: true
 
-          # On form submission, validate that client has MCI ID (if required)
-          validate :validate_mci_id_exists, on: :form_submission, if: :persisted?
+          # On Client form submission, validate that client has MCI ID (if required)
+          #
+          # NOTE: important that this only happens when _client_ form is submitted, not other form types.
+          # When submitting a Client Enrollment form, the Enrollment handles validation of MCI presence.
+          validate :validate_mci_id_exists, on: :client_form
 
           # remove referrals where this client is the the HOH
           def destroy_hoh_external_referrals
@@ -46,19 +49,27 @@ module HmisExternalApis
             where.or(arel_table[:id].in(client_ids))
           end
 
-          def validate_mci_id_exists
+          private def validate_mci_id_exists
             return unless HmisExternalApis::AcHmis::Mci.enabled?
             # Valid if client has an MCI ID, or is going to create one
             return if ac_hmis_mci_ids.exists? || send(:create_mci_id)
 
-            # MCI clearance is required unless this client is ONLY enrolled at SO/NBN program(s)
-            ptypes = HmisExternalApis::AcHmis::Mci::PROJECT_TYPES_NOT_REQUIRING_CLEARANCE
-            so_nbn_enrollment_count = enrollments.with_project_type(ptypes).size
-            only_enrolled_at_so_nbn = so_nbn_enrollment_count.positive? && so_nbn_enrollment_count == enrollments.size
+            # If brand new Client record (NOT in context of an Enrollment), then MCI clearance is required
+            clearance_required = true if new_record?
+
+            if persisted?
+              # MCI clearance is required unless this client is ONLY enrolled at SO/NBN program(s)
+              ptypes = HmisExternalApis::AcHmis::Mci::PROJECT_TYPES_NOT_REQUIRING_CLEARANCE
+              so_nbn_enrollment_count = enrollments.with_project_type(ptypes).size
+              only_so_nbn_enrollments = so_nbn_enrollment_count.positive? && so_nbn_enrollment_count == enrollments.size
+              clearance_required = !only_so_nbn_enrollments
+            end
+
+            return unless clearance_required
 
             # Add in some custom options (handled by HmisErrors::Error) so it shows up on the correct fields
             full_msg = HmisExternalApis::AcHmis::Mci::MCI_REQUIRED_MSG
-            errors.add :id, :required, attribute_override: :mci_id, readable_attribute: 'MCI ID', full_message: full_msg unless only_enrolled_at_so_nbn
+            errors.add :id, :required, attribute_override: :mci_id, readable_attribute: 'MCI ID', full_message: full_msg
           end
         end
       end
