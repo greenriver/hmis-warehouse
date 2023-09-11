9e2684a8b83c851f3cc4b355352224b6c08f5f54
Gig <gig@greenriver.org>
2023-09-11 16:33:04 -0400

    Reduce complexity of income fragment (#3488)

--- drivers/hmis/spec/models/hmis/form/form_processor_spec.rb
+++ drivers/hmis/spec/models/hmis/form/form_processor_spec.rb
@@ -29,11 +29,8 @@ RSpec.describe Hmis::Form::FormProcessor, type: :model do
       assessment = Hmis::Hud::CustomAssessment.new_with_defaults(enrollment: e1, user: u1, form_definition: fd, assessment_date: Date.yesterday)
       assessment.form_processor.hud_values = {
         'IncomeBenefit.incomeFromAnySource' => 'YES',
-        'IncomeBenefit.earned' => nil,
         'IncomeBenefit.earnedAmount' => nil,
-        'IncomeBenefit.unemployment' => 'YES',
         'IncomeBenefit.unemploymentAmount' => 100,
-        'IncomeBenefit.otherIncomeSource' => 'NO',
         'IncomeBenefit.otherIncomeAmount' => 0,
         'IncomeBenefit.otherIncomeSourceIdentify' => HIDDEN,
       }
@@ -58,11 +55,8 @@ RSpec.describe Hmis::Form::FormProcessor, type: :model do
       assessment = Hmis::Hud::CustomAssessment.new_with_defaults(enrollment: e1, user: u1, form_definition: fd, assessment_date: Date.yesterday)
       assessment.form_processor.hud_values = {
         'IncomeBenefit.incomeFromAnySource' => nil,
-        'IncomeBenefit.earned' => nil,
         'IncomeBenefit.earnedAmount' => nil,
-        'IncomeBenefit.unemployment' => nil,
         'IncomeBenefit.unemploymentAmount' => nil,
-        'IncomeBenefit.otherIncomeSource' => nil,
         'IncomeBenefit.otherIncomeAmount' => nil,
         'IncomeBenefit.otherIncomeSourceIdentify' => HIDDEN,
       }
