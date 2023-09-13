d3f54d0312fda0b7c0bb8ec6ee9ef30c0761a111
Gig Ashton <gig@greenriver.org>
2023-09-13 16:55:13 -0400

    Require name and ident components for ac

--- drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
+++ drivers/hmis_external_apis/extensions/hmis/hud/client_extension.rb
@@ -49,23 +49,35 @@ module HmisExternalApis
             where.or(arel_table[:id].in(client_ids))
           end
 
+          private def requires_mci_clearance?
+            # If brand new Client record (NOT in context of an Enrollment), then MCI clearance is required
+            return true if new_record?
+
+            # MCI clearance is required unless this client is ONLY enrolled at SO/NBN program(s)
+            ptypes = HmisExternalApis::AcHmis::Mci::PROJECT_TYPES_NOT_REQUIRING_CLEARANCE
+            so_nbn_enrollment_count = enrollments.with_project_type(ptypes).size
+            only_so_nbn_enrollments = so_nbn_enrollment_count.positive? && so_nbn_enrollment_count == enrollments.size
+
+            !only_so_nbn_enrollments
+          end
+
           private def validate_mci_id_exists
             return unless HmisExternalApis::AcHmis::Mci.enabled?
+
+            # First and Last are required, regardless of context
+            errors.add :first_name, :required unless first_name.present? || names.find(&:primary)&.first.present?
+            errors.add :last_name, :required unless last_name.present? || names.find(&:primary)&.last.present?
+
+            return unless requires_mci_clearance?
+
+            # For contexts where clearance is required, enforce presence of additional fields.
+            # This is needed so that you can't go back and remove DOB for a client that has already cleared, for example.
+            errors.add :dob, :required unless dob.present?
+            errors.add :dob_data_quality, :invalid, message: 'must be Full Name' unless dob_data_quality == 1
+            errors.add :name_data_quality, :invalid, message: 'must be Full DOB' unless name_data_quality == 1
+
             # Valid if client has an MCI ID, or is going to create one
             return if ac_hmis_mci_ids.exists? || send(:create_mci_id)
-
-            # If brand new Client record (NOT in context of an Enrollment), then MCI clearance is required
-            clearance_required = true if new_record?
-
-            if persisted?
-              # MCI clearance is required unless this client is ONLY enrolled at SO/NBN program(s)
-              ptypes = HmisExternalApis::AcHmis::Mci::PROJECT_TYPES_NOT_REQUIRING_CLEARANCE
-              so_nbn_enrollment_count = enrollments.with_project_type(ptypes).size
-              only_so_nbn_enrollments = so_nbn_enrollment_count.positive? && so_nbn_enrollment_count == enrollments.size
-              clearance_required = !only_so_nbn_enrollments
-            end
-
-            return unless clearance_required
 
             # Add in some custom options (handled by HmisErrors::Error) so it shows up on the correct fields
             full_msg = HmisExternalApis::AcHmis::Mci::MCI_REQUIRED_MSG
