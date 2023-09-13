777a991ef0a0579844e4709f1f0d5ba50d7ea179
Sandy Wise <sandy@greenriver.org>
2023-09-13 10:33:21 -0400

    Don't re-read file contents

--- app/models/health/health_file.rb
+++ app/models/health/health_file.rb
@@ -60,13 +60,19 @@ module Health
       self.user_id ||= user_id
       self.client_id ||= client_id
 
-      if File.exist?(file.path) && file.read.present?
-        self.content = file.read
-        self.content_type = file.content_type
-        self.size = content.size
-        self.name = file.filename
+      if File.exist?(file.path)
+        file_content = file.read
+        if file_content.present?
+          self.content = file_content
+          self.content_type = file.content_type
+          self.size = content.size
+          self.name = file.filename
 
-        save!
+          save!
+        else
+          error_message = "set_calculated! with blank file contents. id: #{id}, user_id: #{user_id}, client_id: #{client_id}"
+          send_single_notification(error_message, 'HealthFile')
+        end
       else
         error_message = "set_calculated! without upload. id: #{id}, user_id: #{user_id}, client_id: #{client_id}"
         send_single_notification(error_message, 'HealthFile')
