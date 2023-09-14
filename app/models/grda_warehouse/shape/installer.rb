c4d9dc3c6c458e57bb76303963eb640f8c73d7e6
Todd Blackman <tdblackman@gmail.com>
2023-09-14 20:16:58 +0000

    Fixes shape pruning bug

--- app/models/grda_warehouse/shape/installer.rb
+++ app/models/grda_warehouse/shape/installer.rb
@@ -49,16 +49,16 @@ module GrdaWarehouse
       def prune!(klasses = [Coc, County])
         return unless State.any?
 
-        if Rails.env.development?
-          Rails.logger.warn "Not pruning #{klasses} since env is development"
-          return
-        end
+        # if Rails.env.development?
+        #   Rails.logger.warn "Not pruning #{klasses} since env is development"
+        #   return
+        # end
 
         Rails.logger.info 'Pruning out-of-state geometries'
 
         # Don't need to prune places or towns. They come in on a per-state basis
         klasses.each do |klass|
-          if klass.is_a?(State) || klass.is_a?(ZipCode)
+          if klass.in?([State, ZipCode]) # .include?(klass) == State || klass == ZipCode
             Rails.logger.warn "Not pruning #{klass} which we do not prune anymore"
             next
           end
