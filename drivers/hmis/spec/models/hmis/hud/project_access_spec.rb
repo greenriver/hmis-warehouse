d2a80f23c710621ace7b796bdf5d04a023334081
Gig Ashton <gig@greenriver.org>
2023-09-16 09:55:47 -0400

    Add tests

---
+++ drivers/hmis/spec/models/hmis/hud/project_access_spec.rb
@@ -0,0 +1,48 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+require_relative '../../../requests/hmis/login_and_permissions'
+
+RSpec.describe Hmis::Hud::Project, type: :model do
+  before(:all) do
+    cleanup_test_environment
+  end
+  after(:all) do
+    cleanup_test_environment
+  end
+
+  let!(:ds1) { create :hmis_data_source }
+  let!(:o1) { create :hmis_hud_organization, data_source: ds1 }
+  let!(:p1) { create :hmis_hud_project, data_source: ds1, organization: o1 }
+  let!(:p2) { create :hmis_hud_project, data_source: ds1, organization: o1 }
+
+  let!(:user_with_no_access) { create(:hmis_user, data_source: ds1) }
+
+  let!(:user_with_p1_access) do
+    hmis_user = create(:hmis_user, data_source: ds1)
+    create_access_control(hmis_user, p1, with_permission: [:can_view_clients, :can_view_project])
+    hmis_user
+  end
+
+  let!(:user_with_limited_access) do
+    hmis_user = create(:hmis_user, data_source: ds1)
+    create_access_control(hmis_user, p1, with_permission: [:can_manage_incoming_referrals])
+    hmis_user
+  end
+
+  describe 'viewable_by scope' do
+    it 'includes projects where I have can_view_project' do
+      viewable_projects = Hmis::Hud::Project.viewable_by(user_with_p1_access)
+      expect(viewable_projects).to contain_exactly(p1)
+    end
+
+    it 'excludes projects where I dont have can_view_project, even if I have other permissions there' do
+      viewable_projects = Hmis::Hud::Project.viewable_by(user_with_limited_access)
+      expect(viewable_projects).to be_empty
+    end
+  end
+end
