1101410673817e1a2f9738c89c099a24c454726d
Elliot Anders <elliot@greenriver.org>
2023-09-18 11:37:26 -0500

    Attempted fix for missing source_types on client locations

--- app/models/grda_warehouse/place.rb
+++ app/models/grda_warehouse/place.rb
@@ -11,8 +11,6 @@ module GrdaWarehouse
     belongs_to :shape_zip_code, class_name: 'GrdaWarehouse::Shape::ZipCode', primary_key: 'zcta5ce10', foreign_key: 'zipcode', optional: true
     belongs_to :shape_state, class_name: 'GrdaWarehouse::Shape::State', primary_key: 'stusps', foreign_key: 'state', optional: true
 
-    NominatimApiPaused = Class.new(StandardError)
-
     def self.lookup_lat_lon(query: nil, city: nil, state: nil, postalcode: nil, country: 'us')
       place = lookup(query: query, city: city, state: state, postalcode: postalcode, country: country)&.lat_lon
       [place.try(:[], 'lat'), place.try(:[], 'lon'), place.try(:[], 'bounds')]
@@ -25,9 +23,11 @@ module GrdaWarehouse
       place = @places[key] # Look in memory cache
       place = @places[key] = Place.find_by(location: key) if place.blank? # Look in database
       if place.blank?
+        return if Rails.cache.read(['Nominatim', 'API PAUSE'])
+
         place = @places[key] = begin
           # we see a ton of missing zeros at the beginning of zipcodes, store what we have, but lookup the correct value
-          nr = nominatim_lookup(query, city, state, postalcode.rjust(5, '0'), country)
+          nr = nominatim_lookup(query, city, state, postalcode&.rjust(5, '0'), country)
           if nr.present?
             lat_lon = { lat: nr.coordinates.first, lon: nr.coordinates.last, bounds: nr.boundingbox }.with_indifferent_access
             Place.create!(
@@ -41,14 +41,12 @@ module GrdaWarehouse
         end
       end
       place
-    rescue NominatimApiPaused
-      return nil
-    rescue StandardError
-      send_single_notification('Error contacting the OSM Nominatim API.', 'NominatimWarning')
+    rescue StandardError => e
+      send_single_notification("Error contacting the OSM Nominatim API.: #{e.message}", 'NominatimWarning')
     end
 
     def self.nominatim_lookup(query, city, state, postalcode, country)
-      raise NominatimApiPaused if Rails.cache.read(['Nominatim', 'API PAUSE'])
+      return if Rails.cache.read(['Nominatim', 'API PAUSE'])
 
       address = [query, city, state, postalcode, country].compact.join(',')
       n = Geocoder.search(address)
@@ -59,16 +57,16 @@ module GrdaWarehouse
       result = n.first
       @rate_limit = Time.current
 
-      result
+      return result
     rescue Faraday::ConnectionFailed
       # we've probably been banned, let the API cool off
       Rails.cache.write(['Nominatim', 'API PAUSE'], true, expires_in: 1.hours)
-      raise NominatimApiPaused
     rescue StandardError => e
       # The API returns various errors which we don't want to prevent continuing with other attempts.
       # Just send it along to sentry and take a quick break
       Sentry.capture_exception(e)
       sleep 1
     end
+    nil
   end
 end
