3a03c89f4c7d1fa7c12d509864e30a3fda48c654
Gig Ashton <gig@greenriver.org>
2023-09-19 17:09:01 -0400

    Assign name fields before saving so they can be validated on

--- drivers/hmis/app/models/hmis/hud/validators/client_validator.rb
+++ drivers/hmis/app/models/hmis/hud/validators/client_validator.rb
@@ -17,11 +17,7 @@ class Hmis::Hud::Validators::ClientValidator < Hmis::Hud::Validators::BaseValida
     Hmis::Hud::Client.hmis_configuration(version: '2024').except(*IGNORED)
   end
 
-  def self.primary_first_or_last_required_full_message
-    'Primary name must have a First or Last name'
-  end
-
-  def self.client_first_or_last_required_full_message
+  def self.first_or_last_required_full_message
     'First or Last name is required'
   end
 
@@ -39,21 +35,13 @@ class Hmis::Hud::Validators::ClientValidator < Hmis::Hud::Validators::BaseValida
         record.errors.add :dob, :out_of_range, message: self.class.too_old_dob_message if record.dob < (Date.current - 120.years)
       end
 
+      # First or Last exists
+      record.errors.add(:first_name, :invalid, full_message: self.class.first_or_last_required_full_message) unless record.first_name.present? || record.last_name.present?
+
+      # Exactly 1 primary name
       if record.names.any?
-        # Exactly 1 primary name
         primary_names = record.names.reject(&:marked_for_destruction?).select(&:primary?)
         record.errors.add :names, :invalid, full_message: 'One primary name is required' if primary_names.size != 1
-
-        # First OR Last exists on primary
-        if primary_names.any?
-          name_record = primary_names.first
-          first = name_record.first
-          last = name_record.last
-          record.errors.add(:names, :invalid, full_message: self.class.primary_first_or_last_required_full_message) unless first.present? || last.present?
-        end
-      else
-        # First OR Last exists on client
-        record.errors.add(:first_name, :invalid, full_message: self.class.client_first_or_last_required_full_message) unless record.first_name.present? || record.last_name.present?
       end
     end
   end
