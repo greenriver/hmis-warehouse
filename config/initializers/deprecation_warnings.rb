6b82fdfe8e46e154b6f1c2131a92b1be73c678b1
Elliot Anders <elliot@greenriver.org>
2023-09-20 11:12:33 -0500

    Fix error when warning about deprecations; fix deprecations in enrollment/disenrollment report

--- config/initializers/deprecation_warnings.rb
+++ config/initializers/deprecation_warnings.rb
@@ -1,6 +1,6 @@
+include NotifierConfig
 Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('deprecation.rails') do |_name, _start, _finish, _id, payload|
-    include NotifierConfig
     setup_notifier('DeprecationWarning')
     @notifier.ping(
       payload[:message],
