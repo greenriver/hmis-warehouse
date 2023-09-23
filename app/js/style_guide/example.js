10d4cacab70d78204f540ee0cb482cac1e4f4185
Elliot Anders <elliot@greenriver.org>
2023-09-23 12:05:50 -0500

    ESBuild watch working manually

---
+++ app/js/style_guide/example.js
@@ -0,0 +1,5 @@
+const message = "Hello, esbuild!";
+console.log(message);
+console.log('66')
+// WATCH with:
+// ./node_modules/.bin/esbuild app/js/**/*.js --bundle --outdir=app/assets/builds --watch
