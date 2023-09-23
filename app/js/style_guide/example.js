5035675185d7c69712b57b993a9049f01597669f
Elliot Anders <elliot@greenriver.org>
2023-09-23 12:11:26 -0500

    Notes on how to run esbuild

--- app/js/style_guide/example.js
+++ app/js/style_guide/example.js
@@ -1,5 +1,5 @@
 const message = "Hello, esbuild!";
 console.log(message);
-console.log('66')
+console.log('69')
 // WATCH with:
 // ./node_modules/.bin/esbuild app/js/**/*.js --bundle --outdir=app/assets/builds --watch
