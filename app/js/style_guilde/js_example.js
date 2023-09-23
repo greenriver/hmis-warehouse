fc19f768fff8041e4ed3d4b1714f0d16e0df1da3
Elliot Anders <elliot@greenriver.org>
2023-09-23 12:57:50 -0500

    WIP: almost working?

--- app/js/style_guilde/js_example.js
+++ app/js/style_guilde/js_example.js
@@ -4,4 +4,6 @@ const div = d3.selectAll('div')
 // console.log(div)
 d3.select('.output')
   .text(`${div.size()} divs found`)
-console.debug('fu')
+console.log('test12')
+// WATCH with:
+// ./node_modules/.bin/esbuild app/js/**/*.js --bundle --outdir=app/assets/builds --watch
