b18e4d6c3e95efbaf8c44e84824341889b97e6b5
Gig Ashton <gig@greenriver.org>
2023-09-25 11:04:47 -0400

    Update factories

--- drivers/hmis/spec/factories/hmis/hud/disabilities.rb
+++ drivers/hmis/spec/factories/hmis/hud/disabilities.rb
@@ -8,8 +8,8 @@ FactoryBot.define do
   factory :hmis_disability, class: 'Hmis::Hud::Disability' do
     data_source { association :hmis_data_source }
     user { association :hmis_hud_user, data_source: data_source }
-    enrollment { association :hmis_hud_enrollment, data_source: data_source }
     client { association :hmis_hud_client, data_source: data_source }
+    enrollment { association :hmis_hud_enrollment, data_source: data_source, client: client }
     sequence(:DisabilitiesID, 500)
     disability_type { 7 }
     disability_response { 1 }
