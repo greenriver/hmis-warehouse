49043d764124e803760914d742a9b06f002f176d
Gig <gig@greenriver.org>
2023-09-26 15:00:24 -0400

    Importer updates (#3561)

--- drivers/hmis_external_apis/app/models/hmis_external_apis/sh_hmis/importers/s3_importer.rb
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/sh_hmis/importers/s3_importer.rb
@@ -1,12 +1,19 @@
 module HmisExternalApis::ShHmis::Importers
+  # importer = HmisExternalApis::ShHmis::Importers::S3Importer.new
+  # importer.list_files
+  # importer.select_latest_file
+  # importer.fetch_and_unzip_latest_file
+  # importer.run_migration!(clobber: true)
   class S3Importer
     attr_accessor :creds
+    attr_accessor :s3
     attr_accessor :s3_file_substr
     attr_accessor :dir
 
     def initialize(s3_file_substr: nil, dir: 'var/migration/data')
       # GrdaWarehouse::RemoteCredentials::S3.create!(username: 'unknown', encrypted_password: '', bucket: <bucket name>, path: <prefix>, slug: 'hmis_migration_files', active: true)
       self.creds = GrdaWarehouse::RemoteCredentials::S3.active.find_by(slug: 'hmis_migration_files')
+      self.s3 = creds.s3
       self.s3_file_substr = s3_file_substr
       self.dir = dir
 
@@ -14,7 +21,7 @@ module HmisExternalApis::ShHmis::Importers
     end
 
     def list_files
-      creds.s3.list_objects(prefix: creds.s3_prefix)
+      s3.list_objects(prefix: creds.s3_prefix)
     end
 
     def select_latest_file
@@ -40,7 +47,7 @@ module HmisExternalApis::ShHmis::Importers
       log_file = "var/migration/#{Date.current.strftime('%Y-%m-%d')}-migration-log.txt"
       ENV['SH_HMIS_IMPORT_LOG_FILE'] = log_file
 
-      importer = HmisExternalApis::AcHmis::Importers::CustomDataElementsImporter.new(dir: dir, clobber: clobber)
+      importer = HmisExternalApis::ShHmis::Importers::CustomDataElementsImporter.new(dir: dir, clobber: clobber)
       importer.run!
 
       # Upload log file to S3
