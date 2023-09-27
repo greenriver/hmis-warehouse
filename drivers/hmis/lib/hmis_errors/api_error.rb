f5662fa14e2afdcfbda86a6b6eec7b7ec3a72a6c
Theron Toomey <ttoomey@greenriver.org>
2023-09-27 18:23:07 +0000

    185905983-improved-generic-api-error-message

--- drivers/hmis/lib/hmis_errors/api_error.rb
+++ drivers/hmis/lib/hmis_errors/api_error.rb
@@ -7,7 +7,8 @@
 module HmisErrors
   class ApiError < StandardError
     attr_reader :display_message
-    INTERNAL_ERROR_DISPLAY_MESSAGE = 'An error occurred'.freeze
+    # these instructive error messages should probably live in the front-end. It's convenient to have them here for now
+    INTERNAL_ERROR_DISPLAY_MESSAGE = 'An error occurred on this page. The error has been reported and will be investigated by our support team. Please reload the page and try again. Contact your administrator if the problem persists.'.freeze
     STALE_OBJECT_ERROR = "Your changes couldn't be saved because someone else made updates to the same data while you were working on it. Please refresh the page and try saving your changes again. If the issue persists, contact support for assistance.".freeze
 
     def initialize(msg = nil, display_message: INTERNAL_ERROR_DISPLAY_MESSAGE)
