834eba98944e7aa514cf5263a09056476aa4ca14
Elliot Anders <elliot@greenriver.org>
2023-10-03 12:02:37 -0500

    Centralize superset url definition

--- drivers/superset/app/models/superset.rb
+++ drivers/superset/app/models/superset.rb
@@ -2,4 +2,11 @@ module Superset
   def self.table_name_prefix
     'superset_'
   end
+
+  def self.superset_base_url
+    fqdn = ENV.fetch('FQDN', 'warehouse.openpath.host')
+    tokens = fqdn.split('.')
+    tokens = [tokens[0], 'superset', tokens[1..]].flatten
+    'https://' + ENV.fetch('SUPERSET_FQDN', tokens.join('.'))
+  end
 end
