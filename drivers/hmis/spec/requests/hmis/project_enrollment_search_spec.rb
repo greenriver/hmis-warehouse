a6680c536179fb2a7e52af43cc529e0e0b51a3ea
Theron Toomey <ttoomey@greenriver.org>
2023-10-04 22:29:16 +0000

    186114231-enrollment-performance-improvement
    household search specs

---
+++ drivers/hmis/spec/requests/hmis/project_enrollment_search_spec.rb
@@ -0,0 +1,81 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+require_relative 'login_and_permissions'
+require_relative '../../support/hmis_base_setup'
+
+RSpec.describe Hmis::GraphqlController, type: :request do
+  before(:all) do
+    cleanup_test_environment
+  end
+  after(:all) do
+    cleanup_test_environment
+  end
+
+  include_context 'hmis base setup'
+  let(:search_term) { 'Foobarbaz' }
+  let!(:access_control) { create_access_control(hmis_user, p1) }
+  let!(:c1) { create :hmis_hud_client, data_source: ds1 }
+  let!(:e1) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c1 }
+
+  let!(:c2) { create :hmis_hud_client, data_source: ds1, last_name: search_term }
+  let!(:e2) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c2 }
+
+  let!(:c3) { create :hmis_hud_client, data_source: ds1, DOB: Date.current - 30.years }
+  let!(:e3) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c3 }
+
+  # let!(:s1) { create :hmis_hud_service, data_source: ds1, enrollment: e1, client: c1 }
+  # let!(:cst1) { create :hmis_custom_service_type_for_hud_service, data_source: ds1, custom_service_category: csc1, user: u1 }
+
+  before(:each) do
+    hmis_login(user)
+  end
+
+  describe 'project services & enrollments query' do
+    let(:query) do
+      <<~GRAPHQL
+        query TestQuery($id: ID!, $filters: HouseholdFilterOptions!) {
+          project(id: $id) {
+            id
+            households(limit: 10, offset: 0, filters: $filters) {
+              nodesCount
+              nodes {
+                id
+              }
+            }
+          }
+        }
+      GRAPHQL
+    end
+
+    it 'filters households by search term' do
+      filters = { searchTerm: search_term }
+      response, result = post_graphql(id: p1.id, filters: filters) { query }
+      expect(response.status).to eq 200
+      [e2].map(&:household_id).tap do |expected|
+        expect(expected.size).to eq 1
+        households = result.dig('data', 'project', 'households', 'nodes')
+        expect(households.map { |r| r.fetch('id') }).to eq expected
+      end
+    end
+
+    it 'filters households by age' do
+      filters = { "hohAgeRange": 'Ages25to34' }
+      response, result = post_graphql(id: p1.id, filters: filters) { query }
+      expect(response.status).to eq 200
+      [e3].map(&:household_id).tap do |expected|
+        expect(expected.size).to eq 1
+        households = result.dig('data', 'project', 'households', 'nodes')
+        expect(households.map { |r| r.fetch('id') }).to eq expected
+      end
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include GraphqlHelpers
+end
