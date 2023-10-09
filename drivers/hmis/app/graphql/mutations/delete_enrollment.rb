d9914f9312abdbd072226b6b88873781ce1b8a6e
Gig Ashton <gig@greenriver.org>
2023-10-09 11:36:07 -0400

    Update WIP enrollment deletion logic

--- drivers/hmis/app/graphql/mutations/delete_enrollment.rb
+++ drivers/hmis/app/graphql/mutations/delete_enrollment.rb
@@ -13,12 +13,17 @@ module Mutations
     def resolve(id:)
       enrollment = Hmis::Hud::Enrollment.viewable_by(current_user).find_by(id: id)
       raise HmisErrors::ApiError, 'Record not found' unless enrollment.present?
-      raise HmisErrors::ApiError, 'Access denied' unless current_user.permissions_for?(enrollment, :can_delete_enrollments)
+      # WIP Enrollments can be deleted if user has "can_edit_enrollments" access for this project
+      raise HmisErrors::ApiError, 'Access denied' unless current_user.permissions_for?(enrollment, :can_edit_enrollments)
 
       errors = []
       if enrollment.in_progress?
         enrollment.destroy
       else
+        # Deleting non-WIP Enrollments requires can_delete_enrollments. Currently not supported via this mutation, though.
+        # See DeleteAssessment mutation for deletiong of non-WIP enrollments.
+        raise HmisErrors::ApiError, 'Access denied' unless current_user.permissions_for?(enrollment, :can_delete_enrollments)
+
         errors << HmisErrors::Error.new(:base, full_message: 'Completed enrollments can not be deleted. Please exit the client instead.')
       end
 
