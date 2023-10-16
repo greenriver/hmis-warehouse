e06780ffe574ca943fb8f496a4b761d82e763168
Elliot Anders <elliot@greenriver.org>
2023-10-16 08:35:01 -0500

    Copyright cleanpu; additional ids

--- drivers/hud_lsa/app/models/hud_lsa/generators/fy2023/lsa_comparison_tool.rb
+++ drivers/hud_lsa/app/models/hud_lsa/generators/fy2023/lsa_comparison_tool.rb
@@ -4,11 +4,6 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-###
-# Copyright 2016 - 2023 Green River Data Analysis, LLC
-#
-# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
-###
 require 'csv'
 module HudLsa::Generators::Fy2023
   class LsaComparisonTool
@@ -66,6 +61,8 @@ module HudLsa::Generators::Fy2023
         'VendorEmail',
         'OrganizationID',
         'ProjectCoCID',
+        'AffiliationID',
+        'HMISParticipationID',
       ]
     end
   end
