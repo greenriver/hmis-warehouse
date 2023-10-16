e414b08d46675056c9114ff56c1feab303f0a388
Elliot Anders <elliot@greenriver.org>
2023-10-16 07:52:38 -0500

    Tests to confirm all jobs are completed when calling work_off; updates for some calls that weren't always being completed

--- app/models/access_group.rb
+++ app/models/access_group.rb
@@ -82,7 +82,7 @@ class AccessGroup < ApplicationRecord
 
   def self.delayed_system_group_maintenance(group: nil)
     delay.maintain_system_groups_no_named_arguments(group)
-    Delayed::Worker.new.work_off if Rails.env.test?
+    Delayed::Worker.new.work_off(1_000) if Rails.env.test?
   end
 
   def self.maintain_system_groups_no_named_arguments(group)
