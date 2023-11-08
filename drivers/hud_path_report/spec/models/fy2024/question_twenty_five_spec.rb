5b665610ed08da27040c22279d68d0bac5daa760
Theron Toomey <ttoomey@greenriver.org>
2023-11-08 16:56:41 +0000

    185769378-update-path-to-2024
    tests

---
+++ drivers/hud_path_report/spec/models/fy2024/question_twenty_five_spec.rb
@@ -0,0 +1,50 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+require_relative 'path_context'
+
+RSpec.describe HudPathReport::Generators::Fy2024::QuestionTwentyFive, type: :model do
+  include_context 'path context FY2024'
+
+  before(:all) do
+    default_setup
+    run(default_filter, HudPathReport::Generators::Fy2024::QuestionTwentyFive::QUESTION_NUMBER)
+  end
+
+  after(:all) do
+    cleanup
+  end
+
+  # DEPRECATED_FY2024 - these are expected to fail until we re-write the report as destination codes have changed
+  # it 'counts exits foster care' do
+  #   expect(report_result.answer(question: 'Q25', cell: 'B14').summary).to eq(1)
+  # end
+  #
+  # it 'sums the institutional destinations' do
+  #   expect(report_result.answer(question: 'Q25', cell: 'B20').summary).to eq(1)
+  # end
+
+  it 'other subtotals are zero' do
+    [12, 36].each do |row|
+      expect(report_result.answer(question: 'Q25', cell: 'B' + row.to_s).summary).to eq(0)
+    end
+  end
+
+  it 'counts stayers' do
+    expect(report_result.answer(question: 'Q25', cell: 'B41').summary).to eq(1)
+  end
+
+  it 'counts total' do
+    expect(report_result.answer(question: 'Q25', cell: 'B42').summary).to eq(2)
+  end
+end
---
+++ drivers/hud_path_report/spec/models/fy2024/question_twenty_six_spec.rb
@@ -0,0 +1,129 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+require 'rails_helper'
+require_relative 'path_context'
+
+RSpec.describe HudPathReport::Generators::Fy2024::QuestionTwentySix, type: :model do
+  include_context 'path context FY2024'
+
+  before(:all) do
+    default_setup
+    run(default_filter, HudPathReport::Generators::Fy2024::QuestionTwentySix::QUESTION_NUMBER)
+  end
+
+  after(:all) do
+    cleanup
+  end
+
+  # DEPRECATED_FY2024 - these are expected to fail until we re-write the report
+  # describe 'genders' do
+  #   it 'counts identifies as female' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C2').summary).to eq(1)
+  #   end
+  #   it 'counts clients' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C10').summary).to eq(2)
+  #   end
+  # end
+
+  describe 'ages' do
+    it 'counts minors' do
+      expect(report_result.answer(question: 'Q26', cell: 'C13').summary).to eq(1)
+    end
+    it 'counts ages 18-24' do
+      expect(report_result.answer(question: 'Q26', cell: 'C14').summary).to eq(1)
+    end
+    # DEPRECATED_FY2024 - these are expected to fail until we re-write the report
+    # it 'counts clients' do
+    #   expect(report_result.answer(question: 'Q26', cell: 'C10').summary).to eq(2)
+    # end
+  end
+
+  describe 'race' do
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0].each_with_index do |count, index|
+      it "counts by race: index: #{index}" do
+        expect(report_result.answer(question: 'Q26', cell: "C#{index + 24}").summary).to eq(count)
+      end
+    end
+  end
+
+  # DEPRECATED_FY2024 - these are expected to fail until we re-write the report ethnicity became a race
+  # describe 'ethnicities' do
+  #   it 'counts identifies as non-hispanic/latino' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C31').summary).to eq(1)
+  #   end
+  #   it 'counts clients' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C36').summary).to eq(2)
+  #   end
+  # end
+
+  describe 'veteran statuses' do
+    it 'counts veterans' do
+      expect(report_result.answer(question: 'Q26', cell: 'C35').summary).to eq(1)
+    end
+    it 'counts clients' do
+      expect(report_result.answer(question: 'Q26', cell: 'C40').summary).to eq(1)
+    end
+  end
+
+  describe 'co-occurring disorder' do
+    it 'counts yes' do
+      expect(report_result.answer(question: 'Q26', cell: 'C41').summary).to eq(1)
+    end
+    it 'counts clients' do
+      expect(report_result.answer(question: 'Q26', cell: 'C44').summary).to eq(2)
+    end
+  end
+
+  describe 'soar connection' do
+    it 'counts yes' do
+      expect(report_result.answer(question: 'Q26', cell: 'C45').summary).to eq(1)
+    end
+    it 'counts clients' do
+      expect(report_result.answer(question: 'Q26', cell: 'C50').summary).to eq(2)
+    end
+  end
+
+  # DEPRECATED_FY2024 - these are expected to fail until we re-write the report living situations changed
+  # describe 'prior living situation' do
+  #   it 'counts safe haven' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C56').summary).to eq(1)
+  #   end
+  #   it 'counts clients' do
+  #     # Line 57 is missing from spec, so subtract 1 from all lines after this poing
+  #     expect(report_result.answer(question: 'Q26', cell: 'C84').summary).to eq(2)
+  #   end
+  # end
+
+  it 'counts clients with length of stay' do
+    expect(report_result.answer(question: 'Q26', cell: 'C88').summary).to eq(0)
+  end
+
+  describe 'chronically homeless' do
+    it 'counts yeses' do
+      expect(report_result.answer(question: 'Q26', cell: 'C89').summary).to eq(0)
+    end
+    it 'counts clients' do
+      expect(report_result.answer(question: 'Q26', cell: 'C92').summary).to eq(2)
+    end
+  end
+
+  # DEPRECATED_FY2024 - these are expected to fail until we re-write the report DV column changed
+  # describe 'domestic violence' do
+  #   it 'counts yeses' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C99').summary).to eq(1)
+  #   end
+  #   it 'counts clients' do
+  #     expect(report_result.answer(question: 'Q26', cell: 'C104').summary).to eq(1)
+  #   end
+  # end
+end
