4842a5a6f34f8a0e8666448a7e842de6302cdb4c
Elliot Anders <elliot@greenriver.org>
2023-11-09 12:58:33 -0500

    Minor typo fix, comment removal

--- app/models/cohort_columns/rrh_desired.rb
+++ app/models/cohort_columns/rrh_desired.rb
@@ -9,7 +9,7 @@ module CohortColumns
     attribute :column, String, lazy: true, default: :rrh_desired
     attribute :translation_key, String, lazy: true, default: 'Interested in RRH'
     attribute :title, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.translation_key) }
-    attribute :description_translation_key, String, lazy: true, default: ->(model, _attr) { "#{model.translation_key} Description" } # NO TRANSLATION KEY
+    attribute :description_translation_key, String, lazy: true, default: ->(model, _attr) { "#{model.translation_key} Description" }
     attribute :description, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.description_translation_key) }
 
     def cast_value(val)
