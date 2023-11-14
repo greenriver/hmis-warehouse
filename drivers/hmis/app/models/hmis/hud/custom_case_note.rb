722e97d9bb8756983de835fa36141a6770d2957a
Gig Ashton <gig@greenriver.org>
2023-11-14 15:31:05 -0500

    Add project relationship to case note

--- drivers/hmis/app/models/hmis/hud/custom_case_note.rb
+++ drivers/hmis/app/models/hmis/hud/custom_case_note.rb
@@ -8,6 +8,8 @@ class Hmis::Hud::CustomCaseNote < Hmis::Hud::Base
   self.table_name = :CustomCaseNote
   self.sequence_name = "public.\"#{table_name}_id_seq\""
 
+  include ::Hmis::Hud::Concerns::ClientProjectEnrollmentRelated
+
   belongs_to :client, **hmis_relation(:PersonalID, 'Client')
   belongs_to :enrollment, **hmis_enrollment_relation, optional: true
   belongs_to :user, **hmis_relation(:UserID, 'User')
