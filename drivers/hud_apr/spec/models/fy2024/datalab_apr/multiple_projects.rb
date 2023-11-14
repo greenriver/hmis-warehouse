6ac06eca1a025976446ddcb994f85a4cad6a7075
Elliot <elliot@greenriver.org>
2023-11-14 07:35:56 -0500

    Additional skips for new test kit results; more debugging to come (#3732)

--- drivers/hud_apr/spec/models/fy2024/datalab_apr/multiple_projects.rb
+++ drivers/hud_apr/spec/models/fy2024/datalab_apr/multiple_projects.rb
@@ -418,8 +418,10 @@ RSpec.shared_context 'datalab multiple projects apr', shared_context: :metadata 
         # pending https://airtable.com/appFAz3WpgFmIJMm6/shr8TvO6KfAZ3mOJd/tblYhwasMJptw5fjj/viw7VMUmDdyDL70a7/recZbEHrNyt7aUsfw
         skip: [
           'B3',
+          'C3', # new
           'D3',
           'B5',
+          'C5', # new
           'D5',
         ],
       )
