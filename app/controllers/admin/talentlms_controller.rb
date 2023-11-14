646d27051f0c404f1a74acd7ee1c1d3aa4c9a35d
Dave G <dgreiner@greenriver.org>
2023-11-14 11:35:56 -0500

    Update talentlms controller to better align with rails conventions

--- app/controllers/admin/talentlms_controller.rb
+++ app/controllers/admin/talentlms_controller.rb
@@ -7,32 +7,28 @@
 module Admin
   class TalentlmsController < ApplicationController
     before_action :require_can_manage_config!
-    before_action :set_config, only: [:index, :edit, :update]
 
     def index
+      @config = config_scope.first_or_initialize
+      redirect_to new_admin_talentlm_path if @config.new_record?
+      redirect_to edit_admin_talentlm_path(@config) unless @config.new_record?
     end
 
     def new
       @config = config_scope.first_or_initialize
-      # If a record already exists, we want to be editing. This redirect will
-      # send the user to edit the existing record if one is found.
-      # If we ever have the need for multiple records at once, this will need to be adjusted.
-      redirect_to edit_admin_talentlm_path(@config) unless @config.new_record?
     end
 
     def create
-      @config = config_scope.new
-      if @config.update(config_params)
-        respond_with(@config, location: edit_admin_talentlm_path(@config))
-      else
-        render 'new'
-      end
+      @config = config_scope.create(config_params)
+      respond_with(@config, location: admin_talentlms_path(@config))
     end
 
     def edit
+      @config = config_scope.first_or_initialize
     end
 
     def update
+      @config = config_scope.first_or_initialize
       @config.update(config_params)
       respond_with(@config, location: edit_admin_talentlm_path(@config))
     end
@@ -47,17 +43,6 @@ module Admin
         :api_key,
         :courseid,
       )
-    end
-
-    private def set_config
-      @config = config_scope.first_or_initialize
-
-      # Checks to make sure we are working with the expected record in the database.
-      # If a record already exists, we want to be editing that record. If one does
-      # not exist, we want to be creating it.
-      # If we ever have the need for multiple records at once, this will need to be adjusted.
-      redirect_to new_admin_talentlm_path if @config.new_record?
-      redirect_to edit_admin_talentlm_path(@config) if @config.id && @config.id != params[:id].to_i
     end
   end
 end
