fb23d7334b22b27841c8af04453f68f416e290bd
ZachCMP <zparks@greenriver.org>
2023-11-15 11:00:35 -0800

    Add test for multi-member households

--- drivers/hmis/spec/requests/hmis/project_enrollment_search_spec.rb
+++ drivers/hmis/spec/requests/hmis/project_enrollment_search_spec.rb
@@ -144,6 +144,22 @@ RSpec.describe Hmis::GraphqlController, type: :request do
         expect(response.status).to eq 200
         expect(result.dig('data', 'project', 'enrollments', 'nodes')).to contain_exactly(include('id' => e4.id.to_s), include('id' => e6.id.to_s))
       end
+
+      it 'should filter annuals due excluding household members with recent enrollments' do
+        # Had an assessment 2 years ago, annual is due
+        e4 = create(:hmis_hud_enrollment, data_source: ds1, project: p1, client: c3, entry_date: Date.today - 3.years)
+        create(:hmis_custom_assessment, data_source: ds1, enrollment: e4, data_collection_stage: 5, assessment_date: Date.today - 2.years)
+        # Had an assessment today, annual is not due
+        e5 = create(:hmis_hud_enrollment, data_source: ds1, household_id: e4.household_id, project: p1, client: c3, entry_date: Date.today - 3.years)
+        create(:hmis_custom_assessment, data_source: ds1, enrollment: e5, data_collection_stage: 5, assessment_date: Date.today)
+        # Annual not due yet this year, but assessment was not done last year, annual is due
+        e6 = create(:hmis_hud_enrollment, data_source: ds1, household_id: e4.household_id, project: p1, client: c3, entry_date: Date.today - 3.years)
+        create(:hmis_custom_assessment, data_source: ds1, enrollment: e6, data_collection_stage: 5, assessment_date: Date.today)
+
+        response, result = post_graphql(id: p1.id, filters: { "householdTasks": ['ANNUAL_DUE'] }) { query }
+        expect(response.status).to eq 200
+        expect(result.dig('data', 'project', 'enrollments', 'nodes')).to contain_exactly(include('id' => e4.id.to_s))
+      end
     end
   end
 end
