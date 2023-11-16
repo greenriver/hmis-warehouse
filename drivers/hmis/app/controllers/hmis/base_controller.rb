8e61e0b6a8f32c80683b37660e4b794f20edff46
Gig <gig@greenriver.org>
2023-11-16 16:24:21 -0500

    Log true user as whodunnit when impersonating (#3743)

--- drivers/hmis/app/controllers/hmis/base_controller.rb
+++ drivers/hmis/app/controllers/hmis/base_controller.rb
@@ -46,7 +46,10 @@ class Hmis::BaseController < ActionController::Base
 
   # PaperTrail whodunnit (set in ApplicationController) uses this method to determine the label to be stored
   def user_for_paper_trail
-    current_hmis_user&.id
+    return 'unauthenticated' unless current_hmis_user.present?
+    return current_hmis_user.id unless impersonating?
+
+    "#{true_hmis_user.id} as #{current_hmis_user.id}"
   end
 
   def set_app_user_header
