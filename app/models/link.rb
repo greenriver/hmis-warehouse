eb974c599242165a2a3e7c9fa60e27ac546c232a
Todd Blackman <tdblackman@gmail.com>
2023-11-17 14:03:22 -0500

    This might help with probe timeouts. I think it's the only database hit from the homepage

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,8 +5,10 @@
 ###
 
 class Link < ApplicationRecord
-  scope :for, ->(location) do
-    where(location: location)
+  def self.for(location)
+    @cache ||= {}
+
+    @cache[location] ||= where(location: location)
   end
 
   def available_locations
