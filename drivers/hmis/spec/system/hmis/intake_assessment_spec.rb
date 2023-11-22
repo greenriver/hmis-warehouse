4e8984e26cf138b9cba7fdf250ad7c05c3bff818
Theron Toomey <ttoomey@greenriver.org>
2023-11-22 23:01:33 +0000

    186395195-stale-object-error-when-submitting-household-assessments
    * e2e test for intake assessment submission
    * disable animations to speed up e2e tests
    * capture test errors on teardown

---
+++ drivers/hmis/spec/system/hmis/intake_assessment_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+require_relative '../../requests/hmis/login_and_permissions'
+require_relative '../../support/hmis_base_setup'
+
+RSpec.feature 'Enrollment/household management', type: :system do
+  include_context 'hmis base setup'
+  # could parse CAPYBARA_APP_HOST
+  let!(:ds1) { create(:hmis_data_source, hmis: 'localhost') }
+  let!(:c1) { create :hmis_hud_client, data_source: ds1, user: u1, first_name: 'Quentin', last_name: 'Coldwater' }
+  let!(:c2) { create :hmis_hud_client, data_source: ds1, user: u1, first_name: 'Alice', last_name: 'Quinn' }
+  let!(:unit1) { create :hmis_unit, project: p1, user: user, name: 'unit 1' }
+  let!(:unit2) { create :hmis_unit, project: p1, user: user, name: 'unit 2' }
+  let!(:access_control) { create_access_control(hmis_user, p1) }
+  # need with_coc so enrollment isn't blocked by CoC prompt
+  let!(:p1) { create :hmis_hud_project, data_source: ds1, organization: o1, user: u1, with_coc: true }
+
+  let(:today) { Date.today }
+
+  context 'An active project' do
+    before(:each) do
+      make_household(enrollment_factory: :hmis_hud_wip_enrollment)
+      sign_in(hmis_user)
+      disable_transitions
+      # click_link 'Open Path HMIS'
+      click_link 'Projects'
+      click_link p1.project_name
+      click_link 'Enrollments'
+    end
+
+    def make_household(household_id: Hmis::Hud::Base.generate_uuid, enrollment_factory:)
+      [
+        [c1, { RelationshipToHoH: 1 }],
+        [c2, { RelationshipToHoH: 99 }],
+      ].each do |client, enrollment_attrs|
+        enrollment_attrs.merge!(
+          client: client,
+          HouseholdID: household_id,
+          project: p1,
+          entry_date: today - 5.days,
+          user: u1,
+        )
+        create(enrollment_factory, **enrollment_attrs)
+      end
+      Hmis::Hud::Household.where(household_id: household_id).first!
+    end
+
+    def fill_in_assessment
+      # FIXME- this almost works
+      # default_option = 'Client prefers not to answer'
+      # mui_select default_option, from: 'Prior Living Situation'
+      # mui_select default_option, from: 'Length of stay in prior living situation'
+      # mui_choose default_option, from: 'Income from Any Source'
+      # mui_choose default_option, from: 'Non-Cash Benefits from Any Source'
+      # mui_choose default_option, from: 'Covered by Health Insurance'
+      # mui_select default_option, from: 'Disabling Condition'
+      # mui_select default_option, from: 'Survivor of Domestic Violence'
+    end
+
+    context 'with wip household' do
+      it 'can submit an intake assessment' do
+        fill_in 'Search Clients', with: c1.last_name
+        click_link c1.brief_name
+        click_link 'Assessments'
+        click_link 'Finish Intake'
+
+        assert_text('Household Intake')
+        assert_text(p1.project_name)
+        assert_text(c2.brief_name)
+        assert_text(c1.brief_name)
+        click_button 'Save Assessment'
+        fill_in_assessment
+        click_button 'Next'
+        click_button 'Save Assessment'
+        fill_in_assessment
+        click_button 'Complete Intake'
+
+        assert_text "Complete Entry to #{p1.project_name}"
+
+        # This checkbox doesn't behave reliably, we shouldn't need sleep()
+        sleep(1)
+        with_hidden { check('select all') }
+        sleep(1)
+
+        row_numbers = [1, 2]
+        row_numbers.each do |row|
+          within(:xpath, "//table/tbody/tr[#{row}]") do
+            assert_text('In Progress')
+          end
+        end
+        click_button 'Submit (2) Intake Assessments'
+
+        expect do
+          click_button 'Confirm'
+          row_numbers.each do |row|
+            within(:xpath, "//table/tbody/tr[#{row}]") do
+              assert_text('Submitted')
+            end
+          end
+        end.to change(c1.enrollments.not_in_progress, :count).by(1).
+          and change(c2.enrollments.not_in_progress, :count).by(1)
+      end
+    end
+  end
+end
--- spec/support/e2e_setup.rb
+++ spec/support/e2e_setup.rb
@@ -5,12 +5,15 @@ if ENV['RUN_SYSTEM_TESTS']
 end
 
 # test helper methods
-module SystemSpecHelper
+RSpec.shared_context 'SystemSpecHelper' do
   # from user factory
   DEFAULT_USER_PASSWORD = Digest::SHA256.hexdigest('abcd1234abcd1234')
 
   def sign_in(user, password: DEFAULT_USER_PASSWORD)
+    # this should go into before-each but that seems to hang up some tests
     visit('/')
+    disable_transitions
+
     fill_in 'Email Address', with: user.email
     fill_in 'Password', with: password
     click_button('Sign In')
@@ -26,10 +29,20 @@ module SystemSpecHelper
     find_field(id, type: :hidden).set(value)
   end
 
+  def mui_choose(choice, from:)
+    label = find('label', text: from)
+    id = label['id']
+    within("[aria-labelledby='#{id}']") do
+      choose(choice)
+    end
+  end
+
   def mui_select(choice, from:)
     label = find('label', text: from)
     id = label['for']
-    find("##{id}").click
+    # we seem to have invalid ids such as "3.917A.1"
+    # find("##{id}").click
+    find("[id='#{id}']").click
     find('li[role=option]', text: choice).click
   end
 
@@ -42,13 +55,22 @@ module SystemSpecHelper
       Capybara.ignore_hidden_elements = last_value
     end
   end
+
+  def disable_transitions
+    @disable_transitions_js ||= File.read(Rails.root.join('spec/assets/disable_transitions.js'))
+    browser.add_script_tag(content: @disable_transitions_js)
+  end
+
+  def browser
+    page.driver.browser
+  end
 end
 
 RSpec.configure do |config|
   next unless ENV['RUN_SYSTEM_TESTS']
 
   config.include E2eTests::Helpers, type: :system
-  config.include SystemSpecHelper, type: :system
+  config.include_context 'SystemSpecHelper', type: :system
 
   config.prepend_before(:each, type: :system) do
     # Use JS driver always
