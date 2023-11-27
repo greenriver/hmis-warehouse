52e87ac49467b779a15600360b98c119d4981f3d
Elliot <elliot@greenriver.org>
2023-11-27 15:43:05 -0500

    Apply suggestions from code review
    Co-authored-by: Dave G <149399758+dtgreiner@users.noreply.github.com>
    Co-authored-by: Sandy Wise <33072305+awisema@users.noreply.github.com>

--- app/models/cohort_columns/individual_in_most_recent_enrollment.rb
+++ app/models/cohort_columns/individual_in_most_recent_enrollment.rb
@@ -9,7 +9,7 @@ module CohortColumns
     attribute :column, String, lazy: true, default: :individual_in_most_recent_homeless_enrollment
     attribute :translation_key, String, lazy: true, default: 'Presented as Individual'
     attribute :title, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.translation_key) }
-    attribute :description_translation_key, String, lazy: true, default: 'Client presented as an individual (with no other household members) in the most recent homeless enrollment'
+    attribute :description_translation_key, String, lazy: true, default: 'Client presented as an individual (i.e., with no other household members) in the most recent homeless enrollment'
     attribute :description, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.description_translation_key) }
 
     def cast_value(val)
