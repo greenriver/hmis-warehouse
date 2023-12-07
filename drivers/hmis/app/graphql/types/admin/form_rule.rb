19cc710250f1a26ebc084b1e11b3dbaf8f224782
Gig Ashton <gig@greenriver.org>
2023-12-07 09:22:18 -0500

    Cleanup

--- drivers/hmis/app/graphql/types/admin/form_rule.rb
+++ drivers/hmis/app/graphql/types/admin/form_rule.rb
@@ -15,33 +15,32 @@ module Types
       arg :form_type, [Types::Forms::Enums::FormRole]
       arg :active_status, [Types::HmisSchema::Enums::ActiveStatus]
       arg :system_form, [Types::HmisSchema::Enums::SystemStatus]
-      arg :service_category, [ID]
       arg :project_type, [Types::HmisSchema::Enums::ProjectType]
       arg :applied_to_project, ID
     end
 
     field :id, ID, null: false
+    # Form Definition info
     field :definition_identifier, String, null: false
     field :definition_id, ID, null: true
     field :definition_role, Types::Forms::Enums::FormRole, null: true
     field :definition_title, String, null: true
-
-    # field :definition, Types::Forms::FormDefinition, null: false
-
-    # Applicability fields
-    field :project, HmisSchema::Project, null: true
-    field :organization, HmisSchema::Organization, null: true
-    field :service_category, HmisSchema::ServiceCategory, null: true, method: :custom_service_category
-    field :service_type, HmisSchema::ServiceType, null: true, method: :custom_service_type
+    # Applicability rule fields
+    field :project_type, Types::HmisSchema::Enums::ProjectType, null: true
     field :funder, Types::HmisSchema::Enums::Hud::FundingSource, null: true
     field :other_funder, String, null: true
-    field :project_type, Types::HmisSchema::Enums::ProjectType, null: true
+    field :organization, HmisSchema::Organization, null: true
+    field :project, HmisSchema::Project, null: true
+    field :service_category, HmisSchema::ServiceCategory, null: true, method: :custom_service_category
+    field :service_type, HmisSchema::ServiceType, null: true, method: :custom_service_type
     field :data_collected_about, Types::Forms::Enums::DataCollectedAbout, null: true
-
-    field :system, Boolean, null: false
+    # Status and metadata
     field :active, Boolean, null: false
+    field :system, Boolean, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    # TODO ADD: list of projects that this rule ends up being applied to
 
     def definition_id
       load_ar_association(object, :definition)&.id
