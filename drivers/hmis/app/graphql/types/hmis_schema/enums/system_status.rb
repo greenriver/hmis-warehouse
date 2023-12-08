449b2c15ef11826ead7edf4e9a362f59c732af2d
Gig <gig@greenriver.org>
2023-12-08 09:11:45 -0500

    Resolve data collection rules (#3804)

---
+++ drivers/hmis/app/graphql/types/hmis_schema/enums/system_status.rb
@@ -0,0 +1,14 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+module Types
+  class HmisSchema::Enums::SystemStatus < Types::BaseEnum
+    value 'SYSTEM', 'System'
+    value 'NON_SYSTEM', 'Non-System'
+  end
+end
