5918151400742da83109ca8c3c79b9cda53f5fae
Elliot Anders <elliot@greenriver.org>
2023-12-13 15:59:37 -0500

    Use safety_assured for indexes

--- db/warehouse/migrate/20231203221840_idx_warehouse_versions.rb
+++ db/warehouse/migrate/20231203221840_idx_warehouse_versions.rb
@@ -1,10 +1,10 @@
 class IdxWarehouseVersions < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-
   def change
     # indexes for audit reports
-    add_index :versions, :client_id, algorithm: :concurrently
-    add_index :versions, :enrollment_id, algorithm: :concurrently
-    add_index :versions, :project_id, algorithm: :concurrently
+    safety_assured do
+      add_index :versions, :client_id, algorithm: :concurrently
+      add_index :versions, :enrollment_id, algorithm: :concurrently
+      add_index :versions, :project_id, algorithm: :concurrently
+    end
   end
 end
--- db/warehouse/migrate/20231212212354_add_data_source_to_files.rb
+++ db/warehouse/migrate/20231212212354_add_data_source_to_files.rb
@@ -1,6 +1,7 @@
 class AddDataSourceToFiles < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
   def change
-    add_reference :files, :data_source, null: true, index: {algorithm: :concurrently}
+    safety_assured do
+      add_reference :files, :data_source, null: true, index: {algorithm: :concurrently}
+    end
   end
 end
