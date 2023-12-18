cb0b74f52ee711e4cf6e25882c8e0cdc13264348
Elliot Anders <elliot@greenriver.org>
2023-12-18 11:23:19 -0500

    Additional default help file links

--- app/models/grda_warehouse/help.rb
+++ app/models/grda_warehouse/help.rb
@@ -93,6 +93,81 @@ class GrdaWarehouse::Help < GrdaWarehouseBase
         action_name: 'index',
         external_url: "#{DEFAULT_HELP_URL}/First-Time-Homeless",
       },
+      {
+        controller_path: 'project_pass_fail/warehouse_reports/project_pass_fail',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Project-Pass-Fail",
+      },
+      {
+        controller_path: 'inactive_client_report/warehouse_reports/reports',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Client-Activity-Report",
+      },
+      {
+        controller_path: 'client_documents_report/warehouse_reports/reports',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Client-Documents-Report",
+      },
+      {
+        controller_path: 'core_demographics_report/warehouse_reports/core',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Core-Demographics",
+      },
+      {
+        controller_path: 'core_demographics_report/warehouse_reports/demographic_summary',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Demographic-Summary-Report",
+      },
+      {
+        controller_path: 'homeless_summary_report/warehouse_reports/reports',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/System-Performance-Measures-by-Sub-Population",
+      },
+      {
+        controller_path: 'performance_measurement/warehouse_reports/reports',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/CoC-Performance-Measurement-Dashboard",
+      },
+      {
+        controller_path: 'boston_reports/warehouse_reports/community_of_origins',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Community-of-Origin",
+      },
+      {
+        controller_path: 'system_pathways/warehouse_reports/reports',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/System-Pathways-Report",
+      },
+      {
+        controller_path: 'clients_sub_pop/dashboards/clients',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
+      {
+        controller_path: 'adults_with_children_sub_pop/dashboards/adults_with_children',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
+      {
+        controller_path: 'adult_only_households_sub_pop/dashboards/adult_only_households',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
+      {
+        controller_path: 'child_only_households_sub_pop/dashboards/child_only_households',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
+      {
+        controller_path: 'non_veterans_sub_pop/dashboards/non_veterans',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
+      {
+        controller_path: 'veterans_sub_pop/dashboards/veterans',
+        action_name: 'index',
+        external_url: "#{DEFAULT_HELP_URL}/Population-Dashboards",
+      },
     ]
   end
 
