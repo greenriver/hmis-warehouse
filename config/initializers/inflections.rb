ee99abb558b6ec7bbf7edf3cdd41616fc3ec057a
Gig Ashton <gig@greenriver.org>
2023-12-29 11:49:32 -0500

    Add extensions

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -17,6 +17,7 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'census', 'censuses'
   inflect.irregular 'supplemental_enrollment_data', 'supplemental_enrollment_data'
+  inflect.irregular 'hmis', 'hmis'
 
   # rails_drivers fails to load extensions for "plural" names since classify() will singularize the driver name
   # https://github.com/degica/rails_drivers/blob/9ae472566aab064be43dbc355d4b5bb611c6d360/lib/rails_drivers/extensions.rb#L23
