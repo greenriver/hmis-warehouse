a18c730e463b9a1a3fd4955032ae0868a6d9afda
Theron Toomey <ttoomey@greenriver.org>
2024-01-12 03:59:35 +0000

    186821553-rds-metrics

---
+++ app/models/grda_warehouse/postgres_inspector.rb
@@ -0,0 +1,87 @@
+###
+# Copyright 2016 - 2023 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# == GrdaWarehouse::PostgresInspector
+#
+# Utility to help monitor health and growth of postgres database
+#
+class GrdaWarehouse::PostgresInspector
+  include SafeInspectable
+  delegate :select_all, :quote, to: :connection
+
+  attr_accessor :connection
+
+  def initialize(connection)
+    self.connection = connection
+  end
+
+  def table_stats(schema: 'public')
+    select_all(format(TABLE_STATS_SQL, schema: quote(schema)))
+  end
+
+  def index_stats(schema: 'public')
+    select_all(format(INDEX_STATS_SQL, schema: quote(schema)))
+  end
+
+  def database_stats
+    db_name = connection.current_database
+    select_all(format(DATABASE_STATS_SQL, db_name: quote(db_name)))
+  end
+
+  DATABASE_STATS_SQL = <<~SQL.freeze
+    SELECT
+      pg_database_size(%{db_name})::bigint AS total_size,
+      CASE
+        WHEN (blks_hit + blks_read) = 0 THEN 0.0
+        ELSE round((blks_hit / (blks_hit + blks_read)::float)::numeric, 2)::float
+      END AS cache_hit_ratio,
+      temp_bytes,
+      deadlocks,
+      stats_reset
+    FROM
+      pg_stat_database
+    WHERE
+      datname = %{db_name}
+  SQL
+
+  TABLE_STATS_SQL = <<~SQL.freeze
+    SELECT
+      t.tablename,
+      c.reltuples::bigint AS num_rows,
+      pg_relation_size(quote_ident(t.tablename)::text) AS table_size,
+      (pg_total_relation_size(quote_ident(t.tablename)::text) - pg_relation_size(quote_ident(t.tablename)::text)) AS total_index_size
+    FROM
+      pg_tables t
+    LEFT OUTER JOIN
+      pg_class c ON t.tablename = c.relname
+    WHERE
+      t.schemaname = %{schema}
+    ORDER BY
+      t.tablename;
+  SQL
+
+  INDEX_STATS_SQL = <<~SQL.freeze
+    SELECT
+      t.tablename,
+      ipg.relname AS index_name,
+      idx_scan AS number_of_scans,
+      idx_tup_read AS tuples_read,
+      idx_tup_fetch AS tuples_fetched,
+      pg_relation_size(ipg.oid) AS index_size
+    FROM
+      pg_tables t
+    JOIN
+      pg_index x ON x.indrelid = (SELECT oid FROM pg_class WHERE relname = t.tablename)
+    JOIN
+      pg_class ipg ON ipg.oid = x.indexrelid
+    JOIN
+      pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid
+    WHERE
+      t.schemaname = %{schema}
+    ORDER BY
+      t.tablename, ipg.relname;
+  SQL
+end
