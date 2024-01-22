a48776c13ae4a0777a94188ae09201975776f1fc
Elliot Anders <elliot@greenriver.org>
2024-01-22 14:35:06 -0500

    Freeze an array

--- drivers/performance_measurement/app/models/performance_measurement/equity_analysis/builder.rb
+++ drivers/performance_measurement/app/models/performance_measurement/equity_analysis/builder.rb
@@ -218,7 +218,7 @@ module PerformanceMeasurement::EquityAnalysis
         ['Count', 'count'],
         ['Percentage', 'percentage'],
         ['Rate', 'rate'],
-      ]
+      ].freeze
     end
   end
 end
