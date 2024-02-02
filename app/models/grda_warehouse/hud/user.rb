03f4d64baafea0726d99871ee02cce54fcf19dde
Elliot <elliot@greenriver.org>
2024-02-02 15:18:38 -0500

    Copyright -> 2024 (#3953)
    * Update old copyrights to 2023
    
    * Standardize 2023 copyright
    
    * Update copyright to 2024
    
    * Copyright notices
    
    * Revert rubocop todo

--- app/models/grda_warehouse/hud/user.rb
+++ app/models/grda_warehouse/hud/user.rb
@@ -1,5 +1,5 @@
 ###
-# Copyright 2016 - 2023 Green River Data Analysis, LLC
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
 #
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
