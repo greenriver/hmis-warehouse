fee49afac5e916c0f3ebe58327191fe3416c2e47
Theron Toomey <ttoomey@greenriver.org>
2024-02-02 15:31:55 +0000

    add-documentation-to-custom-hud-classes

--- drivers/hmis/app/models/hmis/hud/custom_service_category.rb
+++ drivers/hmis/app/models/hmis/hud/custom_service_category.rb
@@ -4,7 +4,7 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-# "CustomServiceCategory" is NOT a HUD record type. Although it uses CamelCase conventions, this model is particular to Open Path
+# "CustomServiceCategory" is NOT a HUD defined record type. Although it uses CamelCase conventions, this model is particular to Open Path. CamelCase is used for compatibility with "Appendix C - Custom file transfer template"in the HUD HMIS CSV spec. This specifies optional additional CSV files with the naming convention of Custom*.csv
 
 class Hmis::Hud::CustomServiceCategory < Hmis::Hud::Base
   self.table_name = :CustomServiceCategories
