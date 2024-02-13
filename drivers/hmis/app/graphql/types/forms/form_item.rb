a800fa62172264a7fc91bc5c3960bf8c0210473c
martha <medwards@greenriver.org>
2024-02-13 17:38:27 -0500

    Replace variant with component

--- drivers/hmis/app/graphql/types/forms/form_item.rb
+++ drivers/hmis/app/graphql/types/forms/form_item.rb
@@ -23,7 +23,6 @@ module Types
     field :type, Types::Forms::Enums::ItemType, null: false
     field :prefill, Boolean, 'Whether to allow pre-filling this section from a previous assessment', null: false
     field :component, Types::Forms::Enums::Component, 'Component to use for display/input of this item', null: true
-    field :variant, Types::Forms::Enums::FormGroupVariant, 'Component variant to use for display', null: true
     field :text, String, 'Primary text for the item', null: true
     field :brief_text, String, 'Label to use for placeholder and population table', null: true
     field :readonly_text, String, 'Text to use for the item when displayed in read-only view', null: true
