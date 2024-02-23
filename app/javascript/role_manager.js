807cb6f6c20f7771c6e320e348f2cc180a5a3fef
Elliot Anders <elliot@greenriver.org>
2024-02-23 16:09:55 -0500

    Role permissions save; need saving UI

--- app/javascript/role_manager.js
+++ app/javascript/role_manager.js
@@ -3,7 +3,7 @@ window.App.StimulusApp = window.App.StimulusApp || {}
 
 App.StimulusApp.register('role-manager', class extends Stimulus.Controller {
   static get targets() {
-    return ['permissionCategory', 'roleToggle', 'roleColumn']
+    return ['permissionCategory', 'roleToggle', 'roleColumn', 'individualPermission', 'inputWrapper']
   }
   // This version of stimulus doesn't seem to support values
   // static values = {
@@ -15,6 +15,7 @@ App.StimulusApp.register('role-manager', class extends Stimulus.Controller {
   connect() {
     this.element['roleManager'] = this // allow access to this controller from other controllers
     console.log('role manager connected', this.roleToggleTargets)
+    this.path = $(this.inputWrapperTarget).data('roleManagerFormValue')
   }
 
   toggleSection(e) {
@@ -48,4 +49,56 @@ App.StimulusApp.register('role-manager', class extends Stimulus.Controller {
       });
     }
   }
+
+  save(e) {
+    let target = $(e.currentTarget)
+    let target_role_id = target.data('roleManagerRoleValue')
+    let key = target.data('roleManagerPermissionValue')
+    let checked = $(target).is(':checked')
+    let value = checked ? '1' : '0'
+    let data = { role: { } }
+    data.role[key] = value
+    console.log(target_role_id, key, checked, this.path, data)
+    $.ajax({
+      type: 'PATCH',
+      dataType: 'JSON',
+      url: `${this.path}/${target_role_id}`,
+      data: data,
+    })
+  }
 });
+
+
+// submitChanges() {
+//   $(this.submitActionSelector).trigger('blur')
+//   if (this.isSaving) return
+//   this.saving()
+//   const {
+//     tableContainerSelector,
+//     tableObjectHeadingSelector,
+//     tableInputSelector,
+//   } = this.props
+//   const rolePromises =
+//     this.$tableContainer.data('objects')
+//       .map((id, i) => {
+//         const inputBaseQuery = `${tableInputSelector}[data-role=${id}] input`
+//         const inputs = this.$tableContainer.find(`${inputBaseQuery}.dirty`)
+//         if (inputs.length) {
+//           inputs.add(`${tableContainerSelector} input[name=authenticity_token]`)
+//           return $.ajax({
+//             type: 'PATCH',
+//             dataType: 'JSON',
+//             url: `${this.patch_url}/${id}`,
+//             data: this.$tableContainer.find(inputBaseQuery).serialize(),
+//           })
+//         } else {
+//           return null
+//         }
+//       })
+//   Promise.all(rolePromises)
+//     .then(() => {
+//       this.confirmSaved()
+//     }).catch((error) => {
+//       this.confirmSaved(error)
+//     })
+// }
