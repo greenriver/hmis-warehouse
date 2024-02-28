cc4675e682cf1ed7ad75245baec8ed0e0d9d30fa
Theron Toomey <ttoomey@greenriver.org>
2024-02-28 01:06:24 +0000

    186704906-public-pages

--- db/warehouse/migrate/20240223002628_add_static_forms.rb
+++ db/warehouse/migrate/20240223002628_add_static_forms.rb
@@ -3,23 +3,22 @@ class AddStaticForms < ActiveRecord::Migration[6.1]
     create_table :hmis_static_forms do |t|
       t.timestamps
       t.string :name, null: false
-      t.string :version, null: false
-      t.jsonb :fields, null: false
-      t.string :remote_location
+      t.string :content_version, null: false
+      t.text :content, null: false
+      t.jsonb :form_definition, null: false
+      t.string :object_key, index: { unique: true }
     end
-    add_index :hmis_static_forms, [:name, :version], name: 'uidx_hmis_static_forms', unique: true
+    add_index :hmis_static_forms, [:name, :content_version], name: 'uidx_hmis_static_forms', unique: true
 
     create_table :hmis_static_form_submissions do |t|
       t.timestamps
       t.datetime :submitted_at
-      t.integer :spam_score
+      t.float :spam_score
       t.string :status, null: false, default: 'new'
-      t.string :form_name, null: false
-      t.string :form_version, null: false
-      t.string :remote_location
+      t.string :form_content_version, null: false, index: true
+      t.string :object_key, index: { unique: true }
       t.jsonb :data, null: false
       t.text :notes
     end
-    add_index :hmis_static_form_submissions, :remote_location, name: 'uidx_hmis_static_form_submissions', unique: true
   end
 end
