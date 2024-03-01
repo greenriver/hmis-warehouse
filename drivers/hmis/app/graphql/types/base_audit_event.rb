15eab3b4a3ee01cd6644b95d54b53c397a4e0ae4
martha <medwards@greenriver.org>
2024-03-01 12:47:47 -0500

    Avoid n+1 issue when loading custom assessment title
    Co-authored-by: Theron Toomey <ttoomey@greenriver.org>

--- drivers/hmis/app/graphql/types/base_audit_event.rb
+++ drivers/hmis/app/graphql/types/base_audit_event.rb
@@ -113,7 +113,7 @@ module Types
 
     private def custom_assessment_title
       ca = load_ar_scope(scope: Hmis::Hud::CustomAssessment.with_deleted, id: object.item_id)
-      ca&.form_processor&.definition&.title
+      ca ? load_ar_association(object, :definition)&.title : nil
     end
 
     # NOTE: will be nil if this is a 'destroy' event
