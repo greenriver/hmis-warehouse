a0dc51384fd51ebd25d883fcd338b87727a04aa4
Theron Toomey <ttoomey@greenriver.org>
2024-03-06 05:54:53 +0000

    186704906-public-pages

--- drivers/hmis/app/graphql/types/hmis_schema/has_external_form_submissions.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/has_external_form_submissions.rb
@@ -23,7 +23,7 @@ module Types
       end
 
       def resolve_external_form_submissions(scope = object.external_form_submissions, filters: nil)
-        raise 'filters tbd' if filters.present?
+        # FIXME filters TBD
         return [] unless current_user.can_manage_external_form_submissions?
 
         scope.order(submitted_at: :desc, id: :desc)
