c9d8b762920cf197d59d47d957ffc0ac13d791b6
Theron Toomey <ttoomey@greenriver.org>
2024-03-08 10:18:32 -0500

    Update drivers/hmis/app/graphql/mutations/update_external_form_submission.rb
    Co-authored-by: martha <medwards@greenriver.org>

--- drivers/hmis/app/graphql/mutations/update_external_form_submission.rb
+++ drivers/hmis/app/graphql/mutations/update_external_form_submission.rb
@@ -13,7 +13,7 @@ module Mutations
     field :errors, [Types::HmisSchema::ValidationError], null: false, resolver: Resolvers::ValidationErrors
 
     def resolve(id:, input:)
-      record = Hmis::HmisSchema::ExternalFormSubmission.find(id)
+      record = HmisExternalApis::ExternalForms::FormSubmission.find(id)
       raise 'Access denied' unless allowed?(permissions: [:can_manage_external_form_submissions])
 
       record.assign_attributes(**input.to_params)
