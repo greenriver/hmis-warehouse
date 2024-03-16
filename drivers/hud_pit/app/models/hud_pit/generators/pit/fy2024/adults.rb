a33d3ed92cbf8bd0d0ae36f1a2f8a98e4d86f3c5
Sandy Wise <33072305+awisema@users.noreply.github.com>
2024-03-16 10:03:08 -0400

    Aw/hic pit 185769387 (#3993)
    * WIP: 2024 PIT
    
    * WIP: 2024 HIC
    
    * Apply changes from merge conflict.
    
    * Update warehouse schema.
    
    * return the filter in lsa_filter update method instead of the end date
    
    * 2024 pit questions
    
    * Update queston names and minor code cleanup
    
    * 2024 version of DomesticViolenceSurvivor
    
    * Use more understandable language
    
    ---------
    
    Co-authored-by: Dave G <dgreiner@greenriver.org>
    Co-authored-by: Elliot Anders <elliot@greenriver.org>

---
+++ drivers/hud_pit/app/models/hud_pit/generators/pit/fy2024/adults.rb
@@ -0,0 +1,88 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+module HudPit::Generators::Pit::Fy2024
+  class Adults < Base
+    QUESTION_NUMBER = 'Adult Only (without children)'.freeze
+
+    def self.filter_pending_associations(pending_associations)
+      pending_associations.select { |_, row| row[:household_type].to_s == 'adults_only' }
+    end
+
+    def run_question!
+      @report.start(QUESTION_NUMBER, [QUESTION_NUMBER])
+
+      calculate
+
+      @report.complete(QUESTION_NUMBER)
+    end
+
+    private def project_types
+      [
+        project_type_es_clause,
+        project_type_th_clause,
+        project_type_sh_clause,
+        project_type_so_clause,
+      ]
+    end
+
+    private def rows
+      [
+        :households,
+        :clients,
+        :youth,
+        :age_25_34,
+        :age_35_44,
+        :age_45_54,
+        :age_55_64,
+        :age_over_64,
+        :woman,
+        :man,
+        :culturally_specific_identity,
+        :transgender,
+        :non_binary,
+        :questioning,
+        :different_identity,
+        :more_than_one_gender,
+        :native_ak,
+        :native_ak_latino,
+        :asian,
+        :asian_latino,
+        :black_af_american,
+        :black_af_american_latino,
+        :latino_only,
+        :mid_east_na,
+        :mid_east_na_latino,
+        :native_pi,
+        :native_pi_latino,
+        :white,
+        :white_latino,
+        :multi_racial,
+        :multi_racial_latino,
+        :chronic_clients,
+      ]
+    end
+
+    private def calculate
+      table_name = QUESTION_NUMBER
+      metadata = {
+        header_row: [
+          'Persons in Households without Children',
+          'Emergency',
+          'Transitional',
+          'Safe Haven',
+          'Outreach',
+        ],
+        row_labels: row_labels,
+        first_column: 'B',
+        last_column: 'E',
+        first_row: 2,
+        last_row: rows.count + 1,
+      }
+      populate_table(table_name, metadata)
+    end
+  end
+end
