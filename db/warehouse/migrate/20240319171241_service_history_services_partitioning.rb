244a2da5e8214f4ea5f31d0a504539f171ed2c02
Todd Blackman <tdblackman@gmail.com>
2024-03-20 12:03:13 -0400

    lint fixes

--- db/warehouse/migrate/20240319171241_service_history_services_partitioning.rb
+++ db/warehouse/migrate/20240319171241_service_history_services_partitioning.rb
@@ -11,17 +11,17 @@ class ServiceHistoryServicesPartitioning < ActiveRecord::Migration[6.1]
       test_query = %(EXPLAIN SELECT "date" FROM "service_history_services" WHERE  "date" between '2022-06-01' AND '2022-06-30')
       original_plan = execute(test_query).to_a.map(&:values).join("\n")
 
-      Rails.logger.info "Preparing a temporary table we can copy the structure of"
+      Rails.logger.info 'Preparing a temporary table we can copy the structure of'
       execute %(CREATE TABLE service_history_services_tmp (LIKE service_history_services INCLUDING ALL))
       execute %(ALTER TABLE service_history_services_tmp DROP CONSTRAINT service_history_services_tmp_pkey)
       execute %(ALTER TABLE service_history_services_tmp ADD PRIMARY KEY (id, "date"))
 
-      Rails.logger.info "Creating partitioned table"
+      Rails.logger.info 'Creating partitioned table'
       execute %(CREATE TABLE service_history_services_partitioned (LIKE service_history_services_tmp INCLUDING ALL) PARTITION BY RANGE ("date"))
       execute %(DROP TABLE service_history_services_tmp)
 
-      Rails.logger.info "Detatching subtables from parent"
-      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year,table|
+      Rails.logger.info 'Detatching subtables from parent'
+      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year, table|
         execute %(ALTER TABLE #{table} NO INHERIT service_history_services)
         execute %(ALTER TABLE #{table} DROP constraint "service_history_services_#{year}_date_check")
       end
@@ -29,16 +29,16 @@ class ServiceHistoryServicesPartitioning < ActiveRecord::Migration[6.1]
       execute %(ALTER TABLE service_history_services_remainder NO INHERIT service_history_services)
       execute %(ALTER TABLE service_history_services_remainder DROP constraint "service_history_services_remainder_date_check")
 
-      Rails.logger.info "Reatatching subtables to new parent"
-      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year,table|
+      Rails.logger.info 'Reatatching subtables to new parent'
+      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year, table|
         # Inclusive at lower end, and exclusive at upper end.
-        dates = [Date.new(year, 1, 1).to_s(:db), Date.new(year+1, 1, 1).to_s(:db)]
+        dates = [Date.new(year, 1, 1).to_s(:db), Date.new(year + 1, 1, 1).to_s(:db)]
         execute %(ALTER TABLE service_history_services_partitioned ATTACH PARTITION #{table} FOR VALUES FROM ('#{dates[0]}') TO ('#{dates[1]}'))
       end
 
       execute %(ALTER TABLE service_history_services_partitioned ATTACH PARTITION service_history_services_remainder DEFAULT)
 
-      Rails.logger.info "Renaming tables to make new partitioned table available to the application"
+      Rails.logger.info 'Renaming tables to make new partitioned table available to the application'
       execute %(ALTER TABLE service_history_services RENAME TO service_history_services_was_for_inheritance)
       execute %(ALTER TABLE service_history_services_partitioned RENAME TO service_history_services)
 
@@ -52,15 +52,15 @@ class ServiceHistoryServicesPartitioning < ActiveRecord::Migration[6.1]
 
   def down
     safety_assured do
-      Rails.logger.info "Detatching subtables"
-      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year,table|
+      Rails.logger.info 'Detatching subtables'
+      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |_year, table|
         execute %(ALTER TABLE service_history_services DETACH PARTITION #{table})
       end
 
       execute %(ALTER TABLE service_history_services DETACH PARTITION service_history_services_remainder)
 
-      Rails.logger.info "Retatching subtables to inherited"
-      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year,table|
+      Rails.logger.info 'Retatching subtables to inherited'
+      GrdaWarehouse::ServiceHistoryService.sub_tables.each do |year, table|
         execute %(ALTER TABLE #{table} ADD constraint "service_history_services_#{year}_date_check" CHECK (date >= '#{year}-01-01'::date AND date <= '#{year}-12-31'::date) )
         execute %(ALTER TABLE #{table} INHERIT service_history_services_was_for_inheritance)
       end
@@ -68,13 +68,13 @@ class ServiceHistoryServicesPartitioning < ActiveRecord::Migration[6.1]
       execute %(ALTER TABLE service_history_services_remainder ADD constraint "service_history_services_remainder_date_check" CHECK (date < '2000-01-01'::date OR date > '2050-12-31'::date) )
       execute %(ALTER TABLE service_history_services_remainder INHERIT service_history_services_was_for_inheritance)
 
-      Rails.logger.info "Renaming tables to make old partitioned table available to the application"
+      Rails.logger.info 'Renaming tables to make old partitioned table available to the application'
       execute %(ALTER TABLE service_history_services RENAME TO service_history_services_delete_me)
       execute %(ALTER TABLE service_history_services_was_for_inheritance RENAME TO service_history_services)
 
       results = execute(%(select count(*) AS cnt FROM service_history_services_delete_me))
       if results.to_a.first['cnt'] == 0
-         execute(%(DROP TABLE service_history_services_delete_me))
+        execute(%(DROP TABLE service_history_services_delete_me))
       else
         # This shouldn't be possible. Just being extra careful.
         raise "For some reason service_history_services_delete_me wasn't empty!!!!"
@@ -85,7 +85,7 @@ class ServiceHistoryServicesPartitioning < ActiveRecord::Migration[6.1]
   end
 
   def service_history_view
-    execute("DROP VIEW public.service_history")
+    execute('DROP VIEW public.service_history')
 
     execute(<<~SQL)
       CREATE VIEW public.service_history AS
