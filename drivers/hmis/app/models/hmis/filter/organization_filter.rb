80780c000ce82d7068a910e93e7379767e5dde6a
martha <medwards@greenriver.org>
2024-03-29 15:23:49 -0400

    Add search filter for Organizations query

---
+++ drivers/hmis/app/models/hmis/filter/organization_filter.rb
@@ -0,0 +1,20 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+class Hmis::Filter::OrganizationFilter < Hmis::Filter::BaseFilter
+  def filter_scope(scope)
+    scope = ensure_scope(scope)
+    scope.
+      yield_self(&method(:with_search_term)).
+      yield_self(&method(:clean_scope))
+  end
+
+  protected
+
+  def with_search_term(scope)
+    with_filter(scope, :search_term) { scope.matching_search_term(input.search_term) }
+  end
+end
