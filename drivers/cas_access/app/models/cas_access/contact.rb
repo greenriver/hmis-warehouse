9c1e0aeb2da4d55bfa02c28817dfc46601a5128e
Dave G <dgreiner@greenriver.org>
2024-04-01 15:49:13 -0400

    Add active and closed matches to CAS user directory download

--- drivers/cas_access/app/models/cas_access/contact.rb
+++ drivers/cas_access/app/models/cas_access/contact.rb
@@ -8,5 +8,7 @@ module CasAccess
   class Contact < CasBase
     self.table_name = :contacts
     belongs_to :user, optional: true
+    has_many :clent_opportunity_match_contacts, class_name: 'CasAccess::ClientOpportunityMatchContact', inverse_of: :contact
+    has_many :client_opportunity_matches, through: :clent_opportunity_match_contacts, source: :match
   end
 end
--- drivers/cas_access/app/models/cas_access/opportunity.rb
+++ drivers/cas_access/app/models/cas_access/opportunity.rb
@@ -9,5 +9,12 @@ module CasAccess
     self.table_name = :opportunities
     belongs_to :voucher, optional: true
     has_many :programs, through: :voucher
+    has_many :opportunity_contacts
+
+    has_one :status_match, -> { where arel_table[:active].eq(true).or(arel_table[:closed].eq(true).and(arel_table[:closed_reason].eq('success'))) }, class_name: 'ClientOpportunityMatch'
+    has_many :closed_matches, -> do
+      where(closed: true).
+        order(updated_at: :desc)
+    end, class_name: 'ClientOpportunityMatch'
   end
 end
