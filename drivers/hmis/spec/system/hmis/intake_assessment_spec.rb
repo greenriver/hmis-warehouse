5798a53682c885bf26eaabf53197ee3c9797b62e
martha <medwards@greenriver.org>
2024-04-01 14:21:13 -0400

    Get existing e2e tests running (#4193)
    * Get existing e2e tests running
    
    * Respond to PR comments
    
    * Add readme for e2e tests
    
    * Add test for date validation
    
    * Add test for enrolling in project with multiple CoCs
    
    * Add comments to make docs more discoverable

--- drivers/hmis/spec/system/hmis/intake_assessment_spec.rb
+++ drivers/hmis/spec/system/hmis/intake_assessment_spec.rb
@@ -12,7 +12,8 @@ RSpec.feature 'Enrollment/household management', type: :system do
   let!(:unit2) { create :hmis_unit, project: p1, user: user, name: 'unit 2' }
   let!(:access_control) { create_access_control(hmis_user, p1) }
   # need with_coc so enrollment isn't blocked by CoC prompt
-  let!(:p1) { create :hmis_hud_project, data_source: ds1, organization: o1, user: u1, with_coc: true }
+  # need funder so that certain form fields (Income, Health Insurance, etc.) show up for intakes in this project
+  let!(:p1) { create :hmis_hud_project, data_source: ds1, organization: o1, user: u1, with_coc: true, funders: [20] }
 
   let(:today) { Date.current }
 
@@ -48,13 +49,13 @@ RSpec.feature 'Enrollment/household management', type: :system do
       default_option = 'Client prefers not to answer'
       mui_select default_option, from: 'Prior Living Situation'
       mui_select default_option, from: 'Length of stay in prior living situation'
-      mui_choose default_option, from: 'Income from Any Source'
-      mui_choose default_option, from: 'Non-Cash Benefits from Any Source'
-      mui_choose default_option, from: 'Covered by Health Insurance'
-      mui_select default_option, from: 'Disabling Condition'
+      mui_radio_choose default_option, from: 'Income from Any Source'
+      mui_radio_choose default_option, from: 'Non-Cash Benefits from Any Source'
+      mui_radio_choose default_option, from: 'Covered by Health Insurance'
+      mui_table_select default_option, row: 'Overall Disabling Condition', column: 'Status'
       mui_select default_option, from: 'Survivor of Domestic Violence'
       click_button 'Save Assessment'
-      assert_text(/Last saved [0-9] seconds ago/)
+      assert_text(/Last saved [0-9] seconds? ago/)
     end
 
     context 'with wip household' do
