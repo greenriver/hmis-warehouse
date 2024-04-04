615440331b2d9e65645b0868fa5f799726addd72
Gig Ashton <gig@greenriver.org>
2024-04-04 12:06:06 -0400

    Dont save client when uploading image

--- drivers/hmis/app/graphql/mutations/update_client_image.rb
+++ drivers/hmis/app/graphql/mutations/update_client_image.rb
@@ -8,20 +8,17 @@ module Mutations
   class UpdateClientImage < BaseMutation
     argument :client_id, ID, required: true
     argument :image_blob_id, ID, required: true
-    argument :lock_version, Integer, required: false
 
     field :client, Types::HmisSchema::Client, null: true
 
-    def resolve(client_id:, image_blob_id:, lock_version: nil)
+    def resolve(client_id:, image_blob_id:)
       client = Hmis::Hud::Client.visible_to(current_user).find_by(id: client_id)
 
       raise HmisErrors::ApiError, 'Record not found' unless client.present?
       raise HmisErrors::ApiError, 'Access denied' unless current_user.permissions_for?(client, :can_edit_clients)
 
       client.image_blob_id = image_blob_id
-      client.lock_version = lock_version if lock_version
-      client.save!
-      client = client.reload
+      client.save_image_blob_as_client_headshot!
 
       {
         client: client,
