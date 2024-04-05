59b58c583386219d6165c216123772141ad8d148
Gig <gig@greenriver.org>
2024-04-05 12:08:01 -0400

    Display contact info from HMIS (#4232)
    * Display contact info from HMIS
    
    * Preload

--- drivers/hmis/extensions/grda_warehouse/hud/client_extension.rb
+++ drivers/hmis/extensions/grda_warehouse/hud/client_extension.rb
@@ -10,11 +10,34 @@ module Hmis::GrdaWarehouse::Hud
 
     included do
       has_many :custom_client_addresses, **Hmis::Hud::Base.hmis_relation(:PersonalID, 'CustomClientAddress'), inverse_of: :client
+      has_many :custom_client_contact_points, **Hmis::Hud::Base.hmis_relation(:PersonalID, 'CustomClientContactPoint'), inverse_of: :client
       has_many :hmis_custom_assessments, through: :enrollments
       has_many :hmis_source_custom_assessments, through: :source_enrollments, source: :hmis_custom_assessments
 
       def as_hmis
         Hmis::Hud::Client.find(id)
+      end
+
+      # Contact information used by the Warehouse Client model
+      # to populate the Client Dashboard and CAS
+      def most_recent_home_phone_hmis
+        custom_client_contact_points.home_phones.with_value.max_by(&:DateUpdated)&.value
+      end
+
+      def most_recent_cell_or_other_phone_hmis
+        # prefer most recent number that is explicitly marked as mobile, otherwise latest number for other/unknown
+        [
+          custom_client_contact_points.mobile_phones.with_value.max_by(&:DateUpdated)&.value,
+          custom_client_contact_points.other_or_unknown_phones.with_value.max_by(&:DateUpdated)&.value,
+        ].compact.first
+      end
+
+      def most_recent_work_or_school_phone_hmis
+        custom_client_contact_points.work_or_school_phones.max_by(&:DateUpdated)&.value
+      end
+
+      def most_recent_email_hmis
+        custom_client_contact_points.emails.with_value.max_by(&:DateUpdated)&.value
       end
     end
   end
