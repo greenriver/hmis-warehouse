41ae1f3bcf10845b3a578a17ab8fb0bec06e2044
Gig <gig@greenriver.org>
2024-04-05 09:27:57 -0400

    Dont update client record when uploading image (#4226)
    * Accept lock version on image update
    
    * Dont save client when uploading image

--- drivers/hmis/app/models/hmis/hud/client.rb
+++ drivers/hmis/app/models/hmis/hud/client.rb
@@ -88,20 +88,24 @@ class Hmis::Hud::Client < Hmis::Hud::Base
   after_create :warehouse_identify_duplicate_clients
   after_update :warehouse_match_existing_clients
   before_save :set_source_hash
-  after_save do
+  after_save :save_image_blob_as_client_headshot!
+
+  # The client creation form sets image_blob_id
+  # This is also called directly by UpdateClientImage operation
+  def save_image_blob_as_client_headshot!
     current_image_blob = ActiveStorage::Blob.find_by(id: image_blob_id)
     self.image_blob_id = nil
-    if current_image_blob
-      file = GrdaWarehouse::ClientFile.new(
-        client_id: id,
-        user_id: user.id,
-        name: 'Client Headshot',
-        visible_in_window: false,
-      )
-      file.tag_list.add('Client Headshot')
-      file.client_file.attach(current_image_blob)
-      file.save!
-    end
+    return unless current_image_blob
+
+    file = GrdaWarehouse::ClientFile.new(
+      client_id: id,
+      user_id: user.id,
+      name: 'Client Headshot',
+      visible_in_window: false,
+    )
+    file.tag_list.add('Client Headshot')
+    file.client_file.attach(current_image_blob)
+    file.save!
   end
 
   # Includes clients where..
