257a68a833d9bc6c249895229ed4f7a5c028fca2
Theron Toomey <ttoomey@greenriver.org>
2024-04-07 15:26:32 +0000

    rails-7-upgrade
    fix content type in factory

--- spec/factories/grda_warehouse/client_files.rb
+++ spec/factories/grda_warehouse/client_files.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :client_file, class: 'GrdaWarehouse::ClientFile' do
     association :client, factory: :grda_warehouse_hud_client
     association :user, factory: :user
-    client_file { Rack::Test::UploadedFile.new('spec/fixtures/files/images/test_photo.jpg', 'image/jpg') }
+    client_file { Rack::Test::UploadedFile.new('spec/fixtures/files/images/test_photo.jpg', 'image/jpeg') }
     name { 'Test File' }
     visible_in_window { true }
   end
@@ -10,7 +10,7 @@ FactoryBot.define do
   factory :client_file_coc_roi, class: 'GrdaWarehouse::ClientFile' do
     association :client, factory: :grda_warehouse_hud_client
     association :user, factory: :user
-    client_file { Rack::Test::UploadedFile.new('spec/fixtures/files/images/test_photo.jpg', 'image/jpg') }
+    client_file { Rack::Test::UploadedFile.new('spec/fixtures/files/images/test_photo.jpg', 'image/jpeg') }
     name { 'CoC Roi' }
     visible_in_window { true }
   end
