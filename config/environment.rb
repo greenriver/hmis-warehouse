76babb98db719205f616572571bc5f5cc2e47066
Theron Toomey <ttoomey@greenriver.org>
2024-04-08 15:21:55 +0000

    rails-7-upgrade
    restore rescue/notify when app fails to boot

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require_relative 'application'
 
-# FIXME, error handling
-Rails.application.initialize!
+begin
+  Rails.application.initialize!
+rescue StandardError, ActiveRecord::ConnectionNotEstablished => e
+  # Rescue when we don't yet have a DB, maybe related to LSA, or just delay, but this
+  # only occurs on deploy and console boot on deployed instances, and appears to have
+  # no negative impacts
+  Sentry.capture_exception(e)
+  Rails.logger.fatal("Application failed to start: #{e.message}")
+end
