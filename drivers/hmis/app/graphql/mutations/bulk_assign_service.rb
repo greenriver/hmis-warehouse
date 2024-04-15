8fd9c6cd09c4f4c273500f54f241a415332762be
Gig Ashton <gig@greenriver.org>
2024-04-15 09:23:04 -0400

    Bed Nights dont error if entry date has a warning

--- drivers/hmis/app/graphql/mutations/bulk_assign_service.rb
+++ drivers/hmis/app/graphql/mutations/bulk_assign_service.rb
@@ -54,6 +54,7 @@ module Mutations
             raise 'bulk service assignment generated invalid enrollment' unless enrollment.valid?
 
             entry_date_errors = Hmis::Hud::Validators::EnrollmentValidator.validate_entry_date(enrollment)
+            entry_date_errors.reject!(&:warning?)
             error_out(entry_date_errors.first.full_message) unless entry_date_errors.empty?
 
             # Attempt to assign this enrollment to a unit if this project has units. This is AC-specific for now, and does
