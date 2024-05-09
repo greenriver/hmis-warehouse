c7d34804299b07371cd87755a78af6562fb7a5eb
Gig Ashton <gig@greenriver.org>
2024-05-09 16:07:07 -0400

    Update definition_validation_spec

--- drivers/hmis/spec/models/hmis/form/definition_validation_spec.rb
+++ drivers/hmis/spec/models/hmis/form/definition_validation_spec.rb
@@ -8,122 +8,83 @@ require 'rails_helper'
 require_relative '../../../support/hmis_base_setup'
 
 RSpec.describe Hmis::Form::Definition, type: :model do
-  before(:all) do
-    cleanup_test_environment
-  end
-  after(:all) do
-    cleanup_test_environment
-  end
-
-  include_context 'hmis base setup'
-
-  HUD_ASSESSMENT_ROLES = [:INTAKE, :UPDATE, :ANNUAL, :EXIT].freeze
-
-  let!(:e2) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c1, user: u1, entry_date: 2.weeks.ago }
-  let!(:exit2) { create :hmis_hud_exit, enrollment: e2, data_source: ds1, client: c1, user: u1, exit_date: 1.week.ago }
-
-  let(:default_args) do
-    {
-      enrollment: e2,
-      ignore_warnings: true,
-    }
-  end
-
-  describe 'Validating form values on HUD assessments' do
-    # FIXME: Update this test to test against a mocked definition
-    let(:completed_values_for_update) do
+  describe 'Validating submitted form values against FormDefinition' do
+    let!(:definition) { create :hmis_form_definition }
+    let(:completed_values) do
       {
-        "information-date-input": '2023-02-15',
-        "3.16": 'SC-501',
-        "4.02.2": 'CLIENT_PREFERS_NOT_TO_ANSWER',
-        "4.03.2": 'YES',
-        "4.03.3": nil,
-        "4.03.4": nil,
-        "4.03.5": nil,
-        "4.03.6": nil,
-        "4.03.7": nil,
-        "4.03.8": true,
-        "4.03.A": 'other description', # required field when enabled
-        "4.04.2": 'NO',
-        "4.05.2": 'NO',
-        "4.06.2": 'NO',
-        "4.07.2": 'NO',
-        "4.08.2": 'NO',
-        "4.09.2": 'NO',
-        "4.10.2": 'NO',
-        "3.08": 'CLIENT_PREFERS_NOT_TO_ANSWER',
-        "4.11.2": 'NO',
+        linkid_date: '2023-02-15',
+        linkid_required: 2,
+        linkid_choice: 'foo',
       }
     end
-    # It doesn't matter what the raw values are set to, but all the keys need to be present
-    let(:empty_values_for_update) { completed_values_for_update.keys.map { |k| [k, nil] }.to_h }
 
-    it 'should have no errors when Update assessment is completely filled in' do
-      definition = Hmis::Form::Definition.find_by(role: :UPDATE)
-      errors = definition.validate_form_values(completed_values_for_update.stringify_keys)
+    it 'should pass when all fields are filled in' do
+      errors = definition.validate_form_values(completed_values.stringify_keys)
+
       expect(errors).to be_empty
     end
 
-    it 'should return warnings for warn_if_empty items' do
-      definition = Hmis::Form::Definition.find_by(role: :UPDATE)
-      values = {
-        **completed_values_for_update,
-        "4.11.2": 'YES',
-        "4.11.A": nil,
-        "4.11.B": nil,
-      }
-      expected_errors = [
-        {
-          type: :data_not_collected,
-          severity: :warning,
-          readable_attribute: 'When DV Occurred',
-          link_id: '4.11.A',
-        },
-        {
-          type: :data_not_collected,
-          severity: :warning,
-          readable_attribute: 'Currently Fleeing DV',
-          link_id: '4.11.B',
-        },
-      ]
+    context 'required item behavior' do
+      [
+        # [<value>, <should error if required>]
+        [nil, true],
+        ['', true],
+        [[], true],
+        ['DATA_NOT_COLLECTED', false], # DNC is a valid answer for a required field
+        [0, false],
+        ['value', false],
+        [Hmis::Hud::Processors::Base::HIDDEN_FIELD_VALUE, false], # hidden field should not generate an error
+      ].each do |value, should_error|
+        it "should #{should_error ? '' : 'not'} error on #{value.nil? ? 'nil' : value}" do
+          errors = definition.validate_form_values({
+            **completed_values,
+            linkid_required: value,
+          }.stringify_keys)
 
-      errors = definition.validate_form_values(values.stringify_keys)
-      expect(errors.map(&:to_h)).to match(expected_errors.map { |h| a_hash_including(**h) })
+          if should_error
+            expected_error = { type: :required, severity: :error, link_id: 'linkid_required', readable_attribute: 'The Required Field' }
+            expect(errors.map(&:to_h)).to contain_exactly(a_hash_including(expected_error))
+          else
+            expect(errors).to be_empty, errors.map(&:to_h)
+          end
+        end
+      end
     end
 
-    it 'should warn if conditional warn_if_empty field is enabled' do
-      definition = Hmis::Form::Definition.find_by(role: :UPDATE)
-      values = {
-        **completed_values_for_update,
-        "4.03.A": nil,
-      }
-      expected_errors = [
-        {
-          type: :data_not_collected,
-          severity: :warning,
-          readable_attribute: 'Other benefits source',
-        },
-      ]
+    context 'warn_if_empty item behavior' do
+      [
+        # [<value>, <should warn on  WARN_IF_EMPTY>]
+        [nil, true],
+        ['', true],
+        [[], true],
+        ['DATA_NOT_COLLECTED', true],
+        [0, false],
+        ['value', false],
+        [Hmis::Hud::Processors::Base::HIDDEN_FIELD_VALUE, false], # hidden field should not generate a warning
+      ].each do |value, should_warn|
+        it "should #{should_warn ? '' : 'not'} warn on #{value.nil? ? 'nil' : value}" do
+          errors = definition.validate_form_values({
+            **completed_values,
+            linkid_choice: value,
+          }.stringify_keys)
 
-      errors = definition.validate_form_values(values.stringify_keys)
-      expect(errors.map(&:to_h)).to match(expected_errors.map { |h| a_hash_including(**h) })
+          if should_warn
+            expected_error = { type: :data_not_collected, severity: :warning, link_id: 'linkid_choice', readable_attribute: 'Choice field' }
+            expect(errors.map(&:to_h)).to contain_exactly(a_hash_including(expected_error))
+          else
+            expect(errors).to be_empty, errors.map(&:to_h)
+          end
+        end
+      end
     end
-  end
 
-  describe 'Validating mock form validation' do
-    # Test using factory because actual forms don't have any required fields aside from assessment date
-    let!(:factory_form_definition) { create :hmis_form_definition }
-    it 'should error if required field is nil' do
-      values = { 'linkid_required': nil }
-      expected_errors = [
-        {
-          type: :required,
-          severity: :error,
-          readable_attribute: 'The Required Field',
-        },
-      ]
-      errors = factory_form_definition.validate_form_values(values.stringify_keys)
-      expect(errors.map(&:to_h)).to match(expected_errors.map { |h| a_hash_including(**h) })
+    it 'should raise on unrecognized link_id' do
+      expect do
+        definition.validate_form_values({
+          **completed_values,
+          some_fake_link_id: 'foo',
+        }.stringify_keys)
+      end.to raise_error(RuntimeError, /Unrecognized link ID: some_fake_link_id/)
     end
   end
 end
