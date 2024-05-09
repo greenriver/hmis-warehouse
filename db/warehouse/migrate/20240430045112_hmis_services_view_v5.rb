abe3bd2c3fcef1a9a721421a9bb7f8ea9a2094f9
Theron Toomey <ttoomey@greenriver.org>
2024-05-09 01:28:33 -0400

    5961 hmis services hud type idx (#4318)
    More minimal approach to improving hmis service performance
    * add index to hud type cols on services
    * remove custom service type join from hmis_services_view
    * remove custom service association from hmis services and adjust mutations around service creation
    * run analyze on relevant hud tables

---
+++ db/warehouse/migrate/20240430045112_hmis_services_view_v5.rb
@@ -0,0 +1,9 @@
+class HmisServicesViewV5 < ActiveRecord::Migration[6.1]
+  def up
+    update_view :hmis_services, version: 5
+  end
+
+  def down
+    update_view :hmis_services, version: 4
+  end
+end
