9b585fc2c62c0096516b26a0bd8538bd5168f0d2
Elliot <elliot@greenriver.org>
2024-05-11 11:48:36 -0400

    Update db/warehouse/migrate/20240510230733_fix_yaml_encoded_recurring_exports.rb
    Co-authored-by: Theron Toomey <ttoomey@greenriver.org>

--- db/warehouse/migrate/20240510230733_fix_yaml_encoded_recurring_exports.rb
+++ db/warehouse/migrate/20240510230733_fix_yaml_encoded_recurring_exports.rb
@@ -9,7 +9,7 @@ class FixYamlEncodedRecurringExports < ActiveRecord::Migration[7.0]
         'project_group_ids',
       ].each do |field|
         options[field] = YAML.load(options[field].first).reject { |m| m.blank? } if options[field]&.first&.include?('---')
-        exp.update(options: options)
+        exp.update!(options: options)
       end
     end
   end
