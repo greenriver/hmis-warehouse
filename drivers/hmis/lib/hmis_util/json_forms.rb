67d88535dc4e21bedb42882f9a54fffe3127627f
Gig Ashton <gig@greenriver.org>
2024-05-13 10:03:16 -0400

    Address feedback

--- drivers/hmis/lib/hmis_util/json_forms.rb
+++ drivers/hmis/lib/hmis_util/json_forms.rb
@@ -6,6 +6,9 @@
 
 module HmisUtil
   class JsonForms
+    JsonFormException = Class.new(StandardError)
+    private_constant :JsonFormException
+
     DATA_DIR = 'drivers/hmis/lib/form_data'.freeze
 
     def self.seed_all
@@ -242,10 +245,10 @@ module HmisUtil
       # Validate final definition
       begin
         validate_definition(form_definition, role)
-      rescue StandardError => e
+      rescue JsonFormException => e
         # If there was an error, _try_ to print out the exact value that failed by traversing the json path
         match_path = /property '(.*)'/.match(e.to_s)
-        if match_path.size == 2
+        if match_path&.size == 2
           dig_path = match_path[1].split('/').map(&:presence).compact.map { |s| Integer(s, exception: false) || s }
           problem_item = form_definition.dig(*dig_path)
         end
@@ -425,7 +428,7 @@ module HmisUtil
 
     # on_error allows customization of error handling incase we want to collect them instead of raising
     public def validate_definition(json, role = nil)
-      on_error = ->(err) { block_given? ? yield(err) : raise(err) }
+      on_error = ->(err) { block_given? ? yield(err) : raise(JsonFormException, err) }
       Hmis::Form::Definition.validate_json(json, valid_pick_lists: valid_pick_lists, &on_error)
       schema_errors = Hmis::Form::Definition.validate_schema(json)
       return unless schema_errors.present?
