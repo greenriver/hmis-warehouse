bf88c442f8d3cea93d292c489583042cb55c9b01
Elliot Anders <elliot@greenriver.org>
2024-06-05 19:58:19 -0400

    Don't use concurrency on index creation

--- db/warehouse/migrate/20240605155445_add_form_processor_owner_index.rb
+++ db/warehouse/migrate/20240605155445_add_form_processor_owner_index.rb
@@ -1,7 +1,7 @@
 class AddFormProcessorOwnerIndex < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction! # needed for index creation
+  # disable_ddl_transaction! # needed for index creation
 
   def change
-    add_index :hmis_form_processors, [:owner_id, :owner_type], algorithm: :concurrently, unique: true, name: 'one_form_processor_per_owner'
+    add_index :hmis_form_processors, [:owner_id, :owner_type], unique: true, name: 'one_form_processor_per_owner'
   end
 end
