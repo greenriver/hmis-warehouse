7be1c3b3e7a5566ec7571edb76f2d4c7365121ec
Elliot Anders <elliot@greenriver.org>
2024-06-05 20:59:58 -0400

    Security update; Fix for indexing;

--- db/warehouse/migrate/20240605155445_add_form_processor_owner_index.rb
+++ db/warehouse/migrate/20240605155445_add_form_processor_owner_index.rb
@@ -2,6 +2,8 @@ class AddFormProcessorOwnerIndex < ActiveRecord::Migration[7.0]
   # disable_ddl_transaction! # needed for index creation
 
   def change
-    add_index :hmis_form_processors, [:owner_id, :owner_type], unique: true, name: 'one_form_processor_per_owner'
+    safety_assured do
+      add_index :hmis_form_processors, [:owner_id, :owner_type], unique: true, name: 'one_form_processor_per_owner'
+    end
   end
 end
