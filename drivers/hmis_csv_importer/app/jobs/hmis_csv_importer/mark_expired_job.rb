21917a2085240295a9a723d4b67c75d760b3e141
Theron Toomey <ttoomey@greenriver.org>
2024-06-06 01:45:57 +0000

    6000-prune-csv-staging-and-source-tables

--- drivers/hmis_csv_importer/app/jobs/hmis_csv_importer/mark_expired_job.rb
+++ drivers/hmis_csv_importer/app/jobs/hmis_csv_importer/mark_expired_job.rb
@@ -1,46 +1,65 @@
 module HmisCsvImporter
   class MarkExpiredJob < ApplicationJob
-    def perform(now: DateTime.current, recent_imports_to_keep: 10, keep_deleted_records_for: 2.weeks)
+    #
+    # @param data_source_id [Integer, nil] the ID of the data source
+    # @param now [DateTime] the time for the purposes of relative time calculation
+    # @param retained_imports [Integer] the number of retained import records
+    # @param retain_deleted_records_for [Duration] the retention period for deleted records
+    def perform(data_source_id: nil, now: DateTime.current, retained_imports: 10, retain_deleted_records_for: 2.weeks)
+      @data_source_id = data_source_id
       @now = now.to_datetime
-      @expire_deleted_records_before = now - keep_deleted_records_for
-      @recent_imports_to_keep = recent_imports_to_keep
+      @expire_deleted_records_before = now - retain_deleted_records_for
+      @retained_imports = retained_imports * files_per_import
 
-      total = 0
       models.each do |model|
-        # FIXME, should include select clause to improve performance
-        candidates_for_expiration = model.
-          where.not(loader_id: recent_import_ids).
-          preload(:destination_record_with_deleted)
-
-        puts "#{model.name} total: #{candidates_for_expiration.size}"
-        candidates_for_expiration.in_batches(of: 1_000) do |batch|
-          expired_ids = []
-
-          batch.each do |record|
-            destination = record.destination_record_with_deleted
-            expired_ids.push(record.id) if expired?(record, destination)
-          end
-          batch.where.not(id: expired_ids).update_all(expired: false)
-          model.where(id: expired_ids).update_all(expired: true)
-          total += expired_ids.size
-        end
-        puts "#{model.name} expired: #{candidates_for_expiration.where(expired: true).size}"
+        process_modal(model)
       end
-
-      total
     end
 
     protected
 
+    def process_model(model)
+      candidates_for_expiration = model.
+        where.not(loader_id: recent_import_ids, data_source_id: @data_source_id).
+        preload(:destination_record_with_deleted).
+        select(:id, model.hud_key, :data_source_id, :DateDeleted, :loader_id)
+
+      puts "#{model.name} total: #{candidates_for_expiration.size}"
+      candidates_for_expiration.in_batches(of: 1_000) do |batch|
+        expired_ids = []
+
+        batch.each do |record|
+          destination = record.destination_record_with_deleted
+          expired_ids.push(record.id) if expired?(record, destination)
+        end
+        batch.where.not(id: expired_ids).update_all(expired: false)
+        model.where(id: expired_ids).update_all(expired: true)
+      end
+      puts "#{model.name} expired: #{candidates_for_expiration.where(expired: true).size}"
+    end
+
+    def files_per_import
+      GrdaWarehouse::HmisImportConfig.active.
+        where(data_source_id: @data_source_id).
+        maximum(:file_count)
+    end
+
     def recent_import_ids
-      @recent_import_ids ||= HmisCsvImporter::Loader::LoaderLog.order(created_at: :desc).limit(@recent_imports_to_keep).pluck(:id)
+      @recent_import_ids = HmisCsvImporter::Loader::LoaderLog.
+        where(data_source_id: @data_source_id).
+        order(created_at: :desc).
+        limit(@retained_imports).pluck(:id)
     end
 
     def expired?(record, destination)
-      date_deleted = destination&.DateDeleted || record.DateDeleted
-      return true if date_deleted && date_deleted < @expire_deleted_records_before
+      # The record is no-longer attached to a destination and not included in the recent loaders (via batch selection)
+      return true if destination.nil?
 
-      false
+      date_deleted = destination.DateDeleted || record.DateDeleted
+      return false if date_deleted.nil?
+
+      # The destination record (or this record) has been deleted for longer than the retention period
+      date_deleted < @expire_deleted_records_before
     end
 
     def models
@@ -49,7 +68,10 @@ module HmisCsvImporter
         HmisCsvImporter::Loader::Loader.loadable_files,
         #  #HmisCsvImporter::Importer::Importer.importable_files,
       ].flat_map(&:values)
-      all.filter { |m| m.reflect_on_association(:destination_record_with_deleted) }
+      all.filter do |model|
+        # keep Export records indefinitely. There's only ever one row of metadata.
+        model.name.demodulize != 'Export' && model.reflect_on_association(:destination_record_with_deleted)
+      end
     end
   end
 end
