bcf42caf312baab5be6491339d8d7339cc6854ec
Elliot Anders <elliot@greenriver.org>
2024-06-10 08:37:46 -0400

    Monkey patch to remove image/bmp from invalid types since it's valid

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,3 +1,10 @@
 silence_warnings do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 end
+Rails.application.reloader.to_prepare do
+  # ActiveStorage::Blob::INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 = ActiveStorage::Blob::INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 - ['image/bmp']
+  class ActiveStorage::Blob
+    types = remove_const(:INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7) - ['image/bmp']
+    INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 = types
+  end
+end
