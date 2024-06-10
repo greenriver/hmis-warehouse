8935b0e7eb771039d85bf21cdd4dc2820c9dfdff
Elliot Anders <elliot@greenriver.org>
2024-06-10 08:39:36 -0400

    Comment why

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -2,7 +2,7 @@ silence_warnings do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 end
 Rails.application.reloader.to_prepare do
-  # ActiveStorage::Blob::INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 = ActiveStorage::Blob::INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 - ['image/bmp']
+  # Remove image/bmp from the list of invalid content types, it is valid
   class ActiveStorage::Blob
     types = remove_const(:INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7) - ['image/bmp']
     INVALID_VARIABLE_CONTENT_TYPES_DEPRECATED_IN_RAILS_7 = types
