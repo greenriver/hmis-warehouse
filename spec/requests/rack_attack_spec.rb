bc67d44f45d62a759804024349630d8f5549ac75
Theron Toomey <ttoomey@greenriver.org>
2024-07-05 14:31:08 +0000

    6239-insufficient-rate-limiting

--- spec/requests/rack_attack_spec.rb
+++ spec/requests/rack_attack_spec.rb
@@ -7,25 +7,19 @@ describe Rack::Attack, type: :request do
 
   # request a path repeatedly and return
   # the request number when it returns `throttled_status`
-  def till_throttled(method, path, options = {})
-    requests_to_send = options.delete(:requests_to_send)
-    throttled_status = options.delete(:throttled_status) || 429
-    options[:params] ||= {}
-    # inject randomness to make sure we aren't matching on params
+  def till_throttled(requests_to_send:, throttled_status: 429, &block)
     requests_sent = 0
     status_encountered = false
-    requests_to_send.times do |_|
-      options[:params][:randomness] = SecureRandom.hex
-      send(method, path, params: options[:params])
+    requests_to_send.times do |cnt|
+      block.arity == 0 ? yield : yield(cnt)
       requests_sent += 1
-      # puts "#{path} #{options} #{requests_sent}/#{requests_to_send} #{response.status}"
       status_encountered = response.status == throttled_status
       break if status_encountered
     end
     status_encountered ? requests_sent : nil
   end
 
-  before do
+  before(:each) do
     Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
     Timecop.scale(1 / 50.to_f)
   end
@@ -34,15 +28,10 @@ describe Rack::Attack, type: :request do
     describe 'when hitting the homepage' do
       let(:throttled_at) { 11 }
       let(:request_limit) { (throttled_at * 4).to_i }
-      let(:path) { '/' }
+      let(:path) { root_path(rack_attack_enabled: true) }
 
       it 'throttle excessive requests by IP address - enabled' do
-        requests_sent = till_throttled(
-          :get,
-          path,
-          requests_to_send: request_limit,
-          params: { rack_attack_enabled: true },
-        )
+        requests_sent = till_throttled(requests_to_send: request_limit) { get(path) }
         expect(requests_sent).to be_between(throttled_at, throttled_at + 1)
       end
     end
@@ -50,18 +39,28 @@ describe Rack::Attack, type: :request do
     describe 'and posting to the sign-in page' do
       let(:throttled_at) { 11 }
       let(:request_limit) { (throttled_at * 4).to_i }
-      let(:path) { '/users/sign_in' }
+      let(:path) { user_session_path(rack_attack_enabled: true) }
 
       it 'throttle excessive requests by user - enabled' do
-        requests_sent = till_throttled(
-          :post,
-          path,
-          requests_to_send: request_limit,
-          params: {
-            rack_attack_enabled: true,
-            user: { email: 'test@example.com', password: 'password' },
-          },
-        )
+        requests_sent = till_throttled(requests_to_send: request_limit) do
+          post(path, params: {user: { email: "test@example.com", password: 'incorrect' }})
+        end
+        expect(requests_sent).to be_between(throttled_at, throttled_at + 1)
+      end
+    end
+
+    describe 'and posting to the hmis sign-in end point' do
+      let(:throttled_at) { 11 }
+      let(:request_limit) { (throttled_at * 4).to_i }
+      let(:path) { '/hmis/login' }
+
+      it 'throttle excessive requests by user - enabled' do
+        req_count = 0
+        requests_sent = till_throttled(requests_to_send: request_limit) do
+          post hmis_user_session_path(hmis_user: { email: user.email, password: user.password })
+          payload = { user: { email: 'test@example.com', password: 'password' } }
+          post hmis_user_session_path, params: payload.to_json, headers: { "CONTENT_TYPE" => "application/json" }
+        end
         expect(requests_sent).to be_between(throttled_at, throttled_at + 1)
       end
     end
