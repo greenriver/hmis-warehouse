446233427556fa14f321970b2249895e6c7d70d3
martha <medwards@greenriver.org>
2024-07-09 17:12:10 -0400

    Fix bug on client assessment filtering (#4528)

--- drivers/hmis/spec/requests/hmis/assessments/filter_assessment_spec.rb
+++ drivers/hmis/spec/requests/hmis/assessments/filter_assessment_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
     hmis_login(user)
   end
 
-  let(:query) do
+  let(:project_assessments_query) do
     <<~GRAPHQL
       query GetProjectAssessments(
         $id: ID!
@@ -51,15 +51,38 @@ RSpec.describe Hmis::GraphqlController, type: :request do
     GRAPHQL
   end
 
+  let(:client_assessments_query) do
+    <<~GRAPHQL
+      query GetClientAssessments(
+        $id: ID!
+        $filters: AssessmentFilterOptions = null
+      ) {
+        client(id: $id) {
+          assessments(
+            filters: $filters
+          ) {
+            nodes {
+              id
+              role
+              definition {
+                title
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+  end
+
   it 'should return all assessments when no filter is passed' do
-    response, result = post_graphql(id: p1.id, filters: {}) { query }
+    response, result = post_graphql(id: p1.id, filters: {}) { project_assessments_query }
     expect(response.status).to eq 200
     assessments = result.dig('data', 'project', 'assessments', 'nodes')
     expect(assessments.count).to eq 3
   end
 
   it 'should return only intake when intake filter is passed' do
-    response, result = post_graphql(id: p1.id, filters: { assessment_name: 'INTAKE' }) { query }
+    response, result = post_graphql(id: p1.id, filters: { assessment_name: 'INTAKE' }) { project_assessments_query }
     expect(response.status).to eq 200
     assessments = result.dig('data', 'project', 'assessments', 'nodes')
     expect(assessments.count).to eq 1
@@ -67,7 +90,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
   end
 
   it 'should return both intake and fully custom assessment when both filters are passed' do
-    response, result = post_graphql(id: p1.id, filters: { assessment_name: ['INTAKE', 'fully-custom-assessment-identifier'] }) { query }
+    response, result = post_graphql(id: p1.id, filters: { assessment_name: ['INTAKE', 'fully-custom-assessment-identifier'] }) { project_assessments_query }
     expect(response.status).to eq 200
     assessments = result.dig('data', 'project', 'assessments', 'nodes')
     expect(assessments.count).to eq 2
@@ -77,10 +100,17 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
   it 'should return intake even if it is not tied to a definition' do
     intake_assessment.form_processor.update!(definition: nil)
-    response, result = post_graphql(id: p1.id, filters: { assessment_name: ['INTAKE'] }) { query }
+    response, result = post_graphql(id: p1.id, filters: { assessment_name: ['INTAKE'] }) { project_assessments_query }
     expect(response.status).to eq 200
     assessments = result.dig('data', 'project', 'assessments', 'nodes')
     expect(assessments.count).to eq 1
     expect(assessments.first['id']).to eq(intake_assessment.id.to_s)
+  end
+
+  it 'should return all assessments for project when project filter is passed' do
+    response, result = post_graphql(id: c1.id, filters: { project: [p1.id] }) { client_assessments_query }
+    expect(response.status).to eq 200
+    assessments = result.dig('data', 'client', 'assessments', 'nodes')
+    expect(assessments.count).to eq 3
   end
 end
