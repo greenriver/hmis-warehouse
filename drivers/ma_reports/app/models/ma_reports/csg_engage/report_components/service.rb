115ecc2c7499c538ce74432f6effde8d214f4c18
zachcmp <zparks88@gmail.com>
2024-07-10 14:05:52 -0400

    Fixup CSG service nodes

--- drivers/ma_reports/app/models/ma_reports/csg_engage/report_components/service.rb
+++ drivers/ma_reports/app/models/ma_reports/csg_engage/report_components/service.rb
@@ -12,32 +12,34 @@ module MaReports::CsgEngage::ReportComponents
       @service = service
     end
 
-    field('CaseWorker')
-    field('CheckAmount')
-    field('CheckDate')
-    field('CheckNumber')
-    field('DollarsExpended')
-    field('NoEndDate')
-    field('Note')
-    field('PayAmount')
-    field('PayDate')
-    field('PayFiscalYear')
-    field('PayFundingSource')
-    field('PayHasPayment')
-    field('PayNote1')
-    field('PayNote2')
-    field('PayPayeeAddress1')
-    field('PayPayeeAddress2')
-    field('PayPayeeAddress3')
-    field('PayPayeeName')
-    field('ServiceDateTimeBegin') { service.DateProvided&.strftime('%m/%d/%Y') }
-    field('ServiceDateTimeEnd') { nil }
-    field('ServiceProvided') do
-      HudUtility2024.service_type_provided(service.Recordtype, service.TypeProvided) || 'Unknown Service Type'
-    rescue StandardError
-      'Unknown Service Type'
+    subfield('Service') do
+      field('CaseWorker')
+      field('CheckAmount')
+      field('CheckDate')
+      field('CheckNumber')
+      field('DollarsExpended')
+      field('NoEndDate')
+      field('Note')
+      field('PayAmount')
+      field('PayDate')
+      field('PayFiscalYear')
+      field('PayFundingSource')
+      field('PayHasPayment')
+      field('PayNote1')
+      field('PayNote2')
+      field('PayPayeeAddress1')
+      field('PayPayeeAddress2')
+      field('PayPayeeAddress3')
+      field('PayPayeeName')
+      field('ServiceDateTimeBegin') { service.DateProvided&.strftime('%m/%d/%Y') }
+      field('ServiceDateTimeEnd') { nil }
+      field('ServiceProvided') do
+        HudUtility2024.service_type_provided(service.Recordtype, service.TypeProvided) || 'Unknown Service Type'
+      rescue StandardError
+        'Unknown Service Type'
+      end
+      field('ServiceReviewDate')
+      field('UnitsOfService')
     end
-    field('ServiceReviewDate')
-    field('UnitsOfService')
   end
 end
