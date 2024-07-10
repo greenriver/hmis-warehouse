348ac054b611ef8febff0d17e42ec1d79e3bf978
Elliot Anders <elliot@greenriver.org>
2024-07-10 11:00:22 -0400

    Default to 30M

--- drivers/hmis_csv_importer/app/jobs/hmis_csv_importer/cleanup/expire_base_job.rb
+++ drivers/hmis_csv_importer/app/jobs/hmis_csv_importer/cleanup/expire_base_job.rb
@@ -34,7 +34,7 @@ module HmisCsvImporter::Cleanup
     # @param retain_after_date [DateTime] the date after which records are retained
     # @param max_per_run [Integer] stop processing if we delete more records than this
     # @param dry_run [Boolean] do not run delete statements
-    def perform(model_name: nil, retain_item_count: 5, retain_after_date: DateTime.current - 2.weeks, max_per_run: 300_000_000, batch_size: 500_000, dry_run: true)
+    def perform(model_name: nil, retain_item_count: 5, retain_after_date: DateTime.current - 2.weeks, max_per_run: 30_000_000, batch_size: 500_000, dry_run: true)
       @retain_item_count = retain_item_count
       @retain_after_date = retain_after_date
       @dry_run = dry_run
