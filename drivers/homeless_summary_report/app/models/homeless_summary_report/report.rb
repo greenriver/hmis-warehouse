f6a87fb07506cf6d0b5bd07622a42b09e56d29d4
Sandy Wise <sandy@greenriver.org>
2024-07-15 16:42:04 -0400

    From review: Extract demographic filters for future reuse

--- drivers/homeless_summary_report/app/models/homeless_summary_report/report.rb
+++ drivers/homeless_summary_report/app/models/homeless_summary_report/report.rb
@@ -823,40 +823,9 @@ module HomelessSummaryReport
     end
 
     def self.demographic_variants
-      variants = {}.tap do |h|
-        HudUtility2024.race_ethnicity_combinations.keys.each do |combination|
-          h[combination] = {
-            name: HudUtility2024.race_ethnicity_combinations[combination],
-            extra_filters: {
-              race_ethnicity_combinations: [combination],
-            },
-            demographic_filters: [:filter_for_race_ethnicity_combinations],
-          }
-        end
-      end
-
+      variants = ::Filters::DemographicFilters.race_ethnicity_combination_filters
+      variants.merge!(::Filters::DemographicFilters.subpopulation_filters)
       variants.merge!(
-        fleeing_dv: {
-          name: 'Currently Fleeing DV',
-          extra_filters: {
-            currently_fleeing: [1],
-          },
-          demographic_filters: [:filter_for_dv_currently_fleeing],
-        },
-        veteran: {
-          name: 'Veterans',
-          extra_filters: {
-            veteran_statuses: [1],
-          },
-          demographic_filters: [:filter_for_veteran_status],
-        },
-        has_disability: {
-          name: 'With Indefinite and Impairing Disability',
-          extra_filters: {
-            indefinite_disabilities: [1],
-          },
-          demographic_filters: [:filter_for_indefinite_disabilities],
-        },
         has_rrh_move_in_date: {
           name: 'Moved in to RRH',
           extra_filters: {
