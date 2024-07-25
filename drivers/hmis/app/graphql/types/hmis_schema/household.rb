f3f334f738ef84e99437c602f96ce35532b865e3
martha <medwards@greenriver.org>
2024-07-25 14:15:40 -0400

    Use id rather than deleted_at as tiebreaker
    Co-authored-by: Theron Toomey <ttoomey@greenriver.org>

--- drivers/hmis/app/graphql/types/hmis_schema/household.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/household.rb
@@ -53,11 +53,11 @@ module Types
     def staff_assignments(is_currently_assigned: true)
       # There's no current use case for returning all (both currently assigned and formerly assigned)
       # in the same query, but we could update this to support that use case if it arises.
-      scope = load_ar_association(object, :staff_assignments).order(created_at: :desc)
+      scope = load_ar_association(object, :staff_assignments).order(created_at: :desc, id: desc)
       if is_currently_assigned
         scope
       else
-        scope.with_deleted.where.not(deleted_at: nil).order(created_at: :desc, deleted_at: :desc)
+        scope.with_deleted.where.not(deleted_at: nil)
       end
     end
   end
