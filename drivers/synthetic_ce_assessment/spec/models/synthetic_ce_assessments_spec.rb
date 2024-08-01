b65fd5449e8b760915921dc0d166c4b07c5bbea1
Elliot Anders <elliot@greenriver.org>
2024-07-31 20:00:30 -0400

    Fixup failing test

--- drivers/synthetic_ce_assessment/spec/models/synthetic_ce_assessments_spec.rb
+++ drivers/synthetic_ce_assessment/spec/models/synthetic_ce_assessments_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe 'Synthetic CE Assessment sync', type: :model do
   let!(:project_config) { create :synthetic_ce_assessment_project_config, project: project, assessment_type: 3, assessment_level: 1, prioritization_status: 2 }
 
   it 'creates synthetic assessments for the enrollment where the project is flagged' do
-    assessments =  SyntheticCeAssessment::EnrollmentCeAssessment.all
+    scope = SyntheticCeAssessment::EnrollmentCeAssessment.all
     expect do
       SyntheticCeAssessment::EnrollmentCeAssessment.sync
     end.to change(scope, :count).by(1)
-     expect(scope.first&.source).to eq(enrollment)
+    expect(scope.first&.source).to eq(enrollment)
   end
 
   it 'removes orphaned synthetic assessments' do
