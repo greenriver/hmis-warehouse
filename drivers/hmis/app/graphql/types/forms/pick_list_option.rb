68a6c94819e1f273d59c2af82c5afb835acdfb0d
martha <medwards@greenriver.org>
2024-08-02 10:28:41 -0400

    Restrict continuum projects picklist to HMIS data source

--- drivers/hmis/app/graphql/types/forms/pick_list_option.rb
+++ drivers/hmis/app/graphql/types/forms/pick_list_option.rb
@@ -146,7 +146,8 @@ module Types
 
         form_types.map { |ft| { code: ft[:value], label: ft[:desc] } }
       when 'CONTINUUM_PROJECTS'
-        Hmis::Hud::Project.where(continuum_project: true).
+        Hmis::Hud::Project.
+          where(data_source_id: user.hmis_data_source_id, continuum_project: true).
           preload(:organization).
           sort_by_option(:organization_and_name).
           map(&:to_pick_list_option)
