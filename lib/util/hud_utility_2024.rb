41c4e3debac13d0c45221d775e1b99bf6dadcfad
Dave G <dgreiner@greenriver.org>
2024-08-05 16:08:32 -0400

    move ssn regex to hud utility and expand it to allow for akphanumeric characters

--- lib/util/hud_utility_2024.rb
+++ lib/util/hud_utility_2024.rb
@@ -10,6 +10,8 @@ module HudUtility2024
 
   module_function
 
+  SSN_RGX = /(\w{3})[^\w]?(\w{2})[^\w]?(\w{4})/
+
   def races(multi_racial: false)
     return race_field_name_to_description unless multi_racial
 
