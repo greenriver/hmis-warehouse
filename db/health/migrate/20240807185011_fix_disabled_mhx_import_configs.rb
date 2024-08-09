22e6d8da418212d93ec3759c391e5c92c6d4a65b
Sandy Wise <sandy@greenriver.org>
2024-08-09 11:10:47 -0400

    Missed edit to FixDisabledMhxImportConfigs

--- db/health/migrate/20240807185011_fix_disabled_mhx_import_configs.rb
+++ db/health/migrate/20240807185011_fix_disabled_mhx_import_configs.rb
@@ -1,5 +1,5 @@
 class FixDisabledMhxImportConfigs < ActiveRecord::Migration[7.0]
-  def change
+  def up
     Health::ImportConfig.where(kind: 'disabled_medicaid_hmis_exchange').
       update_all(
         kind: 'medicaid_hmis_exchange',
