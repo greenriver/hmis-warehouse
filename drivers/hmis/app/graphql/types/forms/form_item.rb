edc0a3604bd3e0061506e0024e8272acf81e3a5d
martha <medwards@greenriver.org>
2024-08-31 15:14:48 -0400

    Add comment on form item

--- drivers/hmis/app/graphql/types/forms/form_item.rb
+++ drivers/hmis/app/graphql/types/forms/form_item.rb
@@ -41,8 +41,8 @@ module Types
     field :autofill_values, [Forms::AutofillValue], 'Value(s) to autofill based on conditional logic', null: true
     field :service_detail_type, Forms::Enums::ServiceDetailType, 'Whether to apply this field to all clients or a single client when bulk creating', null: true, deprecation_reason: 'from old bulk services implementation, no longer supported'
 
-    field :rule, GraphQL::Types::JSON, null: true
-    field :custom_rule, GraphQL::Types::JSON, null: true
+    field :rule, GraphQL::Types::JSON, 'Rules that apply to this item', null: true
+    field :custom_rule, GraphQL::Types::JSON, 'Custom rules that apply to this item', null: true
 
     # field mapping
     field :mapping, Types::Forms::FieldMapping, null: true
