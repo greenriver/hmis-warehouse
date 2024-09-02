61735d2e5792b8d1e016742188e51dcc3680511a
Theron Toomey <ttoomey@greenriver.org>
2024-09-02 15:04:37 +0000

    6429-pg-16

---
+++ docker/pg16/upgrade.sh
@@ -0,0 +1,37 @@
+#!/bin/env bash
+
+set -x
+set -oi pipefail
+
+# If you need to start over for some reason, destroy the db container and run:
+# docker volume rm hmis-warehouse_dbdata_pg16
+
+docker-compose stop db_previous db
+
+docker-compose up -d db_previous db
+
+sleep 3
+
+docker-compose logs db_previous db
+
+export FILENAME=db_previous.dumpall.sql
+
+# docker-compose exec db_previous pg_dumpall --help
+
+mkdir -p tmp/dumps
+
+echo Dumping old database
+docker-compose exec \
+  db_previous pg_dumpall \
+  --exclude-database=template\* \
+  --exclude-database=postgres \
+  > tmp/dumps/$FILENAME
+
+echo Importing to new database
+docker-compose exec \
+  db psql -c "\i /tmp/dumps/$FILENAME"
+
+docker-compose exec \
+  db psql -c "ALTER USER postgres WITH PASSWORD 'postgres'"
+
+docker-compose stop db_previous
