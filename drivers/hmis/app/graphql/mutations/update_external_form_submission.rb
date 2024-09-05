a9621b661414974afa3002db1fddce6f88c4bf7f
martha <medwards@greenriver.org>
2024-09-04 22:13:29 -0400

    PR feedback, still wip

--- drivers/hmis/app/graphql/mutations/update_external_form_submission.rb
+++ drivers/hmis/app/graphql/mutations/update_external_form_submission.rb
@@ -39,34 +39,14 @@ module Mutations
       end
 
       if record.status_changed? && record.status == 'reviewed' && !record.spam
-        definition = record.definition
+        project = Hmis::Hud::Project.viewable_by(current_user).find_by(id: project_id)
+        access_denied! unless project
 
-        # Only if there are Client and/or Enrollment fields in the form definition, initialize an enrollment
-        # (which will in turn initialize a Client, inside the form processor).
-        if definition.link_id_item_hash.values.find { |item| ['ENROLLMENT', 'CLIENT'].include?(item.mapping.record_type) }
-          project = Hmis::Hud::Project.viewable_by(current_user).find_by(id: project_id)
-
-          access_denied! unless project
+        if record.definition.creates_client_or_enrollment?
           access_denied! unless current_permission?(permission: :can_edit_enrollments, entity: project)
-
-          record.build_enrollment(project: project, data_source: project.data_source, entry_date: record.created_at)
-          # Assume that required values on Client and Enrollment (e.g. relationship to HoH) are present
-          # and correctly mapped in raw_values. The form processor record validator will fail otherwise.
         end
 
-        form_processor = record.form_processor || record.build_form_processor(definition: definition)
-
-        form_processor.hud_values = record.form_values
-        # We skip the form_processor.collect_form_validations step, because the external form has already been
-        # submitted. If it's invalid, there is nothing the user can do about it now.
-        # Also skip form_processor.collect_record_validations since e don't want to completely block from processing
-        # if the external submission results in invalid values. We should process and allow correction after-the-fact.
-
-        # Run to create CDEs, and client/enrollment if applicable
-        form_processor.run!(user: current_user)
-
-        errors.deduplicate!
-        return { errors: errors } if errors.any?
+        record.run_form_processor(project, current_user)
 
         if record.enrollment&.new_record?
           # Raise with user-facing error message so that we get notified (rather than returning errors in the response body)
@@ -76,8 +56,7 @@ module Mutations
           record.enrollment.client.save!
           record.enrollment.save_new_enrollment!
         end
-
-        form_processor.save!
+        record.form_processor.save!
       end
 
       record.save! # checked for validity above
--- drivers/hmis_external_apis/app/models/hmis_external_apis/external_forms/form_submission.rb
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/external_forms/form_submission.rb
@@ -10,6 +10,8 @@ module HmisExternalApis::ExternalForms
     belongs_to :definition, class_name: 'Hmis::Form::Definition'
     # Enrollment that was generated as a result of processing this form submission. Only applicable for certain external forms, like the PIT.
     belongs_to :enrollment, class_name: 'Hmis::Hud::Enrollment', optional: true
+    has_one :form_processor, class_name: 'Hmis::Form::FormProcessor', as: :owner
+
     include ::Hmis::Hud::Concerns::FormSubmittable
 
     # The recaptcha spam score is a float between 0 (likely spam) and 1.0 (likely real)
@@ -56,6 +58,27 @@ module HmisExternalApis::ExternalForms
 
     def form_values
       raw_data.reject { |key, _| EXTRANEOUS_KEYS.include?(key) }
+    end
+
+    def run_form_processor(project, current_user)
+      # Only if there are Client and/or Enrollment fields in the form definition, initialize an enrollment
+      # (which will in turn initialize a Client, inside the form processor).
+      if definition.creates_client_or_enrollment?
+        build_enrollment(project: project, data_source: project.data_source, entry_date: created_at)
+        # Assume that required values on Client and Enrollment (e.g. relationship to HoH) are present
+        # and correctly mapped in raw_values. The form processor record validator will fail otherwise.
+      end
+
+      form_processor = build_form_processor(definition: definition)
+
+      form_processor.hud_values = form_values
+      # We skip the form_processor.collect_form_validations step, because the external form has already been
+      # submitted. If it's invalid, there is nothing the user can do about it now.
+      # Also skip form_processor.collect_record_validations since e don't want to completely block from processing
+      # if the external submission results in invalid values. We should process and allow correction after-the-fact.
+
+      # Run to create CDEs, and client/enrollment if applicable
+      form_processor.run!(user: current_user)
     end
   end
 end
