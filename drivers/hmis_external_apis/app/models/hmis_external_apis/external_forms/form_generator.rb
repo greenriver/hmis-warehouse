a44ddbde911b5d2a5839688173ae69487b20f341
Gig Ashton <gig@greenriver.org>
2024-09-05 16:47:40 -0400

    Fix validation bug, support helper text

--- drivers/hmis_external_apis/app/models/hmis_external_apis/external_forms/form_generator.rb
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/external_forms/form_generator.rb
@@ -77,7 +77,7 @@ module HmisExternalApis::ExternalForms
         when 'EMAIL'
           render_form_input(label: node['text'], name: node_name(node), required: node['required'], input_type: 'email', input_invalid_feedback: 'Enter a valid email address')
         else
-          render_form_input(label: node['text'], name: node_name(node), required: node['required'], input_type: 'text')
+          render_form_input(label: node['text'], name: node_name(node), required: node['required'], input_type: 'text', input_helper: node['helper_text'])
         end
       end
     end
@@ -96,7 +96,7 @@ module HmisExternalApis::ExternalForms
 
     def render_text_node(node)
       render_form_group(node: node) do
-        render_form_textarea(label: node['text'], name: node_name(node), required: node['required'])
+        render_form_textarea(label: node['text'], name: node_name(node), required: node['required'], input_helper: node['helper_text'])
       end
     end
 
