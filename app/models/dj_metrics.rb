59ec5a4d38d06f397816ce71a9538f633d88e21a
Todd Blackman <tdblackman@gmail.com>
2024-09-16 13:10:08 -0400

    Debugging encoding problem on EKS

--- app/models/dj_metrics.rb
+++ app/models/dj_metrics.rb
@@ -65,11 +65,13 @@ class DjMetrics
     Delayed::Job.where('failed_at IS NULL').where('locked_by IS NULL').group(:queue).count.each do |queue, size|
       @queues << queue
       others.delete(queue)
+      Rails.logger.info "Setting #{queue} to size #{size}"
       dj_queue_size_metric.set(size, labels: { queue: queue })
     end
 
     # These are the ones that are now empty (if any)
     others.each do |queue|
+      Rails.logger.info "Setting #{queue} to size 0"
       dj_queue_size_metric.set(0, labels: { queue: queue })
     end
   end
