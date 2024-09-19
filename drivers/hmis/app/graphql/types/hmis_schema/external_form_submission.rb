91177122df0e6453f76f31f01784cd525ae9c0a7
Gig <gig@greenriver.org>
2024-09-19 12:36:27 -0400

    Resolve key/value summary fields on form submission type (#4720)

--- drivers/hmis/app/graphql/types/hmis_schema/external_form_submission.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/external_form_submission.rb
@@ -22,6 +22,9 @@ module Types
     field :definition, Forms::FormDefinition, null: false
     field :values, GraphQL::Types::JSON, null: true
     custom_data_elements_field # Resolve for backwards compatibility. Can remove in a future release
+    field :enrollment_id, ID, null: true, description: 'Enrollment that was generated from this submission, if any'
+    field :client_id, ID, null: true, description: 'Client that was generated from this submission, if any'
+    field :summary_fields, [HmisSchema::KeyValue], null: false, description: 'Key/value responses for certain summary-level form questions'
 
     def definition
       load_ar_association(object, :definition)
@@ -33,6 +36,43 @@ module Types
       # a client, and then a user manually updates the client's name in HMIS, the form review UI should still display
       # the client name that was originally submitted with the form.
       object.form_values
+    end
+
+    # "Summary" fields are a subset of the form's fields that are displayed on the external forms review table.
+    # Includes Client first/last name plus any CDEDs where show_in_summary is true.
+    Field = Struct.new(:id, :key, :value, keyword_init: true)
+    def summary_fields
+      cdeds_by_key = load_ar_association(definition, :custom_data_element_definitions).
+        select(&:show_in_summary).
+        index_by(&:key).stringify_keys
+
+      cded_keys = cdeds_by_key.keys.to_set
+
+      object.form_values.map do |key, value|
+        value = sanitized_value(value)
+        next unless value
+
+        case key.to_s
+        when 'Client.firstName'
+          Field.new(id: 'first', key: 'First Name', value: value)
+        when 'Client.lastName'
+          Field.new(id: 'last', key: 'Last Name', value: value)
+        when cded_keys
+          cded = cdeds_by_key[key]
+          Field.new(id: cded.id, key: cded.label, value: value)
+        end
+      end.compact.sort_by(&:key)
+    end
+
+    def client_id
+      enrollment = load_ar_association(object, :enrollment)
+      return unless enrollment
+
+      load_ar_association(enrollment, :client)&.id
+    end
+
+    private def sanitized_value(value)
+      value&.to_s&.strip&.truncate(100)&.gsub(/[[:cntrl:]]/, '').presence
     end
   end
 end
