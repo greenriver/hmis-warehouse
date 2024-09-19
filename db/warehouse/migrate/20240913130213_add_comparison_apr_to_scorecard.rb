d56945027e8024ce92de5f1943e008e4500a4010
Elliot Anders <elliot@greenriver.org>
2024-09-19 14:50:20 -0400

    Possible migration fix for deployment

--- db/warehouse/migrate/20240913130213_add_comparison_apr_to_scorecard.rb
+++ db/warehouse/migrate/20240913130213_add_comparison_apr_to_scorecard.rb
@@ -2,6 +2,8 @@ class AddComparisonAprToScorecard < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    add_reference :boston_project_scorecard_reports, :comparison_apr, null: true, index: {algorithm: :concurrently}
+    safety_assured do
+      add_reference :boston_project_scorecard_reports, :comparison_apr, null: true
+    end
   end
 end
