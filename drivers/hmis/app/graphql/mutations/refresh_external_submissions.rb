693ba46f105f3f77acab8c400640b015e7c4f56a
martha <medwards@greenriver.org>
2024-09-24 10:45:02 -0400

    Add mutation for refreshing external forms (#4730)
    * Add mutation for refreshing external forms
    
    * Increase max key count
    
    * Fix schema
    
    * PR feedback
    
    * Make priority negative
    
    * Revert 10_000 change

---
+++ drivers/hmis/app/graphql/mutations/refresh_external_submissions.rb
@@ -0,0 +1,20 @@
+#  Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+#  License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+#
+
+module Mutations
+  class RefreshExternalSubmissions < CleanBaseMutation
+    field :success, Boolean, null: false
+
+    def resolve
+      handlers = ['HmisExternalApis::ConsumeExternalFormSubmissionsJob']
+      return { success: true } if Delayed::Job.queued?(handlers) || Delayed::Job.running?(handlers)
+
+      queue = ENV.fetch('DJ_LONG_QUEUE_NAME', :long_running)
+      HmisExternalApis::ConsumeExternalFormSubmissionsJob.delay(priority: -5, queue: queue)
+
+      { success: true }
+    end
+  end
+end
