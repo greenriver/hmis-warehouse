860dc14f417be714557ed42ec2455b9e9972bea0
Elliot Anders <elliot@greenriver.org>
2024-09-30 16:31:16 -0400

    Additional session config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,4 +6,6 @@ Rails.application.config.session_store :redis_store,
   servers: ["redis://#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"],
   expire_after: 12.hours, # If this expires while a session is open, the user is logged out
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
+  httponly: true,
+  same_site: :lax,
   secure: true
