7ced071c605d55b5b718162706349e6feecd3981
Elliot Anders <elliot@greenriver.org>
2024-09-30 16:21:40 -0400

    Provide default redis config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :redis_store,
-  servers: ["redis://#{ENV['CACHE_HOST']}:#{ENV['CACHE_PORT']}/#{ENV['CACHE_DB']}/session"],
+  servers: ["redis://#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"],
   expire_after: 12.hours, # If this expires while a session is open, the user is logged out
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
   secure: true
