e7e7e38ad8ae0d8f6f3f9c5ab980ccc55ee4d577
Elliot Anders <elliot@greenriver.org>
2024-09-30 16:40:59 -0400

    Rubocop fixups

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,12 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store,
+Rails.application.config.session_store(
+  :redis_store,
   servers: ["redis://#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"],
   expire_after: 12.hours, # If this expires while a session is open, the user is logged out
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
   httponly: true,
   same_site: :lax,
-  secure: true
+  secure: true,
+)
