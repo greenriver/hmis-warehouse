50ece3f328aed8b2907a21fc30f4a2bfcf32c499
Elliot Anders <elliot@greenriver.org>
2024-10-01 14:53:31 -0400

    Cleanup commented code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,14 +1,6 @@
 # Rails.logger.debug "Running initializer in #{__FILE__}"
 
 # Be sure to restart your server when you modify this file.
-
-# Rails.application.config.session_store :cookie_store, key: '_boston-hmis_session', httponly: true, secure: Rails.env.production?
-
-# method = ENV.fetch('CACHE_SSL', false).to_s == 'true' ? 'rediss://' : 'redis://'
-# method = !(Rails.env.development? || Rails.env.test?) ? 'rediss://' : 'redis://'
-# method = 'rediss://'
-# redis_url = "#{method}#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"
-#
 Rails.application.config.session_store(
   :redis_store,
   servers: [
