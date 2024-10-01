f4bba868bd002f7a5c60a72f1476a53da4a7ac3a
martha <medwards@greenriver.org>
2024-10-01 13:46:17 -0400

    Fix double-layer bug with case management permission

--- drivers/hmis/app/models/hmis/staff_assignment.rb
+++ drivers/hmis/app/models/hmis/staff_assignment.rb
@@ -11,4 +11,9 @@ class Hmis::StaffAssignment < Hmis::HmisBase
   belongs_to :user, class_name: 'Hmis::User'
   belongs_to :household, class_name: 'Hmis::Hud::Household', primary_key: [:data_source_id, :HouseholdID], foreign_key: [:data_source_id, :household_id], inverse_of: :staff_assignments
   belongs_to :staff_assignment_relationship, foreign_key: :hmis_staff_assignment_relationship_id, class_name: 'Hmis::StaffAssignmentRelationship'
+
+  scope :viewable_by, ->(user) do
+    # Special case this, rather than using EnrollmentRelated concern, because we have to join through Household
+    joins(household: :enrollments).merge(Hmis::Hud::Enrollment.viewable_by(user))
+  end
 end
