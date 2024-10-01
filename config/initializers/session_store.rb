c499d2b93ccd0fc4f4c1c7d100f76b7641294e84
Elliot Anders <elliot@greenriver.org>
2024-10-01 14:32:43 -0400

    Attempt a different connection mechanism

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,12 +5,20 @@
 # Rails.application.config.session_store :cookie_store, key: '_boston-hmis_session', httponly: true, secure: Rails.env.production?
 
 # method = ENV.fetch('CACHE_SSL', false).to_s == 'true' ? 'rediss://' : 'redis://'
-method = !(Rails.env.development? || Rails.env.test?) ? 'rediss://' : 'redis://'
-method = 'rediss://'
-redis_url = "#{method}#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"
+# method = !(Rails.env.development? || Rails.env.test?) ? 'rediss://' : 'redis://'
+# method = 'rediss://'
+# redis_url = "#{method}#{ENV.fetch('CACHE_HOST', 'redis')}:#{ENV.fetch('CACHE_PORT', 6379)}/#{ENV.fetch('CACHE_DB', 1)}/session"
+#
 Rails.application.config.session_store(
   :redis_store,
-  servers: [redis_url],
+  servers: [
+    {
+      ssl: ENV.fetch('CACHE_SSL', false).to_s == 'true',
+      host: ENV.fetch('CACHE_HOST', 'redis'),
+      port: ENV.fetch('CACHE_PORT', 6379),
+      db: ENV.fetch('CACHE_DB', 1),
+    }
+  ],
   expire_after: 12.hours, # If this expires while a session is open, the user is logged out
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
   httponly: true,
