f550cdd12e2ad040c59c774056850bc55ce4d0de
Theron Toomey <ttoomey@greenriver.org>
2024-10-03 13:17:52 -0400

    6399-hopwa-caper-report (#4648)
    * 6399-hopwa-caper-report
    
    squash my working branch into r130
    
    * 6399-hopwa-caper-report
    
    * 6399-hopwa-caper-report
    
    schema
    
    * 6399-hopwa-caper-report
    
    * 6399-hopwa-caper-report
    
    * 6399-hopwa-caper-report
    
    pr feedback
    
    * 6399-hopwa-caper-report
    
    * 6399-hopwa-caper-report-squash cleanup
    
    * 6399-hopwa-caper-report-squash
    
    get filters working in test
    
    * sort index by title
    
    * 6399-hopwa-caper-report
    
    allow multiple filters, rm unused sheet
    
    * strmu sheet fix
    
    * 6399-hopwa-caper-report-squash
    
    * 6399-hopwa-caper-report-squash
    
    cleanup, tests
    
    * 6399-hopwa-caper-report-squash
    
    add docs
    
    * 6399-hopwa-caper-report-squash
    
    cleanup, exit destination cleanup
    
    * 6399-hopwa-caper-report-squash
    
    adjust for new spec
    * use enrollment dates instead of services to select households
    * for longevity, count individuals rather than household
    * adjustment to "transitional housing" exit to only be 302
    * do not report FA Amount as total
    
    * 6399-hopwa-caper-report-squash
    
    * 6399-hopwa-caper-report-squash
    
    * 6399-hopwa-caper-report-squash
    
    adjust housing outcomes, add comments
    
    * 6399-hopwa-caper-report-squash
    
    longevity calc sum/adjustments, use client_id for groups
    
    * 6399-hopwa-caper-report-squash
    
    fix dupe row
    
    * 6399-hopwa-caper-report-squash
    
    pr feedback
    
    * 6399-hopwa-caper-report-squash
    
    longevity
    
    * 6399-hopwa-caper-report-squash
    
    fix tests
    
    * 6399-hopwa-caper-report-squash
    
    bugfix
    
    * 6399-hopwa-caper-report-squash
    
    fixes & improvements
    
    * 6399-hopwa-caper-report-squash
    
    bugfix
    
    * 6399-hopwa-caper-report-squash
    
    fixes
    
    * 6399-hopwa-caper-report-squash
    
    rubcop
    
    * 6399-hopwa-caper-report-squash
    
    limit relevant enrollments
    
    * 6399-hopwa-caper-report-squash
    
    * 6399-hopwa-caper-report-squash
    
    bugfixes
    
    * 6399-hopwa-caper-report-squash
    
    cell details
    
    * 6399-hopwa-caper-report-squash
    
    project type fix
    
    * 6399-hopwa-caper-report-squash
    
    do not restrict enrollments by project funder
    
    * Add PercentAMI to HOPWA assessments
    
    * Remove SSVF label
    
    * 6399-hopwa-caper-report-squash
    
    filter ui fix
    
    * nudge ci
    
    * bugfix
    
    ---------
    
    Co-authored-by: Gig Ashton <gig@greenriver.org>
    Co-authored-by: Elliot <elliot@greenriver.org>

--- app/models/grda_warehouse/service_history_enrollment.rb
+++ app/models/grda_warehouse/service_history_enrollment.rb
@@ -8,6 +8,7 @@
 #
 # ServiceHistoryEnrollments flatten Hud Enrollments and related records to serve reporting needs. These records are
 # generated automatically. There is a 1:1 correspondence with Hud Enrollment records
+#   created by GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find_each(&:rebuild_service_history!)
 class GrdaWarehouse::ServiceHistoryEnrollment < GrdaWarehouseBase
   include RailsDrivers::Extensions
   include ArelHelper
