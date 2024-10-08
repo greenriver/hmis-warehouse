fca80fdf23aeb6825175bbed4a25f582831b58bb
Todd Blackman <tdblackman@gmail.com>
2024-10-08 10:01:19 -0400

    no eager load on rails runner calls

--- bin/deploy_tasks.sh
+++ bin/deploy_tasks.sh
@@ -58,7 +58,7 @@ echo "...rake db:seed took $(expr $T2 - $T1) seconds"
 
 echo 'Installing cron'
 T1=`date +%s`
-bundle exec rails runner ./config/deploy/docker/lib/cron_installer.rb
+EAGER_LOAD=false bundle exec rails runner ./config/deploy/docker/lib/cron_installer.rb
 T2=`date +%s`
 echo "..../bin/cron_installer.rb took $(expr $T2 - $T1) seconds"
 
