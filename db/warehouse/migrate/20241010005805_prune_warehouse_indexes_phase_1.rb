b4ca28cbfb2f00a2fe8e1f50d42f56cb9386aade
Theron Toomey <ttoomey@greenriver.org>
2024-10-19 19:43:30 +0000

    6625-db-index-improvement
    safety check

--- db/warehouse/migrate/20241010005805_prune_warehouse_indexes_phase_1.rb
+++ db/warehouse/migrate/20241010005805_prune_warehouse_indexes_phase_1.rb
@@ -15,22 +15,32 @@ class PruneWarehouseIndexesPhase1 < ActiveRecord::Migration[7.0]
 
   protected
 
-  def check_index_table(index_name, table_name)
+  def check_unused_index_table(index_name, table_name)
     sql = <<~SQL
-      SELECT t.relname
+      SELECT t.relname,
+             idx.relname AS index_name,
+             coalesce(idx_stat.idx_scan, 0) AS scan_count
       FROM pg_index i
       JOIN pg_class t ON t.oid = i.indrelid
       JOIN pg_class idx ON idx.oid = i.indexrelid
+      LEFT JOIN pg_stat_user_indexes idx_stat
+             ON idx.oid = idx_stat.indexrelid
       WHERE idx.relname = $1
     SQL
-    result = GrdaWarehouseBase.connection.select_value(sql, 'SQL', [index_name])
-    raise "expected index #{index_name} to be on #{table_name} but was #{result}" unless result == table_name
+
+    result = GrdaWarehouseBase.connection.exec_query(sql, 'SQL', [index_name]).first
+
+    raise "expected index #{index_name} to be on #{table_name} but was #{result['relname']}" unless result && result['relname'] == table_name
+
+    return unless result['scan_count'].to_i.positive?
+
+    warn "Index #{index_name} on #{table_name} is in use (scan count > 0)."
   end
 
   # it seems that the index names aren't consistent between prod and dev. Attempt to remove the index by name only
   def remove_index_safely(table:, index_name:, columns:)
     if index_exists?(table, columns, name: index_name)
-      check_index_table(index_name, table) # additional safety check
+      check_unused_index_table(index_name, table) # additional safety check
       remove_index(table, name: index_name)
     # we could try and remove equivalent index but there's less certainty that is is unused if the name doesn't match
     # elsif Rails.env.development? && index_exists?(table, columns)
