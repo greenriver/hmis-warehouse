9a0ec3e2a228161e9b06b410662382d77aa89477
Elliot Anders <elliot@greenriver.org>
2024-11-13 11:31:00 -0500

    Adjust batch to match code changes

--- db/warehouse/migrate/20241112181349_set_missing_timestamps.rb
+++ db/warehouse/migrate/20241112181349_set_missing_timestamps.rb
@@ -1,9 +1,9 @@
 class SetMissingTimestamps < ActiveRecord::Migration[7.0]
   def up
     # Destination Clients
-    import_batch = []
     GrdaWarehouse::Hud::Client.destination.where(date_deleted: nil, date_created: nil).
       preload(:warehouse_client_destination).find_in_batches do |client_batch|
+        import_batch = []
         client_batch.each do |client|
           client.date_created = client.warehouse_client_destination.min(&:created_at)
           client.date_updated ||= client.warehouse_client_destination.min(&:updated_at)
@@ -19,9 +19,9 @@ class SetMissingTimestamps < ActiveRecord::Migration[7.0]
       end
 
     # Source Clients
-    import_batch = []
     GrdaWarehouse::Hud::Client.source.where(date_deleted: nil, date_created: nil).
       preload(:warehouse_client_source).find_in_batches do |client_batch|
+        import_batch = []
         client_batch.each do |client|
           client.date_created = client.warehouse_client_source.created_at
           client.date_updated ||= client.warehouse_client_source.updated_at
