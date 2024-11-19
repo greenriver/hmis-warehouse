2e35573b47c7adf5437fe28217be4d08947d122b
Elliot <elliot@greenriver.org>
2024-11-19 16:00:49 -0500

    Eslint upgrade (#4942)
    * Upgrade eslint instead of dependent cross-spawn
    
    * New eslint config
    
    * Remove replaced eslint config
    
    * Add back recommended configs
    
    ---------
    
    Co-authored-by: Gig Ashton <gig@greenriver.org>

--- app/assets/javascripts/background_render.js
+++ app/assets/javascripts/background_render.js
@@ -1,20 +1,22 @@
+// eslint-disable-next-line no-undef
 App.StimulusApp.register('background-render', class extends Stimulus.Controller {
   static get values() {
-    return {renderId: String, url: String, fetchParams: Object}
+    return {renderId: String, url: String, fetchParams: Object};
   }
 
   connect() {
     // console.log('Connected')
     // set a unique id for this render
-    this.renderIdValue = this.uuid()
+    this.renderIdValue = this.uuid();
 
     // subscribe to the channel and watch for updates from action_cable
-    this.subscription = App.cable.subscriptions.create({channel: "BackgroundRenderChannel", id: this.renderIdValue}, {
+    this.subscription = App.cable.subscriptions.create({channel: 'BackgroundRenderChannel', id: this.renderIdValue}, {
       connected: () => {
-        this.fetch()
+        this.fetch();
       },
       received: (data) => {
-        if (data.cableReady) CableReady.perform(data.operations)
+        // eslint-disable-next-line no-undef
+        if (data.cableReady) CableReady.perform(data.operations);
       }
     });
 
@@ -31,25 +33,26 @@ App.StimulusApp.register('background-render', class extends Stimulus.Controller 
     // send an ajax request to kick off the render job
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-    const body = this.fetchParamsValue
-    body.render_id = this.renderIdValue
+    const body = this.fetchParamsValue;
+    body.render_id = this.renderIdValue;
 
+    // eslint-disable-next-line no-undef
     fetch(this.urlValue, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "X-CSRF-Token": csrfToken,
-        "Content-Type": "application/json"
+        'X-CSRF-Token': csrfToken,
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
     }).catch( () => {
-      window.alert("Sorry, an error occurred while loading page content.  Please refresh the page and try again.")
-    })
+      window.alert('Sorry, an error occurred while loading page content.  Please refresh the page and try again.');
+    });
   }
 
   disconnect() {
-    App.cable.subscriptions.remove(this.subscription)
+    App.cable.subscriptions.remove(this.subscription);
   }
 
 
 
-})
+});
