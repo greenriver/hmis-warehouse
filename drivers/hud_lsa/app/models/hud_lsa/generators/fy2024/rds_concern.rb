748a73173c61c7f9c060c33c2515291b1f949a50
Elliot Anders <elliot@greenriver.org>
2024-11-26 13:51:16 -0500

    Additional error correction

--- drivers/hud_lsa/app/models/hud_lsa/generators/fy2024/rds_concern.rb
+++ drivers/hud_lsa/app/models/hud_lsa/generators/fy2024/rds_concern.rb
@@ -107,7 +107,9 @@ module HudLsa::Generators::Fy2024::RdsConcern
           klass.connection.execute(sql)
           @s3_feature_enabled = true
           log_and_ping('RDS S3 Integration to completed') # Probably don't need this long-term, tracking timing now
-        rescue TinyTds::Error # FIXME: this needs to be more specific
+        rescue TinyTds::Error => e
+          raise unless e.full_message.include?('process of being enabled')
+
           log_and_ping('Waiting for RDS S3 Integration to complete') # Probably don't need this long-term, tracking timing now
           sleep(60)
         end
@@ -142,6 +144,8 @@ module HudLsa::Generators::Fy2024::RdsConcern
     )
   rescue Aws::RDS::Errors::InvalidParameterValue => e
     raise e unless e.full_message.include?('only one ARN associated')
+  rescue ActiveRecord::StatementInvalid => e
+    raise e unless e.full_message.include?('process of being enabled')
   end
 
   private def wait_for_s3_file_transfer(file:, klass:)
