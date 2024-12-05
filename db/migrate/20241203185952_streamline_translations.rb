aa1d17323af9596c1a0fd9d10df84c68e9c0b6c4
Elliot Anders <elliot@greenriver.org>
2024-12-05 13:48:56 -0500

    Translation updates

--- db/migrate/20241203185952_streamline_translations.rb
+++ db/migrate/20241203185952_streamline_translations.rb
@@ -1,7 +1,33 @@
 class StreamlineTranslations < ActiveRecord::Migration[7.0]
   def up
-    raise "FIXME: this still needs a down"
-    keys = {
+    translations.each do |old_key, new_key|
+      t = Translation.find_by(key: old_key)
+      next unless t
+      next t.destroy! if old_key.in?(replaced)
+
+      t.text ||= old_key # If we never translated it, just use the old key as the translation
+      t.key = new_key # set the new key as the key
+      t.save!
+    end
+  end
+
+  def down
+    translations.each do |old_key, new_key|
+      t = Translation.find_by(key: new_key)
+      next unless t
+
+      t.key = old_key
+      # never translated, reset the translation to empty
+      t.text = nil if t.text == old_key
+      t.save!
+    end
+    replaced.each do |old_key|
+      Translation.create(key: old_key)
+    end
+  end
+
+  def translations
+    {
       'the Boston Emergency Shelter Commission' => 'the Emergency Shelter Commission',
       'MA-500 Boston Continuum of Care FY2022 Renewal Project Scoring Tool' => 'FY2022 Renewal Project Scoring Tool',
       'Boston HMIS staff at DND' => 'HMIS staff at the CoC',
@@ -11,22 +37,12 @@ class StreamlineTranslations < ActiveRecord::Migration[7.0]
       'Boston DND Warehouse' => 'Open Path HMIS Warehouse',
       'Ending Veteran & Chronic Homelessness in Boston' => 'Ending Homelessness',
     }
-
-    replaced = [
-      'City of Boston DND Warehouse', # used in emails
-      'Boston DND HMIS Warehouse',
-    ]
-    keys.each do |old_key, new_key|
-      next t.destroy! if t.in?(replaced)
-
-      t = Translation.find_by(key: old_key)
-      t.text ||= old_key # If we never translated it, just use the old key as the translation
-      t.key = new_key # set the new key as the key
-      t.save!
-    end
   end
 
-  def down
-
+  def replaced
+    [
+      'City of Boston DND Warehouse',
+      'Boston DND HMIS Warehouse',
+    ]
   end
 end
