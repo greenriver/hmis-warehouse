2e96348e5a7f67bd547f50ce706bfedea398df8d
Dave G <dgreiner@greenriver.org>
2024-12-17 11:39:53 -0500

    rubocop

--- db/warehouse/migrate/20241216184819_talent_configuration_flags.rb
+++ db/warehouse/migrate/20241216184819_talent_configuration_flags.rb
@@ -1,6 +1,6 @@
 class TalentConfigurationFlags < ActiveRecord::Migration[7.0]
   def change
     add_column :configs, :default_lms_email_to_warehouse_email, :boolean
-    add_column :talentlms_configs, :allow_automatic_redirect_to_course, :boolean, default: true  
+    add_column :talentlms_configs, :allow_automatic_redirect_to_course, :boolean, default: true
   end
 end
