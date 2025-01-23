b82326fff634de076b42cb3e945a28541a044584
Elliot Anders <elliot@greenriver.org>
2025-01-23 18:06:50 -0500

    Fix for failing test

--- spec/models/grda_warehouse/lookups/coc_code_spec.rb
+++ spec/models/grda_warehouse/lookups/coc_code_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe GrdaWarehouse::Lookups::CocCode, type: :model do
   let!(:data_source) { create :data_source_fixed_id }
-  let!(:organization) { create :grda_warehouse_hud_organization }
-  let!(:project_1) { create :grda_warehouse_hud_project, ProjectType: 1, OrganizationID: organization.OrganizationID }
+  let!(:organization) { create :grda_warehouse_hud_organization, data_source_id: data_source.id }
+  let!(:project_1) { create :grda_warehouse_hud_project, ProjectType: 1, OrganizationID: organization.OrganizationID, data_source_id: data_source.id }
   let!(:project_coc_coc_1) { create :grda_warehouse_hud_project_coc, ProjectID: project_1.ProjectID, data_source_id: data_source.id, CoCCode: 'XX-500' }
-  let!(:project_2) { create :grda_warehouse_hud_project, ProjectType: 3, OrganizationID: organization.OrganizationID }
+  let!(:project_2) { create :grda_warehouse_hud_project, ProjectType: 3, OrganizationID: organization.OrganizationID, data_source_id: data_source.id }
   let!(:project_coc_coc_2) { create :grda_warehouse_hud_project_coc, ProjectID: project_2.ProjectID, data_source_id: data_source.id, CoCCode: 'XX-501' }
-  let!(:project_3) { create :grda_warehouse_hud_project, ProjectType: 3, OrganizationID: organization.OrganizationID }
+  let!(:project_3) { create :grda_warehouse_hud_project, ProjectType: 3, OrganizationID: organization.OrganizationID, data_source_id: data_source.id }
   let!(:project_coc_coc_3) { create :grda_warehouse_hud_project_coc, ProjectID: project_3.ProjectID, data_source_id: data_source.id, CoCCode: 'XX-502' }
   let!(:user) { create :user }
   let!(:coc_codes) { create_list :lookup_coc, 5 }
