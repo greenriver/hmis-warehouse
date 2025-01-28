de9625f6130374d83c700e973c6baba947dd062c
Dave G <149399758+dtgreiner@users.noreply.github.com>
2025-01-28 08:19:23 -0500

    APR/CAPER Q21 - annual assessment calculation (#5061)
    Co-authored-by: Elliot <elliot@greenriver.org>

--- drivers/hud_apr/spec/models/fy2024/datalab_apr/multiple_projects.rb
+++ drivers/hud_apr/spec/models/fy2024/datalab_apr/multiple_projects.rb
@@ -271,10 +271,6 @@ RSpec.shared_context 'datalab multiple projects apr', shared_context: :metadata 
       compare_results(
         file_path: result_file_prefix + results_dir,
         question: 'Q21',
-        skip: [
-          'C14', # expected '5.0000' (5), got '3.0000' (3)
-          'C15', # expected '138.0000' (138), got '140.0000' (140)
-        ],
       )
     end
 
