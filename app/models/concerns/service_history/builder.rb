96c1c81987dfae316dde6954eb743c734ffc6631
Elliot Anders <elliot@greenriver.org>
2025-02-11 08:54:26 -0500

    Ensure we never pull a cached query

--- app/models/concerns/service_history/builder.rb
+++ app/models/concerns/service_history/builder.rb
@@ -121,11 +121,12 @@ module ServiceHistory::Builder
 
     # Class method
     private def builder_batch_job_scope
+      @counter ||= 1
       Delayed::Job.uncached do
         Delayed::Job.where(failed_at: nil).
           # Cache buster - for whatever reason `uncached` is completely inconsistent.
           # We ALWAYS want this to query the database.
-          where("1 != #{rand(2..50)}").
+          where("1 != #{@counter += 1}").
           jobs_for_class('ServiceHistory::RebuildEnrollments')
       end
     end
