526dc86b257986e2964be11ff891d13f4301ec26
Dave G <149399758+dtgreiner@users.noreply.github.com>
2025-02-24 11:59:46 -0500

    rubocop fixes for client expiration PR and ruby 3.3.7 string literals (#5163)

--- app/controllers/clients/notes_controller.rb
+++ app/controllers/clients/notes_controller.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: false
+
 module Clients
   class NotesController < ApplicationController
     include AjaxModalRails::Controller
--- db/warehouse/migrate/20250221151129_add_expiration_date_to_client_note.rb
+++ db/warehouse/migrate/20250221151129_add_expiration_date_to_client_note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: false
+
 class AddExpirationDateToClientNote < ActiveRecord::Migration[7.0]
   def change
     add_column :client_notes, :expiration_date, :date
