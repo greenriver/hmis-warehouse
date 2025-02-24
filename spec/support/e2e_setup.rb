ea121e36dacd97e2a9c74988454889042d8f3899
Gig Ashton <gig@greenriver.org>
2025-02-24 12:10:24 -0500

    Fix comments

--- spec/support/e2e_setup.rb
+++ spec/support/e2e_setup.rb
@@ -4,11 +4,6 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
-#  Copyright 2016 - 2025 Green River Data Analysis, LLC
-#
-#  License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
-#
-
 # frozen_string_literal: false
 
 if ENV['RUN_SYSTEM_TESTS']
