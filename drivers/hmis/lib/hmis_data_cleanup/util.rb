304562533d3dca6b2b5c498165e88869fd09012b
martha <medwards@greenriver.org>
2025-02-26 17:37:04 -0500

    Add some frozen string literals comments

--- drivers/hmis/lib/hmis_data_cleanup/util.rb
+++ drivers/hmis/lib/hmis_data_cleanup/util.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: false
+
 require 'csv'
 
 # Utility functions for cleaning up HMIS data during a migration.
