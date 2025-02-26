3d5b7a16b20a3536b3af5176512e92cc57146f74
Theron Toomey <ttoomey@greenriver.org>
2025-02-26 15:18:01 +0000

    add missing index

--- db/warehouse_structure.sql
+++ db/warehouse_structure.sql
@@ -278,7 +278,7 @@ CREATE FUNCTION public.service_history_service_insert_trigger() RETURNS trigger
             INSERT INTO service_history_services_2001 VALUES (NEW.*);
          ELSIF  ( NEW.date BETWEEN DATE '2000-01-01' AND DATE '2000-12-31' ) THEN
             INSERT INTO service_history_services_2000 VALUES (NEW.*);
-        
+
       ELSE
         INSERT INTO service_history_services_remainder VALUES (NEW.*);
         END IF;
@@ -52439,6 +52439,12 @@ CREATE UNIQUE INDEX idx_tpc_uniqueness ON public.enrollment_extras USING btree (
 
 
 --
+-- Name: idx_youth_eds_hud_keys; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX idx_youth_eds_hud_keys ON public."YouthEducationStatus" USING btree ("EnrollmentID", data_source_id, "PersonalID");
+
+--
 -- Name: income_benefits_date_created; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -65847,6 +65853,5 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20250121204933'),
 ('20250203142317'),
 ('20250213173031'),
-('20250222234500');
-
-
+('20250222234500'),
+('20250226145730');
