80260b442eee0150f3756c3260472b6afabf1444
Elliot Anders <elliot@greenriver.org>
2025-02-27 09:22:29 -0500

    Frozen string literals for migrations

--- db/warehouse/migrate/20250226211623_add_last_used_on_to_import_overrides.rb
+++ db/warehouse/migrate/20250226211623_add_last_used_on_to_import_overrides.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLastUsedOnToImportOverrides < ActiveRecord::Migration[7.0]
   def change
     add_column :import_overrides, :last_used_on, :date
--- db/warehouse/migrate/20250226212117_default_last_used_on_import_overrides.rb
+++ db/warehouse/migrate/20250226212117_default_last_used_on_import_overrides.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DefaultLastUsedOnImportOverrides < ActiveRecord::Migration[7.0]
   def up
     HmisCsvImporter::ImportOverride.update_all(last_used_on: Date.current)
