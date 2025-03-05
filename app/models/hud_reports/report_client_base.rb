299080e0567bec57d8d021338a130afeabf9935a
Dave G <dgreiner@greenriver.org>
2025-03-05 08:51:46 -0500

    applicaiton helper methods

--- app/models/hud_reports/report_client_base.rb
+++ app/models/hud_reports/report_client_base.rb
@@ -11,7 +11,7 @@ module HudReports
   class ReportClientBase < GrdaWarehouseBase
     include ActionView::Helpers
     include ActionView::Context
-    include ApplicationHelper
+
     self.abstract_class = true
 
     has_many :report_clients, as: :universe_membership, dependent: :destroy
@@ -21,9 +21,11 @@ module HudReports
       # This will separate the object and method in order to call the appropriate column/method
       # while similarly allowing the method to be called if the column is not passed using dot notation.
       cell = col.to_s.split('.').inject(self) do |obj, method|
-        value = obj[method]
-        value = obj.send(method) if value.nil?
-        value
+        if obj.class.column_names.include?(method)
+          obj[method]
+        else
+          obj.send(method)
+        end
       end
 
       return ActionController::Base.helpers.content_tag(:pre, JSON.pretty_generate(cell)) if include_content_tag && (cell.is_a?(Array) || cell.is_a?(Hash))
@@ -61,6 +63,53 @@ module HudReports
       else
         cell
       end
+    end
+
+    # Including Applicaiton Helper had name clashes with classes inheriting from this base class.
+    # Including this method here as a work around.
+    def yes_no(boolean, include_icon: true, include_content_tag: true)
+      return 'Not Specified' if boolean.nil?
+
+      case boolean
+      when true, 'Yes'
+        if include_content_tag
+          capture do
+            concat content_tag :span, nil, class: 'icon-checkmark o-color--positive' if include_icon
+            concat ' Yes'
+          end
+        else
+          'Yes'
+        end
+      when false, 'No'
+        if include_content_tag
+          capture do
+            concat content_tag :span, nil, class: 'icon-cross o-color--danger' if include_icon
+            concat ' No'
+          end
+        else
+          'No'
+        end
+      when 'Refused'
+        if include_content_tag
+          capture do
+            concat content_tag :span, nil, class: 'icon-warning o-color--warning' if include_icon
+            concat ' Refused/Unsure'
+          end
+        else
+          'Refused/Unsure'
+        end
+      end
+    end
+
+    # Including Applicaiton Helper had name clashes with classes inheriting from this base class.
+    # Including this method here as a work around.
+    def masked_ssn(number, include_content_tag: true)
+      # pad with leading 0s if we don't have enough characters
+      number = number.to_s.rjust(9, '0') if number.present?
+      value = number.to_s.gsub(HudUtility2024::SSN_RGX, 'XXX-XX-\3')
+      return value unless include_content_tag
+
+      ActionController::Base.helpers.content_tag :span, number.to_s.gsub(HudUtility2024::SSN_RGX, 'XXX-XX-\3')
     end
   end
 end
