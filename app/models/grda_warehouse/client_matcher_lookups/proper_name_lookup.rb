1bc072b4fca5e1985fe501080cb0dd324b5a6430
Elliot Anders <elliot@greenriver.org>
2025-03-08 17:01:27 -0500

    Convert anything that isn't UTF-8 as I18n.transliterate will error otherwise

--- app/models/grda_warehouse/client_matcher_lookups/proper_name_lookup.rb
+++ app/models/grda_warehouse/client_matcher_lookups/proper_name_lookup.rb
@@ -38,9 +38,9 @@ module GrdaWarehouse::ClientMatcherLookups
     def normalize(str)
       return nil unless str.present?
 
-      # Convert to UTF-8 if we received as ASCII-8BIT as I18n.transliterate expects UTF-8
+      # Convert to UTF-8 if we received as anything else as I18n.transliterate expects UTF-8
       # This conversion will strip weirdly encoded characters silently
-      str = str.encode('UTF-8', 'ASCII-8BIT', invalid: :replace, undef: :replace, replace: '') if str.encoding == 'ASCII-8BIT'
+      str = str.encode(Encoding::UTF_8, invalid: :replace, undef: :replace, replace: '') unless str.encoding == Encoding::UTF_8
 
       str = I18n.transliterate(str) if transliterate
       str.downcase.strip.gsub(/[^a-z0-9]/, '').presence
