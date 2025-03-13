a4f5b830b8fee3b1a429d350b9928efa5401598c
Elliot <elliot@greenriver.org>
2025-03-13 19:09:34 -0400

    Move relevant state into the database (#5200)
    * Move relevant state into the database
    
    * Rubocop
    
    * Brakeman shifting

---
+++ db/warehouse/migrate/20250313125655_add_relevant_state_to_site_config.rb
@@ -0,0 +1,8 @@
+
+# frozen_string_literal: true
+
+class AddRelevantStateToSiteConfig < ActiveRecord::Migration[7.0]
+  def change
+    add_column :configs, :relevant_state_codes, :string, default: ENV['RELEVANT_COC_STATE'] || 'MA', null: false
+  end
+end
