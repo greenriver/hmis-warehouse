b15646df63a686a186056a3106cda7dd268ac9fd
Elliot Anders <elliot@greenriver.org>
2025-03-13 09:15:35 -0400

    Move relevant state into the database

---
+++ db/warehouse/migrate/20250313125655_add_relevant_state_to_site_config.rb
@@ -0,0 +1,5 @@
+class AddRelevantStateToSiteConfig < ActiveRecord::Migration[7.0]
+  def change
+    add_column :configs, :relevant_state_codes, :string, default: ENV['RELEVANT_COC_STATE'] || 'MA', null: false
+  end
+end
