73544128855496745e05f813c7c4539c29bdc038
Elliot Anders <elliot@greenriver.org>
2025-03-13 09:23:29 -0400

    Rubocop

--- db/warehouse/migrate/20250313125655_add_relevant_state_to_site_config.rb
+++ db/warehouse/migrate/20250313125655_add_relevant_state_to_site_config.rb
@@ -1,3 +1,6 @@
+
+# frozen_string_literal: true
+
 class AddRelevantStateToSiteConfig < ActiveRecord::Migration[7.0]
   def change
     add_column :configs, :relevant_state_codes, :string, default: ENV['RELEVANT_COC_STATE'] || 'MA', null: false
