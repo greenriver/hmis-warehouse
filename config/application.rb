06aba21db4b9c74e67166caf8af7baf90874bb81
Elliot Anders <elliot@greenriver.org>
2025-03-23 14:55:36 -0400

    Rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,12 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https: //github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+
 require_relative 'boot'
 
 require 'rails/all'
