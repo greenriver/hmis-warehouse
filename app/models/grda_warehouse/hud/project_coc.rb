b0ffc9a4170a4b456a5f0cc66e824b0d71582b29
Elliot Anders <elliot@greenriver.org>
2025-03-23 18:55:41 -0400

    Make project coc -> lookup_coc optional so it allows nil CoCCodes

--- app/models/grda_warehouse/hud/project_coc.rb
+++ app/models/grda_warehouse/hud/project_coc.rb
@@ -23,10 +23,11 @@ module GrdaWarehouse::Hud
     belongs_to :project, **hud_assoc(:ProjectID, 'Project'), inverse_of: :project_cocs, optional: true
     belongs_to :export, **hud_assoc(:ExportID, 'Export'), inverse_of: :project_cocs, optional: true
     belongs_to :user, **hud_assoc(:UserID, 'User'), inverse_of: :project_cocs, optional: true
+    belongs_to :lookup_coc, class_name: '::GrdaWarehouse::Lookups::CocCode', foreign_key: :CoCCode, primary_key: :coc_code, inverse_of: :project_cocs, optional: true
+    belongs_to :data_source
+
     has_many :geographies, class_name: 'GrdaWarehouse::Hud::Geography', primary_key: [:ProjectID, :CoCCode, :data_source_id], foreign_key: [:ProjectID, :CoCCode, :data_source_id], inverse_of: :project_coc
     has_many :inventories, class_name: 'GrdaWarehouse::Hud::Inventory', primary_key: [:ProjectID, :CoCCode, :data_source_id], foreign_key: [:ProjectID, :CoCCode, :data_source_id], inverse_of: :project_coc
-    belongs_to :data_source
-    belongs_to :lookup_coc, class_name: '::GrdaWarehouse::Lookups::CocCode', foreign_key: :CoCCode, primary_key: :coc_code, inverse_of: :project_cocs
 
     # hide previous declaration of :importable, we'll use this one
     replace_scope :importable, -> do
