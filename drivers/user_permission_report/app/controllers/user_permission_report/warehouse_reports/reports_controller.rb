9f020efd2d3532fe0f7174e116393335e782654e
Gig Ashton <gig@greenriver.org>
2025-03-24 18:17:49 -0400

    Add hmis sheet to user permission report

--- drivers/user_permission_report/app/controllers/user_permission_report/warehouse_reports/reports_controller.rb
+++ drivers/user_permission_report/app/controllers/user_permission_report/warehouse_reports/reports_controller.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 module UserPermissionReport::WarehouseReports
   class ReportsController < ApplicationController
     before_action :set_group_associations
@@ -40,5 +42,41 @@ module UserPermissionReport::WarehouseReports
       }
     end
     helper_method :associated_items
+
+    def hmis_access_report_rows
+      hmis_users = Hmis::User.order(:last_name, :first_name).
+        includes(access_controls: [:access_group, :user_group, :role])
+
+      hmis_users.map do |user|
+        next unless user.access_controls.any? # non-HMIS user
+
+        # One row per Access Control that this User belongs to
+        user.access_controls.map do |access_control|
+          entities = hmis_collection_entities(access_control.access_group_id)
+
+          {
+            name: user.name,
+            email: user.email,
+            user_id: user.id,
+            # TODO add status
+            # TODO add last HMIS login
+            role_name: access_control.role.name,
+            role_permissions: access_control.role.granted_permissions.map { |perm| perm.to_s.humanize }.join(', '),
+            collection_name: access_control.access_group.name,
+            data_sources_in_collection: entities[:data_sources].join("\n").presence,
+            organizations_in_collection: entities[:organizations].join("\n").presence,
+            projects_in_collection: entities[:projects].join("\n").presence,
+            inherited_from_user_group: access_control.user_group.name,
+          }
+        end
+      end.compact.flatten(1)
+    end
+    helper_method :hmis_access_report_rows
+
+    private def hmis_collection_entities(collection_id)
+      @entities_by_collection_id ||= Hmis::AccessGroup.all.map { |ag| [ag.id, ag.entity_names] }.to_h
+
+      @entities_by_collection_id[collection_id]
+    end
   end
 end
