e98f58e2a3f9928afa3232d6802e8d35f13a5cec
Elliot Anders <elliot@greenriver.org>
2025-03-25 11:13:41 -0400

    Rubocop for seed maker

--- db/seed_maker.rb
+++ db/seed_maker.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 require 'faker'
 
 # To use SeedMaker outside of `db:seed` call `require ./db/seed_maker`
