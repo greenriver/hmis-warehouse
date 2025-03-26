d99a30fc4c9056ea5b6e8f6b0ce40afac4d3f13f
martha <medwards@greenriver.org>
2025-03-26 19:38:41 -0400

    Reorder comment

--- drivers/hmis/app/graphql/mutations/bulk_assign_service.rb
+++ drivers/hmis/app/graphql/mutations/bulk_assign_service.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 ###
 # Copyright 2016 - 2025 Green River Data Analysis, LLC
 #
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
-
-# frozen_string_literal: true
 
 module Mutations
   class BulkAssignService < CleanBaseMutation
