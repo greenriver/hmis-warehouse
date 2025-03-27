b061e2504be391db1782c0d611b4c6f3293cb288
martha <medwards@greenriver.org>
2025-03-27 08:59:07 -0400

    Demonstrate conditional enrollment creation

--- drivers/hmis/spec/models/hmis/ce/referral_workflow_side_effects_spec.rb
+++ drivers/hmis/spec/models/hmis/ce/referral_workflow_side_effects_spec.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+###
+# Copyright  - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
 require_relative '../../../support/ce_spec_helper'
 
 RSpec.describe Hmis::Ce::ReferralEnroller, type: :model do
@@ -19,31 +25,31 @@ RSpec.describe Hmis::Ce::ReferralEnroller, type: :model do
     )
   end
 
-  let!(:provider_acceptance_task) do # Modify task set up in 'ce spec helper' to have a side effect that creates an enrollment
-    create(
-      :hmis_workflow_definition_task,
-      template: workflow_template,
-      name: 'Provider Acceptance',
-      swimlane: case_manager_swimlane,
-      trigger_config: [
-        {
-          event: 'complete_step',
-          message: 'create_enrollment',
-        },
-      ],
-    )
-  end
-
   let!(:coc1) { create :hmis_hud_project_coc, data_source: ds1, project: project, coc_code: 'CO-500' }
 
-  before do
-    engine.start_workflow!(user: hmis_user)
-    client_acceptance = engine.active_steps.sole
-    engine.start_step!(client_acceptance, user: hmis_user)
-    engine.complete_step!(client_acceptance, user: hmis_user, submitted_values: {})
-  end
-
   describe 'workflow with side effect that creates an enrollment' do
+    let!(:provider_acceptance_task) do # Modify task set up in 'ce spec helper' to have a side effect that creates an enrollment
+      create(
+        :hmis_workflow_definition_task,
+        template: workflow_template,
+        name: 'Provider Acceptance',
+        swimlane: case_manager_swimlane,
+        trigger_config: [
+          {
+            event: 'complete_step',
+            message: 'create_enrollment',
+          },
+        ],
+      )
+    end
+
+    before do
+      engine.start_workflow!(user: hmis_user)
+      client_acceptance = engine.active_steps.sole
+      engine.start_step!(client_acceptance, user: hmis_user)
+      engine.complete_step!(client_acceptance, user: hmis_user, submitted_values: {})
+    end
+
     it 'creates an enrollment and associates it with the referral' do
       expect do
         current_step = engine.active_steps.sole
@@ -130,68 +136,32 @@ RSpec.describe Hmis::Ce::ReferralEnroller, type: :model do
         expect(enrollment.enrollment_coc).to eq(coc2.coc_code)
       end
     end
-  end
 
-  describe 'workflow with side effect that creates a move-in date' do
-    let!(:move_in_date_form_def) do
-      create(
-        :hmis_form_definition,
-        role: :CE_REFERRAL_STEP,
-        definition: {
-          'item': [
-            {
-              'type': 'GROUP',
-              'link_id': 'q1',
-              'item': [
-                {
-                  'type': 'DATE',
-                  'link_id': 'move_in_date',
-                  'required': true,
-                  'text': 'Move-in Date',
-                  'mapping': { 'field_name': 'moveInDate', 'record_type': 'ENROLLMENT' },
-                },
-              ],
-            },
-          ],
-        },
-      )
-    end
-
-    let!(:provider_acceptance_task) do
-      create(
-        :hmis_workflow_definition_task,
-        template: workflow_template,
-        name: 'Provider Acceptance',
-        swimlane: case_manager_swimlane,
-        form_definition: move_in_date_form_def,
-        trigger_config: [
-          # This one has 2 side effects:
-          { # 1. Create an enrollment
-            event: 'complete_step',
-            message: 'create_enrollment',
+    describe 'workflow with side effect that creates a move-in date' do
+      let!(:move_in_date_form_def) do
+        create(
+          :hmis_form_definition,
+          role: :CE_REFERRAL_STEP,
+          definition: {
+            'item': [
+              {
+                'type': 'GROUP',
+                'link_id': 'q1',
+                'item': [
+                  {
+                    'type': 'DATE',
+                    'link_id': 'move_in_date',
+                    'required': true,
+                    'text': 'Move-in Date',
+                    'mapping': { 'field_name': 'moveInDate', 'record_type': 'ENROLLMENT' },
+                  },
+                ],
+              },
+            ],
           },
-          { # 2. Set move-in date
-            event: 'complete_step',
-            message: 'set_move_in_date',
-          },
-        ],
-      )
-    end
+        )
+      end
 
-    it 'assigns the move-in date attribute on the enrollment' do
-      move_in_date = 2.weeks.ago.to_date
-      expect do
-        current_step = engine.active_steps.sole
-        engine.complete_step!(current_step, user: hmis_user, submitted_values: { 'move_in_date': move_in_date })
-        referral.reload
-      end.to change(Hmis::Hud::Enrollment, :count).by(1).
-        and change(referral, :target_household).from(nil)
-
-      enrollment = referral.target_household.enrollments.sole
-      expect(enrollment.move_in_date).to eq(move_in_date)
-    end
-
-    context 'if enrollment does not exist yet' do
       let!(:provider_acceptance_task) do
         create(
           :hmis_workflow_definition_task,
@@ -200,7 +170,12 @@ RSpec.describe Hmis::Ce::ReferralEnroller, type: :model do
           swimlane: case_manager_swimlane,
           form_definition: move_in_date_form_def,
           trigger_config: [
-            {
+            # This one has 2 side effects:
+            { # 1. Create an enrollment
+              event: 'complete_step',
+              message: 'create_enrollment',
+            },
+            { # 2. Set move-in date
               event: 'complete_step',
               message: 'set_move_in_date',
             },
@@ -208,14 +183,122 @@ RSpec.describe Hmis::Ce::ReferralEnroller, type: :model do
         )
       end
 
-      it 'raises an exception, indicating a workflow configuration issue' do
+      it 'assigns the move-in date attribute on the enrollment' do
+        move_in_date = 2.weeks.ago.to_date
         expect do
           current_step = engine.active_steps.sole
-          engine.complete_step!(current_step, user: hmis_user, submitted_values: { 'move_in_date': 2.weeks.ago.to_date })
+          engine.complete_step!(current_step, user: hmis_user, submitted_values: { 'move_in_date': move_in_date })
           referral.reload
-        end.to raise_error(RuntimeError, /does not have a target household yet/).
-          and not_change(Hmis::Hud::Enrollment, :count)
+        end.to change(Hmis::Hud::Enrollment, :count).by(1).
+          and change(referral, :target_household).from(nil)
+
+        enrollment = referral.target_household.enrollments.sole
+        expect(enrollment.move_in_date).to eq(move_in_date)
       end
+
+      context 'if enrollment does not exist yet' do
+        let!(:provider_acceptance_task) do
+          create(
+            :hmis_workflow_definition_task,
+            template: workflow_template,
+            name: 'Provider Acceptance',
+            swimlane: case_manager_swimlane,
+            form_definition: move_in_date_form_def,
+            trigger_config: [
+              {
+                event: 'complete_step',
+                message: 'set_move_in_date',
+              },
+            ],
+          )
+        end
+
+        it 'raises an exception, indicating a workflow configuration issue' do
+          expect do
+            current_step = engine.active_steps.sole
+            engine.complete_step!(current_step, user: hmis_user, submitted_values: { 'move_in_date': 2.weeks.ago.to_date })
+            referral.reload
+          end.to raise_error(RuntimeError, /does not have a target household yet/).
+            and not_change(Hmis::Hud::Enrollment, :count)
+        end
+      end
+    end
+  end
+
+  # Demonstrating the case where enrollment is created conditionally based on a gateway,
+  # and is associated with a workflow end event (not with a step that has a form).
+  describe 'workflow where "acceptance" event creates an enrollment' do
+    let(:accept_referral) do
+      create(
+        :hmis_workflow_definition_end_event,
+        template: workflow_template,
+        name: 'accept referral',
+        trigger_config: [
+          {
+            event: 'end_workflow',
+            message: 'accept_referral',
+          },
+          {
+            event: 'end_workflow',
+            message: 'create_enrollment',
+          },
+        ],
+      )
+    end
+
+    let(:reject_referral) do
+      create(
+        :hmis_workflow_definition_end_event,
+        template: workflow_template,
+        name: 'reject referral',
+        trigger_config: [
+          {
+            event: 'end_workflow',
+            message: 'reject_referral',
+          },
+        ],
+      )
+    end
+
+    let(:gateway) do
+      create(
+        :hmis_workflow_definition_gateway,
+        template: workflow_template,
+        gateway_type: 'exclusive',
+        name: 'conditional gw',
+      )
+    end
+
+    before do
+      client_acceptance_task.outflows.destroy_all
+      client_acceptance_task.connect_to!(gateway)
+      gateway.connect_to!(accept_referral, condition: 'client_accepts = 1')
+      gateway.connect_to!(reject_referral, condition: 'client_accepts = 0')
+
+      engine.start_workflow!(user: hmis_user)
+      engine.start_step!(engine.active_steps.sole, user: hmis_user)
+    end
+
+    it 'creates the enrollment if client accepts' do
+      expect do
+        engine.complete_step!(engine.active_steps.sole, user: hmis_user, submitted_values: { 'client_accepts': 1 })
+        referral.reload
+      end.to change(Hmis::Hud::Enrollment, :count).by(1).
+        and change(referral, :target_household).from(nil)
+
+      enrollment = referral.target_household.enrollments.sole
+      expect(enrollment.project).to eq(project)
+      expect(enrollment.client).to eq(client)
+      expect(enrollment.in_progress?).to be_truthy
+      expect(enrollment.relationship_to_hoh).to eq(1)
+    end
+
+    it 'does not create the enrollment if client does not accept' do
+      expect do
+        engine.complete_step!(engine.active_steps.sole, user: hmis_user, submitted_values: { 'client_accepts': 0 })
+        referral.reload
+      end.to not_change(Hmis::Hud::Enrollment, :count).
+        and not_change(referral, :target_household).from(nil)
     end
   end
 end
