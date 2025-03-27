7b87a075e61ea1eae16fa6c07a824b6e4ed464fc
Gig Ashton <gig@greenriver.org>
2025-03-27 18:15:15 -0400

    Move HMIS report data logic to class

--- drivers/user_permission_report/app/controllers/user_permission_report/warehouse_reports/reports_controller.rb
+++ drivers/user_permission_report/app/controllers/user_permission_report/warehouse_reports/reports_controller.rb
@@ -15,20 +15,23 @@ module UserPermissionReport::WarehouseReports
         order(:last_name, :first_name).
         includes(:roles, access_groups: @group_associations.keys)
 
-      @include_hmis_report = HmisEnforcement.hmis_enabled? &&
-        current_user.as_hmis_user.can_audit_users? # can audit HMIS users
-
       respond_to do |format|
         format.html do
           @users = @users.text_search(params[:q]) if params[:q].present?
           @pagy, @users = pagy(@users)
         end
         format.xlsx do
+          @hmis_data = report_class.new(current_user).hmis_data
+
           date = Date.current.strftime('%Y-%m-%d')
           filename = "user-permissions-#{date}.xlsx"
           headers['Content-Disposition'] = "attachment; filename=#{filename}"
         end
       end
+    end
+
+    private def report_class
+      ::UserPermissionReport::Report
     end
 
     private def set_group_associations
@@ -46,46 +49,5 @@ module UserPermissionReport::WarehouseReports
       }
     end
     helper_method :associated_items
-
-    def hmis_access_report_rows
-      hmis_users = Hmis::User.order(:last_name, :first_name).
-        includes(access_controls: [:access_group, :user_group, :role])
-
-      warehouse_users = User.all.index_by(&:id)
-
-      hmis_users.map do |user|
-        next unless user.access_controls.any? # non-HMIS user
-
-        # One row per Access Control that this User belongs to
-        user.access_controls.map do |access_control|
-          entities = hmis_collection_entities(access_control.access_group_id)
-
-          {
-            name: user.name,
-            email: user.email,
-            user_id: user.id,
-            status: warehouse_users[user.id].overall_status(current_user).join('; '),
-            # TODO(#7486) include last sign in date for HMIS specifically
-            # last_hmis_login: user.last_hmis_login&.to_time&.to_fs(:db),
-            role_name: access_control.role.name,
-            collection_name: access_control.access_group.name,
-            inherited_from_user_group: access_control.user_group.name,
-            role_permissions: access_control.role.granted_permissions.map { |perm| perm.to_s.humanize }.join(', '),
-            data_sources_in_collection: entities[:data_sources].join("\n").presence,
-            organizations_in_collection: entities[:organizations].join("\n").presence,
-            projects_in_collection: entities[:projects].join("\n").presence,
-          }
-        end
-      end.compact.flatten(1)
-    end
-    helper_method :hmis_access_report_rows
-
-    private def hmis_collection_entities(collection_id)
-      # { collection_id => { data_sources: [names of data sources], organizations: [names of orgs], projects: [names of projects] } }
-      @entities_by_collection_id ||= Hmis::AccessGroup.includes(:data_sources, :organizations, :projects).
-        map { |ag| [ag.id, ag.entity_names] }.to_h
-
-      @entities_by_collection_id[collection_id]
-    end
   end
 end
