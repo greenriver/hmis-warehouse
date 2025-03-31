573edf29976a8893ff8c4ed95fb3de05b6692deb
martha <medwards@greenriver.org>
2025-03-31 10:19:31 -0400

    Add missing frozen string literal

--- db/warehouse/migrate/20250331121904_add_reversible_to_wfe_step.rb
+++ db/warehouse/migrate/20250331121904_add_reversible_to_wfe_step.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class AddReversibleToWfeStep < ActiveRecord::Migration[7.0]
   def change
     add_column :wfe_steps, :reversible, :boolean, null: false, default: true
