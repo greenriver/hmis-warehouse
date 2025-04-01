97bdcf190a566241ba4f9a3beae9043a92600df0
martha <medwards@greenriver.org>
2025-04-01 08:35:49 -0400

    try removing eager loading

--- drivers/hmis/config/initializers/strict_attributes.rb
+++ drivers/hmis/config/initializers/strict_attributes.rb
@@ -9,7 +9,7 @@
 # Next tried this hook without eager loading. strict_attributes! is never called because table_exists? is not yet true
 Rails.application.config.after_initialize do
   # forcing the whole application to eager load is probably not allowed though, is there any other way
-  Rails.application.eager_load! unless Rails.configuration.eager_load
+  # Rails.application.eager_load! unless Rails.configuration.eager_load
 
   Hmis::Hud::Base.descendants.each do |model|
     model.strict_attributes! if model.table_exists?
