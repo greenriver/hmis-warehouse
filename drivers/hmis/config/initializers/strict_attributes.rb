6ba61e928c84ce69da4cfe8528599352f8e74784
martha <medwards@greenriver.org>
2025-04-01 14:22:57 -0400

    comment out

--- drivers/hmis/config/initializers/strict_attributes.rb
+++ drivers/hmis/config/initializers/strict_attributes.rb
@@ -6,28 +6,29 @@
 
 # frozen_string_literal: true
 
-# if changing this code and then testing it using rspec, need to DISABLE_SPRING=1
-Rails.application.config.to_prepare do
-  # without eager_load, Hmis::Hud::Base.descendants == [].
-  Rails.application.eager_load!
-
-  Hmis::Hud::Base.descendants.each do |model|
-    if model.table_exists?
-      model.columns.each do |column|
-        case column.type
-        when :integer
-          model.class_eval do
-            attribute column.name, Hmis::StrictInteger.new
-          end
-        when :decimal
-          model.class_eval do
-            attribute column.name, Hmis::StrictDecimal.new
-          end
-        end
-      end
-    else # rubocop:disable Style/EmptyElse
-      # everybody is loaded at this point, the breakpoint never hits.
-      # binding.pry
-    end
-  end
-end
+# trying commenting this whole thing out to see if tests succeed
+# # if changing this code and then testing it using rspec, need to DISABLE_SPRING=1
+# Rails.application.config.to_prepare do
+#   # without eager_load, Hmis::Hud::Base.descendants == [].
+#   # Rails.application.eager_load!
+#
+#   Hmis::Hud::Base.descendants.each do |model|
+#     if model.table_exists?
+#       model.columns.each do |column|
+#         case column.type
+#         when :integer
+#           model.class_eval do
+#             attribute column.name, Hmis::StrictInteger.new
+#           end
+#         when :decimal
+#           model.class_eval do
+#             attribute column.name, Hmis::StrictDecimal.new
+#           end
+#         end
+#       end
+#     else
+#       # everybody is loaded at this point, the breakpoint never hits.
+#       # binding.pry
+#     end
+#   end
+# end
