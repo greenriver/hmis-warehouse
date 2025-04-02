1cfb17a9872ec976af956afdc1515a2070a2f910
Dave G <dgreiner@greenriver.org>
2025-04-02 11:58:45 -0400

    Split up Active Client Report pdf render into batches to prevent issues with large tables/files causing issues in grover/puppeteer

--- app/models/grda_warehouse/warehouse_reports/document_exports/active_client_report_export.rb
+++ app/models/grda_warehouse/warehouse_reports/document_exports/active_client_report_export.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 module GrdaWarehouse::WarehouseReports::DocumentExports
   class ActiveClientReportExport < ::GrdaWarehouse::DocumentExport
     include ApplicationHelper
@@ -25,9 +27,11 @@ module GrdaWarehouse::WarehouseReports::DocumentExports
 
     def perform
       with_status_progression do
+        pdf = []
         template_file = 'warehouse_reports/client_details/actives/index_pdf'
         layout = 'layouts/open_path_logo_report'
 
+        # Render the first page of the report
         html = PdfGenerator.html(
           controller: controller_class,
           template: template_file,
@@ -35,9 +39,27 @@ module GrdaWarehouse::WarehouseReports::DocumentExports
           user: user,
           assigns: view_assigns,
         )
+        pdf << PdfGenerator.merge_inline_pdfs(PdfGenerator.render_pdf(html))
+
+        # Split up the report table into batches to prevent issues when rendering extremely large tables in a single PDF
+        @report.enrollment_scope.preload(:client, :project, :enrollment).in_batches(of: 5_000).each do |batch|
+          html = PdfGenerator.html(
+            controller: controller_class,
+            layout: layout,
+            template: 'warehouse_reports/client_details/actives/_pdf_table',
+            user: user,
+            assigns: view_assigns.deep_merge({
+                                               batch: batch,
+                                             }),
+          )
+          pdf << PdfGenerator.merge_inline_pdfs(PdfGenerator.render_pdf(html))
+        end
+
+        # Merge all the generated pdf files into a single PDF
         PdfGenerator.new.perform(
-          html: html,
+          html: '',
           file_name: "Active Clients #{DateTime.current.to_fs(:db)}",
+          pdf_data: PdfGenerator.merge_inline_pdfs(pdf),
         ) do |io|
           self.pdf_file = io
         end
