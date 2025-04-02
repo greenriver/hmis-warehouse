4fe650da228c8f1083dc1bd56ad6054d313a4b14
Elliot Anders <elliot@greenriver.org>
2025-04-02 11:50:10 -0400

    Rubocop

--- db/warehouse/migrate/20250331175933_create_cas_analytics_intermediate_tables.rb
+++ db/warehouse/migrate/20250331175933_create_cas_analytics_intermediate_tables.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 class CreateCasAnalyticsIntermediateTables < ActiveRecord::Migration[7.0]
--- db/warehouse/migrate/20250402130025_create_analytics_ce_opportunity_categories.rb
+++ db/warehouse/migrate/20250402130025_create_analytics_ce_opportunity_categories.rb
@@ -1,10 +1,8 @@
-
 # frozen_string_literal: true
 
 class CreateAnalyticsCeOpportunityCategories < ActiveRecord::Migration[7.0]
   def change
     create_view 'analytics.ce_opportunity_categories'
-
     create_view 'analytics.ce_opportunities'
     create_view 'analytics.ce_clients'
     create_view 'analytics.ce_workflow_contacts'
@@ -12,6 +10,5 @@ class CreateAnalyticsCeOpportunityCategories < ActiveRecord::Migration[7.0]
     create_view 'analytics.ce_workflows'
     create_view 'analytics.ce_steps'
     create_view 'analytics.ce_workflow_users'
-
   end
 end
