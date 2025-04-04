8aa72b87976aa5714e83d97e05b833f6b5ce064f
Elliot Anders <elliot@greenriver.org>
2025-04-04 10:39:43 -0400

    Rubocop

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,12 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https: //github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+
 require 'dotenv'
 require 'active_support/core_ext/object/blank'
 Dotenv.load('.env', '.env.local')
