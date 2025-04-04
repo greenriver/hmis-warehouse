2b7e445f6113a6c88f87208a2af45316720bf0ab
Elliot Anders <elliot@greenriver.org>
2025-04-04 08:45:43 -0400

    Rubocop

--- db/warehouse/migrate/20250403232619_create_analytics_client_files.rb
+++ db/warehouse/migrate/20250403232619_create_analytics_client_files.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateAnalyticsClientFiles < ActiveRecord::Migration[7.0]
   def change
     add_column :files, :url, :string
 
-    create_view "analytics.client_files"
-    create_view "analytics.file_tags"
+    create_view 'analytics.client_files'
+    create_view 'analytics.file_tags'
   end
 end
