96488bd89432427448f868917e40c6283b0dc9df
martha <medwards@greenriver.org>
2025-04-04 09:24:45 -0400

    Fix table names and clean unused

--- drivers/hmis/app/models/hmis/hud/concerns/with_strict_attributes.rb
+++ drivers/hmis/app/models/hmis/hud/concerns/with_strict_attributes.rb
@@ -10,23 +10,15 @@ module Hmis::Hud::Concerns::WithStrictAttributes
   extend ActiveSupport::Concern
 
   included do
-    if table_exists?
-      columns.each do |column|
-        case column.type
-        when :integer
-          attribute column.name, Hmis::StrictInteger.new
-        when :decimal
-          attribute column.name, Hmis::StrictDecimal.new
-        end
+    raise "#{self} cannot include Hmis::Hud::Concerns::WithStrictAttributes because the table #{table_name} does not exist. Try moving `self.table_name` assignment to the top of the file." unless table_exists?
+
+    columns.each do |column|
+      case column.type
+      when :integer
+        attribute column.name, Hmis::StrictInteger.new
+      when :decimal
+        attribute column.name, Hmis::StrictDecimal.new
       end
-    else # rubocop:disable Style/EmptyElse
-      # Again, not all models have the table loaded yet. But I'm baffled that it's not the same subset that you see
-      # when calling self.inherited in Hmis::Hud::Base. If you set a breakpoint here, the following models hit:
-      # Hmis::Hud::Project
-      # Hmis::Hud::User
-      # Hmis::Hud::Enrollment
-      # Hmis::Hud::EmploymentEducation
-      # binding.pry
     end
   end
 end
