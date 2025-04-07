6b632eaaa056294ba06abab18800165060cf9013
Elliot Anders <elliot@greenriver.org>
2025-04-07 13:29:33 -0400

    Rubocop fixes

--- db/warehouse/migrate/20250407165234_update_analytics_clients_to_version_3.rb
+++ db/warehouse/migrate/20250407165234_update_analytics_clients_to_version_3.rb
@@ -1,5 +1,13 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https: //github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class UpdateAnalyticsClientsToVersion3 < ActiveRecord::Migration[7.0]
   def change
-    update_view "analytics.clients", version: 3, revert_to_version: 2
+    update_view 'analytics.clients', version: 3, revert_to_version: 2
   end
 end
