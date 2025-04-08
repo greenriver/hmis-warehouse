f9d890ee1f7e4a910dd7247d063b98c13eaa54cd
Gig Ashton <gig@greenriver.org>
2025-04-08 09:38:03 -0400

    Add form definition id to analytics_client_geolocations view

---
+++ db/migrate/20250408133728_update_analytics_client_geolocations_to_version_2.rb
@@ -0,0 +1,5 @@
+class UpdateAnalyticsClientGeolocationsToVersion2 < ActiveRecord::Migration[7.0]
+  def change
+    replace_view :analytics_client_geolocations, version: 2, revert_to_version: 1
+  end
+end
