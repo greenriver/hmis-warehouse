b69ed6bedb48c56c73438d47fdf033bde4814e1d
Dave G <dgreiner@greenriver.org>
2025-04-16 09:53:06 -0400

    rubocop

--- db/warehouse/migrate/20250409145018_populate_cohort_column_table.rb
+++ db/warehouse/migrate/20250409145018_populate_cohort_column_table.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PopulateCohortColumnTable < ActiveRecord::Migration[7.0]
-  def available_columns # rubocop:disable Metrics/AbcSize
+  def available_columns
     [
       'CohortColumns::LastName',
       'CohortColumns::FirstName',
