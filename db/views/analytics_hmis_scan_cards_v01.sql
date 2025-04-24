8eab822a355f0cf51aca3b43b5240fc84f7bd2d8
Gig Ashton <gig@greenriver.org>
2025-04-24 15:27:16 -0400

    Add analytics view for scan card codes

---
+++ db/views/analytics_hmis_scan_cards_v01.sql
@@ -0,0 +1,3 @@
+SELECT * FROM "hmis_scan_card_codes"
+
+-- Note: intentionally including deleted scan card codes to report on full history of scan card creation/deletion
---
+++ db/warehouse/migrate/20250424192101_create_analytics_hmis_scan_cards.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class CreateAnalyticsHmisScanCards < ActiveRecord::Migration[7.0]
+  def change
+    create_view "analytics.hmis_scan_cards"
+  end
+end
