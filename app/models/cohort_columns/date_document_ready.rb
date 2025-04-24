49a04545e957567176259aa6d37a2f887f7c5674
Dave G <149399758+dtgreiner@users.noreply.github.com>
2025-04-24 08:45:54 -0400

    add active flag to cohort columns (#5299)
    * add active flag to cohort columns
    
    * rubocop
    
    * rubocop & other cleanup
    
    * rubocop
    
    * rubocop
    
    * update for tests
    
    * fix some tests
    
    * fix more tests
    
    * tests
    
    * fix more tests relying on cohort column types
    
    * update class name & other code review changes
    
    * update factory file name
    
    * move cohort_column to method and clean up test infrastructure
    
    * update column_state test & remove population migration
    
    * move cohort column spec maintatin to before all
    
    * missed db structure change after merge
    
    * Update app/models/grda_warehouse/cohorts/cohort_column.rb
    
    Co-authored-by: Elliot <elliot@greenriver.org>
    
    ---------
    
    Co-authored-by: Elliot <elliot@greenriver.org>

--- app/models/cohort_columns/date_document_ready.rb
+++ app/models/cohort_columns/date_document_ready.rb
@@ -4,12 +4,14 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 module CohortColumns
   class DateDocumentReady < CohortDate
     attribute :column, String, lazy: true, default: :document_ready_on
     attribute :translation_key, String, lazy: true, default: 'Date Document Ready'
     attribute :title, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.translation_key) }
-    attribute :description_translation_key, String, lazy: true, default: Translation.translate('Manually entered date at which the client became document ready')
+    attribute :description_translation_key, String, lazy: true, default: 'Manually entered date at which the client became document ready'
     attribute :description, String, lazy: true, default: ->(model, _attr) { Translation.translate(model.description_translation_key) }
 
     def available_for_rules?
