e54f51e0871730a97c8083b8f7c77ededbe91e58
Theron Toomey <ttoomey@greenriver.org>
2025-04-26 14:30:32 +0000

    improve migration

--- db/warehouse/migrate/20250426023314_set_replica_for_analytics_app_users.rb
+++ db/warehouse/migrate/20250426023314_set_replica_for_analytics_app_users.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 class SetReplicaForAnalyticsAppUsers < ActiveRecord::Migration[7.0]
-  def change
+  disable_ddl_transaction!
+  def up
     safety_assured do
       execute('ALTER TABLE analytics.app_users REPLICA IDENTITY FULL')
+      result = execute('SELECT relreplident FROM pg_class WHERE oid = \'analytics.app_users\'::regclass')
+      puts "Current replica identity: #{result.first['relreplident']}"
+    end
+  end
+
+  def down
+    safety_assured do
+      execute('ALTER TABLE analytics.app_users REPLICA IDENTITY DEFAULT')
     end
   end
 end
