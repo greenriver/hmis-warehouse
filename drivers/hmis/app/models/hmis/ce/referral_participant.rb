606f2dabe9de6b7d33ad29edba7e337a3bde195f
martha <medwards@greenriver.org>
2025-04-30 08:14:51 -0400

    Add comments

--- drivers/hmis/app/models/hmis/ce/referral_participant.rb
+++ drivers/hmis/app/models/hmis/ce/referral_participant.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-# A role/user who is participating in the referral process
+# A role/user who is participating in the referral process.
+# What is the difference between a ReferralParticipant and a StepAssignment?
+# - A ReferralParticipant represents the general, not-step-specific concept of a "contact" on this referral. It says,
+#   "Whenever a task that's for Swimlane X becomes available on this referral, assign it to User A and User B".
+#   ReferralParticipants can be created by users from the frontend, or they can be created at referral creation time
+#   based on some defaults for the referral context.
+# - A StepAssignment represents the more specific concept of the user who is assigned to this particular task.
+#   StepAssignments are created by the workflow engine when a task becomes available, based on the ReferralParticipants
+#   for this step's Swimlane. A StepAssignment is also created when a user starts a task, even if they aren't a
+#   participant in this swimlane.
 module Hmis::Ce
   class ReferralParticipant < GrdaWarehouseBase
     belongs_to :referral, class_name: 'Hmis::Ce::Referral'
