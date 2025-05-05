019362d8aec31da97698c8125d2be8c187401223
Theron Toomey <ttoomey@greenriver.org>
2025-05-05 03:07:33 +0000

    fix test

--- spec/models/application_notifier_spec.rb
+++ spec/models/application_notifier_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 require 'connection_pool'
 require 'redis'
+require 'webmock/rspec'
 
 RSpec.describe ApplicationNotifier do
   let(:url) { 'https://hooks.slack.com/services/test' }
@@ -10,8 +11,10 @@ RSpec.describe ApplicationNotifier do
   describe '#ping' do
     it 'logs and posts message' do
       notifier = described_class.new(url)
+      stub_request(:post, url)
       expect(Rails.logger).to receive(:info).with('msg')
       notifier.ping('msg')
+      expect(WebMock).to have_requested(:post, url)
     end
   end
 end
