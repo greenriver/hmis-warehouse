0a5c6cca6b5f11ef733d29db0010f9d9d00c7591
martha <medwards@greenriver.org>
2025-05-08 15:50:10 -0400

    Add permission guards for starting step

--- drivers/hmis/app/graphql/mutations/ce/start_ce_referral_step.rb
+++ drivers/hmis/app/graphql/mutations/ce/start_ce_referral_step.rb
@@ -16,12 +16,15 @@ module Mutations
       raise unless Hmis::Ce.configuration.enabled?
 
       referral = Hmis::Ce::Referral.viewable_by(current_user).find(referral_id)
+      project = referral.opportunity.project
       step = nil
       referral.opportunity.with_lock do
         engine = referral.workflow_engine
         step = engine.active_steps.find(step_id)
 
-        # TODO(#7506): permission
+        project_perm = current_permission?(permission: :can_perform_any_referral_tasks, entity: project)
+        assignment_perm = current_permission?(permission: :can_perform_own_referral_tasks, entity: project) && step.assignments.any? { |assignment| assignment.user == current_user }
+        access_denied! unless project_perm || assignment_perm
 
         engine.start_step!(step, user: current_user)
       end
