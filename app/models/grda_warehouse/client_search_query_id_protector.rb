eced78c517f9fd1a6449608bde67a0b30dc75820
Theron Toomey <ttoomey@greenriver.org>
2025-05-09 23:08:07 +0000

    fixes

--- app/models/grda_warehouse/client_search_query_id_protector.rb
+++ app/models/grda_warehouse/client_search_query_id_protector.rb
@@ -6,21 +6,20 @@ module GrdaWarehouse
 
     def encrypt(text)
       # text should be a fingerprint digest
-      raise "Input must contain only alphanumeric characters" unless text.match?(/\A[a-zA-Z0-9]+\z/)
+      raise 'Input must contain only alphanumeric characters' unless text.match?(/\A[a-zA-Z0-9]+\z/)
 
-      salt = random_salt
-      encryptor = build_encryptor(salt)
+      encryptor = build_encryptor
       encrypted = encryptor.encrypt_and_sign(text)
-      payload = "#{salt}:#{encrypted}"
+      payload = encrypted
       Base64.urlsafe_encode64(payload, padding: false)
     end
 
     def decrypt(encrypted_text)
       decoded = Base64.urlsafe_decode64(encrypted_text)
-      salt, encrypted = decoded.split(':', 2)
-      return nil unless salt && encrypted
+      encrypted = decoded
+      return nil unless encrypted
 
-      encryptor = build_encryptor(salt)
+      encryptor = build_encryptor
       encryptor.decrypt_and_verify(encrypted)
     rescue StandardError
       nil
@@ -28,24 +27,22 @@ module GrdaWarehouse
 
     private
 
-    def random_salt
-      SecureRandom.hex(8) # 16 chars of salt
-    end
-
     def key
-      prop = AppConfigProperty.find_by(key: 'client_search/key')
+      prop_name = 'client_search/aes-128-gcm/key'
+      prop = AppConfigProperty.find_by(key: prop_name)
       return prop.value if prop
 
       # If not found, create with upsert
-      random_key = SecureRandom.hex(16)
-      AppConfigProperty.upsert({ key: 'client_search/key', value: random_key }, unique_by: :key)
-      AppConfigProperty.find_by(key: 'client_search/key').value
+      random_key = SecureRandom.random_bytes(16)
+      serialized_key = Base64.strict_encode64(random_key)
+
+      AppConfigProperty.upsert({ key: prop_name, value: serialized_key }, unique_by: :key)
+      AppConfigProperty.find_by(key: prop_name).value
     end
 
-    def build_encryptor(salt)
-      raw_key = [key].pack('H*') # Convert hex string to binary
-      derived_key = OpenSSL::HMAC.digest('SHA256', raw_key, salt)[0, 16] # AES-128 requires 16 bytes
-      ActiveSupport::MessageEncryptor.new(derived_key, cipher: 'aes-128-gcm')
+    def build_encryptor
+      key_bin = Base64.strict_decode64(key)
+      ActiveSupport::MessageEncryptor.new(key_bin, cipher: 'aes-128-gcm')
     end
   end
 end
