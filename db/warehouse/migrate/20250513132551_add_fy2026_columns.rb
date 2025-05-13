292100d753dd90cdf726b34d536fce7103e97ed4
Elliot Anders <elliot@greenriver.org>
2025-05-13 17:45:53 -0400

    Make rubocop happy

--- db/warehouse/migrate/20250513132551_add_fy2026_columns.rb
+++ db/warehouse/migrate/20250513132551_add_fy2026_columns.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddFy2026Columns < ActiveRecord::Migration[7.1]
   def change
     add_column :Client, :Sex, :integer
--- db/warehouse/migrate/20250513134455_create_fy2026_loading_and_import_tables.rb
+++ db/warehouse/migrate/20250513134455_create_fy2026_loading_and_import_tables.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 class CreateFy2026LoadingAndImportTables < ActiveRecord::Migration[7.1]
