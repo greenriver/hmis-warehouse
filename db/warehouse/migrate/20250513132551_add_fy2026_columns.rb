21b35c81cb459fce806322775cfa40f8161ba1a2
Elliot Anders <elliot@greenriver.org>
2025-05-16 17:11:58 -0400

    Rubocop fixes

--- db/warehouse/migrate/20250513132551_add_fy2026_columns.rb
+++ db/warehouse/migrate/20250513132551_add_fy2026_columns.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class AddFy2026Columns < ActiveRecord::Migration[7.1]
   def change
     add_column :Client, :Sex, :integer
--- db/warehouse/migrate/20250516193728_update_aggregated_enrollment_for_fy2026.rb
+++ db/warehouse/migrate/20250516193728_update_aggregated_enrollment_for_fy2026.rb
@@ -1,3 +1,6 @@
+
+# frozen_string_literal: true
+
 class UpdateAggregatedEnrollmentForFy2026 < ActiveRecord::Migration[7.1]
   def change
     add_column :hmis_aggregated_enrollments, :MentalHealthConsultation, :integer
