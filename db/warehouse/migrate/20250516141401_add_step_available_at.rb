9986cdb6037748f720c8c98345d650c1bf308e71
martha <medwards@greenriver.org>
2025-05-16 15:14:39 -0400

    Add missing frozen string literal

--- db/warehouse/migrate/20250516141401_add_step_available_at.rb
+++ db/warehouse/migrate/20250516141401_add_step_available_at.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class AddStepAvailableAt < ActiveRecord::Migration[7.1]
   def up
     add_column :wfe_steps, :available_at, :datetime, null: true
