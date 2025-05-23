795b7067972b014fe00ee16a0ddb9f6c3e5690e6
Elliot Anders <elliot@greenriver.org>
2025-05-23 15:45:41 -0400

    Rubocop; again

--- db/migrate/20250523175114_remove_coc_codes_from_collections.rb
+++ db/migrate/20250523175114_remove_coc_codes_from_collections.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveCoCCodesFromCollections < ActiveRecord::Migration[7.1]
   def change
     safety_assured { remove_column :collections, :coc_codes, :jsonb }
