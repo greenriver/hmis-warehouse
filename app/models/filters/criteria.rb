1e2dfc9939dc0100bdd82aae0d038b39814438f7
Elliot Anders <elliot@greenriver.org>
2025-06-04 15:29:14 -0400

    Convert criteria definitions to a hash to prevent accidental duplication in the future

--- app/models/filters/criteria.rb
+++ app/models/filters/criteria.rb
@@ -62,54 +62,46 @@ module Filters::Criteria
   # - class_name: String name of the criterion class
   #
   # @api private
-  DEFINITIONS = [
-    { tags: [:hud, :warehouse, :project], id: :filter_for_user_access },
-    { tags: [:hud], id: :filter_for_projects_hud },
-    { tags: [:hud], id: :filter_for_project_cocs },
-    { tags: [:hud], id: :filter_for_veteran_status },
-    { tags: [:hud, :warehouse, :client], id: :filter_for_household_type },
-    { tags: [:hud, :warehouse, :client], id: :filter_for_head_of_household },
-    { tags: [:hud, :warehouse, :client], id: :filter_for_age },
-    { tags: [:hud, :warehouse, :client], id: :filter_for_gender },
-    { tags: [:hud, :warehouse, :client], id: :filter_for_race },
-    { tags: [:hud], id: :filter_for_sub_population },
-    { tags: [:hud], id: :filter_for_enrollment_cocs },
-
-    { tags: [:warehouse, :project], id: :filter_for_range },
-    { tags: [:warehouse, :project], id: :filter_for_cocs },
-    { tags: [:warehouse, :project], id: :filter_for_project_type },
-    { tags: [:warehouse, :project], id: :filter_for_projects },
-    { tags: [:warehouse, :project], id: :filter_for_funders },
-    { tags: [:warehouse, :project], id: :filter_for_data_sources },
-    { tags: [:warehouse, :project], id: :filter_for_organizations },
-
-    { tags: [:warehouse, :client], id: :filter_for_veteran_status },
-    { tags: [:warehouse, :client], id: :filter_for_sub_population },
-    { tags: [:warehouse, :client], id: :filter_for_prior_living_situation },
-    { tags: [:warehouse, :client], id: :filter_for_destination },
-    { tags: [:warehouse, :client], id: :filter_for_disabilities },
-    { tags: [:warehouse, :client], id: :filter_for_indefinite_disabilities },
-    { tags: [:warehouse, :client], id: :filter_for_dv_status },
-    { tags: [:warehouse, :client], id: :filter_for_dv_currently_fleeing },
-    { tags: [:warehouse, :client], id: :filter_for_chronic_at_entry },
-    { tags: [:warehouse, :client], id: :filter_for_chronic_status },
-    { tags: [:warehouse, :client], id: :filter_for_rrh_move_in },
-    { tags: [:warehouse, :client], id: :filter_for_psh_move_in },
-    { tags: [:warehouse, :client], id: :filter_for_first_time_homeless_in_past_two_years },
-    { tags: [:warehouse, :client], id: :filter_for_returned_to_homelessness_from_permanent_destination },
-    { tags: [:warehouse, :client], id: :filter_for_ce_homeless },
-    { tags: [:warehouse, :client], id: :filter_for_ce_cls_homeless },
-    { tags: [:warehouse, :client], id: :filter_for_cohorts },
-    { tags: [:warehouse, :client], id: :filter_for_active_roi },
-    { tags: [:warehouse, :client], id: :filter_for_times_homeless },
-    { tags: [:warehouse, :client], id: :filter_for_days_since_contact },
-    { tags: [], id: :filter_for_race_ethnicity_combinations },
-  ].map { |dfn| dfn.merge({ class_name: "Filters::Criteria::#{dfn[:id].to_s.camelize}" }) }.
-    index_by { |h| h[:id] }.
-    freeze
+  DEFINITIONS = {
+    filter_for_user_access: { tags: [:hud, :warehouse, :project] },
+    filter_for_projects_hud: { tags: [:hud] },
+    filter_for_project_cocs: { tags: [:hud] },
+    filter_for_veteran_status: { tags: [:hud, :warehouse, :client] },
+    filter_for_household_type: { tags: [:hud, :warehouse, :client] },
+    filter_for_head_of_household: { tags: [:hud, :warehouse, :client] },
+    filter_for_age: { tags: [:hud, :warehouse, :client] },
+    filter_for_gender: { tags: [:hud, :warehouse, :client] },
+    filter_for_race: { tags: [:hud, :warehouse, :client] },
+    filter_for_sub_population: { tags: [:hud, :warehouse, :client] },
+    filter_for_enrollment_cocs: { tags: [:hud] },
+    filter_for_range: { tags: [:warehouse, :project] },
+    filter_for_cocs: { tags: [:warehouse, :project] },
+    filter_for_project_type: { tags: [:warehouse, :project] },
+    filter_for_projects: { tags: [:warehouse, :project] },
+    filter_for_funders: { tags: [:warehouse, :project] },
+    filter_for_data_sources: { tags: [:warehouse, :project] },
+    filter_for_organizations: { tags: [:warehouse, :project] },
+    filter_for_prior_living_situation: { tags: [:warehouse, :client] },
+    filter_for_destination: { tags: [:warehouse, :client] },
+    filter_for_disabilities: { tags: [:warehouse, :client] },
+    filter_for_indefinite_disabilities: { tags: [:warehouse, :client] },
+    filter_for_dv_status: { tags: [:warehouse, :client] },
+    filter_for_dv_currently_fleeing: { tags: [:warehouse, :client] },
+    filter_for_chronic_at_entry: { tags: [:warehouse, :client] },
+    filter_for_chronic_status: { tags: [:warehouse, :client] },
+    filter_for_rrh_move_in: { tags: [:warehouse, :client] },
+    filter_for_psh_move_in: { tags: [:warehouse, :client] },
+    filter_for_first_time_homeless_in_past_two_years: { tags: [:warehouse, :client] },
+    filter_for_returned_to_homelessness_from_permanent_destination: { tags: [:warehouse, :client] },
+    filter_for_ce_homeless: { tags: [:warehouse, :client] },
+    filter_for_ce_cls_homeless: { tags: [:warehouse, :client] },
+    filter_for_cohorts: { tags: [:warehouse, :client] },
+    filter_for_active_roi: { tags: [:warehouse, :client] },
+    filter_for_times_homeless: { tags: [:warehouse, :client] },
+    filter_for_days_since_contact: { tags: [:warehouse, :client] },
+    filter_for_race_ethnicity_combinations: { tags: [] },
+  }.each_with_object({}) { |(key, dfn), result| result[key] = dfn.merge({ id: key, class_name: "Filters::Criteria::#{key.to_s.camelize}" }) }.freeze
 
   # Lookup table mapping class names to criterion IDs
-  #
-  # @api private
   IDS_BY_CLASS = DEFINITIONS.values.to_h { |dfn| dfn.values_at(:class_name, :id) }.freeze
 end
