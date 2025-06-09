5f1b85343de717c0285144923f6336d8a296f84b
Theron Toomey <ttoomey@greenriver.org>
2025-06-09 15:16:05 +0000

    7049-FY2026-hmis-changes

--- drivers/hmis/lib/hmis_util/service_types.rb
+++ drivers/hmis/lib/hmis_util/service_types.rb
@@ -135,7 +135,7 @@ module HmisUtil
               custom_service_category_id: custom_service_category&.id,
               project_type: project_type,
               funder: funder,
-              system: false, # Thesea aren't system records, they can be deleted.
+              system: false, # These aren't system records, they can be deleted.
               active: true,
             ).first_or_create
           end
