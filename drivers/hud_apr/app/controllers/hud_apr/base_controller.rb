e9f75fa570126fb603db7b8102e682cdd7e51d87
Elliot Anders <elliot@greenriver.org>
2025-06-12 10:58:24 -0400

    Allow picking of version until we've made the transition

--- drivers/hud_apr/app/controllers/hud_apr/base_controller.rb
+++ drivers/hud_apr/app/controllers/hud_apr/base_controller.rb
@@ -9,5 +9,18 @@
 module HudApr
   class BaseController < ::HudReports::BaseController
     before_action :filter
+
+    # This method allow picking the version of the report, we only need this during the transition
+    TodoOrDie('Remove active_report_versions once we are on FY 2026', by: '2025-11-01')
+    def active_report_versions
+      return {} if default_report_version == :fy2026 && ! Rails.env.development?
+
+      {
+        fy2024: 'FY 2024',
+        fy2026: 'FY 2026',
+
+      }.invert.freeze
+    end
+    helper_method :active_report_versions
   end
 end
