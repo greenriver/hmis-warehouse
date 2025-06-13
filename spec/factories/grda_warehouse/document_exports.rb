73bbe0988a69512004dd6cd5d728369ea2ebddce
Theron Toomey <ttoomey@greenriver.org>
2025-06-13 23:33:25 +0000

    rubocop

--- spec/factories/grda_warehouse/document_exports.rb
+++ spec/factories/grda_warehouse/document_exports.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :hmis_document_export, class: 'GrdaWarehouse::DocumentExport' do
     association :user
-    type { 'ProjectScorecard::DocumentExports::ScorecardExport' }
+    type { 'ProjectScorecard::DocumentExports::ScorecardExport' } # any valid subclass will do
     status { DocumentExportBehavior::COMPLETED_STATUS }
   end
 end
--- spec/factories/health/document_exports.rb
+++ spec/factories/health/document_exports.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :health_document_export, class: 'Health::DocumentExport' do
     association :user
-    type { 'Health::DocumentExports::AgencyPerformanceExport' }
+    type { 'Health::DocumentExports::AgencyPerformanceExport' } # any valid subclass will do
     status { DocumentExportBehavior::COMPLETED_STATUS }
   end
 end
