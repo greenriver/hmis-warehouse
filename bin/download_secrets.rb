f6cec5fcaf04e3ddac498778f7f16a325ac2f424
Todd Blackman <tdblackman@gmail.com>
2025-06-16 16:41:17 +0000

    Fetch superset params for warehouse

--- bin/download_secrets.rb
+++ bin/download_secrets.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require_relative '../config/deploy/docker/lib/aws_sdk_helpers'
 
 puts AwsSdkHelpers::Helpers.get_secret(ENV.fetch('SECRET_ARN'))
+puts AwsSdkHelpers::Helpers.superset_env_for_warehouse
