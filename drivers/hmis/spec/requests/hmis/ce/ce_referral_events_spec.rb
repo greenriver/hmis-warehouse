9fe6c9bae12e8618e4d91195093d979d3b4e8f4f
martha <medwards@greenriver.org>
2025-06-18 11:02:37 -0400

    Update tests for better maintainability

--- drivers/hmis/spec/requests/hmis/ce/ce_referral_events_spec.rb
+++ drivers/hmis/spec/requests/hmis/ce/ce_referral_events_spec.rb
@@ -9,38 +9,6 @@ RSpec.describe Hmis::GraphqlController, type: :request do
     hmis_login(user)
   end
 
-  # todo @martha - this is some repeated code that maybe points to need for some restructuring
-  # it seems like THIS is the thing that should be provided by ce_referral_helper, but who knows how many tests that will break.
-  let(:acceptance_gateway) do
-    create(
-      :hmis_workflow_definition_gateway,
-      template: workflow_template,
-      gateway_type: 'exclusive',
-      name: 'acceptance gw',
-    )
-  end
-
-  let(:reject_referral) do
-    create(
-      :hmis_workflow_definition_end_event,
-      template: workflow_template,
-      name: 'reject referral',
-      trigger_config: [
-        {
-          event: 'end_workflow',
-          message: 'reject_referral',
-        },
-      ],
-    )
-  end
-
-  before do
-    client_acceptance_task.outflows.destroy_all
-    client_acceptance_task.connect_to!(acceptance_gateway)
-    acceptance_gateway.connect_to!(reject_referral, condition: 'client_accepted = 0')
-    acceptance_gateway.connect_to!(accept_referral, condition: 'client_accepted = 1')
-  end
-
   before do
     referral.workflow_engine.start_workflow!(user: hmis_user)
   end
@@ -85,16 +53,18 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
     context 'when referral is accepted' do
       before do
-        step = referral.workflow_engine.active_steps.first
-        referral.workflow_engine.start_step!(step, user: hmis_user)
-        referral.workflow_engine.complete_step!(step, user: hmis_user, submitted_values: { client_accepted: 1 })
+        2.times do
+          step = referral.workflow_engine.active_steps.first
+          referral.workflow_engine.start_step!(step, user: hmis_user)
+          referral.workflow_engine.complete_step!(step, user: hmis_user, submitted_values: { client_accepted: 1 })
+        end
       end
 
       it 'returns step completion and accept referral events' do
         response, result = post_graphql(**variables) { query }
         expect(response.status).to eq(200), result.inspect
         events = result.dig('data', 'ceReferral', 'events', 'nodes')
-        expect(events.length).to eq(3)
+        expect(events.length).to eq(4)
         expect(events).to match_array(
           [
             a_hash_including(
@@ -102,6 +72,11 @@ RSpec.describe Hmis::GraphqlController, type: :request do
             ),
             a_hash_including(
               'type' => 'Completed Task',
+              'stepName' => 'Provider Acceptance',
+            ),
+            a_hash_including(
+              'type' => 'Completed Task',
+              'stepName' => 'Client Acceptance',
             ),
             a_hash_including(
               'type' => 'Started Referral',
@@ -113,16 +88,18 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
     context 'when referral is rejected' do
       before do
-        step = referral.workflow_engine.active_steps.first
-        referral.workflow_engine.start_step!(step, user: hmis_user)
-        referral.workflow_engine.complete_step!(step, user: hmis_user, submitted_values: { client_accepted: 0 })
+        2.times do
+          step = referral.workflow_engine.active_steps.first
+          referral.workflow_engine.start_step!(step, user: hmis_user)
+          referral.workflow_engine.complete_step!(step, user: hmis_user, submitted_values: { client_accepted: 0 })
+        end
       end
 
       it 'returns step completion and reject referral events' do
         response, result = post_graphql(**variables) { query }
         expect(response.status).to eq(200), result.inspect
         events = result.dig('data', 'ceReferral', 'events', 'nodes')
-        expect(events.length).to eq(3)
+        expect(events.length).to eq(4)
         expected_user = a_hash_including(
           'id' => hmis_user.id.to_s,
           'name' => hmis_user.name.to_s,
@@ -132,6 +109,11 @@ RSpec.describe Hmis::GraphqlController, type: :request do
             a_hash_including(
               'user' => expected_user,
               'type' => 'Declined Referral',
+            ),
+            a_hash_including(
+              'user' => expected_user,
+              'type' => 'Completed Task',
+              'stepName' => 'Provider Acceptance',
             ),
             a_hash_including(
               'user' => expected_user,
