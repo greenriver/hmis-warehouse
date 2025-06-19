81256dc4e9f71b21123cd3b4287c68ab794e8fb9
Elliot Anders <elliot@greenriver.org>
2025-06-19 08:52:42 -0400

    Rubocop

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,10 @@
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin '@popperjs/core', to: 'popper.js', preload: true
