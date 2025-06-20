ed0d246a76957b7c25df1f9df7f66853c72546a8
Dave G <dgreiner@greenriver.org>
2025-06-20 11:57:02 -0400

    rubocop

--- db/migrate/20250618150200_add_can_receive_not_notifications_to_roles.rb
+++ db/migrate/20250618150200_add_can_receive_not_notifications_to_roles.rb
@@ -1,9 +1,11 @@
-
 ###
 # Copyright 2016 - 2025 Green River Data Analysis, LLC
 #
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
+
+# frozen_string_literal: true
+
 class AddCanReceiveNotNotificationsToRoles < ActiveRecord::Migration[7.1]
   def change
     add_column :roles, :can_receive_cohort_note_notifications, :boolean, default: false
