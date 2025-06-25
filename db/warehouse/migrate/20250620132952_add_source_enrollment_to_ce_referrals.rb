d9d1470e23fd60a57f3d398253b05362243b5850
martha <medwards@greenriver.org>
2025-06-25 11:43:16 -0400

    Fix rubocop

--- db/warehouse/migrate/20250620132952_add_source_enrollment_to_ce_referrals.rb
+++ db/warehouse/migrate/20250620132952_add_source_enrollment_to_ce_referrals.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class AddSourceEnrollmentToCeReferrals < ActiveRecord::Migration[7.1]
   def change
     safety_assured do
