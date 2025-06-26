568dc35e506b615546c47bf12d059afd441afe45
Elliot Anders <elliot@greenriver.org>
2025-06-26 17:31:54 -0400

    Additional adjustments for ajax requests and typos

--- drivers/health_comprehensive_assessment/app/controllers/health_comprehensive_assessment/assessments_controller.rb
+++ drivers/health_comprehensive_assessment/app/controllers/health_comprehensive_assessment/assessments_controller.rb
@@ -34,7 +34,11 @@ module HealthComprehensiveAssessment
       @assessment.update(ca_params)
       # Generate a completed QA if the assessment is newly completed, or the completion date was changed
       @patient.qa_factory_factory.complete_ca(@assessment) if @assessment.completed_on.present? && @assessment.completed_on != prior_completion
-      respond_with @assessment, location: client_health_careplans_path(@client) unless request.xhr?
+      if request.xhr?
+        render json: { status: 'success' }
+      else
+        respond_with @assessment, location: client_health_careplans_path(@client)
+      end
     end
 
     def show
