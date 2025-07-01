ed89994f0d568c18aabbd5e1d83fb76f8778fdb6
Gig Ashton <gig@greenriver.org>
2025-07-01 10:50:28 -0400

    Update casing

--- drivers/hmis/spec/models/hmis/ce/match_engine_spec.rb
+++ drivers/hmis/spec/models/hmis/ce/match_engine_spec.rb
@@ -270,7 +270,7 @@ RSpec.describe Hmis::Ce::Match::Engine, type: :model do
     end
 
     describe 'multi-valued CDE inclusion matching' do
-      let(:requirement_expression) { "includes(`cde.custom_assessment.primary_languages`, 'English')" }
+      let(:requirement_expression) { "INCLUDES(`cde.custom_assessment.primary_languages`, 'English')" }
 
       it 'matches clients with the specified language among multiple values' do
         results = generate_candidates(pool, clients)
@@ -279,7 +279,7 @@ RSpec.describe Hmis::Ce::Match::Engine, type: :model do
     end
 
     describe 'multi-valued CDE exclusion matching' do
-      let(:requirement_expression) { "excludes(`cde.custom_assessment.primary_languages`, 'French')" }
+      let(:requirement_expression) { "EXCLUDES(`cde.custom_assessment.primary_languages`, 'French')" }
 
       it 'excludes clients who have the specified language' do
         results = generate_candidates(pool, clients)
