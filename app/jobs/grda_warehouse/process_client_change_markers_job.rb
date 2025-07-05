858e9a4afbe8b7ec1bc30b0347403a2791032146
Theron Toomey <ttoomey@greenriver.org>
2025-07-05 04:23:11 +0000

    7634-candidate-pool-generation

--- app/jobs/grda_warehouse/process_client_change_markers_job.rb
+++ app/jobs/grda_warehouse/process_client_change_markers_job.rb
@@ -7,6 +7,8 @@
 # frozen_string_literal: true
 
 # Processes dirty client records
+# This is an "incremental update" job. It runs frequently on a batch of recently changed clients. It's designed for low latency
+# If it fails to process a batch, it stops processing and reschedules the itself to re-process the same batch of client changes later
 module GrdaWarehouse
   class ProcessClientChangeMarkersJob < BaseJob
     def self.enqueue_if_not_already_running(...)
@@ -19,18 +21,20 @@ module GrdaWarehouse
           dirty_scope = ::GrdaWarehouse::ClientChangeMarker.dirty
           batch = dirty_scope.order(:client_id).limit(limit).where(client_id: cursor_id...)
 
-          # could include other processing tasks here
-          process_ce_eligibility(batch)
+          catch(:failure) do
+            # could include other processing tasks here. Client may be processed multiple times so tasks should be idempotent
+            process_ce_eligibility(batch)
+          end
 
-          # mark the as processed
-          ::GrdaWarehouse::ClientChangeMarker.mark_processed(batch)
-
-          # requeue job for the future with the next batch
-          if wait_time
+          if failure
+            # we couldn't complete all the needed processing, try again
+            Rails.logger.error "#{self.class.name}: #{failure}"
+            schedule_next_batch(cursor_id: cursor_id, wait_time: wait_time)
+          else
+            # we completed the batch, mark the as processed and schedule the next batch
+            ::GrdaWarehouse::ClientChangeMarker.mark_processed(batch)
             next_cursor_id = batch.any? ? (batch.map(&:client_id).max + 1) : 0
-            ::GrdaWarehouse::ProcessClientChangeMarkersJob.
-              set(wait: wait_time).
-              perform_later(cursor_id: next_cursor_id, wait_time: wait_time)
+            schedule_next_batch(cursor_id: next_cursor_id, wait_time: wait_time)
           end
 
           run.complete!
@@ -40,17 +44,27 @@ module GrdaWarehouse
 
     protected
 
+    # schedule job for the next batch
+    def schedule_next_batch(cursor_id: cursor_id, wait_time: wait_time)
+      return unless wait_time
+
+      ::GrdaWarehouse::ProcessClientChangeMarkersJob.
+        set(wait: wait_time).
+        perform_later(cursor_id: cursor_id, wait_time: wait_time)
+    end
+
     # run the hmis CE eligibility
-    def process_ce_eligibility(matches)
-      return if matches.empty?
+    def process_ce_eligibility(markers)
+      return if markers.empty?
       return unless ::HmisEnforcement.hmis_enabled?
       return unless ::Hmis::Ce.configuration.enabled?
 
-      client_scope = ::GrdaWarehouse::Hud::Client.destination.where(id: matches.map(&:client_id))
+      client_scope = ::GrdaWarehouse::Hud::Client.destination.where(id: markers.map(&:client_id))
       candidate_pool_ids = ::Hmis::Ce::Opportunity.active.pluck(:candidate_pool_id).compact.uniq
       candidate_pool_scope = ::Hmis::Ce::Match::CandidatePool.where(id: candidate_pool_ids)
       candidate_pool_scope.find_each do |pool|
-        ::Hmis::Ce::Match::Engine.call(pool, client_scope)
+        completed = Hmis::Ce::Match::Engine.call(pool, client_scope)
+        throw(:failure, "Failed acquire lock for CE candidate pool #{pool.id}") unless completed
       end
     end
 
--- drivers/hmis/app/jobs/hmis/match_candidates_job.rb
+++ drivers/hmis/app/jobs/hmis/match_candidates_job.rb
@@ -6,6 +6,14 @@
 
 # frozen_string_literal: true
 
+#
+# The job runs nightly over all candidate pools and clients. And be run ad-hoc for specific pools
+# * Update matches for the given pool(s) or all pools if none-specified
+# * Synchronization relies on the advisory lock within Hmis::Ce::Match::Engine, avoiding simultaneous processing of the same pool
+# * The job is willing to wait to acquire the lock for a given pool
+# * Failure to process all pools is an exception, which should cause the job to be retried based on retry configuration
+# * See also ProcessClientChangeMarkersJob
+#
 module Hmis
   class MatchCandidatesJob < BaseJob
     include NotifierConfig
@@ -16,6 +24,7 @@ module Hmis
       # one-off opportunity, don't track
       return _perform(**args) if args[:opportunity_ids]
 
+      # Full-scan
       instrument_as_maintenance_task do |run|
         _perform(**args)
         run.complete!
@@ -24,7 +33,7 @@ module Hmis
 
     # @param opportunity_ids [Array] an array of opportunity IDs to build candidate pools for.
     # If nil, builds pools for all active Opportunities
-    def _perform(opportunity_ids: nil)
+    def _perform(opportunity_ids: nil, lock_timeout_seconds: 300)
       raise unless Hmis::Ce.configuration.enabled?
 
       opportunities = opportunity_ids ? Hmis::Ce::Opportunity.where(id: opportunity_ids) : Hmis::Ce::Opportunity.active
@@ -38,7 +47,8 @@ module Hmis
 
       log("Running the CE match engine for #{candidate_pool_scope.count} candidate pools")
       candidate_pool_scope.find_each do |pool|
-        Hmis::Ce::Match::Engine.call(pool, client_scope)
+        completed = Hmis::Ce::Match::Engine.call(pool, client_scope, lock_timeout_seconds: lock_timeout_seconds)
+        raise "Failed acquire lock for CE candidate pool #{pool.id}" unless completed
       end
     end
 
