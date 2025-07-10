6c136782243d6497f1649f9114b704c430fb0806
martha <medwards@greenriver.org>
2025-07-10 11:34:28 -0400

    Add skeleton AHA mutation and form component

---
+++ drivers/hmis/app/graphql/mutations/ac_hmis/aha_score.rb
@@ -0,0 +1,30 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+module Mutations
+  class AcHmis::AhaScore < CleanBaseMutation
+    description 'Return AHA score for client'
+
+    argument :client_id, ID, required: true
+    field :score, String, null: true
+
+    def resolve(client_id:)
+      # If you have permission to view this client, you can fetch their AHA score
+      client = Hmis::Hud::Client.viewable_by(current_user).find_by(id: client_id)
+      access_denied! unless client.present?
+
+      # aha = HmisExternalApis::AcHmis::Aha.new
+      # response = aha.fetch_score(client)
+
+      {
+        score: '9.5',
+        errors: [],
+      }
+    end
+  end
+end
