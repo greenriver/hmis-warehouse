8801446c40e326498d51d40b7864c527b551dfed
Elliot Anders <elliot@greenriver.org>
2025-07-16 09:23:46 -0400

    Rubocop

--- db/warehouse/migrate/20250716131246_update_analytics_cas_opportunity_categories_to_version_2.rb
+++ db/warehouse/migrate/20250716131246_update_analytics_cas_opportunity_categories_to_version_2.rb
@@ -1,3 +1,12 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+
 class UpdateAnalyticsCasOpportunityCategoriesToVersion2 < ActiveRecord::Migration[7.1]
   def change
     update_view "analytics.cas_opportunity_categories", version: 2, revert_to_version: 1
