efe096ed5bb1442280135732a2fedea266950092
Elliot Anders <elliot@greenriver.org>
2025-07-16 09:29:06 -0400

    Rubocop

--- db/warehouse/migrate/20250716122931_add_opportunity_available_at_to_cas_opportunities.rb
+++ db/warehouse/migrate/20250716122931_add_opportunity_available_at_to_cas_opportunities.rb
@@ -1,3 +1,12 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+
 class AddOpportunityAvailableAtToCasOpportunities < ActiveRecord::Migration[7.1]
   def change
     add_column :cas_analytics_opportunities, :made_available_at, :datetime
