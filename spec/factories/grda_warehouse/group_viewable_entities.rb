a7f83e0cc24f88be7d56e5b5aabbad58030ee0a3
Theron Toomey <ttoomey@greenriver.org>
2025-07-23 14:42:31 +0000

    specs

---
+++ spec/factories/grda_warehouse/group_viewable_entities.rb
@@ -0,0 +1,15 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :grda_warehouse_group_viewable_entity, class: 'GrdaWarehouse::GroupViewableEntity' do
+    access_group { association :access_group }
+    collection { association :collection}
+    entity { association :hud_project}
+  end
+end
