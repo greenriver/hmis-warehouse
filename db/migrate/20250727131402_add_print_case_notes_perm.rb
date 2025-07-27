bc7a51353ab04b76482156dbd7e3f055e7a36fb0
Gig Ashton <gig@greenriver.org>
2025-07-27 09:19:51 -0400

    Add permission for printing case notes

---
+++ db/migrate/20250727131402_add_print_case_notes_perm.rb
@@ -0,0 +1,10 @@
+class AddPrintCaseNotesPerm < ActiveRecord::Migration[7.1]
+  def up
+    ::Hmis::Role.ensure_permissions_exist
+    ::Hmis::Role.reset_column_information
+  end
+
+  def down
+    remove_column :hmis_roles, :can_print_client_case_notes
+  end
+end
