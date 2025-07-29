8b7c75e87d6076569a1e9584798fef0862bde2ac
Elliot Anders <elliot@greenriver.org>
2025-07-29 15:17:44 -0400

    Freeze strings

--- db/warehouse/migrate/20250729183312_ensure_type_provided_type.rb
+++ db/warehouse/migrate/20250729183312_ensure_type_provided_type.rb
@@ -1,3 +1,11 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class EnsureTypeProvidedType < ActiveRecord::Migration[7.1]
   def up
     safety_assured do
