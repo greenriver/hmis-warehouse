68b095bd503031eb983eda48f7bc9f5c585a4406
Elliot Anders <elliot@greenriver.org>
2025-07-31 11:38:01 -0400

    Align permissions for reports and OP Analytics menu with what a user can see on the warehouse reports page; tests to confirm visibility

--- spec/factories/grda_warehouse/warehouse_reports/report_definition.rb
+++ spec/factories/grda_warehouse/warehouse_reports/report_definition.rb
@@ -1,3 +1,11 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :touch_point_report, class: 'GrdaWarehouse::WarehouseReports::ReportDefinition' do
     report_group { 'Reports' }
@@ -18,5 +26,12 @@ FactoryBot.define do
     url { 'core_demographics_report/warehouse_reports/core' }
     name { 'Core Demographics' }
     description { 'Summary data for client demographics across an arbitrary universe.' }
+  end
+
+  factory :op_analytics_report, class: 'GrdaWarehouse::WarehouseReports::ReportDefinition' do
+    report_group { 'Reports' }
+    url { 'superset/warehouse_reports/reports' }
+    name { 'Launch OP Analytics (Superset)' }
+    description { 'An integration with the Apache Superset business intelligence tool.' }
   end
 end
