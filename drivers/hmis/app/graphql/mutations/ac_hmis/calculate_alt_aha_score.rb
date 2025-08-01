646157ed99e593cc6a96e2a7af4d679ba8029b1e
martha <medwards@greenriver.org>
2025-08-01 11:12:30 -0400

    Add assessment id to mutation args

--- drivers/hmis/app/graphql/mutations/ac_hmis/calculate_alt_aha_score.rb
+++ drivers/hmis/app/graphql/mutations/ac_hmis/calculate_alt_aha_score.rb
@@ -10,21 +10,22 @@ module Mutations
   class AcHmis::CalculateAltAhaScore < CleanBaseMutation
     description 'Calculate alternative AHA score based on provided assessment values'
 
+    argument :assessment_id, ID, required: true
     argument :values_by_link_id, Types::JsonObject, required: true
 
     field :score, Integer, null: true
 
-    def resolve(values_by_link_id:)
+    def resolve(assessment_id:, values_by_link_id:)
       errors = HmisErrors::Errors.new
       # Use AHA configuration as proxy to determine whether alt-AHA should be enabled
       errors.add :base, :server_error, full_message: 'AHA connection is not configured' unless HmisExternalApis::AcHmis::Aha.enabled?
       return { errors: errors } if errors.any?
 
-      # check can_edit_enrollments permission (same as needed to edit assessments)
-      access_denied! unless current_user.can_edit_enrollments?
+      assessment = Hmis::Hud::CustomAssessment.find(assessment_id)
+      access_denied! unless current_user.can_edit_enrollments_for(assessment.enrollment)
 
       aha_calculator = HmisExternalApis::AcHmis::AltAhaCalculator.new
-      score = aha_calculator.calculate_score(values_by_link_id)
+      score = aha_calculator.calculate_score(assessment_id, values_by_link_id)
 
       { score: score }
     end
