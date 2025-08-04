c982c579ed1187f393d2a3d0368bd2618e5a2184
Elliot Anders <elliot@greenriver.org>
2025-08-04 14:48:14 -0400

    Rubocop

--- db/warehouse/migrate/20250804124243_create_analytics_cas_referral_users.rb
+++ db/warehouse/migrate/20250804124243_create_analytics_cas_referral_users.rb
@@ -6,7 +6,6 @@
 
 # frozen_string_literal: true
 
-
 class CreateAnalyticsCasReferralUsers < ActiveRecord::Migration[7.1]
   def change
     replace_view 'analytics.cas_referral_users', version: 2, revert_to_version: 1
