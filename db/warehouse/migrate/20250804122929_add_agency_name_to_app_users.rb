d8815dca6510d44953467062cd06a8d527325ca4
Elliot Anders <elliot@greenriver.org>
2025-08-04 14:41:47 -0400

    Rubocop

--- db/warehouse/migrate/20250804122929_add_agency_name_to_app_users.rb
+++ db/warehouse/migrate/20250804122929_add_agency_name_to_app_users.rb
@@ -1,3 +1,11 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class AddAgencyNameToAppUsers < ActiveRecord::Migration[7.1]
   def change
     add_column 'analytics.app_users', :agency_name, :string
--- db/warehouse/migrate/20250804124300_create_analytics_cas_referral_contacts.rb
+++ db/warehouse/migrate/20250804124300_create_analytics_cas_referral_contacts.rb
@@ -1,3 +1,11 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class CreateAnalyticsCasReferralContacts < ActiveRecord::Migration[7.1]
   def change
     replace_view 'analytics.cas_referral_contacts', version: 2, revert_to_version: 1
