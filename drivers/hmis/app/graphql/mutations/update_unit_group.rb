d131cfcbb2a1607968df30936107851bd91894ca
martha <medwards@greenriver.org>
2025-08-25 14:44:47 -0400

    Simplify validation, add spec for create mutation

--- drivers/hmis/app/graphql/mutations/update_unit_group.rb
+++ drivers/hmis/app/graphql/mutations/update_unit_group.rb
@@ -21,25 +21,6 @@ module Mutations
 
       errors = HmisErrors::Errors.new
 
-      # If name is being changed, check that it's still unique in the project.
-      # (This is also validated on the model, but we want to return a more user-friendly error message)
-      if input.name.present? && input.name != unit_group.name
-        # rubocop:disable Style/IfUnlessModifier
-        if Hmis::UnitGroup.where(project_id: unit_group.project_id, name: input.name).exists?
-          errors.add :name, :invalid, message: 'must be unique in the project'
-        end
-        # rubocop:enable Style/IfUnlessModifier
-      end
-
-      # Prevent changing or clearing workflow template if one is already set
-      # rubocop:disable Style/IfUnlessModifier
-      if unit_group.workflow_template_identifier.present? && input.workflow_template_identifier != unit_group.workflow_template_identifier
-        errors.add :workflow_template_identifier, :invalid, message: 'cannot be changed once set'
-      end
-      # rubocop:enable Style/IfUnlessModifier
-
-      return { errors: errors.errors } if errors.any?
-
       unit_group.assign_attributes(
         name: input.name,
         workflow_template_identifier: input.workflow_template_identifier,
