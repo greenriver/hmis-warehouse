b1baf1165e8b46925399d9ef3498589c2cf228e7
Gig Ashton <gig@greenriver.org>
2025-08-27 11:50:44 -0400

    Finish up eligible_unit_groups spec

--- drivers/hmis/spec/requests/hmis/ce/ce_client_eligible_unit_groups_spec.rb
+++ drivers/hmis/spec/requests/hmis/ce/ce_client_eligible_unit_groups_spec.rb
@@ -53,27 +53,58 @@ RSpec.describe Hmis::GraphqlController, type: :request do
     end
 
     let(:variables) do
-      { id: client_proxy.id, limit: 25, offset: 0 }
+      { id: client_proxy.id }
     end
 
-    let!(:candidate_pool) { create :hmis_ce_match_candidate_pool_with_candidates, client_proxies: [client_proxy] }
-    let!(:unit_group) do
-      create(:hmis_unit_group, project: p1, candidate_pool: candidate_pool)
+    context 'when client belongs to one candidate pool tied to one unit group' do
+      let!(:candidate_pool) { create :hmis_ce_match_candidate_pool_with_candidates, client_proxies: [client_proxy] }
+      let!(:unit_group) { create(:hmis_unit_group, project: p1, candidate_pool: candidate_pool) }
+
+      it 'returns the correct unit group details' do
+        response, result = post_graphql(**variables) { query }
+        expect(response.status).to eq(200), result.inspect
+
+        unit_groups_result = result.dig('data', 'ceClient', 'eligibleUnitGroups', 'nodes')
+        expect(unit_groups_result.count).to eq(1)
+        expect(unit_groups_result).to contain_exactly(
+          a_hash_including(
+            'unitGroupId' => unit_group.id.to_s,
+            'unitGroupName' => unit_group.name,
+            'projectName' => unit_group.project.name,
+          ),
+        )
+      end
     end
 
-    it 'returns the correct unit group details' do
-      response, result = post_graphql(**variables) { query }
-      expect(response.status).to eq(200), result.inspect
+    context 'when client belongs to one candidate pool tied to multiple unit group' do
+      let!(:candidate_pool) { create :hmis_ce_match_candidate_pool_with_candidates, client_proxies: [client_proxy] }
+      let!(:unit_groups) { create_list(:hmis_unit_group, 5, project: p1, candidate_pool: candidate_pool) }
 
-      expect(result.dig('data', 'ceClient', 'eligibleUnitGroups', 'nodesCount')).to eq(1)
-      unit_group_data = result.dig('data', 'ceClient', 'eligibleUnitGroups', 'nodes').first
-      expect(unit_group_data['unitGroupId']).to eq(unit_group.id.to_s)
-      expect(unit_group_data['unitGroupName']).to eq(unit_group.name)
-      expect(unit_group_data['projectName']).to eq(unit_group.project.name)
+      it 'returns the correct unit group details' do
+        response, result = post_graphql(**variables) { query }
+        expect(response.status).to eq(200), result.inspect
+
+        unit_groups_result = result.dig('data', 'ceClient', 'eligibleUnitGroups', 'nodes')
+        expect(unit_groups_result.count).to eq(5)
+        expect(unit_groups_result.map { |ug| ug['unitGroupId'] }).to match_array(unit_groups.map { |ug| ug.id.to_s })
+      end
     end
 
-    # TODO: test client belonging to one pool that is tied to multiple unit groups
-    # TODO: test client belonging to pool that is tied to a stale opportunity
+    context 'when client belongs to multiple candidate pools, each tied to multiple unit groups in different projects' do
+      let!(:candidate_pool1) { create :hmis_ce_match_candidate_pool_with_candidates, client_proxies: [client_proxy] }
+      let!(:candidate_pool2) { create :hmis_ce_match_candidate_pool_with_candidates, client_proxies: [client_proxy] }
+      let!(:unit_groups1) { create_list(:hmis_unit_group, 3, candidate_pool: candidate_pool1) }
+      let!(:unit_groups2) { create_list(:hmis_unit_group, 3, candidate_pool: candidate_pool2) }
+
+      it 'returns the correct unit group details' do
+        response, result = post_graphql(**variables) { query }
+        expect(response.status).to eq(200), result.inspect
+
+        unit_groups_result = result.dig('data', 'ceClient', 'eligibleUnitGroups', 'nodes')
+        expect(unit_groups_result.count).to eq(6)
+        expect(unit_groups_result.map { |ug| ug['unitGroupId'] }).to match_array(unit_groups1.map { |ug| ug.id.to_s } + unit_groups2.map { |ug| ug.id.to_s })
+      end
+    end
   end
 end
 
