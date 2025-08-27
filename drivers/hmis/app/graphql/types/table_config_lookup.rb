54c34de1eb8f0468d3b79e53b488bb9f934ec110
Gig Ashton <gig@greenriver.org>
2025-08-27 12:15:02 -0400

    More flexible waitlist table config lookup

--- drivers/hmis/app/graphql/types/table_config_lookup.rb
+++ drivers/hmis/app/graphql/types/table_config_lookup.rb
@@ -23,15 +23,20 @@ module Types
       )
     end
 
-    # Unit group waitlists- really these should be configured at Project Type level ideally
     def unit_group_waitlist(unit_group_id:)
       unit_group = Hmis::UnitGroup.find_by(id: unit_group_id)
       return unless unit_group
 
-      Hmis::TableConfiguration.for_ce_waitlist.find_by(
-        data_source_id: current_user.hmis_data_source_id,
-        owner: unit_group,
-      )
+      # find applicable configuration for this unit group, preferring more specific owners
+      [
+        unit_group,
+        unit_group.project,
+        unit_group.project&.organization,
+        nil,
+      ].each do |owner|
+        config = Hmis::TableConfiguration.for_ce_waitlist.find_by(owner: owner)
+        return config if config
+      end
     end
   end
 end
