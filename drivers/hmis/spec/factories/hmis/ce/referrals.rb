a956a5e104689baf7dc27484b5c49c7ce75b2dee
Gig <gig@greenriver.org>
2025-08-31 13:35:14 -0400

    Update SimpleStateMachine to raise on failed transition (#5704)

--- drivers/hmis/spec/factories/hmis/ce/referrals.rb
+++ drivers/hmis/spec/factories/hmis/ce/referrals.rb
@@ -21,7 +21,7 @@ FactoryBot.define do
     workflow_instance { association :hmis_workflow_execution_instance, template: workflow_template }
     client { association :hmis_hud_client, data_source: data_source }
     referred_by { association :hmis_user, data_source: data_source }
-    status { 'in_progress' }
+    status { 'initialized' }
     referral_origin { Hmis::Ce::Referral::WAITLIST_ORIGIN }
 
     after(:create) do |referral|
