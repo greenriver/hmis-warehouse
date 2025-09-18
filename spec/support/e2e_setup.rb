15d38d248499780240b89146b3bce73292d4bc93
Elliot Anders <elliot@greenriver.org>
2025-09-17 21:00:23 -0400

    WIP

--- spec/support/e2e_setup.rb
+++ spec/support/e2e_setup.rb
@@ -6,6 +6,12 @@
 
 # frozen_string_literal: false
 
+# Use puts for immediate output since Rails.logger might not be ready
+puts '=== E2E Setup Debug ==='
+puts "RUN_SYSTEM_TESTS: #{ENV['RUN_SYSTEM_TESTS'].inspect}"
+puts "RUN_RAILS_SYSTEM_TESTS: #{ENV['RUN_RAILS_SYSTEM_TESTS'].inspect}"
+puts "All ENV vars containing 'SYSTEM': #{ENV.select { |k, _v| k.include?('SYSTEM') }.inspect}"
+
 if ENV['RUN_SYSTEM_TESTS']
   Rails.logger.info '=== Loading E2E Tests Setup ==='
   Rails.logger.info "CHROME_URL: #{ENV['CHROME_URL']}"
@@ -24,6 +30,9 @@ if ENV['RUN_SYSTEM_TESTS']
 
   Capybara.default_driver = E2eTests::DRIVER_NAME
   Rails.logger.info "=== Set default driver to #{E2eTests::DRIVER_NAME} ==="
+else
+  puts '=== E2E Setup SKIPPED - RUN_SYSTEM_TESTS not set ==='
+  puts 'This means system tests will fail with nil browser objects'
 end
 
 # from user factory
