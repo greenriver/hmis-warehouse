e35d719a442206b644def8da0d43b00d9ad34c4f
Elliot Anders <elliot@greenriver.org>
2025-09-27 08:20:19 -0400

    Don't reuse constants across tests

--- drivers/hud_twenty_twenty_two_to_twenty_twenty_four/spec/models/csv_transformer_spec.rb
+++ drivers/hud_twenty_twenty_two_to_twenty_twenty_four/spec/models/csv_transformer_spec.rb
@@ -9,41 +9,42 @@
 require 'rails_helper'
 
 RSpec.describe HudTwentyTwentyTwoToTwentyTwentyFour::CsvTransformer, type: :model do
-  FIXTURES = 'drivers/hud_twenty_twenty_two_to_twenty_twenty_four/spec/fixtures' unless const_defined?(:FIXTURES)
-  FIXTURES_IN = File.join(FIXTURES, 'in') unless const_defined?(:FIXTURES_IN)
-  FIXTURES_OUT = File.join(FIXTURES, 'out') unless const_defined?(:FIXTURES_OUT)
-  TEST_DIR = "tmp/test_#{described_class.name.underscore}".freeze unless const_defined?(:TEST_DIR)
-  SOURCE_DIR = File.join(TEST_DIR, 'in/merged/source') unless const_defined?(:SOURCE_DIR)
-  DEST_DIR = File.join(TEST_DIR, 'out') unless const_defined?(:DEST_DIR)
+  FIXTURES_2022_2024 = 'drivers/hud_twenty_twenty_two_to_twenty_twenty_four/spec/fixtures'
+  FIXTURES_IN_2022_2024 = File.join(FIXTURES_2022_2024, 'in')
+  FIXTURES_OUT_2022_2024 = File.join(FIXTURES_2022_2024, 'out')
+  TEST_DIR_2022_2024 = "tmp/test_#{described_class.name.underscore}".freeze
+  SOURCE_DIR_2022_2024 = File.join(TEST_DIR_2022_2024, 'in/merged/source')
+  DEST_DIR_2022_2024 = File.join(TEST_DIR_2022_2024, 'out')
 
   before(:each) do
     create_test_dir
   end
 
   it 'generates an output' do
-    skip('Ignoring test, no input files') unless File.exist?(File.join(SOURCE_DIR, 'Export.csv'))
+    skip('Ignoring test, no input files') unless File.exist?(File.join(SOURCE_DIR_2022_2024, 'Export.csv'))
 
-    HudTwentyTwentyTwoToTwentyTwentyFour::CsvTransformer.up(SOURCE_DIR, DEST_DIR)
+    HudTwentyTwentyTwoToTwentyTwentyFour::CsvTransformer.up(SOURCE_DIR_2022_2024, DEST_DIR_2022_2024)
 
     expect(compare_test_results).to eq([])
   end
 
   def create_test_dir
     # Clean up any old test data
-    FileUtils.rm_rf(TEST_DIR)
+    FileUtils.rm_rf(TEST_DIR_2022_2024)
 
     # Create/populate the test directories
-    FileUtils.mkdir_p(File.join(TEST_DIR, 'in'))
-    FileUtils.mkdir_p(DEST_DIR)
-    FileUtils.cp_r(FIXTURES_IN, TEST_DIR)
+    FileUtils.mkdir_p(File.join(TEST_DIR_2022_2024, 'in'))
+    FileUtils.mkdir_p(DEST_DIR_2022_2024)
+    FileUtils.cp_r(FIXTURES_IN_2022_2024, TEST_DIR_2022_2024)
   end
 
+  # compare contents fo DEST_DIR to FIXTURES_OUT
   def compare_test_results
     results = []
-    expected_files = Dir.glob(File.join(FIXTURES_OUT, '*.csv'))
+    expected_files = Dir.glob(File.join(FIXTURES_OUT_2022_2024, '*.csv'))
     expected_files.each do |filename|
       basename = File.basename(filename)
-      dest_file = File.join(DEST_DIR, basename)
+      dest_file = File.join(DEST_DIR_2022_2024, basename)
       results << basename unless File.exist?(dest_file) && FileUtils.identical?(filename, dest_file)
     end
     results
