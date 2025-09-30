e789257ef3ab677cea4b52a36f36ae16372ea742
Theron Toomey <ttoomey@greenriver.org>
2025-09-30 21:05:06 +0000

    remove low-value tests

--- spec/models/grda_warehouse/tasks/process_recurring_hmis_exports_spec.rb
+++ spec/models/grda_warehouse/tasks/process_recurring_hmis_exports_spec.rb
@@ -19,29 +19,6 @@ RSpec.describe GrdaWarehouse::Tasks::ProcessRecurringHmisExports, type: :model d
       expect(skip_export).not_to receive(:run)
 
       task.run!
-
-      due_export.destroy!
-      skip_export.destroy!
-    end
-
-    it 'completes the maintenance task run and uses instrumentation' do
-      export = create(:recurring_hmis_export, updated_at: Time.zone.now)
-      allow(task).to receive(:recurring_exports_scope).and_return([export])
-
-      expect(GrdaWarehouse::Tasks::TaskInstrumentation).to receive(:call) do |_name, **_options, &block|
-        block.call(create(:system_maintenance_task_run))
-      end
-
-      task.run!
-
-      export.destroy!
-    end
-  end
-
-  describe '#recurring_exports_scope' do
-    it 'returns all recurring exports' do
-      expect(GrdaWarehouse::RecurringHmisExport).to receive(:all)
-      task.recurring_exports_scope
     end
   end
 end
