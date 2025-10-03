7951ca5c0e08a77a0f83b95e91eabc5fd3330704
Gig Ashton <gig@greenriver.org>
2025-10-02 21:59:49 -0400

    Add state machine spec

--- app/models/concerns/simple_state_machine.rb
+++ app/models/concerns/simple_state_machine.rb
@@ -35,6 +35,7 @@ module SimpleStateMachine
       validates ssm_column, inclusion: {
         in: state_machine_states.map(&:to_s),
         message: "must be one of: #{state_machine_states.map(&:to_s).join(', ')}",
+        allow_nil: true,
       }
 
       # Define event methods
