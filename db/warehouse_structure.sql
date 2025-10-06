2d7023183a151dea8b14361e31b3173533bfb761
martha <medwards@greenriver.org>
2025-10-06 15:47:14 -0400

    reset db file, tidy

--- db/warehouse_structure.sql
+++ db/warehouse_structure.sql
@@ -7159,6 +7159,7 @@ CREATE TABLE public.ce_opportunities (
     id bigint NOT NULL,
     candidate_pool_id bigint,
     project_id bigint NOT NULL,
+    workflow_template_identifier character varying NOT NULL,
     name character varying NOT NULL,
     status character varying NOT NULL,
     expires_at timestamp(6) without time zone,
@@ -24650,8 +24651,7 @@ CREATE TABLE public.hmis_unit_groups (
     deleted_at timestamp without time zone,
     candidate_pool_id bigint,
     ce_event_type integer,
-    unit_type_id bigint,
-    direct_referral_workflow_template_identifier character varying
+    unit_type_id bigint
 );
 
 
@@ -75771,8 +75771,6 @@ ALTER TABLE ONLY public.import_logs
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
-('20251002192540'),
-('20251002192539'),
 ('20250926075333'),
 ('20250923120000'),
 ('20250920182921'),
