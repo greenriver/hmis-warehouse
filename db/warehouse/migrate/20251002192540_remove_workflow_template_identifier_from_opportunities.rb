cfd173eed9ada9b2306411f06f89a73e522ad81d
martha <medwards@greenriver.org>
2025-10-06 16:20:21 -0400

    explain

--- db/warehouse/migrate/20251002192540_remove_workflow_template_identifier_from_opportunities.rb
+++ db/warehouse/migrate/20251002192540_remove_workflow_template_identifier_from_opportunities.rb
@@ -2,6 +2,7 @@
 
 class RemoveWorkflowTemplateIdentifierFromOpportunities < ActiveRecord::Migration[7.1]
   def change
+    # safety_assured because we added this column to Hmis::Ce::Opportunity.ignored_columns.
     safety_assured { remove_column :ce_opportunities, :workflow_template_identifier, :string }
   end
 end
