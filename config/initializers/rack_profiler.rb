0ddf4800e8bae670c1f449c517b44fc7e01aa256
Elliot Anders <elliot@greenriver.org>
2025-10-10 10:10:49 -0400

    Make rubocop happy?

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -3,7 +3,7 @@
 # frozen_string_literal: true
 
 if Rails.env.development? && ENV['RACK_MINI_PROFILER'] != 'off'
-  require "rack-mini-profiler"
+  require 'rack-mini-profiler'
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
