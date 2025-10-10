a1bc52e31b975f595c359eba457e87fc4cec836b
Elliot <elliot@greenriver.org>
2025-10-10 11:33:44 -0400

    Ability to add an HMIS logo to the theme (#5892)

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -17,6 +17,7 @@ module Admin
       @theme.set_theme_default_css!
       @theme.set_theme_default_logo!
       @theme.set_theme_default_print_logo!
+      @theme.set_theme_default_hmis_logo!
     end
 
     def update
@@ -39,6 +40,7 @@ module Admin
         :logo,
         :print_logo,
         :careplan_logo,
+        :hmis_logo,
       )
     end
 
