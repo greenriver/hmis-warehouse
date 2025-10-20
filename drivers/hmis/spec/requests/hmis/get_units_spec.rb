d1c4223ab8bf63c448729d8403e27a0744a56483
martha <medwards@greenriver.org>
2025-10-20 13:58:10 -0400

    fix get_units_spec.rb

--- drivers/hmis/spec/requests/hmis/get_units_spec.rb
+++ drivers/hmis/spec/requests/hmis/get_units_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
     context 'when the unit has an open opportunity' do
       let!(:unit) { create(:hmis_unit, project: project) }
-      let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :open) }
+      let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, status: :open) }
 
       it 'returns the unit with the opportunity' do
         response, result = post_graphql(id: project.id) { query }
@@ -102,7 +102,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
     context 'when the unit has an opportunity with a referral in progress' do
       let!(:unit) { create(:hmis_unit, project: project) }
-      let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :locked) }
+      let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, status: :locked) }
       let!(:referral) { create(:hmis_ce_referral, opportunity: opportunity, data_source: ds1, status: :in_progress) }
 
       it 'returns the unit with the opportunity and referral' do
@@ -145,7 +145,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
       end
 
       context 'when the unit has an open opportunity' do
-        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :open) }
+        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, status: :open) }
 
         it 'acceptingCeReferrals is true' do
           _, result = post_graphql(id: project.id) { query }
@@ -159,7 +159,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
       end
 
       context 'when the unit has an opportunity with referrals in progress' do
-        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :locked) }
+        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, status: :locked) }
         let!(:referral) { create(:hmis_ce_referral, opportunity: opportunity, data_source: ds1, status: :in_progress) }
 
         it 'acceptingCeReferrals is false' do
@@ -174,7 +174,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
       end
 
       context 'when the unit has a closed opportunity, but no open one' do
-        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :closed) }
+        let!(:opportunity) { create(:hmis_ce_opportunity, unit: unit, status: :closed) }
         let!(:referral) { create(:hmis_ce_referral, opportunity: opportunity, data_source: ds1, status: :accepted) }
 
         it 'acceptingCeReferrals is false' do
@@ -193,7 +193,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
       before do
         50.times do
           unit = create :hmis_unit, project: project
-          opportunity = create(:hmis_ce_opportunity, unit: unit, project: project, data_source: ds1, status: :locked)
+          opportunity = create(:hmis_ce_opportunity, unit: unit, status: :locked)
           create(:hmis_ce_referral, opportunity: opportunity, data_source: ds1, status: :in_progress)
         end
       end
@@ -205,7 +205,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
           expect(result.dig('data', 'project', 'units', 'nodesCount')).to eq(50)
           expect(result.dig('data', 'project', 'units', 'nodes', 0, 'latestOpportunity', 'referral')).to be_present
           expect(result.dig('data', 'project', 'units', 'nodes', 0, 'latestOpportunity', 'referral', 'active')).to be_truthy
-        end.to make_database_queries(count: 25..35)
+        end.to make_database_queries(count: 30..40)
       end
     end
 
@@ -262,8 +262,6 @@ RSpec.describe Hmis::GraphqlController, type: :request do
         let!(:opportunity) do
           create(:hmis_ce_opportunity,
                  unit: unit,
-                 project: project,
-                 data_source: ds1,
                  status: :open,
                  stale: true,
                  assignment_rules: [
