d28eda880d6e05e863d0652326f1da24b722ae3f
martha <medwards@greenriver.org>
2025-11-05 15:19:15 -0500

    Fix

--- drivers/hmis/app/models/hmis/hud/processors/enrollment_processor.rb
+++ drivers/hmis/app/models/hmis/hud/processors/enrollment_processor.rb
@@ -82,7 +82,7 @@ module Hmis::Hud::Processors
 
       # If assigned to a different unit: unassign
       # Update the occupancy period directly, instead of using enrollment.release_unit!, to avoid creating a nested transaction.
-      enrollment.active_unit_occupancy.occupancy_period.update!(end_date: Date.current, user: current_user) if enrollment.active_unit_occupancy.present?
+      enrollment.active_unit_occupancy.occupancy_period.update!(end_date: Date.current, user: @processor.current_user) if enrollment.active_unit_occupancy.present?
 
       # Assign to specified unit
       enrollment.assign_unit(unit: unit, start_date: Date.current, user: @processor.current_user)
