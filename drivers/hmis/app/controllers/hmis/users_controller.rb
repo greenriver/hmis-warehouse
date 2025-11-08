15f9048aa176f9b793f2fcfca586142babcaca81
Elliot Anders <elliot@greenriver.org>
2025-11-08 07:03:08 -0500

    Initial changes to get HMIS sign in working

--- drivers/hmis/app/controllers/hmis/users_controller.rb
+++ drivers/hmis/app/controllers/hmis/users_controller.rb
@@ -15,9 +15,27 @@ class Hmis::UsersController < Hmis::BaseController
   #
   # This is called by the frontend on initial page load, to determine whether
   # there is a currently active session.
+  #
+  # Returns 401 if not authenticated, user data if authenticated.
   def show
-    payload = current_hmis_user&.current_user_api_values || {}
+    return render_json_error(401, :unauthenticated) unless current_hmis_user
+
+    payload = current_hmis_user.current_user_api_values || {}
     payload[:impersonating] = impersonating?
+
+    # Calculate session duration from JWT token expiration
+    access_token = request.headers['HTTP_X_FORWARDED_ACCESS_TOKEN']
+    if access_token.present?
+      jwt_helper = JwtHelper.new(access_token: access_token)
+      if jwt_helper.token? && jwt_helper.validate!
+        expiration_time = jwt_helper.expiration_time
+        if expiration_time
+          # Return remaining seconds until expiration
+          payload[:sessionDuration] = [(expiration_time - Time.current).to_i, 0].max
+        end
+      end
+    end
+
     render json: payload
   end
 
