e51e1d920dea15dbec44ae8821142d0ea94231c1
Theron Toomey <ttoomey@greenriver.org>
2025-11-10 12:59:58 -0500

    8519 ssh algorithms (#5985)
    Add support for curve25519-sha256 in ssh/sftp

--- drivers/claims_reporting/app/models/claims_reporting/importer.rb
+++ drivers/claims_reporting/app/models/claims_reporting/importer.rb
@@ -9,6 +9,10 @@
 require 'net/sftp'
 require 'zip'
 
+# support curve25519-sha256
+# verify with bundle exec ruby -r x25519  -r net/ssh -e "p Net::SSH::Transport::Algorithms::ALGORITHMS[:kex]"
+require 'x25519'
+
 module ClaimsReporting
   class Importer
     include NotifierConfig
