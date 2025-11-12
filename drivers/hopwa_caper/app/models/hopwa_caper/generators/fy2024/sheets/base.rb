bde68527dbf74a5d14b403d2b8e9be146d1ad3ce
Theron Toomey <ttoomey@greenriver.org>
2025-11-12 13:28:45 -0500

    8395 hopwa caper updates (#5984)
    Implements new version of HOPWA CAPER, adds new sheets and adjusts demographic breakdown for HUD 2026 spec changes.

--- drivers/hopwa_caper/app/models/hopwa_caper/generators/fy2024/sheets/base.rb
+++ drivers/hopwa_caper/app/models/hopwa_caper/generators/fy2024/sheets/base.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ###
 # Copyright 2016 - 2025 Green River Data Analysis, LLC
 #
@@ -6,29 +8,10 @@
 
 module HopwaCaper::Generators::Fy2024::Sheets
   class Base < ::HudReports::QuestionBase
-    def initialize(generator = nil, report = nil, options: {})
-      super
-      report.options.with_indifferent_access.merge(user_id: report.user_id) if options.blank?
-    end
+    READ_ONLY_MESSAGE = HopwaCaper::Generators::Fy2024::Generator::UNSUPPORTED_MESSAGE
 
-    protected
-
-    def question_sheet(question:)
-      sheet = HudReports::QuestionSheet.new(report: @report, question: question)
-      if block_given?
-        builder = sheet.builder
-        yield(builder)
-        sheet.build(builder)
-      end
-      sheet
-    end
-
-    def report_enrollment_universe
-      HopwaCaper::Enrollment
-    end
-
-    def overlapping_enrollments(scope)
-      scope.overlapping_range(start_date: @report.start_date, end_date: @report.end_date)
+    def run_question!
+      raise NotImplementedError, READ_ONLY_MESSAGE
     end
   end
 end
