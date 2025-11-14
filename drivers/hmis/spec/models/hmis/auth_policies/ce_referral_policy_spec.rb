77b4f039632594be8f57bdd390e190396bb952f4
martha <medwards@greenriver.org>
2025-11-14 12:41:37 -0500

    add test coverage

--- drivers/hmis/spec/models/hmis/auth_policies/ce_referral_policy_spec.rb
+++ drivers/hmis/spec/models/hmis/auth_policies/ce_referral_policy_spec.rb
@@ -186,35 +186,38 @@ RSpec.describe Hmis::AuthPolicies::CeReferralPolicy, type: :model do
     end
 
     context 'when user can manage outgoing referrals on source project' do
+      let!(:access_control) { create_access_control(user, source_project, with_permission: [:can_manage_outgoing_referrals]) }
+
       it 'returns true' do
-        create_access_control(user, source_project, with_permission: [:can_manage_outgoing_referrals])
         expect(policy.can_view_summary?).to be true
       end
     end
 
-    context 'when referral has a target enrollment' do
-      let(:target_enrollment) { create :hmis_hud_enrollment, project: project, client: client, data_source: data_source }
-      let(:referral) do
-        create(
-          :hmis_ce_referral,
-          opportunity: opportunity,
-          workflow_instance: workflow_instance,
-          client: client,
-          referred_by: user,
-          status: 'initialized',
-          source_enrollment: source_enrollment,
-          target_enrollment: target_enrollment,
-        )
-      end
+    context 'when user can view enrollments at the target project' do
+      let!(:access_control) { create_access_control(user, project, with_permission: [:can_view_project, :can_view_enrollment_details]) }
 
-      it 'returns true if user can view target enrollment' do
-        create_access_control(user, project, with_permission: [:can_view_enrollment_details])
-        expect(policy.can_view_summary?).to be true
-      end
-
-      it 'returns false if user cannot view target enrollment' do
-        create_access_control(user, project, with_permission: [:can_view_project])
+      it 'returns false when referral has no target enrollment' do
         expect(policy.can_view_summary?).to be false
+      end
+
+      context 'when referral has a target enrollment' do
+        let(:target_enrollment) { create :hmis_hud_enrollment, project: project, client: client, data_source: data_source }
+        let(:referral) do
+          create(
+            :hmis_ce_referral,
+            opportunity: opportunity,
+            workflow_instance: workflow_instance,
+            client: client,
+            referred_by: user,
+            status: 'initialized',
+            source_enrollment: source_enrollment,
+            target_enrollment: target_enrollment,
+          )
+        end
+
+        it 'returns true' do
+          expect(policy.can_view_summary?).to be true
+        end
       end
     end
 
