92a7f18287e97a300a3a6df1ae51ef61a639dc53
Theron Toomey <ttoomey@greenriver.org>
2025-11-14 21:13:38 +0000

    up timeout

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -3,7 +3,7 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 48.hours
+Delayed::Worker.max_run_time = 72.hours
 
 Delayed::Worker.default_queue_name = 'default_priority'
 Delayed::Worker.read_ahead = 2
