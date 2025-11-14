eea058ba47bc9735f24536b2df4d85788afc3cc6
Theron Toomey <ttoomey@greenriver.org>
2025-11-14 19:08:23 +0000

    extend dj timeout

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 30.hours
+Delayed::Worker.max_run_time = 48.hours
 
 Delayed::Worker.default_queue_name = 'default_priority'
 Delayed::Worker.read_ahead = 2
