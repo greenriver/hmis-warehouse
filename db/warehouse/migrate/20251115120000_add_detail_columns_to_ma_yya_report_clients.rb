51e1f1c5399a141ab937b7179067ce7acf12d71d
Elliot <elliot@greenriver.org>
2025-11-17 13:46:41 -0500

    Migrate columns individually (#6012)

--- db/warehouse/migrate/20251115120000_add_detail_columns_to_ma_yya_report_clients.rb
+++ db/warehouse/migrate/20251115120000_add_detail_columns_to_ma_yya_report_clients.rb
@@ -8,16 +8,14 @@
 
 class AddDetailColumnsToMaYyaReportClients < ActiveRecord::Migration[7.1]
   def change
-    change_table :ma_yya_report_clients, bulk: true do |t|
-      t.integer :entry_current_living_situation_code
-      t.date :previous_universe_entry_date
-      t.integer :project_type_at_entry
-      t.string :homelessness_basis
-      t.integer :homeless_enrollment_project_type
-      t.integer :latest_homeless_cls_in_range_code
-      t.integer :latest_homeless_cls_code
-      t.integer :previous_universe_project_type
-      t.integer :homeless_enrollment_project_type_during_range
-    end
+    add_column :ma_yya_report_clients, :entry_current_living_situation_code, :integer
+    add_column :ma_yya_report_clients, :previous_universe_entry_date, :date
+    add_column :ma_yya_report_clients, :project_type_at_entry, :integer
+    add_column :ma_yya_report_clients, :homelessness_basis, :string
+    add_column :ma_yya_report_clients, :homeless_enrollment_project_type, :integer
+    add_column :ma_yya_report_clients, :latest_homeless_cls_in_range_code, :integer
+    add_column :ma_yya_report_clients, :latest_homeless_cls_code, :integer
+    add_column :ma_yya_report_clients, :previous_universe_project_type, :integer
+    add_column :ma_yya_report_clients, :homeless_enrollment_project_type_during_range, :integer
   end
 end
