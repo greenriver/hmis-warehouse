0db0dfc955382522aa75be15f8f12cb06c63cb5d
martha <medwards@greenriver.org>
2025-11-24 12:14:05 -0500

    remove unneeded

--- drivers/hmis/app/models/hmis/auth_policies/hmis_project_policy.rb
+++ drivers/hmis/app/models/hmis/auth_policies/hmis_project_policy.rb
@@ -33,13 +33,7 @@ class Hmis::AuthPolicies::HmisProjectPolicy < Hmis::AuthPolicies::BasePolicy
     project_permissions.include?(:can_manage_outgoing_referrals)
   end
 
-  def can_view_outgoing_referral_details?
-    # can_view_outgoing_referral_details grants full detail access
-    project_permissions.include?(:can_view_outgoing_referral_details)
-  end
-
   def can_view_outgoing_referral_summaries?
-    # can_manage_outgoing_referrals only grants summary-level access
     project_permissions.include?(:can_view_outgoing_referral_details) || project_permissions.include?(:can_manage_outgoing_referrals)
   end
 
