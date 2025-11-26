ba69aa2a2fcf4db00023919b97d1824c6a33f3ee
Elliot Anders <elliot@greenriver.org>
2025-11-26 14:27:53 -0500

    Append IDs to projects and organizations

---
+++ drivers/hmis_csv_importer/app/models/hmis_csv_importer/hmis_csv_cleanup/append_project_id.rb
@@ -0,0 +1,50 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+module HmisCsvImporter::HmisCsvCleanup
+  class AppendProjectId < Base
+    def cleanup!
+      project_batch = []
+
+      project_scope.find_each do |project|
+        project.ProjectName = "#{project.ProjectName} (#{project.ProjectID})"
+        project.set_source_hash
+        project_batch << project
+      end
+
+      project_source.import(
+        project_batch,
+        on_duplicate_key_update: {
+          conflict_target: conflict_target(project_source),
+          columns: [:ProjectName, :source_hash],
+        },
+      )
+    end
+
+    def project_scope
+      project_source.
+        where(importer_log_id: @importer_log.id)
+    end
+
+    def project_source
+      importable_file_class('Project')
+    end
+
+    def self.description
+      'Append the HMIS ProjectID to the ProjectName field'
+    end
+
+    def self.enable
+      {
+        import_cleanups: {
+          'Project': ['HmisCsvImporter::HmisCsvCleanup::AppendProjectId'],
+        },
+      }
+    end
+  end
+end
