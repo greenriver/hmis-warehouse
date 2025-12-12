e14d9e1562a6b1a8a1f343c2213f922f11b268a9
Elliot <elliot@greenriver.org>
2025-12-12 11:30:24 -0500

    Debugging Nightly Processing (#6077)
    * Log some debugging info for rebuild of enrollments as part of the nightly process
    
    * Sort debugging
    
    * bug fix for service history job rebuilding
    
    * Formalize debugging for SHS processing
    
    ---------
    
    Co-authored-by: Dave Greiner <dgreiner@greenriver.org>
    Co-authored-by: Dave G <149399758+dtgreiner@users.noreply.github.com>

--- app/models/concerns/service_history/builder.rb
+++ app/models/concerns/service_history/builder.rb
@@ -13,7 +13,7 @@ module ServiceHistory::Builder
   DEFAULT_MAX_WAIT_SECONDS = 21_600
 
   # Advisory lock name
-  ADVISORY_LOCK_NAME = 'rebuild_enrollments'.freeze
+  ADVISORY_LOCK_NAME = 'rebuild_enrollments'
 
   # Queue delayed jobs for the enrollments associated with destination clients
   #
@@ -147,8 +147,8 @@ module ServiceHistory::Builder
       en_ids = scope.distinct.joins(:project, :destination_client).order(:data_source_id).pluck(:id, :data_source_id).map(&:first).uniq
       already_queued = Set.new
       builder_batch_job_scope.each do |dj|
-        en_ids = dj.payload_object.instance_variable_get(:@enrollment_ids)
-        already_queued += en_ids
+        queued_ids = dj.payload_object.instance_variable_get(:@enrollment_ids)
+        already_queued += queued_ids
       end
       en_ids -= already_queued.to_a
       Rails.logger.info "Found #{en_ids.count} enrollments needing processing"
