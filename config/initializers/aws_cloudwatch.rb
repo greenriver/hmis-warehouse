593a2514c62f274c842af9c548129a3e6c03e469
Theron Toomey <ttoomey@greenriver.org>
2025-12-19 15:36:19 +0000

    avoid cloudwatch in test/development

--- config/initializers/aws_cloudwatch.rb
+++ config/initializers/aws_cloudwatch.rb
@@ -1,6 +1,6 @@
 # Rails.logger.debug "Running initializer in #{__FILE__}"
 
-if ENV['EKS'] != 'true'
+if ENV['EKS'] != 'true' && (Rails.env.staging? || Rails.env.production?)
   Rails.configuration.to_prepare do
     def find_log_stream_name
       log_group = ENV.fetch('TARGET_GROUP_NAME', nil)
