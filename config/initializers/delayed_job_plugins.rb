c77ca6866b0bb58b1336dd2a378c2fc707c63388
Theron Toomey <ttoomey@greenriver.org>
2025-12-21 04:24:20 +0000

    cleanup

--- config/initializers/delayed_job_plugins.rb
+++ config/initializers/delayed_job_plugins.rb
@@ -1,50 +1,22 @@
 # frozen_string_literal: true
 
 class SignalHandlerPlugin < Delayed::Plugin
-  # While standard Delayed Job workers run in a single-threaded process,
-  # thread-safety will support future threaded worker configurations
-  # and parallelized test environments.
-  @registry_mutex = Mutex.new
-  @active_workers = {}.compare_by_identity # { thread_object => worker_instance }
+  class Registry < ActiveSupport::CurrentAttributes
+    attribute :worker
+  end
 
   class << self
-    attr_reader :registry_mutex
-
-    def register_worker(worker)
-      @registry_mutex.synchronize do
-        @active_workers[Thread.current] = worker
-      end
-    end
-
-    def unregister_worker
-      @registry_mutex.synchronize do
-        @active_workers.delete(Thread.current)
-      end
-    end
-
     def current_worker_stopping?
-      worker = nil
-      @registry_mutex.synchronize do
-        worker = @active_workers[Thread.current]
-      end
-
-      worker&.stop? || false
-    end
-
-    # For testing purposes
-    def reset!
-      @registry_mutex.synchronize do
-        @active_workers = {}.compare_by_identity
-      end
+      Registry.worker&.stop? || false
     end
   end
 
   callbacks do |lifecycle|
     lifecycle.around(:perform) do |worker, _job, &block|
-      SignalHandlerPlugin.register_worker(worker)
+      Registry.worker = worker
       block&.call
     ensure
-      SignalHandlerPlugin.unregister_worker
+      Registry.reset
     end
   end
 end
