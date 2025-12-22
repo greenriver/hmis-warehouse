031bcc5016c829ecbe5610a8741d42925ed0a5e8
Theron Toomey <ttoomey@greenriver.org>
2025-12-22 16:29:46 +0000

    spec

--- spec/models/hud_reports/household_context_builder_spec.rb
+++ spec/models/hud_reports/household_context_builder_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe HudReports::HouseholdContextBuilder, type: :model do
   let(:generator) do
     instance_double(
       HudReports::GeneratorBase,
-      client_scope: GrdaWarehouse::Hud::Client.where(id: [client_hoh.destination_client.id, client_child.destination_client.id]),
+      client_scope: GrdaWarehouse::Hud::Client.all,
       report_scope_source: GrdaWarehouse::ServiceHistoryEnrollment.entry,
     )
   end
@@ -116,6 +116,95 @@ RSpec.describe HudReports::HouseholdContextBuilder, type: :model do
         child_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_child.id)
         expect(child_context.inherited_chronic_status).to be true
       end
+
+      it 'passes chronic status from another adult to the child if HoH is not chronic' do
+        # HoH not chronic
+        hud_enrollment_hoh.update!(DisablingCondition: 0)
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find(hud_enrollment_hoh.id).create_service_history!(true)
+
+        # Add another adult who is chronic
+        adult_2 = create_client_with_warehouse_link(dob: 30.years.ago)
+        create(:hud_enrollment,
+               PersonalID: adult_2.PersonalID,
+               ProjectID: project.ProjectID,
+               HouseholdID: 'HH123',
+               RelationshipToHoH: 3, # Spouse/Partner
+               EntryDate: '2020-01-01',
+               DisablingCondition: 1,
+               ContinuouslyHomelessOneYear: 1,
+               TimesHomelessPastThreeYears: 4,
+               MonthsHomelessPastThreeYears: 112,
+               data_source_id: data_source.id)
+
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find_each(&:rebuild_service_history!)
+
+        builder.call
+        child_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_child.id)
+        expect(child_context.inherited_chronic_status).to be true
+      end
+
+      it 'passes indeterminate chronic status (DK/R) from HoH to the child' do
+        # HoH has DK/R
+        hud_enrollment_hoh.update!(DisablingCondition: 8) # 8 = DK
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find(hud_enrollment_hoh.id).create_service_history!(true)
+
+        builder.call
+        child_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_child.id)
+        expect(child_context.inherited_chronic_detail).to eq('dk_or_r')
+      end
+    end
+
+    context 'with move-in date inheritance' do
+      before do
+        hud_enrollment_hoh.update!(MoveInDate: '2020-06-01')
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find(hud_enrollment_hoh.id).create_service_history!(true)
+      end
+
+      it 'inherits HoH move-in date when member is present' do
+        builder.call
+        child_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_child.id)
+        expect(child_context.inherited_move_in_date).to eq(Date.parse('2020-06-01'))
+      end
+
+      it 'uses own entry date when joining after HoH move-in' do
+        # Child joins in July
+        hud_enrollment_child.update!(EntryDate: '2020-07-01')
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find(hud_enrollment_child.id).create_service_history!(true)
+
+        builder.call
+        child_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_child.id)
+        expect(child_context.inherited_move_in_date).to eq(Date.parse('2020-07-01'))
+      end
+    end
+
+    context 'with different household types' do
+      it 'identifies adults only households' do
+        # Remove child and add another adult
+        hud_enrollment_child.destroy
+        adult_2 = create_client_with_warehouse_link(dob: 30.years.ago)
+        create(:hud_enrollment,
+               PersonalID: adult_2.PersonalID,
+               ProjectID: project.ProjectID,
+               HouseholdID: 'HH123',
+               RelationshipToHoH: 3,
+               EntryDate: '2020-01-01',
+               data_source_id: data_source.id)
+
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find_each(&:rebuild_service_history!)
+
+        builder.call
+        expect(HudReports::HouseholdContext.pluck(:household_type).uniq).to eq(['adults_only'])
+      end
+
+      it 'identifies children only households' do
+        # Change HoH to child
+        child_2 = create_client_with_warehouse_link(dob: 12.years.ago)
+        hud_enrollment_hoh.update!(PersonalID: child_2.PersonalID)
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find_each(&:rebuild_service_history!)
+
+        builder.call
+        expect(HudReports::HouseholdContext.pluck(:household_type).uniq).to eq(['children_only'])
+      end
     end
 
     context 'with synthetic household IDs' do
@@ -177,10 +266,46 @@ RSpec.describe HudReports::HouseholdContextBuilder, type: :model do
         )
       end
 
-      it 'identifies the HoH as a parenting youth' do
+    it 'identifies the HoH as a parenting youth' do
         described_class.new(generator_youth, report).call
         youth_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_youth.id)
         expect(youth_context.is_parenting_youth).to be true
+      end
+
+      it 'does not identify as parenting youth if there is a member over 25' do
+        # Add a 40 year old member
+        adult_over_25 = create_client_with_warehouse_link(dob: 40.years.ago)
+        create(:hud_enrollment,
+               PersonalID: adult_over_25.PersonalID,
+               ProjectID: project.ProjectID,
+               HouseholdID: 'HH-YOUTH',
+               RelationshipToHoH: 3,
+               EntryDate: '2020-01-01',
+               data_source_id: data_source.id)
+
+        GrdaWarehouse::Tasks::ServiceHistory::Enrollment.find_each(&:rebuild_service_history!)
+
+        described_class.new(generator, report).call
+        youth_context = HudReports::HouseholdContext.find_by(service_history_enrollment_id: enrollment_youth.id)
+        expect(youth_context.is_parenting_youth).to be false
+        expect(youth_context.has_other_clients_over_25).to be true
+      end
+    end
+
+    describe 'HouseholdQueryService' do
+      let(:service) { HudReports::HouseholdQueryService.new(report, GrdaWarehouse::ServiceHistoryEnrollment.arel_table) }
+
+      before { builder.call }
+
+      it 'joins household context correctly' do
+        scope = service.with_household_context(GrdaWarehouse::ServiceHistoryEnrollment.entry)
+        expect(scope.to_sql).to include('INNER JOIN hud_report_household_contexts AS hh_ctx')
+        expect(scope.to_sql).to include("hh_ctx.report_instance_id = #{report.id}")
+      end
+
+      it 'provides correct sub-population clauses' do
+        expect(service.sub_populations['Without Children'].to_sql).to include('"hh_ctx"."household_type" = \'adults_only\'')
+        expect(service.sub_populations['Chronically Homeless'].to_sql).to match(/"hh_ctx"\."inherited_chronic_status" = (true|TRUE)/)
       end
     end
   end
