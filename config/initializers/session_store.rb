fad03e89709c2a2461e2554ee8774edf9cad9df8
Elliot Anders <elliot@greenriver.org>
2025-12-30 16:32:42 -0500

    Make rubocop really happy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -10,7 +10,7 @@ Rails.application.config.session_store(
       host: ENV.fetch('CACHE_HOST', 'redis'),
       port: ENV.fetch('CACHE_PORT', 6379),
       db: ENV.fetch('CACHE_DB', 1),
-    }
+    },
   ],
   expire_after: 12.hours, # If this expires while a session is open, the user is logged out
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
