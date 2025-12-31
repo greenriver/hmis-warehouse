0d747ff91d62cbc2f2cd323b9721bcda27d604b1
Dave Greiner <dgreiner@greenriver.org>
2025-12-31 12:48:37 -0500

    remove commented out skipped tests

--- drivers/hud_apr/spec/models/fy2026/datalab_caper/organization_t_hp.rb
+++ drivers/hud_apr/spec/models/fy2026/datalab_caper/organization_t_hp.rb
@@ -351,13 +351,7 @@ RSpec.shared_context 'datalab organization t hp caper', shared_context: :metadat
       compare_results(
         file_path: result_file_prefix + results_dir,
         question: 'Q24a',
-        # skip: [
-        #   'B15', # expected '5.0000' (5), got '4.0000' (4)
-        #   'C15', # expected '5.0000' (5), got '4.0000' (4)
-        #   'B16', # expected '48.0000' (48), got '47.0000'
-        #   'C16', # expected '16.0000' (16), got '15.0000' (15)
-        # ],
-        detail_columns: [:personal_id, :first_date_in_program, :last_date_in_program, :housing_assessment, :subsidy_information],
+        # detail_columns: [:personal_id, :first_date_in_program, :last_date_in_program, :housing_assessment, :subsidy_information],
       )
     end
 
