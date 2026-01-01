7d65f897b5949e7cac26a847a0467624f838ab3a
Elliot Anders <elliot@greenriver.org>
2026-01-01 18:53:57 -0500

    Make AgencyUserLookup safe for use outside of request contexts

--- app/services/health/agency_user_lookup.rb
+++ app/services/health/agency_user_lookup.rb
@@ -8,29 +8,46 @@
 
 module Health
   class AgencyUserLookup
-    def self.build_cache
-      return if RequestStore.store[:agency_user_lookup_cache].present?
-
-      active_user_ids = User.active.pluck(:id)
-
-      # Single query to get all agency users for active users
-      agency_users = Health::AgencyUser.where(user_id: active_user_ids)
-
-      # Build hash: { agency_id => [user_id1, user_id2, ...] }
-      lookup_hash = agency_users.each_with_object({}) do |au, hash|
-        hash[au.agency_id] ||= []
-        hash[au.agency_id] << au.user_id
+    class << self
+      def build_cache
+        # Cache the lookup hash in RequestStore for reuse within this request
+        cache = build_lookup_hash
+        RequestStore.store[:agency_user_lookup_cache] = cache if request_store_available?
+        cache
       end
 
-      RequestStore.store[:agency_user_lookup_cache] = {
-        user_ids: active_user_ids,
-        by_agency: lookup_hash,
-      }
-    end
+      def user_ids_for_agency(agency_id)
+        cache = if request_store_available? && RequestStore.store[:agency_user_lookup_cache].present?
+          RequestStore.store[:agency_user_lookup_cache]
+        else
+          build_lookup_hash
+        end
+        cache[:by_agency][agency_id] || []
+      end
 
-    def self.user_ids_for_agency(agency_id)
-      build_cache
-      RequestStore.store[:agency_user_lookup_cache][:by_agency][agency_id] || []
+      private
+
+      def request_store_available?
+        defined?(RequestStore) && RequestStore.store.is_a?(Hash)
+      end
+
+      def build_lookup_hash
+        active_user_ids = User.active.pluck(:id)
+
+        # Single query to get all agency users for active users
+        agency_users = Health::AgencyUser.where(user_id: active_user_ids)
+
+        # Build hash: { agency_id => [user_id1, user_id2, ...] }
+        lookup_hash = agency_users.each_with_object({}) do |au, hash|
+          hash[au.agency_id] ||= []
+          hash[au.agency_id] << au.user_id
+        end
+
+        {
+          user_ids: active_user_ids,
+          by_agency: lookup_hash,
+        }
+      end
     end
   end
 end
