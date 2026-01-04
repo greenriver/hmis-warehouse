597818fa0775a7990e3fd466cac2e1307efbb538
Theron Toomey <ttoomey@greenriver.org>
2026-01-04 23:58:03 +0000

    fix?

--- spec/support/delayed_job_helpers.rb
+++ spec/support/delayed_job_helpers.rb
@@ -8,7 +8,19 @@ module DelayedJobHelpers
   # Calls work_off until no more jobs are ready to run.
   def self.work_off_all_ready_jobs(job_limit: 1_000)
     worker = Delayed::Worker.new
-    successes, failures = worker.work_off(job_limit)
-    (successes + failures)
+    total_processed = 0
+
+    # We loop because some jobs may enqueue others.
+    # We cap at 1,000 iterations to avoid infinite loops
+    1_000.times do
+      break if Delayed::Job.uncached { Delayed::Job.where(failed_at: nil).count.zero? }
+      break if total_processed >= job_limit
+
+      successes, failures = worker.work_off(job_limit - total_processed)
+      total_processed += (successes + failures)
+      sleep(0.03) # Short sleep to allow for any DB propagation or async enqueues
+    end
+
+    total_processed
   end
 end
