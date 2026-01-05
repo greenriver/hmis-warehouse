3c810ca447f3aead876936c4d0a99b782fa33020
Gig Ashton <gig@greenriver.org>
2026-01-05 17:26:20 -0500

    Update comment

--- drivers/hmis_external_apis/app/models/hmis_external_apis/ac_hmis/exporters/current_waitlists_export.rb
+++ drivers/hmis_external_apis/app/models/hmis_external_apis/ac_hmis/exporters/current_waitlists_export.rb
@@ -84,9 +84,12 @@ module HmisExternalApis::AcHmis::Exporters
         group_by(&:candidate_pool_id)
     end
 
+    # Only include candidate pools that are referenced by unit groups with CE waitlists enabled ('active_for_current_eligibility' scope).
+    # Note: This almost matches the ceClients query that backs the global eligible clients list, but not exactly.
+    # The difference is here we only include candidate pools that are referenced by unit groups, and not those referenced by stale opportunities.
     def candidate_pool_ids_for_data_source
       @candidate_pool_ids_for_data_source ||= Hmis::Ce::Match::CandidatePool.
-        active_for_current_eligibility. # matches logic of ce_clients fields, which backs the global eligible clients list on the admin screen
+        active_for_current_eligibility.
         joins(unit_groups: :project).
         where(Hmis::Hud::Project.arel_table[:data_source_id].eq(data_source.id)).
         distinct.
