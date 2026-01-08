2fe31478485aed258136c64f174b970228b24468
martha <medwards@greenriver.org>
2026-01-08 17:36:35 -0500

    add spec for mutation

--- drivers/hmis/app/graphql/mutations/ce/assign_ce_default_contacts.rb
+++ drivers/hmis/app/graphql/mutations/ce/assign_ce_default_contacts.rb
@@ -8,52 +8,76 @@
 
 module Mutations
   module Ce
-    # todo @martha - add spec
     class AssignCeDefaultContacts < Mutations::CleanBaseMutation
       argument :input, Types::HmisSchema::CeDefaultContactsInput, required: true
 
       field :default_contacts, [Types::HmisSchema::CeDefaultContact], null: false
 
       def resolve(input:)
+        # Upfront, load all swimlanes and users from the input to confirm they are valid inputs (which depends on the owner)
+        swimlane_ids = input.contacts.map(&:swimlane_id).uniq
+        swimlanes = Hmis::WorkflowDefinition::Swimlane.where(id: swimlane_ids)
+        user_ids = input.contacts.map(&:user_ids).flatten.uniq
+        users = Hmis::User.where(id: user_ids)
+
         if input.project_id.present?
           owner = Hmis::Hud::Project.viewable_by(current_user).find(input.project_id)
           access_denied! unless policy_for(owner, policy_type: :hmis_project).can_manage_ce_default_contacts?
+
+          swimlanes = swimlanes.joins(:template).
+            merge(Hmis::WorkflowDefinition::Template.ce.published.viewable_by(current_user).used_in_projects([owner.id]))
+          users = users.can_perform_own_referral_tasks_for(owner).or(users.can_perform_any_referral_tasks_for(owner))
         else
           # If no project_id, it's a global assignment. Owner is the current user's HMIS data source
           owner = GrdaWarehouse::DataSource.find(current_user.hmis_data_source_id)
           access_denied! unless policy_for(GrdaWarehouse::DataSource, policy_type: :ce_admin).can_manage_contacts?
+
+          users = users.can_perform_own_referral_tasks.or(users.can_perform_any_referral_tasks)
         end
 
-        seen_assignment_ids = []
-        swimlane_ids = input.contacts.map(&:swimlane_id).uniq
-        swimlanes = Hmis::WorkflowDefinition::Swimlane.where(id: swimlane_ids).index_by(&:id)
+        raise "Swimlane(s) not found: #{swimlane_ids.join(', ')}" unless swimlanes.size == swimlane_ids.size
+        raise "User(s) not found: #{user_ids.join(', ')}" unless users.size == user_ids.size
 
-        user_ids = input.contacts.flat_map(&:user_ids).uniq
-        users = Hmis::User.where(id: user_ids).index_by(&:id)
+        # Load all existing assignments for this owner and swimlanes
+        existing_assignments = Hmis::Ce::DefaultSwimlaneAssignment.
+          where(owner: owner, swimlane_id: swimlane_ids).
+          index_by { |a| [a.user_id, a.swimlane_id] }
 
-        # Process each swimlane-to-users mapping
+        # Determine which assignments should exist, based on the input
+        desired_keys = Set.new # keys are [user_id, swimlane_id]
+        to_create = []
+
         input.contacts.each do |contact_input|
-          swimlane = swimlanes[contact_input.swimlane_id.to_i]
+          swimlane_id = contact_input.swimlane_id.to_i
 
           contact_input.user_ids.each do |user_id|
-            user = users[user_id.to_i]
+            user_id = user_id.to_i
+            key = [user_id, swimlane_id]
+            desired_keys << key
 
-            # Find or create the assignment
-            assignment = Hmis::Ce::DefaultSwimlaneAssignment.find_or_create_by!(
-              user: user,
-              swimlane: swimlane,
+            # If this assignment doesn't exist yet, add it to to_create
+            next if existing_assignments[key]
+
+            to_create << Hmis::Ce::DefaultSwimlaneAssignment.new(
+              swimlane_id: swimlane_id,
+              user_id: user_id,
               owner: owner,
             )
-
-            seen_assignment_ids << assignment.id
           end
         end
 
-        # Remove existing assignments for these swimlanes that are not included in the input
-        Hmis::Ce::DefaultSwimlaneAssignment.
-          where(owner: owner, swimlane_id: swimlane_ids).
-          where.not(id: seen_assignment_ids).
-          each(&:destroy!)
+        # Bulk create new assignments
+        Hmis::Ce::DefaultSwimlaneAssignment.import!(to_create) unless to_create.empty?
+
+        # Determine which existing assignments to remove
+        assignments_to_remove = existing_assignments.reject { |key, _| desired_keys.include?(key) }.values
+
+        # Bulk delete removed assignments
+        if assignments_to_remove.any?
+          Hmis::Ce::DefaultSwimlaneAssignment.
+            where(id: assignments_to_remove.map(&:id)).
+            destroy_all
+        end
 
         # Return all current assignments for this owner and the provided swimlanes
         assignments = Hmis::Ce::DefaultSwimlaneAssignment.where(owner: owner, swimlane_id: swimlane_ids)
---
+++ drivers/hmis/spec/requests/hmis/ce/assign_ce_default_contacts_spec.rb
@@ -0,0 +1,438 @@
+###
+# Copyright 2016 - 2026 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../login_and_permissions'
+require_relative '../../../support/hmis_base_setup'
+
+RSpec.describe 'AssignCeDefaultContacts Mutation', type: :request do
+  include_context 'hmis base setup'
+
+  subject(:mutation) do
+    <<~GRAPHQL
+      mutation AssignCeDefaultContacts($input: CeDefaultContactsInput!) {
+        assignCeDefaultContacts(input: $input) {
+          defaultContacts {
+            id
+            user {
+              id
+            }
+            swimlane {
+              id
+              name
+            }
+            project {
+              id
+            }
+            organization {
+              id
+            }
+            unitGroup {
+              id
+            }
+            global
+          }
+        }
+      }
+    GRAPHQL
+  end
+
+  let!(:workflow_template) { create(:hmis_workflow_definition_template, data_source: ds1, status: 'published', template_type: 'ce_referral') }
+  let!(:swimlane1) { create(:hmis_workflow_definition_swimlane, template: workflow_template, name: 'Case Managers') }
+  let!(:swimlane2) { create(:hmis_workflow_definition_swimlane, template: workflow_template, name: 'Housing Navigators') }
+  let!(:user1) { create(:hmis_user, data_source: ds1) }
+  let!(:user2) { create(:hmis_user, data_source: ds1) }
+  let!(:user3) { create(:hmis_user, data_source: ds1) }
+
+  # Grant users permission to perform referral tasks
+  let!(:user1_access) { create_access_control(user1, p1, with_permission: [:can_view_project, :can_perform_own_referral_tasks]) }
+  let!(:user2_access) { create_access_control(user2, p1, with_permission: [:can_view_project, :can_perform_own_referral_tasks]) }
+  let!(:user3_access) { create_access_control(user3, p1, with_permission: [:can_view_project, :can_perform_own_referral_tasks]) }
+
+  # Set up project to use the workflow template
+  let!(:unit_group) { create(:hmis_unit_group, project: p1, workflow_template: workflow_template) }
+
+  # Setup access control at data source level (covers both global and project-level operations)
+  let!(:access_control) do
+    create_access_control(
+      hmis_user,
+      ds1,
+      with_permission: [:can_view_clients, :can_view_project, :can_administrate_coordinated_entry],
+    )
+  end
+
+  before(:each) do
+    hmis_login(user)
+  end
+
+  # Shared input for global operations
+  let(:global_contacts_input) do
+    {
+      input: {
+        contacts: [
+          {
+            swimlaneId: swimlane1.id,
+            userIds: [user1.id, user2.id],
+          },
+          {
+            swimlaneId: swimlane2.id,
+            userIds: [user3.id],
+          },
+        ],
+      },
+    }
+  end
+
+  # Shared input for project operations
+  let(:project_contacts_input) do
+    {
+      input: {
+        projectId: p1.id,
+        contacts: [
+          {
+            swimlaneId: swimlane1.id,
+            userIds: [user1.id, user2.id],
+          },
+        ],
+      },
+    }
+  end
+
+  describe 'global contacts' do
+    let(:input) { global_contacts_input }
+
+    it 'creates global default contacts' do
+      expect do
+        response, result = post_graphql(input) { mutation }
+        expect(response.status).to eq(200), result.inspect
+
+        contacts = result.dig('data', 'assignCeDefaultContacts', 'defaultContacts')
+        expect(contacts.size).to eq(3)
+
+        # Check that all contacts are marked as global
+        expect(contacts.all? { |c| c['global'] == true }).to be true
+        expect(contacts.all? { |c| c['project'].nil? }).to be true
+
+        # Verify the assignments are returned grouped by swimlane
+        swimlane1_contacts = contacts.select { |c| c['swimlane']['id'] == swimlane1.id.to_s }
+        expect(swimlane1_contacts.size).to eq(2)
+        expect(swimlane1_contacts.map { |c| c['user']['id'] }).to contain_exactly(user1.id.to_s, user2.id.to_s)
+        swimlane2_contacts = contacts.select { |c| c['swimlane']['id'] == swimlane2.id.to_s }
+        expect(swimlane2_contacts.size).to eq(1)
+        expect(swimlane2_contacts.first['user']['id']).to eq(user3.id.to_s)
+      end.to change(Hmis::Ce::DefaultSwimlaneAssignment, :count).by(3)
+
+      # Verify the assignments were created with the data source as owner
+      assignments = Hmis::Ce::DefaultSwimlaneAssignment.where(owner: ds1)
+      expect(assignments.pluck(:user_id, :swimlane_id)).to contain_exactly(
+        [user1.id, swimlane1.id],
+        [user2.id, swimlane1.id],
+        [user3.id, swimlane2.id],
+      )
+    end
+
+    context 'with existing assignments' do
+      let!(:existing_assignment1) do
+        create(:hmis_ce_default_swimlane_assignment, user: user1, swimlane: swimlane1, owner: ds1)
+      end
+      let!(:existing_assignment2) do
+        create(:hmis_ce_default_swimlane_assignment, user: user2, swimlane: swimlane1, owner: ds1)
+      end
+      let!(:existing_assignment3) do
+        create(:hmis_ce_default_swimlane_assignment, user: user3, swimlane: swimlane2, owner: ds1)
+      end
+
+      context 'when removing all users from a swimlane' do
+        let(:input) do
+          {
+            input: {
+              contacts: [
+                { swimlaneId: swimlane1.id, userIds: [] },
+                { swimlaneId: swimlane2.id, userIds: [user3.id] },
+              ],
+            },
+          }
+        end
+
+        it 'removes assignments for the specified swimlane' do
+          expect do
+            response, result = post_graphql(input) { mutation }
+            expect(response.status).to eq(200), result.inspect
+          end.to change { Hmis::Ce::DefaultSwimlaneAssignment.where(owner: ds1, swimlane: swimlane1).count }.from(2).to(0)
+
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.where(owner: ds1, swimlane: swimlane2).count).to eq(1)
+        end
+      end
+
+      context 'when updating users' do
+        let(:input) do
+          {
+            input: {
+              contacts: [
+                { swimlaneId: swimlane1.id, userIds: [user1.id] },
+                { swimlaneId: swimlane2.id, userIds: [user3.id] },
+              ],
+            },
+          }
+        end
+
+        it 'removes unspecified users and keeps specified users without creating duplicates' do
+          response, result = post_graphql(input) { mutation }
+          expect(response.status).to eq(200), result.inspect
+
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.exists?(id: existing_assignment1.id)).to be true
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.exists?(id: existing_assignment2.id)).to be false
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.exists?(id: existing_assignment3.id)).to be true
+
+          contacts = result.dig('data', 'assignCeDefaultContacts', 'defaultContacts')
+          expect(contacts.size).to eq(2)
+        end
+      end
+    end
+  end
+
+  describe 'project contacts' do
+    let(:input) { project_contacts_input }
+
+    it 'creates project-specific default contacts' do
+      expect do
+        response, result = post_graphql(input) { mutation }
+        expect(response.status).to eq(200), result.inspect
+
+        contacts = result.dig('data', 'assignCeDefaultContacts', 'defaultContacts')
+        expect(contacts.size).to eq(2)
+
+        expect(contacts.all? { |c| c['global'] == false }).to be true
+        expect(contacts.all? { |c| c['project']['id'] == p1.id.to_s }).to be true
+      end.to change(Hmis::Ce::DefaultSwimlaneAssignment, :count).by(2)
+
+      assignments = Hmis::Ce::DefaultSwimlaneAssignment.where(owner: p1)
+      expect(assignments.pluck(:user_id, :swimlane_id)).to contain_exactly(
+        [user1.id, swimlane1.id],
+        [user2.id, swimlane1.id],
+      )
+    end
+
+    context 'with existing assignments' do
+      let!(:existing_assignment1) do
+        create(:hmis_ce_default_swimlane_assignment, user: user1, swimlane: swimlane1, owner: p1)
+      end
+      let!(:existing_assignment2) do
+        create(:hmis_ce_default_swimlane_assignment, user: user2, swimlane: swimlane1, owner: p1)
+      end
+
+      context 'when updating users' do
+        let(:input) do
+          {
+            input: {
+              projectId: p1.id,
+              contacts: [{ swimlaneId: swimlane1.id, userIds: [user1.id, user3.id] }],
+            },
+          }
+        end
+
+        it 'removes user2, adds user3, and does not recreate duplicates' do
+          response, result = post_graphql(input) { mutation }
+          expect(response.status).to eq(200), result.inspect
+
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.where(owner: p1).count).to eq(2)
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.exists?(id: existing_assignment1.id)).to be true
+          expect(Hmis::Ce::DefaultSwimlaneAssignment.exists?(id: existing_assignment2.id)).to be false
+
+          assignments = Hmis::Ce::DefaultSwimlaneAssignment.where(owner: p1)
+          expect(assignments.pluck(:user_id)).to contain_exactly(user1.id, user3.id)
+        end
+      end
+    end
+  end
+
+  describe 'permissions' do
+    # Shared examples for permission testing
+    shared_examples 'requires can_administrate_coordinated_entry permission' do
+      context 'when user has can_administrate_coordinated_entry permission' do
+        it 'allows the operation' do
+          response, result = post_graphql(input) { mutation }
+          expect(response.status).to eq(200), result.inspect
+          expect(result.dig('data', 'assignCeDefaultContacts', 'defaultContacts')).to be_present
+        end
+      end
+
+      context 'when user lacks can_administrate_coordinated_entry permission' do
+        before do
+          remove_permissions(access_control, :can_administrate_coordinated_entry)
+        end
+
+        it 'denies access' do
+          expect_access_denied post_graphql(input) { mutation }
+        end
+      end
+    end
+
+    context 'for global contacts' do
+      let(:input) { global_contacts_input }
+      it_behaves_like 'requires can_administrate_coordinated_entry permission'
+    end
+
+    context 'for project contacts' do
+      let(:input) { project_contacts_input }
+      it_behaves_like 'requires can_administrate_coordinated_entry permission'
+    end
+  end
+
+  describe 'validation' do
+    shared_examples 'raises an error and does not create default contacts' do
+      it 'raises an error and does not create default contacts' do
+        expect do
+          expect_gql_error post_graphql(input) { mutation }
+        end.not_to change(Hmis::Ce::DefaultSwimlaneAssignment, :count)
+      end
+    end
+
+    context 'when user lacks permission to perform referral tasks' do
+      let!(:user_without_permission) { create(:hmis_user, data_source: ds1) }
+
+      let(:input) do
+        {
+          input: {
+            projectId: p1.id,
+            contacts: [
+              {
+                swimlaneId: swimlane1.id,
+                userIds: [user_without_permission.id],
+              },
+            ],
+          },
+        }
+      end
+
+      it_behaves_like 'raises an error and does not create default contacts'
+    end
+
+    context 'when non-existent swimlane is passed' do
+      let(:input) do
+        {
+          input: {
+            projectId: p1.id,
+            contacts: [
+              {
+                swimlaneId: 999999,
+                userIds: [user1.id],
+              },
+            ],
+          },
+        }
+      end
+
+      it_behaves_like 'raises an error and does not create default contacts'
+    end
+
+    context 'when swimlane exists but is not applicable to the project' do
+      let!(:other_template) { create(:hmis_workflow_definition_template, data_source: ds1, status: 'published', template_type: 'ce_referral') }
+      let!(:other_swimlane) { create(:hmis_workflow_definition_swimlane, template: other_template, name: 'Other') }
+
+      let(:input) do
+        {
+          input: {
+            projectId: p1.id,
+            contacts: [
+              {
+                swimlaneId: other_swimlane.id,
+                userIds: [user1.id],
+              },
+            ],
+          },
+        }
+      end
+
+      it_behaves_like 'raises an error and does not create default contacts'
+    end
+
+    context 'when non-existent user is passed' do
+      let(:input) do
+        {
+          input: {
+            projectId: p1.id,
+            contacts: [
+              {
+                swimlaneId: swimlane1.id,
+                userIds: [999999],
+              },
+            ],
+          },
+        }
+      end
+
+      it_behaves_like 'raises an error and does not create default contacts'
+    end
+  end
+
+  describe 'query performance' do
+    shared_examples 'performs efficiently with batch operations' do
+      it 'makes a reasonable number of queries' do
+        expect do
+          response, result = post_graphql(input) { mutation }
+          expect(response.status).to eq(200), result.inspect
+        end.to make_database_queries(count: 8..15)
+      end
+    end
+
+    context 'with multiple swimlanes and users' do
+      let!(:additional_users) { create_list(:hmis_user, 10, data_source: ds1) }
+      let!(:additional_user_access) do
+        additional_users.map { |u| create_access_control(u, p1, with_permission: [:can_view_project, :can_perform_own_referral_tasks]) }
+      end
+      let!(:additional_swimlanes) { create_list(:hmis_workflow_definition_swimlane, 3, template: workflow_template) }
+
+      let(:input) do
+        {
+          input: {
+            contacts: additional_swimlanes.map do |swimlane|
+              {
+                swimlaneId: swimlane.id,
+                userIds: additional_users.sample(3).map(&:id),
+              }
+            end,
+          },
+        }
+      end
+
+      it_behaves_like 'performs efficiently with batch operations'
+    end
+
+    context 'with existing assignments' do
+      let!(:existing_assignments) do
+        5.times.map do |i|
+          swimlane = create(:hmis_workflow_definition_swimlane, template: workflow_template, name: "Swimlane #{i}")
+          user = create(:hmis_user, data_source: ds1)
+          create_access_control(user, p1, with_permission: [:can_view_project, :can_perform_own_referral_tasks])
+          create(:hmis_ce_default_swimlane_assignment, user: user, swimlane: swimlane, owner: ds1)
+          { swimlane: swimlane, user: user }
+        end
+      end
+
+      let(:input) do
+        {
+          input: {
+            contacts: existing_assignments.map do |assignment|
+              {
+                swimlaneId: assignment[:swimlane].id,
+                userIds: [assignment[:user].id, user1.id],
+              }
+            end,
+          },
+        }
+      end
+
+      it_behaves_like 'performs efficiently with batch operations'
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include GraphqlHelpers
+end
