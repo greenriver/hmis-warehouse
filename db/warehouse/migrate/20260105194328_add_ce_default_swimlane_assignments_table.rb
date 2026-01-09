da7e334043838d05b7742ebde1713efbad2b48de
martha <medwards@greenriver.org>
2026-01-09 14:05:52 -0500

    restore user index to table

--- db/warehouse/migrate/20260105194328_add_ce_default_swimlane_assignments_table.rb
+++ db/warehouse/migrate/20260105194328_add_ce_default_swimlane_assignments_table.rb
@@ -3,7 +3,7 @@
 class AddCeDefaultSwimlaneAssignmentsTable < ActiveRecord::Migration[7.2]
   def change
     create_table :ce_default_swimlane_assignments do |t|
-      t.references :user, null: false, index: false
+      t.references :user, null: false, index: true
       t.references :swimlane, null: false, foreign_key: { to_table: :wfd_swimlanes }
       t.references :owner, polymorphic: true, null: false
       t.timestamp :deleted_at
