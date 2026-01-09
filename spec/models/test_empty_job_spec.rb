bbdeaf6a4eaa6cebd9b64928aa8ae3dd3fd659ab
Theron Toomey <ttoomey@greenriver.org>
2026-01-09 00:53:03 +0000

    Revert "fix?"
    This reverts commit d5dc3c84658cdac7c16fa173e65fdc849cac6a87.

--- spec/models/test_empty_job_spec.rb
+++ spec/models/test_empty_job_spec.rb
@@ -4,19 +4,10 @@ require 'rails_helper'
 
 RSpec.describe TestEmptyJob, type: :model do
   describe 'when more than 100 jobs' do
-    # Disable transactional fixtures to ensure jobs are committed to the database
-    # and visible to the Delayed::Worker's separate database connection
     before do
-      self.use_transactional_tests = false
-      Delayed::Job.delete_all
       222.times do
         Delayed::Job.enqueue TestEmptyJob.new
       end
-    end
-
-    after do
-      Delayed::Job.delete_all
-      self.use_transactional_tests = true
     end
 
     it 'works off all jobs' do
