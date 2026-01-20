53ad07dc3be390e871e0a01ecc2596fe890f7376
Elliot <elliot@greenriver.org>
2026-01-20 09:22:16 -0500

    Pure Ruby Uploader for Health Care files (#6167)
    * Tests for existing healthcare uploads
    
    * File content validator
    
    * Replace and remove CarrierWave uploaders
    
    * Attempting to work around removal of carrierwave
    
    * Adjust file validation
    
    * Tighten file type validations
    
    * Adjustments from review

--- app/models/concerns/health_files.rb
+++ app/models/concerns/health_files.rb
@@ -9,6 +9,7 @@
 module HealthFiles
   extend ActiveSupport::Concern
   included do
+    # file_cache helps preserve uploads between form resubmissions
     accepts_nested_attributes_for :health_file, allow_destroy: true, reject_if: proc { |att| att['file'].blank? && att['file_cache'].blank? && att['note'].blank? }
     validates_associated :health_file
 
