1f6c29199729f59f14d8e653039d9e36c202268a
Gig Ashton <gig@greenriver.org>
2026-01-27 16:08:54 -0500

    Wip tests

--- drivers/hmis/app/jobs/hmis/transfer_enrollment_client_job.rb
+++ drivers/hmis/app/jobs/hmis/transfer_enrollment_client_job.rb
@@ -18,9 +18,6 @@ module Hmis
     #
     # Usage:
     #   Hmis::TransferEnrollmentClientJob.perform_now(enrollment_id: enrollment.id, to_client_id: new_client.id, dry_run: true)
-    #
-    # NOTE: This job intentionally uses update_column/update_all so it can update PersonalID references
-    # without updating timestamps on associated records or leaving a papertrail.
     def perform(enrollment_id:, to_client_id:, dry_run: false)
       enrollment = Hmis::Hud::Enrollment.find_by(id: enrollment_id)
       raise ArgumentError, 'Enrollment must be provided' unless enrollment
@@ -30,25 +27,23 @@ module Hmis
 
       from_client = enrollment.client
       raise ArgumentError, 'Could not find existing client for enrollment' unless from_client
-
       raise ArgumentError, 'Clients must be in the same data source' unless from_client.data_source_id == to_client.data_source_id
       raise ArgumentError, 'Enrollment must be in the same data source as to_client' unless enrollment.data_source_id == to_client.data_source_id
 
-      if dry_run
-        update_personal_ids(enrollment: enrollment, from_client: from_client, to_client: to_client, dry_run: true)
-        Rails.logger.info('Dry run complete')
-        return
-      end
-
       Hmis::Hud::Base.transaction do
-        update_personal_ids(enrollment: enrollment, from_client: from_client, to_client: to_client, dry_run: false)
-        enrollment.invalidate_processing!
+        # Update PersonalID on the Enrollment
+        update_personal_ids(enrollment: enrollment, from_client: from_client, to_client: to_client, dry_run: dry_run)
+
+        # Update PersonalID references on associated records (e.g. IncomeBenefit, CustomAssessment, etc)
+        enrollment_scope = Hmis::Hud::Enrollment.where(id: enrollment.id)
+        HmisDataCleanup::Util.fix_incorrect_personal_id_references!(enrollment_scope: enrollment_scope, dry_run: dry_run)
+
+        # Invalidate hash so it gets reprocessed next time
+        enrollment.invalidate_processing! unless dry_run
       end
 
-      # Queue service history processing (if not already queued)
-      Hmis::Hud::Enrollment.queue_service_history_processing!
-
-      Rails.logger.info 'Completed successfully'
+      # Queue service history processing to reprocess the enrollment
+      Hmis::Hud::Enrollment.queue_service_history_processing! unless dry_run
     end
 
     private
@@ -62,10 +57,6 @@ module Hmis
         enrollment.update_column(:PersonalID, to_client.personal_id)
         Rails.logger.info "Transferred Enrollment #{enrollment.id} from Client #{from_client.id} (PersonalID: #{from_client.personal_id}) to Client #{to_client.id} (PersonalID: #{to_client.personal_id})"
       end
-
-      # Update PersonalID references on associated records
-      enrollment_scope = Hmis::Hud::Enrollment.where(id: enrollment.id)
-      HmisDataCleanup::Util.fix_incorrect_personal_id_references!(enrollment_scope: enrollment_scope, dry_run: dry_run)
     end
   end
 end
