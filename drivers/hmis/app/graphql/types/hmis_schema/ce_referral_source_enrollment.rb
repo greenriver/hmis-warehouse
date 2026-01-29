5c4fb73236931cf3d2bf78a4107f5c0e64c2c75c
martha <medwards@greenriver.org>
2026-01-29 18:07:02 -0500

    Remove deprecated field otherHouseholdMemberNames from schema

--- drivers/hmis/app/graphql/types/hmis_schema/ce_referral_source_enrollment.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/ce_referral_source_enrollment.rb
@@ -40,10 +40,6 @@ module Types
     field :relationship_to_ho_h, HmisSchema::Enums::Hud::RelationshipToHoH, null: false, default_value: 99
     field :household_size, Integer, null: false
     field :household_members, [Types::HmisSchema::CeReferralSourceHouseholdMember], null: false
-
-    # TODO(#8767) - remove in next release
-    field :other_household_member_names, [String], null: false, deprecation_reason: 'Use household_members field instead'
-
     field :assessments, [Types::HmisSchema::AssessmentSummary], null: false
 
     field :source_client_meets_eligibility, Boolean, null: false, description: 'Whether the source client on their own meets the eligibility criteria for the opportunity.'
