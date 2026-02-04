b84f7c44678fa65ab2ee4f75ee46432e2fe44869
martha <medwards@greenriver.org>
2026-02-04 09:58:31 +0100

    update candidate event factory

--- drivers/hmis/spec/factories/hmis/ce/match_candidate_events.rb
+++ drivers/hmis/spec/factories/hmis/ce/match_candidate_events.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :hmis_ce_match_candidate_event, class: 'Hmis::Ce::Match::CandidateEvent' do
     association(:candidate_pool, factory: :hmis_ce_match_candidate_pool)
+    unit_group { association(:hmis_unit_group, candidate_pool: candidate_pool) }
     association(:client_proxy, factory: :hmis_ce_client_proxy)
     snapshot { {} }
     event_name { 'add' } # add, update, remove
