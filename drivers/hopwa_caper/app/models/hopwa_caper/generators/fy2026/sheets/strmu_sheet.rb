6c2c38fe91c613724ff4790fcf6609f1d24704e3
Theron Toomey <ttoomey@greenriver.org>
2026-02-11 21:20:44 +0000

    8775-strmu-distinct-assistance-fix

--- drivers/hopwa_caper/app/models/hopwa_caper/generators/fy2026/sheets/strmu_sheet.rb
+++ drivers/hopwa_caper/app/models/hopwa_caper/generators/fy2026/sheets/strmu_sheet.rb
@@ -77,10 +77,13 @@ module HopwaCaper::Generators::Fy2026::Sheets
         )
       end
 
+      # row 7
       # Multi-type: households with more than one distinct service type, aggregated by HOH client ID
+      service_type_filter_cases = service_type_filters.all.
+        map.with_index { |f, idx| "WHEN type_provided IN (#{f.codes.join(', ')}) THEN #{idx}" }
       multi_type_hoh_client_ids = services_with_hoh(relevant_services).
         group('hoh.destination_client_id').
-        having('COUNT(DISTINCT type_provided) > 1').
+        having("COUNT(DISTINCT CASE #{service_type_filter_cases.join(' ')} END) > 1").
         select('hoh.destination_client_id')
 
       add_household_enrollments_row(
