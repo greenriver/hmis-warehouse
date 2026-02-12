33ee16a94e53d256f376f06f9cadfea0090fc43e
martha <medwards@greenriver.org>
2026-02-12 17:25:19 +0100

    remove formal fk constraints, keep indices

--- db/warehouse/migrate/20260212154318_add_unit_group_assignment_table.rb
+++ db/warehouse/migrate/20260212154318_add_unit_group_assignment_table.rb
@@ -3,9 +3,11 @@
 class AddUnitGroupAssignmentTable < ActiveRecord::Migration[7.2]
   def change
     # records the history of which CE candidate pool a unit group was assigned to at a given time.
+    # Foreign key constraints are intentionally omitted to preserve historical records even if the
+    # unit group or candidate pool is deleted.
     create_table :ce_pool_unit_group_assignments do |t|
-      t.references :unit_group, null: false, foreign_key: { to_table: :hmis_unit_groups }
-      t.references :candidate_pool, null: false, foreign_key: { to_table: :ce_match_candidate_pools }
+      t.references :unit_group, null: false, index: true
+      t.references :candidate_pool, null: false, index: true
       t.datetime :started_at, null: false
       t.datetime :ended_at, null: true
       t.timestamps
--- drivers/hmis/app/models/hmis/ce/match/candidate_pool_unit_group_assignment.rb
+++ drivers/hmis/app/models/hmis/ce/match/candidate_pool_unit_group_assignment.rb
@@ -4,6 +4,8 @@ module Hmis::Ce::Match
   class CandidatePoolUnitGroupAssignment < GrdaWarehouseBase
     self.table_name = 'ce_pool_unit_group_assignments'
 
+    # Foreign key constraints in the DB on these two fields are intentionally omitted,
+    # to preserve historical records even if the  unit group or candidate pool is deleted.
     belongs_to :unit_group, class_name: 'Hmis::UnitGroup'
     belongs_to :candidate_pool, class_name: 'Hmis::Ce::Match::CandidatePool'
 
