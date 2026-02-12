7d637f44a8ade9654277b07d895f909200618536
Elliot <elliot@greenriver.org>
2026-02-12 10:29:02 -0500

    YYA Report Updates (#6230)
    * Add 14-17 age range
    
    * Be explicit in which values are included in TotalYYAServedPrevention
    
    * Additional tests
    
    * Include filter when checking ages to ensure dates are consistent

--- app/models/filters/filter_base.rb
+++ app/models/filters/filter_base.rb
@@ -987,6 +987,7 @@ module Filters
         zero_to_four: '0 - 4',
         five_to_ten: '5 - 10',
         eleven_to_fourteen: '11 - 14',
+        fourteen_to_seventeen: '14 - 17',
         fifteen_to_seventeen: '15 - 17',
         under_eighteen: '< 18',
         eighteen_to_twenty_four: '18 - 24',
