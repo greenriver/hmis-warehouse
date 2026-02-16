8730796a620e55a6e0562ffa23e61883a999113d
martha <medwards@greenriver.org>
2026-02-16 14:01:51 +0100

    add migration for cded reporting_key

---
+++ db/warehouse/migrate/20260216105433_add_reporting_key_to_custom_data_element_definitions.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AddReportingKeyToCustomDataElementDefinitions < ActiveRecord::Migration[7.2]
+  def change
+    # Add nullable reporting_key column
+    add_column :CustomDataElementDefinitions, :reporting_key, :string
+
+    # When reporting_key is present, it should be unique for its owner type
+    safety_assured do
+      add_index :CustomDataElementDefinitions,
+                [:owner_type, :reporting_key],
+                unique: true,
+                where: '"reporting_key" IS NOT NULL',
+                name: 'index_cded_on_owner_type_and_reporting_key'
+    end
+  end
+end
+
+# rails db:migrate:up:warehouse VERSION=20260216105433
+# rails db:migrate:down:warehouse VERSION=20260216105433
