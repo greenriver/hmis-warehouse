4244e2d05b94ef323adc86b18ff9f0e11bd63307
martha <medwards@greenriver.org>
2026-02-26 14:23:08 +0100

    Add Rake task to bulk void clients (#6270)
    * Add Rake task to bulk void clients
    
    * Fix mistake
    
    * synthetic assessment
    
    * revert editor_user_ids change
    
    * move to ac folder and rm rake task
    
    * use rails logger
    
    * log clients not voided
    
    * Add comments
    
    * wrap in transaction
    
    * exit hhm
    
    * add instruction about log level
    
    * add spec
    
    * save created_by_user and updated_by_user on exit
    
    * improve logging
    
    * Don't exit enrollment if any hhm is wip
    
    * self-review
    
    * PR suggestion

---
+++ drivers/hmis_external_apis/app/jobs/hmis_external_apis/ac_hmis/bulk_voider.rb
@@ -0,0 +1,171 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+# Bulk void a list of destination clients from the waitlist and exit them from the Coordinated Entry project.
+#
+# Usage: HmisExternalApis::AcHmis::BulkVoider.new.perform(destination_client_ids: [1637, 39094], ce_project_id: 201, dry_run: true)
+#
+# For more useful log output, set the log level to INFO rather than DEBUG, so that we can easily copy and paste the list of enrollment IDs that are processed.
+# Rails.logger.level = Logger::INFO
+# HmisExternalApis::AcHmis::BulkVoider.new.perform(destination_client_ids: [1637, 39094], ce_project_id: 201, dry_run: true)
+# Rails.logger.level = Logger::DEBUG
+#
+# destination_client_ids: a list of destination client IDs to void
+# ce_project_id: the ID of the Coordinated Entry project
+# dry_run: if true, logs the enrollments that would be processed, without taking action
+#
+# Notes:
+# - This could re-void an enrollment that’s already voided, or a client that wasn't deemed eligible to begin with.
+#   If the client has an open enrollment in the CE project, then a Void Assessment will be generated, regardless of whether they are on the waitlist.
+# - This will *not* void clients that are exited from the CE project, even if they are still on the waitlist.
+module HmisExternalApis::AcHmis
+  class BulkVoider
+    VOID_REASON_TEXT = 'This household has been exited from Coordinated Entry and removed from the Homeless Housing Program Waitlist as part of the CE Waitlist Management Process due to no contact with Coordinated Entry in at least 45 days.'
+
+    # Hard-coded expectations of the Void Assessment form shape
+    VOID_FORM_IDENTIFIER = 'void_assessment'
+    VOID_CDED_KEY = 'void_assessment_void_all_referrals'
+    VOID_REASON_CDED_KEY = 'void_assessment_void_reason'
+
+    def perform(destination_client_ids:, ce_project_id:, dry_run: false)
+      # Expect the given project ID to be open on the current date, and to be a Coordinated Entry project (14)
+      project = Hmis::Hud::Project.hmis.open_on_date.where(project_type: 14).find(ce_project_id)
+      @data_source_id = project.data_source_id
+      @hud_system_user = Hmis::Hud::User.system_user(data_source_id: @data_source_id)
+      @current_date = Date.current
+
+      # Find the Void Assessment and validate the expected CDEDs exist
+      @void_definition = Hmis::Form::Definition.published.find_by!(identifier: VOID_FORM_IDENTIFIER)
+      cded_scope = Hmis::Hud::CustomDataElementDefinition.for_type(Hmis::Hud::CustomAssessment.sti_name).where(data_source_id: @data_source_id)
+      @void_cded = cded_scope.find_by!(key: VOID_CDED_KEY)
+      @void_reason_cded = cded_scope.find_by!(key: VOID_REASON_CDED_KEY)
+
+      Rails.logger.info "Bulk voiding #{destination_client_ids.count} destination clients for CE project #{ce_project_id}"
+
+      source_client_ids = Hmis::WarehouseClient.where(destination_id: destination_client_ids).pluck(:source_id)
+
+      # Find open enrollments for the given clients in the CE project
+      enrollments = Hmis::Hud::Enrollment.
+        open_excluding_wip.
+        where(project: project).
+        joins(:client).
+        where(client: { id: source_client_ids })
+
+      # Log client IDs that we won't process because they don't have an open enrollment in the CE project
+      source_ids_with_enrollment = enrollments.distinct.pluck(Arel.sql('client.id'))
+      destination_ids_with_enrollment = Hmis::WarehouseClient.
+        where(source_id: source_ids_with_enrollment, destination_id: destination_client_ids).
+        pluck(:destination_id)
+      destination_ids_without_enrollment = destination_client_ids.map(&:to_s) - destination_ids_with_enrollment.map(&:to_s)
+
+      Rails.logger.info "Destination client IDs with no enrollment to process (#{destination_ids_without_enrollment.size}): #{destination_ids_without_enrollment.join(', ')}" if destination_ids_without_enrollment.any?
+
+      return if enrollments.empty?
+
+      if dry_run
+        Rails.logger.info "DRY RUN: Would have processed the following #{enrollments.count} enrollments (and their household members):\n#{enrollments.pluck(:enrollment_id).join("\n")}"
+        return
+      end
+
+      # Enrollments to make a void assessment for (clients in the provided list)
+      enrollment_ids_to_void = enrollments.pluck(:id).to_set
+
+      Rails.logger.info "Processing enrollments (#{enrollments.count}):"
+
+      skipped = []
+
+      Hmis::Hud::Base.transaction do
+        enrollments.preload(household: :enrollments).each do |enrollment|
+          # Can't auto-exit incomplete members. Skip entirely and log that we skipped
+          if enrollment.household.any_wip?
+            skipped << [enrollment.enrollment_id, enrollment.client.warehouse_id]
+            next
+          end
+
+          enrollment.household.enrollments.open_excluding_wip.each do |e|
+            # Create a void assessment only if the client was in the provided list
+            create_void_assessment(e) if enrollment_ids_to_void.include?(e.id)
+
+            # Exit the household member
+            create_exit(e)
+
+            Rails.logger.info e.enrollment_id + (enrollment_ids_to_void.include?(e.id) ? '' : " (household member of #{enrollment.enrollment_id})")
+          end
+        end
+      end
+
+      if skipped.any?
+        Rails.logger.info "Skipped #{skipped.count} enrollments because they have incomplete household members:"
+        skipped.each do |enrollment_id, client_id|
+          Rails.logger.info "#{enrollment_id} (client #{client_id})"
+        end
+      end
+      Rails.logger.info 'Completed processing all enrollments'
+    end
+
+    private
+
+    def create_exit(enrollment)
+      # Create an Exit record exiting the client from the CE project.
+      # TODO - This is copied from Auto Exit Job, and should be refactored to a shared place, that can also be used for bulk-exiting enrollments (#6917)
+      exit_record = Hmis::Hud::Exit.new(
+        personal_id: enrollment.personal_id,
+        enrollment_id: enrollment.enrollment_id,
+        data_source_id: enrollment.data_source_id,
+        user_id: @hud_system_user.user_id,
+        exit_date: @current_date,
+        destination: ::HudHelper.util.destination_no_exit_interview_completed,
+      )
+      exit_assessment = Hmis::Hud::CustomAssessment.new(
+        user_id: @hud_system_user.user_id,
+        assessment_date: @current_date,
+        data_collection_stage: 3,
+        data_source_id: enrollment.data_source_id,
+        personal_id: enrollment.personal_id,
+        enrollment_id: enrollment.enrollment_id,
+        created_by_hud_user: @hud_system_user,
+        updated_by_hud_user: @hud_system_user,
+      )
+      exit_assessment.build_form_processor(exit: exit_record)
+      raise ActiveRecord::RecordInvalid, exit_record if exit_record.invalid?
+
+      exit_assessment.save!
+    end
+
+    def create_void_assessment(enrollment)
+      assessment = Hmis::Hud::CustomAssessment.new(
+        user: @hud_system_user,
+        assessment_date: @current_date,
+        data_collection_stage: 99,
+        data_source_id: enrollment.data_source_id,
+        personal_id: enrollment.personal_id,
+        enrollment_id: enrollment.enrollment_id,
+        created_by_hud_user: @hud_system_user,
+        updated_by_hud_user: @hud_system_user,
+        definition: @void_definition,
+        wip: false,
+      )
+      assessment.build_form_processor(definition: @void_definition)
+      assessment.save!
+      assessment.form_processor.save!
+
+      assessment.custom_data_elements.create!(
+        data_element_definition: @void_cded,
+        user: @hud_system_user,
+        data_source_id: assessment.data_source_id,
+        value_boolean: true,
+      )
+      assessment.custom_data_elements.create!(
+        data_element_definition: @void_reason_cded,
+        user: @hud_system_user,
+        data_source_id: assessment.data_source_id,
+        value_string: VOID_REASON_TEXT,
+      )
+    end
+  end
+end
---
+++ drivers/hmis_external_apis/spec/jobs/hmis_external_apis/ac_hmis/bulk_voider_spec.rb
@@ -0,0 +1,145 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HmisExternalApis::AcHmis::BulkVoider, type: :job do
+  let!(:data_source) { create(:hmis_primary_data_source) }
+  let!(:ce_project) { create(:hmis_hud_project, data_source: data_source, project_type: 14) }
+  let!(:void_definition) do
+    create(
+      :hmis_form_definition,
+      identifier: HmisExternalApis::AcHmis::BulkVoider::VOID_FORM_IDENTIFIER,
+      role: :CUSTOM_ASSESSMENT,
+      status: Hmis::Form::Definition::PUBLISHED,
+      data_source: data_source,
+      definition: {
+        item: [
+          { type: 'DATE', link_id: 'linkid_date', required: true, text: 'Assessment Date', assessment_date: true, mapping: { field_name: 'assessmentDate' } },
+          { type: 'BOOLEAN', link_id: 'void_all_referrals', required: true, text: 'Void All Referrals', mapping: { custom_field_key: 'void_assessment_void_all_referrals' } },
+          { type: 'STRING', link_id: 'void_reason', required: true, text: 'Reason for Voiding', mapping: { custom_field_key: 'void_assessment_void_reason' } },
+        ],
+      },
+    )
+  end
+  # Form definition factory created the CDEDs; find them by key
+  let(:void_cded) { Hmis::Hud::CustomDataElementDefinition.find_by(key: 'void_assessment_void_all_referrals') }
+  let(:void_reason_cded) { Hmis::Hud::CustomDataElementDefinition.find_by(key: 'void_assessment_void_reason') }
+
+  def perform_bulk_void(destination_client_ids)
+    described_class.new.perform(
+      destination_client_ids: destination_client_ids,
+      ce_project_id: ce_project.id,
+      dry_run: false,
+    )
+  end
+
+  describe '#perform' do
+    let!(:client) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+    let!(:enrollment) { create(:hmis_hud_enrollment, data_source: data_source, project: ce_project, client: client) }
+
+    it 'creates exit and void assessment for client with open CE enrollment' do
+      expect do
+        perform_bulk_void([client.warehouse_id])
+      end.to change(Hmis::Hud::Exit, :count).by(1).
+        and change(Hmis::Hud::CustomAssessment, :count).by(2) # exit assessment + void assessment
+
+      enrollment.reload
+      expect(enrollment.exit).to be_present
+      expect(enrollment.exit.exit_date).to eq(Date.current)
+      expect(enrollment.exit.destination).to eq(::HudHelper.util.destination_no_exit_interview_completed)
+
+      exit_assessment = Hmis::Hud::CustomAssessment.where(enrollment_id: enrollment.enrollment_id, data_collection_stage: 3).sole
+      expect(exit_assessment.created_by_hud_user).to eq(Hmis::Hud::User.system_user(data_source_id: data_source.id))
+      expect(exit_assessment.updated_by_hud_user).to eq(Hmis::Hud::User.system_user(data_source_id: data_source.id))
+
+      void_assessment = Hmis::Hud::CustomAssessment.where(enrollment_id: enrollment.enrollment_id, data_collection_stage: 99).sole
+      expect(void_assessment.custom_data_elements.count).to eq(2)
+      expect(void_assessment.custom_data_elements.find_by(data_element_definition: void_cded).value_boolean).to eq(true)
+      expect(void_assessment.custom_data_elements.find_by(data_element_definition: void_reason_cded).value_string).
+        to include('CE Waitlist Management Process')
+      expect(void_assessment.created_by_hud_user).to eq(Hmis::Hud::User.system_user(data_source_id: data_source.id))
+      expect(void_assessment.updated_by_hud_user).to eq(Hmis::Hud::User.system_user(data_source_id: data_source.id))
+    end
+
+    context 'when client is already exited' do
+      let!(:exited_client) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+      let!(:exited_enrollment) { create(:hmis_hud_enrollment, data_source: data_source, project: ce_project, client: exited_client, exit_date: Date.current) }
+
+      it 'does not create void assessment for the exited client' do
+        expect do
+          perform_bulk_void([client.warehouse_id, exited_client.warehouse_id])
+        end.to change(Hmis::Hud::Exit, :count).by(1).
+          and change(Hmis::Hud::CustomAssessment.where(data_collection_stage: 99), :count).by(1)
+
+        # No void assessment was created for the already exited client
+        expect(exited_enrollment.reload.custom_assessments.where(data_collection_stage: 99).count).to eq(0)
+      end
+    end
+
+    context 'when there are multiple household members' do
+      let!(:spouse) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+      let!(:spouse_enrollment) { create(:hmis_hud_enrollment, data_source: data_source, project: ce_project, client: spouse, household_id: enrollment.household_id, relationship_to_hoh: 3) }
+      let!(:child) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+      let!(:child_enrollment) { create(:hmis_hud_enrollment, data_source: data_source, project: ce_project, client: child, household_id: enrollment.household_id, relationship_to_hoh: 2) }
+
+      it 'exits all household members and creates void assessment only for the requested client' do
+        expect do
+          perform_bulk_void([client.warehouse_id])
+          [enrollment, spouse_enrollment, child_enrollment].each(&:reload)
+        end.to change(Hmis::Hud::Exit, :count).by(3).
+          and change(Hmis::Hud::CustomAssessment.where(data_collection_stage: 99), :count).by(1)
+
+        expect(enrollment.exit).to be_present
+        expect(spouse_enrollment.exit).to be_present
+        expect(child_enrollment.exit).to be_present
+
+        void_assessment = Hmis::Hud::CustomAssessment.where(enrollment_id: enrollment.enrollment_id, data_collection_stage: 99).sole
+        expect(void_assessment.custom_data_elements.count).to eq(2)
+        expect(void_assessment.custom_data_elements.find_by(data_element_definition: void_cded).value_boolean).to eq(true)
+        expect(void_assessment.custom_data_elements.find_by(data_element_definition: void_reason_cded).value_string).
+          to include('CE Waitlist Management Process')
+
+        expect(spouse_enrollment.custom_assessments.where(data_collection_stage: 99).count).to eq(0)
+        expect(child_enrollment.custom_assessments.where(data_collection_stage: 99).count).to eq(0)
+      end
+
+      context 'when there is an incomplete intake for a hhm' do
+        let!(:other_hhm) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+        let!(:other_hhm_enrollment) { create(:hmis_hud_wip_enrollment, data_source: data_source, project: ce_project, client: other_hhm, household_id: enrollment.household_id, relationship_to_hoh: 2) }
+
+        let!(:unrelated) { create(:hmis_hud_client_with_warehouse_client, data_source: data_source) }
+        let!(:unrelated_enrollment) { create(:hmis_hud_enrollment, data_source: data_source, project: ce_project, client: unrelated) }
+
+        it 'does not exit or void any of the enrollments' do
+          expect do
+            perform_bulk_void([client.warehouse_id, unrelated.warehouse_id])
+          end.to change(Hmis::Hud::Exit, :count).by(1).
+            and change(Hmis::Hud::CustomAssessment.where(data_collection_stage: 99), :count).by(1)
+
+          expect(enrollment.reload.exit).not_to be_present
+          expect(spouse_enrollment.reload.exit).not_to be_present
+          expect(child_enrollment.reload.exit).not_to be_present
+          expect(other_hhm_enrollment.reload.exit).not_to be_present
+
+          # Only the unrelated enrollment is exited
+          expect(unrelated_enrollment.reload.exit).to be_present
+        end
+      end
+    end
+
+    describe 'with dry_run: true' do
+      it 'does not create exits or void assessments' do
+        expect do
+          described_class.new.perform(destination_client_ids: [client.warehouse_id], ce_project_id: ce_project.id, dry_run: true)
+        end.to not_change(Hmis::Hud::Exit, :count).
+          and not_change(Hmis::Hud::CustomAssessment, :count)
+      end
+    end
+  end
+end
