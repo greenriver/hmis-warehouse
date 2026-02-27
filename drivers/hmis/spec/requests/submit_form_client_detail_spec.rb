525809e04b0c4fc4ea801b403db25ff1ccda1a09
martha <medwards@greenriver.org>
2026-02-27 11:19:55 +0100

    apply suggestion of simplified tests

--- drivers/hmis/spec/requests/submit_form_client_detail_spec.rb
+++ drivers/hmis/spec/requests/submit_form_client_detail_spec.rb
@@ -9,88 +9,63 @@
 require 'rails_helper'
 require_relative 'hmis/login_and_permissions'
 require_relative '../support/hmis_base_setup'
+require_relative '../support/submit_form_spec_helpers'
 
-RSpec.describe Hmis::GraphqlController, type: :request do
+RSpec.describe 'SubmitForm for CLIENT_DETAIL', type: :request do
   include_context 'hmis base setup'
+  before(:each) do
+    hmis_login(user)
+  end
+
+  CDE_KEY = 'preferred_contact'
 
   let!(:access_control) { create_access_control(hmis_user, p1) }
   # Enroll c1 in p1 so the client is viewable by the user
   let!(:e1) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c1, user: u1 }
-  let!(:cded) { create :hmis_custom_data_element_definition, owner_type: 'Hmis::Hud::Client', key: 'preferred_contact', field_type: :string, data_source: ds1 }
+  let!(:cded) { create :hmis_custom_data_element_definition, owner_type: 'Hmis::Hud::Client', key: CDE_KEY, field_type: :string, data_source: ds1 }
   let!(:definition) do
     create :hmis_form_definition, role: :CLIENT_DETAIL, definition: {
       'item' => [
         {
           'type' => 'STRING',
-          'link_id' => 'preferred_contact',
+          'link_id' => CDE_KEY,
           'text' => 'Preferred Contact Method',
-          'mapping' => { 'custom_field_key' => 'preferred_contact' },
+          'mapping' => { 'custom_field_key' => CDE_KEY },
         },
       ],
     }
   end
-  # CLIENT_DETAIL form instances have no entity (global)
-  let!(:instance) { create :hmis_form_instance, definition: definition, entity: nil, active: true }
 
-  let(:mutation) do
-    <<~GRAPHQL
-      mutation SubmitForm($input: SubmitFormInput!) {
-        submitForm(input: $input) {
-          record {
-            ... on Client {
-              id
-            }
-          }
-          errors {
-            type
-            attribute
-            message
-          }
-        }
-      }
-    GRAPHQL
+  let(:input) do
+    {
+      form_definition_id: definition.id,
+      record_id: c1.id,
+      values: { CDE_KEY => 'email' },
+      hud_values: { CDE_KEY => 'email' },
+    }
   end
 
-  before(:each) do
-    hmis_login(user)
+  it 'saves the custom data element on the client' do
+    expect do
+      submit_form(input)
+      c1.reload
+    end.to change(c1.custom_data_elements, :count).by(1).
+      and change(cded.values, :count).by(1)
+
+    expect(c1.custom_data_elements.sole.value_string).to eq('email')
+    expect(c1.custom_data_elements.sole.data_element_definition).to eq(cded)
   end
 
-  describe 'SubmitForm for CLIENT_DETAIL' do
-    let(:input) do
-      {
-        form_definition_id: definition.id,
-        record_id: c1.id,
-        values: { 'preferred_contact' => 'email' },
-        hud_values: { 'preferred_contact' => 'email' },
-      }
-    end
+  context 'when user lacks can_edit_clients permission' do
+    before { remove_permissions(access_control, :can_edit_clients) }
 
-    it 'saves the custom data element on the client' do
-      response, result = post_graphql(input: { input: input }) { mutation }
-      expect(response.status).to eq(200), result.inspect
-
-      record = result.dig('data', 'submitForm', 'record')
-      errors = result.dig('data', 'submitForm', 'errors')
-
-      expect(errors).to be_empty
-      expect(record).to be_present
-      expect(record['id']).to eq(c1.id.to_s)
-
-      cde = c1.reload.custom_data_elements.find_by(data_element_definition: cded)
-      expect(cde).to be_present
-      expect(cde.value_string).to eq('email')
-    end
-
-    context 'when user lacks can_edit_clients permission' do
-      before { remove_permissions(access_control, :can_edit_clients) }
-
-      it 'returns access denied' do
-        expect_gql_error post_graphql(input: { input: input }) { mutation }, message: /not authorized/
-      end
+    it 'returns access denied' do
+      expect_gql_error submit_form(input, expect_raise: true), message: /not authorized/
     end
   end
 end
 
 RSpec.configure do |c|
   c.include GraphqlHelpers
+  c.include SubmitFormSpecHelpers
 end
--- drivers/hmis/spec/requests/submit_form_occurrence_point_spec.rb
+++ drivers/hmis/spec/requests/submit_form_occurrence_point_spec.rb
@@ -9,71 +9,46 @@
 require 'rails_helper'
 require_relative 'hmis/login_and_permissions'
 require_relative '../support/hmis_base_setup'
+require_relative '../support/submit_form_spec_helpers'
 
-RSpec.describe Hmis::GraphqlController, type: :request do
+RSpec.describe 'SubmitForm for OCCURRENCE_POINT', type: :request do
   include_context 'hmis base setup'
-
-  let!(:access_control) { create_access_control(hmis_user, p1) }
-  let!(:e1) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c1, user: u1 }
-  let!(:definition) { create :occurrence_point_form }
-  let!(:instance) { create :hmis_form_instance, role: :OCCURRENCE_POINT, entity: p1, active: true, definition: definition }
-
-  let(:move_in_date) { '2024-06-01' }
-
-  let(:mutation) do
-    <<~GRAPHQL
-      mutation SubmitForm($input: SubmitFormInput!) {
-        submitForm(input: $input) {
-          record {
-            ... on Enrollment {
-              id
-              moveInDate
-            }
-          }
-          #{error_fields}
-        }
-      }
-    GRAPHQL
-  end
-
   before(:each) do
     hmis_login(user)
   end
 
-  describe 'SubmitForm for OCCURRENCE_POINT' do
-    let(:input) do
-      {
-        form_definition_id: definition.id,
-        record_id: e1.id,
-        values: { 'date' => move_in_date },
-        hud_values: { 'Enrollment.moveInDate' => move_in_date },
-      }
-    end
+  let!(:access_control) { create_access_control(hmis_user, p1) }
+  let!(:e1) { create :hmis_hud_enrollment, data_source: ds1, project: p1, client: c1, user: u1 }
+  let!(:definition) { create :occurrence_point_form }
 
-    it 'saves the move-in date on the enrollment' do
-      response, result = post_graphql(input: { input: input }) { mutation }
-      expect(response.status).to eq(200), result.inspect
+  let(:move_in_date) { '2024-06-01' }
 
-      record = result.dig('data', 'submitForm', 'record')
-      errors = result.dig('data', 'submitForm', 'errors')
+  let(:input) do
+    {
+      form_definition_id: definition.id,
+      record_id: e1.id,
+      values: { 'date' => move_in_date },
+      hud_values: { 'Enrollment.moveInDate' => move_in_date },
+    }
+  end
 
-      expect(errors).to be_empty
-      expect(record).to be_present
-      expect(record['id']).to eq(e1.id.to_s)
-      expect(record['moveInDate']).to eq(move_in_date)
-      expect(e1.reload.move_in_date.strftime('%Y-%m-%d')).to eq(move_in_date)
-    end
+  it 'saves the move-in date on the enrollment' do
+    expect do
+      submit_form(input)
+      e1.reload
+    end.to change(e1, :move_in_date).from(nil).to(Date.parse(move_in_date))
+  end
 
-    context 'when user lacks can_edit_enrollments permission' do
-      before { remove_permissions(access_control, :can_edit_enrollments) }
+  context 'when user lacks can_edit_enrollments permission' do
+    before { remove_permissions(access_control, :can_edit_enrollments) }
 
-      it 'returns access denied' do
-        expect_gql_error post_graphql(input: { input: input }) { mutation }, message: /not authorized/
-      end
+    it 'returns access denied' do
+      expect_gql_error submit_form(input, expect_raise: true), message: /not authorized/
     end
   end
 end
 
 RSpec.configure do |c|
   c.include GraphqlHelpers
+  c.include SubmitFormSpecHelpers
 end
