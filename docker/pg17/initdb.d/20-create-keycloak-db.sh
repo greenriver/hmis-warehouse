a60b9b5e71b5a439f032d1dc656f3c4af3f3e9e7
Elliot Anders <elliot@greenriver.org>
2026-03-01 08:26:13 -0500

    Remove unused method; update docs

--- docker/pg17/initdb.d/20-create-keycloak-db.sh
+++ docker/pg17/initdb.d/20-create-keycloak-db.sh
@@ -2,7 +2,7 @@
 # Create the Keycloak database if it doesn't already exist.
 # This runs once during postgres cluster initialization (new data volume).
 # For existing installations without this database, run:
-#   docker compose run --rm keycloak-db-init
+#   docker compose exec db psql -U postgres -c 'CREATE DATABASE keycloak'
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
   SELECT 'CREATE DATABASE keycloak' WHERE NOT EXISTS (
