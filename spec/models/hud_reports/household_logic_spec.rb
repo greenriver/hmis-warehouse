05016275576a62f028c37e7374c498a27cba6fe4
Theron Toomey <ttoomey@greenriver.org>
2026-03-13 15:49:47 +0000

    improve houshold logic specs

--- spec/models/hud_reports/household_logic_spec.rb
+++ spec/models/hud_reports/household_logic_spec.rb
@@ -96,6 +96,98 @@ RSpec.describe HudReports::HouseholdLogic do
       result = described_class.calculate_chronic_status([hoh, adult2], adult2, hoh)
       expect(result[:status]).to be true
     end
+
+    context 'when calculating PIT chronic status' do
+      let(:hoh) do
+        {
+          client_id: 1,
+          entry_date: Date.parse('2020-01-01'),
+          age: 40,
+          pit_chronic_status: false,
+          pit_chronic_detail: 'no',
+        }
+      end
+
+      let(:child) do
+        {
+          client_id: 2,
+          entry_date: Date.parse('2020-01-01'),
+          age: 10,
+          pit_chronic_status: true,
+          pit_chronic_detail: 'yes',
+        }
+      end
+
+      it 'does not allow HoH to inherit from child' do
+        result = described_class.calculate_chronic_status([hoh, child], hoh, hoh, chronic_status_key: :pit_chronic_status)
+        expect(result[:status]).to be false
+      end
+
+      it 'allows child to inherit from another chronic adult' do
+        hoh[:pit_chronic_status] = false
+        adult2 = {
+          client_id: 3,
+          entry_date: Date.parse('2020-01-01'),
+          age: 35,
+          pit_chronic_status: true,
+          pit_chronic_detail: 'yes',
+        }
+
+        result = described_class.calculate_chronic_status([hoh, child, adult2], child, hoh, chronic_status_key: :pit_chronic_status)
+        expect(result[:status]).to be true
+        expect(result[:detail]).to eq('yes')
+      end
+    end
+
+    context 'with indeterminate (DK/R) data' do
+      it 'inherits DK/R status from HoH for children' do
+        hoh[:chronic_status] = false
+        hoh[:chronic_detail] = 'dk_or_r'
+        child[:chronic_status] = false
+        child[:chronic_detail] = 'no'
+
+        result = described_class.calculate_chronic_status(members, child, hoh)
+        expect(result[:status]).to be false
+        expect(result[:detail]).to eq('dk_or_r')
+      end
+
+      it 'inherits from HoH if child has missing status' do
+        hoh[:chronic_status] = true
+        hoh[:chronic_detail] = 'yes'
+        child[:chronic_status] = false
+        child[:chronic_detail] = 'missing'
+
+        result = described_class.calculate_chronic_status(members, child, hoh)
+        expect(result[:status]).to be true
+        expect(result[:detail]).to eq('yes')
+      end
+    end
+  end
+
+  describe '.calculate_is_parenting_youth' do
+    let(:youth_hoh) { { age: 20, relationship_to_hoh: 1 } }
+    let(:child) { { age: 2, relationship_to_hoh: 2 } }
+    let(:members) { [youth_hoh, child] }
+
+    it 'returns true for a youth household with children' do
+      expect(described_class.calculate_is_parenting_youth(youth_hoh, members)).to be true
+    end
+
+    it 'returns false if there is a member over 24' do
+      adult = { age: 25, relationship_to_hoh: 3 }
+      expect(described_class.calculate_is_parenting_youth(youth_hoh, [youth_hoh, child, adult])).to be false
+    end
+
+    it 'returns false if there are no children (relationship 2)' do
+      spouse = { age: 20, relationship_to_hoh: 3 }
+      expect(described_class.calculate_is_parenting_youth(youth_hoh, [youth_hoh, spouse])).to be false
+    end
+
+    it 'considers youth up to age 24 as children for parenting youth status' do
+      # APR nuance: child can be up to 24 in a youth household
+      older_child = { age: 24, relationship_to_hoh: 2 }
+      expect(described_class.calculate_is_parenting_youth(youth_hoh, [youth_hoh, older_child])).to be true
+    end
   end
 
   describe '.calculate_move_in_date' do
