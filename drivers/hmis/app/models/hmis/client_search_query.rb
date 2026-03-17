859ceb52aa2e1c597141869d78eda0f563856878
martha <medwards@greenriver.org>
2026-03-17 10:53:19 +0000

    fiddle w todos

--- drivers/hmis/app/models/hmis/client_search_query.rb
+++ drivers/hmis/app/models/hmis/client_search_query.rb
@@ -6,7 +6,7 @@ module Hmis
     belongs_to :created_by, class_name: 'Hmis::User'
 
     ALLOWED_PARAMS = ['text_search'].freeze
-    # todo @martha
+    # todo @martha - get advanced search working
     # ALLOWED_PARAMS = ['q', 'client'].freeze
     # ALLOWED_CLIENT_PARAMS = ['first_name', 'last_name', 'dob', 'ssn'].freeze
   end
