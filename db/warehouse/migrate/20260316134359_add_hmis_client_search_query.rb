97884c3d30dbe5a5bbe420241da6863e75c16fb6
martha <medwards@greenriver.org>
2026-03-25 18:32:39 +0100

    add data_source_id to hmis_client_search_query

--- db/warehouse/migrate/20260316134359_add_hmis_client_search_query.rb
+++ db/warehouse/migrate/20260316134359_add_hmis_client_search_query.rb
@@ -7,9 +7,10 @@ class AddHmisClientSearchQuery < ActiveRecord::Migration[7.2]
     create_table :hmis_client_search_queries, id: :uuid do |t|
       t.timestamps
       t.references :created_by, null: false, index: false
+      t.references :data_source, null: false, index: false
       t.jsonb :params, null: false
       t.string :fingerprint, null: false, comment: 'hash of normalized search parameters used for deduplication and efficient query retrieval'
     end
-    add_index :hmis_client_search_queries, :fingerprint, name: 'uidx_hmis_client_search_queries', unique: true
+    add_index :hmis_client_search_queries, [:data_source_id, :created_by_id, :fingerprint], name: 'uidx_hmis_client_search_queries', unique: true
   end
 end
