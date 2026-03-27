b9a5555a1b9704d47fa7360995909a410f5acbc8
Gig <gig@greenriver.org>
2026-03-27 14:04:25 -0400

    Ensure compliant HUD Service form rules exist on deploy (#6309)

--- drivers/hmis/lib/hmis_util/service_types.rb
+++ drivers/hmis/lib/hmis_util/service_types.rb
@@ -11,137 +11,32 @@ module HmisUtil
     # Ensures all HUD service types exist in CustomServiceTypes table,
     # and organizes them by Category according to the record type.
     def self.seed_hud_service_types(data_source_id)
-      system_user = Hmis::User.system_user
-      system_user.hmis_data_source_id = data_source_id
-      hud_user = Hmis::Hud::User.from_user(system_user)
+      raise "Not an HMIS data source: #{data_source_id}" unless GrdaWarehouse::DataSource.hmis.exists?(id: data_source_id)
+
+      Rails.logger.info "Seeding HUD Service Types for DS##{data_source_id}"
+      hud_user = Hmis::Hud::User.system_user(data_source_id: data_source_id)
 
       HudHelper.util.record_types.each do |record_type, category_name|
-        category = Hmis::Hud::CustomServiceCategory.where(
+        # Find or create service category (eg "SSVF Service")
+        category = Hmis::Hud::CustomServiceCategory.create_with(
+          user_id: hud_user.user_id,
+        ).find_or_create_by!(
           name: category_name,
           data_source_id: data_source_id,
-        ).first_or_create(user_id: hud_user.user_id)
+        )
 
+        # For each HUD TypeProvided under this RecordType, ensure a CustomServiceType record exists
         type_provided_map = Hmis::Hud::Validators::ServiceValidator::TYPE_PROVIDED_MAP[record_type]
         type_provided_map.each do |type_provided, service_name|
-          Hmis::Hud::CustomServiceType.where(
+          Hmis::Hud::CustomServiceType.create_with(
+            user_id: hud_user.user_id,
             name: service_name,
+            custom_service_category: category,
+          ).find_or_create_by!(
             hud_record_type: record_type,
             hud_type_provided: type_provided,
-            custom_service_category: category,
             data_source_id: data_source_id,
-          ).first_or_create(user_id: hud_user.user_id)
-        end
-      end
-    end
-
-    BED_NIGHT_CONFIG = {
-      record_type: 200, # BedNight
-      project_types: [
-        1, # Emergency Shelter - NbN
-      ],
-    }.freeze
-
-    P1_PATH_SERVICE_CONFIG = {
-      record_type: 141, # P1 Services Provided - PATH Funded
-      funders: [21], # HHS: PATH - Street Outreach & Supportive Services Only
-    }.freeze
-
-    P2_PATH_REFERRAL_CONFIG = {
-      record_type: 161, # P2 Referrals Provided - PATH
-      funders: [21], # HHS: PATH - Street Outreach & Supportive Services Only
-    }.freeze
-
-    R14_RHY_SERVICE_CONFIG = {
-      record_type: 142, # R14 RHY Service Connections
-      # All HHS RHY funders except the Street Outreach one (25), plus YHDP (43)
-      funders: HudHelper.util.funding_sources.select { |_, v| v.start_with?('HHS: RHY') }.keys - [25] + [43],
-    }.freeze
-
-    W1_HOPWA_SERVICE_CONFIG = {
-      record_type: 143, # W1 Services Provided – HOPWA
-      # Note: commenting out projects to leave the baseline more flexible. HOPWA services can optionally
-      # be recorded for any service type.
-      # project_types: [
-      #   0, # Emergency Shelter - Entry Exit
-      #   1, # Emergency Shelter - NbN
-      #   2, # Transitional Housing
-      #   3, # PH - Permanent Supportive Housing
-      #   6, # Services Only
-      #   12, # Homelessness Prevention
-      # ],
-      funders: HudHelper.util.funding_sources.select { |_, v| v.start_with?('HUD: HOPWA') }.keys,
-    }.freeze
-
-    W2_HOPWA_FINANCIAL_CONFIG = {
-      record_type: 151, # W2 Financial Assistance - HOPWA
-      # Note: commenting out projects to leave the baseline more flexible. HOPWA services can optionally
-      # be recorded for any service type.
-      # project_types: [
-      #   6, # Services Only
-      #   12, # Homelessness Prevention
-      # ],
-      funders: HudHelper.util.funding_sources.select { |_, v| v.start_with?('HUD: HOPWA') }.keys,
-    }.freeze
-
-    V2_SSVF_SERVICE_CONFIG = {
-      record_type: 144, # V2 Services Provided – SSVF
-      # 33 - VA: SSVF - Collection required only for RRH(13) & HP(12)
-      # Optional for all other VA
-      funders: HudHelper.util.funding_sources.select { |_, v| v.start_with?('VA:') }.keys,
-    }.freeze
-
-    V3_SSVF_FINANCIAL_CONFIG = {
-      record_type: 152, # V3 Financial Assistance – SSVF
-      funders: HudHelper.util.funding_sources.select { |_, v| v.start_with?('VA:') }.keys,
-    }.freeze
-
-    V8_HUD_VASH_VOUCHER_CONFIG = {
-      record_type: 210, # V8 HUD-VASH Voucher Tracking
-      project_types: [3], # PSH
-      funders: [20], # HUD: HUD/VASH
-    }.freeze
-
-    C2_MOVING_ON_CONFIG = {
-      record_type: 300, # C2 Moving On Assistance Provided
-      funders: [2], # HUD: CoC - Permanent Supportive Housing
-    }.freeze
-
-    HUD_SERVICE_INSTANCE_CONFIG = [
-      BED_NIGHT_CONFIG,
-      P1_PATH_SERVICE_CONFIG,
-      P2_PATH_REFERRAL_CONFIG,
-      R14_RHY_SERVICE_CONFIG,
-      W1_HOPWA_SERVICE_CONFIG,
-      W2_HOPWA_FINANCIAL_CONFIG,
-      V2_SSVF_SERVICE_CONFIG,
-      V3_SSVF_FINANCIAL_CONFIG,
-      V8_HUD_VASH_VOUCHER_CONFIG,
-      C2_MOVING_ON_CONFIG,
-    ].freeze
-
-    # Set up initial Form Instances exist for each HUD Service Type according to the configuration.
-    # These are NOT system-level instances; they can be deleted or changed as needed.
-    # TODO(#8874): refactor and move this to HudComplianceFormInstanceMaintainer so it runs on deploy
-    def self.seed_hud_service_form_instances
-      HUD_SERVICE_INSTANCE_CONFIG.each do |config|
-        (record_type, project_types, funders) = config.values_at(:record_type, :project_types, :funders)
-
-        custom_service_category = Hmis::Hud::CustomServiceType.where(hud_record_type: record_type).first&.custom_service_category
-        raise "Category not found for record type #{record_type}" unless custom_service_category.present?
-
-        # Create a Form Instance for each combination of project type + funder
-        (project_types || [nil]).each do |project_type|
-          (funders || [nil]).each do |funder|
-            puts "Creating instance for '#{custom_service_category&.name}' ProjectType:#{project_type} Funder:#{funder}"
-            Hmis::Form::Instance.where(
-              definition_identifier: 'service', # The default definition used for all HUD Services
-              custom_service_category_id: custom_service_category&.id,
-              project_type: project_type,
-              funder: funder,
-              system: false, # TODO(#8874) change these to system rules so they cannot be deleted
-              active: true,
-            ).first_or_create
-          end
+          )
         end
       end
     end
--- drivers/hmis/spec/lib/hmis_util/hud_compliance_form_instance_maintainer_spec.rb
+++ drivers/hmis/spec/lib/hmis_util/hud_compliance_form_instance_maintainer_spec.rb
@@ -11,15 +11,18 @@ require 'rails_helper'
 RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form_state do
   let!(:data_source) { create(:hmis_data_source) }
 
+  def maintainer(**opts)
+    described_class.new(data_source_id: data_source.id, **opts)
+  end
+
   before(:each) do
     # Start all tests with the correct, HUD-compliant database state.
     # In individual test setup, delete instances when testing re-creation behavior.
-    described_class.new.ensure_all_system_instances_exist!
+    maintainer.ensure_all_system_instances_exist!
   end
 
   describe '#ensure_all_system_instances_exist!' do
     it 'is idempotent' do
-      maintainer = described_class.new
       initial_count = Hmis::Form::Instance.count
 
       maintainer.ensure_all_system_instances_exist!
@@ -35,7 +38,7 @@ RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form
       deleted_count = 2
       count_after_deletion = Hmis::Form::Instance.count
 
-      described_class.new.ensure_all_system_instances_exist!
+      maintainer.ensure_all_system_instances_exist!
 
       expect(Hmis::Form::Instance.count).to eq(count_after_deletion + deleted_count)
       expect(Hmis::Form::Instance.system.where(definition_identifier: 'client')).to exist
@@ -47,7 +50,7 @@ RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form
       Hmis::Form::Instance.system.where(definition_identifier: 'base-exit').delete_all
       count_before = Hmis::Form::Instance.count
 
-      described_class.new.ensure_all_system_instances_exist!
+      maintainer.ensure_all_system_instances_exist!
 
       expect(Hmis::Form::Instance.count).to eq(count_before + 2)
       expect(Hmis::Form::Instance.system.where(definition_identifier: 'base-intake')).to exist
@@ -59,7 +62,7 @@ RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form
       client_instance.update!(active: false, system: false)
 
       expect do
-        described_class.new.ensure_all_system_instances_exist!
+        maintainer.ensure_all_system_instances_exist!
         client_instance.reload
       end.to change(client_instance, :active).from(false).to(true).
         and change(client_instance, :system).from(false).to(true).
@@ -70,13 +73,51 @@ RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form
       Hmis::Form::Instance.system.where(definition_identifier: 'current_living_situation').delete_all
       expected_specs = HudHelper.util.current_living_situation_funder_applicability_requirements
 
-      described_class.new.ensure_all_system_instances_exist!
+      maintainer.ensure_all_system_instances_exist!
 
       rules = Hmis::Form::Instance.active.system.
         where(definition_identifier: 'current_living_situation')
       expect(rules.count).to eq(expected_specs.size)
       actual_specs = rules.map { |r| { project_type: r.project_type, funder: r.funder }.compact_blank }.to_set
       expect(actual_specs).to eq(expected_specs.to_set)
+    end
+
+    it 'creates system instances for service form that match HudHelper.util.service_form_funder_applicability_requirements per data source' do
+      HmisUtil::ServiceTypes.seed_hud_service_types(data_source.id)
+      Hmis::Form::Instance.system.where(definition_identifier: 'service').delete_all
+
+      requirements = HudHelper.util.service_form_funder_applicability_requirements
+      expected_specs = Set.new
+      requirements.each do |config|
+        category = Hmis::Hud::CustomServiceType.where(
+          data_source_id: data_source.id,
+          hud_record_type: config[:record_type],
+        ).first&.custom_service_category
+        raise "missing CustomServiceCategory for record_type #{config[:record_type]} in DS##{data_source.id}" unless category
+
+        config[:applicability_requirements].each do |requirement|
+          expected_specs << {
+            custom_service_category_id: category.id,
+            data_collected_about: config[:data_collected_about].to_s,
+            project_type: requirement[:project_type],
+            funder: requirement[:funder],
+          }
+        end
+      end
+
+      maintainer.ensure_all_system_instances_exist!
+
+      rules = Hmis::Form::Instance.active.system.where(definition_identifier: 'service')
+      expect(rules.count).to eq(expected_specs.size)
+      actual_specs = rules.map do |r|
+        {
+          custom_service_category_id: r.custom_service_category_id,
+          data_collected_about: r.data_collected_about.to_s,
+          project_type: r.project_type,
+          funder: r.funder,
+        }
+      end.to_set
+      expect(actual_specs).to eq(expected_specs)
     end
 
     context 'with dry run' do
@@ -88,7 +129,7 @@ RSpec.describe HmisUtil::HudComplianceFormInstanceMaintainer, :manages_hmis_form
         count_after_deletion = Hmis::Form::Instance.count
 
         allow(Rails.logger).to receive(:info)
-        described_class.new(dry_run: true).ensure_all_system_instances_exist!
+        maintainer(dry_run: true).ensure_all_system_instances_exist!
 
         expect(Rails.logger).to have_received(:info).with(
           a_string_including('HUD Form Compliance (dry run)', 'Would create (2):', 'client', 'project', 'Would update (1):', 'move_in_date'),
