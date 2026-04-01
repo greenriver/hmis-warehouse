33b256793161a0d38e8caf0bf984e6cb97937e27
martha <medwards@greenriver.org>
2026-04-01 12:54:12 +0200

    try cleaning up in after(all)

--- drivers/hmis/spec/support/hmis_json_forms_seed.rb
+++ drivers/hmis/spec/support/hmis_json_forms_seed.rb
@@ -8,10 +8,10 @@
 
 # Ensures the canonical HMIS test data source exists and loads JSON form definitions (see HmisUtil::JsonForms).
 #
-# When used alongside `include_context 'hmis base setup'`, this context's `let(:ds1)` often overrides the
-# factory-backed `let!(:ds1)` from base setup
+# When used alongside 'hmis base setup', this context's `let(:ds1)` overrides the factory-based `ds1`.
 RSpec.shared_context 'hmis json forms seed', shared_context: :metadata do
-  before(:all) do
+  # Seed forms in a before(:all) block to improve performance
+  before(:all) do # todo @martha - would be faster to do before(:suite) but this needs to be an rspec config. explore later
     ds = GrdaWarehouse::DataSource.find_or_create_by!(
       hmis: GraphqlHelpers::HMIS_HOSTNAME,
       name: 'HMIS',
@@ -21,5 +21,14 @@ RSpec.shared_context 'hmis json forms seed', shared_context: :metadata do
     HmisUtil::JsonForms.seed_all(data_source_id: ds.id)
   end
 
+  # Use find_by! since we already created ds1 in before_all
   let(:ds1) { GrdaWarehouse::DataSource.hmis.find_by(hmis: GraphqlHelpers::HMIS_HOSTNAME) }
+
+  after(:all) do
+    # Clean up the data source so that other tests can create their own ds unimpeded
+    data_source = GrdaWarehouse::DataSource.find_by(hmis: GraphqlHelpers::HMIS_HOSTNAME)
+    Hmis::Form::Definition.where(data_source: data_source).destroy_all
+    Hmis::Form::Instance.where(data_source: data_source).destroy_all
+    data_source.destroy!
+  end
 end
