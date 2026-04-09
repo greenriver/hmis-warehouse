caebb039d40eb84bb324054efb5cad31fa7b5209
martha <medwards@greenriver.org>
2026-04-09 22:39:17 +0200

    fix occurrence point forms

--- drivers/hmis/spec/requests/hmis/enrollment_occurrence_point_spec.rb
+++ drivers/hmis/spec/requests/hmis/enrollment_occurrence_point_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Hmis::GraphqlController, type: :request do
 
     # custom form where 'question_2' is only collected for project p1
     let!(:definition) do
-      create(:hmis_form_definition, identifier: 'occurrence_point_form_with_custom_rule', definition: { 'item' => [
+      create(:hmis_form_definition, identifier: 'occurrence_point_form_with_custom_rule', data_source: ds1, definition: { 'item' => [
                {
                  'text': 'Question 1',
                  'type': 'STRING',
@@ -69,8 +69,8 @@ RSpec.describe Hmis::GraphqlController, type: :request do
                },
              ] })
     end
-    let!(:instance1) { create(:hmis_form_instance, role: :OCCURRENCE_POINT, entity: p1, active: true, definition: definition) }
-    let!(:instance2) { create(:hmis_form_instance, role: :OCCURRENCE_POINT, entity: p2, active: true, definition: definition) }
+    let!(:instance1) { create(:hmis_form_instance, role: :OCCURRENCE_POINT, entity: p1, active: true, definition: definition, data_source: ds1) }
+    let!(:instance2) { create(:hmis_form_instance, role: :OCCURRENCE_POINT, entity: p2, active: true, definition: definition, data_source: ds1) }
 
     def query_forms(enrollment_id)
       response, result = post_graphql(enrollment_id: enrollment_id) { query }
@@ -104,6 +104,22 @@ RSpec.describe Hmis::GraphqlController, type: :request do
         items = forms.find { |form| form['id'] == expected_id }.dig('definition', 'definition', 'item')
         expect(items.size).to eq(1)
         expect(items).not_to include(a_hash_including('linkId' => 'question_2'))
+      end
+    end
+
+    context 'with occurrence point forms set up in multi-hmis' do
+      let!(:ds2) { create(:hmis_data_source) }
+      let!(:ds2_project) { create(:hmis_hud_project, data_source: ds2) }
+      # occurrence point form with the same identifier in another data source
+      let!(:ds2_definition) { create(:hmis_form_definition, identifier: 'form_in_ds2', role: :OCCURRENCE_POINT, data_source: ds2) }
+      # default rule (entity: nil), so it applies to all enrollments
+      let!(:ds2_instance) { create(:hmis_form_instance, role: :OCCURRENCE_POINT, active: true, definition: ds2_definition, entity: nil, data_source: ds2) }
+
+      it 'only returns forms for the current data source' do
+        forms = query_forms(e2.id)
+        expect(forms.count).to eq(1)
+        form_definition_ids = forms.map { |form| form.dig('definition', 'id') }
+        expect(form_definition_ids).to contain_exactly(definition.id.to_s) # does not include ds2_definition
       end
     end
   end
