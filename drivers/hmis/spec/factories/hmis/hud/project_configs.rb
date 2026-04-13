5481fee415e7d7f332d4db6abd64e3f59b30d55f
martha <medwards@greenriver.org>
2026-04-13 13:08:27 +0200

    update factories

--- drivers/hmis/spec/factories/hmis/hud/project_configs.rb
+++ drivers/hmis/spec/factories/hmis/hud/project_configs.rb
@@ -7,30 +7,31 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :hmis_project_auto_enter_config, class: 'Hmis::ProjectAutoEnterConfig' do
+  # Shared defaults for all Hmis::ProjectConfig STI types.
+  factory :hmis_project_config_base, class: 'Hmis::ProjectConfig' do
     association :data_source, factory: :hmis_data_source
     created_at { Time.current }
     updated_at { Time.current }
+
+    # Match data source when project or organization is set.
+    after(:build) do |config, evaluator|
+      config.data_source = evaluator.project.data_source if evaluator.project.present?
+      config.data_source = evaluator.organization.data_source if evaluator.organization.present?
+    end
   end
 
-  factory :hmis_project_auto_exit_config, class: 'Hmis::ProjectAutoExitConfig' do
-    association :data_source, factory: :hmis_data_source
-    created_at { Time.current }
-    updated_at { Time.current }
+  factory :hmis_project_auto_enter_config, parent: :hmis_project_config_base, class: 'Hmis::ProjectAutoEnterConfig' do
+  end
+
+  factory :hmis_project_auto_exit_config, parent: :hmis_project_config_base, class: 'Hmis::ProjectAutoExitConfig' do
     config_options { { 'length_of_absence_days': 30 }.to_json }
   end
 
-  factory :hmis_project_staff_assignment_config, class: 'Hmis::ProjectStaffAssignmentConfig' do
-    association :data_source, factory: :hmis_data_source
+  factory :hmis_project_staff_assignment_config, parent: :hmis_project_config_base, class: 'Hmis::ProjectStaffAssignmentConfig' do
     association :project, factory: :hmis_hud_project
-    created_at { Time.current }
-    updated_at { Time.current }
   end
 
-  factory :hmis_project_ce_config, class: 'Hmis::ProjectCeConfig' do
-    association :data_source, factory: :hmis_data_source
-    created_at { Time.current }
-    updated_at { Time.current }
+  factory :hmis_project_ce_config, parent: :hmis_project_config_base, class: 'Hmis::ProjectCeConfig' do
     enabled { true }
 
     transient do
@@ -48,9 +49,6 @@ FactoryBot.define do
     end
   end
 
-  factory :hmis_project_sends_direct_ce_referrals_config, class: 'Hmis::ProjectSendsDirectCeReferralsConfig' do
-    association :data_source, factory: :hmis_data_source
-    created_at { Time.current }
-    updated_at { Time.current }
+  factory :hmis_project_sends_direct_ce_referrals_config, parent: :hmis_project_config_base, class: 'Hmis::ProjectSendsDirectCeReferralsConfig' do
   end
 end
