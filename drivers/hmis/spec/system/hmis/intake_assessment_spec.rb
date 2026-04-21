b5340ed33481f5b9c39d29a71eba3194754006ba
Gig Ashton <gig@greenriver.org>
2026-04-21 09:28:07 -0400

    Fix system test failure

--- drivers/hmis/spec/system/hmis/intake_assessment_spec.rb
+++ drivers/hmis/spec/system/hmis/intake_assessment_spec.rb
@@ -133,6 +133,14 @@ RSpec.feature 'Intake Assessment for Household', type: :system do
         # Submit both intakes and wait for submission to complete
         submit_household_intakes(household_size: 2)
 
+        # Confirm all intakes are submitted
+        # FIXME: seems like there is a frontend bug causing the page to sometimes
+        # navigate to the submitted HoH's assessment, instead of staying on the summary
+        # tab. That needs to be fixed on the frontend. This test uses the below assertion
+        # to wait for submission to complete (rather than asserting on the Summary Tab
+        # showing submitted status) to get around that bug.
+        expect(page).not_to have_button('Submit (2) Intake Assessments')
+
         # HoH enrollment entry date is updated
         expect(hoh_enrollment.reload.entry_date).to eq(new_entry_date.to_date)
         # Non-HoH enrollment entry date is not updated
