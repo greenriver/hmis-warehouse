102cd97e19df721d1934c688e4a162044cf26258
martha <medwards@greenriver.org>
2026-04-22 14:00:55 -0400

    fix/update spec for consistency

--- drivers/hmis/spec/models/hmis/auth_policies/organization_policy_spec.rb
+++ drivers/hmis/spec/models/hmis/auth_policies/organization_policy_spec.rb
@@ -14,64 +14,74 @@ RSpec.describe Hmis::AuthPolicies::HmisOrganizationPolicy, type: :model do
   let!(:other_organization) { create(:hmis_hud_organization, data_source: data_source) }
   let(:user) { create(:hmis_user, data_source: data_source) }
 
-  describe 'Instance policy' do
+  describe 'Instance' do
     let(:policy) { user.policy_for(organization, policy_type: :hmis_organization) }
 
-    context 'with can_edit_organization permission' do
-      let!(:access_control) { create_access_control(user, organization, with_permission: :can_edit_organization) }
+    describe '#can_edit?' do
+      it 'returns false without permissions' do
+        expect(policy.can_edit?).to be false
+      end
 
-      it 'grants can_edit?' do
-        expect(policy.can_edit?).to be true
-        expect(policy.can_create_project?).to be false
-        expect(policy.can_destroy?).to be false
+      context 'with can_edit_organization permission' do
+        let!(:access_control) { create_access_control(user, organization, with_permission: :can_edit_organization) }
+
+        it 'returns true' do
+          expect(policy.can_edit?).to be true
+        end
+      end
+
+      context 'when permissions are only granted on a different organization' do
+        let!(:access_control) { create_access_control(user, other_organization) } # full perms at other org
+
+        it 'returns false' do
+          expect(policy.can_edit?).to be false
+        end
       end
     end
 
-    context 'with can_delete_organization permission' do
-      let!(:access_control) { create_access_control(user, organization, with_permission: :can_delete_organization) }
-
-      it 'grants can_destroy?' do
-        expect(policy.can_edit?).to be false
+    describe '#can_create_project?' do
+      it 'returns false without permissions' do
         expect(policy.can_create_project?).to be false
-        expect(policy.can_destroy?).to be true
+      end
+
+      context 'with can_view_project and can_edit_project_details' do
+        let!(:access_control) { create_access_control(user, organization, with_permission: [:can_view_project, :can_edit_project_details]) }
+
+        it 'returns true' do
+          expect(policy.can_create_project?).to be true
+        end
       end
     end
 
-    context 'with can_edit_project_details permission' do
-      let!(:access_control) { create_access_control(user, organization, with_permission: [:can_view_project, :can_edit_project_details]) }
-
-      it 'grants can_create_project?' do
-        expect(policy.can_edit?).to be false
-        expect(policy.can_create_project?).to be true
+    describe '#can_destroy?' do
+      it 'returns false without permissions' do
         expect(policy.can_destroy?).to be false
       end
-    end
 
-    context 'without permissions (even if permissions are granted at another organization)' do
-      let!(:access_control) { create_access_control(user, other_organization) } # full perms at other org
+      context 'with can_delete_organization permission' do
+        let!(:access_control) { create_access_control(user, organization, with_permission: :can_delete_organization) }
 
-      it 'denies all' do
-        expect(policy.can_edit?).to be false
-        expect(policy.can_create_project?).to be false
-        expect(policy.can_destroy?).to be false
+        it 'returns true' do
+          expect(policy.can_destroy?).to be true
+        end
       end
     end
   end
 
-  describe 'Global policy' do
+  describe 'Global' do
     let(:policy) { user.policy_for(Hmis::Hud::Organization, policy_type: :hmis_organization) }
 
-    context 'with can_edit_organization permission' do
-      before { create_access_control(user, data_source, with_permission: :can_edit_organization) }
-
-      it 'grants can_create?' do
-        expect(policy.can_create?).to be true
-      end
-    end
-
-    context 'without can_edit_organization permission' do
-      it 'denies can_create?' do
+    describe '#can_create?' do
+      it 'returns false without permissions' do
         expect(policy.can_create?).to be false
+      end
+
+      context 'with can_edit_organization at the data source' do
+        before { create_access_control(user, data_source, with_permission: :can_edit_organization) }
+
+        it 'returns true' do
+          expect(policy.can_create?).to be true
+        end
       end
     end
   end
--- drivers/hmis/spec/requests/hmis/ce/ce_referral_access_spec.rb
+++ drivers/hmis/spec/requests/hmis/ce/ce_referral_access_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe Hmis::GraphqlController, type: :request do
   before(:each) { hmis_login(user) }
 
   let(:access_query) do
+    # Resolve all access fields except canViewReferralDetails, which will always be true when fetching a single referral by ID.
+    # See project_outgoing_ce_referrals_spec.rb for detailed spec of that access field.
     <<~GRAPHQL
       query CeReferralAccess($id: ID!) {
         ceReferral(id: $id) {
           id
           access {
-            canViewReferralDetails
             canAssignReferralTasks
             canViewSourceEnrollmentDetails
             canViewTargetProject
@@ -41,40 +42,26 @@ RSpec.describe Hmis::GraphqlController, type: :request do
     result.dig('data', 'ceReferral', 'access')
   end
 
-  shared_examples 'expect ce referral access field' do |field, expected|
-    it "sets #{field} to #{expected.inspect}" do
-      expect(referral_access[field]).to eq(expected)
-    end
-  end
-
   describe 'CeReferral access fields' do
-    describe 'canViewReferralDetails' do
-      include_examples 'expect ce referral access field', 'canViewReferralDetails', true
-
-      context 'when the user can only view the referral summary' do
-        before { remove_permissions(ds_access_control, :can_view_referral_details) }
-        let!(:source_project) { create(:hmis_hud_project, data_source: ds1, user: u1, project_name: 'Source Project') }
-        let!(:source_enrollment) { create(:hmis_hud_enrollment, client: client, project: source_project, data_source: ds1) }
-        before { referral.update!(source_enrollment: source_enrollment) }
-        let!(:source_project_access_control) { create_access_control(hmis_user, source_project, with_permission: [:can_view_project, :can_manage_outgoing_referrals]) }
-
-        include_examples 'expect ce referral access field', 'canViewReferralDetails', false
-      end
-    end
-
     describe 'canAssignReferralTasks' do
-      include_examples 'expect ce referral access field', 'canAssignReferralTasks', false
+      it 'returns false' do
+        expect(referral_access['canAssignReferralTasks']).to be false
+      end
 
       context 'when the user can assign referral tasks' do
         before { add_permissions(ds_access_control, :can_assign_referral_tasks) }
 
-        include_examples 'expect ce referral access field', 'canAssignReferralTasks', true
+        it 'returns true' do
+          expect(referral_access['canAssignReferralTasks']).to be true
+        end
       end
     end
 
     describe 'canViewSourceEnrollmentDetails' do
       context 'without a source enrollment on the referral' do
-        include_examples 'expect ce referral access field', 'canViewSourceEnrollmentDetails', false
+        it 'returns false' do
+          expect(referral_access['canViewSourceEnrollmentDetails']).to be false
+        end
       end
 
       context 'with a source enrollment but the user cannot view enrollment details' do
@@ -83,20 +70,26 @@ RSpec.describe Hmis::GraphqlController, type: :request do
         before { referral.update!(source_enrollment: source_enrollment) }
 
         context 'when the user cannot view enrollment details' do
-          include_examples 'expect ce referral access field', 'canViewSourceEnrollmentDetails', false
+          it 'returns false' do
+            expect(referral_access['canViewSourceEnrollmentDetails']).to be false
+          end
         end
 
         context 'when the user can view enrollment details' do
           before { add_permissions(ds_access_control, :can_view_enrollment_details) }
 
-          include_examples 'expect ce referral access field', 'canViewSourceEnrollmentDetails', true
+          it 'returns true' do
+            expect(referral_access['canViewSourceEnrollmentDetails']).to be true
+          end
         end
       end
     end
 
     describe 'canViewTargetProject' do
       context 'when the user can view the target project' do
-        include_examples 'expect ce referral access field', 'canViewTargetProject', true
+        it 'returns true' do
+          expect(referral_access['canViewTargetProject']).to be true
+        end
       end
 
       context 'when user sees the referral via own-referral assignment, without project view' do
@@ -109,8 +102,10 @@ RSpec.describe Hmis::GraphqlController, type: :request do
           step.assignments.create!(user: hmis_user)
         end
 
-        include_examples 'expect ce referral access field', 'canViewTargetProject', false
-        include_examples 'expect ce referral access field', 'canViewReferralDetails', true
+        it 'returns false for canViewTargetProject and true for canViewReferralDetails' do
+          expect(referral_access['canViewTargetProject']).to be false
+          expect(referral_access['canViewReferralDetails']).to be true
+        end
       end
     end
 
@@ -124,7 +119,9 @@ RSpec.describe Hmis::GraphqlController, type: :request do
           create_access_control(hmis_user, ds1, with_permission: [:can_view_referrals, :can_perform_any_referral_tasks, :can_view_project])
         end
 
-        include_examples 'expect ce referral access field', 'canCreateReferralNote', true
+        it 'returns true' do
+          expect(referral_access['canCreateReferralNote']).to be true
+        end
       end
 
       context 'when user can perform own referral tasks' do
@@ -133,7 +130,9 @@ RSpec.describe Hmis::GraphqlController, type: :request do
         end
 
         context 'and user is not assigned' do
-          include_examples 'expect ce referral access field', 'canCreateReferralNote', false
+          it 'returns false' do
+            expect(referral_access['canCreateReferralNote']).to be false
+          end
         end
 
         context 'and user is assigned' do
@@ -142,7 +141,9 @@ RSpec.describe Hmis::GraphqlController, type: :request do
             referral.workflow_engine.assign_task!(referral.workflow_engine.active_steps.first)
           end
 
-          include_examples 'expect ce referral access field', 'canCreateReferralNote', true
+          it 'returns true' do
+            expect(referral_access['canCreateReferralNote']).to be true
+          end
         end
       end
 
@@ -151,7 +152,9 @@ RSpec.describe Hmis::GraphqlController, type: :request do
           create_access_control(hmis_user, ds1, with_permission: [:can_view_referrals, :can_view_project])
         end
 
-        include_examples 'expect ce referral access field', 'canCreateReferralNote', false
+        it 'returns false' do
+          expect(referral_access['canCreateReferralNote']).to be false
+        end
       end
     end
   end
