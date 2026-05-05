412c33fcdb45533dcb5d6bc20d54a259407fbb48
Theron Toomey <ttoomey@greenriver.org>
2026-05-05 01:15:59 +0000

    fix: 9131 wire staging table prune to new csv versions

--- drivers/hmis_csv_importer/spec/jobs/hmis_csv_importer/cleanup/expire_importers_job_spec.rb
+++ drivers/hmis_csv_importer/spec/jobs/hmis_csv_importer/cleanup/expire_importers_job_spec.rb
@@ -24,4 +24,29 @@ RSpec.describe HmisCsvImporter::Cleanup::ExpireImportersJob, type: :model do
     }
     HmisCsvImporter::Cleanup::ExpireImportersJob.new.perform(**default_options.merge(options))
   end
+
+  describe '#models' do
+    let(:job) { described_class.new }
+
+    it 'includes importer classes from all active versions (>= 2024)' do
+      models = job.send(:models)
+      expect(models).to include(HmisCsvTwentyTwentyFour::Importer::Organization)
+      expect(models).to include(HmisCsvTwentyTwentySix::Importer::Organization)
+    end
+
+    it 'excludes importer classes from frozen legacy versions (< 2024)' do
+      models = job.send(:models)
+      expect(models).not_to include(HmisCsvTwentyTwenty::Importer::Organization)
+      expect(models).not_to include(HmisCsvTwentyTwentyTwo::Importer::Organization)
+    end
+
+    it 'raises if an active-version module does not implement expiring_importer_classes' do
+      stub_module = Module.new
+      stub_const('StubDataLake2028', stub_module)
+      allow(Rails.application.config).to receive(:hmis_data_lakes).and_return(
+        Rails.application.config.hmis_data_lakes.merge('2028' => 'StubDataLake2028'),
+      )
+      expect { job.send(:models) }.to raise_error(/StubDataLake2028.*expiring_importer_classes/)
+    end
+  end
 end
--- drivers/hmis_csv_importer/spec/jobs/hmis_csv_importer/cleanup/expire_loaders_job_spec.rb
+++ drivers/hmis_csv_importer/spec/jobs/hmis_csv_importer/cleanup/expire_loaders_job_spec.rb
@@ -24,4 +24,29 @@ RSpec.describe HmisCsvImporter::Cleanup::ExpireLoadersJob, type: :model do
     }
     HmisCsvImporter::Cleanup::ExpireLoadersJob.new.perform(**default_options.merge(options))
   end
+
+  describe '#models' do
+    let(:job) { described_class.new }
+
+    it 'includes loader classes from all active versions (>= 2024)' do
+      models = job.send(:models)
+      expect(models).to include(HmisCsvTwentyTwentyFour::Loader::Organization)
+      expect(models).to include(HmisCsvTwentyTwentySix::Loader::Organization)
+    end
+
+    it 'excludes loader classes from frozen legacy versions (< 2024)' do
+      models = job.send(:models)
+      expect(models).not_to include(HmisCsvTwentyTwenty::Loader::Organization)
+      expect(models).not_to include(HmisCsvTwentyTwentyTwo::Loader::Organization)
+    end
+
+    it 'raises if an active-version module does not implement expiring_loader_classes' do
+      stub_module = Module.new
+      stub_const('StubDataLake2028', stub_module)
+      allow(Rails.application.config).to receive(:hmis_data_lakes).and_return(
+        Rails.application.config.hmis_data_lakes.merge('2028' => 'StubDataLake2028'),
+      )
+      expect { job.send(:models) }.to raise_error(/StubDataLake2028.*expiring_loader_classes/)
+    end
+  end
 end
