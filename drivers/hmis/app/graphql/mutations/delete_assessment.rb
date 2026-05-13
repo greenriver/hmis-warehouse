8d20352f97468ab656140d30568124ca64078dc3
martha <medwards@greenriver.org>
2026-05-13 09:17:52 -0400

    assume there will always be an enrollment for an assessment

--- drivers/hmis/app/graphql/mutations/delete_assessment.rb
+++ drivers/hmis/app/graphql/mutations/delete_assessment.rb
@@ -38,21 +38,19 @@ module Mutations
         record.form_processor.destroy_related_records!
 
         enrollment = record.enrollment
-        if enrollment.present?
 
-          # Deleting the Exit Assessment "un-exits" the client by deleting the Exit record,
-          # and moving the referral back to "accepted" status
-          enrollment.accept_referral!(current_user: current_user) if record.exit? && !is_wip
+        # Deleting the Exit Assessment "un-exits" the client by deleting the Exit record,
+        # and moving the referral back to "accepted" status
+        enrollment.accept_referral!(current_user: current_user) if record.exit? && !is_wip
 
-          # Deleting the Intake deletes the enrollment.
-          # Don't delete the enrollment if it has any other intakes besides this one. (Not allowed in HMIS, likely a data issue from import)
-          if record.intake? && !enrollment.intake_assessment&.present?
-            # If this is the HoH, delete all household enrollments to avoid leaving behind a household with no HoH
-            if enrollment.head_of_household?
-              destroy_household!(hoh_enrollment: enrollment)
-            else
-              enrollment.destroy!
-            end
+        # Deleting the Intake deletes the enrollment.
+        # Don't delete the enrollment if it has any other intakes besides this one. (Not allowed in HMIS, likely a data issue from import)
+        if record.intake? && !enrollment.intake_assessment.present?
+          # If this is the HoH, delete all household enrollments to avoid leaving behind a household with no HoH
+          if enrollment.head_of_household?
+            destroy_household!(hoh_enrollment: enrollment)
+          else
+            enrollment.destroy!
           end
         end
 
