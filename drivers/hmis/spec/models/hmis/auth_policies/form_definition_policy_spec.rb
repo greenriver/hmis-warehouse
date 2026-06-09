778ca982375b62f2badd69e81a8561ca4bee8823
martha <medwards@greenriver.org>
2026-06-09 17:22:04 -0400

    Use data-configuration policies, not direct permission checks (#6538)
    * Use policy pattern for root access query
    
    * fix system tests
    
    * changes to form definition policy and usages
    
    * changes to service types/categories
    
    * fix test failures
    
    * rm duplicative tests after merge
    
    * revert change from bad merge
    
    * revert unneeded behavior change when form is not found
    
    * rename for clarity
    
    * Restore comment

--- drivers/hmis/spec/models/hmis/auth_policies/form_definition_policy_spec.rb
+++ drivers/hmis/spec/models/hmis/auth_policies/form_definition_policy_spec.rb
@@ -5,165 +5,244 @@ require 'rails_helper'
 RSpec.describe Hmis::AuthPolicies::FormDefinitionPolicy, type: :model do
   let(:data_source) { create(:hmis_data_source) }
   let(:user) { create(:hmis_user, data_source: data_source) }
-  let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft') }
+  let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft', data_source: data_source) }
   let(:policy) { user.policy_for(form_definition, policy_type: :form_definition) }
   let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_view_clients]) }
 
-  shared_examples 'allows non-admin form roles' do
-    it 'returns true for SERVICE forms' do
-      form_definition.update(role: 'SERVICE')
-      expect(policy.can_manage_form?).to be true
-    end
+  describe 'Instance' do
+    describe '#can_manage_form? and other form management permissions' do
+      shared_examples 'returns false' do
+        it 'returns false' do
+          expect(policy.can_manage_form?).to be false
+          expect(policy.can_create_draft?).to be false
+          expect(policy.can_edit_draft?).to be false
+          expect(policy.can_publish?).to be false
+          expect(policy.can_delete?).to be false
+        end
+      end
 
-    it 'returns true for CUSTOM_ASSESSMENT forms' do
-      form_definition.update(role: 'CUSTOM_ASSESSMENT')
-      expect(policy.can_manage_form?).to be true
-    end
-  end
+      shared_examples 'returns true' do
+        it 'returns true' do
+          expect(policy.can_manage_form?).to be true
+          expect(policy.can_create_draft?).to be true
+          expect(policy.can_edit_draft?).to be true
+          expect(policy.can_publish?).to be true
+          expect(policy.can_delete?).to be true
+        end
+      end
 
-  shared_examples 'always blocks version controlled forms' do
-    it 'returns false for managed in version control forms' do
-      form_definition.update(managed_in_version_control: true)
-      expect(policy.can_manage_form?).to be false
-    end
-  end
+      context 'when the user does not have permission' do
+        include_examples 'returns false'
+      end
 
-  describe 'Instance#can_manage_form?' do
-    context 'when user has no permissions' do
-      it 'returns false' do
-        expect(policy.can_manage_form?).to be false
+      context 'when the user has permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection]) }
+        include_examples 'returns true'
+
+        context 'when the form is managed in version control' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft', managed_in_version_control: true, data_source: data_source) }
+          include_examples 'returns false'
+        end
+
+        context 'when the form is admin-editable-only' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft', admin_editable_only: true, data_source: data_source) }
+          include_examples 'returns false'
+        end
+
+        context 'when the form is a super-admin-only form role' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'CE_REFERRAL_STEP', status: 'draft', data_source: data_source) }
+          include_examples 'returns false'
+
+          context 'when the user has can_administrate_config permission' do
+            let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection, :can_administrate_config]) }
+            include_examples 'returns true'
+          end
+        end
+
+        context 'when the form is published' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'published', data_source: data_source) }
+
+          it 'disallows deletion' do
+            expect(policy.can_delete?).to be false
+          end
+        end
       end
     end
 
-    context 'when user has can_manage_forms but not can_administrate_config permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms]) }
-
-      include_examples 'allows non-admin form roles'
-      include_examples 'always blocks version controlled forms'
-
-      it 'returns false for system forms' do
-        form_definition.update(role: 'CLIENT')
-        expect(policy.can_manage_form?).to be false
+    describe '#can_duplicate?' do
+      it 'returns false when the user does not have permission' do
+        expect(policy.can_duplicate?).to be false
       end
 
-      it 'returns false for admin-editable-only forms' do
-        form_definition.update(admin_editable_only: true)
-        expect(policy.can_manage_form?).to be false
-      end
-    end
+      context 'when the user has permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection]) }
+        it 'returns true' do
+          expect(policy.can_duplicate?).to be true
+        end
 
-    context 'when user has can_administrate_config permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_administrate_config]) }
+        context 'when the form is managed in version control' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft', managed_in_version_control: true, data_source: data_source) }
+          it 'returns true' do
+            expect(policy.can_duplicate?).to be true
+          end
+        end
 
-      include_examples 'allows non-admin form roles'
-      include_examples 'always blocks version controlled forms'
+        context 'when the form is admin-editable-only' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'SERVICE', status: 'draft', admin_editable_only: true, data_source: data_source) }
+          it 'returns true' do
+            expect(policy.can_duplicate?).to be true
+          end
+        end
 
-      it 'returns true for system forms' do
-        form_definition.update(role: 'CLIENT')
-        expect(policy.can_manage_form?).to be true
-      end
+        context 'when the form is a super-admin-only form role' do
+          let(:form_definition) { create(:hmis_form_definition, role: 'CE_REFERRAL_STEP', data_source: data_source) }
+          it 'returns false' do
+            expect(policy.can_duplicate?).to be false
+          end
 
-      it 'returns true for admin-editable-only forms' do
-        form_definition.update(admin_editable_only: true)
-        expect(policy.can_manage_form?).to be true
-      end
-    end
-  end
-
-  describe 'Global#can_create?' do
-    let(:policy) { user.policy_for(Hmis::Form::Definition, policy_type: :form_definition) }
-    context 'when user has can_manage_forms permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms]) }
-      it 'returns true for non-super-admin form roles' do
-        expect(policy.can_create?(role: 'SERVICE')).to be true
-        expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be true
-      end
-
-      it 'returns false for super-admin form roles' do
-        expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be false
-        expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be false
-      end
-
-      it 'returns false when role is nil' do
-        expect(policy.can_create?(role: nil)).to be false
-      end
-      it 'returns false when role is invalid' do
-        expect(policy.can_create?(role: 'NOT_A_ROLE')).to be false
+          context 'when the user has can_administrate_config permission' do
+            let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection, :can_administrate_config]) }
+            it 'returns true' do
+              expect(policy.can_duplicate?).to be true
+            end
+          end
+        end
       end
     end
 
-    context 'when user has can_administrate_config permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_administrate_config]) }
-      it 'returns true for all form roles' do
-        expect(policy.can_create?(role: 'SERVICE')).to be true
-        expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be true
-        expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be true
-        expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be true
+    describe '#can_configure_form? and form rule management permissions' do
+      shared_examples 'returns false' do
+        it 'returns false' do
+          expect(policy.can_configure_form?).to be false
+          expect(policy.can_add_form_rule?).to be false
+          expect(policy.can_delete_form_rule?).to be false
+        end
       end
-    end
 
-    context 'when user has can_administrate_config but not can_manage_forms permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_administrate_config]) }
-
-      it 'returns false' do
-        expect(policy.can_create?(role: 'SERVICE')).to be false
-        expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be false
+      shared_examples 'returns true' do
+        it 'returns true' do
+          expect(policy.can_configure_form?).to be true
+          expect(policy.can_add_form_rule?).to be true
+          expect(policy.can_delete_form_rule?).to be true
+        end
       end
-    end
 
-    context 'when user does not have can_manage_forms permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_view_clients]) }
-      it 'returns false for all form roles' do
-        expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be false
-        expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be false
+      context 'when the user does not have permission' do
+        include_examples 'returns false'
       end
-    end
-  end
 
-  describe 'Global#can_manage_forms?' do
-    let(:policy) { user.policy_for(Hmis::Form::Definition, policy_type: :form_definition) }
-
-    it 'returns false when user does not have can_manage_forms permission' do
-      expect(policy.can_manage_forms?).to be false
-    end
-
-    context 'when user has can_manage_forms permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms]) }
-
-      it 'returns true' do
-        expect(policy.can_manage_forms?).to be true
+      context 'when the user has permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_configure_data_collection]) }
+        include_examples 'returns true'
       end
-    end
-  end
 
-  describe 'Global#can_manage_form_rules?' do
-    let(:policy) { user.policy_for(Hmis::Form::Definition, policy_type: :form_definition) }
+      context 'when the form is a super-admin-only form role' do
+        let(:form_definition) { create(:hmis_form_definition, role: 'CE_REFERRAL_STEP', data_source: data_source) }
+        include_examples 'returns false'
 
-    it 'returns false when user does not have can_configure_data_collection permission' do
-      expect(policy.can_manage_form_rules?).to be false
-    end
-
-    context 'when user has can_configure_data_collection permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_configure_data_collection]) }
-
-      it 'returns true' do
-        expect(policy.can_manage_form_rules?).to be true
+        context 'when the user has can_administrate_config permission' do
+          let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_configure_data_collection, :can_manage_forms, :can_administrate_config]) }
+          include_examples 'returns true'
+        end
       end
     end
   end
 
-  describe 'Global#can_administrate_config?' do
+  describe 'Global' do
     let(:policy) { user.policy_for(Hmis::Form::Definition, policy_type: :form_definition) }
 
-    it 'returns false when user does not have can_administrate_config permission' do
-      expect(policy.can_administrate_config?).to be false
+    describe '#can_create?' do
+      context 'when user has can_manage_forms permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection]) }
+        it 'returns true for non-super-admin form roles' do
+          expect(policy.can_create?(role: 'SERVICE')).to be true
+          expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be true
+        end
+
+        it 'returns false for super-admin form roles' do
+          expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be false
+          expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be false
+        end
+
+        it 'returns false when role is nil' do
+          expect(policy.can_create?(role: nil)).to be false
+        end
+
+        it 'returns false when role is invalid' do
+          expect(policy.can_create?(role: 'NOT_A_ROLE')).to be false
+        end
+      end
+
+      context 'when user has can_administrate_config permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection, :can_administrate_config]) }
+        it 'returns true for all form roles' do
+          expect(policy.can_create?(role: 'SERVICE')).to be true
+          expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be true
+          expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be true
+          expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be true
+        end
+      end
+
+      context 'when user has can_administrate_config but not can_manage_forms permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_administrate_config]) }
+
+        it 'returns false' do
+          expect(policy.can_create?(role: 'SERVICE')).to be false
+          expect(policy.can_create?(role: 'CE_REFERRAL_STEP')).to be false
+        end
+      end
+
+      context 'when user does not have can_manage_forms permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_view_clients]) }
+
+        it 'returns false for all form roles' do
+          expect(policy.can_create?(role: 'CUSTOM_ASSESSMENT')).to be false
+          expect(policy.can_create?(role: 'CURRENT_LIVING_SITUATION')).to be false
+        end
+      end
     end
 
-    context 'when user has can_administrate_config permission' do
-      let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_administrate_config]) }
+    describe '#can_manage_forms?' do
+      it 'returns false when user does not have can_manage_forms permission' do
+        expect(policy.can_manage_forms?).to be false
+      end
 
-      it 'returns true' do
-        expect(policy.can_administrate_config?).to be true
+      context 'when user has can_manage_forms permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_manage_forms, :can_configure_data_collection]) }
+
+        it 'returns true' do
+          expect(policy.can_manage_forms?).to be true
+        end
+      end
+    end
+
+    describe '#can_configure_forms? and #can_manage_form_rules?' do
+      it 'returns false when user does not have can_configure_data_collection permission' do
+        expect(policy.can_configure_forms?).to be false
+        expect(policy.can_manage_form_rules?).to be false
+      end
+
+      context 'when user has can_configure_data_collection permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_configure_data_collection]) }
+
+        it 'returns true' do
+          expect(policy.can_configure_forms?).to be true
+          expect(policy.can_manage_form_rules?).to be true
+        end
+      end
+    end
+
+    describe '#can_administrate_config?' do
+      it 'returns false when user does not have can_administrate_config permission' do
+        expect(policy.can_administrate_config?).to be false
+      end
+
+      context 'when user has can_administrate_config permission' do
+        let!(:access_control) { create_access_control(user, data_source, with_permission: [:can_administrate_config, :can_manage_forms, :can_configure_data_collection]) }
+
+        it 'returns true' do
+          expect(policy.can_administrate_config?).to be true
+        end
       end
     end
   end
