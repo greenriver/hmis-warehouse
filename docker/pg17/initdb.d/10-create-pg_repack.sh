161f74ca5b4123e351a0a3240ade6a1b20ca057b
Elliot <elliot@greenriver.org>
2026-06-17 16:52:07 -0400

    [#8738] Copyright in DB directory (#6616)
    * Copyright in DB directory
    
    * Adjust file setup for copyright
    
    * Additional missed files
    
    * Rubocop fixes
    
    * Another frozen string literal

--- docker/pg17/initdb.d/10-create-pg_repack.sh
+++ docker/pg17/initdb.d/10-create-pg_repack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 ###
-# Copyright 2016 - 2025 Green River Data Analysis, LLC
+# Copyright Green River Data Group, Inc.
 #
 # License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
 ###
+
 set -euo pipefail
 
 # Runs only at first-time cluster initialization
@@ -12,5 +14,3 @@ databases=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres -Atc
 for db in $databases; do
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION IF NOT EXISTS pg_repack WITH SCHEMA public VERSION '1.5.3';"
 done
-
-
--- lib/tasks/jobs.rake
+++ lib/tasks/jobs.rake
@@ -1,3 +1,9 @@
+###
+# Copyright Green River Data Group, Inc.
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
 namespace :jobs do
   desc "Check delayed job queue for hung jobs"
   task check_queue: [:environment] do
