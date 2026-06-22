00b979e7e97e6f42f4cae549230e7764ccb7d67d
martha <medwards@greenriver.org>
2026-06-22 15:14:17 -0400

    clean up to be more generic

---
+++ drivers/hmis/app/graphql/types/hmis_schema/has_ce_match_rules.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Types
+  module HmisSchema
+    module HasCeMatchRules
+      extend ActiveSupport::Concern
+
+      included do
+        field :effective_ce_match_rule_count, Integer, null: false, description: 'Number of CE match rules that apply to this record, including inherited rules.'
+        field :local_ce_match_rule_count, Integer, null: false, description: 'Number of CE match rules owned directly by this record.'
+        field :effective_ce_match_rule_groups, [HmisSchema::CeMatchRuleGroup], null: false, description: 'All CE match rules that apply to this record, including inherited rules, grouped by their owner.'
+      end
+
+      def local_ce_match_rule_count
+        access_denied! unless ce_match_rule_policy.can_manage?
+
+        dataloader.with(Sources::CeMatchRuleOwnerCountSource, owner_type: object.class.sti_name).load(object.id)
+      end
+
+      def effective_ce_match_rule_count
+        access_denied! unless ce_match_rule_policy.can_manage?
+
+        dataloader.with(Sources::CeMatchRuleEffectiveCountSource, owner_class: object.class).load(object)
+      end
+
+      # Not for batch
+      def effective_ce_match_rule_groups
+        access_denied! unless ce_match_rule_policy.can_manage?
+
+        rules_by_owner = Hmis::Ce::Match::Rule.eligibility_and_priority_rules_for_entity(object).
+          group_by { |rule| [rule.owner_type, rule.owner_id] }
+
+        ce_match_rule_group_owners.map do |owner|
+          OpenStruct.new(
+            owner: owner,
+            rules: rules_by_owner.fetch([owner.class.sti_name, owner.id], []),
+            local: owner == object,
+          )
+        end
+      end
+
+      private
+
+      def ce_match_rule_group_owners
+        # Implement in the including class
+        raise NotImplementedError
+      end
+
+      def ce_match_rule_policy
+        @ce_match_rule_policy ||= policy_for(Hmis::Ce::Match::Rule, policy_type: :ce_match_rule)
+      end
+    end
+  end
+end
--- drivers/hmis/app/graphql/types/hmis_schema/organization.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/organization.rb
@@ -11,6 +11,7 @@ module Types
     include Types::HmisSchema::HasProjects
     include Types::HmisSchema::HasCustomDataElements
     include Types::HmisSchema::HasHudMetadata
+    include Types::HmisSchema::HasCeMatchRules
 
     def self.configuration
       Hmis::Hud::Organization.hmis_configuration(version: '2024')
@@ -24,9 +25,6 @@ module Types
     field :id, ID, null: false
     field :hud_id, ID, null: false, method: :organization_id
     field :organization_name, String, null: false
-    field :effective_ce_match_rule_count, Integer, null: false, description: 'Number of rules that apply to this organization, including inherited rules.'
-    field :local_ce_match_rule_count, Integer, null: false, description: 'Number of rules owned directly by this organization.'
-    field :effective_ce_match_rule_groups, [HmisSchema::CeMatchRuleGroup], null: false
     projects_field :projects, filter_args: { omit: [:organization, :ce_enabled], type_name: 'ProjectsForEnrollment' }
     field :victim_service_provider, HmisSchema::Enums::Hud::NoYesMissing, null: false, default_value: 99
     field :description, String, null: true
@@ -44,49 +42,15 @@ module Types
       resolve_projects(object.projects, **args)
     end
 
-    def local_ce_match_rule_count
-      access_denied! unless ce_match_rule_policy.can_manage?
-
-      dataloader.with(Sources::CeMatchRuleOwnerCountSource, owner_type: Hmis::Hud::Organization.sti_name).load(object.id)
-    end
-
-    def effective_ce_match_rule_count
-      access_denied! unless ce_match_rule_policy.can_manage?
-
-      dataloader.with(Sources::CeMatchRuleEffectiveCountSource, owner_class: Hmis::Hud::Organization).load(object)
-    end
-
-    # Not for batch
-    def effective_ce_match_rule_groups
-      access_denied! unless ce_match_rule_policy.can_manage?
-
-      rules_by_owner = effective_ce_match_rules.group_by { |rule| [rule.owner_type, rule.owner_id] }
-      [
-        ce_match_rule_group(owner: object.data_source, rules_by_owner: rules_by_owner, local: false),
-        ce_match_rule_group(owner: object, rules_by_owner: rules_by_owner, local: true),
-      ]
-    end
-
     def self.organizations(scope = Hmis::Hud::Organization.all, user:)
       scope.viewable_by(user)
     end
 
     private
 
-    def effective_ce_match_rules
-      @effective_ce_match_rules ||= Hmis::Ce::Match::Rule.eligibility_and_priority_rules_for_entity(object)
-    end
-
-    def ce_match_rule_policy
-      @ce_match_rule_policy ||= policy_for(Hmis::Ce::Match::Rule, policy_type: :ce_match_rule)
-    end
-
-    def ce_match_rule_group(owner:, rules_by_owner:, local:)
-      OpenStruct.new(
-        owner: owner,
-        rules: rules_by_owner.fetch([owner.class.sti_name, owner.id], []),
-        local: local,
-      )
+    # Used by the HasCeMatchRules concern
+    def ce_match_rule_group_owners
+      [object.data_source, object]
     end
   end
 end
