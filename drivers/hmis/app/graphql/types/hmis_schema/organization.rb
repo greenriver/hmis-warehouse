a629cae24e72bcb528932c2b9650639ed0ae5eda
martha <medwards@greenriver.org>
2026-06-26 15:02:39 -0400

    Add Organization match rules pages (#6640)
    * First pass at working client and definition/cded queries for picklists
    
    * clean up repeated code
    
    * add basic spec
    
    * use new CeMatchField schema type instead of fake FormItems
    
    * separate form definitions and fields into 2 queries
    
    * resolve todo related to permissions/policies
    
    * self-review ,move things around
    
    * rearrange
    
    * fix perms in spec
    
    * Add project details to unit group warning
    
    * further updates to unit group warning data shape
    
    * first pass global rules queries
    
    * make ce match rules query more generic, filterable
    
    * fix tests
    
    * working draft, needs more thought
    
    * refactor
    
    * respond to more PR comments
    
    * remove labels added by ai
    
    * Fix test
    
    * add can_view to ce match rule policy
    
    * clean up filtering a bit
    
    * fix bad merge
    
    * rename ...Owner to ...OwnerType
    
    * first pass at org rules
    
    * fix perf problem
    
    * clean up to be more generic
    
    * update description
    
    Co-authored-by: Gig <gig@greenriver.org>
    
    * update description
    
    Co-authored-by: Gig <gig@greenriver.org>
    
    * schema update for descriptions
    
    * support project filtering and org unit group counts
    
    * ai review
    
    * revert change that caused graphql problems
    
    * avoid n+1
    
    Co-authored-by: Gig <gig@greenriver.org>
    
    * update global filter on root ce match rule query
    
    * Omit priority schemes for now
    
    * self-review
    
    * stop paginating group rules
    
    ---------
    
    Co-authored-by: Gig <gig@greenriver.org>

--- drivers/hmis/app/graphql/types/hmis_schema/organization.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/organization.rb
@@ -11,6 +11,7 @@ module Types
     include Types::HmisSchema::HasProjects
     include Types::HmisSchema::HasCustomDataElements
     include Types::HmisSchema::HasHudMetadata
+    include Types::HmisSchema::HasCeMatchRules
 
     def self.configuration
       Hmis::Hud::Organization.hmis_configuration(version: '2024')
@@ -18,11 +19,13 @@ module Types
 
     available_filter_options do
       arg :search_term, String
+      arg :ce_waitlists_enabled, Boolean
     end
 
     field :id, ID, null: false
     field :hud_id, ID, null: false, method: :organization_id
     field :organization_name, String, null: false
+    field :ce_waitlist_unit_group_count, Integer, null: false, description: 'Number of unit groups under this organization in projects that have waitlist referrals enabled.'
     projects_field :projects, filter_args: { omit: [:organization, :ce_enabled], type_name: 'ProjectsForEnrollment' }
     field :victim_service_provider, HmisSchema::Enums::Hud::NoYesMissing, null: false, default_value: 99
     field :description, String, null: true
@@ -40,8 +43,21 @@ module Types
       resolve_projects(object.projects, **args)
     end
 
+    def ce_waitlist_unit_group_count
+      # Doesn't need to be separately authorized with ce_match_rule_policy.can_manage?
+      # because the unit group count isn't sensitive. If you can see the org, you can request this field
+      load_ar_association(object, :ce_waitlist_unit_groups).size
+    end
+
     def self.organizations(scope = Hmis::Hud::Organization.all, user:)
       scope.viewable_by(user)
+    end
+
+    private
+
+    # Used by the HasCeMatchRules concern
+    def ce_match_rule_group_owners
+      [object.data_source, object]
     end
   end
 end
