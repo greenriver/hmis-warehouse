aa45a809f40a466f0a2471e7bda828341d0e97ae
Theron Toomey <ttoomey@greenriver.org>
2026-07-05 11:35:14 -0400

    Update app/controllers/user_training_controller.rb
    Co-authored-by: Elliot <eanders@greenriver.org>

--- app/controllers/user_training_controller.rb
+++ app/controllers/user_training_controller.rb
@@ -48,7 +48,7 @@ class UserTrainingController < ApplicationController
           # Ensure the user is enrolled in the course
           lms.enroll(config, course_id)
 
-          # reset progress if course is expired (check against API competition date)
+          # reset progress if course is expired (check against API completion date)
           lms.reset_user_progress(config, course_id) if lms.training_expired?(config, course_id)
 
           completed_on = lms.complete?(config, course_id)
