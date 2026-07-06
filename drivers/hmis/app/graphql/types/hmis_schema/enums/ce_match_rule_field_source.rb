a5a7a010882ae93cf129a2fb36d646751190f927
martha <medwards@greenriver.org>
2026-07-06 10:43:58 -0400

    Include user-facing label in Field Source enum

--- drivers/hmis/app/graphql/types/hmis_schema/enums/ce_match_rule_field_source.rb
+++ drivers/hmis/app/graphql/types/hmis_schema/enums/ce_match_rule_field_source.rb
@@ -10,7 +10,16 @@ module Types
   class HmisSchema::Enums::CeMatchRuleFieldSource < Types::BaseEnum
     graphql_name 'CeMatchRuleFieldSource'
 
-    value 'CLIENT', description: 'A client field, such as current_age'
-    value 'CUSTOM_DATA_ELEMENT', description: 'A custom data element, such as cde.custom_assessment.my_score'
+    # Client fields, such as current_age
+    value 'CLIENT', 'Client'
+
+    # Custom data element fields, such as cde.custom_assessment.my_score.
+    # Not using "Custom Assessment" field as the user-facing label,
+    # because we will also expose CDEs with other ownership types (e.g. custom field on Client or Enrollment)
+    value 'CUSTOM_DATA_ELEMENT', 'Custom'
+
+    # Future choices:
+    # value 'PSDE', 'HUD Program Specific Data Element' # or "HUD Assessment", but we will likely include non-assessment-based PSDEs (like move-in date) in the same namepsace
+    # value 'HOUSEHOLD', 'Household' # e.g. household size, youngest member age, etc.
   end
 end
