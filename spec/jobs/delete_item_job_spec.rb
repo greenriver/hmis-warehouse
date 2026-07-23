5824db318018b0243432c67270f37ccfd3b36301
Dave Greiner <dgreiner@greenriver.org>
2026-07-23 09:29:32 -0400

    Delete system collections when the owning entity is deleted
    Cohorts, project groups, and data sources create system collections and related access records. Those records were not removed when the entity was deleted. Add EntityAccess#remove_system_collections! and call it from cohort delete, project group delete (including bulk delete), and data source dependent cleanup in DeleteItemJob. Run cleanup after the entity delete commits. Rescue and log errors so cleanup does not block deletion. Keep the existing nightly cleanup task.

---
+++ spec/jobs/delete_item_job_spec.rb
@@ -0,0 +1,26 @@
+###
+# Copyright Green River Data Group, Inc.
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DeleteItemJob, type: :job do
+  describe '#perform for a DataSource' do
+    let(:data_source) { create(:grda_warehouse_data_source) }
+
+    it 'destroys the data source and removes its system collection and access control' do
+      access_control = data_source.editable_access_control # data sources support editable only
+      collection = access_control.collection
+
+      described_class.new.perform(item_class: 'GrdaWarehouse::DataSource', item_id: data_source.id)
+
+      expect(GrdaWarehouse::DataSource.find_by(id: data_source.id)).to be_nil
+      expect(Collection.find_by(id: collection.id)).to be_nil
+      expect(AccessControl.find_by(id: access_control.id)).to be_nil
+    end
+  end
+end
