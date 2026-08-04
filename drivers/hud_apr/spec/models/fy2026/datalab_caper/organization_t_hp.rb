74227e0d6e526982baf5a2c10f6dd0babd5ac393
Dave Greiner <dgreiner@greenriver.org>
2026-08-04 09:22:05 -0400

    audit and cleanup skipped tests

--- drivers/hud_apr/spec/models/fy2026/datalab_caper/organization_t_hp.rb
+++ drivers/hud_apr/spec/models/fy2026/datalab_caper/organization_t_hp.rb
@@ -52,15 +52,11 @@ RSpec.shared_context 'datalab organization t hp caper', shared_context: :metadat
       )
     end
 
-    # pending https://www.hudexchange.info/program-support/my-question/?askaquestionaction=public%3Amain.answer&key=20077029-802F-4997-903A87560D233223
+    # previous https://www.hudexchange.info/program-support/my-question/?askaquestionaction=public%3Amain.answer&key=20077029-802F-4997-903A87560D233223
     it 'Q5a' do
       compare_results(
         file_path: result_file_prefix + results_dir,
         question: 'Q5a',
-        skip: [
-          'B17', # expected '1.0000' (1), got '0.0000' (0)
-          'C17', # expected '1.0000' (1), got '0.0000' (0)
-        ],
       )
     end
 
