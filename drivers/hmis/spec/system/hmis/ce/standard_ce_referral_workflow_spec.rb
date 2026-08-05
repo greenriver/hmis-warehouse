c60cb18d705336d603faa8537ee931cbcc05ff8e
martha <medwards@greenriver.org>
2026-08-05 16:20:35 -0400

    seed demo forms in test

--- drivers/hmis/spec/system/hmis/ce/standard_ce_referral_workflow_spec.rb
+++ drivers/hmis/spec/system/hmis/ce/standard_ce_referral_workflow_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature 'Standard CE Referral Workflow', type: :system do
       # Seed the CE Referral Step forms.
       # In the test env, JsonForms defaults to env_key: 'test', which picks up the Standard
       # workflow forms in lib/form_data/test/ce_referral_steps.
-      HmisUtil::JsonForms.new(data_source_id: ds1.id, generate_cdeds: true).
+      HmisUtil::JsonForms.new(data_source_id: ds1.id, env_key: 'demo', generate_cdeds: true).
         seed_record_form_definitions(roles: [:CE_REFERRAL_STEP])
       CeWorkflows::Shared::CeBuilderUtils.create_state_machine_custom_statuses(ds1)
 
