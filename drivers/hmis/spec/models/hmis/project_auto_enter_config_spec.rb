63df4b155f24495d0d8f07339073c52a9c051cbb
martha <medwards@greenriver.org>
2026-08-12 13:16:45 -0400

    update code and spec

--- drivers/hmis/spec/models/hmis/project_auto_enter_config_spec.rb
+++ drivers/hmis/spec/models/hmis/project_auto_enter_config_spec.rb
@@ -40,12 +40,12 @@ RSpec.describe Hmis::ProjectAutoEnterConfig, type: :model do
     expect(config).to be_nil
   end
 
-  it 'should return nil if an auto-enter config exists, but is not enabled' do
+  it 'should return nil if an auto-enter config exists, but is soft-deleted' do
     auto_enter_config = Hmis::ProjectAutoEnterConfig.create!(project: p1, data_source: ds1)
     config = Hmis::ProjectAutoEnterConfig.detect_best_config_for_project(p1)
     expect(config).not_to be_nil
-    auto_enter_config.enabled = false
-    auto_enter_config.save!
+    auto_enter_config.destroy!
+    expect(auto_enter_config.reload.deleted_at).not_to be_nil
     config = Hmis::ProjectAutoEnterConfig.detect_best_config_for_project(p1)
     expect(config).to be_nil
   end
