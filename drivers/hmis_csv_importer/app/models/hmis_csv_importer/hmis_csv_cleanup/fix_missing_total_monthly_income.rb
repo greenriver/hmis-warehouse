78aed41c96d5449f2dbe49df131eef060704e67c
martha <medwards@greenriver.org>
2026-08-19 12:16:25 -0400

    Add importer extension to fix missing TotalMonthlyIncome (#6871)
    * reviewed so far
    
    * finish first pass
    
    * clean up
    
    * resolve todos
    
    * update comments, docstring
    
    * remove unused test
    
    * refactor
    
    * renames
    
    * add spec coverage
    
    * revert unneeded change
    
    * update for legibility
    
    * fix console helper - don't restrict to hmis only
    
    * respond to PR feedback

---
+++ drivers/hmis_csv_importer/app/models/hmis_csv_importer/hmis_csv_cleanup/fix_missing_total_monthly_income.rb
@@ -0,0 +1,44 @@
+###
+# Copyright Green River Data Group, Inc.
+#
+# License detail: https://github.com/greenriver/hmis-warehouse/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
+module HmisCsvImporter::HmisCsvCleanup
+  # Opt-in, off-by-default pre-ingest cleanup that fills in a missing TotalMonthlyIncome by summing the
+  # individual income source amounts.
+  # Only touches staging rows for the current importer_log where the record
+  # indicates income (IncomeFromAnySource = 1) but has a null TotalMonthlyIncome.
+  # Does not fix records where the TotalMonthlyIncome disagrees with the individual income source amounts.
+  class FixMissingTotalMonthlyIncome < Base
+    def cleanup!
+      Hmis::Hud::DataIntegrity::MissingTotalMonthlyIncomeFixer.run!(
+        scope: income_benefit_scope,
+        conflict_target: conflict_target(income_benefit_source),
+      )
+    end
+
+    def income_benefit_scope
+      income_benefit_source.where(importer_log_id: @importer_log.id)
+    end
+
+    def income_benefit_source
+      importable_file_class('IncomeBenefit')
+    end
+
+    def self.description
+      'When TotalMonthlyIncome is null, fill it in by summing income source amounts. ' \
+      'Does not change TotalMonthlyIncome when it is already present, even if it disagrees with the source amounts.'
+    end
+
+    def self.enable
+      {
+        import_cleanups: {
+          'IncomeBenefit': ['HmisCsvImporter::HmisCsvCleanup::FixMissingTotalMonthlyIncome'],
+        },
+      }
+    end
+  end
+end
