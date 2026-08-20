d3d8a8b0e652d47a9b6ff903ff11d82b02466f7d
Elliot <eanders@greenriver.org>
2026-08-19 20:10:43 -0400

    Additional test cleanup

--- drivers/hmis_csv_importer/spec/models/loader/hud_key_remapper_spec.rb
+++ drivers/hmis_csv_importer/spec/models/loader/hud_key_remapper_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe HmisCsvImporter::Loader::HudKeyRemapper, type: :model do
   end
 
   describe 'the admin-extension interface' do
-    it 'is checked only when pre_process_hooks[remap_hud_keys] is set' do
+    it 'is checked only when pre_process_hooks[described_class.name] is set' do
       data_source = create(:data_source, pre_process_hooks: { 'HmisCsvImporter::Loader::HudKeyRemapper' => true })
       expect(described_class.checked?(data_source)).to eq(true)
       expect(described_class.checked?(create(:data_source))).to eq(false)
